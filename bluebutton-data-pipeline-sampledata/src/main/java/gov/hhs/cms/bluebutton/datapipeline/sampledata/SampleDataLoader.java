cf7acd87f28a0a02b74a4e4454c700f3f0dd6424
Karl M. Davis <karl@justdavis.com>
2016-03-01 15:02:58 -0500

    Work in Progress: Started stubbing out some of the app code.
    Committing now to move work to other laptop.
    
    CBBD-10

---
+++ bluebutton-data-pipeline-sampledata/src/main/java/gov/hhs/cms/bluebutton/datapipeline/sampledata/SampleDataLoader.java
@@ -0,0 +1,10 @@
+package gov.hhs.cms.bluebutton.datapipeline.sampledata;
+
+/**
+ * Loads sample data into the specified database.
+ */
+public final class SampleDataLoader {
+	public SampleDataSummary loadSampleData(){
+		
+	}
+}
---
+++ bluebutton-data-pipeline-sampledata/src/main/java/gov/hhs/cms/bluebutton/datapipeline/sampledata/SampleDataSummary.java
@@ -0,0 +1,8 @@
+package gov.hhs.cms.bluebutton.datapipeline.sampledata;
+
+/**
+ * Stores a summary of the results of a data load operation.
+ */
+public final class SampleDataSummary {
+	
+}
