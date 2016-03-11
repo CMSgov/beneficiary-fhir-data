0c61919bb1c6b3b1503cbfa7e049b669b4037449
Karl M. Davis <karl@justdavis.com>
2016-03-11 12:31:24 -0500

    Made the CurrentBenficiary relationships with claims bidirectional.
    This will be needed to ensure that beneficiaries can be extracted from
    the CCW as a rooted object graph that includes all of their related
    claims.

--- bluebutton-data-pipeline-ccw/src/main/java/gov/hhs/cms/bluebutton/datapipeline/ccw/jdo/CurrentBeneficiary.java
+++ bluebutton-data-pipeline-ccw/src/main/java/gov/hhs/cms/bluebutton/datapipeline/ccw/jdo/CurrentBeneficiary.java
@@ -1,8 +1,12 @@
 package gov.hhs.cms.bluebutton.datapipeline.ccw.jdo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.jdo.annotations.Column;
+import javax.jdo.annotations.Extension;
+import javax.jdo.annotations.Order;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
@@ -21,6 +25,10 @@ public class CurrentBeneficiary {
 	@Persistent
 	@Column(name = "BENE_BIRTH_DT", allowsNull = "true")
 	private LocalDate birthDate;
+
+	@Persistent(mappedBy = "beneficiary")
+	@Order(extensions = @Extension(vendorName = "datanucleus", key = "list-ordering", value = "beneficiary ASC"))
+	private List<PartAClaimFact> partAClaimFacts = new ArrayList<>();
 
 	/**
 	 * Constructs a new {@link CurrentBeneficiary} instance.
@@ -60,5 +68,13 @@ public class CurrentBeneficiary {
 	public CurrentBeneficiary setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
 		return this;
+	}
+
+	/**
+	 * @return the {@link PartAClaimFact}s associated with this
+	 *         {@link CurrentBeneficiary}
+	 */
+	public List<PartAClaimFact> getPartAClaimFacts() {
+		return partAClaimFacts;
 	}
 }
