41f02ae416f63156b079676eda888633009f191d
Shaun Brockhoff <shaun.brockhoff@gdit.com>
2016-07-01 15:24:27 -0500

    Update comment structure to match the carrier branch.
    CBBD-90

--- bluebutton-data-pipeline-rif/src/main/java/gov/hhs/cms/bluebutton/datapipeline/rif/model/PartDEventRow.java
+++ bluebutton-data-pipeline-rif/src/main/java/gov/hhs/cms/bluebutton/datapipeline/rif/model/PartDEventRow.java
@@ -5,7 +5,24 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 /**
- * Models rows from <code>PDE</code> RIF Files.
+ * <p>
+ * Models rows from {@link RifFileType#PDE} RIF Files.
+ * </p>
+ * <p>
+ * The RIF file layout used here is specific to the Blue Button API's ETL
+ * process. The layouts of these files is detailed in the
+ * <code>bluebutton-data-pipeline-rif/dev/rif-record-layout.xlsx</code> file.
+ * The columns contained in the files are largely similar to those detailed in
+ * <a href="https://www.ccwdata.org/web/guest/data-dictionaries">CCW: Data
+ * Dictionaries</a>.
+ * </p>
+ * 
+ * <p>
+ * Design Note: This class is too painful to maintain as a bean, so I've
+ * stripped it down to just a struct. To be clear, this is <strong>not</strong>
+ * immutable and thus <strong>not</strong> thread-safe (and it really shouldn't
+ * need to be).
+ * </p>
  */
 public class PartDEventRow {
 	public static final int COMPOUND_CODE_NOT_COMPOUNDED = 1;
@@ -19,160 +36,101 @@ public class PartDEventRow {
 	public static final String PRSCRBR_ID_QLFYR_CD_NPI = "01";
 
 	/**
-	 * FIXME is this the schema version or the record version?
-	 * 
 	 * @see Column#VERSION
 	 */
 	public int version;
 
 	/**
-	 * Indicates whether the record for the row is an insert, update, or delete.
-	 * 
 	 * @see Column#DML_IND
 	 */
 	public RecordAction recordAction;
 
 	/**
-	 * The CCW's assigned ID for this Part D Event.
-	 * 
 	 * @see Column#PDE_ID
 	 */
 	public String partDEventId;
 
 	/**
-	 * The CCW's assigned ID for the beneficiary this Part D Event is linked to.
-	 * 
 	 * @see Column#BENE_ID
 	 */
 	public String beneficiaryId;
 
 	/**
-	 * The date that the prescription was filled.
-	 * 
 	 * @see Column#SRVC_DT
 	 */
 	public LocalDate prescriptionFillDate;
 
 	/**
-	 * The date that the plan paid the pharmacy for the prescription.
-	 * 
 	 * @see Column#PD_DT
 	 */
 	public Optional<LocalDate> paymentDate;
 
 	/**
-	 * The two-letter code that represents the type of pharmacy provider
-	 * identifier that is used in the {@link #serviceProviderId} field.
-	 * 
 	 * @see Column#SRVC_PRVDR_ID_QLFYR_CD
 	 */
 	public String serviceProviderIdQualiferCode;
 
 	/**
-	 * The ID of the pharmacy or physicians' office that dispensed the drug, the
-	 * type of which is described by the {@link #serviceProviderIdQualiferCode}
-	 * field.
-	 * 
 	 * @see Column#SRVC_PRVDR_ID
 	 */
 	public String serviceProviderId;
 
 	/**
-	 * The two-letter code that represents the type of prescribe identifier that
-	 * is used in the {@link #prescriberId} field.
-	 * 
 	 * @see Column#PRSCRBR_ID_QLFYR_CD
 	 */
 	public String prescriberIdQualifierCode;
 
 	/**
-	 * The ID of the practitioner that prescribed the drug, the type of which is
-	 * described by the {@link #prescriberIdQualifierCode} field.
-	 * 
 	 * @see Column#PRSCRBR_ID
 	 */
 	public String prescriberId;
 
 	/**
-	 * The prescription reference number assigned by the pharmacy at the time
-	 * the prescription is filled.
-	 * 
 	 * @see Column#RX_SRVC_RFRNC_NUM
 	 */
 	public Long prescriptionReferenceNumber;
 
 	/**
-	 * The 11-digit code representing the drug using the National Drug Code
-	 * (NDC).
-	 * 
 	 * @see Column#PROD_SRVC_ID
 	 */
 	public String nationalDrugCode;
 
 	/**
-	 * The ID representing the latest contract assigned to the beneficiary at
-	 * the time of payment reconciliation. Note the first letter of the ID is a
-	 * letter representing the type of plan.
-	 * 
 	 * @see Column#PLAN_CNTRCT_REC_ID
 	 */
 	public String planContractId;
 
 	/**
-	 * The ID representing the latest benefit package the beneficiary was
-	 * enrolled in for the year.
-	 * 
 	 * @see Column#PLAN_PBP_REC_NUM
 	 */
 	public String planBenefitPackageId;
 
 	/**
-	 * Two-digit code representing whether the dispensed drug was compounded or
-	 * mixed.
-	 * 
 	 * @see Column#CMPND_CD
 	 */
 	public Integer compoundCode;
 
 	/**
-	 * One-digit code representing the prescriber's instructions regarding
-	 * substitution of generic equivalents or the order to dispense the specific
-	 * prescribed medication.
-	 * 
 	 * @see Column#DAW_PROD_SLCTN_CD
 	 */
 	public String dispenseAsWrittenProductSelectionCode;
 
 	/**
-	 * The number of units, grams, milligrams, or other dispensed in the current
-	 * drug event. For compounded items, the quantity dispensed is the total of
-	 * all ingredients. (Note: After some research, it does not appear there is
-	 * another field that specifies what unit this is.)
-	 * 
 	 * @see Column#QTY_DPSNSD_NUM
 	 */
 	public BigDecimal quantityDispensed;
 
 	/**
-	 * The number of days' supply of medication dispensed, entered by the
-	 * pharmacy.
-	 * 
 	 * @see Column#DAYS_SUPLY_NUM
 	 */
 	public Integer daysSupply;
 
 	/**
-	 * The number fill of the currently dispensed supply, with 0 being the
-	 * original fill and 1-99 representing each refill.
-	 * 
 	 * @see Column#FILL_NUM
 	 */
 	public Integer fillNumber;
 
 	/**
-	 * One-digit code representing how the pharmacy dispensed the complete
-	 * quantity of the prescription.
-	 * 
 	 * @see Column#DSPNSNG_STUS_CD
 	 */
 	public Optional<Character> dispensingStatuscode;
@@ -247,118 +205,132 @@ public class PartDEventRow {
 		DML_IND,
 
 		/**
-		 * Type: <code>CHAR</code>, max chars: 15
+		 * Type: <code>CHAR</code>, max chars: 15. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/pde_id.txt">
+		 * CCW Data Dictionary: PDE_ID</a>, though note that this instance of +
+		 * * the field is unencrypted.
 		 */
 		PDE_ID,
 
 		/**
-		 * Type: <code>CHAR</code>, max chars: 15
+		 * Type: <code>CHAR</code>, max chars: 15. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/bene_id.txt">
+		 * CCW Data Dictionary: BENE_ID</a>, though note that this instance of +
+		 * * the field is unencrypted.
 		 */
 		BENE_ID,
 
 		/**
-		 * Type: <code>DATE</code>, max chars: 8
+		 * Type: <code>DATE</code>, max chars: 8. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/srvc_dt.txt">
+		 * CCW Data Dictionary: SRVC_DT</a>.
 		 */
 		SRVC_DT,
 
 		/**
-		 * Type: <code>DATE</code>, max chars: 8, <code>NULLABLE</code>
+		 * Type: <code>DATE</code>, max chars: 8, <code>Optional</code>. See
+		 * <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/pd_dt.txt">
+		 * CCW Data Dictionary: PD_DT</a>.
 		 */
 		PD_DT,
 
 		/**
-		 * Type: <code>CHAR</code>, max chars: 2, <code>NULLABLE</code>
-		 * 
-		 * @see <a href=
-		 *      "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/srvc_prvdr_id_qlfyr_cd.txt">
-		 *      Data Dictionary</a>
+		 * Type: <code>CHAR</code>, max chars: 2. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/srvc_prvdr_id_qlfyr_cd.txt">
+		 * CCW Data Dictionary: SRVC_PRVDR_ID_QLFYR_CD</a>.
 		 */
 		SRVC_PRVDR_ID_QLFYR_CD,
 
 		/**
-		 * Type: <code>CHAR</code>, max chars: 15
+		 * Type: <code>CHAR</code>, max chars: 15. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/srvc_prvdr_id.txt">
+		 * CCW Data Dictionary: SRVC_PRVDR_ID</a>.
 		 */
 		SRVC_PRVDR_ID,
 
 		/**
-		 * Type: <code>CHAR</code>, max chars: 2, <code>NULLABLE</code>
-		 * 
-		 * @see <a href=
-		 *      "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/prscrbr_id_qlfyr_cd.txt">
-		 *      Data Dictionary</a>
+		 * Type: <code>CHAR</code>, max chars: 2. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/prscrbr_id_qlfyr_cd.txt">
+		 * CCW Data Dictionary: PRSCRBR_ID_QLFYR_CD</a>.
 		 */
 		PRSCRBR_ID_QLFYR_CD,
 
 		/**
-		 * Type: <code>CHAR</code>, max chars: 15
+		 * Type: <code>CHAR</code>, max chars: 15. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/prscrbr_id.txt">
+		 * CCW Data Dictionary: PRSCRBR_ID</a>.
 		 */
 		PRSCRBR_ID,
 
 		/**
-		 * Type: <code>NUM</code>, max chars: 12
+		 * Type: <code>NUM</code>, max chars: 12. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/rx_srvc_rfrnc_num.txt">
+		 * CCW Data Dictionary: RX_SRVC_RFRNC_NUM</a>.
 		 */
 		RX_SRVC_RFRNC_NUM,
 
 		/**
-		 * Type: <code>CHAR</code>, max chars: 19
-		 * 
-		 * TODO Link to official NDC directory?
+		 * Type: <code>CHAR</code>, max chars: 19. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/prod_srvc_id.txt">
+		 * CCW Data Dictionary: PROD_SRVC_ID</a>.
 		 */
 		PROD_SRVC_ID,
 
 		/**
-		 * Type: <code>CHAR</code>, max chars: 5
-		 * 
-		 * @see <a href=
-		 *      "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/plan_cntrct_rec_id.txt">
-		 *      Data Dictionary</a>
+		 * Type: <code>CHAR</code>, max chars: 5. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/plan_cntrct_rec_id.txt">
+		 * CCW Data Dictionary: PLAN_CNTRCT_REC_ID</a>.
 		 */
 		PLAN_CNTRCT_REC_ID,
 
 		/**
-		 * Type: <code>CHAR</code>, max chars: 3
+		 * Type: <code>CHAR</code>, max chars: 3. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/plan_pbp_rec_num.txt">
+		 * CCW Data Dictionary: PLAN_PBP_REC_NUM</a>.
 		 */
 		PLAN_PBP_REC_NUM,
 
 		/**
-		 * Type: <code>NUM</code>, max chars: 2, <code>NULLABLE</code>
-		 * 
-		 * @see <a href=
-		 *      "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/cmpnd_cd.txt">
-		 *      Data Dictionary</a>
+		 * Type: <code>NUM</code>, max chars: 2. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/cmpnd_cd.txt">
+		 * CCW Data Dictionary: CMPND_CD</a>.
 		 */
 		CMPND_CD,
 
 		/**
-		 * Type: <code>CHAR</code>, max chars: 1, <code>NULLABLE</code>
-		 * 
-		 * @see <a href=
-		 *      "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/daw_prod_slctn_cd.txt">
-		 *      Data Dictionary</a>
+		 * Type: <code>CHAR</code>, max chars: 1. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/daw_prod_slctn_cd.txt">
+		 * CCW Data Dictionary: DAW_PROD_SLCTN_CD</a>.
 		 */
 		DAW_PROD_SLCTN_CD,
 
 		/**
-		 * Type: <code>NUM</code>, max chars: 12
+		 * Type: <code>NUM</code>, max chars: 12. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/qty_dspnsd_num.txt">
+		 * CCW Data Dictionary: QTY_DPSNSD_NUM</a>.
 		 */
 		QTY_DPSNSD_NUM,
 
 		/**
-		 * Type: <code>NUM</code>, max chars: 3, <code>NULLABLE</code>
+		 * Type: <code>NUM</code>, max chars: 3. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/days_suply_num.txt">
+		 * CCW Data Dictionary: DAYS_SUPLY_NUM</a>.
 		 */
 		DAYS_SUPLY_NUM,
 
 		/**
-		 * Type: <code>NUM</code>, max chars: 3
+		 * Type: <code>NUM</code>, max chars: 3. See <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/fill_num.txt">
+		 * CCW Data Dictionary: FILL_NUM</a>.
 		 */
 		FILL_NUM,
 
 		/**
-		 * Type: <code>CHAR</code>, max chars: 1, <code>NULLABLE</code>
-		 * 
-		 * @see <a href=
-		 *      "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/dspnsng_stus_cd.txt">
-		 *      Data Dictionary</a>
+		 * Type: <code>CHAR</code>, max chars: 1, <code>Optional</code>. See
+		 * <a href=
+		 * "https://www.ccwdata.org/cs/groups/public/documents/datadictionary/dspnsng_stus_cd.txt">
+		 * CCW Data Dictionary: DSPNSNG_STUS_CD</a>.
 		 */
 		DSPNSNG_STUS_CD
 		// TODO FIll in rest of fields
