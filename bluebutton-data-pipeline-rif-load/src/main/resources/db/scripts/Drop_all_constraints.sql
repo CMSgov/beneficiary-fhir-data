bed26ab6a86ae127db24096c3c209eba5628ddb8
Karl M. Davis <karl@justdavis.com>
2017-06-29 13:29:41 -0400

    Whoops: forgot to ensure that one of the indexes is dropped.
    CBBF-53

--- bluebutton-data-pipeline-rif-load/src/main/resources/db/scripts/Drop_all_constraints.sql
+++ bluebutton-data-pipeline-rif-load/src/main/resources/db/scripts/Drop_all_constraints.sql
@@ -21,6 +21,7 @@ alter table "HHAClaims" drop constraint ${logic.if-exists} "HHAClaims_pkey";
 alter table "HospiceClaimLines" drop constraint ${logic.if-exists} "HospiceClaimLines_pkey";
 alter table "HospiceClaims" drop constraint ${logic.if-exists} "HospiceClaims_pkey";
 alter table "InpatientClaimLines" drop constraint ${logic.if-exists} "InpatientClaimLines_pkey";
+alter table "InpatientClaims" drop constraint ${logic.if-exists} "InpatientClaims_pkey";
 alter table "OutpatientClaimLines" drop constraint ${logic.if-exists} "OutpatientClaimLines_pkey";
 alter table "OutpatientClaims" drop constraint ${logic.if-exists} "OutpatientClaims_pkey";
 alter table "PartDEvents" drop constraint ${logic.if-exists} "PartDEvents_pkey";
