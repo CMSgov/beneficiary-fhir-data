758471ce2c0425a9cf3f36022248bf54375b237e
Karl M. Davis <karl@justdavis.com>
2017-08-11 16:01:15 -0400

    Add support for Coverage resources.
    This was largely just migrated over from the old DataTransformer mapping.
    
    CBBF-66

---
+++ bluebutton-server-app/src/main/java/gov/hhs/cms/bluebutton/server/app/stu3/providers/CoverageTransformer.java
@@ -0,0 +1,159 @@
+package gov.hhs.cms.bluebutton.server.app.stu3.providers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.hl7.fhir.dstu3.model.Coverage;
+import org.hl7.fhir.dstu3.model.Coverage.CoverageStatus;
+
+import com.justdavis.karl.misc.exceptions.BadCodeMonkeyException;
+
+import gov.hhs.cms.bluebutton.data.model.rif.Beneficiary;
+
+/**
+ * Transforms CCW {@link Beneficiary} instances into FHIR {@link Coverage}
+ * resources.
+ */
+final class CoverageTransformer {
+	// TODO move constants and methods into here, where approp.
+
+	/**
+	 * @param medicareSegment
+	 *            the {@link MedicareSegment} to generate a {@link Coverage}
+	 *            resource for
+	 * @param beneficiary
+	 *            the {@link Beneficiary} to generate a {@link Coverage}
+	 *            resource for
+	 * @return the {@link Coverage} resource that was generated
+	 */
+	public static Coverage transform(MedicareSegment medicareSegment, Beneficiary beneficiary) {
+		Objects.requireNonNull(medicareSegment);
+
+		if (medicareSegment == MedicareSegment.PART_A)
+			return transformPartA(beneficiary);
+		else if (medicareSegment == MedicareSegment.PART_B)
+			return transformPartB(beneficiary);
+		else if (medicareSegment == MedicareSegment.PART_D)
+			return transformPartD(beneficiary);
+		else
+			throw new BadCodeMonkeyException();
+	}
+
+	/**
+	 * @param beneficiary
+	 *            the CCW {@link Beneficiary} to generate the {@link Coverage}s
+	 *            for
+	 * @return the FHIR {@link Coverage} resources that can be generated from
+	 *         the specified {@link Beneficiary}
+	 */
+	public static List<Coverage> transform(Beneficiary beneficiary) {
+		return Arrays.stream(MedicareSegment.values()).map(s -> transform(s, beneficiary)).collect(Collectors.toList());
+	}
+
+	/**
+	 * @param beneficiary
+	 *            the {@link Beneficiary} to generate a
+	 *            {@link MedicareSegment#PART_A} {@link Coverage} resource for
+	 * @return {@link MedicareSegment#PART_A} {@link Coverage} resource for the
+	 *         specified {@link Beneficiary}
+	 */
+	private static Coverage transformPartA(Beneficiary beneficiary) {
+		Objects.requireNonNull(beneficiary);
+
+		Coverage coverage = new Coverage();
+		coverage.setId(TransformerUtils.buildCoverageId(MedicareSegment.PART_A, beneficiary));
+		if (beneficiary.getPartATerminationCode().isPresent() && beneficiary.getPartATerminationCode().get().equals('0'))
+			coverage.setStatus(CoverageStatus.ACTIVE);
+		else
+			coverage.setStatus(CoverageStatus.CANCELLED);
+		coverage.getGrouping().setSubGroup(TransformerConstants.COVERAGE_PLAN)
+				.setSubPlan(TransformerConstants.COVERAGE_PLAN_PART_A);
+		coverage.setType(
+				TransformerUtils.createCodeableConcept(TransformerConstants.COVERAGE_PLAN,
+						TransformerConstants.COVERAGE_PLAN_PART_A));
+		coverage.setBeneficiary(TransformerUtils.referencePatient(beneficiary));
+		if (beneficiary.getMedicareEnrollmentStatusCode().isPresent()) {
+			TransformerUtils.addExtensionCoding(coverage, TransformerConstants.CODING_SYSTEM_CCW_BENE_MDCR_STATUS_CD,
+					TransformerConstants.CODING_SYSTEM_CCW_BENE_MDCR_STATUS_CD,
+					"" + beneficiary.getMedicareEnrollmentStatusCode().get());
+		}
+		if (beneficiary.getEntitlementCodeOriginal().isPresent()) {
+			TransformerUtils.addExtensionCoding(coverage,
+					TransformerConstants.CODING_SYSTEM_CCW_MEDICARE_ENTITLEMENT_ORIGINAL,
+					TransformerConstants.CODING_SYSTEM_CCW_MEDICARE_ENTITLEMENT_ORIGINAL,
+					"" + beneficiary.getEntitlementCodeOriginal().get());
+		}
+		if (beneficiary.getEntitlementCodeCurrent().isPresent()) {
+			TransformerUtils.addExtensionCoding(coverage,
+					TransformerConstants.CODING_SYSTEM_CCW_MEDICARE_ENTITLEMENT_CURRENT,
+					TransformerConstants.CODING_SYSTEM_CCW_MEDICARE_ENTITLEMENT_CURRENT,
+					"" + beneficiary.getEntitlementCodeCurrent().get());
+		}
+		if (beneficiary.getEndStageRenalDiseaseCode().isPresent()) {
+			TransformerUtils.addExtensionCoding(coverage, TransformerConstants.CODING_SYSTEM_CCW_ESRD_INDICATOR,
+					TransformerConstants.CODING_SYSTEM_CCW_ESRD_INDICATOR,
+					"" + beneficiary.getEndStageRenalDiseaseCode().get());
+		}
+
+		return coverage;
+	}
+
+	/**
+	 * @param beneficiary
+	 *            the {@link Beneficiary} to generate a
+	 *            {@link MedicareSegment#PART_B} {@link Coverage} resource for
+	 * @return {@link MedicareSegment#PART_B} {@link Coverage} resource for the
+	 *         specified {@link Beneficiary}
+	 */
+	private static Coverage transformPartB(Beneficiary beneficiary) {
+		Objects.requireNonNull(beneficiary);
+
+		Coverage coverage = new Coverage();
+		coverage.setId(TransformerUtils.buildCoverageId(MedicareSegment.PART_B, beneficiary));
+		if (beneficiary.getPartBTerminationCode().isPresent()
+				&& beneficiary.getPartBTerminationCode().get().equals('0'))
+			coverage.setStatus(CoverageStatus.ACTIVE);
+		else
+			coverage.setStatus(CoverageStatus.CANCELLED);
+		coverage.getGrouping().setSubGroup(TransformerConstants.COVERAGE_PLAN)
+				.setSubPlan(TransformerConstants.COVERAGE_PLAN_PART_B);
+		coverage.setType(TransformerUtils.createCodeableConcept(TransformerConstants.COVERAGE_PLAN,
+				TransformerConstants.COVERAGE_PLAN_PART_B));
+		coverage.setBeneficiary(TransformerUtils.referencePatient(beneficiary));
+		if (beneficiary.getMedicareEnrollmentStatusCode().isPresent()) {
+			TransformerUtils.addExtensionCoding(coverage, TransformerConstants.CODING_SYSTEM_CCW_BENE_MDCR_STATUS_CD,
+					TransformerConstants.CODING_SYSTEM_CCW_BENE_MDCR_STATUS_CD,
+					"" + beneficiary.getMedicareEnrollmentStatusCode().get());
+		}
+
+		return coverage;
+	}
+
+	/**
+	 * @param beneficiary
+	 *            the {@link Beneficiary} to generate a
+	 *            {@link MedicareSegment#PART_D} {@link Coverage} resource for
+	 * @return {@link MedicareSegment#PART_D} {@link Coverage} resource for the
+	 *         specified {@link Beneficiary}
+	 */
+	private static Coverage transformPartD(Beneficiary beneficiary) {
+		Objects.requireNonNull(beneficiary);
+
+		Coverage coverage = new Coverage();
+		coverage.setId(TransformerUtils.buildCoverageId(MedicareSegment.PART_D, beneficiary));
+		coverage.getGrouping().setSubGroup(TransformerConstants.COVERAGE_PLAN)
+				.setSubPlan(TransformerConstants.COVERAGE_PLAN_PART_D);
+		coverage.setType(TransformerUtils.createCodeableConcept(TransformerConstants.COVERAGE_PLAN,
+				TransformerConstants.COVERAGE_PLAN_PART_D));
+		coverage.setBeneficiary(TransformerUtils.referencePatient(beneficiary));
+		if (beneficiary.getMedicareEnrollmentStatusCode().isPresent()) {
+			TransformerUtils.addExtensionCoding(coverage, TransformerConstants.CODING_SYSTEM_CCW_BENE_MDCR_STATUS_CD,
+					TransformerConstants.CODING_SYSTEM_CCW_BENE_MDCR_STATUS_CD,
+					"" + beneficiary.getMedicareEnrollmentStatusCode().get());
+		}
+
+		return coverage;
+	}
+}
---
+++ bluebutton-server-app/src/test/java/gov/hhs/cms/bluebutton/server/app/stu3/providers/CoverageTransformerTest.java
@@ -0,0 +1,142 @@
+package gov.hhs.cms.bluebutton.server.app.stu3.providers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.hl7.fhir.dstu3.model.Coverage;
+import org.hl7.fhir.dstu3.model.Coverage.CoverageStatus;
+import org.hl7.fhir.exceptions.FHIRException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import gov.hhs.cms.bluebutton.data.model.rif.Beneficiary;
+import gov.hhs.cms.bluebutton.data.model.rif.samples.StaticRifResource;
+import gov.hhs.cms.bluebutton.data.model.rif.samples.StaticRifResourceGroup;
+import gov.hhs.cms.bluebutton.server.app.ServerTestUtils;
+
+/**
+ * Unit tests for {@link CoverageTransformer}.
+ */
+public final class CoverageTransformerTest {
+	/**
+	 * Verifies that
+	 * {@link CoverageTransformer#transform(MedicareSegment, Beneficiary)} works
+	 * as expected when run against the
+	 * {@link StaticRifResource#SAMPLE_A_CARRIER} {@link Beneficiary}.
+	 * 
+	 * @throws FHIRException
+	 *             (indicates test failure)
+	 */
+	@Test
+	public void transformSampleARecord() throws FHIRException {
+		List<Object> parsedRecords = ServerTestUtils
+				.parseData(Arrays.asList(StaticRifResourceGroup.SAMPLE_A.getResources()));
+		Beneficiary beneficiary = parsedRecords.stream().filter(r -> r instanceof Beneficiary).map(r -> (Beneficiary) r)
+				.findFirst().get();
+
+		Coverage partACoverage = CoverageTransformer.transform(MedicareSegment.PART_A, beneficiary);
+		assertPartAMatches(beneficiary, partACoverage);
+
+		Coverage partBCoverage = CoverageTransformer.transform(MedicareSegment.PART_B, beneficiary);
+		assertPartBMatches(beneficiary, partBCoverage);
+
+		Coverage partDCoverage = CoverageTransformer.transform(MedicareSegment.PART_D, beneficiary);
+		assertPartDMatches(beneficiary, partDCoverage);
+	}
+
+	/**
+	 * Verifies that the specified {@link MedicareSegment#PART_A}
+	 * {@link Coverage} "looks like" it should, if it were produced from the
+	 * specified {@link Beneficiary}.
+	 * 
+	 * @param beneficiary
+	 *            the {@link Beneficiary} that the specified {@link Coverage}
+	 *            should match
+	 * @param coverage
+	 *            the {@link Coverage} to verify
+	 */
+	static void assertPartAMatches(Beneficiary beneficiary, Coverage coverage) {
+		Assert.assertNotNull(coverage);
+
+		Assert.assertEquals(TransformerUtils.buildCoverageId(MedicareSegment.PART_A, beneficiary).getIdPart(),
+				coverage.getIdElement().getIdPart());
+		Assert.assertEquals(TransformerConstants.COVERAGE_PLAN, coverage.getGrouping().getSubGroup());
+		Assert.assertEquals(TransformerConstants.COVERAGE_PLAN_PART_A, coverage.getGrouping().getSubPlan());
+		Assert.assertEquals(CoverageStatus.ACTIVE, coverage.getStatus());
+
+		if (beneficiary.getMedicareEnrollmentStatusCode().isPresent())
+			TransformerTestUtils.assertExtensionCodingEquals(coverage,
+					TransformerConstants.CODING_SYSTEM_CCW_BENE_MDCR_STATUS_CD,
+					TransformerConstants.CODING_SYSTEM_CCW_BENE_MDCR_STATUS_CD,
+					beneficiary.getMedicareEnrollmentStatusCode().get());
+
+		if (beneficiary.getEntitlementCodeOriginal().isPresent())
+			TransformerTestUtils.assertExtensionCodingEquals(coverage,
+					TransformerConstants.CODING_SYSTEM_CCW_MEDICARE_ENTITLEMENT_ORIGINAL,
+					TransformerConstants.CODING_SYSTEM_CCW_MEDICARE_ENTITLEMENT_ORIGINAL,
+					String.valueOf(beneficiary.getEntitlementCodeOriginal().get()));
+		if (beneficiary.getEntitlementCodeCurrent().isPresent())
+			TransformerTestUtils.assertExtensionCodingEquals(coverage,
+					TransformerConstants.CODING_SYSTEM_CCW_MEDICARE_ENTITLEMENT_CURRENT,
+					TransformerConstants.CODING_SYSTEM_CCW_MEDICARE_ENTITLEMENT_CURRENT,
+					String.valueOf(beneficiary.getEntitlementCodeCurrent().get()));
+		if (beneficiary.getEndStageRenalDiseaseCode().isPresent())
+			TransformerTestUtils.assertExtensionCodingEquals(coverage,
+					TransformerConstants.CODING_SYSTEM_CCW_ESRD_INDICATOR,
+					TransformerConstants.CODING_SYSTEM_CCW_ESRD_INDICATOR,
+					String.valueOf(beneficiary.getEndStageRenalDiseaseCode().get()));
+	}
+
+	/**
+	 * Verifies that the specified {@link MedicareSegment#PART_B}
+	 * {@link Coverage} "looks like" it should, if it were produced from the
+	 * specified {@link Beneficiary}.
+	 * 
+	 * @param beneficiary
+	 *            the {@link Beneficiary} that the specified {@link Coverage}
+	 *            should match
+	 * @param coverage
+	 *            the {@link Coverage} to verify
+	 */
+	static void assertPartBMatches(Beneficiary beneficiary, Coverage coverage) {
+		Assert.assertNotNull(coverage);
+
+		Assert.assertEquals(TransformerUtils.buildCoverageId(MedicareSegment.PART_B, beneficiary).getIdPart(),
+				coverage.getIdElement().getIdPart());
+		Assert.assertEquals(TransformerConstants.COVERAGE_PLAN, coverage.getGrouping().getSubGroup());
+		Assert.assertEquals(TransformerConstants.COVERAGE_PLAN_PART_B, coverage.getGrouping().getSubPlan());
+		Assert.assertEquals(CoverageStatus.ACTIVE, coverage.getStatus());
+
+		if (beneficiary.getMedicareEnrollmentStatusCode().isPresent())
+			TransformerTestUtils.assertExtensionCodingEquals(coverage,
+					TransformerConstants.CODING_SYSTEM_CCW_BENE_MDCR_STATUS_CD,
+					TransformerConstants.CODING_SYSTEM_CCW_BENE_MDCR_STATUS_CD,
+					beneficiary.getMedicareEnrollmentStatusCode().get());
+	}
+
+	/**
+	 * Verifies that the specified {@link MedicareSegment#PART_D}
+	 * {@link Coverage} "looks like" it should, if it were produced from the
+	 * specified {@link Beneficiary}.
+	 * 
+	 * @param beneficiary
+	 *            the {@link Beneficiary} that the specified {@link Coverage}
+	 *            should match
+	 * @param coverage
+	 *            the {@link Coverage} to verify
+	 */
+	static void assertPartDMatches(Beneficiary beneficiary, Coverage coverage) {
+		Assert.assertNotNull(coverage);
+
+		Assert.assertEquals(TransformerUtils.buildCoverageId(MedicareSegment.PART_D, beneficiary).getIdPart(),
+				coverage.getIdElement().getIdPart());
+		Assert.assertEquals(TransformerConstants.COVERAGE_PLAN, coverage.getGrouping().getSubGroup());
+		Assert.assertEquals(TransformerConstants.COVERAGE_PLAN_PART_D, coverage.getGrouping().getSubPlan());
+
+		if (beneficiary.getMedicareEnrollmentStatusCode().isPresent())
+			TransformerTestUtils.assertExtensionCodingEquals(coverage,
+					TransformerConstants.CODING_SYSTEM_CCW_BENE_MDCR_STATUS_CD,
+					TransformerConstants.CODING_SYSTEM_CCW_BENE_MDCR_STATUS_CD,
+					beneficiary.getMedicareEnrollmentStatusCode().get());
+	}
+}
