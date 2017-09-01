15e164a48a4224d946bf8f219e63d9ff808927ee
Karl M. Davis <karl@justdavis.com>
2017-09-01 16:14:24 -0400

    Added in Travis CI tests.
    Won't work yet, until the app JAR is available in Maven Central.

---
+++ .travis/docker_launch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Bail on the first error.
+set -e
+
+# Echo all commands before running them.
+set -v
+
+# Build and start the container, running systemd and ssh.
+docker build --tag ansible_test_bluebutton_data_pipeline/${PLATFORM} ./.travis/docker_platforms/${PLATFORM}
+docker run \
+	--cap-add=SYS_ADMIN \
+	--detach \
+	-p 127.0.0.1:13022:22 \
+	--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro \
+	--tmpfs /run \
+	--tmpfs /run/lock \
+	--name ansible_test_bluebutton_data_pipeline.${PLATFORM} \
+	ansible_test_bluebutton_data_pipeline/${PLATFORM}
+
