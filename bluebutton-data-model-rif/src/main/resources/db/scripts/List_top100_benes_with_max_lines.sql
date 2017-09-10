452979ea649fbc32992fbe83e3f8d47f4fab5c15
Karl M. Davis <karl@justdavis.com>
2017-09-09 23:08:32 -0400

    Reworked claim-counts-by-bene query.
    Now provides us with more knobs to tune, increasing the amount of data
    it can uncover.

--- bluebutton-data-model-rif/src/main/resources/db/scripts/List_top100_benes_with_max_lines.sql
+++ bluebutton-data-model-rif/src/main/resources/db/scripts/List_top100_benes_with_max_lines.sql
@@ -1,46 +1,204 @@
 /*
-* Use the following command once you have established a tmux session on the prod etl server
-* You will be prompted for the database password*
-*
-* psql --host=db.prod.cmsfhir.systems --dbname=fhirdb2 --username=svc_fhir_etl --password -f '/u01/bb-etl/bene_max_lines';
-*
-* Following query will query all claim line tables to determine how many total claim lines there are for all claim types at the beneficiary id
-* level so we can tell how much data could possibly be returned to the front-end application to ensure it can handle this volume
-*/
+ * Calculate each beneficiary's total number of claims and claim lines.
+ * 
+ * These queries are intended to be run manually, by copy-pasting them into an
+ * interactive `psql` session, or somesuch.
+ * 
+ * It is helpful for several reasons:
+ * 
+ * 1. Allows one to select good/useful beneficiaries to test against, depending
+ *    on how much data one wants to be getting back.
+ * 2. Assists in determining what the system's performance characteristics are
+ *    and/or need to be.
+ */
 
-create view bene_max_lines_view as (select beneId, sum(count)
- from
- (select "HHAClaims"."beneficiaryId" as beneId,  count(*)  from "HHAClaims" inner join "HHAClaimLines" on "HHAClaims"."claimId" = "HHAClaimLines"."parentClaim"
- group by beneId
- union all
- select "HospiceClaims"."beneficiaryId" as beneId,  count(*)  from "HospiceClaims" inner join "HospiceClaimLines" on "HospiceClaims"."claimId" = "HospiceClaimLines"."parentClaim"
- group by beneId
- union all
- select "DMEClaims"."beneficiaryId" as beneId,  count(*)  from "DMEClaims" inner join "DMEClaimLines" on "DMEClaims"."claimId" = "DMEClaimLines"."parentClaim"
- group by beneId
- union all
- select "SNFClaims"."beneficiaryId" as beneId,  count(*)  from "SNFClaims" inner join "SNFClaimLines" on "SNFClaims"."claimId" = "SNFClaimLines"."parentClaim"
- group by beneId
- union all
- select "InpatientClaims"."beneficiaryId" as beneId,  count(*)  from "InpatientClaims" inner join "InpatientClaimLines" on "InpatientClaims"."claimId" = "InpatientClaimLines"."parentClaim"
- group by beneId
- union all
- select "OutpatientClaims"."beneficiaryId" as beneId,  count(*)  from "OutpatientClaims" inner join "OutpatientClaimLines" on "OutpatientClaims"."claimId" = "OutpatientClaimLines"."parentClaim"
- group by beneId
- union all
- select "CarrierClaims"."beneficiaryId" as beneId,  count(*)  from "CarrierClaims" inner join "CarrierClaimLines" on "CarrierClaims"."claimId" = "CarrierClaimLines"."parentClaim"
- group by beneId
-)as beneTable group by beneTable.beneId order by sum(count) desc limit 100);
+-- Count each beneficiary's total number of claims and/or claim lines,
+-- separately by claim types and combined.
+-- Has an (optional) `where` clause that restricts it to benes that have at
+-- least one of every claim type.
+with
+  carrier_claim_counts as (
+    select "CarrierClaims"."beneficiaryId" as beneficiary_id, count(distinct "CarrierClaims"."claimId") as carrier_claim_count, count(*) as carrier_claim_line_count
+      from "CarrierClaims" inner join "CarrierClaimLines" on "CarrierClaims"."claimId" = "CarrierClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,dme_claim_counts as (
+    select "DMEClaims"."beneficiaryId" as beneficiary_id, count(distinct "DMEClaims"."claimId") as dme_claim_count, count(*) as dme_claim_line_count
+      from "DMEClaims" inner join "DMEClaimLines" on "DMEClaims"."claimId" = "DMEClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,hha_claim_counts as (
+    select "HHAClaims"."beneficiaryId" as beneficiary_id, count(distinct "HHAClaims"."claimId") as hha_claim_count, count(*) as hha_claim_line_count
+      from "HHAClaims" inner join "HHAClaimLines" on "HHAClaims"."claimId" = "HHAClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,hospice_claim_counts as (
+    select "HospiceClaims"."beneficiaryId" as beneficiary_id, count(distinct "HospiceClaims"."claimId") as hospice_claim_count, count(*) as hospice_claim_line_count
+      from "HospiceClaims" inner join "HospiceClaimLines" on "HospiceClaims"."claimId" = "HospiceClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,inpatient_claim_counts as (
+    select "InpatientClaims"."beneficiaryId" as beneficiary_id, count(distinct "InpatientClaims"."claimId") as inpatient_claim_count, count(*) as inpatient_claim_line_count
+      from "InpatientClaims" inner join "InpatientClaimLines" on "InpatientClaims"."claimId" = "InpatientClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,outpatient_claim_counts as (
+    select "OutpatientClaims"."beneficiaryId" as beneficiary_id, count(distinct "OutpatientClaims"."claimId") as outpatient_claim_count, count(*) as outpatient_claim_line_count
+      from "OutpatientClaims" inner join "OutpatientClaimLines" on "OutpatientClaims"."claimId" = "OutpatientClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,partd_event_counts as (
+    select "PartDEvents"."beneficiaryId" as beneficiary_id, count(*) as partd_event_count
+      from "PartDEvents"
+      group by beneficiary_id
+  )
+  ,snf_claim_counts as (
+    select "SNFClaims"."beneficiaryId" as beneficiary_id, count(distinct "SNFClaims"."claimId") as snf_claim_count, count(*) as snf_claim_line_count
+      from "SNFClaims" inner join "SNFClaimLines" on "SNFClaims"."claimId" = "SNFClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+select
+    "Beneficiaries"."beneficiaryId" as beneficiary_id
+    ,(
+      coalesce(carrier_claim_counts.carrier_claim_count, 0)
+      + coalesce(dme_claim_counts.dme_claim_count, 0)
+      + coalesce(hha_claim_counts.hha_claim_count, 0)
+      + coalesce(hospice_claim_counts.hospice_claim_count, 0)
+      + coalesce(inpatient_claim_counts.inpatient_claim_count, 0)
+      + coalesce(outpatient_claim_counts.outpatient_claim_count, 0)
+      + coalesce(partd_event_counts.partd_event_count, 0)
+      + coalesce(snf_claim_counts.snf_claim_count, 0)
+    ) as total_claim_count
+    ,(
+      coalesce(carrier_claim_counts.carrier_claim_line_count, 0)
+      + coalesce(dme_claim_counts.dme_claim_line_count, 0)
+      + coalesce(hha_claim_counts.hha_claim_line_count, 0)
+      + coalesce(hospice_claim_counts.hospice_claim_line_count, 0)
+      + coalesce(inpatient_claim_counts.inpatient_claim_line_count, 0)
+      + coalesce(outpatient_claim_counts.outpatient_claim_line_count, 0)
+      + coalesce(partd_event_counts.partd_event_count, 0)
+      + coalesce(snf_claim_counts.snf_claim_line_count, 0)
+    ) as total_claim_line_count
+    ,coalesce(carrier_claim_counts.carrier_claim_count, 0) as carrier_claim_count
+    ,coalesce(carrier_claim_counts.carrier_claim_line_count, 0) as carrier_claim_line_count
+    ,coalesce(dme_claim_counts.dme_claim_count, 0) as dme_claim_count
+    ,coalesce(dme_claim_counts.dme_claim_line_count, 0) as dme_claim_line_count
+    ,coalesce(hha_claim_counts.hha_claim_count, 0) as hha_claim_count
+    ,coalesce(hha_claim_counts.hha_claim_line_count, 0) as hha_claim_line_count
+    ,coalesce(hospice_claim_counts.hospice_claim_count, 0) as hospice_claim_count
+    ,coalesce(hospice_claim_counts.hospice_claim_line_count, 0) as hospice_claim_line_count
+    ,coalesce(inpatient_claim_counts.inpatient_claim_count, 0) as inpatient_claim_count
+    ,coalesce(inpatient_claim_counts.inpatient_claim_line_count, 0) as inpatient_claim_line_count
+    ,coalesce(outpatient_claim_counts.outpatient_claim_count, 0) as outpatient_claim_count
+    ,coalesce(outpatient_claim_counts.outpatient_claim_line_count, 0) as outpatient_claim_line_count
+    ,coalesce(partd_event_counts.partd_event_count, 0) as partd_event_count
+    ,coalesce(snf_claim_counts.snf_claim_count, 0) as snf_claim_count
+    ,coalesce(snf_claim_counts.snf_claim_line_count, 0) as snf_claim_line_count
+  from 
+    "Beneficiaries"
+    left join carrier_claim_counts on "Beneficiaries"."beneficiaryId" = carrier_claim_counts.beneficiary_id
+    left join dme_claim_counts on "Beneficiaries"."beneficiaryId" = dme_claim_counts.beneficiary_id
+    left join hha_claim_counts on "Beneficiaries"."beneficiaryId" = hha_claim_counts.beneficiary_id
+    left join hospice_claim_counts on "Beneficiaries"."beneficiaryId" = hospice_claim_counts.beneficiary_id
+    left join inpatient_claim_counts on "Beneficiaries"."beneficiaryId" = inpatient_claim_counts.beneficiary_id
+    left join outpatient_claim_counts on "Beneficiaries"."beneficiaryId" = outpatient_claim_counts.beneficiary_id
+    left join partd_event_counts on "Beneficiaries"."beneficiaryId" = partd_event_counts.beneficiary_id
+    left join snf_claim_counts on "Beneficiaries"."beneficiaryId" = snf_claim_counts.beneficiary_id
+  -- The `*_count > 0` where clauses can be disabled. Only use them if you want to find beneficiaries who have at least 1 of ALL claim types.
+  where
+    carrier_claim_line_count > 0
+    and dme_claim_line_count > 0
+    and hha_claim_line_count > 0
+    and hospice_claim_line_count > 0
+    and inpatient_claim_line_count > 0
+    and outpatient_claim_line_count > 0
+    and partd_event_count > 0
+    and snf_claim_line_count > 0
+  order by total_claim_line_count desc
+  limit 100;
 
-/*
-*  Following statement will copy the results from the above view to the prod etl server in the following directory
-*
-*/
+-- Results for SAMPLE_C data:
+--  beneficiary_id | total_claim_count | total_claim_line_count |
+-- ----------------+-------------------+------------------------+
+--  416500         |               896 |                   3786 |        
+--  299601         |               995 |                   3659 |        
+--  358620         |              1206 |                   3606 |        
+--  978915         |              1135 |                   3549 |        
+--  876472         |               886 |                   3443 |
 
-\copy (select * from bene_max_lines_view) to '/u01/bb-etl/bene_max_lines.txt'; 
 
-/*
-*  Following statement will delete the view created above
-*/
+-- This version of the query returns line count percentiles, i.e. "X% of the beneficiaries have >= Y claim lines".
+with
+  carrier_claim_counts as (
+    select "CarrierClaims"."beneficiaryId" as beneficiary_id, count(distinct "CarrierClaims"."claimId") as carrier_claim_count, count(*) as carrier_claim_line_count
+      from "CarrierClaims" inner join "CarrierClaimLines" on "CarrierClaims"."claimId" = "CarrierClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,dme_claim_counts as (
+    select "DMEClaims"."beneficiaryId" as beneficiary_id, count(distinct "DMEClaims"."claimId") as dme_claim_count, count(*) as dme_claim_line_count
+      from "DMEClaims" inner join "DMEClaimLines" on "DMEClaims"."claimId" = "DMEClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,hha_claim_counts as (
+    select "HHAClaims"."beneficiaryId" as beneficiary_id, count(distinct "HHAClaims"."claimId") as hha_claim_count, count(*) as hha_claim_line_count
+      from "HHAClaims" inner join "HHAClaimLines" on "HHAClaims"."claimId" = "HHAClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,hospice_claim_counts as (
+    select "HospiceClaims"."beneficiaryId" as beneficiary_id, count(distinct "HospiceClaims"."claimId") as hospice_claim_count, count(*) as hospice_claim_line_count
+      from "HospiceClaims" inner join "HospiceClaimLines" on "HospiceClaims"."claimId" = "HospiceClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,inpatient_claim_counts as (
+    select "InpatientClaims"."beneficiaryId" as beneficiary_id, count(distinct "InpatientClaims"."claimId") as inpatient_claim_count, count(*) as inpatient_claim_line_count
+      from "InpatientClaims" inner join "InpatientClaimLines" on "InpatientClaims"."claimId" = "InpatientClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,outpatient_claim_counts as (
+    select "OutpatientClaims"."beneficiaryId" as beneficiary_id, count(distinct "OutpatientClaims"."claimId") as outpatient_claim_count, count(*) as outpatient_claim_line_count
+      from "OutpatientClaims" inner join "OutpatientClaimLines" on "OutpatientClaims"."claimId" = "OutpatientClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,partd_event_counts as (
+    select "PartDEvents"."beneficiaryId" as beneficiary_id, count(*) as partd_event_count
+      from "PartDEvents"
+      group by beneficiary_id
+  )
+  ,snf_claim_counts as (
+    select "SNFClaims"."beneficiaryId" as beneficiary_id, count(distinct "SNFClaims"."claimId") as snf_claim_count, count(*) as snf_claim_line_count
+      from "SNFClaims" inner join "SNFClaimLines" on "SNFClaims"."claimId" = "SNFClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+select
+    unnest(percentile_disc(array[0.25, 0.5, 0.75, 0.9, 0.99, 1])
+      within group (order by (
+        coalesce(carrier_claim_counts.carrier_claim_line_count, 0)
+        + coalesce(dme_claim_counts.dme_claim_line_count, 0)
+        + coalesce(hha_claim_counts.hha_claim_line_count, 0)
+        + coalesce(hospice_claim_counts.hospice_claim_line_count, 0)
+        + coalesce(inpatient_claim_counts.inpatient_claim_line_count, 0)
+        + coalesce(outpatient_claim_counts.outpatient_claim_line_count, 0)
+        + coalesce(partd_event_counts.partd_event_count, 0)
+        + coalesce(snf_claim_counts.snf_claim_line_count, 0)
+      ))
+    )
+  from 
+    "Beneficiaries"
+    left join carrier_claim_counts on "Beneficiaries"."beneficiaryId" = carrier_claim_counts.beneficiary_id
+    left join dme_claim_counts on "Beneficiaries"."beneficiaryId" = dme_claim_counts.beneficiary_id
+    left join hha_claim_counts on "Beneficiaries"."beneficiaryId" = hha_claim_counts.beneficiary_id
+    left join hospice_claim_counts on "Beneficiaries"."beneficiaryId" = hospice_claim_counts.beneficiary_id
+    left join inpatient_claim_counts on "Beneficiaries"."beneficiaryId" = inpatient_claim_counts.beneficiary_id
+    left join outpatient_claim_counts on "Beneficiaries"."beneficiaryId" = outpatient_claim_counts.beneficiary_id
+    left join partd_event_counts on "Beneficiaries"."beneficiaryId" = partd_event_counts.beneficiary_id
+    left join snf_claim_counts on "Beneficiaries"."beneficiaryId" = snf_claim_counts.beneficiary_id;
 
-drop view bene_max_lines_view;
+-- When run against the SAMPLE_C data, this gave me the following (in 476630.642 ms):
+--  unnest 
+-- --------
+--      36  -- 25th percentile total claim line count (for sample beneficiaries)
+--     111  -- 50th percentile total claim line count (for sample beneficiaries)
+--     257  -- 75th percentile total claim line count (for sample beneficiaries)
+--     489  -- 90th percentile total claim line count (for sample beneficiaries)
+--    1304  -- 99th percentile total claim line count (for sample beneficiaries)
+--    9237  -- 100th percentile total claim line count (for sample beneficiaries)
