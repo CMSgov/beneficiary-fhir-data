cbd07716918cbb83821b419d8b6ff60902516948
Karl M. Davis <karl@justdavis.com>
2017-10-04 15:02:10 -0400

    Restructured tests to allow local runs.

---
+++ test/post-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Stop immediately if any command returns a non-zero result.
+set -e
+
+# Determine the directory that this script is in.
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Run everything from that directory.
+cd "${SCRIPT_DIR}"
+
+# Remove the Docker instance used in the tests.
+sudo docker rm --force ${CONTAINER_PREFIX}.${PLATFORM}
