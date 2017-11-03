3dac16ade0ae4b12dd2dfb256085f603236aad97
Karl M. Davis <karl@justdavis.com>
2017-11-03 14:24:39 -0400

    Added query to count benes and claims.
    CBBD-320

--- bluebutton-data-model-rif/src/main/resources/db/scripts/Beneficiary_and_claim_statistics.sql
+++ bluebutton-data-model-rif/src/main/resources/db/scripts/Beneficiary_and_claim_statistics.sql
@@ -1,16 +1,113 @@
 /*
- * Calculate each beneficiary's total number of claims and claim lines.
+ * Queries for high-level statistics on beneficiaries and claims data.
  * 
  * These queries are intended to be run manually, by copy-pasting them into an
  * interactive `psql` session, or somesuch.
- * 
- * It is helpful for several reasons:
- * 
- * 1. Allows one to select good/useful beneficiaries to test against, depending
- *    on how much data one wants to be getting back.
- * 2. Assists in determining what the system's performance characteristics are
- *    and/or need to be.
  */
+
+-- Count the total numbers of beneficiaries and claims.
+with
+  carrier_claim_counts as (
+    select "CarrierClaims"."beneficiaryId" as beneficiary_id, count(distinct "CarrierClaims"."claimId") as carrier_claim_count, count(*) as carrier_claim_line_count
+      from "CarrierClaims" inner join "CarrierClaimLines" on "CarrierClaims"."claimId" = "CarrierClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,dme_claim_counts as (
+    select "DMEClaims"."beneficiaryId" as beneficiary_id, count(distinct "DMEClaims"."claimId") as dme_claim_count, count(*) as dme_claim_line_count
+      from "DMEClaims" inner join "DMEClaimLines" on "DMEClaims"."claimId" = "DMEClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,hha_claim_counts as (
+    select "HHAClaims"."beneficiaryId" as beneficiary_id, count(distinct "HHAClaims"."claimId") as hha_claim_count, count(*) as hha_claim_line_count
+      from "HHAClaims" inner join "HHAClaimLines" on "HHAClaims"."claimId" = "HHAClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,hospice_claim_counts as (
+    select "HospiceClaims"."beneficiaryId" as beneficiary_id, count(distinct "HospiceClaims"."claimId") as hospice_claim_count, count(*) as hospice_claim_line_count
+      from "HospiceClaims" inner join "HospiceClaimLines" on "HospiceClaims"."claimId" = "HospiceClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,inpatient_claim_counts as (
+    select "InpatientClaims"."beneficiaryId" as beneficiary_id, count(distinct "InpatientClaims"."claimId") as inpatient_claim_count, count(*) as inpatient_claim_line_count
+      from "InpatientClaims" inner join "InpatientClaimLines" on "InpatientClaims"."claimId" = "InpatientClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,outpatient_claim_counts as (
+    select "OutpatientClaims"."beneficiaryId" as beneficiary_id, count(distinct "OutpatientClaims"."claimId") as outpatient_claim_count, count(*) as outpatient_claim_line_count
+      from "OutpatientClaims" inner join "OutpatientClaimLines" on "OutpatientClaims"."claimId" = "OutpatientClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+  ,partd_event_counts as (
+    select "PartDEvents"."beneficiaryId" as beneficiary_id, count(*) as partd_event_count
+      from "PartDEvents"
+      group by beneficiary_id
+  )
+  ,snf_claim_counts as (
+    select "SNFClaims"."beneficiaryId" as beneficiary_id, count(distinct "SNFClaims"."claimId") as snf_claim_count, count(*) as snf_claim_line_count
+      from "SNFClaims" inner join "SNFClaimLines" on "SNFClaims"."claimId" = "SNFClaimLines"."parentClaim"
+      group by beneficiary_id
+  )
+select
+    count("Beneficiaries"."beneficiaryId") as beneficiaries_count
+    ,count("Beneficiaries"."beneficiaryId") filter (
+      where (
+        coalesce(carrier_claim_counts.carrier_claim_count, 0)
+        + coalesce(dme_claim_counts.dme_claim_count, 0)
+        + coalesce(hha_claim_counts.hha_claim_count, 0)
+        + coalesce(hospice_claim_counts.hospice_claim_count, 0)
+        + coalesce(inpatient_claim_counts.inpatient_claim_count, 0)
+        + coalesce(outpatient_claim_counts.outpatient_claim_count, 0)
+        + coalesce(partd_event_counts.partd_event_count, 0)
+        + coalesce(snf_claim_counts.snf_claim_count, 0)
+      ) > 0
+    ) as beneficiaries_with_claims_count
+    ,count("Beneficiaries"."beneficiaryId") filter (where coalesce(carrier_claim_counts.carrier_claim_count, 0) > 0) as beneficiaries_with_carrier_claims_count
+    ,count("Beneficiaries"."beneficiaryId") filter (where coalesce(dme_claim_counts.dme_claim_count, 0) > 0) as beneficiaries_with_dme_claims_count
+    ,count("Beneficiaries"."beneficiaryId") filter (where coalesce(hha_claim_counts.hha_claim_count, 0) > 0) as beneficiaries_with_hha_claims_count
+    ,count("Beneficiaries"."beneficiaryId") filter (where coalesce(hospice_claim_counts.hospice_claim_count, 0) > 0) as beneficiaries_with_hospice_claims_count
+    ,count("Beneficiaries"."beneficiaryId") filter (where coalesce(inpatient_claim_counts.inpatient_claim_count, 0) > 0) as beneficiaries_with_inpatient_claims_count
+    ,count("Beneficiaries"."beneficiaryId") filter (where coalesce(outpatient_claim_counts.outpatient_claim_count, 0) > 0) as beneficiaries_with_outpatient_claims_count
+    ,count("Beneficiaries"."beneficiaryId") filter (where coalesce(partd_event_counts.partd_event_count, 0) > 0) as beneficiaries_with_partd_events_count
+    ,count("Beneficiaries"."beneficiaryId") filter (where coalesce(snf_claim_counts.snf_claim_count, 0) > 0) as beneficiaries_with_snf_claims_count
+    ,sum((
+      coalesce(carrier_claim_counts.carrier_claim_count, 0)
+      + coalesce(dme_claim_counts.dme_claim_count, 0)
+      + coalesce(hha_claim_counts.hha_claim_count, 0)
+      + coalesce(hospice_claim_counts.hospice_claim_count, 0)
+      + coalesce(inpatient_claim_counts.inpatient_claim_count, 0)
+      + coalesce(outpatient_claim_counts.outpatient_claim_count, 0)
+      + coalesce(partd_event_counts.partd_event_count, 0)
+      + coalesce(snf_claim_counts.snf_claim_count, 0)
+    )) as claims_count
+    ,sum(coalesce(carrier_claim_counts.carrier_claim_count, 0)) as carrier_claims_count
+    ,sum(coalesce(dme_claim_counts.dme_claim_count, 0)) as dme_claims_count
+    ,sum(coalesce(hha_claim_counts.hha_claim_count, 0)) as hha_claims_count
+    ,sum(coalesce(hospice_claim_counts.hospice_claim_count, 0)) as hospice_claims_count
+    ,sum(coalesce(inpatient_claim_counts.inpatient_claim_count, 0)) as inpatient_claims_count
+    ,sum(coalesce(outpatient_claim_counts.outpatient_claim_count, 0)) as outpatient_claims_count
+    ,sum(coalesce(partd_event_counts.partd_event_count, 0)) as partd_events_count
+    ,sum(coalesce(snf_claim_counts.snf_claim_count, 0)) as snf_claims_count
+  from 
+    "Beneficiaries"
+    left join carrier_claim_counts on "Beneficiaries"."beneficiaryId" = carrier_claim_counts.beneficiary_id
+    left join dme_claim_counts on "Beneficiaries"."beneficiaryId" = dme_claim_counts.beneficiary_id
+    left join hha_claim_counts on "Beneficiaries"."beneficiaryId" = hha_claim_counts.beneficiary_id
+    left join hospice_claim_counts on "Beneficiaries"."beneficiaryId" = hospice_claim_counts.beneficiary_id
+    left join inpatient_claim_counts on "Beneficiaries"."beneficiaryId" = inpatient_claim_counts.beneficiary_id
+    left join outpatient_claim_counts on "Beneficiaries"."beneficiaryId" = outpatient_claim_counts.beneficiary_id
+    left join partd_event_counts on "Beneficiaries"."beneficiaryId" = partd_event_counts.beneficiary_id
+    left join snf_claim_counts on "Beneficiaries"."beneficiaryId" = snf_claim_counts.beneficiary_id;
+
+-- Results for SAMPLE_C data:
+--  beneficiaries_count | beneficiaries_with_claims_count | beneficiaries_with_carrier_claims_count | beneficiaries_with_dme_claims_count | beneficiaries_with_hha_claims_count | beneficiaries_with_hospice_claims_count | beneficiaries_with_inpatient_claims_count | beneficiaries_with_outpatient_claims_count | beneficiaries_with_partd_events_count | beneficiaries_with_snf_claims_count | claims_count | carrier_claims_count | dme_claims_count | hha_claims_count | hospice_claims_count | inpatient_claims_count | outpatient_claims_count | partd_events_count | snf_claims_count 
+-- ---------------------+---------------------------------+-----------------------------------------+-------------------------------------+-------------------------------------+-----------------------------------------+-------------------------------------------+--------------------------------------------+---------------------------------------+-------------------------------------+--------------+----------------------+------------------+------------------+----------------------+------------------------+-------------------------+--------------------+------------------
+--              1000000 |                          902636 |                                  651658 |                              226704 |                               90276 |                                   22949 |                                    179751 |                                     504134 |                                734509 |                               50436 |    109914678 |             32943217 |          2320363 |           228623 |               106462 |                 384616 |                 6195549 |           67566673 |           169175
+-- (1 row)
+-- 
+-- Time: 499023.863 ms  (8 minutes)
+-- 
+-- Running this against the production data took 231578878.926 ms  (64 hours)
+
 
 -- Count each beneficiary's total number of claims and/or claim lines,
 -- separately by claim types and combined.
