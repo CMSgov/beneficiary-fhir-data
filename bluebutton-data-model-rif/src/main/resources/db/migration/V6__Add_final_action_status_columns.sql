c6e5283d4b91d74aaec19d4c7050015af86b15e6
Karl M. Davis <karl@justdavis.com>
2017-11-21 12:07:44 -0500

    Whoops, fixed more dumb mistakes.
    (This is what happens when you build _after_ you push.)
    
    CBBD-361

--- bluebutton-data-model-rif/src/main/resources/db/migration/V6__Add_final_action_status_columns.sql
+++ bluebutton-data-model-rif/src/main/resources/db/migration/V6__Add_final_action_status_columns.sql
@@ -7,34 +7,34 @@
  * * http://issues.hhsdevcloud.us/browse/CBBD-361
  */
 
-alter table "SNFClaims" add column "finalAction" char(1);
-update table "SNFClaims" set "finalAction" 'F';
-alter table "SNFClaims" alter column "finalAction" set not null;
+alter table "CarrierClaims" add column "finalAction" char(1);
+update "CarrierClaims" set "finalAction" = 'F';
+alter table "CarrierClaims" alter column "finalAction" set not null;
 
 alter table "DMEClaims" add column "finalAction" char(1);
-update table "DMEClaims" set "finalAction" 'F';
+update "DMEClaims" set "finalAction" = 'F';
 alter table "DMEClaims" alter column "finalAction" set not null;
 
 alter table "HHAClaims" add column "finalAction" char(1);
-update table "HHAClaims" set "finalAction" 'F';
+update "HHAClaims" set "finalAction" = 'F';
 alter table "HHAClaims" alter column "finalAction" set not null;
 
 alter table "HospiceClaims" add column "finalAction" char(1);
-update table "HospiceClaims" set "finalAction" 'F';
+update "HospiceClaims" set "finalAction" = 'F';
 alter table "HospiceClaims" alter column "finalAction" set not null;
 
 alter table "InpatientClaims" add column "finalAction" char(1);
-update table "InpatientClaims" set "finalAction" 'F';
+update "InpatientClaims" set "finalAction" = 'F';
 alter table "InpatientClaims" alter column "finalAction" set not null;
 
 alter table "OutpatientClaims" add column "finalAction" char(1);
-update table "OutpatientClaims" set "finalAction" 'F';
+update "OutpatientClaims" set "finalAction" = 'F';
 alter table "OutpatientClaims" alter column "finalAction" set not null;
 
 alter table "PartDEvents" add column "finalAction" char(1);
-update table "PartDEvents" set "finalAction" 'F';
+update "PartDEvents" set "finalAction" = 'F';
 alter table "PartDEvents" alter column "finalAction" set not null;
 
 alter table "SNFClaims" add column "finalAction" char(1);
-update table "SNFClaims" set "finalAction" 'F';
+update "SNFClaims" set "finalAction" = 'F';
 alter table "SNFClaims" alter column "finalAction" set not null;
