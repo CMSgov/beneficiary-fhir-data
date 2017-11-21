6d1f0ebf3194da64e99034f915d901321e467f3b
Karl M. Davis <karl@justdavis.com>
2017-11-21 12:02:24 -0500

    Whoops, fixed previous commit.
    Hadn't saved file before committing.
    
    CBBD-361

--- bluebutton-data-model-rif/src/main/resources/db/migration/V6__Add_final_action_status_columns.sql
+++ bluebutton-data-model-rif/src/main/resources/db/migration/V6__Add_final_action_status_columns.sql
@@ -1,29 +1,40 @@
 /*
- * Alters the claim tables to add `finalAction` columns to each.
+ * Alters the claim tables to add `finalAction` columns to each, backfilling
+ * existing rows with the value 'F'.
  * 
- * See http://issues.hhsdevcloud.us/browse/CBBD-301.
+ * See:
+ * * http://issues.hhsdevcloud.us/browse/CBBD-301
+ * * http://issues.hhsdevcloud.us/browse/CBBD-361
  */
 
-alter table "CarrierClaims"
-  add column "finalAction" char(1);
+alter table "SNFClaims" add column "finalAction" char(1);
+update table "SNFClaims" set "finalAction" 'F';
+alter table "SNFClaims" alter column "finalAction" set not null;
 
-alter table "DMEClaims"
-  add column "finalAction" char(1);
+alter table "DMEClaims" add column "finalAction" char(1);
+update table "DMEClaims" set "finalAction" 'F';
+alter table "DMEClaims" alter column "finalAction" set not null;
 
-alter table "HHAClaims"
-  add column "finalAction" char(1);
+alter table "HHAClaims" add column "finalAction" char(1);
+update table "HHAClaims" set "finalAction" 'F';
+alter table "HHAClaims" alter column "finalAction" set not null;
 
-alter table "HospiceClaims"
-  add column "finalAction" char(1);
+alter table "HospiceClaims" add column "finalAction" char(1);
+update table "HospiceClaims" set "finalAction" 'F';
+alter table "HospiceClaims" alter column "finalAction" set not null;
 
-alter table "InpatientClaims"
-  add column "finalAction" char(1);
+alter table "InpatientClaims" add column "finalAction" char(1);
+update table "InpatientClaims" set "finalAction" 'F';
+alter table "InpatientClaims" alter column "finalAction" set not null;
 
-alter table "OutpatientClaims"
-  add column "finalAction" char(1);
+alter table "OutpatientClaims" add column "finalAction" char(1);
+update table "OutpatientClaims" set "finalAction" 'F';
+alter table "OutpatientClaims" alter column "finalAction" set not null;
 
-alter table "PartDEvents"
-  add column "finalAction" char(1);
+alter table "PartDEvents" add column "finalAction" char(1);
+update table "PartDEvents" set "finalAction" 'F';
+alter table "PartDEvents" alter column "finalAction" set not null;
 
-alter table "SNFClaims"
-  add column "finalAction" char(1);
+alter table "SNFClaims" add column "finalAction" char(1);
+update table "SNFClaims" set "finalAction" 'F';
+alter table "SNFClaims" alter column "finalAction" set not null;
