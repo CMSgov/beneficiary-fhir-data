8f4c3f26f66d68ab84be8e09e301facf96d68e72
glovercloud <glovercloud@comcast.net>
2017-12-20 09:59:18 -0700

    CBBF-86: Moved the exception handiling into the CustomSamplerClient based class.  Also added a handler for FhirClientConnectionException so it will come out as a 504 error in the jmeter log.  Renamed local runTest implementation to executeTest and made it pure virtual in the base class so tests can override it.  Also avoids confusion with the runTest used by the SamplerClient.

--- src/main/java/gov/hhs/cms/bluebutton/fhirstress/backend/CustomSamplerClient.java
+++ src/main/java/gov/hhs/cms/bluebutton/fhirstress/backend/CustomSamplerClient.java
@@ -2,10 +2,13 @@ package gov.hhs.cms.bluebutton.fhirstress.backend;
 
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
 
 import gov.hhs.cms.bluebutton.fhirstress.utils.FhirClient;
 //import gov.hhs.cms.bluebutton.fhirstress.utils.RifParser;
@@ -13,6 +16,8 @@ import gov.hhs.cms.bluebutton.fhirstress.utils.FhirClient;
 
 //import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.IGenericClient;
+import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
+import ca.uhn.fhir.rest.client.exceptions.FhirClientConnectionException;
 
 public abstract class CustomSamplerClient extends AbstractJavaSamplerClient {
 	protected static final String PARAM_SERVER = "fhir_server";
@@ -22,6 +27,8 @@ public abstract class CustomSamplerClient extends AbstractJavaSamplerClient {
 	protected static final String RIFFILE = "riffile";
 	protected static final String DELIMITER = "delimiter";
 	protected static final String HOSTNAME_UNKNOWN = "unknown-host";
+
+  protected abstract void executeTest();
 
 	protected String hostName;
 	protected IGenericClient client;
@@ -71,6 +78,46 @@ public abstract class CustomSamplerClient extends AbstractJavaSamplerClient {
 		// Find the IDs that can be queried in each sample run.
 		//this.patientIds = findPatientIds();
 	}
+
+	/**
+	 * @see org.apache.jmeter.protocol.java.sampler.JavaSamplerClient#runTest(org.apache.jmeter.protocol.java.sampler.JavaSamplerContext)
+	 */
+	@Override
+	public SampleResult runTest(JavaSamplerContext context) {
+		SampleResult sample = new SampleResult();
+		sample.sampleStart();
+		try {
+			executeTest();
+
+			sample.sampleEnd();
+			sample.setSuccessful(true);
+			sample.setResponseMessage("Sample succeeded on host: " + hostName);
+			sample.setResponseCodeOK();
+		} catch (IllegalArgumentException|InvalidRequestException|FhirClientConnectionException e) {
+			// Mark this sample iteration as failed.
+			sample.sampleEnd();
+			sample.setSuccessful(false);
+			sample.setResponseMessage("Exception: " + e);
+
+			/*
+			 * Serialize the stack trace to a String and attach it to the sample
+			 * results.
+			 */
+			StringWriter stringWriter = new StringWriter();
+			e.printStackTrace(new java.io.PrintWriter(stringWriter));
+			sample.setResponseData(stringWriter.toString(), StandardCharsets.UTF_8.name());
+			sample.setDataType(org.apache.jmeter.samplers.SampleResult.TEXT);
+      if (e instanceof FhirClientConnectionException) {
+			  sample.setResponseCode("504");
+      }
+      else {
+			  sample.setResponseCode("500");
+      }
+		}
+
+		return sample;
+	}
+
 
 	/**
 	 * @return the hostname of the system this code is running on, or
--- src/main/java/gov/hhs/cms/bluebutton/fhirstress/backend/RetrieveEobsByDate.java
+++ src/main/java/gov/hhs/cms/bluebutton/fhirstress/backend/RetrieveEobsByDate.java
@@ -1,13 +1,10 @@
 package gov.hhs.cms.bluebutton.fhirstress.backend;
 
-import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
-import org.apache.jmeter.samplers.SampleResult;
 
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.Bundle.BundleEntryComponent;
@@ -18,8 +15,6 @@ import org.hl7.fhir.dstu3.model.Patient;
 import org.hl7.fhir.dstu3.model.Coverage;
 import gov.hhs.cms.bluebutton.server.app.stu3.providers.TransformerUtils;
 import gov.hhs.cms.bluebutton.server.app.stu3.providers.MedicareSegment;
-
-import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
 
 import gov.hhs.cms.bluebutton.fhirstress.utils.BenefitIdMgr;
 
@@ -42,69 +37,10 @@ public final class RetrieveEobsByDate extends CustomSamplerClient {
 	}
 
 	/**
-	 * @return the {@link Patient#getId()}s that should be queried during sample
-	 *         executions
+	 * Test Implementation 
 	 */
-	private List<String> findPatientIds() {
-		List<String> patientIds = new ArrayList<>(100);
-
-		Bundle bundleOfResults = client.search().forResource(Patient.class).returnBundle(Bundle.class)
-				.elementsSubset(DomainResource.RES_ID.getParamName()).execute();
-		while (bundleOfResults != null && patientIds.size() < 100) {
-			// Add in all of the patient IDs from this page.
-			for (BundleEntryComponent bundleEntry : bundleOfResults.getEntry()) {
-				patientIds.add(bundleEntry.getResource().getId());
-			}
-
-			// Grab the next page, if there is one.
-			if (bundleOfResults.getLink(Bundle.LINK_NEXT) != null) {
-				bundleOfResults = client.loadPage().next(bundleOfResults).execute();
-			} else {
-				bundleOfResults = null;
-			}
-		}
-
-		return patientIds;
-	}
-
-	/**
-	 * @see org.apache.jmeter.protocol.java.sampler.JavaSamplerClient#runTest(org.apache.jmeter.protocol.java.sampler.JavaSamplerContext)
-	 */
-	@Override
-	public SampleResult runTest(JavaSamplerContext context) {
-		SampleResult sample = new SampleResult();
-		sample.sampleStart();
-		try {
-			runTest();
-
-			sample.sampleEnd();
-			sample.setSuccessful(true);
-			sample.setResponseMessage("Sample succeeded on host: " + hostName);
-			sample.setResponseCodeOK();
-		} catch (IllegalArgumentException|InvalidRequestException e) {
-			// Mark this sample iteration as failed.
-			sample.sampleEnd();
-			sample.setSuccessful(false);
-			sample.setResponseMessage("Exception: " + e);
-
-			/*
-			 * Serialize the stack trace to a String and attach it to the sample
-			 * results.
-			 */
-			StringWriter stringWriter = new StringWriter();
-			e.printStackTrace(new java.io.PrintWriter(stringWriter));
-			sample.setResponseData(stringWriter.toString(), StandardCharsets.UTF_8.name());
-			sample.setDataType(org.apache.jmeter.samplers.SampleResult.TEXT);
-			sample.setResponseCode("500");
-		}
-
-		return sample;
-	}
-
-	/**
-	 * Actually run the test.
-	 */
-	private void runTest() {
+  @Override
+	protected void executeTest() {
     // Removed the Rif parser to speed things up
     //if (this.parser.hasNext()) 
     //{  
