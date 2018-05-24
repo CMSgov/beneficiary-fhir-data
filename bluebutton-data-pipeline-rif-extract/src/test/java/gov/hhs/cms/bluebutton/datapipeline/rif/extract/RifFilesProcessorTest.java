92d5ff00439f8d6b0cc6ab0e5a1380ade9542c3c
dho937 <dave.holdgrafer@gdit.com>
2018-05-23 22:26:31 -0500

    ndc changes

--- bluebutton-data-pipeline-rif-extract/src/test/java/gov/hhs/cms/bluebutton/datapipeline/rif/extract/RifFilesProcessorTest.java
+++ bluebutton-data-pipeline-rif-extract/src/test/java/gov/hhs/cms/bluebutton/datapipeline/rif/extract/RifFilesProcessorTest.java
@@ -175,7 +175,7 @@ public final class RifFilesProcessorTest {
 		Assert.assertEquals("01", pdeRow.getPrescriberIdQualifierCode());
 		Assert.assertEquals("444555666", pdeRow.getPrescriberId());
 		Assert.assertEquals(new BigDecimal(799999), pdeRow.getPrescriptionReferenceNumber());
-		Assert.assertEquals("987654331212", pdeRow.getNationalDrugCode());
+		Assert.assertEquals("51270012299", pdeRow.getNationalDrugCode());
 		Assert.assertEquals("H9999", pdeRow.getPlanContractId());
 		Assert.assertEquals("020", pdeRow.getPlanBenefitPackageId());
 		Assert.assertEquals(1, pdeRow.getCompoundCode());
@@ -328,7 +328,7 @@ public final class RifFilesProcessorTest {
 		Assert.assertFalse(claimLine.getRxNumber().isPresent());
 		Assert.assertEquals(new BigDecimal("42.0"), claimLine.getHctHgbTestResult());
 		Assert.assertEquals("R1", claimLine.getHctHgbTestTypeCode().get());
-		Assert.assertEquals("777666666", claimLine.getNationalDrugCode().get());
+		Assert.assertEquals("51270012299", claimLine.getNationalDrugCode().get());
 		Assert.assertEquals("BB889999AA", claimLine.getCliaLabNumber().get());
 		Assert.assertEquals(new BigDecimal(0), claimLine.getAnesthesiaUnitCount());
 	}
