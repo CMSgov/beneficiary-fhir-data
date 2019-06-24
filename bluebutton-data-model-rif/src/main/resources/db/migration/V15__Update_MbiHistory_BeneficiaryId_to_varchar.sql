d44f6a2673f589b5ef790dd5f4d1f06bcbb771c6
Jacob Geer <jacob.geer@gdit.com>
2019-06-24 09:24:33 -0500

    Updating comments
    BLUEBUTTON-1010

--- bluebutton-data-model-rif/src/main/resources/db/migration/V15__Update_MbiHistory_BeneficiaryId_to_varchar.sql
+++ bluebutton-data-model-rif/src/main/resources/db/migration/V15__Update_MbiHistory_BeneficiaryId_to_varchar.sql
@@ -1,5 +1,6 @@
 /*
- * Alters the MedicareBeneficiaryIdHistory table's `beneficiaryId` to be a varchar(15), making it consistent with our representation in other tables. 
+ * Alters the MedicareBeneficiaryIdHistory table's `beneficiaryId` to be a varchar(15),
+ * making it consistent with the current representation in our other tables. 
  * 
  * See https://jira.cms.gov/browse/BLUEBUTTON-1010.
  */
