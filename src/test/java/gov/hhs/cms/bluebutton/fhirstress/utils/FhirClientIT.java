d5636094d41cbb9e790b0ffe04e7d7f8555b22db
Christopher Hayes <chris@cthayes.net>
2019-07-18 08:57:37 -0400

    Update test fhir url to test env
    BLUEBUTTON-758
    BLUEBUTTON-220

--- src/test/java/gov/hhs/cms/bluebutton/fhirstress/utils/FhirClientIT.java
+++ src/test/java/gov/hhs/cms/bluebutton/fhirstress/utils/FhirClientIT.java
@@ -1,11 +1,11 @@
 package gov.hhs.cms.bluebutton.fhirstress.utils;
 
+import org.hl7.fhir.dstu3.model.Patient;
+
+import ca.uhn.fhir.rest.client.api.IGenericClient;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
-import org.hl7.fhir.dstu3.model.Patient;
-import ca.uhn.fhir.rest.client.api.IGenericClient;
 
 /**
  * Unit test for simple FhirClient.
@@ -14,8 +14,7 @@ public class FhirClientIT extends TestCase {
 	/**
 	 * Create the test case
 	 *
-	 * @param testName
-	 *            name of the test case
+	 * @param testName name of the test case
 	 */
 	public FhirClientIT(String testName) {
 		super(testName);
@@ -32,8 +31,8 @@ public class FhirClientIT extends TestCase {
 	 * Test FHIR server connectivity
 	 */
 	public void testFhirClient() {
-		IGenericClient client = FhirClient.create("https://fhir.backend.bluebutton.hhsdevcloud.us/v1/fhir",
-				"./dev/ssl-stores");
+		IGenericClient client = FhirClient.create(
+				"https://internal-tsbb10lb01-758855236.us-east-1.elb.amazonaws.com/v1/fhir", "./dev/ssl-stores");
 		Patient patient = client.read().resource(Patient.class).withId("20140000005499").execute();
 		assertEquals("Doe", patient.getName().get(0).getFamily());
 	}
