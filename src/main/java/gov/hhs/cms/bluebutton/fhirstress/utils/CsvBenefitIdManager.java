a9972b4f729dcd8a94975b030e7e289372cadda7
Christopher Hayes <chris@cthayes.net>
2019-07-30 16:48:31 -0400

    Try to load csv file
    BLUEBUTTON-759

--- src/main/java/gov/hhs/cms/bluebutton/fhirstress/utils/CsvBenefitIdManager.java
+++ src/main/java/gov/hhs/cms/bluebutton/fhirstress/utils/CsvBenefitIdManager.java
@@ -1,10 +1,47 @@
 package gov.hhs.cms.bluebutton.fhirstress.utils;
 
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
 public class CsvBenefitIdManager implements BenefitIdManager {
+	CSVParser parser;
+	Iterator<CSVRecord> i;
+	File f;
+
+	public CsvBenefitIdManager() {
+		f = Paths.get("test-bene-id.csv").toFile();
+		init();
+	}
+
+	private void init() {
+		try {
+			parser = CSVParser.parse(f, Charset.defaultCharset(), CSVFormat.DEFAULT);
+			i = parser.iterator();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
-	public String nextId() {
-		return "12162";
+	public String nextId() { 
+		CSVRecord r;
+		try {
+			r = i.next();
+		} catch (NoSuchElementException e) {
+			// If we get an exception here it's likely that we hit the end of file
+			// So I want to restart from the beginning of the file.
+			init();
+			r = i.next();
+		}
+
+		return r.get(0);
 	}
 
 }
