eb16d5c809df535dbfafae68a067b55316e9dbb0
Christopher Hayes <chris@cthayes.net>
2019-08-16 14:37:39 -0400

    Increase Timeout to Expected value of 30 seconds
    BLUEBUTTON-1189

--- bluebutton-server-app/src/main/java/gov/hhs/cms/bluebutton/server/app/SpringConfiguration.java
+++ bluebutton-server-app/src/main/java/gov/hhs/cms/bluebutton/server/app/SpringConfiguration.java
@@ -60,7 +60,7 @@ public class SpringConfiguration {
 	public static final String PROP_DB_USERNAME = "bbfhir.db.username";
 	public static final String PROP_DB_PASSWORD = "bbfhir.db.password";
 	public static final String PROP_DB_CONNECTIONS_MAX = "bbfhir.db.connections.max";
-	public static final int TRANSACTION_TIMEOUT = 2;
+	public static final int TRANSACTION_TIMEOUT = 30;
 
 	/**
 	 * This fake JDBC URL prefix indicates to {@link SpringConfiguration} and
