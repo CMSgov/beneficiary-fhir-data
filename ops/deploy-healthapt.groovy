e08b3f663711d2e0ead03c58311bb02f11094ffd
Karl M. Davis <karl.davis@cms.hhs.gov>
2019-08-30 17:47:09 -0400

    Fix Docker to make whole repo available.
    I'm not 100% sure, but I think the previous build errors were caused by
    the fact that the Docker calls (at the outer level) were inside the
    `dir` block, which was causing Docker to only mount that portion of the
    tree inside the container? We'll see if this fixes the problem,
    anyways...
    
    BLUEBUTTON-1086

--- ops/deploy-healthapt.groovy
+++ ops/deploy-healthapt.groovy
@@ -34,64 +34,62 @@ def deployManagement(AmiIds amiIds) {
  * @throws RuntimeException An exception will be bubbled up if the deploy tooling returns a non-zero exit code.
  */
 def deploy(String envId, AmiIds amiIds, AppBuildResults appBuildResults) {
-	dir ('ops/ansible/playbooks-healthapt') {
-		// Ensure the Ansible image is ready to go.
-		insideAnsibleContainer {
-			// Just some general "what does the container look like" debug
-			// logging. Useful for if/when things go sideways.
-			sh 'cat /etc/passwd'
-			sh 'echo $USER && echo $UID && echo $HOME && whoami'
-			sh 'pwd && ls -la'
-			sh 'ls -la ../roles'
-			sh 'ansible --version'
+	// Ensure the Ansible image is ready to go.
+	insideAnsibleContainer {
+		// Just some general "what does the container look like" debug
+		// logging. Useful for if/when things go sideways.
+		sh 'cat /etc/passwd'
+		sh 'echo $USER && echo $UID && echo $HOME && whoami'
+		sh 'pwd && ls -la'
+		sh 'ls -la ../roles'
+		sh 'ansible --version'
 
-			// Verify the play's syntax before we run it.
-			sh './ansible-playbook-wrapper backend.yml --inventory=hosts_test --syntax-check'
-		}
+		// Verify the play's syntax before we run it.
+		sh './ansible-playbook-wrapper backend.yml --inventory=hosts_test --syntax-check'
+	}
 
-		// Define env-specific variables.
-		def envGroupName;
-		def envLimitName;
-		def extraVarsText;
-		if (envId == "test" ) {
-			envGroupName = 'env_test'
-			envLimitName = 'ts'
-		} else if (envId == "prod-stg") {
-			envGroupName = 'env_dor'
-			envLimitName = 'dp'
-		} else if (envId == "prod") {
-			envGroupName = 'env_prod'
-			envLimitName = 'pd'
-		} else {
-			throw new IllegalArgumentException("Unsupported environment ID: '${envId}'.")
-		}
+	// Define env-specific variables.
+	def envGroupName;
+	def envLimitName;
+	def extraVarsText;
+	if (envId == "test" ) {
+		envGroupName = 'env_test'
+		envLimitName = 'ts'
+	} else if (envId == "prod-stg") {
+		envGroupName = 'env_dor'
+		envLimitName = 'dp'
+	} else if (envId == "prod") {
+		envGroupName = 'env_prod'
+		envLimitName = 'pd'
+	} else {
+		throw new IllegalArgumentException("Unsupported environment ID: '${envId}'.")
+	}
 
-		// Seatbelt: don't deploy anywhere important until everything is tested.
-		// TODO: remove this when ready
-		if (envId != "test") {
-			echo 'Production deploys not enabled yet.'
-			return
-		}
-		if (envId != "test") {
-			// Just in case that 'return' doesn't work as expected...
-			throw new IllegalStateException('do not deploy to prod yet')
-		}
+	// Seatbelt: don't deploy anywhere important until everything is tested.
+	// TODO: remove this when ready
+	if (envId != "test") {
+		echo 'Production deploys not enabled yet.'
+		return
+	}
+	if (envId != "test") {
+		// Just in case that 'return' doesn't work as expected...
+		throw new IllegalStateException('do not deploy to prod yet')
+	}
 
-		// Run the deploy to the specified environment.
-		insideAnsibleContainer {
-			writeFile file: 'extra_vars.json', encoding: 'UTF-8', text: """\
-			{
-				"limit_envs": [
-					"${envLimitName}"
-				],
-				"data_pipeline_jar": "../../../${appBuildResults.dataPipelineUberJar}",
-				"data_server_container": "../../../${appBuildResults.dataServerContainerZip}",
-				"data_server_container_name": "${appBuildResults.dataServerContainerName}",
-				"data_server_war": "../../../${appBuildResults.dataServerWar}"
-			}
-			""".stripIndent()
-			sh "./ansible-playbook-wrapper backend.yml --limit=localhost:${envGroupName} --extra-vars \"@extra_vars.json\""
+	// Run the deploy to the specified environment.
+	insideAnsibleContainer {
+		writeFile file: 'extra_vars.json', encoding: 'UTF-8', text: """\
+		{
+			"limit_envs": [
+				"${envLimitName}"
+			],
+			"data_pipeline_jar": "../../../${appBuildResults.dataPipelineUberJar}",
+			"data_server_container": "../../../${appBuildResults.dataServerContainerZip}",
+			"data_server_container_name": "${appBuildResults.dataServerContainerName}",
+			"data_server_war": "../../../${appBuildResults.dataServerWar}"
 		}
+		""".stripIndent()
+		sh "./ansible-playbook-wrapper backend.yml --limit=localhost:${envGroupName} --extra-vars \"@extra_vars.json\""
 	}
 }
 
@@ -136,13 +134,15 @@ public <V> V insideAnsibleContainer(Closure<V> body) {
 
 		// Prepend the specified closure with some needed in-container setup.
 		def bodyWithSetup = {
-			// Copy the SSH config and keys and fix permissions.
-			sh 'cp --recursive --no-target-directory /root/.ssh_jenkins /root/.ssh'
-			sh 'chmod -R u=rw,g=,o= /root/.ssh'
+			dir ('ops/ansible/playbooks-healthapt') {
+				// Copy the SSH config and keys and fix permissions.
+				sh 'cp --recursive --no-target-directory /root/.ssh_jenkins /root/.ssh'
+				sh 'chmod -R u=rw,g=,o= /root/.ssh'
 
-			// Link the project's Ansible roles to where they're expected.
-			sh 'ln -s /etc/ansible/roles roles_external'
-			body.call()
+				// Link the project's Ansible roles to where they're expected.
+				sh 'ln -s /etc/ansible/roles roles_external'
+				body.call()
+			}
 		}
 
 		// Now start the container with the above args and run the specified
