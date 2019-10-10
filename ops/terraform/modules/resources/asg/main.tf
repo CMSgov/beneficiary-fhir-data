4bab010f7cdf7e60770be784ba00d1024fbedb60
Nathan Dister <nathan.dister@adhocteam.us>
2019-10-10 14:31:29 -0400

    Increase wait_for_capacity_timeout to 20m (#117)

--- ops/terraform/modules/resources/asg/main.tf
+++ ops/terraform/modules/resources/asg/main.tf
@@ -156,7 +156,7 @@ resource "aws_autoscaling_group" "main" {
   # If an lb is defined, wait for the ELB 
   min_elb_capacity          = var.lb_config == null ? null : var.asg_config.min
   wait_for_elb_capacity     = var.lb_config == null ? null : var.asg_config.min
-  wait_for_capacity_timeout = var.lb_config == null ? null : "10m"
+  wait_for_capacity_timeout = var.lb_config == null ? null : "20m"
 
   health_check_grace_period = 300
   health_check_type         = var.lb_config == null ? "EC2" : "ELB" # Failures of ELB healthchecks are asg failures
