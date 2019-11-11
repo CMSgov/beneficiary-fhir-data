b7333efba269a998281f02afec04f562d9437a93
Karl M. Davis <karl.davis@cms.hhs.gov>
2019-11-10 23:54:30 -0500

    Refactored `CcwCodebookVariable` construction.
    Still feels a bit gross, but is at least more concise.

--- apps/bfd-server-plaid/src/ccw_codebook.rs
+++ apps/bfd-server-plaid/src/ccw_codebook.rs
@@ -14,15 +14,28 @@ pub struct CcwCodebookVariable {
     // TODO flesh out
 }
 
+struct CcwCodebookVariableBuilder {
+    id: &'static str,
+    label: Option<&'static str>,
+    value_groups: Vec<ValueGroup>,
+}
+
 struct ValueGroup {
     #[allow(dead_code)]
-    description: Option<String>,
+    description: Option<&'static str>,
+    values: Vec<Value>,
+}
+
+struct ValueGroupBuilder {
+    parent: CcwCodebookVariableBuilder,
+    #[allow(dead_code)]
+    description: Option<&'static str>,
     values: Vec<Value>,
 }
 
 struct Value {
-    code: String,
-    description: String,
+    code: &'static str,
+    description: &'static str,
 }
 
 impl CcwCodebookVariable {
@@ -43,60 +56,107 @@ impl CcwCodebookVariable {
          * to be safe.
          */
         if matching_values.len() == 1 {
-            Some(matching_values.get(0).unwrap().description.clone())
+            Some(matching_values.get(0).unwrap().description.to_string())
         } else {
             None
         }
     }
+
+    /// Returns a `CcwCodebookVariableBuilder` for a `CcwCodebookVariable` with the specified `id`.
+    fn new(id: &'static str) -> CcwCodebookVariableBuilder {
+        CcwCodebookVariableBuilder {
+            id: id,
+            label: None,
+            value_groups: vec![],
+        }
+    }
+}
+
+/// A builder for `CcwCodebookVariable`s.
+impl CcwCodebookVariableBuilder {
+    /// Sets `CcwCodebookVariable.label` to the specified value.
+    fn label(mut self, label: &'static str) -> CcwCodebookVariableBuilder {
+        self.label = Some(label);
+        self
+    }
+
+    /// Returns a `ValueGroupBuilder`, which will add a new `ValueGroup` to
+    /// `CcwCodebookVariable.value_groups` when `ValueGroupBuilder.group_end()` is called.
+    fn group_start(self) -> ValueGroupBuilder {
+        ValueGroupBuilder {
+            parent: self,
+            description: None,
+            values: vec![],
+        }
+    }
+
+    /// Completes the `CcwCodebookVariableBuilder`, returning a new `CcwCodebookVariable` based on
+    /// it.
+    fn build(self) -> CcwCodebookVariable {
+        CcwCodebookVariable {
+            id: self.id,
+            label: self.label.unwrap(),
+            value_groups: self.value_groups,
+        }
+    }
+}
+
+/// A builder for `ValueGroup`s.
+impl ValueGroupBuilder {
+    /// Adds a new `Value` to `ValueGroup.values`.
+    fn value(mut self, code: &'static str, description: &'static str) -> ValueGroupBuilder {
+        self.values.push(Value {
+            code: code,
+            description,
+        });
+        self
+    }
+
+    /// Completes the `ValueGroupBuilder`, adding the `ValueGroup` to the original
+    /// `CcwCodebookVariableBuilder` and then returning that `CcwCodebookVariableBuilder`.
+    fn group_end(mut self) -> CcwCodebookVariableBuilder {
+        self.parent.value_groups.push(ValueGroup {
+            description: self.description,
+            values: self.values,
+        });
+        self.parent
+    }
 }
 
 lazy_static! {
-    pub static ref PDE_ID: CcwCodebookVariable = CcwCodebookVariable {
-        id: "PDE_ID",
-        label: "CCW Encrypted Part D Event Number",
-        value_groups: vec![],
-    };
-    pub static ref RX_SRVC_RFRNC_NUM: CcwCodebookVariable = CcwCodebookVariable {
-        id: "RX_SRVC_RFRNC_NUM",
-        label: "RX Service Reference Number",
-        value_groups: vec![],
-    };
-    pub static ref PLAN_CNTRCT_REC_ID: CcwCodebookVariable = CcwCodebookVariable {
-        id: "PLAN_CNTRCT_REC_ID",
-        label: "Plan Contract ID",
-        value_groups: vec![],
-    };
-    pub static ref PLAN_PBP_REC_NUM: CcwCodebookVariable = CcwCodebookVariable {
-        id: "PLAN_PBP_REC_NUM",
-        label: "Plan Benefit Package ID",
-        value_groups: vec![],
-    };
-    pub static ref CVRD_D_PLAN_PD_AMT: CcwCodebookVariable = CcwCodebookVariable {
-        id: "CVRD_D_PLAN_PD_AMT",
-        label: "Amount paid by Part D plan for the PDE (drug is covered by Part D)",
-        value_groups: vec![],
-    };
-    pub static ref NCVRD_PLAN_PD_AMT: CcwCodebookVariable = CcwCodebookVariable {
-        id: "NCVRD_PLAN_PD_AMT",
-        label: "Amount paid by Part D plan for the PDE (drug is not covered by Part D)",
-        value_groups: vec![],
-    };
-    pub static ref DRUG_CVRG_STUS_CD: CcwCodebookVariable = CcwCodebookVariable {
-        id: "DRUG_CVRG_STUS_CD",
-        label: "Drug Coverage Status Code",
-        value_groups: vec![ValueGroup {
-            description: None,
-            values: vec![Value {
-                code: "C".to_string(),
-                description: "Covered".to_string(),
-            },
-            Value {
-                code: "E".to_string(),
-                description: "Supplemental drugs (reported by plans that provide Enhanced Alternative coverage)".to_string(),
-            },Value {
-                code: "O".to_string(),
-                description: "Over-the-counter drugs".to_string(),
-            }],
-        }],
-    };
+    pub static ref PDE_ID: CcwCodebookVariable = CcwCodebookVariable::new("PDE_ID")
+        .label("CCW Encrypted Part D Event Number")
+        .build();
+    pub static ref RX_SRVC_RFRNC_NUM: CcwCodebookVariable =
+        CcwCodebookVariable::new("RX_SRVC_RFRNC_NUM")
+            .label("RX Service Reference Number")
+            .build();
+    pub static ref PLAN_CNTRCT_REC_ID: CcwCodebookVariable =
+        CcwCodebookVariable::new("PLAN_CNTRCT_REC_ID")
+            .label("Plan Contract ID")
+            .build();
+    pub static ref PLAN_PBP_REC_NUM: CcwCodebookVariable =
+        CcwCodebookVariable::new("PLAN_PBP_REC_NUM")
+            .label("Plan Benefit Package ID")
+            .build();
+    pub static ref CVRD_D_PLAN_PD_AMT: CcwCodebookVariable =
+        CcwCodebookVariable::new("CVRD_D_PLAN_PD_AMT")
+            .label("Amount paid by Part D plan for the PDE (drug is covered by Part D)")
+            .build();
+    pub static ref NCVRD_PLAN_PD_AMT: CcwCodebookVariable =
+        CcwCodebookVariable::new("NCVRD_PLAN_PD_AMT")
+            .label("Amount paid by Part D plan for the PDE (drug is not covered by Part D)")
+            .build();
+    pub static ref DRUG_CVRG_STUS_CD: CcwCodebookVariable =
+        CcwCodebookVariable::new("DRUG_CVRG_STUS_CD")
+            .label("Drug Coverage Status Code")
+            .group_start()
+            .value("C", "Covered")
+            .value(
+                "E",
+                "Supplemental drugs (reported by plans that provide Enhanced Alternative coverage)"
+            )
+            .value("O", "Over-the-counter drugs")
+            .group_end()
+            .build();
 }
