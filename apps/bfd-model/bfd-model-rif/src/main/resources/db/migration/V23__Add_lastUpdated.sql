a245933b2e8f394560fb23e904024ac4dd54bdb5
Rick Hawes <richard.hawes@cms.hhs.gov>
2020-02-04 21:07:28 -0500

    Updates to the comments

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V23__Add_lastUpdated.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V23__Add_lastUpdated.sql
@@ -55,7 +55,8 @@ create sequence loadedFiles_loadedFileId_seq ${logic.sequence-start} 1 ${logic.s
 -- batches, creating a record that represents that batch was considered to be efficient enough.
 -- More importantly since RIF record batches are put inside a transaction, the LoadedBatches
 -- table will always be consistent with the loaded RIF records. Beneficiaries is not an array
--- because Hibernate doesn't work well with that type. 
+-- because Hibernate doesn't work well with that type.
+--
 create table "LoadedBatches" (
   "loadedBatchId" bigint primary key,
   "loadedFileId" bigint not null,               
--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V24__Add_Loaded_index.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V24__Add_Loaded_index.sql
@@ -1,6 +1,7 @@
 /*
- * Add an index on the created timestamp of LoadedBatches table which is frequently searched for column.
- * The index creation is done to avoid mixin transactional and non-transactional statements in migrations
+ * Add an index on the created timestamp of LoadedBatches table which is a frequently searched column.
+ * The index creation is done in a separate migration to avoid mixed
+ * transactional and non-transactional statements in migrations
  */
 
 create index ${logic.index-create-concurrently} "LoadedBatches_created_index"
