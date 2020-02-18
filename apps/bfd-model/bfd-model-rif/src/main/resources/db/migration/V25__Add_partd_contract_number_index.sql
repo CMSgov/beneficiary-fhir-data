b44cefd1c67d21d3445259d45350f5d24378117f
whytheplatypus <whytheplatypus@gmail.com>
2020-02-18 15:56:25 -0500

    Create indexes for partdcontactnumber columns

---
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V25__Add_partd_contract_number_index.sql
@@ -0,0 +1,36 @@
+
+create index ${logic.index-create-concurrently} "Beneficiaries_partd_contract_number_jan_id_idx"
+    on "Beneficiaries" ${logic.using-hash} ("partDContractNumberJanId");
+
+create index ${logic.index-create-concurrently} "Beneficiaries_partd_contract_number_feb_id_idx"
+    on "Beneficiaries" ${logic.using-hash} ("partDContractNumberFebId");
+
+create index ${logic.index-create-concurrently} "Beneficiaries_partd_contract_number_mar_id_idx"
+    on "Beneficiaries" ${logic.using-hash} ("partDContractNumberMarId");
+
+create index ${logic.index-create-concurrently} "Beneficiaries_partd_contract_number_apr_id_idx"
+    on "Beneficiaries" ${logic.using-hash} ("partDContractNumberAprId");
+
+create index ${logic.index-create-concurrently} "Beneficiaries_partd_contract_number_may_id_idx"
+    on "Beneficiaries" ${logic.using-hash} ("partDContractNumberMayId");
+
+create index ${logic.index-create-concurrently} "Beneficiaries_partd_contract_number_jun_id_idx"
+    on "Beneficiaries" ${logic.using-hash} ("partDContractNumberJunId");
+
+create index ${logic.index-create-concurrently} "Beneficiaries_partd_contract_number_jul_id_idx"
+    on "Beneficiaries" ${logic.using-hash} ("partDContractNumberJulId");
+
+create index ${logic.index-create-concurrently} "Beneficiaries_partd_contract_number_aug_id_idx"
+    on "Beneficiaries" ${logic.using-hash} ("partDContractNumberAugId");
+
+create index ${logic.index-create-concurrently} "Beneficiaries_partd_contract_number_sept_id_idx"
+    on "Beneficiaries" ${logic.using-hash} ("partDContractNumberSeptId");
+
+create index ${logic.index-create-concurrently} "Beneficiaries_partd_contract_number_oct_id_idx"
+    on "Beneficiaries" ${logic.using-hash} ("partDContractNumberOctId");
+
+create index ${logic.index-create-concurrently} "Beneficiaries_partd_contract_number_nov_id_idx"
+    on "Beneficiaries" ${logic.using-hash} ("partDContractNumberNovId");
+
+create index ${logic.index-create-concurrently} "Beneficiaries_partd_contract_number_dec_id_idx"
+    on "Beneficiaries" ${logic.using-hash} ("partDContractNumberDecId");
