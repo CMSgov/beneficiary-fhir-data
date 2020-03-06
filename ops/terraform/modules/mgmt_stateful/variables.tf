c4cc8c5b46106463a9f9df7644c755ec29702554
jzulim <john.zulim@adhocteam.us>
2020-03-06 12:07:24 -0800

    Fixup

--- ops/terraform/modules/mgmt_stateful/variables.tf
+++ ops/terraform/modules/mgmt_stateful/variables.tf
@@ -2,8 +2,3 @@ variable "env_config" {
   description       = "All high-level info for the whole vpc"
   type              = object({env=string, tags=map(string), azs=string})
 }
-
-variable "app_subnet" {
-  description       = "The bfd-mgmt-azX-app tpo use. MGMT is az1 and MGMT-TEST is az3"
-  type              = string
-}
