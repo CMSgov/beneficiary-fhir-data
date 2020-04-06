0d08b186b8fe1c2d58574cc1b2bea602345e531f
Rick Hawes <richard.hawes@cms.hhs.gov>
2020-04-06 09:09:04 -0400

    Improve testing

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/stu3/providers/PatientLinkBuilderTest.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/stu3/providers/PatientLinkBuilderTest.java
@@ -10,12 +10,12 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 public class PatientLinkBuilderTest {
-  public static String BASE_CONTRACT_URL =
+  public static String TEST_CONTRACT_URL =
       "https://localhost:443/v1/fhir/Patient?_has:Coverage.extension=https://bluebutton.cms.gov/resources/variables/ptdcntrct02|S0000";
 
   @Test
   public void noCountTest() {
-    PatientLinkBuilder paging = new PatientLinkBuilder(BASE_CONTRACT_URL);
+    PatientLinkBuilder paging = new PatientLinkBuilder(TEST_CONTRACT_URL);
 
     Assert.assertFalse(paging.isPagingRequested());
     Assert.assertTrue(paging.isFirstPage());
@@ -29,7 +29,7 @@ public class PatientLinkBuilderTest {
 
   @Test
   public void emptyPageTest() {
-    PatientLinkBuilder paging = new PatientLinkBuilder(BASE_CONTRACT_URL + "&_count=10");
+    PatientLinkBuilder paging = new PatientLinkBuilder(TEST_CONTRACT_URL + "&_count=10");
 
     Assert.assertTrue(paging.isPagingRequested());
     Assert.assertTrue(paging.isFirstPage());
@@ -38,12 +38,13 @@ public class PatientLinkBuilderTest {
     Bundle bundle = new Bundle();
     Assert.assertTrue(bundle.getLink().isEmpty());
     paging.addLinks(bundle);
+    Assert.assertNotNull(bundle.getLink(Constants.LINK_SELF));
     Assert.assertNotNull(bundle.getLink(Constants.LINK_FIRST));
   }
 
   @Test
   public void onePageTest() {
-    PatientLinkBuilder paging = new PatientLinkBuilder(BASE_CONTRACT_URL + "&_count=10");
+    PatientLinkBuilder paging = new PatientLinkBuilder(TEST_CONTRACT_URL + "&_count=10");
 
     Assert.assertTrue(paging.isPagingRequested());
     Assert.assertTrue(paging.isFirstPage());
@@ -53,6 +54,7 @@ public class PatientLinkBuilderTest {
     TransformerUtils.addResourcesToBundle(bundle, Collections.singletonList(new Patient()));
     Assert.assertTrue(bundle.getLink().isEmpty());
     paging.addLinks(bundle);
+    Assert.assertNotNull(bundle.getLink(Constants.LINK_SELF));
     Assert.assertNotNull(bundle.getLink(Constants.LINK_FIRST));
     UriComponents firstLink =
         UriComponentsBuilder.fromUriString(bundle.getLink(Constants.LINK_FIRST).getUrl()).build();
@@ -63,7 +65,7 @@ public class PatientLinkBuilderTest {
   @Test
   public void fullPageTest() {
     // test a page with a page size of 1 and 1 patient in the result
-    PatientLinkBuilder paging = new PatientLinkBuilder(BASE_CONTRACT_URL + "&_count=1");
+    PatientLinkBuilder paging = new PatientLinkBuilder(TEST_CONTRACT_URL + "&_count=1");
 
     Assert.assertTrue(paging.isPagingRequested());
     Assert.assertTrue(paging.isFirstPage());
@@ -76,6 +78,7 @@ public class PatientLinkBuilderTest {
     Assert.assertTrue(bundle.getLink().isEmpty());
     paging.addLinks(bundle);
 
+    Assert.assertNotNull(bundle.getLink(Constants.LINK_SELF));
     Assert.assertNotNull(bundle.getLink(Constants.LINK_FIRST));
     Assert.assertNotNull(bundle.getLink(Constants.LINK_NEXT));
     UriComponents nextLink =
