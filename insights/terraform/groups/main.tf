f6ce557e4f95f4a629a281aca8ee56534fc2778a
Rick Hawes <richard.hawes@cms.hhs.gov>
2020-04-21 16:09:26 -0400

    Made groups for the backend.tf convention

--- insights/terraform/groups/main.tf
+++ insights/terraform/groups/main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_version = "~> 0.12"
-  # Use the common terraform bucket for all of BFD's state
-  backend "s3" {
-    bucket         = "bfd-tf-state"
-    key            = "bfd-insights/groups/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "bfd-tf-table"
-    encrypt        = "1"
-    kms_key_id     = "alias/bfd-tf-state"
-  }
-}
-
-provider "aws" {
-  version = "~> 2.57"
-  region = "us-east-1"
-}
-
 locals {
   # Add new group members here
   member_map  = {
