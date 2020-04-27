1e84cea9191f6b4fcae6e36edb4155f850d8efae
Nathan Dister <nathan.dister@adhocteam.us>
2020-04-27 14:46:51 -0400

    BLUEBUTTON-1864: Medicare Opt Out bucket fixes, AB2D onboarding (#260)
    * DenyNonExplicit does not work properly, fix KMS policy, remove offboarded user
    
    * Add AB2D roles that need read access to prod-sbx bucket

--- ops/terraform/env/test/stateful/main.tf
+++ ops/terraform/env/test/stateful/main.tf
@@ -59,8 +59,8 @@ module "stateful" {
   victor_ops_url      = var.victor_ops_url
 
   medicare_opt_out_config = {
-    read_roles        = ["arn:aws:iam::755619740999:role/dpc-dev-consent-execution-role"]
+    read_roles        = ["arn:aws:iam::755619740999:role/dpc-dev-consent-execution-role", "arn:aws:iam::349849222861:role/Ab2dInstanceRole", "arn:aws:iam::777200079629:role/Ab2dInstanceRole", "arn:aws:iam::330810004472:role/Ab2dInstanceRole"]
     write_roles       = ["arn:aws:iam::755619740999:role/bcda-dev-nfs-instance", "arn:aws:iam::755619740999:role/bcda-test-nfs-instance"]
-    admin_users       = ["arn:aws:iam::577373831711:user/DS7H", "arn:aws:iam::577373831711:user/VZG9", "arn:aws:iam::577373831711:user/BYSK"]
+    admin_users       = ["arn:aws:iam::577373831711:user/DS7H", "arn:aws:iam::577373831711:user/VZG9"]
   }
 }
