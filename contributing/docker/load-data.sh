623accbf176c8023c9f995c192f879fa4592ef6d
jzulim <john.zulim@adhocteam.us>
2020-05-07 10:32:22 -0700

    Renaming bfddb to local_bfddb for safety.

--- contributing/docker/load-data.sh
+++ contributing/docker/load-data.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
+set -e
 
 cd $BFD_MOUNT_POINT/apps;
 
-mvn clean install -DskipITs
+mvn clean install -DskipITs -DskipTests
 
 cd $BFD_MOUNT_POINT/apps/bfd-pipeline/bfd-pipeline-rif-load;
 
-mvn -Dits.db.url=jdbc:postgresql://bfddb:5432/bfd -Dits.db.username=bfd -Dits.db.password=InsecureLocalDev -Dit.test=RifLoaderIT#loadLocalSyntheticData clean verify
+mvn -Dits.db.url=jdbc:postgresql://local_bfddb:5432/bfd -Dits.db.username=bfd -Dits.db.password=InsecureLocalDev -Dit.test=RifLoaderIT#loadLocalSyntheticData clean verify
--- contributing/docker/start-server.sh
+++ contributing/docker/start-server.sh
@@ -6,8 +6,6 @@ mvn clean install -DskipITs
 
 cd $BFD_MOUNT_POINT/apps/bfd-server
 
-#./bfd-server-war/src/main/config/server-start.sh -j $JAVA_HOME/jre -m -Xmx4g -t /app/apps/bfd-server/bfd-server-war/target -u jdbc:postgresql://bfddb:InsecureLocalDev@bfddb:5432/bfd;
-
-mvn -X -Dits.db.url="jdbc:postgresql://bfddb:5432/bfd?user=bfd&password=InsecureLocalDev" --projects bfd-server-war package dependency:copy antrun:run org.codehaus.mojo:exec-maven-plugin:exec@server-start
+mvn -X -Dits.db.url="jdbc:postgresql://local_bfddb:5432/bfd?user=bfd&password=InsecureLocalDev" --projects bfd-server-war package dependency:copy antrun:run org.codehaus.mojo:exec-maven-plugin:exec@server-start
 
 tail -f $BFD_MOUNT_POINT/apps/bfd-server/bfd-server-war/target/server-work/server-console.log
