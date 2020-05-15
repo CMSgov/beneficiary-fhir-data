5e508038f4914db585d9f5274fd16cfacd91a0d6
Nathan Dister <nathan.dister@adhocteam.us>
2020-05-15 11:00:26 -0400

    Adjustments based on analyzing our traffic

--- ops/terraform/modules/stateless/main.tf
+++ ops/terraform/modules/stateless/main.tf
@@ -344,52 +344,52 @@ module "bfd_server_alarm_all_500s" {
     alarm_name       = "all-500s"
     partner_name     = "all"
     metric_prefix    = "http-requests/count-500"
-    eval_periods     = "30"
+    eval_periods     = "10"
     period           = "60"
-    datapoints       = "30"
+    datapoints       = "10"
     statistic        = "Sum"
     ext_statistic    = null
-    threshold        = "5.0"
+    threshold        = "8.0"
     alarm_notify_arn = data.aws_sns_topic.cloudwatch_alarms.arn
     ok_notify_arn    = data.aws_sns_topic.cloudwatch_ok.arn
   }
 }
 
-module "bfd_server_alarm_all_eob_6s" {
+module "bfd_server_alarm_all_eob_4s-p95" {
   source = "../resources/bfd_server_alarm"
 
   env    = var.env_config.env
 
   alarm_config = {
-    alarm_name       = "all-eob-6s"
+    alarm_name       = "all-eob-4s-p95"
     partner_name     = "all"
     metric_prefix    = "http-requests/latency/eobAll"
-    eval_periods     = "30"
-    period           = "60"
-    datapoints       = "30"
+    eval_periods     = "10"
+    period           = "300"
+    datapoints       = "10"
     statistic        = null
-    ext_statistic    = "p99"
-    threshold        = "6000.0"
+    ext_statistic    = "p95"
+    threshold        = "4000.0"
     alarm_notify_arn = data.aws_sns_topic.cloudwatch_alarms.arn
     ok_notify_arn    = data.aws_sns_topic.cloudwatch_ok.arn
   }
 }
 
-module "bfd_server_alarm_mct_eob_6s" {
+module "bfd_server_alarm_mct_eob_4s_p95" {
   source = "../resources/bfd_server_alarm"
 
   env    = var.env_config.env
 
   alarm_config = {
-    alarm_name       = "mct-eob-6s"
+    alarm_name       = "mct-eob-4s-p95"
     partner_name     = "mct"
     metric_prefix    = "http-requests/latency/eobAll"
     eval_periods     = "10"
     period           = "60"
     datapoints       = "10"
     statistic        = null
-    ext_statistic    = "p99"
-    threshold        = "6000.0"
+    ext_statistic    = "p95"
+    threshold        = "4000.0"
     alarm_notify_arn = data.aws_sns_topic.cloudwatch_alarms.arn
     ok_notify_arn    = data.aws_sns_topic.cloudwatch_ok.arn
   }
