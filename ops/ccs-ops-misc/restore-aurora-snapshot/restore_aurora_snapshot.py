c292731e2b724a23c530b8b36495eb05cc840549
Nathan Dister <nathan.dister@adhocteam.us>
2020-05-19 18:33:22 -0400

    Add more validation, prep next stage of restore

--- ops/ccs-ops-misc/restore-aurora-snapshot/restore_aurora_snapshot.py
+++ ops/ccs-ops-misc/restore-aurora-snapshot/restore_aurora_snapshot.py
@@ -1,5 +1,6 @@
 import boto3
 import botocore.exceptions
+import sys
 
 def main():
     print("--------------------------------------")
@@ -14,7 +15,7 @@ def main():
             source_db_cluster = rds_client.describe_db_clusters(DBClusterIdentifier = source_db_cluster_identifier)["DBClusters"][0]
             source_db_cluster_snapshot_all = rds_client.describe_db_cluster_snapshots(DBClusterIdentifier = source_db_cluster_identifier)["DBClusterSnapshots"]
             break
-        except (botocore.exceptions.ClientError):
+        except botocore.exceptions.ClientError:
             print("Invalid cluster identifier, try again")
 
     print("\nAvailable snapshots:")
@@ -27,12 +28,24 @@ def main():
             source_db_cluster_snapshot = rds_client.describe_db_cluster_snapshots(DBClusterSnapshotIdentifier = source_db_cluster_snapshot_identifier)["DBClusterSnapshots"][0]
             restore_db_cluster_identifier = source_db_cluster_snapshot["DBClusterSnapshotIdentifier"][4:] # Remove "rds:" from snapshot identifier for use as new cluster name
             break
-        except (botocore.exceptions.ClientError):
+        except botocore.exceptions.ClientError:
             print("Invalid cluster snapshot identifier, try again")
 
     print(f"\nSource aurora cluster identifier: {source_db_cluster_identifier}")
     print(f"Source aurora cluster snapshot identifier: {source_db_cluster_snapshot_identifier}")
     print(f"Restore aurora cluster identifier: {restore_db_cluster_identifier}")
+
+    verify_restore = input("\nAre the above details correct (y/n)? ")
+    if verify_restore.lower() != "y":
+        sys.exit("\nExiting script")
+
+    # try:
+    #     print("\nProceeding with restore")
+    #     restore_db_cluster = rds_client.restore_db_cluster_from_snapshot(
+
+    #     )
+    # except botocore.exceptions.ClientError as client_err:
+    #     sys.exit(client_err)
 
 if __name__ == "__main__":
     main()
