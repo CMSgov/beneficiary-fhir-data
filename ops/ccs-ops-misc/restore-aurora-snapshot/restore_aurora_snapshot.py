2ac93a002786a1cc964ded7b358dae2357305c95
Nathan Dister <nathan.dister@adhocteam.us>
2020-05-20 14:20:13 -0400

    Add source instance data, restore cluster routine and wait

--- ops/ccs-ops-misc/restore-aurora-snapshot/restore_aurora_snapshot.py
+++ ops/ccs-ops-misc/restore-aurora-snapshot/restore_aurora_snapshot.py
@@ -1,6 +1,7 @@
 import boto3
 import botocore.exceptions
 import sys
+import time
 
 def main():
     print("--------------------------------------")
@@ -13,10 +14,11 @@ def main():
         try:
             source_db_cluster_identifier = input("\nCluster identifier: ")
             source_db_cluster = rds_client.describe_db_clusters(DBClusterIdentifier = source_db_cluster_identifier)["DBClusters"][0]
+            source_db_cluster_instances = rds_client.describe_db_instances(Filters = [{"Name":"db-cluster-id", "Values":[source_db_cluster_identifier]}])["DBInstances"]
             source_db_cluster_snapshot_all = rds_client.describe_db_cluster_snapshots(DBClusterIdentifier = source_db_cluster_identifier)["DBClusterSnapshots"]
             break
-        except botocore.exceptions.ClientError:
-            print("Invalid cluster identifier, try again")
+        except botocore.exceptions.ClientError as client_err:
+            print(client_err)
 
     print("\nAvailable snapshots:")
     for snapshot in source_db_cluster_snapshot_all:
@@ -28,8 +30,8 @@ def main():
             source_db_cluster_snapshot = rds_client.describe_db_cluster_snapshots(DBClusterSnapshotIdentifier = source_db_cluster_snapshot_identifier)["DBClusterSnapshots"][0]
             restore_db_cluster_identifier = source_db_cluster_snapshot["DBClusterSnapshotIdentifier"][4:] # Remove "rds:" from snapshot identifier for use as new cluster name
             break
-        except botocore.exceptions.ClientError:
-            print("Invalid cluster snapshot identifier, try again")
+        except botocore.exceptions.ClientError as client_err:
+            print(client_err)
 
     print(f"\nSource aurora cluster identifier: {source_db_cluster_identifier}")
     print(f"Source aurora cluster snapshot identifier: {source_db_cluster_snapshot_identifier}")
@@ -38,15 +40,41 @@ def main():
     verify_restore = input("\nAre the above details correct (y/n)? ")
     if verify_restore.lower() != "y":
         sys.exit("\nExiting script")
-    else:
-        print("\nProceeding with restore")
 
-    # try:
-    #     restore_db_cluster = rds_client.restore_db_cluster_from_snapshot(
+    try:
+        print("\nRestoring aurora cluster...")
+        restore_db_cluster = rds_client.restore_db_cluster_from_snapshot(
+            AvailabilityZones = source_db_cluster["AvailabilityZones"],
+            DBClusterIdentifier = restore_db_cluster_identifier,
+            SnapshotIdentifier = source_db_cluster_snapshot_identifier,
+            Engine = source_db_cluster["Engine"],
+            EngineVersion = source_db_cluster["EngineVersion"],
+            Port = source_db_cluster["Port"],
+            DBSubnetGroupName = source_db_cluster["DBSubnetGroup"],
+            VpcSecurityGroupIds = [security_group["VpcSecurityGroupId"] for security_group in source_db_cluster["VpcSecurityGroups"]],
+            KmsKeyId = source_db_cluster["KmsKeyId"],
+            EnableCloudwatchLogsExports = source_db_cluster["EnabledCloudwatchLogsExports"],
+            EngineMode = source_db_cluster["EngineMode"],
+            DBClusterParameterGroupName = source_db_cluster["DBClusterParameterGroup"],
+            DeletionProtection = source_db_cluster["DeletionProtection"],
+            CopyTagsToSnapshot = source_db_cluster["CopyTagsToSnapshot"]
+        )
+    except botocore.exceptions.ClientError as client_err:
+        sys.exit(client_err)
 
-    #     )
-    # except botocore.exceptions.ClientError as client_err:
-    #     sys.exit(client_err)
+    while True:
+        restore_db_cluster = rds_client.describe_db_clusters(DBClusterIdentifier = restore_db_cluster_identifier)["DBClusters"][0]
+        if restore_db_cluster["Status"].lower() == "available":
+            break
+        time.sleep(30)
+        print(".", end = "")
+
+    try:
+        print("Restore of aurora cluster complete!")
+        print("Creating cluster instances...")
+
+    except botocore.exceptions.ClientError as client_err:
+        sys.exit(client_err)
 
 if __name__ == "__main__":
     main()
