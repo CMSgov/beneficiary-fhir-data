8a2e22c63ad53775ec65ec07ac11bb0e68c9b9ed
Chris Brune <cbrune@fearless.tech>
2020-07-27 09:32:41 -0400

    SPacing and alignment of code

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/stu3/providers/PartDEventTransformer.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/stu3/providers/PartDEventTransformer.java
@@ -271,26 +271,32 @@ final class PartDEventTransformer {
         case "01":
           eob.setOrganization(
               TransformerUtils.createIdentifierReference(
-                  TransformerConstants.CODING_NPI_US, claimGroup.getServiceProviderId()));
+                  TransformerConstants.CODING_NPI_US, 
+                  claimGroup.getServiceProviderId()));
           eob.setFacility(
               TransformerUtils.createIdentifierReference(
-                  TransformerConstants.CODING_NPI_US, claimGroup.getServiceProviderId()));
+                  TransformerConstants.CODING_NPI_US, 
+                  claimGroup.getServiceProviderId()));
           break;
         case "06":
           eob.setOrganization(
               TransformerUtils.createIdentifierReference(
-                  TransformerConstants.CODING_UPIN, claimGroup.getServiceProviderId()));
+                  TransformerConstants.CODING_UPIN, 
+                  claimGroup.getServiceProviderId()));
           eob.setFacility(
               TransformerUtils.createIdentifierReference(
-                  TransformerConstants.CODING_UPIN, claimGroup.getServiceProviderId()));
+                  TransformerConstants.CODING_UPIN, 
+                  claimGroup.getServiceProviderId()));
           break;
         case "07":
           eob.setOrganization(
               TransformerUtils.createIdentifierReference(
-                  TransformerConstants.CODING_NCPDP, claimGroup.getServiceProviderId()));
+                  TransformerConstants.CODING_NCPDP, 
+                  claimGroup.getServiceProviderId()));
           eob.setFacility(
               TransformerUtils.createIdentifierReference(
-                  TransformerConstants.CODING_NCPDP, claimGroup.getServiceProviderId()));
+                  TransformerConstants.CODING_NCPDP, 
+                  claimGroup.getServiceProviderId()));
           break;
         case "08":
           eob.setOrganization(
@@ -315,10 +321,12 @@ final class PartDEventTransformer {
         case "99":
           eob.setOrganization(
               TransformerUtils.createIdentifierReference(
-                  TransformerConstants.CODING_OTHER, claimGroup.getServiceProviderId()));
+                  TransformerConstants.CODING_OTHER, 
+                  claimGroup.getServiceProviderId()));
           eob.setFacility(
               TransformerUtils.createIdentifierReference(
-                  TransformerConstants.CODING_OTHER, claimGroup.getServiceProviderId()));
+                  TransformerConstants.CODING_OTHER, 
+                  claimGroup.getServiceProviderId()));
           break;
       }
 
