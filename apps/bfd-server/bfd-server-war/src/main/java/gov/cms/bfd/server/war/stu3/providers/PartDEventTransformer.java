6cde8711858d191f965a94c1ede5c9c6a70dd64e
Chris Brune <cbrune@fearless.tech>
2020-08-07 17:27:44 -0400

    Changes to part d events

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/stu3/providers/PartDEventTransformer.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/stu3/providers/PartDEventTransformer.java
@@ -247,7 +247,7 @@ final class PartDEventTransformer {
                 CcwCodebookVariable.DAYS_SUPLY_NUM, claimGroup.getDaysSupply()));
 
     /*
-     * This chart is to dosplay the different code values for the different service provider id qualifer 
+     * This chart is to dosplay the different code values for the different service provider id qualifer
      * codes below
      *   Code	    Code value
      *   01        National Provider Identifier (NPI)
