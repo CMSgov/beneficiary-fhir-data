5340bafd959c967833c97c68432ce26999c211c6
Chris Brune <cbrune@fearless.tech>
2020-08-19 19:08:54 -0400

    Got rid of schema

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V26__Add_fields_to_BeneficiariesInpatient.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V26__Add_fields_to_BeneficiariesInpatient.sql
@@ -3,18 +3,20 @@
  * will populate the column as new beneficaries are added or existing beneficaries are updated. 
  */
 
-alter table public."Beneficiaries" add column "derivedMailingAddress1" varchar(40);
-alter table public."Beneficiaries" add column "derivedMailingAddress2" varchar(40);
-alter table public."Beneficiaries" add column "derivedMailingAddress3" varchar(40);
-alter table public."Beneficiaries" add column "derivedMailingAddress4" varchar(40);
-alter table public."Beneficiaries" add column "derivedMailingAddress5" varchar(40);
-alter table public."Beneficiaries" add column "derivedMailingAddress6" varchar(40);
-alter table public."Beneficiaries" add column "derivedCityName" varchar(100);
-alter table public."Beneficiaries" add column "derivedStateCode" varchar(2);
-alter table public."Beneficiaries" add column "derivedZipCode" varchar(9);
-alter table public."Beneficiaries" add column "mbiEffectiveDate" date;
-alter table public."Beneficiaries" add column "mbiObsoleteDate" date;
-alter table public."Beneficiaries" add column "beneLinkKey" numeric(38);
-alter table public."BeneficiariesHistory" add column "mbiEffectiveDate" date;
-alter table public."BeneficiariesHistory" add column "mbiObsoleteDate" date;
-alter table public."InpatientClaimLines" add column "clmUncompensatedCareAmount" numeric(38, 2);
+alter table "Beneficiaries" add column "derivedMailingAddress1" varchar(40);
+alter table "Beneficiaries" add column "derivedMailingAddress2" varchar(40);
+alter table "Beneficiaries" add column "derivedMailingAddress3" varchar(40);
+alter table "Beneficiaries" add column "derivedMailingAddress4" varchar(40);
+alter table "Beneficiaries" add column "derivedMailingAddress5" varchar(40);
+alter table "Beneficiaries" add column "derivedMailingAddress6" varchar(40);
+alter table "Beneficiaries" add column "derivedCityName" varchar(100);
+alter table "Beneficiaries" add column "derivedStateCode" varchar(2);
+alter table "Beneficiaries" add column "derivedZipCode" varchar(9);
+alter table "Beneficiaries" add column "mbiEffectiveDate" date;
+alter table "Beneficiaries" add column "mbiObsoleteDate" date;
+alter table "Beneficiaries" add column "beneLinkKey" numeric(38);
+
+alter table "BeneficiariesHistory" add column "mbiEffectiveDate" date;
+alter table "BeneficiariesHistory" add column "mbiObsoleteDate" date;
+
+alter table "InpatientClaimLines" add column "clmUncompensatedCareAmount" numeric(38, 2);
