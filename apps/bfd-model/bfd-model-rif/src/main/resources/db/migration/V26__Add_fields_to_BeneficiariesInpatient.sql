fd2665cdf34a753788609c2444fe42418d3545b9
Chris Brune <cbrune@fearless.tech>
2020-08-26 15:00:42 -0400

    Added fi document control num and fi original control num

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V26__Add_fields_to_BeneficiariesInpatient.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V26__Add_fields_to_BeneficiariesInpatient.sql
@@ -25,3 +25,17 @@ alter table "CarrierClaimLines" add column "clmControlNumber" varchar(23) not nu
 
 alter table "DMEClaimLines" add column "clmControlNumber" varchar(23) not null;
 
+alter table "HHAClaimLines" add column "fiDocumentClaimControlNumber" varchar(23) not null;
+alter table "HHAClaimLines" add column "fiOriginalClaimControlNumber" varchar(23) not null;
+
+alter table "HospiceClaimLines" add column "fiDocumentClaimControlNumber" varchar(23) not null;
+alter table "HospiceClaimLines" add column "fiOriginalClaimControlNumber" varchar(23) not null;
+
+alter table "InpatientClaimLines" add column "fiDocumentClaimControlNumber" varchar(23) not null;
+alter table "InpatientClaimLines" add column "fiOriginalClaimControlNumber" varchar(23) not null;
+
+alter table "OutpatientClaimLines" add column "fiDocumentClaimControlNumber" varchar(23) not null;
+alter table "OutpatientClaimLines" add column "fiOriginalClaimControlNumber" varchar(23) not null;
+
+alter table "SNFClaimLines" add column "fiDocumentClaimControlNumber" varchar(23) not null;
+alter table "SNFClaimLines" add column "fiOriginalClaimControlNumber" varchar(23) not null;
