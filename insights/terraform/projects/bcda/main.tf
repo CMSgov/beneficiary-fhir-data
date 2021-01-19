13cfaa6aa02dd21b106bd7200cd541e9b2c0942e
Oren Fromberg <oren@adhocteam.us>
2021-01-19 10:24:30 -0500

    Adding opensbx and prod insights tables

--- insights/terraform/projects/bcda/main.tf
+++ insights/terraform/projects/bcda/main.tf
@@ -31,65 +31,65 @@ data "aws_kms_alias" "moderate_cmk" {
 ## Athena workgroup
 
 module "workgroup" {
-  source          = "../../modules/workgroup"
-  bucket          = module.bucket.id
-  bucket_cmk      = module.bucket.bucket_cmk
-  name            = local.database
-  tags            = local.tags
+  source     = "../../modules/workgroup"
+  bucket     = module.bucket.id
+  bucket_cmk = module.bucket.bucket_cmk
+  name       = local.database
+  tags       = local.tags
 }
 
 ## Database for the project
 
 module "database" {
-  source          = "../../modules/database"
-  database        = local.database
-  bucket          = module.bucket.id
-  bucket_cmk      = module.bucket.bucket_cmk
-  tags            = local.tags
+  source     = "../../modules/database"
+  database   = local.database
+  bucket     = module.bucket.id
+  bucket_cmk = module.bucket.bucket_cmk
+  tags       = local.tags
 }
 
 ## Glue setup
 
 module "glue_jobs" {
-  source          = "../../modules/jobs"
-  project         = local.project
-  tags            = local.tags
+  source  = "../../modules/jobs"
+  project = local.project
+  tags    = local.tags
 
   # Setup access to both the AB2D and common moderate bucket
-  buckets         = [
-    {bucket = module.bucket.arn, cmk = module.bucket.bucket_cmk_arn},
-    {bucket = data.aws_s3_bucket.moderate_bucket.arn, cmk = data.aws_kms_alias.moderate_cmk.arn }
+  buckets = [
+    { bucket = module.bucket.arn, cmk = module.bucket.bucket_cmk_arn },
+    { bucket = data.aws_s3_bucket.moderate_bucket.arn, cmk = data.aws_kms_alias.moderate_cmk.arn }
   ]
 }
 
-module "get_num_jobs_opensbx" {
-  source          = "../../modules/table"
-  database        = module.database.name          # adds a dependency
-  table           = "opensbx_get_num_jobs" # "${environment}_${name of job}"
-  description     = "gets number of data requests (jobs)"
-  bucket          = module.bucket.id
-  bucket_cmk      = module.bucket.bucket_cmk
-  tags            = local.tags
-  partitions      = local.partitions
+module "insights_data_table_opensbx" {
+  source      = "../../modules/table"
+  database    = module.database.name
+  table       = "opensbx_insights"
+  description = "opensbx insights data"
+  bucket      = module.bucket.id
+  bucket_cmk  = module.bucket.bucket_cmk
+  tags        = local.tags
+  partitions  = local.partitions
   columns = [
-    {name="name", type="string", comment=""},
-    {name="timestamp", type="bigint", comment=""},
-    {name="data", type="array<struct<count:string>>", comment=""},
+    { name = "name", type = "string", comment = "" },
+    { name = "timestamp", type = "bigint", comment = "" },
+    { name = "json_result", type = "string", comment = "" },
   ]
 }
 
-module "get_num_queued_tasks_opensbx" {
-  source          = "../../modules/table"
-  database        = module.database.name          # adds a dependency
-  table           = "opensbx_get_num_queued_tasks" # "${environment}_${name of job}"
-  description     = "gets number of queued job tasks"
-  bucket          = module.bucket.id
-  bucket_cmk      = module.bucket.bucket_cmk
-  tags            = local.tags
-  partitions      = local.partitions
+module "insights_data_table_prod" {
+  source      = "../../modules/table"
+  database    = module.database.name
+  table       = "prod_insights"
+  description = "prod insights data"
+  bucket      = module.bucket.id
+  bucket_cmk  = module.bucket.bucket_cmk
+  tags        = local.tags
+  partitions  = local.partitions
   columns = [
-    {name="name", type="string", comment=""},
-    {name="timestamp", type="bigint", comment=""},
-    {name="data", type="array<struct<count:string>>", comment=""},
+    { name = "name", type = "string", comment = "" },
+    { name = "timestamp", type = "bigint", comment = "" },
+    { name = "json_result", type = "string", comment = "" },
   ]
 }
