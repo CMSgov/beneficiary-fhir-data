9c04386f444fe7e8ae4891201075afbb733068e7
jzulim <john.zulim@adhocteam.us>
2021-01-27 15:51:11 -0800

    uncomment getopt validation

--- apps/bfd-server/bfd-server-war/src/main/config/server-start.sh
+++ apps/bfd-server/bfd-server-war/src/main/config/server-start.sh
@@ -41,7 +41,7 @@ scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TEMP=`getopt \
 	j:m:v:t:u:e: \
 	$*`
-#if [ $? != 0 ] ; then echo "Terminating." >&2 ; exit 1 ; fi
+if [ $? != 0 ] ; then echo "Terminating." >&2 ; exit 1 ; fi
 
 # Note the quotes around `$TEMP': they are essential!
 eval set -- "$TEMP"
