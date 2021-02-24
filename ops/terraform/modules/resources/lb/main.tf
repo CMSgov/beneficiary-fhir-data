8801776e051ea59d12fd2ff8cb98d643d38d62e5
keithdadkins <keithdadkins@me.com>
2021-02-24 11:02:50 -0600

    Format existing terraform code to meet Terraform language conventions.
    This is the result of running `terraform fmt -recursive`. The `terraform
    fmt` command rewrites terraform code to a canonical format and style.
    E.g, gets rid of all the crazy spaces and tabs found in our code base.

--- ops/terraform/modules/resources/lb/main.tf
+++ ops/terraform/modules/resources/lb/main.tf
@@ -3,8 +3,8 @@
 # Create an internal application LB with TCP listeners. 
 #
 locals {
-  tags        = merge({Layer=var.layer, role=var.role}, var.env_config.tags)
-  log_prefix  = "${var.role}_elb_access_logs" 
+  tags       = merge({ Layer = var.layer, role = var.role }, var.env_config.tags)
+  log_prefix = "${var.role}_elb_access_logs"
 }
 
 ##
@@ -25,8 +25,8 @@ data "aws_subnet" "app_subnets" {
   vpc_id            = var.env_config.vpc_id
   availability_zone = var.env_config.azs[count.index]
   filter {
-    name    = "tag:Layer"
-    values  = [var.layer] 
+    name   = "tag:Layer"
+    values = [var.layer]
   }
 }
 
@@ -45,63 +45,63 @@ data "aws_s3_bucket" "logs" {
 # A ELB v1 TCP allows the EC2 instance to terminate the TLS connection, matching the HealthApt environment. 
 #
 resource "aws_elb" "main" {
-  name                  = "bfd-${var.env_config.env}-${var.role}"
-  tags                  = local.tags
+  name = "bfd-${var.env_config.env}-${var.role}"
+  tags = local.tags
 
-  internal              = !var.is_public
-  subnets               = data.aws_subnet.app_subnets[*].id # Gives AZs and VPC association
-  security_groups       = [aws_security_group.lb.id]
+  internal        = ! var.is_public
+  subnets         = data.aws_subnet.app_subnets[*].id # Gives AZs and VPC association
+  security_groups = [aws_security_group.lb.id]
 
-  cross_zone_load_balancing   = false   # Match HealthApt
-  idle_timeout                = 60      # (seconds) Match HealthApt
+  cross_zone_load_balancing   = false # Match HealthApt
+  idle_timeout                = 60    # (seconds) Match HealthApt
   connection_draining         = true
   connection_draining_timeout = 60
 
   listener {
-    lb_protocol         = "TCP"
-    lb_port             = var.ingress.port
-    instance_protocol   = "TCP"
-    instance_port       = var.egress.port
+    lb_protocol       = "TCP"
+    lb_port           = var.ingress.port
+    instance_protocol = "TCP"
+    instance_port     = var.egress.port
   }
 
   health_check {
-    healthy_threshold   = 5   # Match HealthApt
-    unhealthy_threshold = 2   # Match HealthApt
+    healthy_threshold   = 5 # Match HealthApt
+    unhealthy_threshold = 2 # Match HealthApt
     target              = "TCP:${var.egress.port}"
-    interval            = 10  # (seconds) Match HealthApt
-    timeout             = 5   # (seconds) Match HealthApt
-  } 
+    interval            = 10 # (seconds) Match HealthApt
+    timeout             = 5  # (seconds) Match HealthApt
+  }
 
   access_logs {
-    enabled             = false
-    bucket              = var.log_bucket
-    bucket_prefix       = local.log_prefix
-    interval            = 5   # (minutes) Match HealthApt      
+    enabled       = false
+    bucket        = var.log_bucket
+    bucket_prefix = local.log_prefix
+    interval      = 5 # (minutes) Match HealthApt      
   }
 }
 
 # Security Group for LB
 #
 resource "aws_security_group" "lb" {
-  name            = "bfd-${var.env_config.env}-${var.role}-lb"
-  description     = "Allow access to the ${var.role} load-balancer"
-  vpc_id          = var.env_config.vpc_id
-  tags            = merge({Name="bfd-${var.env_config.env}-${var.role}-lb"}, local.tags)
+  name        = "bfd-${var.env_config.env}-${var.role}-lb"
+  description = "Allow access to the ${var.role} load-balancer"
+  vpc_id      = var.env_config.vpc_id
+  tags        = merge({ Name = "bfd-${var.env_config.env}-${var.role}-lb" }, local.tags)
 
   ingress {
-    from_port     = var.ingress.port
-    to_port       = var.ingress.port
-    protocol      = "tcp"
-    cidr_blocks   = var.ingress.cidr_blocks
-    description   = var.ingress.description
+    from_port   = var.ingress.port
+    to_port     = var.ingress.port
+    protocol    = "tcp"
+    cidr_blocks = var.ingress.cidr_blocks
+    description = var.ingress.description
   }
 
   egress {
-    from_port     = var.egress.port
-    to_port       = var.egress.port
-    protocol      = "tcp"
-    cidr_blocks   = var.egress.cidr_blocks
-    description   = var.egress.description
+    from_port   = var.egress.port
+    to_port     = var.egress.port
+    protocol    = "tcp"
+    cidr_blocks = var.egress.cidr_blocks
+    description = var.egress.description
   }
 }
 
