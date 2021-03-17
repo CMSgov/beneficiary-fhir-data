b67e05eebb373544c5e3d83a2a6bc856a3b6c638
Chris Brune <cbrune@fearless.tech>
2021-03-17 11:44:53 -0400

    Create index for yearmonth and partdcontractnumber

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V28__Create_BeneficiaryMonthlytable_Index.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V28__Create_BeneficiaryMonthlytable_Index.sql
@@ -1,4 +1,4 @@
--- Create "beneficiaryId" column indexes for claim tables.
+-- Create index for BeneficiaryMonthly for column indexes yearMonth and partDContractNumberId.
 
 CREATE INDEX "BeneficiaryMonthly_yearmonth_partDContractNumberId_idx" 
 ON "BeneficiaryMonthly" ("yearMonth", "partDContractNumberId");
