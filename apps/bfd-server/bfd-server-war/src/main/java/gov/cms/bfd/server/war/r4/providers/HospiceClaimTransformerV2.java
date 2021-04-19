75f9d4b278ac07701e6471c6995669f057becece
Jeremy Rosenfeld <jeremy.rosenfeld@adhocteam.us>
2021-04-19 13:33:49 -0700

    Remap item qty to ndc quantity for Hospice

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/r4/providers/HospiceClaimTransformerV2.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/r4/providers/HospiceClaimTransformerV2.java
@@ -191,8 +191,7 @@ public class HospiceClaimTransformerV2 {
       // REV_CNTR_RATE_AMT          => ExplanationOfBenefit.item.adjudication
       // REV_CNTR_TOT_CHRG_AMT      => ExplanationOfBenefit.item.adjudication
       // REV_CNTR_NCVRD_CHRG_AMT    => ExplanationOfBenefit.item.adjudication
-      // REV_CNTR_UNIT_CNT          => ExplanationOfBenefit.item.quantity
-      // REV_CNTR_NDC_QTY           => TODO: ??
+      // REV_CNTR_NDC_QTY           => ExplanationOfBenefit.item.quantity
       // REV_CNTR_NDC_QTY_QLFR_CD   => ExplanationOfBenefit.modifier
       TransformerUtilsV2.mapEobCommonItemRevenue(
           item,
@@ -201,7 +200,6 @@ public class HospiceClaimTransformerV2 {
           line.getRateAmount(),
           line.getTotalChargeAmount(),
           line.getNonCoveredChargeAmount(),
-          line.getUnitCount(),
           line.getNationalDrugCodeQuantity(),
           line.getNationalDrugCodeQualifierCode());
 
