4d36f90db55dc5af82e769888cf211b5037c006b
Andrew Fulks <andrew@adhocteam.us>
2021-04-22 17:05:22 -0500

    BFD-619 Removed files for filtering prototype that were accidentally included in the branch

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/commons/adapter/ClaimLineAdaptor.java
+++
@@ -1,9 +0,0 @@
-package gov.cms.bfd.server.war.commons.adapter;
-
-import java.util.Optional;
-
-public abstract class ClaimLineAdaptor implements ClaimLineAdaptorInterface {
-  public Optional<String> getNationalDrugCode() {
-    return Optional.empty();
-  }
-}
--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/commons/adapter/ClaimLineAdaptorInterface.java
+++
@@ -1,7 +0,0 @@
-package gov.cms.bfd.server.war.commons.adapter;
-
-import java.util.Optional;
-
-public interface ClaimLineAdaptorInterface {
-  public Optional<String> getNationalDrugCode();
-}
