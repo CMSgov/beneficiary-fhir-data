a7efa65080b52e6f90566d7e8dacedabb579473f
mackec1 <chris.mackenzie@adhocteam.us>
2021-05-04 16:55:58 -0400

    Minor changes (mainly convert tab to spaces) per code review

--- sql/bene_monthly_audit.sql
+++ sql/bene_monthly_audit.sql
@@ -2,23 +2,24 @@ DROP TABLE IF EXISTS public.beneficiary_monthly_audit;
 
 CREATE TABLE public.beneficiary_monthly_audit
 (
-    "parentBeneficiary" 				character varying(255) NOT NULL,
-    "yearMonth" 						date NOT NULL,
-    "fipsStateCntyCode" 				character varying(5),
-    "medicareStatusCode" 				character varying(2),
-    "entitlementBuyInInd" 				character(1),
-    "hmoIndicatorInd" 					character(1),
-    "partCContractNumberId" 			character varying(5),
-    "partCPbpNumberId" 					character varying(3) ,
-    "partCPlanTypeCode" 				character varying(3),
-    "partDContractNumberId" 			character varying(5),
-    "partDPbpNumberId" 					character varying(3),
-    "partDSegmentNumberId" 				character varying(3),
-    "partDRetireeDrugSubsidyInd" 		char(1),
-    "medicaidDualEligibilityCode" 		character varying(2),
-    "partDLowIncomeCostShareGroupCode"	character varying(2),
-    "operation"                			char(1)   NOT NULL,
-    "updateTs"                 			timestamp NOT NULL
+    "parentBeneficiary"                    character varying(255) NOT NULL,
+    "yearMonth"                            date NOT NULL,
+    "fipsStateCntyCode"                    character varying(5),
+    "medicareStatusCode"                   character varying(2),
+    "entitlementBuyInInd"                  character(1),
+    "hmoIndicatorInd"                      character(1),
+    "partCContractNumberId"                character varying(5),
+    "partCPbpNumberId"                     character varying(3) ,
+    "partCPlanTypeCode"                    character varying(3),
+    "partDContractNumberId"                character varying(5),
+    "partDPbpNumberId"                     character varying(3),
+    "partDSegmentNumberId"                 character varying(3),
+    "partDRetireeDrugSubsidyInd"           char(1),
+    "medicaidDualEligibilityCode"          character varying(2),
+    "partDLowIncomeCostShareGroupCode"     character varying(2),
+    "operation"                            char(1)   NOT NULL,
+    "updateTs"                             timestamp NOT NULL,
+    CONSTRAINT "BeneficiaryMonthly_pkey"   PRIMARY KEY ("parentBeneficiary", "yearMonth")
 )
 WITH (
     OIDS = FALSE
--- sql/track_bene_monthly_change_func.sql
+++ sql/track_bene_monthly_change_func.sql
@@ -6,31 +6,11 @@ AS $beneficiary_monthly_audit$
         -- on BeneficiaryMonthly; variable TG_OP denotes the operation.
         --
         IF (TG_OP = 'UPDATE') THEN
-        /*
-        	IF (
-        		OLD."parentBeneficiary" 				<> NEW."parentBeneficiary"
-			OR	OLD."yearMonth"  						<> NEW."yearMonth"
-			OR	OLD."fipsStateCntyCode" 				<> NEW."fipsStateCntyCode"
-			OR	OLD."medicareStatusCode" 				<> NEW."medicareStatusCode"
-			OR	OLD."entitlementBuyInInd" 				<> NEW."entitlementBuyInInd"
-			OR	OLD."hmoIndicatorInd" 					<> NEW."hmoIndicatorInd"
-			OR	OLD."partCContractNumberId" 			<> NEW."partCContractNumberId"
-			OR	OLD."partCPbpNumberId" 					<> NEW."partCContractNumberId"
-			OR	OLD."partCPlanTypeCode" 				<> NEW."partCPlanTypeCode"
-			OR	OLD."partDContractNumberId" 			<> NEW."partDContractNumberId"
-			OR	OLD."partDPbpNumberId" 					<> NEW."partDPbpNumberId"
-			OR	OLD."partDSegmentNumberId" 				<> NEW."partDSegmentNumberId"
-			OR	OLD."partDRetireeDrugSubsidyInd" 		<> NEW."partDRetireeDrugSubsidyInd"
-			OR	OLD."medicaidDualEligibilityCode" 		<> NEW."medicaidDualEligibilityCode"
-			OR	OLD."partDLowIncomeCostShareGroupCode" 	<> NEW."partDLowIncomeCostShareGroupCode"
-			)
-			THEN
-		*/
-				INSERT INTO public.beneficiary_monthly_audit VALUES (OLD.*, 'U', now());
-			--END IF;
+                INSERT INTO public.beneficiary_monthly_audit VALUES (OLD.*, 'U', now());
+            --END IF;
             RETURN NEW;
         ELSIF (TG_OP = 'INSERT') THEN
-			INSERT INTO public.beneficiary_monthly_audit VALUES(NEW.*, 'I', now());
+            INSERT INTO public.beneficiary_monthly_audit VALUES(NEW.*, 'I', now());
             RETURN NEW;
         END IF;
         RETURN NULL; -- result is ignored since this is an AFTER trigger
