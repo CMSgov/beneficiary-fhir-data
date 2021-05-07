fa0d388fb25cee9bf23572c5f55aa4e4882139ce
Chris Brune <cbrune@fearless.tech>
2021-05-07 17:44:45 -0400

    Added a script to delete the negative claim ids in the three different claim types, Carrier, Inpatient, and Outpatient

---
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/scripts/Delete_Synthetic_Negative_Claims.sql
@@ -0,0 +1,23 @@
+DELETE 
+FROM "public"."CarrierClaimLines"
+WHERE "parentClaim" LIKE '-%';
+
+DELETE 
+FROM "public"."CarrierClaims"
+WHERE "claimId" LIKE '-%';
+
+DELETE 
+FROM "public"."InpatientClaimLines"
+WHERE "parentClaim" LIKE '-%';
+
+DELETE 
+FROM "public"."InpatientClaims"
+WHERE "claimId" LIKE '-%';
+
+DELETE 
+FROM "public"."OutpatientClaimLines"
+WHERE "parentClaim" LIKE '-%';
+
+DELETE 
+FROM "public"."OutpatientClaims"
+WHERE "claimId" LIKE '-%';
