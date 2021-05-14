0cdb38e4790fee2658fa9f59e5e77e55e08048b7
Jeremy Rosenfeld <jeremy.rosenfeld@adhocteam.us>
2021-05-14 16:30:31 -0700

    add unit tests to line items for dme

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/DMEClaimTransformerV2Test.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/DMEClaimTransformerV2Test.java
@@ -3,21 +3,37 @@ package gov.cms.bfd.server.war.r4.providers;
 import ca.uhn.fhir.context.FhirContext;
 import com.codahale.metrics.MetricRegistry;
 import gov.cms.bfd.model.rif.DMEClaim;
-import gov.cms.bfd.model.rif.samples.StaticRifResource;
 import gov.cms.bfd.model.rif.samples.StaticRifResourceGroup;
 import gov.cms.bfd.server.war.ServerTestUtils;
 import gov.cms.bfd.server.war.commons.MedicareSegment;
+import gov.cms.bfd.server.war.commons.ProfileConstants;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.hl7.fhir.exceptions.FHIRException;
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.DateType;
 import org.hl7.fhir.r4.model.ExplanationOfBenefit;
+import org.hl7.fhir.r4.model.ExplanationOfBenefit.CareTeamComponent;
+import org.hl7.fhir.r4.model.ExplanationOfBenefit.DiagnosisComponent;
+import org.hl7.fhir.r4.model.ExplanationOfBenefit.ExplanationOfBenefitStatus;
+import org.hl7.fhir.r4.model.ExplanationOfBenefit.InsuranceComponent;
+import org.hl7.fhir.r4.model.ExplanationOfBenefit.SupportingInformationComponent;
+import org.hl7.fhir.r4.model.ExplanationOfBenefit.Use;
+import org.hl7.fhir.r4.model.Extension;
+import org.hl7.fhir.r4.model.Reference;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 /** Unit tests for {@link gov.cms.bfd.server.war.v4.providers.DMEClaimTransformerV2}. */
 public final class DMEClaimTransformerV2Test {
+  DMEClaim claim;
+  ExplanationOfBenefit eob;
   /**
    * Generates the Claim object to be used in multiple tests
    *
@@ -40,22 +56,291 @@ public final class DMEClaimTransformerV2Test {
     return claim;
   }
 
-  /**
-   * Verifies that {@link
-   * gov.cms.bfd.server.war.stu3.providers.DMEClaimTransformerV2#transform(Object)} works as
-   * expected when run against the {@link StaticRifResource#SAMPLE_A_DME} {@link DMEClaim}.
-   *
-   * @throws FHIRException (indicates test failure)
-   */
-  @Test
-  public void transformSampleARecord() throws FHIRException {
-    DMEClaim claim = generateClaim();
-
-    assertMatches(
-        claim, DMEClaimTransformerV2.transform(new MetricRegistry(), claim, Optional.of(false)));
+  @Before
+  public void before() {
+    claim = generateClaim();
+    eob = DMEClaimTransformerV2.transform(new MetricRegistry(), claim, Optional.empty());
   }
 
   private static final FhirContext fhirContext = FhirContext.forR4();
+
+  @Test
+  public void shouldSetID() {
+    Assert.assertEquals("dme-" + claim.getClaimId(), eob.getId());
+  }
+
+  @Test
+  public void shouldSetLastUpdated() {
+    Assert.assertNotNull(eob.getMeta().getLastUpdated());
+  }
+
+  @Test
+  public void shouldSetCorrectProfile() {
+    // The base CanonicalType doesn't seem to compare correctly so lets convert it
+    // to a string
+    Assert.assertTrue(
+        eob.getMeta().getProfile().stream()
+            .map(ct -> ct.getValueAsString())
+            .anyMatch(v -> v.equals(ProfileConstants.C4BB_EOB_INPATIENT_PROFILE_URL)));
+  }
+
+  @Test
+  public void shouldSetUse() {
+    Assert.assertEquals(Use.CLAIM, eob.getUse());
+  }
+
+  @Test
+  public void shouldSetFinalAction() {
+    Assert.assertEquals(ExplanationOfBenefitStatus.ACTIVE, eob.getStatus());
+  }
+
+  @Test
+  public void shouldSetBillablePeriod() throws Exception {
+    // We just want to make sure it is set
+    Assert.assertNotNull(eob.getBillablePeriod());
+    Assert.assertEquals(
+        (new SimpleDateFormat("yyy-MM-dd")).parse("2014-02-03"),
+        eob.getBillablePeriod().getStart());
+    Assert.assertEquals(
+        (new SimpleDateFormat("yyy-MM-dd")).parse("2014-02-03"), eob.getBillablePeriod().getEnd());
+  }
+
+  @Test
+  public void shouldReferencePatient() {
+    Assert.assertNotNull(eob.getPatient());
+    Assert.assertEquals("Patient/567834", eob.getPatient().getReference());
+  }
+
+  @Test
+  public void shouldHaveCreatedDate() {
+    Assert.assertNotNull(eob.getCreated());
+  }
+
+  /**
+   * CareTeam list
+   *
+   * <p>Based on how the code currently works, we can assume that the same CareTeam members always
+   * are added in the same order. This means we can look them up by sequence number.
+   */
+  @Test
+  public void shouldHaveCareTeamList() {
+    Assert.assertEquals(2, eob.getCareTeam().size());
+  }
+
+  /**
+   * Testing all of these in one test, just because there isn't a distinct identifier really for
+   * each
+   */
+  @Test
+  public void shouldHaveCareTeamMembers() {
+    // First member
+    CareTeamComponent member1 = TransformerTestUtilsV2.findCareTeamBySequence(1, eob.getCareTeam());
+    Assert.assertEquals("1306849450", member1.getProvider().getIdentifier().getValue());
+
+    // Second member
+    CareTeamComponent member2 = TransformerTestUtilsV2.findCareTeamBySequence(2, eob.getCareTeam());
+    Assert.assertEquals("1244444444", member2.getProvider().getIdentifier().getValue());
+  }
+
+  /** SupportingInfo items */
+  @Test
+  public void shouldHaveSupportingInfoList() {
+    Assert.assertEquals(2, eob.getSupportingInfo().size());
+  }
+
+  @Test
+  public void shouldHaveClaimReceivedDateSupInfo() {
+    SupportingInformationComponent sic =
+        TransformerTestUtilsV2.findSupportingInfoByCode("clmrecvddate", eob.getSupportingInfo());
+
+    SupportingInformationComponent compare =
+        TransformerTestUtilsV2.createSupportingInfo(
+                // We don't care what the sequence number is here
+                sic.getSequence(),
+                // Category
+                Arrays.asList(
+                    new Coding(
+                        "http://hl7.org/fhir/us/carin-bb/CodeSystem/C4BBSupportingInfoType",
+                        "clmrecvddate",
+                        "Claim Received Date"),
+                    new Coding(
+                        "https://bluebutton.cms.gov/resources/codesystem/information",
+                        "https://bluebutton.cms.gov/resources/variables/nch_wkly_proc_dt",
+                        "NCH Weekly Claim Processing Date")))
+            // timingDate
+            .setTiming(new DateType("2014-02-14"));
+
+    Assert.assertTrue(compare.equalsDeep(sic));
+  }
+
+  @Test
+  public void shouldHaveLineHctHgbRsltNumSupInfo() {
+    SupportingInformationComponent sic =
+        TransformerTestUtilsV2.findSupportingInfoByCode(
+            "https://bluebutton.cms.gov/resources/variables/line_hct_hgb_rslt_num",
+            eob.getSupportingInfo());
+
+    SupportingInformationComponent compare =
+        TransformerTestUtilsV2.createSupportingInfo(
+            // We don't care what the sequence number is here
+            sic.getSequence(),
+            // Category
+            Arrays.asList(
+                new Coding(
+                    "http://terminology.hl7.org/CodeSystem/claiminformationcategory",
+                    "info",
+                    "Information"),
+                new Coding(
+                    "https://bluebutton.cms.gov/resources/codesystem/information",
+                    "https://bluebutton.cms.gov/resources/variables/line_hct_hgb_rslt_num",
+                    "Hematocrit / Hemoglobin Test Results")));
+
+    compare.setValue(new Reference("#line-observation-1"));
+
+    Assert.assertTrue(compare.equalsDeep(sic));
+  }
+
+  /** Diagnosis elements */
+  @Test
+  public void shouldHaveDiagnosesList() {
+    Assert.assertEquals(3, eob.getDiagnosis().size());
+  }
+
+  @Test
+  public void shouldHaveDiagnosesMembers() {
+    DiagnosisComponent diag1 =
+        TransformerTestUtilsV2.findDiagnosisByCode("R5555", eob.getDiagnosis());
+
+    DiagnosisComponent cmp1 =
+        TransformerTestUtilsV2.createDiagnosis(
+            // Order doesn't matter
+            diag1.getSequence(),
+            new Coding("http://hl7.org/fhir/sid/icd-10", "R5555", null),
+            new Coding(
+                "http://terminology.hl7.org/CodeSystem/ex-diagnosistype",
+                "principal",
+                "Principal Diagnosis"),
+            null,
+            null);
+
+    Assert.assertTrue(cmp1.equalsDeep(diag1));
+
+    DiagnosisComponent diag2 =
+        TransformerTestUtilsV2.findDiagnosisByCode("R6666", eob.getDiagnosis());
+
+    DiagnosisComponent cmp2 =
+        TransformerTestUtilsV2.createDiagnosis(
+            // Order doesn't matter
+            diag2.getSequence(),
+            new Coding("http://hl7.org/fhir/sid/icd-10", "R6666", null),
+            new Coding(
+                "http://hl7.org/fhir/us/carin-bb/CodeSystem/C4BBClaimDiagnosisType",
+                "other",
+                "Other"),
+            null,
+            null);
+
+    Assert.assertTrue(cmp2.equalsDeep(diag2));
+
+    DiagnosisComponent diag3 =
+        TransformerTestUtilsV2.findDiagnosisByCode("G6666", eob.getDiagnosis());
+
+    DiagnosisComponent cmp3 =
+        TransformerTestUtilsV2.createDiagnosis(
+            // Order doesn't matter
+            diag3.getSequence(),
+            new Coding("http://hl7.org/fhir/sid/icd-10", "G6666", null),
+            new Coding(
+                "http://hl7.org/fhir/us/carin-bb/CodeSystem/C4BBClaimDiagnosisType",
+                "secondary",
+                "Secondary"),
+            null,
+            null);
+
+    Assert.assertTrue(cmp3.equalsDeep(diag3));
+  }
+
+  /** Insurance */
+  @Test
+  public void shouldReferenceCoverageInInsurance() {
+    // Only one insurance object
+    Assert.assertEquals(1, eob.getInsurance().size());
+
+    InsuranceComponent insurance = eob.getInsuranceFirstRep();
+
+    InsuranceComponent compare =
+        new InsuranceComponent()
+            .setCoverage(new Reference().setReference("Coverage/part-a-567834"));
+
+    Assert.assertTrue(compare.equalsDeep(insurance));
+  }
+
+  /** Line Items */
+  @Test
+  public void shouldHaveLineItems() {
+    Assert.assertEquals(1, eob.getItem().size());
+  }
+
+  @Test
+  public void shouldHaveLineItemSequence() {
+    Assert.assertEquals(1, eob.getItemFirstRep().getSequence());
+  }
+
+  @Test
+  public void shouldHaveLineItemCareTeamRef() {
+    // The order isn't important but this should reference a care team member
+    Assert.assertNotNull(eob.getItemFirstRep().getCareTeamSequence());
+    Assert.assertEquals(1, eob.getItemFirstRep().getCareTeamSequence().size());
+  }
+
+  @Test
+  public void shouldHaveLineItemDiagnosisRef() {
+    Assert.assertNotNull(eob.getItemFirstRep().getDiagnosisSequence());
+    Assert.assertEquals(1, eob.getItemFirstRep().getDiagnosisSequence().size());
+  }
+
+  @Test
+  public void shouldHaveLineItemInformationRef() {
+    Assert.assertNotNull(eob.getItemFirstRep().getInformationSequence());
+    Assert.assertEquals(1, eob.getItemFirstRep().getInformationSequence().size());
+  }
+
+  @Test
+  public void shouldHaveLineItemCategory() {
+    CodeableConcept category = eob.getItemFirstRep().getCategory();
+
+    CodeableConcept compare =
+        new CodeableConcept()
+            .setCoding(
+                Arrays.asList(
+                    new Coding(
+                        "https://bluebutton.cms.gov/resources/variables/line_cms_type_srvc_cd",
+                        "P",
+                        "Lump sum purchase of DME, prosthetics orthotics")));
+
+    Assert.assertTrue(compare.equalsDeep(category));
+  }
+
+  @Test
+  public void shouldHaveLineItemProductOrServiceExtension() {
+    Assert.assertNotNull(eob.getItemFirstRep().getProductOrService());
+    Assert.assertEquals(1, eob.getItemFirstRep().getProductOrService().getExtension().size());
+
+    Extension ex =
+        TransformerTestUtilsV2.findExtensionByUrl(
+            "http://hl7.org/fhir/sid/ndc",
+            eob.getItemFirstRep().getProductOrService().getExtension());
+
+    Extension compare =
+        new Extension(
+            "http://hl7.org/fhir/sid/ndc",
+            new Coding(
+                "http://hl7.org/fhir/sid/ndc",
+                "667159747",
+                "TYLENOL EXTRA STRENGTH - ACETAMINOPHEN"));
+
+    Assert.assertTrue(compare.equalsDeep(ex));
+  }
 
   /**
    * Serializes the EOB and prints to the command line
