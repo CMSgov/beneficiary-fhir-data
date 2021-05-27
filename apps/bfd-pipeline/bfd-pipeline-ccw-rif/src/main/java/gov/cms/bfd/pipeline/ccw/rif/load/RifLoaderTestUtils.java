3711f416d50c4fd3c7b7ca6b9a6a50c845e97df3
Ryan Travitz <rtravitz@gmail.com>
2021-05-27 15:56:08 -0700

    Revert "BFD-704: Made BFD Pipeline orchestration more flexible (#539)" (#621)
    This reverts commit ccaa9c05ada8f9b81d9cbd296c6ade659d1a59c6.

--- apps/bfd-pipeline/bfd-pipeline-ccw-rif/src/main/java/gov/cms/bfd/pipeline/ccw/rif/load/RifLoaderTestUtils.java
+++ apps/bfd-pipeline/bfd-pipeline-ccw-rif/src/main/java/gov/cms/bfd/pipeline/ccw/rif/load/RifLoaderTestUtils.java
@@ -6,7 +6,6 @@ import gov.cms.bfd.model.rif.RifFileType;
 import gov.cms.bfd.model.rif.RifFilesEvent;
 import gov.cms.bfd.model.rif.schema.DatabaseSchemaManager;
 import gov.cms.bfd.model.rif.schema.DatabaseTestHelper;
-import gov.cms.bfd.pipeline.sharedutils.DatabaseOptions;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -35,6 +34,9 @@ public final class RifLoaderTestUtils {
 
   /** The value to use for {@link LoadAppOptions#isIdempotencyRequired()}. */
   public static final boolean IDEMPOTENCY_REQUIRED = true;
+
+  /** The value to use for {@link LoadAppOptions#isFixupsEnabled()} */
+  public static final boolean FIXUPS_ENABLED = true;
 
   @SuppressWarnings("unused")
   private static final Logger LOGGER = LoggerFactory.getLogger(RifLoaderTestUtils.class);
@@ -114,11 +116,13 @@ public final class RifLoaderTestUtils {
    */
   public static LoadAppOptions getLoadOptions(DataSource dataSource) {
     return new LoadAppOptions(
-        new DatabaseOptions(dataSource),
         HICN_HASH_ITERATIONS,
         HICN_HASH_PEPPER,
+        dataSource,
         LoadAppOptions.DEFAULT_LOADER_THREADS,
-        IDEMPOTENCY_REQUIRED);
+        IDEMPOTENCY_REQUIRED,
+        FIXUPS_ENABLED,
+        RifLoaderIdleTasks.DEFAULT_PARTITION_COUNT);
   }
 
   /**
@@ -126,11 +130,11 @@ public final class RifLoaderTestUtils {
    * @return a JPA {@link EntityManagerFactory} for the database server used in tests
    */
   public static EntityManagerFactory createEntityManagerFactory(LoadAppOptions options) {
-    if (options.getDatabaseOptions().getDatabaseDataSource() == null) {
+    if (options.getDatabaseDataSource() == null) {
       throw new IllegalStateException("DB DataSource (not URLs) must be used in tests.");
     }
 
-    DataSource dataSource = options.getDatabaseOptions().getDatabaseDataSource();
+    DataSource dataSource = options.getDatabaseDataSource();
     return RifLoader.createEntityManagerFactory(dataSource);
   }
 
