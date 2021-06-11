0c45ae3256a74e4d7e81bbe962a4165bb041fd04
keithdadkins <keithdadkins@me.com>
2021-06-10 19:21:39 -0500

    Revert "[BFD-981] Postgres 12 Cluster Update (#641)"
    This reverts commit 179b308169c499fcd4730cd2b99bf3f20cff143a.

--- ops/terraform/env/prod/stateful/main.tf
+++ ops/terraform/env/prod/stateful/main.tf
@@ -21,8 +21,8 @@ module "stateful" {
     # With aurora you do not designate primary and replicas. Instead, you simply add RDS Instances and
     # Aurora manages the replication. So if you want 1 writer and 3 readers, you set cluster_nodes to 4
     cluster_nodes  = 4
-    engine_version = "12.4"
-    param_version  = "aurora-postgresql12"
+    engine_version = "11.9"
+    param_version  = "aurora-postgresql11"
   }
 
   aurora_node_params = [
