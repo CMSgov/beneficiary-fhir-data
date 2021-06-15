074f02f58a5321c4350294c8af3d726b25cc4ec7
keithdadkins <keithdadkins@me.com>
2021-06-15 16:29:32 -0500

    Update paramaters

--- ops/terraform/env/test/stateful/main.tf
+++ ops/terraform/env/test/stateful/main.tf
@@ -23,9 +23,13 @@ module "stateful" {
   }
 
   aurora_node_params = [
-    { name = "auto_explain.log_min_duration", value = "6000", apply_on_reboot = false },
+    { name = "auto_explain.log_min_duration", value = "1000", apply_on_reboot = false },
+    { name = "auto_explain.log_verbose", value = true, apply_on_reboot = false },
+    { name = "auto_explain.log_nested_statements", value = true, apply_on_reboot = false },
+    { name = "pg_stat_statements.max", value = "2000", apply_on_reboot = true },
+    { name = "pg_stat_statements.track", value = "all", apply_on_reboot = false },
     { name = "shared_preload_libraries", value = "pg_stat_statements,auto_explain", apply_on_reboot = true },
-    { name = "log_min_duration_statement", value = "6000", apply_on_reboot = false },
+    { name = "log_min_duration_statement", value = "1000", apply_on_reboot = false },
     { name = "log_connections", value = "1", apply_on_reboot = false },
     { name = "default_statistics_target", value = "1000", apply_on_reboot = false },
     { name = "random_page_cost", value = "1.1", apply_on_reboot = false },
