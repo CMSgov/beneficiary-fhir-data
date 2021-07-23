3777811fb4c4a35c1db539a148e0a18205df1909
Nathan Dister <nathan.dister@adhocteam.us>
2021-07-23 15:27:35 -0400

    Add CBC build environment container

---
+++ ops/jenkins/cbc-build-push.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DOCKER_TAG=${1:-jdk8-an29-tf12}
+
+docker build --file Dockerfile.cbc-build \
+  --build-arg JAVA_VERSION=${2:-8} \
+  --build-arg ANSIBLE_VERSION=${3:-2.9.24} \
+  --build-arg PACKER_VERSION=${4:-1.6.6} \
+  --build-arg TERRAFORM_VERSION=${5:-0.12.31} \
+  --tag public.ecr.aws/c2o1d8s9/bfd-cbc-build:${DOCKER_TAG} \
+  .
+
+docker push public.ecr.aws/c2o1d8s9/bfd-cbc-build:${DOCKER_TAG}
