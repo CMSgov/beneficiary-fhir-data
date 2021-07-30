b0599fe6d3f504d0af150f820da5035ab3ade699
keithdadkins <keithdadkins@me.com>
2021-07-30 13:24:08 -0500

    fix perms on existing tables

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V32__preadj_schema_perms.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V32__preadj_schema_perms.sql
@@ -13,10 +13,22 @@ ${logic.perms} BEGIN
 ${logic.perms}   IF EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'read_only_bb') THEN
 ${logic.perms}     GRANT USAGE ON SCHEMA pre_adj TO read_only_bb;
 ${logic.perms}     ALTER DEFAULT PRIVILEGES IN SCHEMA pre_adj GRANT SELECT ON TABLES TO read_only_bb;
+${logic.perms}     GRANT SELECT ON TABLE pre_adj."FissClaims" TO read_only_bb;
+${logic.perms}     GRANT SELECT ON TABLE pre_adj."FissDiagnosisCodes" TO read_only_bb;
+${logic.perms}     GRANT SELECT ON TABLE pre_adj."FissProcCodes" TO read_only_bb;
+${logic.perms}     GRANT SELECT ON TABLE pre_adj."McsClaims" TO read_only_bb;
+${logic.perms}     GRANT SELECT ON TABLE pre_adj."McsDetails" TO read_only_bb;
+${logic.perms}     GRANT SELECT ON TABLE pre_adj."McsDiagnosisCodes" TO read_only_bb;
 ${logic.perms}   END IF;
 ${logic.perms}   IF EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'svc_bfd_pipeline_role') THEN
 ${logic.perms}     GRANT USAGE ON SCHEMA pre_adj TO svc_bfd_pipeline_role;
 ${logic.perms}     ALTER DEFAULT PRIVILEGES IN SCHEMA pre_adj GRANT ALL ON TABLES TO svc_bfd_pipeline_role;
 ${logic.perms}     ALTER DEFAULT PRIVILEGES IN SCHEMA pre_adj GRANT ALL ON SEQUENCES TO svc_bfd_pipeline_role;
+${logic.perms}     GRANT ALL ON TABLE pre_adj."FissClaims" TO svc_bfd_pipeline_role;
+${logic.perms}     GRANT ALL ON TABLE pre_adj."FissDiagnosisCodes" TO svc_bfd_pipeline_role;
+${logic.perms}     GRANT ALL ON TABLE pre_adj."FissProcCodes" TO svc_bfd_pipeline_role;
+${logic.perms}     GRANT ALL ON TABLE pre_adj."McsClaims" TO svc_bfd_pipeline_role;
+${logic.perms}     GRANT ALL ON TABLE pre_adj."McsDetails" TO svc_bfd_pipeline_role;
+${logic.perms}     GRANT ALL ON TABLE pre_adj."McsDiagnosisCodes" TO svc_bfd_pipeline_role;
 ${logic.perms}   END IF;
 ${logic.perms} END $$;
