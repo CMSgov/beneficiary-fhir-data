7bed10c43660665551ac1e45df5b4324277dbbae
keithdadkins <keithdadkins@me.com>
2021-08-05 11:57:03 -0500

    load beneficiaries first and implement DRY_RUN

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/scripts/BFD-1010-schema-changes/migrate_data.sh
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/scripts/BFD-1010-schema-changes/migrate_data.sh
@@ -7,47 +7,59 @@ export PGPASSWORD="${PGPASSWORD:-}"
 export PGPORT="${PGPORT:-5432}"
 export PGDATABASE="${PGDATABASE:-}"
 
-# we will process these one by one in order (I do not know which tables are parents, just guessing)
-parent_tables=(beneficiaries beneficiaries_history)
+# when DRY_RUN=true (the default), the script will echo the commands it would run against the db
+export DRY_RUN="${DRY_RUN:-true}"
 
-# these we will run in background jobs, up to $MAX_JOBS at a time
+# tables that need to be processed before child tables, these are processed
+# one after the other in order
+parent_tables=(
+  carrier_claims
+  dme_claims
+  hha_claims
+  hospice_claims
+  inpatient_claims
+  outpatient_claims
+  snf_claims
+)
+
+# these will be run in background jobs, up to $MAX_JOBS at a time
+# not necessarily in order
 child_tables=(
-  beneficiary_monthly
+  carrier_claim_lines
+  dme_claim_lines
+  hha_claim_lines
+  hospice_claim_lines
+  inpatient_claim_lines
+  outpatient_claim_lines
+  snf_claim_lines
+  partd_events
+  beneficiaries_history
   beneficiaries_history_invalid
+  beneficiary_monthly
   loaded_batches
   medicare_beneficiaryid_history
   medicare_beneficiaryid_history_invalid
-  carrier_claims
-  carrier_claim_lines
-  dme_claims
-  dme_claim_lines
-  hha_claims
-  hha_claim_lines
-  hospice_claims
-  hospice_claim_lines
-  inpatient_claims
-  inpatient_claim_lines
-  outpatient_claims
-  outpatient_claim_lines
-  partd_events
-  snf_claims
-  snf_claim_lines
 )
 
+# generates/loads .env file and tests db connection
 setup(){
   if ! [[ -f .env ]]; then
     printf "Generating .env file.. "
     echo -e "export PGHOST=\nexport PGPORT=5432\nexport PGUSER=\nexport PGPASSWORD=\nexport PGDATABASE=" > .env
-    echo -e "export MAX_JOBS=1\n" >> .env
+    echo -e "export MAX_JOBS=1\nexport DRY_RUN=true\n" >> .env
     echo "OK"
     echo "Please update $(PWD)/.env with the appropriate database credentials and run the script again."
     exit
   else
-    # shellcheck disable=SC1091 # tell shellcheck not to worry about checking this .env file
-    source .env
-    if ! psql --quiet --tuples-only -c "select NOW();" >/dev/null 2>&1; then
-      echo "Failed to connect to the database. Did you update the $(PWD)/.env file?"
-      exit 1
+    if $DRY_RUN; then
+      echo "DRY_RUN=true.. skipping db connection check"
+    else
+      # shellcheck disable=SC1091 # tell shellcheck not to worry about checking this .env file
+      source .env
+      if ! psql --quiet --tuples-only -c "select NOW();" >/dev/null 2>&1; then
+        echo "Failed to connect to the database. Did you update the $(PWD)/.env file?"
+        exit 1
+      fi
     fi
   fi
 }
@@ -55,19 +67,25 @@ setup(){
 # load the given table
 # $1 == table name to process - this assumes all tables have a matching ./insert_${tbl_name}.sql file
 load_file(){
+  local start end tbl_name psql_cmd
+  start=$SECONDS
   # I'm not sure how noisy the output is, but if it's a lot, displaying it will slow down the operation
   # due to console paging, if it does, either redirect the output to /dev/null or to a file if you want
   # to review the output later. If you do output to files, and there is a lot of output, be mindful of
   # the amount of freespace on the host. 
   tbl_name="$1"
-  psql_cmd="echo psql --quiet --tuples-only -f ./insert_${tbl_name}.sql"
+  psql_cmd="psql --quiet --tuples-only -f ./insert_${tbl_name}.sql"
+  $DRY_RUN && psql_cmd="echo $psql_cmd"
+
   #if $psql_cmd; then                            # show the output on the console
   #if $psql_cmd >/dev/null; then                 # hide the output
-  if $psql_cmd > "insert_${tbl_name}.log"; then  # redirect output to a file
-    # sleep "$(shuf -i 0-1 -n 1)"  # randomly sleep (used for testing background jobs)
-    echo "  -> successfully loaded $tbl_name"
+  if $psql_cmd > "insert_${tbl_name}.log"; then  # redirect command output to a file
+    $DRY_RUN && sleep "$(shuf -i 0-2 -n 1)"      # randomly sleep to simulate background jobs
+    end=$SECONDS; duration=$(( end - start ))
+    echo "  -> successfully loaded $tbl_name (took $((duration / 60)) minutes)"
   else
-    echo "  -x failed to load $tbl_name. Aborting"
+    end=$SECONDS; duration=$(( end - start ))
+    echo "  -x failed to load $tbl_name (after $((duration / 60)) minutes)"
     exit 1
   fi
 }
@@ -91,6 +109,14 @@ setup
 # track overall time
 total_start=$SECONDS
 
+# beneficiaries table
+echo "LOADING BENEFICIARIES TABLE..."
+bene_start=$SECONDS
+load_file "beneficiaries"
+bene_end=$SECONDS; duration=$(( bene_end - bene_start ))
+echo "beneficiaries loaded after ~$((duration / 60)) minutes"
+echo
+
 # parent tables
 echo "LOADING PARENT TABLES..."
 parent_start=$SECONDS
@@ -106,8 +132,9 @@ echo "LOADING CHILD TABLES..."; child_start=$SECONDS
 process_tables "${child_tables[@]}" # processes all child_tables
 child_end=$SECONDS; duration=$(( child_end - child_start ))
 echo "child tables loaded after ~$((duration / 60)) minutes"
+echo
 
 # done
 total_end=$SECONDS; duration=$(( total_end - total_start ))
-echo -e "\nAll DONE"
+echo "All DONE"
 echo "Total Time: ~$((duration / 60)) minutes"
