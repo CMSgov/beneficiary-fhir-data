cb0ee5413ce82f79e615e47d83fac0647f8e529a
mackec1 <chris.mackenzie@adhocteam.us>
2021-08-09 09:52:09 -0400

    use keith's sh framework for the insert tables

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/scripts/BFD-1010-schema-changes/verify_data.sh
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/scripts/BFD-1010-schema-changes/verify_data.sh
@@ -1,67 +1,128 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -eo pipefail
+export MAX_JOBS="${MAX_JOBS:-1}" # defaults to one, probably max around 4
+export PGHOST="${PGHOST:-}"
+export PGUSER="${PGUSER:-}"
+export PGPASSWORD="${PGPASSWORD:-}"
+export PGPORT="${PGPORT:-5432}"
+export PGDATABASE="${PGDATABASE:-}"
 
-set -o pipefail
-set -e
+# when DRY_RUN=true (the default), the script will echo the commands it would run against the db
+export DRY_RUN="${DRY_RUN:-true}"
 
-# following must be passed in as either environment variables or as cmd-line args (default)
-PGHOST="${DB_HOST:-$1}"
-PGUSER="${DB_USER:-$2}"
-PGPASSWORD="${DB_PSWD:-$3}"
+# these will be run in background jobs, up to $MAX_JOBS at a time
+# not necessarily in order
+verify_tables=(
+beneficiaries
+beneficiaries_history
+beneficiaries_history_invalid_beneficiaries
+beneficiary_monthly
+medicare_beneficiaryid_history
+medicare_beneficiaryid_history_invalid_beneficiaries
+carrier_claims
+carrier_claim_lines
+dme_claims
+dme_claim_lines
+hha_claims
+hha_claim_lines
+hospice_claims
+hospice_claim_lines
+inpatient_claims
+inpatient_claim_lines
+outpatient_claims
+outpatient_claim_lines
+partd_events
+snf_claims
+snf_claim_lines
+)
 
-# other vars that skirt security (a bit)
-PGDATABASE="${DB_NAME:-fihr}"
-export PGPORT="${DB_PORT:-5432}"
+# generates/loads .env file and tests db connection
+setup(){
+  if ! [[ -f .env ]]; then
+    printf "Generating .env file.. "
+    echo -e "export PGHOST=\nexport PGPORT=5432\nexport PGUSER=\nexport PGPASSWORD=\nexport PGDATABASE=" > .env
+    echo -e "export MAX_JOBS=1\nexport DRY_RUN=true\n" >> .env
+    echo "OK"
+    echo "Please update $(PWD)/.env with the appropriate database credentials and run the script again."
+    exit
+  else
+    if $DRY_RUN; then
+      echo "DRY_RUN=true.. skipping db connection check"
+    else
+      # shellcheck disable=SC1091 # tell shellcheck not to worry about checking this .env file
+      source .env
+      if ! psql --quiet --tuples-only -c "select NOW();" >/dev/null 2>&1; then
+        echo "Failed to connect to the database. Did you update the $(PWD)/.env file?"
+        exit 1
+      fi
+    fi
+  fi
+}
 
-if [ -z "$PGHOST" ] || [ -z "$PGUSER" ] || [ -z "$PGPASSWORD" ]; then
-    echo "*****  E r r o r - Missing required variables  *****"
-    echo "$0 requires ENV variable(s) for: DB_HOST, DB_USER, DB_PSWD";
-    echo "or";
-    echo "$0 requires cmd-line args for: <db host> <db username> <db password>";
-    exit 1;
-fi
+# load the given table
+# $1 == table name to process - this assumes all tables have a matching ./insert_${tbl_name}.sql file
+load_file(){
+  local start end tbl_name psql_cmd
+  start=$SECONDS
+  # I'm not sure how noisy the output is, but if it's a lot, displaying it will slow down the operation
+  # due to console paging, if it does, either redirect the output to /dev/null or to a file if you want
+  # to review the output later. If you do output to files, and there is a lot of output, be mindful of
+  # the amount of freespace on the host. 
+  tbl_name="$1"
+  psql_cmd="psql --quiet --tuples-only -f ./verify_${tbl_name}.sql"
+  $DRY_RUN && psql_cmd="echo $psql_cmd"
 
-echo "Testing db connectivity..."
-now=$(psql -h "$PGHOST" -U "$PGUSER" -d "$PGDATABASE" --quiet --tuples-only -c "select NOW();")
-if [[ "$now" == *"2021"* ]]; then
-  echo "db connectivity: OK"
-else
-  echo "db connectivity: FAILED...exiting"
-  exit 1;
-fi
+  #if $psql_cmd; then                            # show the output on the console
+  #if $psql_cmd >/dev/null; then                 # hide the output
+  if $psql_cmd > "verify_${tbl_name}.log"; then  # redirect command output to a file
+    $DRY_RUN && sleep "$(shuf -i 0-2 -n 1)"      # randomly sleep to simulate background jobs
+    end=$SECONDS; duration=$(( end - start ))
+    echo "  -> successfully verifed $tbl_name (took $((duration / 60)) minutes)"
+  else
+    end=$SECONDS; duration=$(( end - start ))
+    echo "  -x failed to verify $tbl_name (after $((duration / 60)) minutes)"
+    exit 1
+  fi
+}
 
-sql_files=(
-"./verify_beneficiaries.sql" \
-"./verify_beneficiaries_history.sql" \
-"./verify_beneficiaries_history_invalid_beneficiaries.sql" \
-"./verify_beneficiary_monthly.sql" \
-#"./verify_loaded_batches.sql" \
-"./verify_medicare_beneficiaryid_history.sql" \
-"./verify_medicare_beneficiaryid_history_invalid_beneficiaries.sql" \
-"./verify_carrier_claims.sql" \
-"./verify_carrier_claim_lines.sql" \
-"./verify_dme_claims.sql" \
-"./verify_dme_claim_lines.sql" \
-"./verify_hha_claims.sql" \
-"./verify_hha_claim_lines.sql" \
-"./verify_hospice_claims.sql" \
-"./verify_hospice_claim_lines.sql" \
-"./verify_inpatient_claims.sql" \
-"./verify_inpatient_claim_lines.sql" \
-"./verify_outpatient_claims.sql" \
-"./verify_outpatient_claim_lines.sql" \
-"./verify_partd_events.sql" \
-"./verify_snf_claims.sql" \
-"./verify_snf_claim_lines.sql" )
+# processes $MAX_JOBS number of tables (files) in parallel
+# $1 a list of tables ex: "process_tables snf_claims snf_claim_lines"
+process_tables(){
+  while [[ -n "$1" ]]; do
+    # always try to keep the max number of jobs running the background
+    while [[ $(jobs -r | wc -l | tr -d " ") < $((MAX_JOBS+1)) ]]; do
+      load_file "$1" &
+      shift
+    done
+  done
+  wait # waits until all background jobs are finished
+}
 
-echo "Begin verifying database..."
-for SQL in "${sql_files[@]}"
-do
-   : 
-   # do whatever on $i
-   echo "begin processing ${SQL} : $(date +'%T.%31')"
-   psql -h "$PGHOST" -U "$PGUSER" -d "$PGDATABASE" --quiet --tuples-only -f "$SQL"
-   echo "finished processing ${SQL} : $(date +'%T.%31')"
+# load and parse .env file
+setup
+
+# track overall time
+total_start=$SECONDS
+
+# migration_errors table 
+echo "CREATING MIGRATION_ERRORS TABLE..."
+bene_start=$SECONDS
+load_file "migration_errors_table"
+bene_end=$SECONDS; duration=$(( bene_end - bene_start ))
+echo "migration_errors table created after ~$((duration / 60)) minutes"
+echo
+
+# parent tables
+echo "VERIFY TABLES..."
+parent_start=$SECONDS
+for t in "${verify_tables[@]}"; do
+  load_file "$t"
 done
+parent_end=$SECONDS; duration=$(( parent_end - parent_start ))
+echo "tables verified after ~$((duration / 60)) minutes"
+echo
 
-echo "All DONE at: $(date +'%T.%31')"
-exit 0;
+# done
+total_end=$SECONDS; duration=$(( total_end - total_start ))
+echo "All DONE"
+echo "Total Time: ~$((duration / 60)) minutes"
