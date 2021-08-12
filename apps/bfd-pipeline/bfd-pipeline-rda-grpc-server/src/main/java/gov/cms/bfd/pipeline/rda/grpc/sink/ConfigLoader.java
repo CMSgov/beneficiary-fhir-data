1836d8d81963122a05161aa20b66321bef10e8f8
Brian Burton <brian.burton@adhocteam.us>
2021-08-12 16:20:13 -0400

    Adds LoadRdaJsonApp.

---
+++ apps/bfd-pipeline/bfd-pipeline-rda-grpc-server/src/main/java/gov/cms/bfd/pipeline/rda/grpc/sink/ConfigLoader.java
@@ -0,0 +1,161 @@
+package gov.cms.bfd.pipeline.rda.grpc.sink;
+
+import com.google.common.base.Strings;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.function.Function;
+
+public class ConfigLoader {
+  private final Function<String, String> source;
+
+  public ConfigLoader(Function<String, String> source) {
+    this.source = source;
+  }
+
+  public static ConfigLoader fromEnvironmentVariables() {
+    return new ConfigLoader(System::getenv);
+  }
+
+  public static ConfigLoader fromSystemProperties() {
+    return new ConfigLoader(System::getProperty);
+  }
+
+  public ConfigLoader withFallback(ConfigLoader fallback) {
+    return new ConfigLoader(
+        name -> {
+          String value = source.apply(name);
+          return Strings.isNullOrEmpty(value) ? fallback.source.apply(name) : value;
+        });
+  }
+
+  public static ConfigLoader fromPropertiesFile(File propertiesFile) throws IOException {
+    Properties props = new Properties();
+    try (Reader in = new BufferedReader(new FileReader(propertiesFile))) {
+      props.load(in);
+    }
+    return new ConfigLoader(props::getProperty);
+  }
+
+  public String stringValue(String name) {
+    final String value = source.apply(name);
+    if (Strings.isNullOrEmpty(value)) {
+      throw new ConfigException(name, "required option not provided");
+    } else {
+      return value;
+    }
+  }
+
+  public String stringValue(String name, String defaultValue) {
+    final String value = source.apply(name);
+    return Strings.isNullOrEmpty(value) ? defaultValue : value;
+  }
+
+  public Optional<String> stringOption(String name) {
+    final String value = source.apply(name);
+    return Strings.isNullOrEmpty(value) ? Optional.empty() : Optional.of(value);
+  }
+
+  public int intValue(String name, int defaultValue) {
+    final String value = source.apply(name);
+    if (Strings.isNullOrEmpty(value)) {
+      return defaultValue;
+    }
+    try {
+      return Integer.parseInt(value);
+    } catch (Exception ex) {
+      throw new ConfigException(name, "not a valid integer", ex);
+    }
+  }
+
+  public Optional<Integer> intOption(String name) {
+    final String value = source.apply(name);
+    if (Strings.isNullOrEmpty(value)) {
+      return Optional.empty();
+    }
+    try {
+      return Optional.of(Integer.parseInt(value));
+    } catch (Exception ex) {
+      throw new ConfigException(name, "not a valid integer", ex);
+    }
+  }
+
+  public <T extends Enum<T>> T enumValue(String name, Function<String, T> parser) {
+    String value = stringValue(name);
+    try {
+      return parser.apply(value);
+    } catch (Exception ex) {
+      throw new ConfigException(name, "not a valid enum value: " + ex.getMessage(), ex);
+    }
+  }
+
+  public File readableFile(String name) {
+    return readableFileOption(name)
+        .orElseThrow(() -> new ConfigException(name, "required option not provided"));
+  }
+
+  public Optional<File> readableFileOption(String name) {
+    Optional<File> file = stringOption(name).map(File::new);
+    file.ifPresent(
+        f -> {
+          if (!f.canRead()) {
+            throw new ConfigException(name, "file is not readable");
+          }
+        });
+    return file;
+  }
+
+  public File writeableFile(String name) {
+    String value = stringValue(name);
+    File file = new File(value);
+    if (file.isFile()) {
+      if (!file.canWrite()) {
+        throw new ConfigException(name, "file is not writeable");
+      }
+    } else if (!file.getParentFile().canWrite()) {
+      throw new ConfigException(name, "file does not exist and parent is not writeable");
+    }
+    return file;
+  }
+
+  public boolean booleanValue(String name, boolean defaultValue) {
+    String value = source.apply(name);
+    if (Strings.isNullOrEmpty(value)) {
+      return defaultValue;
+    }
+    switch (value.toLowerCase()) {
+      case "true":
+        return true;
+      case "false":
+        return false;
+      default:
+        throw new ConfigException(name, "invalid boolean value: " + value);
+    }
+  }
+
+  public static class ConfigException extends RuntimeException {
+    private final String name;
+
+    public ConfigException(String name, String message) {
+      super(message);
+      this.name = name;
+    }
+
+    public ConfigException(String name, String message, Throwable cause) {
+      super(message, cause);
+      this.name = name;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public String getErrorMessage() {
+      return String.format("invalid option: name='%s' message='%s'", name, getMessage());
+    }
+  }
+}
---
+++ apps/bfd-pipeline/bfd-pipeline-rda-grpc-server/src/main/java/gov/cms/bfd/pipeline/rda/grpc/sink/LoadRdaJsonApp.java
@@ -0,0 +1,139 @@
+package gov.cms.bfd.pipeline.rda.grpc.sink;
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Slf4jReporter;
+import com.zaxxer.hikari.HikariDataSource;
+import gov.cms.bfd.model.rif.schema.DatabaseSchemaManager;
+import gov.cms.bfd.pipeline.rda.grpc.AbstractRdaLoadJob;
+import gov.cms.bfd.pipeline.rda.grpc.RdaLoadOptions;
+import gov.cms.bfd.pipeline.rda.grpc.server.EmptyMessageSource;
+import gov.cms.bfd.pipeline.rda.grpc.server.JsonMessageSource;
+import gov.cms.bfd.pipeline.rda.grpc.server.MessageSource;
+import gov.cms.bfd.pipeline.rda.grpc.server.RdaServer;
+import gov.cms.bfd.pipeline.rda.grpc.source.GrpcRdaSource;
+import gov.cms.bfd.pipeline.sharedutils.DatabaseOptions;
+import gov.cms.bfd.pipeline.sharedutils.IdHasher;
+import gov.cms.bfd.pipeline.sharedutils.PipelineApplicationState;
+import gov.cms.bfd.pipeline.sharedutils.PipelineJob;
+import gov.cms.mpsm.rda.v1.ClaimChange;
+import java.io.File;
+import java.time.Clock;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoadRdaJsonApp {
+  private static final Logger logger = LoggerFactory.getLogger(LoadRdaJsonApp.class);
+
+  public static void main(String[] argv) throws Exception {
+    final ConfigLoader options =
+        (argv.length == 1)
+            ? ConfigLoader.fromPropertiesFile(new File(argv[0]))
+            : ConfigLoader.fromSystemProperties();
+    final Config config = new Config(options);
+
+    final MetricRegistry metrics = new MetricRegistry();
+    final Slf4jReporter reporter =
+        Slf4jReporter.forRegistry(metrics)
+            .outputTo(LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME))
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .build();
+    reporter.start(5, TimeUnit.SECONDS);
+    try {
+      logger.info("starting RDA API local server");
+      RdaServer.runWithLocalServer(
+          config::createFissClaimsSource,
+          config::createMcsClaimsSource,
+          port -> {
+            final RdaLoadOptions jobConfig = config.createRdaLoadOptions(port);
+            final DatabaseOptions databaseConfig = config.createDatabaseOptions();
+            final HikariDataSource pooledDataSource =
+                PipelineApplicationState.createPooledDataSource(databaseConfig, metrics);
+            if (config.runSchemaMigration) {
+              logger.info("running database migration");
+              DatabaseSchemaManager.createOrUpdateSchema(pooledDataSource);
+            }
+            final PipelineApplicationState appState =
+                new PipelineApplicationState(
+                    metrics,
+                    pooledDataSource,
+                    PipelineApplicationState.RDA_PERSISTENCE_UNIT_NAME,
+                    Clock.systemUTC());
+            final List<PipelineJob<?>> jobs = config.createPipelineJobs(jobConfig, appState);
+            for (PipelineJob<?> job : jobs) {
+              logger.info("starting job {}", job.getClass().getSimpleName());
+              job.call();
+            }
+          });
+    } finally {
+      reporter.report();
+    }
+  }
+
+  static class Config {
+    private final String hashPepper;
+    private final int hashIterations;
+    private final String dbUrl;
+    private final String dbUser;
+    private final String dbPassword;
+    private final int batchSize;
+    private final boolean runSchemaMigration;
+    private final Optional<File> fissFile;
+    private final Optional<File> mcsFile;
+
+    private Config(ConfigLoader options) {
+      hashPepper = options.stringValue("hash.pepper", "notarealpepper");
+      hashIterations = options.intValue("hash.iterations", 2);
+      dbUrl = options.stringValue("database.url", "jdbc:hsqldb:mem:LoadRdaJsonApp");
+      dbUser = options.stringValue("database.user", "");
+      dbPassword = options.stringValue("database.password", "");
+
+      batchSize = options.intValue("job.batchSize", 10);
+      runSchemaMigration = options.booleanValue("job.migration", false);
+      fissFile = options.readableFileOption("file.fiss");
+      mcsFile = options.readableFileOption("file.mcs");
+    }
+
+    private DatabaseOptions createDatabaseOptions() {
+      return new DatabaseOptions(dbUrl, dbUser, dbPassword, 10);
+    }
+
+    private RdaLoadOptions createRdaLoadOptions(int port) {
+      final IdHasher.Config idHasherConfig = new IdHasher.Config(hashIterations, hashPepper);
+      final AbstractRdaLoadJob.Config jobConfig =
+          new AbstractRdaLoadJob.Config(Duration.ofDays(1), batchSize);
+      final GrpcRdaSource.Config grpcConfig =
+          new GrpcRdaSource.Config("localhost", port, Duration.ofDays(1));
+      return new RdaLoadOptions(jobConfig, grpcConfig, idHasherConfig);
+    }
+
+    private MessageSource<ClaimChange> createFissClaimsSource() {
+      return createClaimsSourceForFile(fissFile);
+    }
+
+    private MessageSource<ClaimChange> createMcsClaimsSource() {
+      return createClaimsSourceForFile(mcsFile);
+    }
+
+    private MessageSource<ClaimChange> createClaimsSourceForFile(Optional<File> jsonFile) {
+      if (jsonFile.isPresent()) {
+        return new JsonMessageSource<>(jsonFile.get(), JsonMessageSource::parseClaimChange);
+      } else {
+        return new EmptyMessageSource<>();
+      }
+    }
+
+    private List<PipelineJob<?>> createPipelineJobs(
+        RdaLoadOptions jobConfig, PipelineApplicationState appState) {
+      List<PipelineJob<?>> answer = new ArrayList<>();
+      fissFile.ifPresent(f -> answer.add(jobConfig.createFissClaimsLoadJob(appState)));
+      mcsFile.ifPresent(f -> answer.add(jobConfig.createMcsClaimsLoadJob(appState)));
+      return answer;
+    }
+  }
+}
