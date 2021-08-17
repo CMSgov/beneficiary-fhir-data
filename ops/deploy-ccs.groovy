11c847334276671ea8d5deef9bd6785ad39ee2df
Nathan Dister <nathan.dister@adhocteam.us>
2021-08-17 17:03:41 -0400

    Fix file not found error with non-persistent workspace in CBC

--- ops/deploy-ccs.groovy
+++ ops/deploy-ccs.groovy
@@ -134,8 +134,8 @@ def buildAppAmis(String gitBranchName, String gitCommitId, AmiIds amiIds, AppBui
 		withCredentials([file(credentialsId: 'bfd-vault-password', variable: 'vaultPasswordFile')]) {
  
 			// both packer builds expect additional variables in a file called `extra_vars.json` in the current directory
-			sh "touch ${workspace}/ops/ansible/playbooks-ccs/extra_vars.json"
 			def varsFile = new File("${workspace}/ops/ansible/playbooks-ccs/extra_vars.json")
+			varsFile.createNewFile()
 
 			varsFile.write(JsonOutput.toJson([
 				data_server_launcher: "${workspace}/${appBuildResults.dataServerLauncher}",
