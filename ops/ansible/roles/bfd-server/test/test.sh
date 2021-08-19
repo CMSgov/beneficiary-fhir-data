ea9b78e31fe6def34155ca369e6cb405fa1c1487
Karl M. Davis <karl.davis@cms.hhs.gov>
2021-08-19 10:12:42 -0400

    Updated ops code to use new app ZIPs.
    Also took care of some additional items while I was at it:
    
    * Added tests for the `bfd-server` and `bfd-pipeline` Ansible roles.
      (Really, was more like revivifying the ancient, and broken, tests, but
      whatever.)
    * Changed the install path for the BFD Pipline to
      `/usr/local/bfd-pipeline`, so it's now consistent with how the BFD
      Server is installed.
    * Fixed some errors/inconsistencies that had crept into the two roles,
      which were flushed out by the tests.
    
    BFD-1062

--- ops/ansible/roles/bfd-server/test/test.sh
+++ ops/ansible/roles/bfd-server/test/test.sh
@@ -19,8 +19,9 @@ ansible-playbook "${TEST_PLAY}" --inventory=inventory --syntax-check
 ansible-playbook "${TEST_PLAY}" --inventory=inventory
 
 # Run the role/playbook again, checking to make sure it's idempotent.
-ansible-playbook "${TEST_PLAY}" --inventory=inventory \
-  | tee /dev/stderr \
-  | grep -q 'docker_container.*changed=0.*failed=0' \
-  && (echo 'Idempotence test: pass' && exit 0) \
-  || (echo 'Idempotence test: fail' && exit 1)
+# Note: This is disabled, as the role is explicitly **not** idempotent, since it rebuilds the server's trust store every time.
+#ansible-playbook "${TEST_PLAY}" --inventory=inventory \
+#  | tee /dev/stderr \
+#  | grep -q 'docker_container.*changed=0.*failed=0' \
+#  && (echo 'Idempotence test: pass' && exit 0) \
+#  || (echo 'Idempotence test: fail' && exit 1)
