3372b0bfa61af2b0e11309f6b3ad67cbe48a45d7
Chris Brune <cbrune@fearless.tech>
2021-08-26 14:30:09 -0400

    added synchronized to singleton for thread safety

--- apps/bfd-pipeline/bfd-pipeline-ccw-rif/src/main/java/gov/cms/bfd/pipeline/ccw/rif/extract/s3/S3MinioConfig.java
+++ apps/bfd-pipeline/bfd-pipeline-ccw-rif/src/main/java/gov/cms/bfd/pipeline/ccw/rif/extract/s3/S3MinioConfig.java
@@ -21,7 +21,7 @@ public class S3MinioConfig {
 
   // Method
   // Static method to create instance of Singleton class
-  synchronized public static S3MinioConfig Singleton() {
+  public static synchronized S3MinioConfig Singleton() {
     // To ensure only one instance is created
     if (single_instance == null) {
       single_instance = new S3MinioConfig();
