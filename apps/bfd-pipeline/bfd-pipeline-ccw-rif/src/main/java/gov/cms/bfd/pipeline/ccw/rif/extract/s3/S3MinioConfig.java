d83821196c17e888985834ccc2650f44c7b58040
Chris Brune <64486927+cbrunefearless@users.noreply.github.com>
2021-09-02 11:29:34 -0400

    [BFD-801] - s3 minio (#749)
    * Setup minio client in s3 utilties
    
    * pass minio system properties to capsule command
    
    * Added instructions to readme
    
    * added synchronized to singleton for thread safety
    
    * Added pr review comments
    
    * Add docker compose file
    
    * New endline at end of docker compose
    
    * Added parameters to readme

---
+++ apps/bfd-pipeline/bfd-pipeline-ccw-rif/src/main/java/gov/cms/bfd/pipeline/ccw/rif/extract/s3/S3MinioConfig.java
@@ -0,0 +1,32 @@
+package gov.cms.bfd.pipeline.ccw.rif.extract.s3;
+
+/**
+ * S3MinioConfig is an object for configuring minio to use as a S3 bucket for local development
+ * Username, password, and minioaddress can be passed in as maven parameters
+ */
+public class S3MinioConfig {
+
+  public String minioUserName;
+  public String minioPassword;
+  public String minioEndpointAddress;
+  public Boolean useMinio;
+
+  private static S3MinioConfig single_instance = null;
+
+  private S3MinioConfig() {
+    minioUserName = System.getProperty("s3.localUser", "bfdLocalS3Dev");
+    minioPassword = System.getProperty("s3.localPass", "bfdLocalS3Dev");
+    minioEndpointAddress = System.getProperty("s3.localAddress", "http://localhost:9000");
+    useMinio = Boolean.parseBoolean(System.getProperty("s3.local"));
+  }
+  /**
+   * Singleton method makes sure there is one instance and one instance only of the S3MinioConfig
+   * Class
+   */
+  public static synchronized S3MinioConfig Singleton() {
+    if (single_instance == null) {
+      single_instance = new S3MinioConfig();
+    }
+    return single_instance;
+  }
+}
