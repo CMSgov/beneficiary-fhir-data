a24b7e5aebd19fc05ed166088ff6dcb5fbfaa465
Chris Trotter <chris.trotter@adhocteam.us>
2021-09-28 14:36:24 -0400

    Modify ops code to use jdk 11

--- ops/jenkins/cbc-build-push.sh
+++ ops/jenkins/cbc-build-push.sh
@@ -3,7 +3,7 @@
 DOCKER_TAG=${1:-jdk8-mvn3-an29-tf12}
 
 docker build --file Dockerfile.cbc-build \
-  --build-arg JAVA_VERSION=${2:-8} \
+  --build-arg JAVA_VERSION=${2:-11} \
   --build-arg MAVEN_VERSION=${3:-3} \
   --build-arg ANSIBLE_VERSION=${4:-2.9.25} \
   --build-arg PACKER_VERSION=${5:-1.6.6} \
