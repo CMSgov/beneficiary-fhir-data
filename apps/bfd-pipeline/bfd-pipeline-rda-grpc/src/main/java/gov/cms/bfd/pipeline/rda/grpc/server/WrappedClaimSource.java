4f9fbd80aca97b5e5e3152a2245ad503ab652c2f
Brian Burton <brian.burton@adhocteam.us>
2021-10-25 09:31:53 -0400

    Sets a maximum size on the cache used to decide change type.

--- apps/bfd-pipeline/bfd-pipeline-rda-grpc/src/main/java/gov/cms/bfd/pipeline/rda/grpc/server/WrappedClaimSource.java
+++ apps/bfd-pipeline/bfd-pipeline-rda-grpc/src/main/java/gov/cms/bfd/pipeline/rda/grpc/server/WrappedClaimSource.java
@@ -1,5 +1,8 @@
 package gov.cms.bfd.pipeline.rda.grpc.server;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.protobuf.Timestamp;
 import gov.cms.mpsm.rda.v1.ChangeType;
 import gov.cms.mpsm.rda.v1.FissClaimChange;
@@ -7,9 +10,6 @@ import gov.cms.mpsm.rda.v1.McsClaimChange;
 import gov.cms.mpsm.rda.v1.fiss.FissClaim;
 import gov.cms.mpsm.rda.v1.mcs.McsClaim;
 import java.time.Clock;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.function.Function;
 
 /**
@@ -19,24 +19,30 @@ import java.util.function.Function;
  * @param <T> either FissClaim or McsClaim
  */
 public class WrappedClaimSource<TChange, TClaim> implements MessageSource<TChange> {
+  // Cache used to select whether to return CHANGE_TYPE_UPDATE or CHANGE_TYPE_INSERT.
+  private static final int KEY_CACHE_SIZE = 20_000;
+
   private final MessageSource<TClaim> source;
   private final Clock clock;
   private long sequenceNumber;
   private final Function<TClaim, String> keyExtractor;
   private final ChangeFactory<TChange, TClaim> changeFactory;
-  private final Set<String> keys;
+  private final Cache<String, String> knownKeys;
 
   /**
-   * Creates a wrapper object to promote each claim from source into a ClaimChange object with
-   * ChangeType of CHANGE_TYPE_UPDATE.
+   * Creates a wrapper object to promote each claim from source into a ClaimChange object. A cache
+   * is used to decide which ChangeType to use. The cache is imperfect but since this class is only
+   * intended for testing with a random claim source it is suitable for testing purposes.
    *
    * @param source the actual source of FISS/MCS claims
    * @param setter lambda to add the claim to the appropriate field in the ClaimChange builder
    */
-  private WrappedClaimSource(
+  @VisibleForTesting
+  WrappedClaimSource(
       MessageSource<TClaim> source,
       Clock clock,
       long sequenceNumber,
+      int keyCacheSize,
       Function<TClaim, String> keyExtractor,
       ChangeFactory<TChange, TClaim> changeFactory) {
     this.source = source;
@@ -44,7 +50,7 @@ public class WrappedClaimSource<TChange, TClaim> implements MessageSource<TChang
     this.sequenceNumber = sequenceNumber;
     this.keyExtractor = keyExtractor;
     this.changeFactory = changeFactory;
-    this.keys = Collections.synchronizedSet(new HashSet<>());
+    this.knownKeys = CacheBuilder.newBuilder().maximumSize(keyCacheSize).build();
   }
 
   @Override
@@ -57,7 +63,8 @@ public class WrappedClaimSource<TChange, TClaim> implements MessageSource<TChang
     final Timestamp timestamp =
         Timestamp.newBuilder().setSeconds(clock.instant().getEpochSecond()).build();
     final TClaim claim = source.next();
-    final boolean inserted = keys.add(keyExtractor.apply(claim));
+    final String key = keyExtractor.apply(claim);
+    final boolean inserted = knownKeys.asMap().putIfAbsent(key, key) == null;
     final ChangeType changeType =
         inserted ? ChangeType.CHANGE_TYPE_INSERT : ChangeType.CHANGE_TYPE_UPDATE;
     return changeFactory.create(timestamp, changeType, sequenceNumber++, claim);
@@ -74,6 +81,7 @@ public class WrappedClaimSource<TChange, TClaim> implements MessageSource<TChang
         source,
         clock,
         startingSequenceNumber,
+        KEY_CACHE_SIZE,
         FissClaim::getDcn,
         (timestamp, type, seq, claim) ->
             FissClaimChange.newBuilder()
@@ -90,6 +98,7 @@ public class WrappedClaimSource<TChange, TClaim> implements MessageSource<TChang
         source,
         clock,
         startingSequenceNumber,
+        KEY_CACHE_SIZE,
         McsClaim::getIdrClmHdIcn,
         (timestamp, type, seq, claim) ->
             McsClaimChange.newBuilder()
