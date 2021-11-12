9a468b78e83a76a399e7abeb6a100ac767e3224a
Eric Buckley <eric.buckley@gmail.com>
2021-11-12 10:26:15 -0800

    reverting nacl to using two rules

--- ops/terraform/modules/resources/rda_nacl/main.tf
+++ ops/terraform/modules/resources/rda_nacl/main.tf
@@ -9,15 +9,30 @@ resource "aws_network_acl" "rda" {
   tags   = var.env_config.tags
 }
 
-# When enabled, allow communication over 443 otherwise shutoff all communication
-resource "aws_network_acl_rule" "rda" {
+# When `mpm_enabled` is set to true, the known block will have a rule number
+# lower than the default to allow communication over the accepted port. Otherwise
+# the rule number will be higher than the default, which will cause ALL
+# communication to stop
+resource "aws_network_acl_rule" "rda_known" {
   count = var.mpm_rda_cidr_block != null ? 1 : 0
 
   network_acl_id = aws_network_acl.rda[0].id
-  rule_number    = 100
-  protocol       = var.mpm_enabled ? "tcp" : -1
-  rule_action    = var.mpm_enabled ? "allow" : "deny"
+  rule_number    = var.mpm_enabled ? 100 : 120
+  protocol       = "tcp"
+  rule_action    = "allow"
   cidr_block     = var.mpm_rda_cidr_block
-  from_port      = var.mpm_enabled ? 443 : 0
-  to_port        = var.mpm_enabled ? 443 : 0
+  from_port      = 443
+  to_port        = 443
+}
+
+resource "aws_network_acl_rule" "rda_default" {
+  count = var.mpm_rda_cidr_block != null ? 1 : 0
+
+  network_acl_id = aws_network_acl.rda[0].id
+  rule_number    = 110
+  protocol       = -1
+  rule_action    = "deny"
+  cidr_block     = var.mpm_rda_cidr_block
+  from_port      = 0
+  to_port        = 0
 }
