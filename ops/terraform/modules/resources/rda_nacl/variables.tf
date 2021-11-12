1a8bed24948b71111a5c59d2e486905634cc2b7a
Eric Buckley <eric.buckley@gmail.com>
2021-11-12 10:51:19 -0800

    adding env_config var to rda_nacl

--- ops/terraform/modules/resources/rda_nacl/variables.tf
+++ ops/terraform/modules/resources/rda_nacl/variables.tf
@@ -1,3 +1,8 @@
+variable "env_config" {
+  description = "All high-level info for the whole vpc"
+  type        = object({ env = string, tags = map(string), vpc_id = string, zone_id = string, azs = list(string) })
+}
+
 variable "mpm_rda_cidr_block" {
   description = "CIDR block of hosts available through the MPM VPC Peered environment"
   type        = string
