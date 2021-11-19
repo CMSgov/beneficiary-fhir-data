cd9583286557f9687fcf31fc4bb476a6a4e86d05
Chris Brune <cbrune@fearless.tech>
2021-11-18 22:49:03 -0500

    Added pipeline stuff

--- apps/bfd-pipeline/bfd-pipeline-app/src/main/assembly/bin/bfd-pipeline-app.sh
+++ apps/bfd-pipeline/bfd-pipeline-app/src/main/assembly/bin/bfd-pipeline-app.sh
@@ -5,6 +5,8 @@ scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bfdPipelineAppJar="$(echo ${scriptDirectory}/bfd-pipeline-app-*.jar)"
 classpath="${bfdPipelineAppJar}:${scriptDirectory}/lib/*"
 mainClass="gov.cms.bfd.pipeline.app.PipelineApplication"
+echo "JAVA HOMEEEE: "
+echo $JAVA_HOME
 [ -n "${JAVA_HOME}" ] && javaExecutable=${JAVA_HOME}/bin/java || javaExecutable=java
 
 exec "${javaExecutable}" -cp "${classpath}" "$@" "${mainClass}"
