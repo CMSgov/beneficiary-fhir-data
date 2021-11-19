f0655356100526f44c83332d3794bbe9a4d48dcc
Chris Brune <cbrune@fearless.tech>
2021-11-19 00:03:15 -0500

    Added shell commands

--- apps/bfd-pipeline/bfd-pipeline-app/src/main/assembly/bin/bfd-pipeline-app.sh
+++ apps/bfd-pipeline/bfd-pipeline-app/src/main/assembly/bin/bfd-pipeline-app.sh
@@ -5,7 +5,11 @@ scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bfdPipelineAppJar="$(echo ${scriptDirectory}/bfd-pipeline-app-*.jar)"
 classpath="${bfdPipelineAppJar}:${scriptDirectory}/lib/*"
 mainClass="gov.cms.bfd.pipeline.app.PipelineApplication"
+export JAVA_HOME=/usr/local/openjdk-11/
+export PATH="${JAVA_HOME}/bin:$PATH"
+
 echo "JAVA HOMEEEE: "
+echo "${JAVA_HOME}"
 echo $JAVA_HOME
 [ -n "${JAVA_HOME}" ] && javaExecutable=${JAVA_HOME}/bin/java || javaExecutable=java
 
