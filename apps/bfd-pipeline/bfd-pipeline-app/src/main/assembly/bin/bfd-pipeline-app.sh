b04e83822e4d3c1e4c6e476966941f1850f663f5
Chris Brune <cbrune@fearless.tech>
2021-11-22 14:04:07 -0500

    Added debugging

--- apps/bfd-pipeline/bfd-pipeline-app/src/main/assembly/bin/bfd-pipeline-app.sh
+++ apps/bfd-pipeline/bfd-pipeline-app/src/main/assembly/bin/bfd-pipeline-app.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
-
+set -x
 scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bfdPipelineAppJar="$(echo ${scriptDirectory}/bfd-pipeline-app-*.jar)"
 classpath="${bfdPipelineAppJar}:${scriptDirectory}/lib/*"
 mainClass="gov.cms.bfd.pipeline.app.PipelineApplication"
-
+echo "JENKINS HOME: "
+echo $JENKINS_HOME
+echo "JENKINS URL: "
+echo JENKINS_URL
 if [[ ! -z "${JENKINS_HOME}" ]] && [[ ! -z "${JENKINS_URL}" ]]; then
 	export JAVA_HOME=/usr/local/openjdk-11/
     export PATH="${JAVA_HOME}/bin:$PATH"
 fi
-
+echo "JAVA HOME: "
+echo $JAVA_HOME
 [ -n "${JAVA_HOME}" ] && javaExecutable=${JAVA_HOME}/bin/java || javaExecutable=java
 
 exec "${javaExecutable}" -cp "${classpath}" "$@" "${mainClass}"
--- apps/bfd-server/bfd-server-launcher/src/main/assembly/bin/bfd-server-launcher.sh
+++ apps/bfd-server/bfd-server-launcher/src/main/assembly/bin/bfd-server-launcher.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
-
+set -x
 scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bfdServerLauncherJar="$(echo ${scriptDirectory}/bfd-server-launcher-*.jar)"
 classpath="${bfdServerLauncherJar}:${scriptDirectory}/lib/*"
 mainClass="gov.cms.bfd.server.launcher.DataServerLauncherApp"
 
+echo "JENKINS HOME: "
+echo $JENKINS_HOME
+echo "JENKINS URL: "
+echo JENKINS_URL
 if [[ ! -z "${JENKINS_HOME}" ]] && [[ ! -z "${JENKINS_URL}" ]]; then
 	export JAVA_HOME=/usr/local/openjdk-11/
     export PATH="${JAVA_HOME}/bin:$PATH"
 fi
-
+echo "JAVA HOME: "
+echo $JAVA_HOME
 [ -n "${JAVA_HOME}" ] && javaExecutable=${JAVA_HOME}/bin/java || javaExecutable=java
 
 exec "${javaExecutable}" -cp "${classpath}" "$@" "${mainClass}"
