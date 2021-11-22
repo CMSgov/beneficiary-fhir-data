4b2cac9b0efd9af30a32436ab761160768e15b32
Chris Brune <cbrune@fearless.tech>
2021-11-22 13:42:16 -0500

    Added jenkins check

--- apps/bfd-pipeline/bfd-pipeline-app/src/main/assembly/bin/bfd-pipeline-app.sh
+++ apps/bfd-pipeline/bfd-pipeline-app/src/main/assembly/bin/bfd-pipeline-app.sh
@@ -4,6 +4,11 @@ scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bfdPipelineAppJar="$(echo ${scriptDirectory}/bfd-pipeline-app-*.jar)"
 classpath="${bfdPipelineAppJar}:${scriptDirectory}/lib/*"
 mainClass="gov.cms.bfd.pipeline.app.PipelineApplication"
+
+if [[ ! -z "${JENKINS_HOME}" ]] && [[ ! -z "${JENKINS_URL}" ]]; then
+	export JAVA_HOME=/usr/local/openjdk-11
+fi
+
 [ -n "${JAVA_HOME}" ] && javaExecutable=${JAVA_HOME}/bin/java || javaExecutable=java
 
 exec "${javaExecutable}" -cp "${classpath}" "$@" "${mainClass}"
--- apps/bfd-server/bfd-server-launcher/src/main/assembly/bin/bfd-server-launcher.sh
+++ apps/bfd-server/bfd-server-launcher/src/main/assembly/bin/bfd-server-launcher.sh
@@ -4,6 +4,11 @@ scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bfdServerLauncherJar="$(echo ${scriptDirectory}/bfd-server-launcher-*.jar)"
 classpath="${bfdServerLauncherJar}:${scriptDirectory}/lib/*"
 mainClass="gov.cms.bfd.server.launcher.DataServerLauncherApp"
+
+if [[ ! -z "${JENKINS_HOME}" ]] && [[ ! -z "${JENKINS_URL}" ]]; then
+	export JAVA_HOME=/usr/local/openjdk-11
+fi
+
 [ -n "${JAVA_HOME}" ] && javaExecutable=${JAVA_HOME}/bin/java || javaExecutable=java
 
 exec "${javaExecutable}" -cp "${classpath}" "$@" "${mainClass}"
