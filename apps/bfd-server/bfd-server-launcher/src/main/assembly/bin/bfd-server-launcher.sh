48cebc91239803309277a253131f53774c2492dc
Chris Brune <cbrune@fearless.tech>
2021-11-22 13:52:38 -0500

    Added export java home

--- apps/bfd-server/bfd-server-launcher/src/main/assembly/bin/bfd-server-launcher.sh
+++ apps/bfd-server/bfd-server-launcher/src/main/assembly/bin/bfd-server-launcher.sh
@@ -6,7 +6,8 @@ classpath="${bfdServerLauncherJar}:${scriptDirectory}/lib/*"
 mainClass="gov.cms.bfd.server.launcher.DataServerLauncherApp"
 
 if [[ ! -z "${JENKINS_HOME}" ]] && [[ ! -z "${JENKINS_URL}" ]]; then
-	export JAVA_HOME=/usr/local/openjdk-11
+	export JAVA_HOME=/usr/local/openjdk-11/
+    export PATH="${JAVA_HOME}/bin:$PATH"
 fi
 
 [ -n "${JAVA_HOME}" ] && javaExecutable=${JAVA_HOME}/bin/java || javaExecutable=java
