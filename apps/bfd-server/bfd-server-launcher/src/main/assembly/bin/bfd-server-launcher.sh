cefb106e7f3e542666ca7687d94bf9181a69b31c
Chris Brune <cbrune@fearless.tech>
2021-11-22 12:12:07 -0500

    Add java home to script

--- apps/bfd-server/bfd-server-launcher/src/main/assembly/bin/bfd-server-launcher.sh
+++ apps/bfd-server/bfd-server-launcher/src/main/assembly/bin/bfd-server-launcher.sh
@@ -4,8 +4,6 @@ scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bfdServerLauncherJar="$(echo ${scriptDirectory}/bfd-server-launcher-*.jar)"
 classpath="${bfdServerLauncherJar}:${scriptDirectory}/lib/*"
 mainClass="gov.cms.bfd.server.launcher.DataServerLauncherApp"
-export BFD_JAVA_HOME=/usr/local/openjdk-11/
-export PATH="${BFD_JAVA_HOME}/bin:$PATH"
-[ -n "${BFD_JAVA_HOME}" ] && javaExecutable=${BFD_JAVA_HOME}/bin/java || javaExecutable=java
+[ -n "${JAVA_HOME}" ] && javaExecutable=${JAVA_HOME}/bin/java || javaExecutable=java
 
 exec "${javaExecutable}" -cp "${classpath}" "$@" "${mainClass}"
