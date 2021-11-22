4edb3aa901ab0c06f4ae0e0f061dc94fede9225a
Chris Brune <cbrune@fearless.tech>
2021-11-22 10:40:47 -0500

    Setting java home

--- apps/bfd-pipeline/bfd-pipeline-app/src/main/assembly/bin/bfd-pipeline-app.sh
+++ apps/bfd-pipeline/bfd-pipeline-app/src/main/assembly/bin/bfd-pipeline-app.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
-set -x
-echo "Starting bfd-pipeline"
+
 scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bfdPipelineAppJar="$(echo ${scriptDirectory}/bfd-pipeline-app-*.jar)"
 classpath="${bfdPipelineAppJar}:${scriptDirectory}/lib/*"
 mainClass="gov.cms.bfd.pipeline.app.PipelineApplication"
-export JAVA_HOME=/usr/local/openjdk-11/
-export PATH="${JAVA_HOME}/bin:$PATH"
-
-echo "JAVA HOMEEEE: "
-echo "${JAVA_HOME}"
-echo $JAVA_HOME
-[ -n "${JAVA_HOME}" ] && javaExecutable=${JAVA_HOME}/bin/java || javaExecutable=java
+export BFD_JAVA_HOME=/usr/local/openjdk-11/
+export PATH="${BFD_JAVA_HOME}/bin:$PATH"
+[ -n "${BFD_JAVA_HOME}" ] && javaExecutable=${BFD_JAVA_HOME}/bin/java || javaExecutable=java
 
 exec "${javaExecutable}" -cp "${classpath}" "$@" "${mainClass}"
