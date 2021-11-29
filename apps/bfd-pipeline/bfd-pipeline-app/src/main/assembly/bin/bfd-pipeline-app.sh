256741e41627be09de28be9345e4575fba677ac6
Chris Brune <cbrune@fearless.tech>
2021-11-29 12:54:21 -0500

    Undo shell scripts

--- apps/bfd-pipeline/bfd-pipeline-app/src/main/assembly/bin/bfd-pipeline-app.sh
+++ apps/bfd-pipeline/bfd-pipeline-app/src/main/assembly/bin/bfd-pipeline-app.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-set -x
 scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bfdPipelineAppJar="$(echo ${scriptDirectory}/bfd-pipeline-app-*.jar)"
 classpath="${bfdPipelineAppJar}:${scriptDirectory}/lib/*"
 mainClass="gov.cms.bfd.pipeline.app.PipelineApplication"
-env
 [ -n "${JAVA_HOME}" ] && javaExecutable=${JAVA_HOME}/bin/java || javaExecutable=java
 
 exec "${javaExecutable}" -cp "${classpath}" "$@" "${mainClass}"
--- apps/bfd-server/bfd-server-launcher/src/main/assembly/bin/bfd-server-launcher.sh
+++ apps/bfd-server/bfd-server-launcher/src/main/assembly/bin/bfd-server-launcher.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-set -x
 scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bfdServerLauncherJar="$(echo ${scriptDirectory}/bfd-server-launcher-*.jar)"
 classpath="${bfdServerLauncherJar}:${scriptDirectory}/lib/*"
 mainClass="gov.cms.bfd.server.launcher.DataServerLauncherApp"
-env
 [ -n "${JAVA_HOME}" ] && javaExecutable=${JAVA_HOME}/bin/java || javaExecutable=java
 
 exec "${javaExecutable}" -cp "${classpath}" "$@" "${mainClass}"
