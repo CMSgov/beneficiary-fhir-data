171eafc3feb9606b90d531c982f2af2e3b3ab1e0
Logan Mitchell <logan.mitchell@adhocteam.us>
2021-11-29 08:27:38 -0800

    Adjust folder structure, write test runner, write command line runner, add v2 tests

---
+++ ops/ccs-ops-misc/load_test/common/test_setup.py
@@ -0,0 +1,32 @@
+import common.config as config
+import common.pull_bene_ids as benes
+import common.pull_hashed_mbis as mbi
+
+
+def generateAndLoadIds():
+    configFile = config.load()
+    return benes.loadData()
+
+def generateAndLoadMbis():
+    configFile = config.load()
+    return mbi.loadData()
+
+def getClientCert():
+    configFile = config.load()
+    return configFile["clientCertPath"]
+
+'''
+If there is a public key to verify the BFD Server's responses
+then it can be passed in with an environment variable. Otherwise,
+the error from the self-signed cert is ignored.
+'''
+def loadServerPublicKey():
+    try:
+        configFile = config.load()
+        server_public_key = configFile["serverPublicKey"]
+        if not server_public_key:
+            return False
+        else:
+            return server_public_key
+    except KeyError:
+        return False
---
+++ ops/ccs-ops-misc/load_test/v2/patient_test_id_includeIdentifiers.py
@@ -0,0 +1,29 @@
+import csv
+import datetime
+import os
+import sys
+import urllib3
+import common.config as config
+import common.test_setup as setup
+from locust import HttpUser, task
+
+server_public_key = setup.loadServerPublicKey()
+
+'''
+If there is no server cert, the warnings are disabled because thousands will appear in the logs and make it difficult
+to see anything else.
+'''
+if not server_public_key:
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+eob_ids = setup.generateAndLoadIds()
+client_cert = setup.getClientCert()
+
+class BFDUser(HttpUser):
+    @task
+    def patient(self):
+        id = eob_ids.pop()
+        self.client.get(f'/v2/fhir/Patient?_id={id}&_format=application%2Ffhir%2Bjson&_IncludeIdentifiers=true',
+                cert=client_cert,
+                verify=server_public_key,
+                name='/v2/fhir/Patient')
