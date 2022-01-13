3a17f6ecf590a5a7f3d6ea1b7db764874fae0826
mackec1 <chris.mackenzie@adhocteam.us>
2022-01-13 11:39:59 -0500

    fix DME claims; was missing the bene_id idx

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V41__Table_column_rename_DMEClaims.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V41__Table_column_rename_DMEClaims.sql
@@ -114,3 +114,6 @@ ${logic.hsql-only-alter} table public.dme_claims add constraint dme_claims_pkey 
 
 ${logic.hsql-only-alter} table public.dme_claim_lines ADD CONSTRAINT dme_claim_lines_clm_id_to_dme_claims FOREIGN KEY (clm_id) REFERENCES public.dme_claims (clm_id);
 ${logic.hsql-only-alter} table public.dme_claims ADD CONSTRAINT dme_claims_bene_id_to_beneficiaries FOREIGN KEY (bene_id) REFERENCES public.beneficiaries(bene_id);
+
+-- both psql and hsql
+ALTER INDEX "DMEClaims_beneficiaryId_idx" RENAME TO dme_claims_beneid_idx;
