60f11f6157014c845f29f63dbd4b05bf73110896
Eric Buckley <eric.buckley@gmail.com>
2022-02-01 18:28:55 -0800

    small changes to make it a little easier to read

--- ops/ccs-ops-misc/load_test/v2/partially_adjudicated.py
+++ ops/ccs-ops-misc/load_test/v2/partially_adjudicated.py
@@ -1,15 +1,14 @@
 import urllib3
-import common.config as config
-import common.data as data
-import common.errors as errors
-import common.test_setup as setup
+
 from locust import HttpUser, task, tag
+
+from common import config, data, errors, test_setup as setup
 
 server_public_key = setup.loadServerPublicKey()
 
 '''
-If there is no server cert, the warnings are disabled because thousands will appear in the logs and make it difficult
-to see anything else.
+If there is no server cert, the warnings are disabled because thousands will
+appear in the logs and make it difficult to see anything else.
 '''
 if not server_public_key:
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -20,53 +19,60 @@ setup.set_locust_env(config.load())
 
 
 class BFDUser(HttpUser):
-    SERVICE_DATE = 'gt2020-01-05'
-    LAST_UPDATED = 'gt2020-05-05'
+    SERVICE_DATE = 'service-date=gt2020-01-05'
+    LAST_UPDATED = '_lastUpdated=gt2020-05-05'
 
-    def hashed_mbi(self):
+    def _mbi(self):
         if len(mbis) == 0:
             errors.no_data_stop_test(self)
         return mbis.pop()
 
-    def base_query(self, query, name):
+    def _get(self, query, name):
         self.client.get(query, cert=client_cert, verify=server_public_key, name=name)
 
     @tag('claim')
     @task
-    def get_claim(self):
-        self.base_query(f'/v2/fhir/Claim?mbi={self.hashed_mbi()}', 'claim')
+    def _get_claim(self):
+        self._get(f'/v2/fhir/Claim?mbi={self._mbi()}', 'claim')
 
     @tag('claim', 'service-date')
     @task
-    def get_claim_with_service_date(self):
-        self.base_query(f'/v2/fhir/Claim?mbi={self.hashed_mbi()}&service-date={self.SERVICE_DATE}', 'claimServiceDate')
+    def _get_claim_with_service_date(self):
+        self._get(f'/v2/fhir/Claim?mbi={self._mbi()}&{self.SERVICE_DATE}',
+                  'claimServiceDate')
 
     @tag('claim', 'last-updated')
     @task
-    def get_claim_with_last_updated(self):
-        self.base_query(f'/v2/fhir/Claim?mbi={self.hashed_mbi()}&_lastUpdated={self.LAST_UPDATED}', 'claimLastUpdated')
+    def _get_claim_with_last_updated(self):
+        self._get(f'/v2/fhir/Claim?mbi={self._mbi()}&{self.LAST_UPDATED}',
+                  'claimLastUpdated')
 
     @tag('claim', 'service-date', 'last-updated')
     @task
-    def get_claim_with_service_date_and_last_updated(self):
-        self.base_query(f'/v2/fhir/Claim?mbi={self.hashed_mbi()}&_lastUpdated={self.LAST_UPDATED}&service-date={self.SERVICE_DATE}', 'claimServiceDateLastUpdated')
+    def _get_claim_with_service_date_and_last_updated(self):
+        self._get(f'/v2/fhir/Claim?mbi={self._mbi()}&{self.LAST_UPDATED}&{self.SERVICE_DATE}',
+                  'claimServiceDateLastUpdated')
 
     @tag("claim-response")
     @task
-    def get_claim_response(self):
-        self.base_query(f'/v2/fhir/ClaimResponse?mbi={self.hashed_mbi()}', 'claimResponse')
+    def _get_claim_response(self):
+        self._get(f'/v2/fhir/ClaimResponse?mbi={self._mbi()}',
+                  'claimResponse')
 
     @tag('claim-response', 'service-date')
     @task
-    def get_claim_response_with_service_date(self):
-        self.base_query(f'/v2/fhir/ClaimResponse?mbi={self.hashed_mbi()}&service-date={self.SERVICE_DATE}', 'claimResponseServiceDate')
+    def _get_claim_response_with_service_date(self):
+        self._get(f'/v2/fhir/ClaimResponse?mbi={self._mbi()}&{self.SERVICE_DATE}',
+                  'claimResponseServiceDate')
 
     @tag('claim-response', 'last-updated')
     @task
-    def get_claim_response_with_last_updated(self):
-        self.base_query(f'/v2/fhir/ClaimResponse?mbi={self.hashed_mbi()}&_lastUpdated={self.LAST_UPDATED}', 'claimResponseLastUpdated')
+    def _get_claim_response_with_last_updated(self):
+        self._get(f'/v2/fhir/ClaimResponse?mbi={self._mbi()}&{self.LAST_UPDATED}',
+                  'claimResponseLastUpdated')
 
     @tag('claim-response', 'service-date', 'last-updated')
     @task
-    def get_claim_response_with_service_date_and_last_updated(self):
-        self.base_query(f'/v2/fhir/ClaimResponse?mbi={self.hashed_mbi()}&_lastUpdated={self.LAST_UPDATED}&service-date={self.SERVICE_DATE}', 'claimResponseServiceDateLastUpdated')
+    def _get_claim_response_with_service_date_and_last_updated(self):
+        self._get(f'/v2/fhir/ClaimResponse?mbi={self._mbi()}&{self.LAST_UPDATED}&{self.SERVICE_DATE}',
+                  'claimResponseServiceDateLastUpdated')
