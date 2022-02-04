dce76d3f8b29c91c5088656b7f123a3cf96ee2c0
Jeremy Rosenfeld <jeremy.rosenfeld@adhocteam.us>
2022-02-04 13:25:26 -0800

    update comments for synthetic data sql queries

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/scripts/Query_synthetic_id_ranges.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/scripts/Query_synthetic_id_ranges.sql
@@ -1,4 +1,15 @@
--- Find the beneficiaries.bene_id range to avoid collisions with synthetic data.
+/*
+ * Queries that should be run in prod-sandbox prior to generating new synthea data in order to ensure that any identifiers 
+ * that are generated do not conflict with existing synthetic data. In the case of claimId (for all non-partd event tables) 
+ * and pde_id (for the partd event table) new synthetic data is appended below the minimum value currently present. 
+ 
+ * In the case of beneficiary IDs (bene_id) and claim group IDs (clm_grp_id) it is necessary to 
+ * use a more complex query to extract all currently occupied ranges of IDs and find a gap that is big enough for the new generated data.
+ */
+ 
+
+-- Find the beneficiaries.bene_id range to avoid collisions with synthetic data. 
+-- The range should have a relatively large gap, preferably contiguous with the last range used.
 WITH ids AS (
   SELECT
     CAST(bene_id AS numeric) AS id
@@ -18,7 +29,7 @@ GROUP BY grouping
 ORDER BY MIN(id);
 
 -- Find  min claim Ids for all claims except Part D Events.
--- This serves as an end range to avoid collisions with future synthetic data batches.
+-- The new synthetic data claim Id start should be 1 less than the value returned to avoid collisions with future synthetic data batches.
 select min(id) from (
 	select CAST(clm_id AS numeric) id 
 	from carrier_claims
@@ -49,16 +60,17 @@ select min(id) from (
 	where bene_id LIKE '-%' AND clm_id LIKE '-%'
 	) as ids;
 	
--- Find  min Part D Event id range to avoid collisions with future synthetic data batches.
+-- Find  min Part D Event (PDE) Ids.
+-- The new synthetic data PDE event Id start should be 1 less than the value returned to avoid collisions with future synthetic data batches.
 select min(id) from (
 	select CAST(pde_id AS numeric) id 
 	from partd_events
 	where bene_id LIKE '-%' AND pde_id LIKE '-%'
 	) as ids;
 
--- Find claim group id ranges.
+-- Find the claim group id range to avoid collisions with synthetic data. 
+-- The range should have a relatively large gap, preferably contiguous with the last range used.
 -- This query should be run before and after generating any synthetic data in prod SBX
--- to gauge ensure how large the next batch of synthetic data can be without overlap.
 WITH ids AS (
 	select CAST(clm_grp_id AS numeric) id 
 	from carrier_claims
