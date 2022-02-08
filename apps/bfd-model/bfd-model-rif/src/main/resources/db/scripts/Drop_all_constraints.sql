6415a4cdd3db0919a34457bd57cbb6a11e8bf828
Chris MacKenzie <chris.mackenzie@adhocteam.us>
2022-02-08 12:05:12 -0500

    BFD-1515: update db scripts to use new table and column names (#931)
    * modified to match new CCW table and column names
    
    * modified to match new CCW table and column names
    
    * fix cut-paste error; didn't paste correctly from pgadmin test window

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/scripts/Drop_all_constraints.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/scripts/Drop_all_constraints.sql
@@ -2,31 +2,162 @@
  * This script will drop all primary keys, foreign keys, and indices used in 
  * the database. This is needed to speed up initial loads.
  */
+ALTER TABLE beneficiaries_history
+	DROP CONSTRAINT IF EXISTS beneficiaries_history_bene_id_to_beneficiary;
 
-alter table "CarrierClaimLines" drop constraint if exists "CarrierClaimLines_parentClaim_to_CarrierClaims";
-alter table "DMEClaimLines" drop constraint if exists "DMEClaimLines_parentClaim_to_DMEClaims";
-alter table "HHAClaimLines" drop constraint if exists "HHAClaimLines_parentClaim_to_HHAClaims";
-alter table "HospiceClaimLines" drop constraint if exists "HospiceClaimLines_parentClaim_to_HospiceClaims";
-alter table "InpatientClaimLines" drop constraint if exists "InpatientClaimLines_parentClaim_to_InpatientClaims";
-alter table "OutpatientClaimLines" drop constraint if exists "OutpatientClaimLines_parentClaim_to_OutpatientClaims";
-alter table "SNFClaimLines" drop constraint if exists "SNFClaimLines_parentClaim_to_SNFClaims";
+ALTER TABLE beneficiary_monthly
+	DROP CONSTRAINT IF EXISTS beneficiary_monthly_bene_id_to_beneficiary;
 
-alter table "Beneficiaries" drop constraint if exists "Beneficiaries_pkey";
-alter table "CarrierClaimLines" drop constraint if exists "CarrierClaimLines_pkey";
-alter table "CarrierClaims" drop constraint if exists "CarrierClaims_pkey";
-alter table "DMEClaimLines" drop constraint if exists "DMEClaimLines_pkey";
-alter table "DMEClaims" drop constraint if exists "DMEClaims_pkey";
-alter table "HHAClaimLines" drop constraint if exists "HHAClaimLines_pkey";
-alter table "HHAClaims" drop constraint if exists "HHAClaims_pkey";
-alter table "HospiceClaimLines" drop constraint if exists "HospiceClaimLines_pkey";
-alter table "HospiceClaims" drop constraint if exists "HospiceClaims_pkey";
-alter table "InpatientClaimLines" drop constraint if exists "InpatientClaimLines_pkey";
-alter table "InpatientClaims" drop constraint if exists "InpatientClaims_pkey";
-alter table "OutpatientClaimLines" drop constraint if exists "OutpatientClaimLines_pkey";
-alter table "OutpatientClaims" drop constraint if exists "OutpatientClaims_pkey";
-alter table "PartDEvents" drop constraint if exists "PartDEvents_pkey";
-alter table "SNFClaimLines" drop constraint if exists "SNFClaimLines_pkey";
-alter table "SNFClaims" drop constraint if exists "SNFClaims_pkey";
+ALTER TABLE carrier_claim_lines
+	DROP CONSTRAINT IF EXISTS carrier_claim_lines_clm_id_to_carrier_claims;
 
-drop index if exists "Beneficiaries_hicn_idx";
-drop index if exists "BeneficiariesHistory_hicn_idx";
+ALTER TABLE carrier_claims
+	DROP CONSTRAINT IF EXISTS carrier_claims_bene_id_to_beneficiaries;
+
+ALTER TABLE dme_claim_lines
+	DROP CONSTRAINT IF EXISTS dme_claim_lines_clm_id_to_dme_claims;
+
+ALTER TABLE dme_claims
+	DROP CONSTRAINT IF EXISTS dme_claims_bene_id_to_beneficiaries;
+
+ALTER TABLE hha_claim_lines
+	DROP CONSTRAINT IF EXISTS hha_claim_lines_clm_id_to_hha_claims;
+
+ALTER TABLE hha_claims
+	DROP CONSTRAINT IF EXISTS hha_claims_bene_id_to_beneficiaries;
+
+ALTER TABLE hospice_claim_lines
+	DROP CONSTRAINT IF EXISTS hospice_claim_lines_clm_id_to_hospice_claims;
+
+ALTER TABLE hospice_claims
+	DROP CONSTRAINT IF EXISTS hospice_claims_bene_id_to_beneficiaries;
+
+ALTER TABLE inpatient_claim_lines
+	DROP CONSTRAINT IF EXISTS inpatient_claim_lines_clm_id_to_inpatient_claims;
+
+ALTER TABLE inpatient_claims
+	DROP CONSTRAINT IF EXISTS inpatient_claims_bene_id_to_beneficiaries;
+
+ALTER TABLE loaded_batches
+	DROP CONSTRAINT IF EXISTS loaded_batches_loaded_file_id;
+
+ALTER TABLE medicare_beneficiaryid_history
+	DROP CONSTRAINT IF EXISTS medicare_beneficiaryid_history_bene_id_to_beneficiaries;
+
+ALTER TABLE outpatient_claim_lines
+	DROP CONSTRAINT IF EXISTS outpatient_claim_lines_clm_id_to_outpatient_claims;
+
+ALTER TABLE outpatient_claims
+	DROP CONSTRAINT IF EXISTS outpatient_claims_bene_id_to_beneficiaries;
+
+ALTER TABLE partd_events
+	DROP CONSTRAINT IF EXISTS partd_events_bene_id_to_beneficiaries;
+
+ALTER TABLE snf_claim_lines
+	DROP CONSTRAINT IF EXISTS snf_claim_lines_clm_id_to_snf_claims;
+
+ALTER TABLE snf_claims
+	DROP CONSTRAINT IF EXISTS snf_claims_bene_id_to_beneficiaries;
+
+-- drop primary keys
+ALTER TABLE beneficiaries_history_invalid_beneficiaries
+	DROP CONSTRAINT IF EXISTS beneficiaries_history_invalid_beneficiaries_pkey;
+
+ALTER TABLE beneficiaries_history
+	DROP CONSTRAINT IF EXISTS beneficiaries_history_pkey;
+
+ALTER TABLE beneficiaries
+	DROP CONSTRAINT IF EXISTS beneficiaries_pkey;
+
+ALTER TABLE beneficiary_monthly
+	DROP CONSTRAINT IF EXISTS beneficiary_monthly_pkey;
+
+ALTER TABLE carrier_claim_lines
+	DROP CONSTRAINT IF EXISTS carrier_claim_lines_pkey;
+
+ALTER TABLE carrier_claims
+	DROP CONSTRAINT IF EXISTS carrier_claims_pkey;
+
+ALTER TABLE dme_claim_lines
+	DROP CONSTRAINT IF EXISTS dme_claim_lines_pkey;
+
+ALTER TABLE dme_claims
+	DROP CONSTRAINT IF EXISTS dme_claims_pkey;
+
+ALTER TABLE hha_claim_lines
+	DROP CONSTRAINT IF EXISTS hha_claim_lines_pkey;
+
+ALTER TABLE hha_claims
+	DROP CONSTRAINT IF EXISTS hha_claims_pkey;
+
+ALTER TABLE hospice_claim_lines
+	DROP CONSTRAINT IF EXISTS hospice_claim_lines_pkey;
+
+ALTER TABLE hospice_claims
+	DROP CONSTRAINT IF EXISTS hospice_claims_pkey;
+
+ALTER TABLE inpatient_claim_lines
+	DROP CONSTRAINT IF EXISTS inpatient_claim_lines_pkey;
+
+ALTER TABLE inpatient_claims
+	DROP CONSTRAINT IF EXISTS inpatient_claims_pkey;
+
+ALTER TABLE loaded_batches
+	DROP CONSTRAINT IF EXISTS loaded_batches_pkey;
+
+ALTER TABLE loaded_files
+	DROP CONSTRAINT IF EXISTS loaded_files_pkey;
+
+ALTER TABLE medicare_beneficiaryid_history_invalid_beneficiaries
+	DROP CONSTRAINT IF EXISTS medicare_beneficiaryid_history_invalid_beneficiaries_pkey;
+
+ALTER TABLE medicare_beneficiaryid_history
+	DROP CONSTRAINT IF EXISTS medicare_beneficiaryid_history_pkey;
+
+ALTER TABLE outpatient_claim_lines
+	DROP CONSTRAINT IF EXISTS outpatient_claim_lines_pkey;
+
+ALTER TABLE outpatient_claims
+	DROP CONSTRAINT IF EXISTS outpatient_claims_pkey;
+
+ALTER TABLE partd_events
+	DROP CONSTRAINT IF EXISTS partd_events_pkey;
+
+ALTER TABLE snf_claim_lines
+	DROP CONSTRAINT IF EXISTS snf_claim_lines_pkey;
+
+ALTER TABLE snf_claims
+	DROP CONSTRAINT IF EXISTS snf_claims_pkey;
+
+-- drop indexes in beneficiaries table
+DROP INDEX IF EXISTS beneficiaries_hicn_idx;
+DROP INDEX IF EXISTS beneficiaries_mbi_hash_idx;
+DROP INDEX IF EXISTS beneficiaries_partd_contract_number_apr_id_idx;
+DROP INDEX IF EXISTS beneficiaries_partd_contract_number_aug_id_idx;
+DROP INDEX IF EXISTS beneficiaries_partd_contract_number_dec_id_idx;
+DROP INDEX IF EXISTS beneficiaries_partd_contract_number_feb_id_idx;
+DROP INDEX IF EXISTS beneficiaries_partd_contract_number_jan_id_idx;
+DROP INDEX IF EXISTS beneficiaries_partd_contract_number_jul_id_idx;
+DROP INDEX IF EXISTS beneficiaries_partd_contract_number_jun_id_idx;
+DROP INDEX IF EXISTS beneficiaries_partd_contract_number_mar_id_idx;
+DROP INDEX IF EXISTS beneficiaries_partd_contract_number_may_id_idx;
+DROP INDEX IF EXISTS beneficiaries_partd_contract_number_nov_id_idx;
+DROP INDEX IF EXISTS beneficiaries_partd_contract_number_oct_id_idx;
+DROP INDEX IF EXISTS beneficiaries_partd_contract_number_sept_id_idx;
+
+DROP INDEX IF EXISTS beneficiaries_history_bene_id_idx;
+DROP INDEX IF EXISTS beneficiaries_history_hicn_idx;
+DROP INDEX IF EXISTS beneficiaries_history_mbi_hash_idx;
+DROP INDEX IF EXISTS beneficiary_monthly_partd_contract_number_year_month_idx;
+DROP INDEX IF EXISTS beneficiary_monthly_year_month_partd_contract_bene_id_idx;
+DROP INDEX IF EXISTS medicare_beneficiaryid_history_bene_id_idx;
+
+DROP INDEX IF EXISTS carrier_claims_bene_id_idx;
+DROP INDEX IF EXISTS dme_claims_bene_id_idx;
+DROP INDEX IF EXISTS hha_claims_bene_id_idx;
+DROP INDEX IF EXISTS hospice_claims_bene_id_idx;
+DROP INDEX IF EXISTS inpatient_claims_bene_id_idx;
+DROP INDEX IF EXISTS loaded_batches_created_idx;
+DROP INDEX IF EXISTS outpatient_claims_bene_id_idx;
+DROP INDEX IF EXISTS partd_events_bene_id_idx;
+DROP INDEX IF EXISTS snf_claims_bene_id_idx;
