ee95ff20445607d918eb17431dc32971d46fb3ed
Logan Mitchell <logan.mitchell@adhocteam.us>
2022-02-09 15:50:42 -0800

    add transformUtils test

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/TransformerUtilsV2Test.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/TransformerUtilsV2Test.java
@@ -1,10 +1,13 @@
 package gov.cms.bfd.server.war.r4.providers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
@@ -100,5 +103,111 @@ public class TransformerUtilsV2Test {
         () -> {
           TransformerUtilsV2.mapEobCommonItemRevenueStatusCode(item, null, statusCode);
         });
+  }
+
+  /**
+   * Verifies that when the fiscalIntermediaryNumber is present on the claim, it is properly mapped
+   * to the supporting info as a value and coding.
+   */
+  @Test
+  public void mapEobCommonGroupInpOutHHAHospiceSNFWhenFiNumberPresentExpectSupportingInfoMapping() {
+
+    ExplanationOfBenefit eob = new ExplanationOfBenefit();
+
+    String fiNum = "12534";
+    String expectedDiscriminator = "https://bluebutton.cms.gov/resources/variables/fi_num";
+
+    TransformerUtilsV2.mapEobCommonGroupInpOutHHAHospiceSNF(
+        eob,
+        Optional.empty(),
+        ' ',
+        ' ',
+        Optional.empty(),
+        "",
+        ' ',
+        Optional.empty(),
+        BigDecimal.ZERO,
+        BigDecimal.ZERO,
+        Optional.of(fiNum),
+        Optional.empty());
+
+    assertNotNull(eob);
+    assertNotNull(eob.getSupportingInfo());
+    assertFalse(eob.getSupportingInfo().isEmpty());
+    // Find the supporting info that has the fi num data
+    ExplanationOfBenefit.SupportingInformationComponent supportingInfo =
+        eob.getSupportingInfo().stream()
+            .filter(
+                si ->
+                    si.getCode().getCoding().stream()
+                        .anyMatch(c -> expectedDiscriminator.equals(c.getSystem())))
+            .findFirst()
+            .orElse(null);
+    assertNotNull(supportingInfo, "Found no supporting info which had FI_NUM coding within.");
+    assertFalse(supportingInfo.isEmpty());
+    Optional<Coding> fiNumCoding =
+        supportingInfo.getCategory().getCoding().stream()
+            .filter(c -> expectedDiscriminator.equals(c.getCode()))
+            .findFirst();
+    assertTrue(
+        fiNumCoding.isPresent(),
+        "Missing expected supporting info category coding for FI_NUM (fiscalIntermediaryNumber)");
+    Optional<Coding> infoCoding =
+        supportingInfo.getCategory().getCoding().stream()
+            .filter(c -> "info".equals(c.getCode()))
+            .findFirst();
+    assertTrue(
+        infoCoding.isPresent(),
+        "Missing expected supporting info category coding for info (claim info category for FI_NUM)");
+
+    // Check Code exists with correct discriminator and value
+    Coding code =
+        supportingInfo.getCode().getCoding().stream()
+            .filter(c -> expectedDiscriminator.equals(c.getSystem()))
+            .findFirst()
+            .orElse(null);
+    assertNotNull(code, "Missing expected eob.coding ");
+    assertEquals(fiNum, supportingInfo.getCode().getCoding().get(0).getCode());
+  }
+
+  /**
+   * Verifies that when the fiscalIntermediaryNumber is not present on the claim, it is not mapped
+   * to the supporting info as a value and coding related to FI_NUM.
+   */
+  @Test
+  public void
+      mapEobCommonGroupInpOutHHAHospiceSNFWhenFiNumberNotPresentExpectNoSupportingInfoMapping() {
+
+    ExplanationOfBenefit eob = new ExplanationOfBenefit();
+
+    String expectedDiscriminator = "https://bluebutton.cms.gov/resources/variables/fi_num";
+
+    TransformerUtilsV2.mapEobCommonGroupInpOutHHAHospiceSNF(
+        eob,
+        Optional.empty(),
+        ' ',
+        ' ',
+        Optional.empty(),
+        "",
+        ' ',
+        Optional.empty(),
+        BigDecimal.ZERO,
+        BigDecimal.ZERO,
+        Optional.empty(),
+        Optional.empty());
+
+    assertNotNull(eob);
+    assertNotNull(eob.getSupportingInfo());
+    assertFalse(eob.getSupportingInfo().isEmpty());
+    // Should find no supporting info which has fi_num related coding
+    ExplanationOfBenefit.SupportingInformationComponent supportingInfo =
+        eob.getSupportingInfo().stream()
+            .filter(
+                si ->
+                    si.getCode().getCoding().stream()
+                        .anyMatch(c -> expectedDiscriminator.equals(c.getSystem())))
+            .findFirst()
+            .orElse(null);
+    assertNull(supportingInfo);
   }
 }
