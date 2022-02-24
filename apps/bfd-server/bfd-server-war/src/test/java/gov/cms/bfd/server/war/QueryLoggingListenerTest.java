de813a539dd60432375ab131b509605cb9879b07
Kyle Robbins <kyle.robbins@adhocteam.us>
2022-02-24 14:36:23 -0500

    [PACA-459] Add partially adj queries to QueryLoggingListener

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/QueryLoggingListenerTest.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/QueryLoggingListenerTest.java
@@ -144,4 +144,34 @@ public final class QueryLoggingListenerTest {
 
     assertEquals(QueryType.EOBS_BY_BENE_ID_SNF, QueryType.computeQueryType(queryInfo));
   }
+
+  /** Verifies that {@link QueryType#FISS_CLAIM} works as expected. */
+  @Test
+  public void computeQueryType_FISS_CLAIM() {
+    QueryInfo queryInfo =
+        new QueryInfo(
+            "select diagcodes0_.\"dcn\" as dcn1_2_0_, diagcodes0_.\"priority\" as priority2_2_0_, diagcodes0_.\"dcn\" as dcn1_2_1_, diagcodes0_.\"priority\" as priority2_2_1_, diagcodes0_.\"bitFlags\" as bitflags3_2_1_, diagcodes0_.\"diagCd2\" as diagcd4_2_1_, diagcodes0_.\"diagPoaInd\" as diagpoai5_2_1_, diagcodes0_.\"lastUpdated\" as lastupda6_2_1_ from \"pre_adj\".\"FissDiagnosisCodes\" diagcodes0_ where diagcodes0_.\"dcn\"=?");
+
+    assertEquals(QueryType.FISS_CLAIM, QueryType.computeQueryType(queryInfo));
+  }
+
+  /** Verifies that {@link QueryType#MCS_CLAIM} works as expected. */
+  @Test
+  public void computeQueryType_MCS_CLAIM() {
+    QueryInfo queryInfo =
+        new QueryInfo(
+            "select diagcodes0_.\"idrClmHdIcn\" as idrclmhd1_10_0_, diagcodes0_.\"priority\" as priority2_10_0_, diagcodes0_.\"idrClmHdIcn\" as idrclmhd1_10_1_, diagcodes0_.\"priority\" as priority2_10_1_, diagcodes0_.\"idrDiagCode\" as idrdiagc3_10_1_, diagcodes0_.\"idrDiagIcdType\" as idrdiagi4_10_1_, diagcodes0_.\"lastUpdated\" as lastupda5_10_1_ from \"pre_adj\".\"McsDiagnosisCodes\" diagcodes0_ where diagcodes0_.\"idrClmHdIcn\"=?");
+
+    assertEquals(QueryType.MCS_CLAIM, QueryType.computeQueryType(queryInfo));
+  }
+
+  /** Verifies that {@link QueryType#MBI_CACHE} works as expected. */
+  @Test
+  public void computeQueryType_MBI_CACHE() {
+    QueryInfo queryInfo =
+        new QueryInfo(
+            "select mbi0_.\"mbiId\" as mbiid1_5_0_, mbi0_.\"hash\" as hash2_5_0_, mbi0_.\"lastUpdated\" as lastupda3_5_0_, mbi0_.\"mbi\" as mbi4_5_0_, mbi0_.\"oldHash\" as oldhash5_5_0_ from \"pre_adj\".\"MbiCache\" mbi0_ where mbi0_.\"mbiId\"=?");
+
+    assertEquals(QueryType.MBI_CACHE, QueryType.computeQueryType(queryInfo));
+  }
 }
