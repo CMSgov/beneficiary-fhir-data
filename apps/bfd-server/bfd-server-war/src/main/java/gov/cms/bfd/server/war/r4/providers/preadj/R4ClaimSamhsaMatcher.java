c662ee2ac4067939ef7d61c5db6ecd68637d520e
Kyle Robbins <kyle.robbins@adhocteam.us>
2022-03-02 11:52:34 -0500

    [PACA-461] PR Fix - Simplify method logic

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/r4/providers/preadj/R4ClaimSamhsaMatcher.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/r4/providers/preadj/R4ClaimSamhsaMatcher.java
@@ -1,13 +1,10 @@
 package gov.cms.bfd.server.war.r4.providers.preadj;
 
 import gov.cms.bfd.server.war.adapters.CodeableConcept;
-import gov.cms.bfd.server.war.adapters.Coding;
 import gov.cms.bfd.server.war.adapters.r4.ClaimAdapter;
 import gov.cms.bfd.server.war.commons.AbstractSamhsaMatcher;
 import gov.cms.bfd.server.war.commons.TransformerConstants;
-import java.util.Set;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import org.hl7.fhir.r4.model.Claim;
 import org.springframework.stereotype.Component;
 
@@ -24,10 +21,6 @@ import org.springframework.stereotype.Component;
 @Component
 public final class R4ClaimSamhsaMatcher extends AbstractSamhsaMatcher<Claim> {
 
-  // Valid system url for productOrService coding
-  private static final Set<String> HCPCS_SYSTEM =
-      Set.of(TransformerConstants.CODING_SYSTEM_CARIN_HCPCS);
-
   /** @see Predicate#test(Object) */
   @Override
   public boolean test(Claim claim) {
@@ -41,20 +34,14 @@ public final class R4ClaimSamhsaMatcher extends AbstractSamhsaMatcher<Claim> {
   /**
    * Checks if the given {@link CodeableConcept} contains only known coding systems.
    *
-   * <p>For v2 FHIR resources, we include backwards compatability for the
-   * https://bluebutton.cms.gov/resources/variables/hcpcs_cd system.
-   *
    * @param procedureConcept the procedure {@link CodeableConcept} to check
-   * @return <code>true</code> if the specified procedure {@link CodeableConcept} contains at least
-   *     one {@link Coding} and only contains {@link Coding}s that have known coding systems, <code>
-   *     false</code> otherwise
+   * @return <code>true</code> if the specified procedure {@link CodeableConcept} contains only the
+   *     carin HCPCS system, <code>false</code> otherwise.
    */
   @Override
   protected boolean containsOnlyKnownSystems(CodeableConcept procedureConcept) {
-    Set<String> codingSystems =
-        procedureConcept.getCoding().stream().map(Coding::getSystem).collect(Collectors.toSet());
-
-    return codingSystems.equals(HCPCS_SYSTEM);
+    return procedureConcept.getCoding().stream()
+        .allMatch(c -> c.getSystem().equals(TransformerConstants.CODING_SYSTEM_CARIN_HCPCS));
   }
 
   /**
