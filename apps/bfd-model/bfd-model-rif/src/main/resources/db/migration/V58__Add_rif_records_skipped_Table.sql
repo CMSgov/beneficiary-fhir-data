31f1806c432b2c1f5ecf7df350f446145ab12f49
Logan Mitchell <logan.mitchell@adhocteam.us>
2022-03-03 09:34:48 -0800

    update formatting

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V58__Add_rif_records_skipped_Table.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V58__Add_rif_records_skipped_Table.sql
@@ -13,19 +13,22 @@ CREATE TABLE skipped_rif_records (
   -- Sequence-generated PK for these records, as no natural PK is feasible.
   record_id            bigint                     NOT NULL PRIMARY KEY,
 
-  -- The timestamp associated with the CCW data set manifest that this record is from.
-  rif_file_timestamp   timestamp with time zone   NOT NULL,
+  -- A code indicating why this particular RIF record(s) were skipped.
+  skip_reason          varchar(50)                NOT NULL,
 
-  -- The `RifFileType` (e.g. `BENEFICIARY`, `CARRIER`, etc.) of the RIF file that this record is from.
+  -- The timestamp associated with the CCW data set manifest that this record(s) is from.
+  rif_file_timestamp timestamp with time zone     NOT NULL,
+
+  -- The `RifFileType` (e.g. `BENEFICIARY`, `CARRIER`, etc.) of the RIF file that this record(s) is from.
   rif_file_type        varchar(48)                NOT NULL,
 
   -- The `DML_IND` of the RIF record(s).
   dml_ind              varchar(6)                 NOT NULL,
 
-  -- The `bene_id` of the beneficiary that this record is of / associated with.
-  bene_id              varchar(15)                     NOT NULL,
+  -- The `bene_id` of the beneficiary that this record(s) is of / associated with.
+  bene_id              varchar(15)                NOT NULL,
 
-  -- The RIF/CSV row or rows representing the record (i.e. beneficiary or claim) that was skipped.
+  -- The RIF/CSV row or rows representing the record(s) (i.e. beneficiary or claim) that was skipped.
   rif_data             ${type.text}               NOT NULL
 );
 
@@ -37,4 +40,4 @@ CREATE SEQUENCE skipped_rif_records_record_id_seq
 -- This index will allow for fast queries to determine whether or not a given `beneficiaries` record has been
 -- impacted by the new load filtering. This will allow the BFD Server to report that status in its `Patient`
 -- responses.
-CREATE INDEX skipped_rif_records_bene_id_idx on skipped_rif_records(bene_id);
+CREATE INDEX skipped_rif_records_bene_id_idx on skipped_rif_records (bene_id);
