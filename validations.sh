5b9761f96b093e0da66552e6d7b85f8799fdbdbf
Kyle Robbins <kyle.robbins@adhocteam.us>
2022-03-09 15:25:19 -0500

    [PACA-477] FHIR Validation script and runner

---
+++ validations.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+failedValidations=()
+
+for file in ./apps/bfd-server/bfd-server-war/src/test/resources/endpoint-responses/v2/*.json; do
+  if ! java -jar validator_cli.jar $file -version 4.0; then
+    failedValidations+=($file)
+  fi
+done
+
+echo "${#failedValidations[@]} resources failed validation"
+
+for file in ${failedValidations[@]}; do
+  echo "    $file"
+done
