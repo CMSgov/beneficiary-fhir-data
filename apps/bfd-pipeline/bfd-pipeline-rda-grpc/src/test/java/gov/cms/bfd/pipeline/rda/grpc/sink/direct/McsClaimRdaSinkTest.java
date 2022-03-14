0660ffc83c22341ac3b028ec52605aa5f2acfe7f
Brian Burton <brian.burton@adhocteam.us>
2022-03-14 10:16:31 -0400

    PACA-238: capture rda api meta data (#908)
    * Adds meta data table tracking FISS and MCS claims from RDA API.
    
    * Removes TODO comment.
    
    * Adds comments based on PR review feedback.

--- apps/bfd-pipeline/bfd-pipeline-rda-grpc/src/test/java/gov/cms/bfd/pipeline/rda/grpc/sink/direct/McsClaimRdaSinkTest.java
+++ apps/bfd-pipeline/bfd-pipeline-rda-grpc/src/test/java/gov/cms/bfd/pipeline/rda/grpc/sink/direct/McsClaimRdaSinkTest.java
@@ -1,10 +1,15 @@
 package gov.cms.bfd.pipeline.rda.grpc.sink.direct;
 
-import static gov.cms.bfd.pipeline.rda.grpc.RdaPipelineTestUtils.*;
+import static gov.cms.bfd.pipeline.rda.grpc.RdaPipelineTestUtils.assertGaugeReading;
+import static gov.cms.bfd.pipeline.rda.grpc.RdaPipelineTestUtils.assertMeterReading;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.codahale.metrics.MetricRegistry;
 import com.google.common.collect.ImmutableList;
@@ -87,6 +92,9 @@ public class McsClaimRdaSinkTest {
     for (RdaChange<PreAdjMcsClaim> change : batch) {
       PreAdjMcsClaim claim = change.getClaim();
       verify(entityManager).merge(claim);
+    }
+    for (RdaChange<PreAdjMcsClaim> change : batch) {
+      verify(entityManager).persist(sink.createMetaData(change));
     }
     // the merge transaction will be committed
     verify(transaction).commit();
