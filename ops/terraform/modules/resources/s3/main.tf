86725ae530305d0d08b456a8c03f259a739166f8
keithdadkins <keithdadkins@me.com>
2022-03-14 14:37:57 -0500

    block s3 public acls/policy at bucket level

--- ops/terraform/modules/resources/s3/main.tf
+++ ops/terraform/modules/resources/s3/main.tf
@@ -45,6 +45,8 @@ resource "aws_s3_bucket" "main" {
 resource "aws_s3_bucket_public_access_block" "main" {
   bucket = aws_s3_bucket.main.id
 
-  block_public_acls   = true
-  block_public_policy = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  block_public_acls       = true
+  restrict_public_buckets = true
 }
