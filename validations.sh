4aebf741956c7e7b9e0a6267bb0f55977039b07f
Kyle Robbins <kyle.robbins@adhocteam.us>
2022-03-16 08:48:20 -0400

    [PACA-477] Limit validations to recent branch changes

--- validations.sh
+++ validations.sh
@@ -2,11 +2,23 @@
 
 failedValidations=()
 
-for file in ./apps/bfd-server/bfd-server-war/src/test/resources/endpoint-responses/v2/*.json; do
-  if ! java -Xmx3G -Xms2G -jar validator_cli.jar $file -version 4.0; then
-    failedValidations+=($file)
-  fi
-done
+ENDPOINT_DIR=apps/bfd-server/bfd-server-war/src/test/resources/endpoint-responses/v2
+
+RECENT_FILES=$(git log --name-only --pretty=oneline --full-index $GITHUB_REF..$GITHUB_BASE_REF | grep -vE '^[0-9a-f]{40} ' | sort | uniq | grep $ENDPOINT_DIR)
+
+if [[ $1 == *-r* ]]; then
+  for file in $RECENT_FILES; do
+    if ! java -Xmx3G -Xms2G -jar validator_cli.jar $file -version 4.0; then
+      failedValidations+=($file)
+    fi
+  done
+else
+  for file in $ENDPOINT_DIR/*.json; do
+    if ! java -Xmx3G -Xms2G -jar validator_cli.jar $file -version 4.0; then
+      failedValidations+=($file)
+    fi
+  done
+fi
 
 echo "${#failedValidations[@]} resources failed validation"
 
