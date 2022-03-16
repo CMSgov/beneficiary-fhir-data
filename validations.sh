0ad64069a8cc1d44fd6a77e06cea064877221c68
Kyle Robbins <kyle.robbins@adhocteam.us>
2022-03-16 09:05:27 -0400

    [PACA-477] Adjust head reference

--- validations.sh
+++ validations.sh
@@ -4,7 +4,7 @@ failedValidations=()
 
 ENDPOINT_DIR=apps/bfd-server/bfd-server-war/src/test/resources/endpoint-responses/v2
 
-RECENT_FILES=$(git log --name-only --pretty=oneline --full-index $GITHUB_REF..$GITHUB_BASE_REF | grep -vE '^[0-9a-f]{40} ' | sort | uniq | grep $ENDPOINT_DIR)
+RECENT_FILES=$(git log --name-only --pretty=oneline --full-index refs/heads/$GITHUB_HEAD_REF..$GITHUB_BASE_REF | grep -vE '^[0-9a-f]{40} ' | sort | uniq | grep $ENDPOINT_DIR)
 
 if [[ $1 == *-r* ]]; then
   for file in $RECENT_FILES; do
