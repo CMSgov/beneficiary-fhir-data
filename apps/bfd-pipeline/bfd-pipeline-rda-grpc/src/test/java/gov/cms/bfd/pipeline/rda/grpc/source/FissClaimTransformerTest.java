8d615fd9e950450ccaf96c094b0b58e688450005
Brian Burton <brian.burton@adhocteam.us>
2022-03-16 10:16:00 -0400

    Adds explanatory javadoc comments to test classes.

--- apps/bfd-pipeline/bfd-pipeline-rda-grpc/src/test/java/gov/cms/bfd/pipeline/rda/grpc/source/FissClaimTransformerTest.java
+++ apps/bfd-pipeline/bfd-pipeline-rda-grpc/src/test/java/gov/cms/bfd/pipeline/rda/grpc/source/FissClaimTransformerTest.java
@@ -56,6 +56,22 @@ import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Unit tests for the {@link FissClaimTransformer}. Unless otherwise stated on a method every test
+ * verifies that one or a set of fields within a source grpc message object for a claim have been
+ * correctly transformed into appropriate values and copied into a new {@link PreAdjFissClaim} JPA
+ * entity object.
+ *
+ * <p>Field tests are performed using an adaptor object appropriate for each type of grpc/jpa object
+ * pair. These adaptor objects ({@link ClaimFieldTester}, {@link AuditTrailFieldTester}, {@link
+ * BeneZPayerFieldTester}, {@link InsuredPayerFieldTester}, and {@link ProcCodeFieldTester}) extend
+ * the {@link ClaimTransformerFieldTester} class and provide class specific implementations of the
+ * methods used to construct and transform objects under test.
+ *
+ * <p>Each individual field test is named after the field it tests and calls appropriate
+ * verification methods for that field. {@see ClaimTransformerFieldTester} for documentation of each
+ * of the verification methods.
+ */
 public class FissClaimTransformerTest {
   // using a fixed Clock ensures our timestamp is predictable
   private final Clock clock = Clock.fixed(Instant.ofEpochMilli(1621609413832L), ZoneOffset.UTC);
