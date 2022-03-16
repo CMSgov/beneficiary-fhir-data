adfa20895707845bbf2ab1ec6475945b957a2294
Chris Trotter <chris.trotter@adhocteam.us>
2022-03-16 09:25:07 -0400

    make better

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/scripts/Select_NonNumeric_BeneAndClaimIds.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/scripts/Select_NonNumeric_BeneAndClaimIds.sql
@@ -6,106 +6,60 @@
 --
 
 -- Find non-numeric bene_id's in claim and claim_lines tables
-SELECT COUNT(*) FROM carrier_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM carrier_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$');
+SELECT clm_id FROM carrier_claim_lines
+WHERE clm_id IN (SELECT clm_id FROM carrier_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$');
 
-SELECT COUNT(*) FROM carrier_claims
-WHERE bene_id !~ E'^([-+])?[0-9\.]+$';
+SELECT bene_id, clm_id FROM carrier_claims
+WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$';
 
-SELECT COUNT(*) FROM dme_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM dme_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$');
+SELECT clm_id FROM dme_claim_lines
+WHERE clm_id IN (SELECT clm_id FROM dme_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$');
 
-SELECT COUNT(*) FROM dme_claims
-WHERE bene_id !~ E'^([-+])?[0-9\.]+$';
+SELECT bene_id, clm_id FROM dme_claims
+WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$';
 
-SELECT COUNT(*) FROM hha_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM hha_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$');
+SELECT clm_id FROM hha_claim_lines
+WHERE clm_id IN (SELECT clm_id FROM hha_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$');
 
-SELECT COUNT(*) FROM hha_claims
-WHERE bene_id !~ E'^([-+])?[0-9\.]+$';
+SELECT bene_id, clm_id FROM hha_claims
+WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$';
 
-SELECT COUNT(*) FROM hospice_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM hospice_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$');
+SELECT clm_id FROM hospice_claim_lines
+WHERE clm_id IN (SELECT clm_id FROM hospice_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$');
 
-SELECT COUNT(*) FROM hospice_claims
-WHERE bene_id !~ E'^([-+])?[0-9\.]+$';
+SELECT bene_id, clm_id FROM hospice_claims
+WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$';
 
-SELECT COUNT(*) FROM inpatient_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM inpatient_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$');
+SELECT clm_id FROM inpatient_claim_lines
+WHERE clm_id IN (SELECT clm_id FROM inpatient_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$');
 
-SELECT COUNT(*) FROM inpatient_claims
-WHERE bene_id !~ E'^([-+])?[0-9\.]+$';
+SELECT bene_id, clm_id FROM inpatient_claims
+WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$';
 
-SELECT COUNT(*) FROM outpatient_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM outpatient_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$');
+SELECT clm_id FROM outpatient_claim_lines
+WHERE clm_id IN (SELECT clm_id FROM outpatient_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$');
 
-SELECT COUNT(*) FROM outpatient_claims
-WHERE bene_id !~ E'^([-+])?[0-9\.]+$';
+SELECT bene_id, clm_id FROM outpatient_claims
+WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$';
 
-SELECT COUNT(*) FROM partd_events
-WHERE bene_id !~ E'^([-+])?[0-9\.]+$';
+SELECT pde_id FROM partd_events
+WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$';
 
-SELECT COUNT(*) FROM snf_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM snf_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$');
+SELECT clm_id FROM snf_claim_lines
+WHERE clm_id IN (SELECT clm_id FROM snf_claims WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$');
 
-SELECT COUNT(*) FROM snf_claims
-WHERE bene_id !~ E'^([-+])?[0-9\.]+$';
+SELECT bene_id, clm_id FROM snf_claims
+WHERE bene_id !~ E'^([-+])?[0-9\.]+$' OR clm_id !~ E'^([-+])?[0-9\.]+$';
 
 -- Find non-numeric bene_id's in beneficiary tables
-SELECT COUNT(*) FROM beneficiaries_history
+SELECT bene_id FROM beneficiaries_history
 WHERE bene_id !~ E'^([-+])?[0-9\.]+$';
 
-SELECT COUNT(*) FROM beneficiary_monthly
+SELECT bene_id FROM beneficiary_monthly
 WHERE bene_id !~ E'^([-+])?[0-9\.]+$';
 
-SELECT COUNT(*) FROM medicare_beneficiaryid_history
+SELECT bene_id FROM medicare_beneficiaryid_history
 WHERE bene_id !~ E'^([-+])?[0-9\.]+$';
 
-SELECT COUNT(*) FROM beneficiaries
+SELECT bene_id FROM beneficiaries
 WHERE bene_id !~ E'^([-+])?[0-9\.]+$';
-
--- Find non-numeric clm_id's in claim tables
-SELECT COUNT(*) FROM carrier_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM carrier_claims WHERE clm_id !~ E'^([-+])?[0-9\.]+$');
-
-SELECT COUNT(*) FROM carrier_claims
-WHERE clm_id !~ E'^([-+])?[0-9\.]+$';
-
-SELECT COUNT(*) FROM dme_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM dme_claims WHERE clm_id !~ E'^([-+])?[0-9\.]+$');
-
-SELECT COUNT(*) FROM dme_claims
-WHERE clm_id !~ E'^([-+])?[0-9\.]+$';
-
-SELECT COUNT(*) FROM hha_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM hha_claims WHERE clm_id !~ E'^([-+])?[0-9\.]+$');
-
-SELECT COUNT(*) FROM hha_claims
-WHERE clm_id !~ E'^([-+])?[0-9\.]+$';
-
-SELECT COUNT(*) FROM hospice_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM hospice_claims WHERE clm_id !~ E'^([-+])?[0-9\.]+$');
-
-SELECT COUNT(*) FROM hospice_claims
-WHERE clm_id !~ E'^([-+])?[0-9\.]+$';
-
-SELECT COUNT(*) FROM inpatient_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM inpatient_claims WHERE clm_id !~ E'^([-+])?[0-9\.]+$');
-
-SELECT COUNT(*) FROM inpatient_claims
-WHERE clm_id !~ E'^([-+])?[0-9\.]+$';
-
-SELECT COUNT(*) FROM outpatient_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM outpatient_claims WHERE clm_id !~ E'^([-+])?[0-9\.]+$');
-
-SELECT COUNT(*) FROM outpatient_claims
-WHERE clm_id !~ E'^([-+])?[0-9\.]+$';
-
-SELECT COUNT(*) FROM partd_events
-WHERE clm_id !~ E'^([-+])?[0-9\.]+$';
-
-SELECT COUNT(*) FROM snf_claim_lines
-WHERE clm_id IN (SELECT clm_id FROM snf_claims WHERE clm_id !~ E'^([-+])?[0-9\.]+$');
-
-SELECT COUNT(*) FROM snf_claims
-WHERE clm_id !~ E'^([-+])?[0-9\.]+$';
