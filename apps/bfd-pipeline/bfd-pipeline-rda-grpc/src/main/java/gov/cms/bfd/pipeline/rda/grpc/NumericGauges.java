5d9de7f8b896a1022f14c9df45218f46261dbd3d
Brian Burton <brian.burton@adhocteam.us>
2022-03-23 15:26:53 -0400

    Corrects comments.

--- apps/bfd-pipeline/bfd-pipeline-rda-grpc/src/main/java/gov/cms/bfd/pipeline/rda/grpc/NumericGauges.java
+++ apps/bfd-pipeline/bfd-pipeline-rda-grpc/src/main/java/gov/cms/bfd/pipeline/rda/grpc/NumericGauges.java
@@ -9,18 +9,17 @@ import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * DropWizard gauges are created using an implementation of an interface that contains a single
- * method to read the current value of the gauge. The {@link com.codahale.metrics.MetricRegistry}
- * remembers the actual gauge but not the getter method or the object backing it. This class
- * provides a way to retain the underlying value store (an {@link
- * java.util.concurrent.atomic.AtomicLong}) and provide access to its setter method on demand using
- * a unique name.
+ * method to read the current value for the gauge. The {@link com.codahale.metrics.MetricRegistry}
+ * remembers the actual gauge but not the object backing it. This class provides a way to retain the
+ * underlying value store (an {@link java.util.concurrent.atomic.AtomicLong}) and provide access to
+ * its setter method on demand using a unique name.
  */
 public class NumericGauges {
   private final Map<String, AtomicLong> gaugeValues = Collections.synchronizedMap(new HashMap<>());
 
   /**
-   * Looks up the {@link AtomicLong} for the given metric name. Creates an returns a new one if none
-   * currently exists. Repeated calls with the same name will return the same object.
+   * Looks up the {@link AtomicLong} for the given metric name. Creates and returns a new one if
+   * none currently exists. Repeated calls with the same name will return the same object.
    *
    * @param gaugeName unique name used to identify the gauge metric in {@link
    *     com.codahale.metrics.MetricRegistry}
@@ -35,7 +34,7 @@ public class NumericGauges {
    * new one if none is currently registered using an {@link AtomicLong} registered with this
    * object. Multiple calls with a given registry and name will return the same gauge and calling
    * {@link NumericGauges#getValueForName} with the same name will always return the underlying
-   * value for the gauge.
+   * object for the gauge.
    *
    * @param appMetrics registry to store the gauge itself
    * @param gaugeName name used to identify the gauge in the registry and the value store in this
