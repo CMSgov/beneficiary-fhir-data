4c63e8be7bff9acf4c0476b5c675646c4daa1be6
Andrew Kramer <andrew.kramer@adhocteam.us>
2022-04-15 10:20:08 -0400

    Adjust run time for ramp-up of clients

--- ops/ccs-ops-misc/load_test/runtests.py
+++ ops/ccs-ops-misc/load_test/runtests.py
@@ -1,10 +1,52 @@
-import os
+import re
+from math import ceil
+from datetime import timedelta
 import sys
 import getopt
 import common.config as config
 import common.test_setup as setup
 from locust.main import main
 from multiprocessing import Process
+
+'''
+Parse a given run time setting (which Locust accepts as combinations of "1m",
+"30s", "2h", etc.), and return the duration in seconds
+'''
+def parse_run_time(run_time):
+    # At least one digit, optional whitespace, single letter
+    base_pattern = r'\d+\s*[A-Z]'
+
+    # At least one base_pattern, optional whitespace in between, and optional
+    # whitespace on either end
+    full_pattern = rf"^\s*({base_pattern}\s*)+\s*$"
+
+    # Verify that the entire string is properly formatted
+    if not re.match(full_pattern, run_time, re.IGNORECASE):
+        return None
+
+    seconds_per_unit = {"s": 1, "m": 60, "h": 3600, "d": 86400}
+    total_seconds = 0
+    matches = re.findall(base_pattern, run_time, re.IGNORECASE)
+
+    for raw_match in matches:
+        match = raw_match.replace(" ", "").lower()
+        total_seconds += int(match[:-1]) * seconds_per_unit[match[-1]]
+
+    return total_seconds
+
+'''
+Adjust the run time of the test to account for the time it takes to instantiate
+and connect all the clients. If a user specifies a one-minute test, but it's
+going to take thirty seconds to ramp up to full clients, then we actually run
+for one minute and thirty seconds, so that we can have the specified time with
+full client capacity. You can optionally reset the statistics to zero at the
+end of this ramp-up period using the --resetStats command line flag.
+'''
+def adjusted_run_time(runTime, maxClients, clientsPerSecond):
+    run_time_seconds = parse_run_time(runTime)
+    if run_time_seconds is None:
+        return None
+    return run_time_seconds + ceil(int(maxClients) // int(clientsPerSecond))
 
 '''
 Runs a specified test via the input args.
@@ -74,6 +116,17 @@ def run_with_params(argv):
             print(helpString)
             sys.exit()
 
+    ## Add on extra time to the run-time to account for ramp-up of clients.
+    adjusted_time = adjusted_run_time(configData.testRunTime,
+        configData.testNumTotalClients, configData.testCreatedClientsPerSecond)
+    if adjusted_time is None:
+        print("Could not determine adjusted run time. Please use a format " +
+            "like \"1m 30s\" for the --testRunTime option")
+        sys.exit(1)
+    configData.testRunTime = f"{adjusted_time}s"
+    print('Run time adjusted to account for ramp-up time. New run time: %s' %
+        timedelta(seconds=adjusted_time))
+
     ## Check if all required params are set
     if not all([configData.homePath, configData.clientCertPath, configData.dbUri, configData.testHost, testFile]):
         print("Missing required arg (See -h for help on params)")
@@ -83,7 +136,7 @@ def run_with_params(argv):
     config.save(configData)
     setup.set_locust_test_name(testFile)
 
-    # strip off extra command line params for locust, or else it tries to parse them
+    ## strip off extra command line params for locust, or else it tries to parse them
     sys.argv = sys.argv[:1]
 
     if(int(workerThreads) > 1):
