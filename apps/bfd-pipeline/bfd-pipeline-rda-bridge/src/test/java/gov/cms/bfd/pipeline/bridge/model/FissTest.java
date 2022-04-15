96cef0bca0178cb1608af1f6c5ae2c1d1de952f8
Brian Burton <brian.burton@adhocteam.us>
2022-04-15 15:02:10 -0400

    Adds unit tests for new methods.

---
+++ apps/bfd-pipeline/bfd-pipeline-rda-bridge/src/test/java/gov/cms/bfd/pipeline/bridge/model/FissTest.java
@@ -0,0 +1,28 @@
+package gov.cms.bfd.pipeline.bridge.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import gov.cms.bfd.model.rif.InpatientClaimColumn;
+import org.junit.jupiter.api.Test;
+
+public class FissTest {
+  /** Verifies that all lists of numbered enum names appear in the proper sequence. */
+  @Test
+  public void testThatNumberedSequenceNamesAreInSequenceOrder() {
+    ModelUtilTest.assertNamesFollowSequence(
+        Fiss.CLM_POA_IND_SW, InpatientClaimColumn.CLM_POA_IND_SW1);
+    ModelUtilTest.assertNamesFollowSequence(Fiss.ICD_DGNS_CD, InpatientClaimColumn.ICD_DGNS_CD1);
+    ModelUtilTest.assertNamesFollowSequence(Fiss.ICD_PRCDR_CD, InpatientClaimColumn.ICD_PRCDR_CD1);
+    ModelUtilTest.assertNamesFollowSequence(Fiss.PRCDR_DT, InpatientClaimColumn.PRCDR_DT1);
+  }
+
+  /**
+   * Pairs of lists are iterated over in the same loop. This test ensures those lists have the same
+   * length to ensure we don't skip any values.
+   */
+  @Test
+  public void testThatListsOfNamesHaveSameLengths() {
+    assertEquals(Fiss.ICD_DGNS_CD.size(), Fiss.CLM_POA_IND_SW.size());
+    assertEquals(Fiss.ICD_PRCDR_CD.size(), Fiss.PRCDR_DT.size());
+  }
+}
---
+++ apps/bfd-pipeline/bfd-pipeline-rda-bridge/src/test/java/gov/cms/bfd/pipeline/bridge/model/McsTest.java
@@ -0,0 +1,25 @@
+package gov.cms.bfd.pipeline.bridge.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import gov.cms.bfd.model.rif.CarrierClaimColumn;
+import org.junit.jupiter.api.Test;
+
+public class McsTest {
+  /** Verifies that all lists of numbered enum names appear in the proper sequence. */
+  @Test
+  public void testThatNumberedSequenceNamesAreInSequenceOrder() {
+    ModelUtilTest.assertNamesFollowSequence(Mcs.ICD_DGNS_CD, CarrierClaimColumn.ICD_DGNS_CD1);
+    ModelUtilTest.assertNamesFollowSequence(
+        Mcs.ICD_DGNS_VRSN_CD, CarrierClaimColumn.ICD_DGNS_VRSN_CD1);
+  }
+
+  /**
+   * Pairs of lists are iterated over in the same loop. This test ensures those lists have the same
+   * length to ensure we don't skip any values.
+   */
+  @Test
+  public void testThatListsOfNamesHaveSameLengths() {
+    assertEquals(Mcs.ICD_DGNS_CD.size(), Mcs.ICD_DGNS_VRSN_CD.size());
+  }
+}
