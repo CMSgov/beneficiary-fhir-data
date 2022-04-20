e549e6f19932aa5a7640b807b459c2fa86e84952
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-04-20 13:31:22 -0400

    Support python versions less than 3.9 by using older type hinting syntax

--- ops/ccs-ops-misc/load_test/common/url_path.py
+++ ops/ccs-ops-misc/load_test/common/url_path.py
@@ -1,9 +1,10 @@
 import time
+from typing import Dict
 from urllib.parse import urlencode
 
 '''
 Creates a query path from a base path (i.e. /v2/fhir/Coverage) and a dictionary of query parameters
 '''
-def create_url_path(path: str, query_params: dict[str, str]):
+def create_url_path(path: str, query_params: Dict[str, str]) -> str:
   params = urlencode(query_params)
   return "{}?{}".format(path, params)
