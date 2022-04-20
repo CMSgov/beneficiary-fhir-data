f9dc8ee220409bbd4a6c3d0af2fb60dfb4472ed1
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-04-20 11:09:49 -0400

    Remove PACA tests from baseline tests

--- ops/ccs-ops-misc/load_test/v2/baseline_tests.py
+++ ops/ccs-ops-misc/load_test/v2/baseline_tests.py
@@ -16,7 +16,6 @@ appear in the logs and make it difficult to see anything else.
 if not server_public_key:
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# pa_mbis = data.load_pa_mbis()
 # mbis = data.load_mbis()
 eob_ids = data.load_bene_ids()
 client_cert = setup.getClientCert()
@@ -27,17 +26,6 @@ cursor_list = data.load_cursors("v2")
 setup.set_locust_env(config.load())
 
 class BFDUser(HttpUser):
-    # SERVICE_DATE = 'service-date=gt2020-01-05'
-    # LAST_UPDATED = '_lastUpdated=gt2020-05-05'
-
-    # def _mbi(self):
-    #     if len(pa_mbis) == 0:
-    #         errors.no_data_stop_test(self)
-    #     return pa_mbis.pop()
-
-    # def _get(self, query, name):
-    #     self.client.get(query, cert=client_cert, verify=server_public_key, name=name)
-        
     def __init__(self, *args, **kwargs):
         self.eob_ids = eob_ids.copy()
         super().__init__(*args, **kwargs)
@@ -107,53 +95,6 @@ class BFDUser(HttpUser):
                 cert=client_cert,
                 verify=server_public_key,
                 name='/v2/fhir/ExplanationOfBenefit search by id')
-
-    # @tag('claim')
-    # @task
-    # def _get_claim(self):
-    #     self._get(f'/v2/fhir/Claim?mbi={self._mbi()}', 'claim')
-
-    # @tag('claim', 'service-date')
-    # @task
-    # def _get_claim_with_service_date(self):
-    #     self._get(f'/v2/fhir/Claim?mbi={self._mbi()}&{self.SERVICE_DATE}',
-    #               'claimServiceDate')
-
-    # @tag('claim', 'last-updated')
-    # @task
-    # def _get_claim_with_last_updated(self):
-    #     self._get(f'/v2/fhir/Claim?mbi={self._mbi()}&{self.LAST_UPDATED}',
-    #               'claimLastUpdated')
-
-    # @tag('claim', 'service-date', 'last-updated')
-    # @task
-    # def _get_claim_with_service_date_and_last_updated(self):
-    #     self._get(f'/v2/fhir/Claim?mbi={self._mbi()}&{self.LAST_UPDATED}&{self.SERVICE_DATE}',
-    #               'claimServiceDateLastUpdated')
-
-    # @tag("claim-response")
-    # @task
-    # def _get_claim_response(self):
-    #     self._get(f'/v2/fhir/ClaimResponse?mbi={self._mbi()}',
-    #               'claimResponse')
-
-    # @tag('claim-response', 'service-date')
-    # @task
-    # def _get_claim_response_with_service_date(self):
-    #     self._get(f'/v2/fhir/ClaimResponse?mbi={self._mbi()}&{self.SERVICE_DATE}',
-    #               'claimResponseServiceDate')
-
-    # @tag('claim-response', 'last-updated')
-    # @task
-    # def _get_claim_response_with_last_updated(self):
-    #     self._get(f'/v2/fhir/ClaimResponse?mbi={self._mbi()}&{self.LAST_UPDATED}',
-    #               'claimResponseLastUpdated')
-
-    # @tag('claim-response', 'service-date', 'last-updated')
-    # @task
-    # def _get_claim_response_with_service_date_and_last_updated(self):
-    #     self._get(f'/v2/fhir/ClaimResponse?mbi={self._mbi()}&{self.LAST_UPDATED}&{self.SERVICE_DATE}',
-    #               'claimResponseServiceDateLastUpdated')
   
     # @task
     # def patient_test_coverageContract(self):
