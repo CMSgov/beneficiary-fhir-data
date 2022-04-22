8dd092e8f7338f30ad8278d1f000ad2345985aa1
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-04-22 11:44:03 -0400

    Properly handle case where params is empty in get() and create_url_path()

--- ops/ccs-ops-misc/load_test/common/url_path.py
+++ ops/ccs-ops-misc/load_test/common/url_path.py
@@ -5,6 +5,9 @@ from urllib.parse import urlencode
 '''
 Creates a query path from a base path (i.e. /v2/fhir/Coverage) and a dictionary of query parameters
 '''
-def create_url_path(path: str, query_params: Dict[str, str]) -> str:
+def create_url_path(path: str, query_params: Dict[str, str] = {}) -> str:
+  if not query_params:
+    return path
+
   params = urlencode(query_params)
   return "{}?{}".format(path, params)
