7408181a8fe7fcbe6da1e191dd7dd328db9aa550
mackec1 <chris.mackenzie@adhocteam.us>
2022-04-26 10:11:10 -0400

    Modify comments

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V65__Add_PreUpdate_Trigger_Beneficiaries.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V65__Add_PreUpdate_Trigger_Beneficiaries.sql
@@ -1,18 +1,15 @@
--- Add a pre-update trigger to the beneficiaries table to populate
--- bene_id_numeric on a record update. There is currently a
--- pre-insert trigger, but we also need a pre-update trigger since
--- the Beneficiary.java ORM object will initialize the native data
--- type to 0 (zero) when the object is instantiated.
+-- Add a pre-[insert|update] trigger to the beneficiaries table to populate
+-- bene_id_numeric on a record update. We need both pre-insert and
+-- pre-update trigger to fire since the Beneficiary.java ORM object will
+-- initialize the native data type to 0 (zero) when the object is instantiated.
 
--- create a database trigger which will be used to imbue a bigint value
--- into the bene_id_numeric column during a table record UPDATE operation.
--- The trigger is defined as a pre-update trigger, which means that prior to
--- record being written, the database engine will execute the trigger, causing
--- the bene_id_numeric to be populated with a value based on a 'cast'
--- of the bene_id (varchar).
+-- The database trigger will be used to imbue a bigint value into the bene_id_numeric
+-- column during a table record INSERT or UPDATE operation what this means is that
+-- prior to a record being written, the database engine will execute the trigger, causing
+-- the bene_id_numeric to be populated with a value based on a 'cast' of the bene_id (varchar).
+
 -- Unfortunately HSQL doesn't support 'insert or update' directive so we have to
 -- have a separate trigger for the update.
-
 ${logic.hsql-only} CREATE TRIGGER beneficiaries_pre_update_trigger BEFORE UPDATE ON public.beneficiaries
 ${logic.hsql-only}    REFERENCING NEW as newrow FOR EACH ROW
 ${logic.hsql-only}       BEGIN ATOMIC
