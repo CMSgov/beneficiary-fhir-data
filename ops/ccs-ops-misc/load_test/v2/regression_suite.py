861d3c3027dd7f8470ece717fc181e08b0336b67
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-04-27 16:31:53 -0400

    Add some documentation comments to each of the regression suites

--- ops/ccs-ops-misc/load_test/v2/regression_suite.py
+++ ops/ccs-ops-misc/load_test/v2/regression_suite.py
@@ -1,3 +1,11 @@
+"""Regression test suite for V2 BFD Server endpoints.
+
+The tests within this Locust test suite hit various endpoints that were
+determined to be representative of typical V2 endpoint loads. When running
+this test suite, all tests in this suite will be run in parallel, with
+equal weighting being applied to each.
+"""
+
 import random
 from typing import Dict
 import urllib3
@@ -21,6 +29,15 @@ cursor_list = data.load_cursors("v2")
 
 class BFDUser(HttpUser):
     def on_start(self):
+        """Run once when a BFDUser is initialized by Locust.
+
+        This method copies the necessary test data (lists of
+        MBIs, beneficiary IDs, and contract cursor URLs) as
+        members of this particular BFDUser instance. We then
+        shuffle these copied lists such that concurrent BFDUsers
+        are not querying the same data at the same time.
+        """
+
         copied_bene_ids = bene_ids.copy()
         random.shuffle(copied_bene_ids)
 
@@ -35,24 +52,54 @@ class BFDUser(HttpUser):
         self.cursor_list = copied_cursor_list
 
     def get_bene_ids(self) -> int:
+      """Returns the next beneficiary ID in this BFDUser's list of IDs.
+
+      This method pops (that is, takes the topmost item) the next beneficiary
+      ID in this instance's list of beneficiary IDs and returns it. If no
+      more IDs are available, this method will stop the test run.
+      """
+
       if len(self.bene_ids) == 0:
             errors.no_data_stop_test(self)
 
       return self.bene_ids.pop()
 
     def get_mbi(self) -> int:
+      """Returns the next MBI in this BFDUser's list of MBIs.
+
+      This method pops (that is, takes the topmost item) the next MBI
+      in this instance's list of MBIs and returns it. If no
+      more MBIs are available, this method will stop the test run.
+      """
+
       if len(self.mbis) == 0:
             errors.no_data_stop_test(self)
 
       return self.mbis.pop()
 
     def get_cursor_path(self) -> str:
+      """Returns the next cursor path in this BFDUser's list of URL paths.
+
+      This method pops (that is, takes the topmost item) the next contract cursor
+      URL path in this instance's list of contract paths and returns it. If no
+      more paths are available, this method will stop the test run.
+      """
+
       if len(self.cursor_list) == 0:
         errors.no_data_stop_test(self)
 
       return self.cursor_list.pop()
 
     def get(self, base_path: str, params: Dict[str, str] = {}, headers: Dict[str, str] = {}, name: str = ''):
+      """Sends a GET request to the endpoint at base_path with the various query string parameters and headers specified.
+
+      This method extends Locust's HttpUser::client.get() method to make creating the requests
+      nicer. Specifically, the query string parameters are specified as a separate dictionary
+      opposed to part of the path, the cert and verify arguments (which will never change)
+      are already set, and Cache-Control headers are automatically set to ensure caching is
+      disabled.
+      """
+
       self.client.get(create_url_path(base_path, params),
                       cert=client_cert,
                       verify=server_public_key,
