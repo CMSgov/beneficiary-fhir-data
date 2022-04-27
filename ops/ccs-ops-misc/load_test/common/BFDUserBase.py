a91aae96d1b7f2bb019f23465297ebf42ae967e1
Andrew Kramer <andrew.kramer@adhocteam.us>
2022-04-27 16:44:50 -0400

    Tests store and use next paginated URL

---
+++ ops/ccs-ops-misc/load_test/common/BFDUserBase.py
@@ -0,0 +1,142 @@
+import random
+from typing import Callable, Dict
+import urllib3
+import common.config as config
+import common.data as data
+import common.test_setup as setup
+import common.validation as validation
+from locust import HttpUser
+import json
+
+setup.set_locust_env(config.load())
+
+'''
+Base Class for Locust tests against BFD. This class should automatically handle
+most of the common tasks that our load tests require.
+'''
+class BFDUserBase(HttpUser):
+
+    '''
+    If a child class is going to use a set of data during the test, such as 
+    Beneficiary IDs, MBI numbers, etc. then it has to be defined in this class
+    field so that we can pre-load that data from the database.
+    '''
+    DATA_REQUIRED = [
+        # 'BENE_IDS',
+        # 'MBIS',
+        # 'CURSORS_V1',
+        # 'CURSORS_V2',
+    ]
+
+    '''
+    If a child class needs to set up a Failsafe event (highly recommended!) to
+    stop the test if SLAs degrade past a certain point, then these are the SLAs
+    to use for that. Also, these are the SLAs to compare against upon test
+    completion.
+    '''
+    SLA_BASELINE = None
+
+    '''
+    Run setup tasks prior to the tests being counted.
+    '''
+    def on_start(self):
+        # Load configuration needed for making requests to the FHIR server
+        self.client_cert = setup.getClientCert()
+        self.server_public_key = setup.loadServerPublicKey()
+        setup.disable_no_cert_warnings(self.server_public_key, urllib3)
+        self.last_updated = data.get_last_updated()
+
+        # Pre-load data needed for creating URLs
+        self.eob_ids = self.load_data('BENE_IDS', data.load_bene_ids)
+        self.mbis = self.load_data('MBIS', data.load_mbis)
+        self.cursor_list_v1 = self.load_data('CURSORS_V1', data.load_cursors,
+                "v1")
+        self.cursor_list_v2 = self.load_data('CURSORS_V2', data.load_cursors,
+                "v2")
+
+        # Create the pools for storing paginated URLs
+        self.url_pools = {}
+
+        # Adds a global failsafe check to ensure that if this test overwhelms 
+        # the database, we bail out and stop hitting the server
+        if hasattr(self, 'SLA_BASELINE'):
+            validation.setup_failsafe_event(self.environment, self.SLA_BASELINE)
+
+
+    '''
+    Run tear-down tasks after the tests have completed.
+    '''
+    def on_stop(self):
+        # Report the various response time percentiles against the SLA
+        if hasattr(self, 'SLA_BASELINE'):
+            validation.check_sla_validation(self.environment, self.SLA_BASELINE)
+
+
+    '''
+    Send one request to the FHIR server and parse the response for pagination
+    '''
+    def get_by_url(self, url: str, headers: Dict[str, str] = {}, name: str = ''):
+        with self.client.get(url,
+                cert=self.client_cert,
+                verify=self.server_public_key,
+                headers={**headers, 'Cache-Control': 'no-store, no-cache'},
+                name=name,
+                catch_response=True
+        ) as response:
+            # This is normally where you would validate a response, but we
+            # don't need to do that. Instead, we're checking it for valid
+            # "next" URLs that we can add to a URL pool.
+            next_url = self.get_next_url(response.text)
+            if next_url is not None:
+                if name not in self.url_pools:
+                    self.url_pools[name] = []
+                self.url_pools[name].append(next_url)
+    
+
+    '''
+    Figure out which URL we should query next and then query the server using 
+    get_by_url()
+    '''
+    def run_task(self, url_callback: Callable, headers: Dict[str, str] = {}, name: str = ''):
+        # First, see if we can generate a URL using the callback
+        try:
+            url = url_callback(self)
+        except IndexError:
+            url = None
+
+        if url is None and name in self.url_pools and self.url_pools[name]:
+            # We can't generate a URL from the callback, but there are still
+            # URLs in the pool of paginated data. Use that.
+            url = self.url_pools[name].pop()
+
+        if url is not None:
+            # Run the test using the URL we found
+            self.get_by_url(url=url, headers=headers, name=name)
+
+
+    # Helper Functions
+
+    '''
+    Check if the given DATA_REQUIRED flag exists on this class, and if it does,
+    pre-load the data from the database.
+    '''
+    def load_data(self, flag_name: str, load_function: Callable, *args):
+        if hasattr(self, 'DATA_REQUIRED') and flag_name in self.DATA_REQUIRED:
+            data_list = load_function(*args).copy()
+            random.shuffle(data_list)
+            return data_list
+        else:
+            return []
+
+    '''
+    Parse the JSON response and return the "next" URL if it exists
+    '''
+    def get_next_url(self, payload):
+        parsed_payload = json.loads(payload)
+        if "link" not in parsed_payload:
+            return None
+        links = parsed_payload["link"]
+        for link in links:
+            if "relation" in link and link["relation"] == "next":
+                return link["url"]
+        return None
--- ops/ccs-ops-misc/load_test/v2/baseline_tests.py
+++ ops/ccs-ops-misc/load_test/v2/baseline_tests.py
@@ -1,126 +1,111 @@
-import random
-from typing import Dict
-import urllib3
-import common.config as config
-import common.data as data
-import common.errors as errors
-import common.test_setup as setup
+from common.BFDUserBase import BFDUserBase
 from common.url_path import create_url_path
-import common.validation as validation
-from locust import HttpUser, task, events, tag
+from common.validation import SLA_V2_BASELINE
+from locust import task
 
-client_cert = setup.getClientCert()
-server_public_key = setup.loadServerPublicKey()
-setup.disable_no_cert_warnings(server_public_key, urllib3)
-setup.set_locust_env(config.load())
-
-mbis = data.load_mbis()
-eob_ids = data.load_bene_ids()
-last_updated = data.get_last_updated()
-cursor_list = data.load_cursors("v2")
-
-class BFDUser(HttpUser):
-    def on_start(self):
-        copied_eob_ids = eob_ids.copy()
-        random.shuffle(copied_eob_ids)
-
-        copied_mbis = mbis.copy()
-        random.shuffle(copied_mbis)
-
-        copied_cursor_list = cursor_list.copy()
-        random.shuffle(copied_cursor_list)
-
-        self.eob_ids = copied_eob_ids
-        self.mbis = copied_mbis
-        self.cursor_list = copied_cursor_list
-
-    def get_eob(self) -> int:
-      if len(self.eob_ids) == 0:
-            errors.no_data_stop_test(self)
-
-      return self.eob_ids.pop()
-
-    def get_mbi(self) -> int:
-      if len(self.mbis) == 0:
-            errors.no_data_stop_test(self)
-
-      return self.mbis.pop()
-
-    def get_cursor_path(self) -> str:
-      if len(self.cursor_list) == 0:
-        errors.no_data_stop_test(self)
-
-      return self.cursor_list.pop()
-
-    def get(self, base_path: str, params: Dict[str, str] = {}, headers: Dict[str, str] = {}, name: str = ''):
-      self.client.get(create_url_path(base_path, params),
-                      cert=client_cert,
-                      verify=server_public_key,
-                      headers={**headers, 'Cache-Control': 'no-store, no-cache'},
-                      name=name)
+class BFDUser(BFDUserBase):
+    DATA_REQUIRED = [ 'BENE_IDS', 'MBIS', 'CURSORS_V2' ]
+    SLA_BASELINE = SLA_V2_BASELINE
 
     @task
     def coverage_test_id_count(self):
-        self.get('/v2/fhir/Coverage', params={'beneficiary': self.get_eob(), '_count': '10'},
-                name='/v2/fhir/Coverage search by id / count=10')
+        def make_url(instance):
+            return create_url_path('/v2/fhir/Coverage', {
+                'beneficiary': instance.eob_ids.pop(),
+                '_count': '10'
+            })
+
+        self.run_task(name='/v2/fhir/Coverage search by id / count=10', url_callback=make_url)
 
     @task
     def coverage_test_id_lastUpdated(self):
-        self.get('/v2/fhir/Coverage', params={'_lastUpdated': f'gt{last_updated}', 'beneficiary': self.get_eob()},
-                name='/v2/fhir/Coverage search by id / lastUpdated (2 weeks)')
+        def make_url(instance):
+            return create_url_path('/v2/fhir/Coverage', {
+                '_lastUpdated': f'gt{instance.last_updated}',
+                'beneficiary': instance.eob_ids.pop(),
+            })
+
+        self.run_task(name='/v2/fhir/Coverage search by id / lastUpdated (2 weeks)', url_callback=make_url)
 
     @task
     def coverage_test_id(self):
-        self.get('/v2/fhir/Coverage', params={'beneficiary': self.get_eob()},
-                name='/v2/fhir/Coverage search by id')
+        def make_url(instance):
+            return create_url_path('/v2/fhir/Coverage', {
+                'beneficiary': instance.eob_ids.pop(),
+            })
+
+        self.run_task(name='/v2/fhir/Coverage search by id', url_callback=make_url)
 
     @task
     def eob_test_id_count(self):
-        self.get('/v2/fhir/ExplanationOfBenefit', params={'patient': self.get_eob(), '_count': '10', '_format': 'application/fhir+json'},
-                name='/v2/fhir/ExplanationOfBenefit search by id / count=10')
+        def make_url(instance):
+            return create_url_path('/v2/fhir/ExplanationOfBenefit', {
+                'patient': instance.eob_ids.pop(),
+                '_count': '10',
+                '_format': 'application/fhir+json'
+            })
+
+        self.run_task(name='/v2/fhir/ExplanationOfBenefit search by id / count=10', url_callback=make_url)
+
 
     @task
     def eob_test_id_includeTaxNumber(self):
-        self.get('/v2/fhir/ExplanationOfBenefit', params={'_lastUpdated': f'gt{last_updated}', 'patient': self.get_eob(), '_IncludeTaxNumbers': 'true', '_format': 'application/fhir+json'},
-                name='/v2/fhir/ExplanationOfBenefit search by id / lastUpdated / includeTaxNumbers = true')
+        def make_url(instance):
+            return create_url_path('/v2/fhir/ExplanationOfBenefit', {
+                '_lastUpdated': f'gt{instance.last_updated}',
+                'patient': instance.eob_ids.pop(),
+                '_IncludeTaxNumbers': 'true',
+                '_format': 'application/fhir+json'
+            })
+
+        self.run_task(name='/v2/fhir/ExplanationOfBenefit search by id / lastUpdated / includeTaxNumbers', url_callback=make_url)
 
     @task
     def eob_test_id(self):
-        self.get('/v2/fhir/ExplanationOfBenefit', params={'patient': self.get_eob(), '_format': 'application/fhir+json'},
-                name='/v2/fhir/ExplanationOfBenefit search by id')
+        def make_url(instance):
+            return create_url_path('/v2/fhir/ExplanationOfBenefit', {
+                'patient': instance.eob_ids.pop(),
+                '_format': 'application/fhir+json'
+            })
+
+        self.run_task(name='/v2/fhir/ExplanationOfBenefit search by id', url_callback=make_url)
 
     @task
     def patient_test_coverageContract(self):
-        self.get(self.get_cursor_path(), headers={"IncludeIdentifiers": "mbi"},
-                name='/v2/fhir/Patient search by coverage contract (all pages)')
+        def make_url(instance):
+            return instance.cursor_list_v2.pop()
+
+        self.run_task(name='/v2/fhir/Patient search by coverage contract (all pages)', headers={"IncludeIdentifiers": "mbi"},
+                url_callback=make_url)
 
     @task
     def patient_test_hashedMbi(self):
-        self.get('/v2/fhir/Patient', params={'identifier': f'https://bluebutton.cms.gov/resources/identifier/mbi-hash|{self.get_mbi()}', '_IncludeIdentifiers': 'mbi'},
-                name='/v2/fhir/Patient search by hashed mbi / _IncludeIdentifiers=mbi')
+        def make_url(instance):
+            return create_url_path('/v2/fhir/Patient', {
+                'identifier': f'https://bluebutton.cms.gov/resources/identifier/mbi-hash|{instance.mbis.pop()}',
+                '_IncludeIdentifiers': 'mbi'
+            })
+
+        self.run_task(name='/v2/fhir/Patient search by hashed mbi / _IncludeIdentifiers=mbi', url_callback=make_url)
 
     @task
     def patient_test_id_lastUpdated(self):
-        self.get('/v2/fhir/Patient', params={'_id': self.get_eob(), '_format': 'application/fhir+json', '_IncludeIdentifiers': 'mbi', '_lastUpdated': f'gt{last_updated}'},
-                name='/v2/fhir/Patient search by id / _IncludeIdentifiers=mbi / last updated (2 weeks)')
+        def make_url(instance):
+            return create_url_path('/v2/fhir/Patient', {
+                '_id': instance.eob_ids.pop(),
+                '_format': 'application/fhir+json',
+                '_IncludeIdentifiers': 'mbi',
+                '_lastUpdated': f'gt{instance.last_updated}'
+            })
+
+        self.run_task(name='/v2/fhir/Patient search by id / _IncludeIdentifiers=mbi / (2 weeks)', url_callback=make_url)
 
     @task
     def patient_test_id(self):
-        self.get('/v2/fhir/Patient', params={'_id': self.get_eob(), '_format': 'application/fhir+json'},
-                name='/v2/fhir/Patient search by id')
+        def make_url(instance):
+            return create_url_path('/v2/fhir/Patient', {
+                '_id': instance.eob_ids.pop(),
+                '_format': 'application/fhir+json'
+            })
 
-'''
-Adds a global failsafe check to ensure that if this test overwhelms the
-database, we bail out and stop hitting the server.
-'''
-@events.init.add_listener
-def on_locust_init(environment, **_kwargs):
-    validation.setup_failsafe_event(environment, validation.SLA_V2_BASELINE)
-
-'''
-Adds a listener that will run when the test ends which checks the various
-response time percentiles against the SLA for this endpoint.
-'''
-@events.test_stop.add_listener
-def on_locust_quit(environment, **_kwargs):
-    validation.check_sla_validation(environment, validation.SLA_V2_BASELINE)
+        self.run_task(name='/v2/fhir/Patient search by id', url_callback=make_url)
