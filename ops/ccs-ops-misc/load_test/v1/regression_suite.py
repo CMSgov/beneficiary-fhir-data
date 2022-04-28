8db3af26647535d302cc7c9ab759bd921c9a4316
Andrew Kramer <andrew.kramer@adhocteam.us>
2022-04-28 12:44:44 -0400

    V1 Regression Suite

--- ops/ccs-ops-misc/load_test/v1/regression_suite.py
+++ ops/ccs-ops-misc/load_test/v1/regression_suite.py
@@ -6,6 +6,7 @@ this test suite, all tests in this suite will be run in parallel, with
 equal weighting being applied to each.
 """
 
+from common.BFDUserBase import BFDUserBase
 import random
 from typing import Dict
 import urllib3
@@ -14,165 +15,138 @@ import common.data as data
 import common.errors as errors
 import common.test_setup as setup
 from common.url_path import create_url_path
-import common.validation as validation
-from locust import HttpUser, task, events, tag
+from common.validation import SLA_V1_BASELINE
+from locust import task
 
-client_cert = setup.getClientCert()
-server_public_key = setup.loadServerPublicKey()
-setup.disable_no_cert_warnings(server_public_key, urllib3)
-setup.set_locust_env(config.load())
+class BFDUser(BFDUserBase):
+    DATA_REQUIRED = [ 'BENE_IDS', 'MBIS', 'CONTRACT_IDS' ]
+    SLA_BASELINE = SLA_V1_BASELINE
 
-mbis = data.load_mbis()
-bene_ids = data.load_bene_ids()
-last_updated = data.get_last_updated()
-cursor_list = data.load_cursors("v1")
-
-class BFDUser(HttpUser):
-    def on_start(self):
-        """Run once when a BFDUser is initialized by Locust.
-        
-        This method copies the necessary test data (lists of 
-        MBIs, beneficiary IDs, and contract cursor URLs) as
-        members of this particular BFDUser instance. We then
-        shuffle these copied lists such that concurrent BFDUsers
-        are not querying the same data at the same time.
-        """
-
-        copied_bene_ids = bene_ids.copy()
-        random.shuffle(copied_bene_ids)
-
-        copied_mbis = mbis.copy()
-        random.shuffle(copied_mbis)
-
-        copied_cursor_list = cursor_list.copy()
-        random.shuffle(copied_cursor_list)
-
-        self.bene_ids = copied_bene_ids
-        self.mbis = copied_mbis
-        self.cursor_list = copied_cursor_list
-
-    def get_bene_id(self) -> int:
-      """Returns the next beneficiary ID in this BFDUser's list of IDs.
-
-      This method pops (that is, takes the topmost item) the next beneficiary
-      ID in this instance's list of beneficiary IDs and returns it. If no
-      more IDs are available, this method will stop the test run. 
-      """
-      
-      if len(self.bene_ids) == 0:
-            errors.no_data_stop_test(self)
-
-      return self.bene_ids.pop()
-
-    def get_mbi(self) -> int:
-      """Returns the next MBI in this BFDUser's list of MBIs.
-
-      This method pops (that is, takes the topmost item) the next MBI 
-      in this instance's list of MBIs and returns it. If no
-      more MBIs are available, this method will stop the test run. 
-      """
-      
-      if len(self.mbis) == 0:
-            errors.no_data_stop_test(self)
-
-      return self.mbis.pop()
-
-    def get_cursor_path(self) -> str:
-      """Returns the next cursor path in this BFDUser's list of URL paths.
-
-      This method pops (that is, takes the topmost item) the next contract cursor 
-      URL path in this instance's list of contract paths and returns it. If no
-      more paths are available, this method will stop the test run. 
-      """
-
-      if len(self.cursor_list) == 0:
-        errors.no_data_stop_test(self)
-
-      return self.cursor_list.pop()
-
-    def get(self, base_path: str, params: Dict[str, str] = {}, headers: Dict[str, str] = {}, name: str = ''):
-      """Sends a GET request to the endpoint at base_path with the various query string parameters and headers specified.
-
-      This method extends Locust's HttpUser::client.get() method to make creating the requests 
-      nicer. Specifically, the query string parameters are specified as a separate dictionary
-      opposed to part of the path, the cert and verify arguments (which will never change)
-      are already set, and Cache-Control headers are automatically set to ensure caching is
-      disabled.
-      """
-
-      self.client.get(create_url_path(base_path, params),
-                      cert=client_cert,
-                      verify=server_public_key,
-                      headers={**headers, 'Cache-Control': 'no-store, no-cache'},
-                      name=name)
 
     @task
     def coverage_test_id_count(self):
-        self.get(f'/v1/fhir/Coverage', params={'beneficiary': f'{self.get_bene_id()}', '_count':'10'},
-                name='/v1/fhir/Coverage search by id / count=10')
+        def make_url(instance):
+            return create_url_path('/v1/fhir/Coverage', {
+                'beneficiary': instance.bene_ids.pop(),
+                '_count': '10'
+            })
+
+        self.run_task(name='/v1/fhir/Coverage search by id / count=10', url_callback=make_url)
+
 
     @task
     def coverage_test_id_lastUpdated(self):
-        self.get(f'/v1/fhir/Coverage', params={'beneficiary': f'{self.get_bene_id()}', '_lastUpdated': f'gt{last_updated}'},
-                name='/v1/fhir/Coverage search by id / lastUpdated (2 weeks)')
-    
+        def make_url(instance):
+            return create_url_path('/v1/fhir/Coverage', {
+                '_lastUpdated': f'gt{instance.last_updated}',
+                'beneficiary': instance.bene_ids.pop(),
+            })
+
+        self.run_task(name='/v1/fhir/Coverage search by id / lastUpdated (2 weeks)', url_callback=make_url)
+
+   
     @task
     def eob_test_id(self):
-        self.get(f'/v1/fhir/ExplanationOfBenefit', params={'patient': f'{self.get_bene_id()}', '_format': 'json'},
-                name='/v1/fhir/ExplanationOfBenefit search by id')
+        def make_url(instance):
+            return create_url_path('/v1/fhir/ExplanationOfBenefit', {
+                'patient': instance.bene_ids.pop(),
+                '_format': 'application/fhir+json'
+            })
+
+        self.run_task(name='/v1/fhir/ExplanationOfBenefit search by id', url_callback=make_url)
+
 
     @task
     def eob_test_id_count_typePDE(self):
-        self.get(f'/v1/fhir/ExplanationOfBenefit', params={'patient': f'{self.get_bene_id()}', '_format': 'json', '_count': '50', '_types': 'PDE'},
-                name='/v1/fhir/ExplanationOfBenefit search by id / type = PDE / count = 50')
+        def make_url(instance):
+            return create_url_path('/v1/fhir/ExplanationOfBenefit', {
+                'patient': instance.bene_ids.pop(),
+                '_format': 'json',
+                '_count': '50',
+                '_types': 'PDE'
+            })
+
+        self.run_task(name='/v1/fhir/ExplanationOfBenefit search by id / type = PDE / count = 50', url_callback=make_url)
+
 
     @task
     def eob_test_id_lastUpdated_count(self):
-        self.get(f'/v1/fhir/ExplanationOfBenefit', params={'patient': f'{self.get_bene_id()}', '_format': 'json', '_count': '100', '_lastUpdated': f'gt{last_updated}',},
-                name='/v1/fhir/ExplanationOfBenefit search by id / lastUpdated / count = 100')
+        def make_url(instance):
+            return create_url_path('/v1/fhir/ExplanationOfBenefit', {
+                'patient': instance.bene_ids.pop(),
+                '_format': 'json',
+                '_count': '100',
+                '_lastUpdated': f'gt{instance.last_updated}'
+            })
 
+        self.run_task(name='/v1/fhir/ExplanationOfBenefit search by id / lastUpdated / count = 100', url_callback=make_url)
+
+    
     @task
     def eob_test_id_lastUpdated_includeTaxNumbers(self):
-        self.get(f'/v1/fhir/ExplanationOfBenefit', params={'patient': f'{self.get_bene_id()}', '_format': 'json', '_lastUpdated': f'gt{last_updated}', '_IncludeTaxNumbers': 'true'},
-                name='/v1/fhir/ExplanationOfBenefit search by id / lastUpdated / includeTaxNumbers = true')
+        def make_url(instance):
+            return create_url_path('/v1/fhir/ExplanationOfBenefit', {
+                'patient': instance.bene_ids.pop(),
+                '_format': 'json',
+                '_lastUpdated': f'gt{instance.last_updated}',
+                '_IncludeTaxNumbers': 'true'
+            })
+
+        self.run_task(name='/v1/fhir/ExplanationOfBenefit search by id / lastUpdated / includeTaxNumbers', url_callback=make_url)
+
 
     @task
     def eob_test_id_lastUpdated(self):
-        self.get(f'/v1/fhir/ExplanationOfBenefit', params={'patient': f'{self.get_bene_id()}', '_format': 'json', '_lastUpdated': f'gt{last_updated}'},
-                name='/v1/fhir/ExplanationOfBenefit search by id / lastUpdated')
+        def make_url(instance):
+            return create_url_path('/v1/fhir/ExplanationOfBenefit', {
+                'patient': instance.bene_ids.pop(),
+                '_format': 'json',
+                '_lastUpdated': f'gt{instance.last_updated}'
+            })
+
+        self.run_task(name='/v1/fhir/ExplanationOfBenefit search by id / lastUpdated', url_callback=make_url)
 
     @task
     def patient_test_id(self):
-        self.get(f'/v1/fhir/Patient/{self.get_bene_id()}',
-                name='/v1/fhir/Patient/id')
+        def make_url(instance):
+            return create_url_path(f'/v1/fhir/Patient/{instance.bene_ids.pop()}', {})
+
+        self.run_task(name='/v1/fhir/Patient/id', url_callback=make_url)
+    
 
     @task
     def patient_test_id_lastUpdated_includeMbi_includeAddress(self):
-        self.get(f'/v1/fhir/Patient', params={'_id': f'{self.get_bene_id()}', '_lastUpdated': f'gt{last_updated}', '_IncludeIdentifiers': 'mbi', '_IncludeTaxNumbers': 'true'},
-                name='/v1/fhir/Patient/id search by id / lastUpdated (2 weeks) / includeTaxNumbers = true / includeIdentifiers = mbi')
+        def make_url(instance):
+            return create_url_path('/v1/fhir/Patient', {
+                '_id': instance.bene_ids.pop(),
+                '_lastUpdated': f'gt{instance.last_updated}',
+                '_IncludeIdentifiers': 'mbi',
+                '_IncludeTaxNumbers': 'true'
+            })
+
+        self.run_task(name='/v1/fhir/Patient/id search by id / (2 weeks) / includeTaxNumbers / mbi', url_callback=make_url)
 
     @task
     def patient_test_coverageContract(self):
-        self.get(self.get_cursor_path(), headers={"IncludeIdentifiers": "mbi"},
-                name='/v1/fhir/Patient search by coverage contract (all pages)')
+        def make_url(instance):
+            contract_data = instance.contract_ids.pop()
+            return create_url_path('/v1/fhir/Patient', {
+                '_has:Coverage.extension': f'https://bluebutton.cms.gov/resources/variables/ptdcntrct01|{contract_data["id"]}',
+                '_has:Coverage.rfrncyr': f'https://bluebutton.cms.gov/resources/variables/rfrnc_yr|{contract_data["year"]}',
+                '_count': 25,
+                '_format': 'json'
+            })
+
+        self.run_task(name='/v1/fhir/Patient search by coverage contract (all pages)', headers={"IncludeIdentifiers": "mbi"},
+                url_callback=make_url)
+
 
     @task
     def patient_test_hashedMbi(self):
-        self.get(f'/v1/fhir/Patient', params={'identifier': f'https://bluebutton.cms.gov/resources/identifier/mbi-hash|{self.get_mbi()}', '_IncludeIdentifiers': 'mbi'},
-                name='/v1/fhir/Patient search by hashed mbi / includeIdentifiers = mbi')
+        def make_url(instance):
+            return create_url_path(f'/v1/fhir/Patient/', {
+                'identifier': f'https://bluebutton.cms.gov/resources/identifier/mbi-hash|{instance.mbis.pop()}',
+                '_IncludeIdentifiers': 'mbi'
+            })
 
-'''
-Adds a global failsafe check to ensure that if this test overwhelms the
-database, we bail out and stop hitting the server.
-'''
-@events.init.add_listener
-def on_locust_init(environment, **_kwargs):
-    validation.setup_failsafe_event(environment, validation.SLA_V1_BASELINE)
-
-'''
-Adds a listener that will run when the test ends which checks the various
-response time percentiles against the SLA for this endpoint.
-'''
-@events.test_stop.add_listener
-def on_locust_quit(environment, **_kwargs):
-    validation.check_sla_validation(environment, validation.SLA_V1_BASELINE)
+        self.run_task(name='/v1/fhir/Patient search by hashed mbi / includeIdentifiers = mbi', url_callback=make_url)
