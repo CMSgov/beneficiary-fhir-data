76c357458d69f70d266af175becd6465021d1e2e
Brian Burton <brian.burton@adhocteam.us>
2022-04-28 08:43:29 -0400

    PACA-520: recognize rda api idle disconnects (#1069)
    * Adds exception for connection resets and logs as info if they happen during idle time.
    
    * Adds unit tests for dropped connection handling.
    
    * Adds configuration setting for RDA API server min time to connection drop.
    
    * Code and comments cleanup.
    
    * Treats DEADLINE_EXCEEDED as dropped connection and adds unit tests.
    
    * Refactors dropped connection handler code.
    
    * Changes requested in PR review.

--- apps/bfd-pipeline/bfd-pipeline-rda-grpc/src/test/java/gov/cms/bfd/pipeline/rda/grpc/source/GrpcResponseStreamTest.java
+++ apps/bfd-pipeline/bfd-pipeline-rda-grpc/src/test/java/gov/cms/bfd/pipeline/rda/grpc/source/GrpcResponseStreamTest.java
@@ -1,10 +1,12 @@
 package gov.cms.bfd.pipeline.rda.grpc.source;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
 
+import gov.cms.bfd.pipeline.rda.grpc.source.GrpcResponseStream.DroppedConnectionException;
 import gov.cms.bfd.pipeline.rda.grpc.source.GrpcResponseStream.StreamInterruptedException;
 import io.grpc.ClientCall;
 import io.grpc.Status;
@@ -12,22 +14,25 @@ import io.grpc.StatusRuntimeException;
 import java.util.Iterator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+/** Unit tests for {@link GrpcResponseStream}. */
 public class GrpcResponseStreamTest {
-  private Iterator<Integer> iterator;
-  private ClientCall<Integer, Integer> clientCall;
+  @Mock private Iterator<Integer> iterator;
+  @Mock private ClientCall<Integer, Integer> clientCall;
   private GrpcResponseStream<Integer> stream;
 
   @BeforeEach
   public void setUp() {
-    iterator = mock(Iterator.class);
-    clientCall = mock(ClientCall.class);
+    MockitoAnnotations.openMocks(this);
     stream = new GrpcResponseStream<>(clientCall, iterator);
   }
 
+  /** Verify that {@link GrpcResponseStream#hasNext()} passes through non-interrupt exceptions. */
   @Test
-  public void hasNextPassesThroughNonInterrupts() {
-    StatusRuntimeException status = Status.DEADLINE_EXCEEDED.asRuntimeException();
+  public void testThatHasNextPassesThroughNonInterrupts() {
+    StatusRuntimeException status = Status.INVALID_ARGUMENT.asRuntimeException();
     doThrow(status).when(iterator).hasNext();
     try {
       stream.hasNext();
@@ -37,9 +42,10 @@ public class GrpcResponseStreamTest {
     }
   }
 
+  /** Verify that {@link GrpcResponseStream#next()} passes through non-interrupt exceptions. */
   @Test
-  public void nextPassesThroughNonInterrupts() {
-    StatusRuntimeException status = Status.DEADLINE_EXCEEDED.asRuntimeException();
+  public void testThatNextPassesThroughNonInterrupts() {
+    StatusRuntimeException status = Status.INTERNAL.asRuntimeException();
     doThrow(status).when(iterator).next();
     try {
       stream.next();
@@ -49,8 +55,9 @@ public class GrpcResponseStreamTest {
     }
   }
 
+  /** Verify that {@link GrpcResponseStream#hasNext()} wraps {@link InterruptedException}s. */
   @Test
-  public void hasNextWrapsInterrupts() {
+  public void testThatHasNextWrapsInterrupts() throws DroppedConnectionException {
     StatusRuntimeException status =
         Status.CANCELLED.withCause(new InterruptedException()).asRuntimeException();
     doThrow(status).when(iterator).hasNext();
@@ -62,8 +69,9 @@ public class GrpcResponseStreamTest {
     }
   }
 
+  /** Verify that {@link GrpcResponseStream#next()} wraps {@link InterruptedException}s. */
   @Test
-  public void nextWrapsInterrupts() {
+  public void testThatNextWrapsInterrupts() throws DroppedConnectionException {
     StatusRuntimeException status =
         Status.CANCELLED.withCause(new InterruptedException()).asRuntimeException();
     doThrow(status).when(iterator).next();
@@ -73,5 +81,59 @@ public class GrpcResponseStreamTest {
     } catch (StreamInterruptedException ex) {
       assertSame(status, ex.getCause());
     }
+  }
+
+  /**
+   * Verify that {@link GrpcResponseStream#hasNext()} wraps exceptions that indicate a supported
+   * type of dropped connection.
+   */
+  @Test
+  public void testThatHasNextWrapsDroppedConnections() throws StreamInterruptedException {
+    StatusRuntimeException status =
+        new StatusRuntimeException(
+            Status.INTERNAL.withDescription(GrpcResponseStream.STREAM_RESET_ERROR_MESSAGE));
+    doThrow(status).when(iterator).hasNext();
+    try {
+      stream.hasNext();
+      fail("exception should have been thrown");
+    } catch (DroppedConnectionException ex) {
+      assertSame(status, ex.getCause());
+    }
+  }
+
+  /**
+   * Verify that {@link GrpcResponseStream#hasNext()} wraps exceptions that indicate a supported
+   * type of dropped connection.
+   */
+  @Test
+  public void testThatNextWrapsDroppedConnections() throws StreamInterruptedException {
+    StatusRuntimeException status = new StatusRuntimeException(Status.DEADLINE_EXCEEDED);
+    doThrow(status).when(iterator).next();
+    try {
+      stream.next();
+      fail("exception should have been thrown");
+    } catch (DroppedConnectionException ex) {
+      assertSame(status, ex.getCause());
+    }
+  }
+
+  /**
+   * Verify that criteria for recognizing need to throw {@link
+   * gov.cms.bfd.pipeline.rda.grpc.source.GrpcResponseStream.DroppedConnectionException} works
+   * properly.
+   */
+  @Test
+  public void testThatDroppedConnectionCriteriaWorkCorrectly() {
+    var exception =
+        new StatusRuntimeException(Status.INTERNAL.withDescription("some other message"));
+    assertFalse(GrpcResponseStream.isStreamResetException(exception));
+
+    exception =
+        new StatusRuntimeException(
+            Status.INTERNAL.withDescription(GrpcResponseStream.STREAM_RESET_ERROR_MESSAGE));
+    assertTrue(GrpcResponseStream.isStreamResetException(exception));
+
+    exception = new StatusRuntimeException(Status.DEADLINE_EXCEEDED);
+    assertTrue(GrpcResponseStream.isStreamResetException(exception));
   }
 }
