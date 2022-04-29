90fad54f3f5b4920205c546e9a39a4c869002a40
Kyle Robbins <kyle.robbins@adhocteam.us>
2022-04-29 11:04:32 -0400

    [PACA-560] Add message error table entity

---
+++ apps/bfd-model/bfd-model-rda/src/main/java/gov/cms/bfd/model/rda/MessageError.java
@@ -0,0 +1,66 @@
+package gov.cms.bfd.model.rda;
+
+import java.time.Instant;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldNameConstants;
+
+/** JPA class for the MessageError table */
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@FieldNameConstants
+@Table(name = "message_error", schema = "rda")
+public class MessageError {
+  private static final String SequenceName = "rda_api_claim_message_errors_id_seq";
+
+  @Id
+  @Column(name = "id", nullable = false, updatable = false)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SequenceName)
+  @SequenceGenerator(
+      name = SequenceName,
+      schema = "rda",
+      sequenceName = SequenceName,
+      allocationSize = 25)
+  @EqualsAndHashCode.Include
+  private Long id;
+
+  /** The message sequence number. */
+  @Column(name = "sequence_number", nullable = false)
+  @EqualsAndHashCode.Include
+  private Long sequenceNumber;
+
+  /** Either dcn (FISS) or idrClmHdIcn (MCS). */
+  @Column(name = "claim_id", length = 25, nullable = false)
+  private String claimId;
+
+  /** Either F (FISS) or M (MCS). */
+  @Column(name = "claim_type", nullable = false)
+  @EqualsAndHashCode.Include
+  private Character claimType;
+
+  /** Timestamp when we inserted the claim from this message into our database. */
+  @Column(name = "received_date")
+  private Instant receivedDate;
+
+  /** The original message that was received, represented as a json string */
+  @Column(name = "message")
+  @Lob
+  private String message;
+}
---
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V66__rda_dead_letter_table.sql
@@ -0,0 +1,20 @@
+/*
+ * This migration adds a message_errors table for tracking messages that resulted in
+ * an error during the message ingestion process via the gRPC pipeline job.  This table will
+ * provide a means to store the instigating data so it can be reviewed and troubleshooted.
+ *
+ * Keeping messages that generated errors in a separate persistent table will prevent it
+ * from being lost due to subsequent claim updates that may clear the error, thus
+ * making it more challenging to resolve the original issue.
+ */
+
+CREATE TABLE rda.message_error (
+    id              bigint                      not null primary key,
+    sequence_number bigint                      not null,
+    claim_id        varchar(25)                 not null,
+    claim_type      char                        not null,
+    received_date   timestamp with time zone    not null,
+    message         text                        not null,
+);
+
+create sequence rda.rda_api_claim_message_error_id_seq as bigint ${logic.sequence-start} 1 ${logic.sequence-increment} 25 no cycle;
