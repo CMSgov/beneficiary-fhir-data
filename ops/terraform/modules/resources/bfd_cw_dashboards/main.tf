2069566787b5688cdf5df526c8ed4033eb0cd188
Mario Smith <mario.smith@adhocteam.us>
2022-05-09 09:56:23 -0400

    BFD-1731 | Fixed Dashboard Body Templatefile Path

--- ops/terraform/modules/resources/bfd_cw_dashboards/main.tf
+++ ops/terraform/modules/resources/bfd_cw_dashboards/main.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_dashboard" "bfd-dashboards" {
   dashboard_name = var.dashboard_name
-  dashboard_body = templatefile("../../../../../modules/resources/bfd_cw_dashboards/templates/bfd-server-logs.json", {
+  dashboard_body = templatefile("../../../../../modules/resources/bfd_cw_dashboards/templates/bfd-dashboards.json", {
     dashboard_namespace = var.dashboard_namespace
   })
 }
