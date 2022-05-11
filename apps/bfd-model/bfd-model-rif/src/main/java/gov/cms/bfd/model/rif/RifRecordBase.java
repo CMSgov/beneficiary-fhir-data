9257fb0fe2ec341899f0b6e3d02086762bd29be0
Logan Mitchell <logan.mitchell@adhocteam.us>
2022-05-11 15:02:12 -0700

    BFD-1712: Enable checkstyle for bfd-model-rif
    Brings bfd-model-rif into compliance with checkstyle and removes the checkstyle skip on this project.

--- apps/bfd-model/bfd-model-rif/src/main/java/gov/cms/bfd/model/rif/RifRecordBase.java
+++ apps/bfd-model/bfd-model-rif/src/main/java/gov/cms/bfd/model/rif/RifRecordBase.java
@@ -3,13 +3,28 @@ package gov.cms.bfd.model.rif;
 import java.time.Instant;
 import java.util.Optional;
 
-/** Common interface for RifRecords */
+/** Common interface for RifRecords. */
 public interface RifRecordBase {
+  /**
+   * Gets the last updated time.
+   *
+   * @return the last updated time
+   */
   Optional<Instant> getLastUpdated();
 
+  /**
+   * Sets the last updated time.
+   *
+   * @param lastUpdated the last updated time
+   */
   default void setLastUpdated(Instant lastUpdated) {
     setLastUpdated(lastUpdated != null ? Optional.of(lastUpdated) : Optional.empty());
   }
 
+  /**
+   * Sets the last updated time.
+   *
+   * @param lastUpdated the last updated time
+   */
   void setLastUpdated(Optional<Instant> lastUpdated);
 }
