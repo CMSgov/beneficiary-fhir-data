ed7783bfc65b80a680585efb0f1d66bb904c2429
Naoufal <naoufal.elhilali@adhocteam.us>
2022-06-02 11:32:33 -0600

    Importing bfd-pipeline dashboard to terraform

--- ops/terraform/modules/resources/bfd_pipeline/main.tf
+++ ops/terraform/modules/resources/bfd_pipeline/main.tf
@@ -261,3 +261,16 @@ module "ec2_instance" {
   # Ensure that the DB is accessible before the BFD Pipeline is launched.
   ec2_depends_on_1 = "aws_security_group_rule.allow_db_primary_access"
 }
+
+# BFD Pipeline CloudWatch Dashboard
+data "template_file" "dashboard-template" {
+  template = "${file("${path.module}/templates/bfd-dashboards.tpl")}"
+  vars = {
+    dashboard_namespace = "bfd-prod/bfd-pipeline"
+  }
+}
+
+resource "aws_cloudwatch_dashboard" "bfd-pipeline-dashboard" {
+  dashboard_name = "bfd-pipeline-${var.env_config.env}"
+  dashboard_body = data.template_file.dashboard-template.rendered
+}
