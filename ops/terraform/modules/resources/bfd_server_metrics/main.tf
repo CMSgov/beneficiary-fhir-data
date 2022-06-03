b5eb8eef41fadd70f51e31f72c2b41d4cd5fdad9
Chris Trotter <chris.trotter@adhocteam.us>
2022-06-03 11:51:33 -0400

    BFD-1822: Add X-Request-ID response header to access.log (#1150)
    * Add X-Request-ID response header to access.log
    
    * fix inaccurate comment and include link to format string docs
    
    * clarify response time unit history

--- ops/terraform/modules/resources/bfd_server_metrics/main.tf
+++ ops/terraform/modules/resources/bfd_server_metrics/main.tf
@@ -16,7 +16,7 @@ locals {
 resource "aws_cloudwatch_log_metric_filter" "http-requests-count" {
   for_each       = local.endpoints
   name           = "bfd-${var.env}/bfd-server/http-requests/count/${each.key}/${var.metric_config.partner_name}"
-  pattern        = "[remote_host_name, remote_logical_username, remote_authenticated_user = \"${var.metric_config.partner_regex}\", timestamp, request = \"${each.value}\", query_string, status_code, bytes, duration_milliseconds, original_query_id, original_query_counter, original_query_timestamp, developer, developer_name, application_id, application, user_id, user, beneficiary_id]"
+  pattern        = "[remote_host_name, remote_logical_username, remote_authenticated_user = \"${var.metric_config.partner_regex}\", timestamp, request = \"${each.value}\", query_string, status_code, bytes, duration_milliseconds, original_query_id, original_query_counter, original_query_timestamp, developer, developer_name, application_id, application, user_id, user, beneficiary_id, request_id]"
   log_group_name = local.log_groups.access
 
   metric_transformation {
@@ -30,7 +30,7 @@ resource "aws_cloudwatch_log_metric_filter" "http-requests-count" {
 # Count HTTP 500s per partner
 resource "aws_cloudwatch_log_metric_filter" "http-requests-count-500" {
   name           = "bfd-${var.env}/bfd-server/http-requests/count-500/${var.metric_config.partner_name}"
-  pattern        = "[remote_host_name, remote_logical_username, remote_authenticated_user = \"${var.metric_config.partner_regex}\", timestamp, request, query_string, status_code = 500, bytes, duration_milliseconds, original_query_id, original_query_counter, original_query_timestamp, developer, developer_name, application_id, application, user_id, user, beneficiary_id]"
+  pattern        = "[remote_host_name, remote_logical_username, remote_authenticated_user = \"${var.metric_config.partner_regex}\", timestamp, request, query_string, status_code = 500, bytes, duration_milliseconds, original_query_id, original_query_counter, original_query_timestamp, developer, developer_name, application_id, application, user_id, user, beneficiary_id, request_id]"
   log_group_name = local.log_groups.access
 
   metric_transformation {
@@ -44,7 +44,7 @@ resource "aws_cloudwatch_log_metric_filter" "http-requests-count-500" {
 # Count HTTP non-2XXs per partner
 resource "aws_cloudwatch_log_metric_filter" "http-requests-count-not-2xx" {
   name           = "bfd-${var.env}/bfd-server/http-requests/count-not-2xx/${var.metric_config.partner_name}"
-  pattern        = "[remote_host_name, remote_logical_username, remote_authenticated_user = \"${var.metric_config.partner_regex}\", timestamp, request, query_string, status_code != 2*, bytes, duration_milliseconds, original_query_id, original_query_counter, original_query_timestamp, developer, developer_name, application_id, application, user_id, user, beneficiary_id]"
+  pattern        = "[remote_host_name, remote_logical_username, remote_authenticated_user = \"${var.metric_config.partner_regex}\", timestamp, request, query_string, status_code != 2*, bytes, duration_milliseconds, original_query_id, original_query_counter, original_query_timestamp, developer, developer_name, application_id, application, user_id, user, beneficiary_id, request_id]"
   log_group_name = local.log_groups.access
 
   metric_transformation {
@@ -59,7 +59,7 @@ resource "aws_cloudwatch_log_metric_filter" "http-requests-count-not-2xx" {
 resource "aws_cloudwatch_log_metric_filter" "http-requests-latency" {
   for_each       = local.endpoints
   name           = "bfd-${var.env}/bfd-server/http-requests/latency/${each.key}/${var.metric_config.partner_name}"
-  pattern        = "[remote_host_name, remote_logical_username, remote_authenticated_user = \"${var.metric_config.partner_regex}\", timestamp, request = \"${each.value}\", query_string, status_code, bytes, duration_milliseconds, original_query_id, original_query_counter, original_query_timestamp, developer, developer_name, application_id, application, user_id, user, beneficiary_id]"
+  pattern        = "[remote_host_name, remote_logical_username, remote_authenticated_user = \"${var.metric_config.partner_regex}\", timestamp, request = \"${each.value}\", query_string, status_code, bytes, duration_milliseconds, original_query_id, original_query_counter, original_query_timestamp, developer, developer_name, application_id, application, user_id, user, beneficiary_id, request_id]"
   log_group_name = local.log_groups.access
 
   metric_transformation {
