1d84c13e6c118c9c07d9f180a4d08aec50f6ebf5
Kyle Robbins <83452106+kyrobbins@users.noreply.github.com>
2022-06-09 09:21:21 -0400

    [PACA-621] Update migration script

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V75__fiss_audit_trail_add_rda_position.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V75__fiss_audit_trail_add_rda_position.sql
@@ -1,1 +1,3 @@
-alter table rda.fiss_audit_trails add column rda_position smallint default 1 not null;
+alter table rda.fiss_audit_trails add column rda_position smallint;
+update rda.fiss_audit_trails set rda_position = (priority + 1);
+alter table rda.fiss_audit_trails alter column rda_position set not null;
