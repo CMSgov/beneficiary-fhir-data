a3d81dac687479ecd7d98b021b2ba98339140a61
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-07-01 14:03:43 -0400

    Correctly check if the Locust process is a worker/master/running in distributed mode

--- ops/ccs-ops-misc/load_test/common/locust_utils.py
+++ ops/ccs-ops-misc/load_test/common/locust_utils.py
@@ -1,8 +1,9 @@
-'''Set up Locust tests from the configuration.
-'''
+"""Set up Locust tests from the configuration.
+"""
 
 import os
 from locust.env import Environment
+from locust.runners import WorkerRunner, MasterRunner
 
 
 def is_locust_worker(env: Environment) -> bool:
@@ -15,7 +16,7 @@ def is_locust_worker(env: Environment) -> bool:
     Returns:
         bool: True if the Runner/thread is a worker, False otherwise
     """
-    return ('LOCUST_MODE_WORKER' in os.environ and os.environ['LOCUST_MODE_WORKER'] == "True") or env.parsed_options.worker
+    return isinstance(env.runner, WorkerRunner)
 
 
 def is_locust_master(env: Environment) -> bool:
@@ -28,7 +29,7 @@ def is_locust_master(env: Environment) -> bool:
     Returns:
         bool: True if the Runner/thread is the "master", False otherwise
     """
-    return ('LOCUST_MODE_MASTER' in os.environ and os.environ['LOCUST_MODE_MASTER'] == "True") or env.parsed_options.master
+    return isinstance(env.runner, MasterRunner)
 
 
 def is_distributed(env: Environment) -> bool:
@@ -40,4 +41,4 @@ def is_distributed(env: Environment) -> bool:
     Returns:
         bool: True if the Locust is running in distributed mode, False otherwise
     """
-    return 'LOCUST_MODE_MASTER' in os.environ or 'LOCUST_MODE_WORKER' in os.environ or env.parsed_options.master or env.parsed_options.worker
+    return is_locust_master(env) or is_locust_worker(env)
