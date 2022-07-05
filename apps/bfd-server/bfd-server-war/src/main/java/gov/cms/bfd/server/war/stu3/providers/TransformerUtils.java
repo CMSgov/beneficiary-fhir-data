b5aa318d4e526b7ad475817a8d9a62659f71fcb9
Chris MacKenzie <chris.mackenzie@adhocteam.us>
2022-07-05 15:13:57 -0400

    BFD-1876: New schema beneficiary related table migration and code (#1183)
    * code changes to match beneficiary* tables
    
    * fix QueryLogging to accout for _new Bene* tables
    
    * fixup FK constraints
    
    * minor update to data type
    
    * rename flyway scripts to account for other flyway scripts merged into master
    
    * fix typo
    
    * fix error for SkippedRif processing
    
    * fix couple of places where beneficiaryId is a String when invoking entity find
    
    * change param name beneficiaryId to beneIdStr to distinguish from beneficiaryId in Beneficiary entity
    
    * change param name beneficiaryId to beneIdStr to distinguish from beneficiaryId in Beneficiary entity
    
    * change BloomFilter to use Long data type which maps to our use of bene_id in db; that change required touching LoadedBatch and LoadedBatchBuilder, and assorted clients of that classes which then affected unit and IT tests
    
    * up flyway script numbers by 1 due to paca merge into master
    
    * fix from verify format
    
    * streamline iterator code
    
    * fix hanging chad in javadoc
    
    * rename flyway scripts version
    
    * javadoc changes per PR review
    
    Co-authored-by: Chris Brune <64486927+cbrunefearless@users.noreply.github.com>

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/stu3/providers/TransformerUtils.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/stu3/providers/TransformerUtils.java
@@ -3369,9 +3369,25 @@ public final class TransformerUtils {
     return bundle;
   }
 
+  /**
+   * Output list of benefificiary IDs to MDC logging
+   *
+   * @param beneIds the {@link Collection} of beneficiary IDs top log
+   */
   public static void logBeneIdToMdc(Collection<String> beneIds) {
     if (!beneIds.isEmpty()) {
       MDC.put("bene_id", String.join(", ", beneIds));
+    }
+  }
+
+  /**
+   * Output beneficiary ID to the MDC logging
+   *
+   * @param beneId the {@link Long} beneficiary ID to log
+   */
+  public static void logBeneIdToMdc(Long beneId) {
+    if (beneId != null) {
+      MDC.put("bene_id", String.valueOf(beneId));
     }
   }
 
