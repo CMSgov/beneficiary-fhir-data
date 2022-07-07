e3f301fd4e5a73a473ff04eb621635c0cd46a1a1
Kyle Robbins <83452106+kyrobbins@users.noreply.github.com>
2022-07-07 11:49:39 -0400

    [PACA-687] Remove unique constraint on mbi in rda.mbi_cache

---
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V85__drop_mbi_ubique_constraint.sql
@@ -0,0 +1,5 @@
+-- Due to hibernate logging corresponding data in constraint violations, we're removing the unique
+-- constraint on the mbi.  The hash is also unique, so it *should* provide duplicate row protection
+-- without leaking sensitive information to the logs
+
+drop index rda.mbi_cache_mbi_idx;
