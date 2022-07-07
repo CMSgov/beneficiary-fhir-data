241beab2879e7845d244eeee23cc514976bde923
mackec1 <chris.mackenzie@adhocteam.us>
2022-07-07 14:41:08 -0400

    changes to have BFD services use db views from entity classes; flyway script to create views from the _new tables

---
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V87__new_schema_create_views.sql
@@ -0,0 +1,105 @@
+-- carrier_claims and carrier_claim_lines
+create view carrier_claim_lines as
+  select * from carrier_claim_lines_new
+  ${logic.psql-only} with check option
+  ;
+create view carrier_claims as
+  select * from carrier_claims_new
+  ${logic.psql-only} with check option
+  ;
+
+  -- dme_claims and dme_claim_lines
+create view dme_claim_lines as
+  select * from dme_claim_lines_new
+  ${logic.psql-only} with check option
+  ;
+create view dme_claims as
+  select * from dme_claims_new
+  ${logic.psql-only} with check option
+  ;
+
+-- hha_claims and hha_claim_lines
+create view hha_claim_lines as
+  select * from hha_claim_lines_new
+  ${logic.psql-only} with check option
+  ;
+create view hha_claims as
+  select * from hha_claims_new
+  ${logic.psql-only} with check option
+  ;
+
+-- hospice_claims and hospice_claim_lines
+create view hospice_claim_lines as
+  select * from hospice_claim_lines_new
+  ${logic.psql-only} with check option
+  ;
+create view hospice_claims as
+  select * from hospice_claims_new
+  ${logic.psql-only} with check option
+  ;
+
+-- inpatient_claims and inpatient_claim_lines
+create view inpatient_claim_lines as
+  select * from inpatient_claim_lines_new
+  ${logic.psql-only} with check option
+  ;
+create view inpatient_claims as
+  select * from inpatient_claims_new
+  ${logic.psql-only} with check option
+  ;
+
+-- outpatient_claims and outpatient_claim_lines
+create view outpatient_claim_lines as
+  select * from outpatient_claim_lines_new
+  ${logic.psql-only} with check option
+  ;
+create view outpatient_claims as
+  select * from outpatient_claims_new
+  ${logic.psql-only} with check option
+  ;
+
+-- partd_events
+create view partd_events as
+  select * from partd_events_new
+  ${logic.psql-only} with check option
+  ;
+
+-- snf_claims and snf_claim_lines
+create view snf_claim_lines as
+  select * from snf_claim_lines_new
+  ${logic.psql-only} with check option
+  ;
+create view snf_claims as
+  select * from snf_claims_new
+  ${logic.psql-only} with check option
+  ;
+
+-- beneficiaries_history
+create view beneficiaries_history as
+  select * from beneficiaries_history_new
+  ${logic.psql-only} with check option
+  ;
+
+-- medicare_beneficiaryid_history
+create view medicare_beneficiaryid_history as
+  select * from medicare_beneficiaryid_history_new
+  ${logic.psql-only} with check option
+  ;
+
+-- beneficiary_monthly
+create view beneficiary_monthly as
+  select * from beneficiary_monthly_new
+  ${logic.psql-only} with check option
+  ;
+
+-- beneficiaries
+create view beneficiaries as
+  select * from beneficiaries_new
+  ${logic.psql-only} with check option
+  ;
+
+-- skipped_rif_records
+create view skipped_rif_records as
+  select * from skipped_rif_records_new
+  ${logic.psql-only} with check option
+  ;
