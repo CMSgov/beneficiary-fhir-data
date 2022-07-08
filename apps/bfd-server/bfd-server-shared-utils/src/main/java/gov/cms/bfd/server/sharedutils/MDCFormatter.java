19c99705a0edff372f763a7abc0d11e8ca1a519f
Andrew Kramer <andrew.kramer@adhocteam.us>
2022-07-08 17:14:51 -0400

    Minor fixes, documentation

--- apps/bfd-server/bfd-server-shared-utils/src/main/java/gov/cms/bfd/server/sharedutils/MDCFormatter.java
+++ apps/bfd-server/bfd-server-shared-utils/src/main/java/gov/cms/bfd/server/sharedutils/MDCFormatter.java
@@ -4,25 +4,24 @@ package gov.cms.bfd.server.sharedutils;
 public class MDCFormatter {
 
   /**
-   * Delimiter that we want to be used to separate the parts of MDC field names; we need to replace
-   * FROM_DELIMITER with this.
-   */
-  public static final String TO_DELIMITER = "_";
-
-  /**
-   * Delimiter that used to be used to separate the parts of MDC field names; we need to replace
+   * Delimiter that used to be used to separate the parts of MDC field names; we want to replace
    * this with TO_DELIMITER.
    */
   public static final String FROM_DELIMITER = ".";
 
   /**
+   * Delimiter that we want to be used to separate the parts of MDC field names; we want to replace
+   * FROM_DELIMITER with this.
+   */
+  public static final String TO_DELIMITER = "_";
+
+  /**
    * Format an identifier for an MDC key. Historically, we have used "." to delimit parts of the MDC
    * keys, such as "http_access.request.header". For AWS CloudWatch Metrics, though, the "."
-   * character is not supported, so we need to change these to "_" with minimal impact on the rest
-   * of the project.
+   * character is not supported, so we need to change these to "_".
    *
-   * @param key Fields to concatenate into a field identifier
-   * @return Text of the field identifier
+   * @param key Fields to concatenate into the final key that we'll put into MDC
+   * @return Text of the key that we'll put into MDC
    */
   public static String formatMdcKey(String key) {
     return key.replace(FROM_DELIMITER, TO_DELIMITER);
