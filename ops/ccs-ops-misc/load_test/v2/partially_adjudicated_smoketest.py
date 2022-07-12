afe85755a68aa9cd6f0838e9c0366cf42d8be129
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-07-12 15:43:12 -0400

    Inline test code and data loading from pac_tests.py into the Locustfiles where it was used; remove pac_tests.py

--- ops/ccs-ops-misc/load_test/v2/partially_adjudicated_smoketest.py
+++ ops/ccs-ops-misc/load_test/v2/partially_adjudicated_smoketest.py
@@ -1,9 +1,32 @@
-from locust import HttpUser, tag, task
+from typing import List
 
-from common.pac_tests import PACTestUser
+from locust import events, tag, task
+from locust.env import Environment
+
+from common import data, db
+from common.bfd_user_base import BFDUserBase
+from common.locust_utils import is_distributed, is_locust_master
+
+master_pac_mbis: List[str] = []
 
 
-class PACUser(PACTestUser):
+@events.init.add_listener
+def _(environment: Environment, **kwargs):
+    if is_distributed(environment) and is_locust_master(environment) or not environment.parsed_options:
+        # Don't bother loading data for the master runner, it doesn't run a test
+        return
+
+    # See https://docs.locust.io/en/stable/extending-locust.html#test-data-management
+    # for Locust's documentation on the test data management pattern used here
+    global master_pac_mbis
+    master_pac_mbis = data.load_from_parsed_opts(
+        environment.parsed_options,
+        db.get_pac_hashed_mbis,
+        use_table_sample=False,
+    )
+
+
+class PACSmokeUser(BFDUserBase):
 
     """
     Tests for Partially Adjudicated Claims endpoints to error check the transformers
@@ -12,18 +35,24 @@ class PACUser(PACTestUser):
     for every MBI.
     """
 
-    @classmethod
-    def _hashed_mbis(cls):
-        return super()._hashed_mbis().copy()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.hashed_mbis = master_pac_mbis.copy()
+
+    def __get(self, resource, name, parameters=None):
+        params = {} if parameters is None else parameters
+        params["mbi"] = self.hashed_mbis.pop()
+
+        self.run_task_by_parameters(base_path=f"/v2/fhir/{resource}", params=params, name=name)
 
     @tag("claim")
     @task
     def get_claim(self):
         """Get single Claim"""
-        self._get_claim()
+        self.__get("Claim", "claim")
 
     @tag("claim-response")
     @task
     def get_claim_response(self):
         """Get single ClaimResponse"""
-        self._get_claim_response()
+        self.__get("ClaimResponse", "claimResponse")
