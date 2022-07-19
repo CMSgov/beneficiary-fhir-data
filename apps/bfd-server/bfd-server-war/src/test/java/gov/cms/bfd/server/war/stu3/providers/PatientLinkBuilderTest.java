84fb81812029892e4028ba71b2c4b6a5308c8583
Jeremy Rosenfeld <jeremy.rosenfeld@adhocteam.us>
2022-07-19 14:24:42 -0700

    revert patient linkbuilder test

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/stu3/providers/PatientLinkBuilderTest.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/stu3/providers/PatientLinkBuilderTest.java
@@ -105,7 +105,7 @@ public class PatientLinkBuilderTest {
 
     Bundle bundle = new Bundle();
     TransformerUtils.addResourcesToBundle(
-        bundle, Collections.singletonList(new Patient().setId("567834")));
+        bundle, Collections.singletonList(new Patient().setId("id")));
     assertTrue(bundle.getLink().isEmpty());
     paging.addLinks(bundle);
     assertNotNull(bundle.getLink(Constants.LINK_SELF));
@@ -118,7 +118,7 @@ public class PatientLinkBuilderTest {
     bundle = new Bundle();
     TransformerUtils.addResourcesToBundle(
         bundle,
-        Collections.singletonList(new Coverage().setBeneficiary(new Reference("Patient/567834"))));
+        Collections.singletonList(new Coverage().setBeneficiary(new Reference("Patient/id"))));
     assertTrue(bundle.getLink().isEmpty());
     paging.addLinks(bundle);
     assertNotNull(bundle.getLink(Constants.LINK_SELF));
@@ -132,7 +132,7 @@ public class PatientLinkBuilderTest {
     TransformerUtils.addResourcesToBundle(
         bundle,
         Collections.singletonList(
-            new ExplanationOfBenefit().setPatient(new Reference("Patient/567834"))));
+            new ExplanationOfBenefit().setPatient(new Reference("Patient/id"))));
     assertTrue(bundle.getLink().isEmpty());
     paging.addLinks(bundle);
     assertNotNull(bundle.getLink(Constants.LINK_SELF));
