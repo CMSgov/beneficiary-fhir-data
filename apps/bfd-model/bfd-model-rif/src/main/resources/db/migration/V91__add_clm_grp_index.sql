f43f9b785e7449b5d93463e8f760696937f9edc0
Logan Mitchell <logan.mitchell@adhocteam.us>
2022-07-19 14:59:53 -0700

    add semicolons

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V91__add_clm_grp_index.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V91__add_clm_grp_index.sql
@@ -10,19 +10,19 @@
 -- =====================
 -- carrier_claims
 -- =====================
-${logic.psql-only} create index concurrently if not exists clm_grp_id_index on public.carrier_claims(clm_grp_id)
+${logic.psql-only} create index concurrently if not exists clm_grp_id_index on public.carrier_claims(clm_grp_id);
 
 -- =====================
 -- inpatient_claims
 -- =====================
-${logic.psql-only} create index concurrently if not exists clm_grp_id_index on public.inpatient_claims(clm_grp_id)
+${logic.psql-only} create index concurrently if not exists clm_grp_id_index on public.inpatient_claims(clm_grp_id);
 
 -- =====================
 -- outpatient_claims
 -- =====================
-${logic.psql-only} create index concurrently if not exists clm_grp_id_index on public.outpatient_claims(clm_grp_id)
+${logic.psql-only} create index concurrently if not exists clm_grp_id_index on public.outpatient_claims(clm_grp_id);
 
 -- =====================
 -- partd_events
 -- =====================
-${logic.psql-only} create index concurrently if not exists clm_grp_id_index on public.partd_events(clm_grp_id)
+${logic.psql-only} create index concurrently if not exists clm_grp_id_index on public.partd_events(clm_grp_id);
