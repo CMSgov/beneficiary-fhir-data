2427b40ecb5adf4d0ff6a62f672481751f71eda7
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-07-20 16:53:55 -0400

    Simplify build-push.sh by specifying multiple tags during build and passing the "--all-tags" flag during push

--- ops/ccs-ops-misc/load_test/lambda/bfd-locust-regression/build-push.sh
+++ ops/ccs-ops-misc/load_test/lambda/bfd-locust-regression/build-push.sh
@@ -6,10 +6,10 @@ readonly GIT_SHORT_HASH
 GIT_SHORT_HASH="$(git rev-parse --short HEAD)"
 readonly AWS_REGION
 AWS_REGION="us-east-1"
-readonly PRIVATE_ECR_URI
-PRIVATE_ECR_URI="$(aws ecr describe-registry --region $AWS_REGION | jq -r '.registryId').dkr.ecr.$AWS_REGION.amazonaws.com"
 readonly PRIVATE_REGISTRY_URI
-PRIVATE_REGISTRY_URI="$PRIVATE_ECR_URI/bfd-mgmt-locust-regression"
+PRIVATE_REGISTRY_URI="$(aws ecr describe-registry --region $AWS_REGION | jq -r '.registryId').dkr.ecr.$AWS_REGION.amazonaws.com"
+readonly IMAGE_NAME
+IMAGE_NAME="$PRIVATE_REGISTRY_URI/bfd-mgmt-locust-regression"
 
 # Overridable defaults
 DOCKER_TAG="${DOCKER_TAG_OVERRIDE:-"${GIT_SHORT_HASH}"}"
@@ -17,11 +17,9 @@ DOCKER_TAG_LATEST="${DOCKER_TAG_LATEST_OVERRIDE:-"latest"}"
 
 docker build . \
   --file "./lambda/bfd-locust-regression/Dockerfile" \
-  --tag "$PRIVATE_REGISTRY_URI:${DOCKER_TAG}" \
+  --tag "$IMAGE_NAME:${DOCKER_TAG}" \
+  --tag "$IMAGE_NAME:${DOCKER_TAG_LATEST}" \
   --platform "linux/amd64"
 
-docker tag "$PRIVATE_REGISTRY_URI:${DOCKER_TAG}" "$PRIVATE_REGISTRY_URI:${DOCKER_TAG_LATEST}"
-
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin "$PRIVATE_ECR_URI"
-docker push "$PRIVATE_REGISTRY_URI:${DOCKER_TAG}"
-docker push "$PRIVATE_REGISTRY_URI:${DOCKER_TAG_LATEST}"
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin "$PRIVATE_REGISTRY_URI"
+docker push "$IMAGE_NAME" --all-tags
