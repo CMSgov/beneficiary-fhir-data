4acfd363c45bb3aed1266f561396a1b6a4e5701e
Andrew Kramer <andrew.kramer@adhocteam.us>
2022-07-20 11:57:42 -0400

    Whitespace fix

--- insights/terraform/modules/table/main.tf
+++ insights/terraform/modules/table/main.tf
@@ -9,7 +9,7 @@ data "aws_kms_key" "bucket_cmk" {
 }
 
 locals {
-  account_id           = data.aws_caller_identity.current.account_id
+  account_id = data.aws_caller_identity.current.account_id
 
   table_parameters = {
     json = {
