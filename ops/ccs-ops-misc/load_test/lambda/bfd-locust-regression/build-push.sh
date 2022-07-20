646f063ef4fbc278aa82ace143bc8142ab4e5eb7
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-07-20 15:58:21 -0400

    Add a starting implementation for Jenkins pipeline to build and push bfd-locust-regression Docker image

---
+++ ops/ccs-ops-misc/load_test/lambda/bfd-locust-regression/build-push.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+# Constants
+readonly GIT_SHORT_HASH
+GIT_SHORT_HASH="$(git rev-parse --short HEAD)"
+readonly AWS_REGION
+AWS_REGION="us-east-1"
+readonly PRIVATE_ECR_URI
+PRIVATE_ECR_URI="$(aws ecr describe-registry --region $AWS_REGION | jq -r '.registryId').dkr.ecr.$AWS_REGION.amazonaws.com"
+readonly PRIVATE_REGISTRY_URI
+PRIVATE_REGISTRY_URI="$PRIVATE_ECR_URI/bfd-mgmt-locust-regression"
+
+# Overridable defaults
+DOCKER_TAG="${DOCKER_TAG_OVERRIDE:-"${GIT_SHORT_HASH}"}"
+DOCKER_TAG_LATEST="${DOCKER_TAG_LATEST_OVERRIDE:-"latest"}"
+
+docker build . \
+  --file "./lambda/bfd-locust-regression/Dockerfile" \
+  --tag "$PRIVATE_REGISTRY_URI:${DOCKER_TAG}" \
+  --platform "linux/amd64"
+
+docker tag "$PRIVATE_REGISTRY_URI:${DOCKER_TAG}" "$PRIVATE_REGISTRY_URI:${DOCKER_TAG_LATEST}"
+
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin "$PRIVATE_ECR_URI"
+docker push "$PRIVATE_REGISTRY_URI:${DOCKER_TAG}"
+docker push "$PRIVATE_REGISTRY_URI:${DOCKER_TAG_LATEST}"
