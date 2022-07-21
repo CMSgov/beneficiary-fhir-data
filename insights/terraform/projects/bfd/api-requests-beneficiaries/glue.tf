eaf18cc60ec480a76f0cef910a64dbe172a2d3bc
Andrew Kramer <andrew.kramer@adhocteam.us>
2022-07-21 10:38:53 -0400

    Standardize terraform resource names

--- insights/terraform/projects/bfd/api-requests-beneficiaries/glue.tf
+++ insights/terraform/projects/bfd/api-requests-beneficiaries/glue.tf
@@ -3,7 +3,7 @@
 # Beneficiaries, which captures when a beneficiary is queried
 
 # Glue Catalog Table to hold Beneficiaries
-module "beneficiaries-table" {
+module "glue-table-beneficiaries" {
   source         = "../../../modules/table"
   table          = "${local.full_name_underscore}_api_requests_beneficiaries"
   description    = "One row per beneficiary query, with the date of the request"
@@ -67,7 +67,7 @@ module "beneficiaries-table" {
 }
 
 # S3 Object for Glue Script
-resource "aws_s3_object" "bfd-populate-beneficiaries" {
+resource "aws_s3_object" "s3-script-populate-beneficiaries" {
   bucket             = data.aws_s3_bucket.bfd-insights-bucket.id
   bucket_key_enabled = false
   content_type       = "application/octet-stream; charset=UTF-8"
@@ -79,20 +79,20 @@ resource "aws_s3_object" "bfd-populate-beneficiaries" {
 }
 
 # Glue Job to populate the beneficiaries table
-resource "aws_glue_job" "bfd-populate-beneficiaries-job" {
+resource "aws_glue_job" "glue-job-populate-beneficiaries" {
   name                      = "${local.full_name}-populate-beneficiaries"
   description               = "Populate the Beneficiaries table"
   glue_version              = "3.0"
   max_retries               = 0
   non_overridable_arguments = {}
   number_of_workers         = 10
-  role_arn                  = data.aws_iam_role.glue-role.arn
+  role_arn                  = data.aws_iam_role.iam-role-glue.arn
   timeout                   = 2880
   worker_type               = "G.1X"
   connections               = []
 
   default_arguments = {
-    "--TempDir"                          = "s3://${aws_s3_object.bfd-populate-beneficiaries.bucket}/temporary/${local.environment}/"
+    "--TempDir"                          = "s3://${data.aws_s3_bucket.bfd-insights-bucket.id}/temporary/${local.environment}/"
     "--class"                            = "GlueApp"
     "--enable-continuous-cloudwatch-log" = "true"
     "--enable-glue-datacatalog"          = "true"
@@ -101,17 +101,17 @@ resource "aws_glue_job" "bfd-populate-beneficiaries-job" {
     "--enable-spark-ui"                  = "true"
     "--job-bookmark-option"              = "job-bookmark-enable"
     "--job-language"                     = "python"
+    "--spark-event-logs-path"            = "s3://${data.aws_s3_bucket.bfd-insights-bucket.id}/sparkHistoryLogs/${local.environment}/"
     "--sourceDatabase"                   = local.database
     "--sourceTable"                      = "${local.full_name_underscore}_api_requests"
-    "--spark-event-logs-path"            = "s3://${aws_s3_object.bfd-populate-beneficiaries.bucket}/sparkHistoryLogs/${local.environment}/"
     "--targetDatabase"                   = local.database
-    "--targetTable"                      = module.beneficiaries-table.name
+    "--targetTable"                      = module.glue-table-beneficiaries.name
   }
 
   command {
     name            = "glueetl"
     python_version  = "3"
-    script_location = "s3://${aws_s3_object.bfd-populate-beneficiaries.bucket}/${aws_s3_object.bfd-populate-beneficiaries.key}"
+    script_location = "s3://${aws_s3_object.s3-script-populate-beneficiaries.bucket}/${aws_s3_object.s3-script-populate-beneficiaries.key}"
   }
 
   execution_property {
@@ -120,7 +120,7 @@ resource "aws_glue_job" "bfd-populate-beneficiaries-job" {
 }
 
 # Crawler for the Beneficiaries table
-resource "aws_glue_crawler" "beneficiaries-crawler" {
+resource "aws_glue_crawler" "glue-crawler-beneficiaries" {
   classifiers   = []
   database_name = local.database
   configuration = jsonencode(
@@ -137,12 +137,12 @@ resource "aws_glue_crawler" "beneficiaries-crawler" {
     }
   )
   name     = "${local.full_name}-beneficiaries-crawler"
-  role     = data.aws_iam_role.glue-role.arn
+  role     = data.aws_iam_role.iam-role-glue.arn
 
   catalog_target {
     database_name = local.database
     tables = [
-      module.beneficiaries-table.name,
+      module.glue-table-beneficiaries.name,
     ]
   }
 
@@ -166,7 +166,7 @@ resource "aws_glue_crawler" "beneficiaries-crawler" {
 # Beneficiary Unique table to track the first time each beneficiary was queried.
 
 # Glue Table for unique beneficiaries
-module "beneficiaries-unique-table" {
+module "glue-table-beneficiaries-unique" {
   source         = "../../../modules/table"
   table          = "${local.full_name_underscore}_api_requests_beneficiaries_unique"
   description    = "One row per beneficiary and the date first seen"
@@ -205,7 +205,7 @@ module "beneficiaries-unique-table" {
 }
 
 # S3 Object for the Glue Script
-resource "aws_s3_object" "bfd-populate-beneficiary-unique" {
+resource "aws_s3_object" "s3-script-populate-beneficiary-unique" {
   bucket             = data.aws_s3_bucket.bfd-insights-bucket.id
   bucket_key_enabled = false
   content_type       = "application/octet-stream; charset=UTF-8"
@@ -216,10 +216,10 @@ resource "aws_s3_object" "bfd-populate-beneficiary-unique" {
 }
 
 # Glue Job to populate the beneficiary_unique table
-resource "aws_glue_job" "bfd-populate-beneficiary-unique-job" {
+resource "aws_glue_job" "glue-job-populate-beneficiary-unique" {
   connections = []
   default_arguments = {
-    "--TempDir"                          = "s3://${aws_s3_object.bfd-populate-beneficiary-unique.bucket}/temporary/${local.environment}/"
+    "--TempDir"                          = "s3://${data.aws_s3_bucket.bfd-insights-bucket.id}/temporary/${local.environment}/"
     "--class"                            = "GlueApp"
     "--enable-continuous-cloudwatch-log" = "true"
     "--enable-glue-datacatalog"          = "true"
@@ -229,25 +229,25 @@ resource "aws_glue_job" "bfd-populate-beneficiary-unique-job" {
     "--initialize"                       = "True"
     "--job-bookmark-option"              = "job-bookmark-disable" # We need to process all records
     "--job-language"                     = "python"
+    "--spark-event-logs-path"            = "s3://${data.aws_s3_bucket.bfd-insights-bucket.id}/sparkHistoryLogs/${local.environment}/"
     "--sourceDatabase"                   = local.database
-    "--sourceTable"                      = module.beneficiaries-table.name
-    "--spark-event-logs-path"            = "s3://${aws_s3_object.bfd-populate-beneficiary-unique.bucket}/sparkHistoryLogs/${local.environment}/"
+    "--sourceTable"                      = module.glue-table-beneficiaries.name
     "--targetDatabase"                   = local.database
-    "--targetTable"                      = module.beneficiaries-unique-table.name
+    "--targetTable"                      = module.glue-table-beneficiaries-unique.name
   }
   glue_version              = "3.0"
   max_retries               = 0
   name                      = "${local.full_name}-populate-beneficiary-unique"
   non_overridable_arguments = {}
   number_of_workers         = 10
-  role_arn                  = data.aws_iam_role.glue-role.arn
+  role_arn                  = data.aws_iam_role.iam-role-glue.arn
   timeout                   = 2880
   worker_type               = "G.1X"
 
   command {
     name            = "glueetl"
     python_version  = "3"
-    script_location = "s3://${aws_s3_object.bfd-populate-beneficiary-unique.bucket}/${aws_s3_object.bfd-populate-beneficiary-unique.key}"
+    script_location = "s3://${aws_s3_object.s3-script-populate-beneficiary-unique.bucket}/${aws_s3_object.s3-script-populate-beneficiary-unique.key}"
   }
 
   execution_property {
@@ -256,7 +256,7 @@ resource "aws_glue_job" "bfd-populate-beneficiary-unique-job" {
 }
 
 # Crawler for the Unique Beneficiaries table
-resource "aws_glue_crawler" "beneficiaries-unique-crawler" {
+resource "aws_glue_crawler" "glue-crawler-beneficiaries-unique" {
   classifiers   = []
   database_name = local.database
   configuration = jsonencode(
@@ -273,12 +273,12 @@ resource "aws_glue_crawler" "beneficiaries-unique-crawler" {
     }
   )
   name     = "${local.full_name}-beneficiaries-unique-crawler"
-  role     = data.aws_iam_role.glue-role.arn
+  role     = data.aws_iam_role.iam-role-glue.arn
 
   catalog_target {
     database_name = local.database
     tables = [
-      module.beneficiaries-unique-table.name
+      module.glue-table-beneficiaries-unique.name
     ]
   }
 
@@ -302,14 +302,14 @@ resource "aws_glue_crawler" "beneficiaries-unique-crawler" {
 # Organizes the Glue jobs / crawlers and runs them in sequence
 
 # Trigger for Populate Beneficiaries Job
-resource "aws_glue_trigger" "bfd-populate-beneficiaries-job-trigger" {
+resource "aws_glue_trigger" "glue-trigger-populate-beneficiaries-job" {
   name          = "${local.full_name}-populate-beneficiaries-job-trigger"
   description   = "Trigger to start the Populate Beneficiaries Glue Job whenever the Crawler completes successfully"
   workflow_name = local.glue_workflow_name
   type          = "CONDITIONAL"
 
   actions {
-    job_name = aws_glue_job.bfd-populate-beneficiaries-job.name
+    job_name = aws_glue_job.glue-job-populate-beneficiaries.name
   }
 
   predicate {
@@ -320,58 +320,58 @@ resource "aws_glue_trigger" "bfd-populate-beneficiaries-job-trigger" {
   }
 }
 
-# Trigger for Populate Beneficiaries Crawler
-resource "aws_glue_trigger" "bfd-beneficiaries-crawler-trigger" {
+# Trigger for API Requests Beneficiaries Crawler
+resource "aws_glue_trigger" "glue-trigger-beneficiaries-crawler" {
   name          = "${local.full_name}-beneficiaries-crawler-trigger"
   description   = "Trigger to start the Beneficiaries Crawler whenever the Populate Beneficiaries Job completes successfully"
   workflow_name = local.glue_workflow_name
   type          = "CONDITIONAL"
 
   actions {
-    crawler_name = aws_glue_crawler.beneficiaries-crawler.name
+    crawler_name = aws_glue_crawler.glue-crawler-beneficiaries.name
   }
 
   predicate {
     conditions {
-      job_name = aws_glue_job.bfd-populate-beneficiaries-job.name
+      job_name = aws_glue_job.glue-job-populate-beneficiaries.name
       state  = "SUCCEEDED"
     }
   }
 }
 
 # Trigger for Populate Beneficiaries Unique Job
-resource "aws_glue_trigger" "bfd-populate-beneficiaries-unique-job-trigger" {
+resource "aws_glue_trigger" "glue-trigger-populate-beneficiaries-unique-job" {
   name          = "${local.full_name}-populate-beneficiaries-unique-job-trigger"
   description   = "Trigger to start the Populate Beneficiaries Unique Job whenever the Beneficiaries Crawler completes successfully"
   workflow_name = local.glue_workflow_name
   type          = "CONDITIONAL"
 
   actions {
-    job_name = aws_glue_job.bfd-populate-beneficiary-unique-job.name
+    job_name = aws_glue_job.glue-job-populate-beneficiary-unique.name
   }
 
   predicate {
     conditions {
-      crawler_name = aws_glue_crawler.beneficiaries-crawler.name
+      crawler_name = aws_glue_crawler.glue-crawler-beneficiaries.name
       crawl_state  = "SUCCEEDED"
     }
   }
 }
 
 # Trigger for Populate Beneficiaries Unique Crawler
-resource "aws_glue_trigger" "bfd-beneficiaries-unique-crawler-trigger" {
+resource "aws_glue_trigger" "glue-trigger-beneficiaries-unique-crawler" {
   name          = "${local.full_name}-beneficiaries-unique-crawler-trigger"
   description   = "Trigger to start the Beneficiaries Unique Crawler whenever the Populate Beneficiaries Unique Job completes successfully"
   workflow_name = local.glue_workflow_name
   type          = "CONDITIONAL"
 
   actions {
-    crawler_name = aws_glue_crawler.beneficiaries-unique-crawler.name
+    crawler_name = aws_glue_crawler.glue-crawler-beneficiaries-unique.name
   }
 
   predicate {
     conditions {
-      job_name = aws_glue_job.bfd-populate-beneficiary-unique-job.name
+      job_name = aws_glue_job.glue-job-populate-beneficiary-unique.name
       state  = "SUCCEEDED"
     }
   }
