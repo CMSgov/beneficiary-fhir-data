99ce50075a2be70bd62dceb6c89a92b5cf0ed4ea
Michael J Burling <michael.burling@adhocteam.us>
2022-07-27 08:41:08 -0500

    Update and migrate state

--- ops/terraform/services/server/server-regression/backend.tf
+++ ops/terraform/services/server/server-regression/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "bfd-tf-state"
-    key            = "services/server/terraform.tfstate"
+    key            = "services/server-regression/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "bfd-tf-table"
     encrypt        = "1"
