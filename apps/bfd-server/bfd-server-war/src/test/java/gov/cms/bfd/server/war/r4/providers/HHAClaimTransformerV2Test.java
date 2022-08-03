40a84d6de4bbd911d7c66e342cd1fe49615a0712
Jeremy Rosenfeld <jeremy.rosenfeld@adhocteam.us>
2022-08-03 08:44:08 -0700

    BFD-1917 Map FI Claim Process Date in V2 (#1259)
    * map ficlmProcdt in v2
    
    * map ficclmprocdt in v1
    
    * update api changelog
    
    * remove v1 mapping
    
    * revert import structure for test files
    
    * update changelog md
    
    * update transformerutil javadoc
    
    * check for null localdate
    
    * update api changelog
    
    * refactor null check for localdate in transformer utils

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/HHAClaimTransformerV2Test.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/HHAClaimTransformerV2Test.java
@@ -975,6 +975,27 @@ public class HHAClaimTransformerV2Test {
   }
 
   /**
+   * Ensures the Fi_Clm_Proc_Dt is correctly mapped to an eob as an extension when the
+   * fiscalIntermediaryClaimProcessDate is present.
+   */
+  @Test
+  public void shouldHaveFiClaimProcessDateExtension() {
+    assertNotNull(eob.getExtension());
+    assertFalse(eob.getExtension().isEmpty());
+
+    Extension ex =
+        TransformerTestUtilsV2.findExtensionByUrl(
+            "https://bluebutton.cms.gov/resources/variables/fi_clm_proc_dt", eob.getExtension());
+
+    Extension compare =
+        new Extension(
+            "https://bluebutton.cms.gov/resources/variables/fi_clm_proc_dt",
+            new DateType("2015-10-30"));
+
+    assertTrue(compare.equalsDeep(ex));
+  }
+
+  /**
    * Serializes the EOB and prints to the command line
    *
    * @throws FHIRException
