fb3ccf51ffbb6d7e657cef3615cba88894dc453a
Morgan Harvey <morgan@adhocteam.us>
2022-08-02 17:01:08 -0700

    Repurpose terraform from server-regression

---
+++ ops/terraform/services/server/server-load/data-sources.tf
@@ -0,0 +1,69 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_ecr_authorization_token" "token" {}
+
+data "aws_kms_key" "cmk" {
+  key_id = "alias/bfd-${local.env}-cmk" # TODO: replace ssm lookup
+}
+
+data "aws_vpc" "main" {
+  filter {
+    name   = "tag:Name"
+    values = [local.vpc_name]
+  }
+}
+
+data "aws_subnets" "main" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+  filter {
+    name   = "tag:Layer"
+    values = [local.layer]
+  }
+}
+
+data "aws_ecr_repository" "ecr" {
+  name = "bfd-mgmt-${local.service}"
+}
+
+data "aws_ecr_image" "image_node" {
+  repository_name = data.aws_ecr_repository.ecr.name
+  image_tag       = local.docker_image_tag_node
+}
+
+data "aws_ecr_image" "image_controller" {
+  repository_name = data.aws_ecr_repository.ecr.name
+  image_tag       = local.docker_image_tag_controller
+}
+
+data "aws_ecr_image" "image_broker" {
+  repository_name = data.aws_ecr_repository.ecr.name
+  image_tag       = local.docker_image_tag_broker
+}
+
+data "aws_ssm_parameter" "docker_image_tag_node" {
+  # TODO: consider making this more environment-specific, versioning RFC in BFD-1743 may inform us of how
+  name = "/bfd/mgmt/server/nonsensitive/server_load_node_latest_image_tag"
+}
+
+data "aws_ssm_parameter" "docker_image_tag_controller" {
+  # TODO: consider making this more environment-specific, versioning RFC in BFD-1743 may inform us of how
+  name = "/bfd/mgmt/server/nonsensitive/server_load_controller_latest_image_tag"
+}
+
+data "aws_ssm_parameter" "docker_image_tag_broker" {
+  # TODO: consider making this more environment-specific, versioning RFC in BFD-1743 may inform us of how
+  name = "/bfd/mgmt/server/nonsensitive/server_load_broker_latest_image_tag"
+}
+
+data "aws_security_group" "rds" {
+  vpc_id = data.aws_vpc.main.id
+  filter {
+    name   = "tag:Name"
+    values = ["bfd-${local.env}-aurora-cluster"] # TODO think harder about this... RE: ssm, ephemeral environments, etc.
+  }
+}
---
+++ ops/terraform/services/server/server-load/main.tf
@@ -0,0 +1,119 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+  shared_tags = {
+    Environment = local.env
+    Layer       = local.layer
+    Name        = "bfd-${local.env}-${local.service}"
+    application = "bfd"
+    business    = "oeda"
+    role        = local.service
+    stack       = local.env
+  }
+
+  account_id = data.aws_caller_identity.current.account_id
+  env        = terraform.workspace
+  layer      = "app"
+  service    = "server-load"
+
+  vpc_name   = "bfd-${local.env}-vpc"
+  queue_name = "bfd-${local.env}-${local.service}"
+
+  docker_image_tag_node = coalesce(var.docker_image_tag_override, nonsensitive(data.aws_ssm_parameter.docker_image_tag_node.value))
+  docker_image_uri_node = "${data.aws_ecr_repository.ecr.repository_url}:${local.docker_image_tag_node}"
+
+  docker_image_tag_controller = coalesce(var.docker_image_tag_override, nonsensitive(data.aws_ssm_parameter.docker_image_tag_controller.value))
+  docker_image_uri_controller = "${data.aws_ecr_repository.ecr.repository_url}:${local.docker_image_tag_controller}"
+
+  docker_image_tag_broker = coalesce(var.docker_image_tag_override, nonsensitive(data.aws_ssm_parameter.docker_image_tag_broker.value))
+  docker_image_uri_broker = "${data.aws_ecr_repository.ecr.repository_url}:${local.docker_image_tag_broker}"
+
+  lambda_timeout_seconds = 600
+  kms_key_arn            = data.aws_kms_key.cmk.arn
+  kms_key_id             = data.aws_kms_key.cmk.key_id
+}
+
+resource "aws_lambda_function" "controller" {
+  function_name = "bfd-${local.env}-${local.service}-controller"
+  description   = "Lambda to run the Locust controller for load testing on the ${local.env} server"
+  tags          = local.shared_tags
+  kms_key_arn   = local.kms_key_arn
+
+  image_uri    = local.docker_image_uri_controller
+  package_type = "Image"
+
+  memory_size = 2048
+  timeout     = local.lambda_timeout_seconds
+  environment {
+    variables = {
+      BFD_ENVIRONMENT = local.env
+    }
+  }
+
+  role = aws_iam_role.this.arn
+  vpc_config {
+    security_group_ids = [aws_security_group.this.id]
+    subnet_ids         = data.aws_subnets.main.ids
+  }
+}
+
+resource "aws_lambda_function" "node" {
+  function_name = "bfd-${local.env}-${local.service}-node"
+  description   = "Lambda to run the Locust worker node for load testing on the ${local.env} server"
+  tags          = local.shared_tags
+  kms_key_arn   = local.kms_key_arn
+
+  image_uri    = local.docker_image_uri_node
+  package_type = "Image"
+
+  memory_size = 2048
+  timeout     = local.lambda_timeout_seconds
+  environment {
+    variables = {
+      BFD_ENVIRONMENT = local.env
+    }
+  }
+
+  role = aws_iam_role.this.arn
+  vpc_config {
+    security_group_ids = [aws_security_group.this.id]
+    subnet_ids         = data.aws_subnets.main.ids
+  }
+}
+
+resource "aws_lambda_function" "broker" {
+  function_name = "bfd-${local.env}-${local.service}-node"
+  description   = "Lambda to run the broker for load testing on the ${local.eng} server"
+  tags          = local.shared_tags
+  kms_key_arn   = local.kms_key_arn
+
+  image_uri    = local.docker_image_uri_broker
+  package_type = "Image"
+
+  memory_size = 2048
+  timeout     = local.lambda_timeout_seconds
+  environment {
+    variables = {
+      BFD_ENVIRONMENT = local.env
+    }
+  }
+
+  role = aws_iam_role.this.arn
+  vpc_config {
+    security_group_ids = [aws_security_group.this.id]
+    subnet_ids         = data.aws_subnets.main.ids
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "broker" {
+  event_source_arn = aws_sqs_queue.broker.arn
+  function_name    = aws_lambda_function.broker.arn
+}
+
+resource "aws_sqs_queue" "broker" {
+  name                       = local.queue_name
+  visibility_timeout_seconds = local.lambda_timeout_seconds
+  kms_master_key_id          = local.kms_key_id
+}
