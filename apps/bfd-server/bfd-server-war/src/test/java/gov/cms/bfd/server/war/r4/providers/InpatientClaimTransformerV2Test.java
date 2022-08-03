3dc4ad60d02c3f3c395a23ae944e13a8143ce5b9
Jeremy Rosenfeld <jeremy.rosenfeld@adhocteam.us>
2022-08-03 10:27:36 -0700

    update test with correct extension size

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/InpatientClaimTransformerV2Test.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/InpatientClaimTransformerV2Test.java
@@ -551,7 +551,7 @@ public final class InpatientClaimTransformerV2Test {
   /** Top level Extensions */
   @Test
   public void shouldHaveKnownExtensions() {
-    assertEquals(8, eob.getExtension().size());
+    assertEquals(9, eob.getExtension().size());
   }
 
   @Test
