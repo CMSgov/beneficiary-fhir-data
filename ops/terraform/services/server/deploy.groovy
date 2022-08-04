fa4c8ffaf9b5166940acd0736ca0ab5e2d47bf33
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-08-04 14:09:30 -0400

    Add SQS pipeline signal queue monitoring to deploy.groovy when running server-regression lambda

--- ops/terraform/services/server/deploy.groovy
+++ ops/terraform/services/server/deploy.groovy
@@ -50,19 +50,28 @@ terraform apply \
     }
 }
 
-/* Runs envionment-specific regression test suite via SQS signal.
+/* Runs envionment-specific regression test suite via SQS signal and returns if the regression suite was successful
  * @param args a {@link Map} must include `bfdEnv`
  * <ul>
+ * <li>awsRegion string the targeted AWS AZ region
  * <li>bfdEnv string represents the targeted BFD SDLC Environment
  * <li>gitBranchName string the name of the current git branch being ran
+ * <li>heartbeatInterval int the interval of time, in seconds, to heartbeat the signal SQS queue
  * </ul>
 */
-def runServerRegression(Map args = [:]) {
+def runServerRegression(Map args = [:], authFunction) {
+    awsRegion = args.awsRegion ?: 'us-east-1'
     bfdEnv = args.bfdEnv
     gitBranchName = args.gitBranchName
+    heartbeatInterval = args.heartbeatInterval ?: 15
 
-    locustSqsQueueName = "bfd-${bfdEnv}-server-regression"
-    locustSqsQueueUrl = sqs.getQueueUrl(locustSqsQueueName)
+    // This queue is used to trigger the lambda
+    lambdaSqsQueueName = "bfd-${bfdEnv}-server-regression"
+    lamdaSqsQueueUrl = sqs.getQueueUrl(lambdaSqsQueueName)
+
+    // This queue is posted to by the lambda to signal to the pipeline
+    // the result of the test run
+    signalSqsQueueName = "bfd-${bfdEnv}-server-regression-signal"
 
     currentBuildId = currentBuild.id
     lastSuccessfulBuildID = getLastSuccessfulBuildNum()
@@ -72,6 +81,13 @@ def runServerRegression(Map args = [:]) {
     sanitizedBranchName = gitBranchName.replaceAll(/[^0-9a-zA-Z_]/, '_')
                                        .replaceAll(/[\_]+/, '_')
                                        .toLowerCase()
+
+    if (canServerRegressionRunProceed(awsRegion, signalSqsQueueName, bfdEnv)) {
+        println "Proceeding to run bfd-${bfdEnv}-server-regression lambda..."
+    } else {
+        println "Halting bfd-${bfdEnv}-server-regression lambda run. Check the SQS Queue ${signalSqsQueueName}"
+        return false
+    }
 
     sqsMessage = writeJSON(returnText: true, json: [
         'host': "https://${bfdEnv}.bfd.cms.gov",
@@ -83,10 +99,90 @@ def runServerRegression(Map args = [:]) {
         'store_tag': "build${currentBuildId}__${sanitizedBranchName}"
     ])
     sqs.sendMessage(
-        sqsQueueUrl: locustSqsQueueUrl,
+        sqsQueueUrl: lamdaSqsQueueUrl,
         sqsMessage: sqsMessage
     )
 
+    hasRegressionSucceeded = monitorServerRegression(
+        sqsQueueName: signalSqsQueueName, 
+        heartbeatInterval: heartbeatInterval,
+        authFunction
+    )
+
+    sqs.purgeQueue(sqsQueueName)
+    return hasRegressionSucceeded
+}
+
+/* Monitors the server-regression-signal SQS queue for when the server-regression lambda finishes */
+boolean monitorServerRegression(Map args = [:], authFunction) {
+    sqsQueueName = args.sqsQueueName
+    awsRegion = args.awsRegion ?: 'us-east-1'
+    heartbeatInterval = args.heartbeatInterval
+
+    sqsQueueUrl = sqs.getQueueUrl(sqsQueueName)
+    while (true) {
+        authFunction()
+        messages = sqs.receiveMessages(
+            sqsQueueUrl: sqsQueueUrl,
+            awsRegion: awsRegion,
+            visibilityTimeoutSeconds: 30,
+            waitTimeSeconds: 20,
+            maxMessages: 1
+        )
+
+        // The server-regression lambda will post only one message once finished,
+        // so we just need to ensure that the number of messages returned is greater
+        // than 0 and then capture the first message
+        if (!messages.isEmpty()) {
+            msg = messages[0]
+
+            printServerRegressionMessage(msg)
+            sqs.deleteMessage(msg.receipt, sqsQueueUrl)
+
+            testRunResult = msg.body.result
+            return testRunResult == 'SUCCESS' ? true : false
+        } else {
+            println "No messages posted to ${sqsQueueName} yet, waiting ${heartbeatInterval} seconds to check again..."
+        }
+
+        sleep(heartbeatInterval)
+    }
+}
+
+// checks for indications of a running server-regression lambda by looking for unconsumed SQS messages
+boolean canServerRegressionRunProceed(String awsRegion, String sqsQueueName, String bfdEnv) {
+    println "Checking server-regression ${sqsQueueName} state..."
+
+    if (sqs.queueExists(sqsQueueName)) {
+        sqsQueueUrl = sqs.getQueueUrl(sqsQueueName)
+        println "Queue ${sqsQueueName} exists. Checking for messages in ${sqsQueueUrl}..."
+        serverRegressionSignalMsgs = sqs.receiveMessages(
+                sqsQueueUrl: sqsQueueUrl,
+                awsRegion: awsRegion,
+                maxMessages: 10,
+                visibilityTimeoutSeconds: 0,
+                waitTimeSeconds: 0)
+        if (serverRegressionSignalMsgs?.isEmpty()) {
+            println "Queue ${sqsQueueName} is empty. bfd-${bfdEnv}-server-regression run can proceed"
+            return true
+        } else {
+            println "Queue ${sqsQueueName} has messages. Is there an ongoing run of the server-regression lambda for the ${bfdEnv} environment?"
+            return false
+        }
+    } else {
+        println "Queue ${sqsQueueName} can not be found. Was the ${sqsQueueName} destroyed?"
+        return false
+    }
+}
+
+// print formatted migrator messages
+def printServerRegressionMessage(Map message) {
+    body = message.body
+    println "Timestamp: ${java.time.LocalDateTime.now().toString()}"
+    println """${body.function_name} finished running with a ${body.result} result, more information:
+                |   Lambda Request ID: ${body.request_id}
+                |   CloudWatch log stream name: ${body.log_stream_name}
+                |   CloudWatch log group name: ${body.log_group_name}""".stripMargin()
 }
 
 /* Gets the build ID of the last successful Jenkins job build of the current branch */
