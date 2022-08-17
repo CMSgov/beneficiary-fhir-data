44454ec7fa3e061c1a82a6c2fe25edf229aa928e
kcirtapfromspace <105461352+kcirtapfromspace@users.noreply.github.com>
2022-08-17 15:15:04 -0600

    Move to using mvn to resolve dependencies

--- ops/ansible/roles/bfd-pipeline/test/test.sh
+++ ops/ansible/roles/bfd-pipeline/test/test.sh
@@ -21,10 +21,3 @@ ansible-playbook "$TEST_PLAY" --inventory=inventory.docker.yaml --syntax-check
 
 # Run the Ansible test case.
 ansible-playbook "$TEST_PLAY" --inventory=inventory.docker.yaml
-
-# Run the role/playbook again, checking to make sure it's idempotent.
-if ansible-playbook "$TEST_PLAY" --inventory=inventory.docker.yaml | tee /dev/stderr | grep -q "${CONTAINER_NAME}.*changed=0.*failed=0"; then
-  echo 'Idempotence test: pass' && exit 0
-else
-  echo 'Idempotence test: fail' && exit 1
-fi
