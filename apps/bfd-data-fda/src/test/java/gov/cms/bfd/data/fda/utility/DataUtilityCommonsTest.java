fb112413f8a6a5da9aa032c02238ec08624ad33a
Chris Brune <cbrune@fearless.tech>
2022-08-25 13:45:40 -0400

    merge conflicts

--- apps/bfd-data-fda/src/test/java/gov/cms/bfd/data/fda/utility/DataUtilityCommonsTest.java
+++ apps/bfd-data-fda/src/test/java/gov/cms/bfd/data/fda/utility/DataUtilityCommonsTest.java
@@ -19,6 +19,7 @@ public final class DataUtilityCommonsTest {
   public void getFDADrugCodesThrowsExceptionWhenFileIsNotADirectory() {
     String outputDir = "../temp/";
     Path tempDir = Paths.get(outputDir);
+    
     try (MockedStatic<Paths> paths = Mockito.mockStatic(Paths.class)) {
       try (MockedStatic<Path> path = Mockito.mockStatic(Path.class)) {
         try (MockedStatic<Files> files = Mockito.mockStatic(Files.class)) {
