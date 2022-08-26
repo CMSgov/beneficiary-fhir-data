5727afe007db674752c246017c260bef6bc93372
Morgan Harvey <morgan@adhocteam.us>
2022-08-26 14:15:59 -0700

    Add self to egress rule for lambda sg

--- ops/terraform/services/server/server-load/security-groups.tf
+++ ops/terraform/services/server/server-load/security-groups.tf
@@ -5,15 +5,17 @@ resource "aws_security_group" "lambda" {
   vpc_id      = data.aws_vpc.main.id
 
   ingress {
-    protocol  = "-1"
-    self      = true # The security group itself will be added as a source to this ingress rule.
-    from_port = 0
-    to_port   = 0
+    protocol    = "-1"
+    self        = true # The security group itself will be added as a source to this ingress rule.
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     protocol    = "-1"
+    self        = true # The security group itself will be added as a source to this egress rule.
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
