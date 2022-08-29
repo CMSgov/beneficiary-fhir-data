74b6c273fdbdb55e07a088231e1cd524fa35b9d4
Morgan Harvey <morgan@adhocteam.us>
2022-08-29 15:27:35 -0700

    Use environment variables instead of hardcoded values

--- apps/utils/locust_tests/lambda/server-load/controller.sh
+++ apps/utils/locust_tests/lambda/server-load/controller.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
+set -eou pipefail
 
-bfd_env="test"
-cluster_id="$(aws ssm get-parameter --name /bfd/${bfd_env}/common/nonsensitive/rds_cluster_identifier --query Parameter.Value --region us-east-1 --output text)"
-username="$(aws ssm get-parameter --name /bfd/${bfd_env}/server/sensitive/vault_data_server_db_username --with-decryption --query Parameter.Value --region us-east-1 --output text)"
-raw_password="$(aws ssm get-parameter --name /bfd/${bfd_env}/server/sensitive/vault_data_server_db_password --with-decryption --query Parameter.Value --region us-east-1 --output text)"
+cluster_id="$(aws ssm get-parameter --name /bfd/${BFD_ENV}/common/nonsensitive/rds_cluster_identifier --query Parameter.Value --region us-east-1 --output text)"
+username="$(aws ssm get-parameter --name /bfd/${BFD_ENV}/server/sensitive/vault_data_server_db_username --with-decryption --query Parameter.Value --region us-east-1 --output text)"
+raw_password="$(aws ssm get-parameter --name /bfd/${BFD_ENV}/server/sensitive/vault_data_server_db_password --with-decryption --query Parameter.Value --region us-east-1 --output text)"
 password="$( python3 -c 'import sys;import urllib.parse;print(urllib.parse.quote(sys.argv[1]))' "$raw_password")"
-cert_key="$(aws ssm get-parameter --name /bfd/${bfd_env}/server/sensitive/test_client_key --with-decryption --query Parameter.Value --region us-east-1 --output text)"
-cert="$(aws ssm get-parameter --name /bfd/${bfd_env}/server/sensitive/test_client_cert --with-decryption --query Parameter.Value --region us-east-1 --output text)"
+cert_key="$(aws ssm get-parameter --name /bfd/${BFD_ENV}/server/sensitive/test_client_key --with-decryption --query Parameter.Value --region us-east-1 --output text)"
+cert="$(aws ssm get-parameter --name /bfd/${BFD_ENV}/server/sensitive/test_client_cert --with-decryption --query Parameter.Value --region us-east-1 --output text)"
 db_dsn="postgres://${username}:${password}@bfd-test-aurora-cluster.cluster-ro-clyryngdhnko.us-east-1.rds.amazonaws.com:5432/fhirdb"
 
 
-cat <<EOF > "/tmp/${bfd_env}-cert.pem"
+cat <<EOF > "/tmp/${BFD_ENV}-cert.pem"
 $cert_key
 $cert
 EOF
 
 locust \
 	--locustfile=high_volume_suite.py \
-	--host='https://test.bfd.cms.gov' \
+	--host="${BFD_TEST_HOST}" \
 	--users=1 \
 	--master \
 	--master-bind-port=5557 \
-	--client-cert-path="/tmp/${bfd_env}-cert.pem" \
+	--client-cert-path="/tmp/${BFD_ENV}-cert.pem" \
 	--database-uri="$db_dsn"\
 	--enable-rebalancing \
 	--spawn-rate=5 \
