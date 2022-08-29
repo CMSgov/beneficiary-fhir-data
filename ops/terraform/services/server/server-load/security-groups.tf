ae1477a155ba5911681836fc7d6b7d3cbd5c3549
Morgan Harvey <morgan@adhocteam.us>
2022-08-29 14:01:20 -0700

    Remove ingress rule from lambda as no ingress traffic now

--- ops/terraform/services/server/server-load/security-groups.tf
+++ ops/terraform/services/server/server-load/security-groups.tf
@@ -4,14 +4,6 @@ resource "aws_security_group" "lambda" {
   tags        = merge(local.shared_tags, { Name = "bfd-${local.env}-${local.service}-lambda" })
   vpc_id      = data.aws_vpc.main.id
 
-  ingress {
-    protocol    = "-1"
-    self        = true # The security group itself will be added as a source to this ingress rule.
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     protocol    = "-1"
