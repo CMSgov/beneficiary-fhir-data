4de7de51a9eb543ccf160f67dbf16f4fc898e9da
Andrew Kramer <andrew.kramer@adhocteam.us>
2022-08-30 11:32:17 -0400

    Runs on demand, not automated for now

--- insights/terraform/projects/bfd/api-requests/glue.tf
+++ insights/terraform/projects/bfd/api-requests/glue.tf
@@ -1511,8 +1511,9 @@ resource "aws_glue_trigger" "glue-crawler-api-requests-crawler" {
   name          = "${local.full_name}-api-requests-crawler-trigger"
   description   = "Trigger to start the API Requests Crawler every day at 4 am UTC"
   workflow_name = aws_glue_workflow.glue-workflow-api-requests.name
-  type          = "SCHEDULED"
-  schedule      = "cron(0 4 * * ? *)" # Every day at 4am UTC
+  type          = "ON_DEMAND"
+  # type          = "SCHEDULED"
+  # schedule      = "cron(0 4 * * ? *)" # Every day at 4am UTC
 
   actions {
     crawler_name = aws_glue_crawler.glue-crawler-api-requests.name
