e55a4920f6640d7ec22d9cda7672f7de5aea2248
Morgan Harvey <morgan@adhocteam.us>
2022-08-30 14:10:14 -0700

    Fix locust args to not give bad cert path

--- apps/utils/locust_tests/lambda/server-load/controller.sh
+++ apps/utils/locust_tests/lambda/server-load/controller.sh
@@ -8,15 +8,7 @@ cluster_id="$(aws ssm get-parameter --name /bfd/${BFD_ENV}/common/nonsensitive/r
 username="$(aws ssm get-parameter --name /bfd/${BFD_ENV}/server/sensitive/vault_data_server_db_username --with-decryption --query Parameter.Value --region us-east-1 --output text)"
 raw_password="$(aws ssm get-parameter --name /bfd/${BFD_ENV}/server/sensitive/vault_data_server_db_password --with-decryption --query Parameter.Value --region us-east-1 --output text)"
 password="$( python3 -c 'import sys;import urllib.parse;print(urllib.parse.quote(sys.argv[1]))' "$raw_password")"
-cert_key="$(aws ssm get-parameter --name /bfd/${BFD_ENV}/server/sensitive/test_client_key --with-decryption --query Parameter.Value --region us-east-1 --output text)"
-cert="$(aws ssm get-parameter --name /bfd/${BFD_ENV}/server/sensitive/test_client_cert --with-decryption --query Parameter.Value --region us-east-1 --output text)"
 db_dsn="postgres://${username}:${password}@bfd-test-aurora-cluster.cluster-ro-clyryngdhnko.us-east-1.rds.amazonaws.com:5432/fhirdb"
-
-
-cat <<EOF > "/tmp/${BFD_ENV}-cert.pem"
-$cert_key
-$cert
-EOF
 
 locust \
 	--locustfile=high_volume_suite.py \
@@ -24,9 +16,10 @@ locust \
 	--users=5000 \
 	--master \
 	--master-bind-port=5557 \
-	--client-cert-path="/tmp/${BFD_ENV}-cert.pem" \
+	--client-cert-path="/tmp/bfd_test_cert.pem" \
 	--database-uri="$db_dsn"\
 	--enable-rebalancing \
 	--spawn-rate=5 \
 	--headless \
-	--only-summary
+	--loglevel=DEBUG \
+	--csv=load
