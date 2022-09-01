857041125eebcaef9928a373ff15aa7a28bb85e4
Brian Burton <brian.burton@adhocteam.us>
2022-09-01 12:49:53 -0400

    Fixes more javadoc issues.

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/r4/providers/pac/ClaimResponseTypeV2.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/r4/providers/pac/ClaimResponseTypeV2.java
@@ -52,11 +52,10 @@ public final class ClaimResponseTypeV2<TEntity>
   /**
    * Constructor is private to ensure only instances defined in this class are allowed.
    *
-   * @param nameForParsing name used when parsing parameter string to find appropriate instance
+   * @param nameForParsing value for {@link ResourceTypeV2#getNameForMetrics()}
    * @param nameForMetrics value returned by {@link ResourceTypeV2#getNameForMetrics}
    * @param entityClass the entity class for the associated resource
-   * @param entityMbiAttribute the attribute name for the mbi value on the entity class
-   * @param entityMbiHashAttribute the attribute name for the mbiHash value on the entity class
+   * @param entityMbiRecordAttribute the attribute name for the mbi value on the entity class
    * @param entityIdAttribute the attribute name for the ID of the entity class
    * @param entityEndDateAttribute the attribute name for the service end date on the entity class
    * @param transformer the transformer used to convert from the given entity to the associated
--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/r4/providers/pac/ClaimTypeV2.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/r4/providers/pac/ClaimTypeV2.java
@@ -51,11 +51,10 @@ public final class ClaimTypeV2<TEntity> extends AbstractResourceTypeV2<Claim, TE
   /**
    * Constructor is private to ensure only instances defined in this class are allowed.
    *
-   * @param nameForParsing name used when parsing parameter string to find appropriate instance
+   * @param nameForParsing value for {@link ResourceTypeV2#getNameForMetrics()}
    * @param nameForMetrics value returned by {@link ResourceTypeV2#getNameForMetrics}
    * @param entityClass the entity class for the associated resource
-   * @param entityMbiAttribute the attribute name for the mbi value on the entity class
-   * @param entityMbiHashAttribute the attribute name for the mbiHash value on the entity class
+   * @param entityMbiRecordAttribute the attribute name for the mbi value on the entity class
    * @param entityIdAttribute the attribute name for the ID of the entity class
    * @param entityEndDateAttribute the attribute name for the service end date on the entity class
    * @param transformer the transformer used to convert from the given entity to the associated
