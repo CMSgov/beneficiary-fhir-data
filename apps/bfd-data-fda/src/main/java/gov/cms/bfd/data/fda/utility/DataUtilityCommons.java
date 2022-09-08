bfb5f258219bdfe695a82d1386e0cddeba0af7e0
Chris Brune <cbrune@fearless.tech>
2022-09-07 20:46:54 -0400

    Delete file if exists

--- apps/bfd-data-fda/src/main/java/gov/cms/bfd/data/fda/utility/DataUtilityCommons.java
+++ apps/bfd-data-fda/src/main/java/gov/cms/bfd/data/fda/utility/DataUtilityCommons.java
@@ -51,14 +51,18 @@ public class DataUtilityCommons {
 
       // If the output file isn't already there, go build it.
       Path convertedNdcDataFile = outputPath.resolve(fdaFile);
-      if (!Files.exists(convertedNdcDataFile)) {
-        try {
-          DataUtilityCommons.buildProductsResource(convertedNdcDataFile, workingDir);
-        } finally {
-          // Recursively delete the working dir.
-          recursivelyDelete(workingDir);
-        }
+
+      if (Files.exists(convertedNdcDataFile)) {
+        Files.delete(convertedNdcDataFile);
       }
+
+      try {
+        DataUtilityCommons.buildProductsResource(convertedNdcDataFile, workingDir);
+      } finally {
+        // Recursively delete the working dir.
+        recursivelyDelete(workingDir);
+      }
+
     } catch (Exception ex) {
       throw new IllegalStateException(ex);
     }
