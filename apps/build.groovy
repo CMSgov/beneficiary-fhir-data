076ded08cf897bc8b9db94d54f2fd44e5a428517
Chris Brune <64486927+cbrunefearless@users.noreply.github.com>
2022-09-09 09:58:54 -0400

    Update apps/build.groovy
    Co-authored-by: Michael J Burling <michael.burling@adhocteam.us>

--- apps/build.groovy
+++ apps/build.groovy
@@ -26,42 +26,60 @@ class AppBuildResults implements Serializable {
  * @throws Exception An exception will be bubbled up if the Maven build fails.
  */
 def build(boolean verboseMaven) {
+	withCredentials([string(credentialsId: 'bfd-aws-account-id', variable: 'ACCOUNT_ID')]) {
+		// Set the auth token for aws code artifact
+		env.CODEARTIFACT_AUTH_TOKEN = sh(
+			returnStdout: true,
+			script: '''
+aws codeartifact get-authorization-token --domain bfd-mgmt \
+ --domain-owner "$ACCOUNT_ID" \
+ --output text --query authorizationToken
+'''
+		).trim()
+
+		// Get our endpoint url for our aws code artifact
+		env.CODEARTIFACT_ENDPOINT = sh(
+			returnStdout: true,
+			script: '''
+aws codeartifact get-repository-endpoint \
+--domain bfd-mgmt --repository bfd-mgmt \
+--format maven --output text
+'''
+		).trim()
+	}
+
+	// Add the authorization token and username for our aws code artifact repository
+	sh '''
+cat <<EOF > ~/.m2/settings.xml
+<settings xmlns=\"http://maven.apache.org/SETTINGS/1.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+xsi:schemaLocation=\"http://maven.apache.org/SETTINGS/1.0.0 https://maven.apache.org/xsd/settings-1.0.0.xsd\">
+    <profiles>
+        <profile>
+            <id>bfd-mgmt-bfd-mgmt</id>
+            <activation>
+            <activeByDefault>true</activeByDefault>
+            </activation>
+            <repositories>
+                <repository>
+                    <id>bfd-mgmt-bfd-mgmt</id>
+                    <url>${CODEARTIFACT_ENDPOINT}</url>
+                </repository>
+            </repositories>
+        </profile>
+    </profiles>
+    <servers>
+        <server>
+        <username>aws</username>
+        <password>${CODEARTIFACT_AUTH_TOKEN}</password>
+        <id>bfd-mgmt-bfd-mgmt</id>
+        </server>
+    </servers>
+</settings>
+EOF
+'''
+
 	dir ('apps') {
 		quietFlags = verboseMaven ? '' : '--quiet --batch-mode'
-  
-  	sh "ACCOUNT_ID=\"$(python3 -c 'import json,sys;print(json.loads(sys.argv[1])[\"Account\"])' \"$(aws sts get-caller-identity)\")\"
-            CODEARTIFACT_ENDPOINT=\"$(aws codeartifact get-repository-endpoint --domain bfd-mgmt --repository bfd-mgmt --format maven --output text)\"
-            CODEARTIFACT_AUTH_TOKEN=\"$(aws codeartifact get-authorization-token --domain bfd-mgmt \
-            --domain-owner \"$ACCOUNT_ID\" \
-            --output text --query authorizationToken)\"
-            cat <<EOF > ~/.m2/settings.xml
-            <settings xmlns=\"http://maven.apache.org/SETTINGS/1.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
-            xsi:schemaLocation=\"http://maven.apache.org/SETTINGS/1.0.0 https://maven.apache.org/xsd/settings-1.0.0.xsd\">
-                <profiles>
-                    <profile>
-                        <id>bfd-mgmt-bfd-mgmt</id>
-                        <activation>
-                        <activeByDefault>true</activeByDefault>
-                        </activation>
-                        <repositories>
-                            <repository>
-                                <id>bfd-mgmt-bfd-mgmt</id>
-                                <url>${CODEARTIFACT_ENDPOINT}</url>
-                            </repository>
-                        </repositories>
-                    </profile>
-                </profiles>
-                <servers>
-                    <server>
-                    <username>aws</username>
-                    <password>${CODEARTIFACT_AUTH_TOKEN}</password>
-                    <id>bfd-mgmt-bfd-mgmt</id>
-                    </server>
-                </servers>
-            </settings>
-          EOF"
-	
-
 		sh "mvn ${quietFlags} --threads 1C --update-snapshots -DskipITs -DskipTests -Dmaven.javadoc.skip=true clean verify"
 	}
 
