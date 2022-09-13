327930566af2fed29f95c7a0bd18298cd6d75232
Chris Brune <cbrune@fearless.tech>
2022-09-13 13:02:56 -0400

    Pr review changes

--- apps/bfd-data-fda/src/main/java/gov/cms/bfd/data/fda/utility/DataUtilityCommons.java
+++ apps/bfd-data-fda/src/main/java/gov/cms/bfd/data/fda/utility/DataUtilityCommons.java
@@ -51,9 +51,6 @@ public class DataUtilityCommons {
 
       // If the output file isn't already there, go build it.
       Path convertedNdcDataFile = outputPath.resolve(fdaFile);
-      if (Files.exists(convertedNdcDataFile)) {
-        Files.delete(convertedNdcDataFile);
-      }
 
       try {
         DataUtilityCommons.buildProductsResource(convertedNdcDataFile, workingDir);
