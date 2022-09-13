7d667d98b6535ca1f1fc1f8c6f87e9c4703d04c4
Chris Brune <cbrune@fearless.tech>
2022-09-13 11:57:08 -0400

    PR review changes

--- apps/bfd-data-fda/src/main/java/gov/cms/bfd/data/fda/utility/DataUtilityCommons.java
+++ apps/bfd-data-fda/src/main/java/gov/cms/bfd/data/fda/utility/DataUtilityCommons.java
@@ -52,16 +52,16 @@ public class DataUtilityCommons {
       // If the output file isn't already there, go build it.
       Path convertedNdcDataFile = outputPath.resolve(fdaFile);
       if (Files.exists(convertedNdcDataFile)) {
-          Files.delete(convertedNdcDataFile);
+        Files.delete(convertedNdcDataFile);
       }
-      
-        try {
-          DataUtilityCommons.buildProductsResource(convertedNdcDataFile, workingDir);
-        } finally {
-          // Recursively delete the working dir.
-          recursivelyDelete(workingDir);
-        }
-      
+
+      try {
+        DataUtilityCommons.buildProductsResource(convertedNdcDataFile, workingDir);
+      } finally {
+        // Recursively delete the working dir.
+        recursivelyDelete(workingDir);
+      }
+
     } catch (Exception ex) {
       throw new IllegalStateException(ex);
     }
