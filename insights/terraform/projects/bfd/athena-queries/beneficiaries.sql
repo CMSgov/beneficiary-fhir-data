5246a7e0d317f26f9db41e347137278a6c1a1ae6
Andrew Kramer <andrew.kramer@adhocteam.us>
2022-09-13 12:29:51 -0400

    Exclude test certs

--- insights/terraform/projects/bfd/athena-queries/beneficiaries.sql
+++ insights/terraform/projects/bfd/athena-queries/beneficiaries.sql
@@ -3,4 +3,9 @@ CREATE OR REPLACE VIEW "bfd_insights_bfd_prod_beneficiaries" AS
         TRY(CAST(TRIM(bene) as bigint)) as bene,
         "bfd_insights_bfd_prod_partners".*
     FROM "bfd_insights_bfd_prod_partners"
-    CROSS JOIN UNNEST(split(benes, ',')) as x(bene);
+    CROSS JOIN UNNEST(split(benes, ',')) as x(bene)
+    WHERE
+        (partner = 'ab2d'
+        OR partner = 'bb2'
+        OR partner = 'bcda'
+        OR partner = 'dpc');
