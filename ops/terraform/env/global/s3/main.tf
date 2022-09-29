cf126309ecc8262bbb5825233ef6e4f8d98dad2b
Michael J Burling <michael.burling@adhocteam.us>
2022-09-29 08:21:52 -0500

    remediate, update bfd-tf-state bucket

--- ops/terraform/env/global/s3/main.tf
+++ ops/terraform/env/global/s3/main.tf
@@ -1,21 +1,33 @@
-## Terraform State and Build Buckets
-#
+locals {
+  bfd_state_bucket = "bfd-tf-state"
+  bcda_aws_account_number = data.aws_ssm_parameter.bcda_aws_account_number.value
+  kms_key_id          = data.aws_kms_key.state.arn
+}
+
 provider "aws" {
-  version = "~> 3.44.0"
+  version = "~> 4"
   region  = "us-east-1"
 }
 
-data "aws_kms_key" "tf_state" {
+data "aws_ssm_parameter" "bcda_aws_account_number" {
+  name            = "/bcda/global/terraform/sensitive/aws_account_number"
+  with_decryption = true
+}
+
+data "aws_kms_key" "state" {
   key_id = "alias/bfd-tf-state"
 }
 
+resource "aws_s3_bucket" "state" {
+  bucket = local.bfd_state_bucket
 
-## TF State Bucket
-#
-resource "aws_s3_bucket" "state_bucket" {
-  bucket = var.bfd_tf_state_bucket
-  acl    = "private"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
+resource "aws_s3_bucket_policy" "state" {
+  bucket = aws_s3_bucket.state.id
   policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -26,7 +38,7 @@ resource "aws_s3_bucket" "state_bucket" {
             "Effect": "Deny",
             "Principal": "*",
             "Action": "s3:PutObject",
-            "Resource": "arn:aws:s3:::${var.bfd_tf_state_bucket}/*",
+            "Resource": "arn:aws:s3:::${local.bfd_state_bucket}/*",
             "Condition": {
               "StringNotEquals": {
                 "s3:x-amz-server-side-encryption": "aws:kms"
@@ -38,78 +50,60 @@ resource "aws_s3_bucket" "state_bucket" {
             "Effect": "Allow",
             "Principal": "*",
             "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::${var.bfd_tf_state_bucket}/*",
+            "Resource": "arn:aws:s3:::${local.bfd_state_bucket}/*",
             "Condition": {
                 "ArnEquals": {
-                    "aws:userid": "arn:aws:iam::${var.bcda_acct_num}:user/Jenkins"
-                }
-                "StringNotEquals": {
-                    "s3:x-amz-server-side-encryption": "aws:kms"
+                    "aws:userid": "arn:aws:iam::${local.bcda_aws_account_number}:user/Jenkins"
                 }
             }
-        }
-    ]
-}
-EOF
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = data.aws_kms_key.tf_state.id
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-
-## Bucket for Packages, RPM, WAR, JAR, etc.
-#
-resource "aws_s3_bucket" "bfd_packages_bucket" {
-  bucket = var.bfd_packages_bucket
-  acl    = "private"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Id": "PutObjPolicy",
-    "Statement": [
-        {
-            "Sid": "DenyUnEncryptedObjectUploads",
-            "Effect": "Deny",
-            "Principal": "*",
-            "Action": "s3:PutObject",
-            "Resource": "arn:aws:s3:::${var.bfd_packages_bucket}/*",
         },
         {
-            "Sid": "JenkinsGetObject",
-            "Effect": "Allow",
+            "Sid": "AllowSSLRequestsOnly",
+            "Effect": "Deny",
             "Principal": "*",
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::${var.bfd_packages_bucket}/*",
+            "Action": "s3:*",
+            "Resource": [
+                "arn:aws:s3:::bfd-tf-state",
+                "arn:aws:s3:::bfd-tf-state/*"
+            ],
             "Condition": {
-                "ArnEquals": {
-                    "aws:userid": "arn:aws:iam::${var.bcda_acct_num}:user/Jenkins"
+                "Bool": {
+                    "aws:SecureTransport": "false"
                 }
             }
         }
     ]
 }
 EOF
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
+  bucket = aws_s3_bucket.state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = local.kms_key_id
+    }
   }
+}
 
-  lifecycle {
-    prevent_destroy = true
+resource "aws_s3_bucket_versioning" "state" {
+  bucket = aws_s3_bucket.state.id
+  versioning_configuration {
+    status = "Enabled"
   }
+}
+
+resource "aws_s3_bucket_acl" "state" {
+  bucket = aws_s3_bucket.state.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "state" {
+  bucket = aws_s3_bucket.state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
