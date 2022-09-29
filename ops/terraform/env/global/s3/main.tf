9b79d0f03e6c4be2c3d70f42e74740f5a98fd3d9
Michael J Burling <michael.burling@adhocteam.us>
2022-09-29 09:35:44 -0500

    remeditate bfd-cloudtrail-logs bucket

--- ops/terraform/env/global/s3/main.tf
+++ ops/terraform/env/global/s3/main.tf
@@ -1,13 +1,17 @@
 locals {
-  bfd_state_bucket = "bfd-tf-state"
+  account_id              = data.aws_caller_identity.current.account_id
+  bfd_state_bucket        = "bfd-tf-state"
   bcda_aws_account_number = data.aws_ssm_parameter.bcda_aws_account_number.value
-  kms_key_id          = data.aws_kms_key.state.arn
+  kms_key_id              = data.aws_kms_key.state.arn
+  cloudtrail_logs_bucket  = "bfd-cloudtrail-logs"
 }
 
 provider "aws" {
   version = "~> 4"
   region  = "us-east-1"
 }
+
+data "aws_caller_identity" "current" {}
 
 data "aws_ssm_parameter" "bcda_aws_account_number" {
   name            = "/bcda/global/terraform/sensitive/aws_account_number"
@@ -106,4 +110,104 @@ resource "aws_s3_bucket_public_access_block" "state" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+
+resource "aws_s3_bucket" "cloudtrail_logs" {
+  bucket = local.cloudtrail_logs_bucket
+
+  # TODO: This is an untagged resource in AWS as of 2022-09-29
+  # tags = {
+  #   role  = TODO
+  #   Layer = TODO
+  # }
+}
+
+# TODO: Address in BFD-2164. Unsure of which key is most appropriate here
+# resource "aws_s3_bucket_server_side_encryption_configuration" "cloudtrail_logs" {
+#   bucket = aws_s3_bucket.cloudtrail_logs.id
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm     = "aws:kms"
+#       kms_master_key_id = local.kms_key_id
+#     }
+#   }
+# }
+#
+
+resource "aws_s3_bucket_versioning" "cloudtrail_logs" {
+  bucket = aws_s3_bucket.cloudtrail_logs.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "cloudtrail_logs" {
+  bucket = aws_s3_bucket.cloudtrail_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# TODO: Address in subsequent task/story. Unsure of which bucket, target prefix
+# resource "aws_s3_bucket_logging" "admin" {
+#   bucket        = aws_s3_bucket.cloudtrail_logs.id
+#   target_bucket = TODO
+#   target_prefix = TODO
+# }
+
+resource "aws_s3_bucket_acl" "cloudtrail_logs" {
+  acl    = "private"
+  bucket = aws_s3_bucket.cloudtrail_logs.id
+}
+
+resource "aws_s3_bucket_policy" "cloudtrail_logs" {
+  bucket = aws_s3_bucket.cloudtrail_logs.id
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck20150319",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "${aws_s3_bucket.cloudtrail_logs.arn}"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite20150319",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "${aws_s3_bucket.cloudtrail_logs.arn}/AWSLogs/${local.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        },
+        {
+            "Sid": "AllowSSLRequestsOnly",
+            "Effect": "Deny",
+            "Principal": "*",
+            "Action": "s3:*",
+            "Resource": [
+                "${aws_s3_bucket.cloudtrail_logs.arn}",
+                "${aws_s3_bucket.cloudtrail_logs.arn}/*"
+            ],
+            "Condition": {
+                "Bool": {
+                    "aws:SecureTransport": "false"
+                }
+            }
+        }
+    ]
+}
+POLICY
 }
