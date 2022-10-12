dffd5768fa9260d0d88194dc5c5f127ea5e01ea6
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-10-12 13:16:54 -0400

    Add metric alarms for /v*/fhir/Patient (by contract, count 4000) endpoint SLOs

--- ops/terraform/modules/resources/bfd_server_slo_alarms/main.tf
+++ ops/terraform/modules/resources/bfd_server_slo_alarms/main.tf
@@ -518,3 +518,100 @@ resource "aws_cloudwatch_metric_alarm" "slo_patient_no_contract_nonbulk_latency_
   datapoints_to_alarm = "1"
   treat_missing_data  = "notBreaching"
 }
+
+resource "aws_cloudwatch_metric_alarm" "slo_patient_by_contract_count_4000_latency_mean_15m_alert" {
+  alarm_name          = "${local.app}-${var.env}-slo-patient-by-contract-count-4000-latency-mean-15m-alert"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  period              = "900"
+  statistic           = "Average"
+  threshold           = "40000"
+
+  alarm_description = join("", [
+    "/v*/fhir/Patient (by contract, count 4000) response mean 15 minute latency exceeded ALERT ",
+    "SLO threshold of 40 seconds for ${local.app} in ${var.env} environment"
+  ])
+
+  metric_name = "${local.metrics.patient_contract_count_4000_latency}/all"
+  namespace   = local.namespace
+
+  alarm_actions = local.alert_arn
+  ok_actions    = local.ok_arn
+
+  datapoints_to_alarm = "1"
+  treat_missing_data  = "notBreaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "slo_patient_by_contract_count_4000_latency_mean_15m_warning" {
+  alarm_name          = "${local.app}-${var.env}-slo-patient-by-contract-count-4000-latency-mean-15m-warning"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  period              = "900"
+  statistic           = "Average"
+  threshold           = "40000"
+
+  alarm_description = join("", [
+    "/v*/fhir/Patient (by contract, count 4000) response mean 15 minute latency exceeded WARNING ",
+    "SLO threshold of 40 seconds for ${local.app} in ${var.env} environment"
+  ])
+
+  metric_name = "${local.metrics.patient_contract_count_4000_latency}/all"
+  namespace   = local.namespace
+
+  alarm_actions = local.warning_arn
+  ok_actions    = local.ok_arn
+
+  datapoints_to_alarm = "1"
+  treat_missing_data  = "notBreaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "slo_patient_by_contract_count_4000_latency_99p_15m_alert" {
+  for_each = concat(local.bulk_partners, local.non_bulk_partners)
+
+  alarm_name          = "${local.app}-${var.env}-slo-patient-by-contract-count-4000-latency-99p-15m-alert-${each.key}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  period              = "900"
+  extended_statistic  = local.ext_stat_99p
+  threshold           = "${local.partner_timeouts_ms[each.key]}"
+
+  alarm_description = join("", [
+    "/v*/fhir/Patient (by contract, count 4000) response 99% 15 minute latency exceeded ALERT ",
+    "SLO threshold of ${local.partner_timeouts_ms[each.key]} ms for partner ${each.key} for ",
+    "${local.app} in ${var.env} environment"
+  ])
+
+  metric_name = "${local.metrics.patient_contract_count_4000_latency}/${each.key}"
+  namespace   = local.namespace
+
+  alarm_actions = local.alert_arn
+  ok_actions    = local.ok_arn
+
+  datapoints_to_alarm = "1"
+  treat_missing_data  = "notBreaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "slo_patient_by_contract_count_4000_latency_99p_15m_warning" {
+  for_each = concat(local.bulk_partners, local.non_bulk_partners)
+
+  alarm_name          = "${local.app}-${var.env}-slo-patient-by-contract-count-4000-latency-99p-15m-warning-${each.key}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  period              = "900"
+  extended_statistic  = local.ext_stat_99p
+  threshold           = "44000"
+
+  alarm_description = join("", [
+    "/v*/fhir/Patient (by contract, count 4000) response 99% 15 minute latency exceeded WARNING ",
+    "SLO threshold of 44 seconds for partner ${each.key} for ${local.app} in ${var.env} environment"
+  ])
+
+  metric_name = "${local.metrics.patient_contract_count_4000_latency}/${each.key}"
+  namespace   = local.namespace
+
+  alarm_actions = local.warning_arn
+  ok_actions    = local.ok_arn
+
+  datapoints_to_alarm = "1"
+  treat_missing_data  = "notBreaching"
+}
