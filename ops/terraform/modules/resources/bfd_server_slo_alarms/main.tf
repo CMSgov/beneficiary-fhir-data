9b30784fd03f88688c5f3f5e374f4a06bd0f8085
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-10-12 11:40:53 -0400

    Add metric alarms for /v*/fhir/Patient (not by contract) endpoint SLOs

--- ops/terraform/modules/resources/bfd_server_slo_alarms/main.tf
+++ ops/terraform/modules/resources/bfd_server_slo_alarms/main.tf
@@ -371,3 +371,150 @@ resource "aws_cloudwatch_metric_alarm" "slo_eob_nonbulk_latency_per_kb_99p_15m_w
   datapoints_to_alarm = "1"
   treat_missing_data  = "notBreaching"
 }
+
+resource "aws_cloudwatch_metric_alarm" "slo_patient_no_contract_latency_mean_15m_alert" {
+  alarm_name          = "${local.app}-${var.env}-slo-patient-no-contract-latency-mean-15m-alert"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  period              = "900"
+  statistic           = "Average"
+  threshold           = "80"
+
+  alarm_description = join("", [
+    "/v*/fhir/Patient (not by contract) response mean 15 minute latency exceeded ALERT SLO ",
+    "threshold of 80 ms for ${local.app} in ${var.env} environment"
+  ])
+
+  metric_name = "${local.metrics.patient_no_contract_latency}/all"
+  namespace   = local.namespace
+
+  alarm_actions = local.alert_arn
+  ok_actions    = local.ok_arn
+
+  datapoints_to_alarm = "1"
+  treat_missing_data  = "notBreaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "slo_patient_no_contract_latency_mean_15m_warning" {
+  alarm_name          = "${local.app}-${var.env}-slo-patient-no-contract-latency-mean-15m-warning"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  period              = "900"
+  statistic           = "Average"
+  threshold           = "60"
+
+  alarm_description = join("", [
+    "/v*/fhir/Patient (not by contract) response mean 15 minute latency exceeded WARNING SLO ",
+    "threshold of 60 ms for ${local.app} in ${var.env} environment"
+  ])
+
+  metric_name = "${local.metrics.patient_no_contract_latency}/all"
+  namespace   = local.namespace
+
+  alarm_actions = local.warning_arn
+  ok_actions    = local.ok_arn
+
+  datapoints_to_alarm = "1"
+  treat_missing_data  = "notBreaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "slo_patient_no_contract_bulk_latency_99p_15m_alert" {
+  for_each = local.bulk_partners
+
+  alarm_name          = "${local.app}-${var.env}-slo-patient-no-contract-bulk-latency-99p-15m-alert-${each.key}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  period              = "900"
+  extended_statistic  = local.ext_stat_99p
+  threshold           = "${local.partner_timeouts_ms[each.key]}"
+
+  alarm_description = join("", [
+    "/v*/fhir/Patient (not by contract) response 99% 15 minute BULK latency exceeded ALERT SLO ",
+    "threshold of ${local.partner_timeouts_ms[each.key]} ms for partner ${each.key} for ",
+    "${local.app} in ${var.env} environment"
+  ])
+
+  metric_name = "${local.metrics.patient_no_contract_latency}/${each.key}"
+  namespace   = local.namespace
+
+  alarm_actions = local.alert_arn
+  ok_actions    = local.ok_arn
+
+  datapoints_to_alarm = "1"
+  treat_missing_data  = "notBreaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "slo_patient_no_contract_bulk_latency_99p_15m_warning" {
+  for_each = local.bulk_partners
+
+  alarm_name          = "${local.app}-${var.env}-slo-patient-no-contract-bulk-latency-99p-15m-warning-${each.key}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  period              = "900"
+  extended_statistic  = local.ext_stat_99p
+  threshold           = "110"
+
+  alarm_description = join("", [
+    "/v*/fhir/Patient (not by contract) response 99% 15 minute BULK latency exceeded WARNING SLO ",
+    "threshold of 110 ms for partner ${each.key} for ${local.app} in ${var.env} environment"
+  ])
+
+  metric_name = "${local.metrics.patient_no_contract_latency}/${each.key}"
+  namespace   = local.namespace
+
+  alarm_actions = local.warning_arn
+  ok_actions    = local.ok_arn
+
+  datapoints_to_alarm = "1"
+  treat_missing_data  = "notBreaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "slo_patient_no_contract_nonbulk_latency_99p_15m_alert" {
+  for_each = local.non_bulk_partners
+
+  alarm_name          = "${local.app}-${var.env}-slo-patient-no-contract-nonbulk-latency-99p-15m-alert-${each.key}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  period              = "900"
+  extended_statistic  = local.ext_stat_99p
+  threshold           = "160"
+
+  alarm_description = join("", [
+    "/v*/fhir/Patient (not by contract) response 99% 15 minute NON-BULK latency exceeded ALERT ",
+    "SLO threshold of 160 ms for partner ${each.key} for ${local.app} in ${var.env} environment"
+  ])
+
+  metric_name = "${local.metrics.patient_no_contract_latency}/${each.key}"
+  namespace   = local.namespace
+
+  alarm_actions = local.alert_arn
+  ok_actions    = local.ok_arn
+
+  datapoints_to_alarm = "1"
+  treat_missing_data  = "notBreaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "slo_patient_no_contract_nonbulk_latency_99p_15m_warning" {
+  for_each = local.non_bulk_partners
+
+  alarm_name          = "${local.app}-${var.env}-slo-patient-no-contract-nonbulk-latency-99p-15m-warning-${each.key}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  period              = "900"
+  extended_statistic  = local.ext_stat_99p
+  threshold           = "110"
+
+  alarm_description = join("", [
+    "/v*/fhir/Patient (not by contract) response 99% 15 minute NON-BULK latency exceeded WARNING ",
+    "SLO threshold of 110 ms for partner ${each.key} for ${local.app} in ${var.env} environment"
+  ])
+
+  metric_name = "${local.metrics.patient_no_contract_latency}/${each.key}"
+  namespace   = local.namespace
+
+  alarm_actions = local.warning_arn
+  ok_actions    = local.ok_arn
+
+  datapoints_to_alarm = "1"
+  treat_missing_data  = "notBreaching"
+}
