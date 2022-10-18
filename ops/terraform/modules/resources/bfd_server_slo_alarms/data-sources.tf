9d5c9c8b15360b5e0453cb48b6790d26e333007f
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-10-18 14:36:01 -0400

    Refactor get-partner-metrics.sh to only retrieve one matching dimension; format data-sources.tf

--- ops/terraform/modules/resources/bfd_server_slo_alarms/data-sources.tf
+++ ops/terraform/modules/resources/bfd_server_slo_alarms/data-sources.tf
@@ -2,10 +2,10 @@ data "external" "metrics_by_partner" {
   for_each = local.metrics
 
   program = [
-    "bash", 
-    "get-partner-metrics.sh", 
-    each.value, 
-    local.namespace, 
+    "bash",
+    "get-partner-metrics.sh",
+    each.value,
+    local.namespace,
     jsonencode(local.partner_regexs)
   ]
 }
--- ops/terraform/modules/resources/bfd_server_slo_alarms/get-partner-metrics.sh
+++ ops/terraform/modules/resources/bfd_server_slo_alarms/get-partner-metrics.sh
@@ -20,7 +20,7 @@ IFS=$'\n' read -r -d '' -a partners < <(
 partner_metrics=()
 for partner in "${partners[@]}"; do
   partner_regex=$(jq -r ".[\"${partner}\"]" <<<"${partners_regex_json}")
-  applicable_dimensions=$(
+  applicable_dimension=$(
     aws cloudwatch list-metrics --namespace "${metric_namespace}" --metric-name "${metric_name}" \
       | jq -c "[
         .Metrics[] 
@@ -28,21 +28,23 @@ for partner in "${partners[@]}"; do
           | select(.Name == \"client_ssl\") 
           | select(.Value | test(\"${partner_regex}\")) 
           | .Value
-      ]"
+      ][0]"
   )
+
+  if [[ "${applicable_dimension}" == "null" ]]; then
+    continue
+  fi
 
   partner_dimensioned_metrics=$(
     jq -c "{
-      ${partner}: [
-        .[] | {
-          name: \"${metric_name}\", 
-          namespace: \"${metric_namespace}\", 
-          dimension: .
-        }
-      ]
-    }" <<<"${applicable_dimensions}"
+      ${partner}: { 
+        name: \"${metric_name}\", 
+        namespace: \"${metric_namespace}\", 
+        dimension: .
+      }
+    }" <<<"${applicable_dimension}"
   )
   partner_metrics+=("${partner_dimensioned_metrics}")
 done
 
-jq -s "." <<<"${partner_metrics[@]}"
+jq -s "add" <<<"${partner_metrics[@]}"
