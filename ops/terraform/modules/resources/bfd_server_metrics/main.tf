361ca672786a320966c1396ab53854ef634ba50c
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-10-20 15:27:46 -0400

    Suffix each metric filter by corresponding dimension to ensure that non-dimensioned filters do not apply over each other

--- ops/terraform/modules/resources/bfd_server_metrics/main.tf
+++ ops/terraform/modules/resources/bfd_server_metrics/main.tf
@@ -13,23 +13,34 @@ locals {
     claim_response_all = "*/fhir/ClaimResponse"
   }
 
-  endpoint_patterns = { for name, pattern in local.endpoints : replace(name, "_", "-") => "$.mdc.http_access_request_uri = \"${pattern}\"" }
+  endpoint_patterns = {
+    for name, pattern in local.endpoints :
+    replace(name, "_", "-") => "$.mdc.http_access_request_uri = \"${pattern}\""
+  }
 
   client_ssl_pattern = "$.mdc.http_access_request_clientSSL_DN = \"*\""
 
-  ssl_dimensions = {
-    all_partners = null
+  filter_variations = {
+    all_partners = {
+      name_suffix = "all"
+      dimensions  = null
+    }
     by_partner = {
-      client_ssl = "$.mdc.http_access_request_clientSSL_DN"
+      name_suffix = "by-partner"
+      dimensions = {
+        client_ssl = "$.mdc.http_access_request_clientSSL_DN"
+      }
     }
   }
 
   endpoint_filters_config = merge([
     for endpoint_key, endpoint_pattern in local.endpoint_patterns : {
-      for filter_type, dimensions in local.ssl_dimensions : "${endpoint_key}_${filter_type}" => {
+      for variation, variation_config in local.filter_variations :
+      "${replace(endpoint_key, "-", "_")}_${variation}" => {
         "resource_name"    = endpoint_key
         "endpoint_pattern" = endpoint_pattern
-        "dimensions"       = dimensions
+        "name_suffix"      = variation_config.name_suffix
+        "dimensions"       = variation_config.dimensions
       }
     }
   ]...)
@@ -39,7 +50,7 @@ locals {
 resource "aws_cloudwatch_log_metric_filter" "http_requests_count" {
   for_each = local.endpoint_filters_config
 
-  name           = "bfd-${var.env}/bfd-server/http-requests/count/${each.value.resource_name}"
+  name           = "bfd-${var.env}/bfd-server/http-requests/count/${each.value.resource_name}/${each.value.name_suffix}"
   log_group_name = local.log_groups.access
 
   pattern = join("", [
@@ -59,7 +70,7 @@ resource "aws_cloudwatch_log_metric_filter" "http_requests_count" {
 resource "aws_cloudwatch_log_metric_filter" "http_requests_latency" {
   for_each = local.endpoint_filters_config
 
-  name           = "bfd-${var.env}/bfd-server/http-requests/latency/${each.value.resource_name}"
+  name           = "bfd-${var.env}/bfd-server/http-requests/latency/${each.value.resource_name}/${each.value.name_suffix}"
   log_group_name = local.log_groups.access
 
   pattern = join("", [
@@ -78,9 +89,9 @@ resource "aws_cloudwatch_log_metric_filter" "http_requests_latency" {
 
 # Count HTTP 5xxs with partner client SSL as dimension
 resource "aws_cloudwatch_log_metric_filter" "http_requests_count_500_responses" {
-  for_each = local.ssl_dimensions
+  for_each = local.filter_variations
 
-  name           = "bfd-${var.env}/bfd-server/http-requests/count/500-responses"
+  name           = "bfd-${var.env}/bfd-server/http-requests/count/500-responses/${each.value.name_suffix}"
   log_group_name = local.log_groups.access
 
   pattern = join("", [
@@ -91,15 +102,15 @@ resource "aws_cloudwatch_log_metric_filter" "http_requests_count_500_responses" 
     name       = "http-requests/count/500-responses"
     namespace  = local.namespace
     value      = "1"
-    dimensions = each.value
+    dimensions = each.value.dimensions
   }
 }
 
 # Count HTTP non-2XXs with partner client SSL as dimension
 resource "aws_cloudwatch_log_metric_filter" "http_requests_count_non_2xx_responses" {
-  for_each = local.ssl_dimensions
+  for_each = local.filter_variations
 
-  name           = "bfd-${var.env}/bfd-server/http-requests/count/non-2xx-responses"
+  name           = "bfd-${var.env}/bfd-server/http-requests/count/non-2xx-responses/${each.value.name_suffix}"
   log_group_name = local.log_groups.access
 
   pattern = join("", [
@@ -111,15 +122,15 @@ resource "aws_cloudwatch_log_metric_filter" "http_requests_count_non_2xx_respons
     name       = "http-requests/count/non-2xx-responses"
     namespace  = local.namespace
     value      = "1"
-    dimensions = each.value
+    dimensions = each.value.dimensions
   }
 }
 
 # Latency for Patient endpoints _not_ by contract (for SLOs) with partner client SSL as dimension
 resource "aws_cloudwatch_log_metric_filter" "http_requests_latency_patient_not_by_contract" {
-  for_each = local.ssl_dimensions
+  for_each = local.filter_variations
 
-  name           = "bfd-${var.env}/bfd-server/http-requests/latency/patient-not-by-contract"
+  name           = "bfd-${var.env}/bfd-server/http-requests/latency/patient-not-by-contract/${each.value.name_suffix}"
   log_group_name = local.log_groups.access
 
   # Terraform HCL has no support for breaking long strings, so the join() function is used as a
@@ -136,16 +147,16 @@ resource "aws_cloudwatch_log_metric_filter" "http_requests_latency_patient_not_b
     name       = "http-requests/latency/patient-not-by-contract"
     namespace  = local.namespace
     value      = "$.mdc.http_access_response_duration_milliseconds"
-    dimensions = each.value
+    dimensions = each.value.dimensions
   }
 }
 
 # Latency for Patient endpoints by contract with count = 4000 (for SLOs) with partner client SSL as
 # dimension
 resource "aws_cloudwatch_log_metric_filter" "http_requests_latency_patient_by_contract_count_4000" {
-  for_each = local.ssl_dimensions
+  for_each = local.filter_variations
 
-  name           = "bfd-${var.env}/bfd-server/http-requests/latency/patient-by-contract-count-4000"
+  name           = "bfd-${var.env}/bfd-server/http-requests/latency/patient-by-contract-count-4000/${each.value.name_suffix}"
   log_group_name = local.log_groups.access
 
   pattern = join("", [
@@ -161,15 +172,15 @@ resource "aws_cloudwatch_log_metric_filter" "http_requests_latency_patient_by_co
     name       = "http-requests/latency/patient-by-contract-count-4000"
     namespace  = local.namespace
     value      = "$.mdc.http_access_response_duration_milliseconds"
-    dimensions = each.value
+    dimensions = each.value.dimensions
   }
 }
 
 # Latency per-KB for all EoB endpoints (for SLOs) with partner client SSL as dimension
 resource "aws_cloudwatch_log_metric_filter" "http_requests_latency_by_kb_eob_all" {
-  for_each = local.ssl_dimensions
+  for_each = local.filter_variations
 
-  name           = "bfd-${var.env}/bfd-server/http-requests/latency-by-kb/eob-all"
+  name           = "bfd-${var.env}/bfd-server/http-requests/latency-by-kb/eob-all/${each.value.name_suffix}"
   log_group_name = local.log_groups.access
 
   pattern = join("", [
@@ -182,16 +193,16 @@ resource "aws_cloudwatch_log_metric_filter" "http_requests_latency_by_kb_eob_all
     name       = "http-requests/latency-by-kb/eob-all"
     namespace  = local.namespace
     value      = "$.mdc.http_access_response_duration_per_kb"
-    dimensions = each.value
+    dimensions = each.value.dimensions
   }
 }
 
 # Latency for all EoB endpoints with no resources returned (for SLOs) with partner client SSL as
 # dimension
 resource "aws_cloudwatch_log_metric_filter" "http_requests_latency_eob_all_no_resources" {
-  for_each = local.ssl_dimensions
+  for_each = local.filter_variations
 
-  name           = "bfd-${var.env}/bfd-server/http-requests/latency/eob-all-no-resources"
+  name           = "bfd-${var.env}/bfd-server/http-requests/latency/eob-all-no-resources/${each.value.name_suffix}"
   log_group_name = local.log_groups.access
 
   pattern = join("", [
@@ -205,7 +216,7 @@ resource "aws_cloudwatch_log_metric_filter" "http_requests_latency_eob_all_no_re
     name       = "http-requests/latency/eob-all-no-resources"
     namespace  = local.namespace
     value      = "$.mdc.http_access_response_duration_milliseconds"
-    dimensions = each.value
+    dimensions = each.value.dimensions
   }
 }
 
