c2469695c34707add8d4bba34be2911fde98ea5f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 01:42:20 +0000

    Adding tarfile member sanitization to extractall()

--- windows_local_dev/devenv-install.py
+++ windows_local_dev/devenv-install.py
@@ -650,7 +650,26 @@ def extract_archive(archive, destination):
             zip_handle.extractall(destination)
     elif archive.endswith('.tar.gz'):
         with tarfile.open(archive) as tar_handle:
-            tar_handle.extractall(destination)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar_handle, destination)
     else:
         raise Exception('Unsupported archive type: ' + archive)
 
