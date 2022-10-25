b0273d28c475c291174410254209563de2d4de71
Jeremy Rosenfeld <jeremy.rosenfeld@adhocteam.us>
2022-10-25 12:55:21 -0700

    add new pointcuts and glue variables

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/TimerInterceptor.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/TimerInterceptor.java
@@ -3,7 +3,6 @@ package gov.cms.bfd.server.war;
 import ca.uhn.fhir.interceptor.api.Hook;
 import ca.uhn.fhir.interceptor.api.Interceptor;
 import ca.uhn.fhir.interceptor.api.Pointcut;
-import ca.uhn.fhir.rest.server.servlet.ServletRequestDetails;
 import gov.cms.bfd.server.sharedutils.BfdMDC;
 import java.sql.Timestamp;
 
@@ -16,36 +15,75 @@ import java.sql.Timestamp;
 @Interceptor
 public class TimerInterceptor {
 
-  /**
-   * Pointcut to log time when a request is pre-handled.
-   *
-   * @param theRequestDetails the {@link ServletRequestDetails} to get request start time
-   */
-  @Hook(Pointcut.SERVER_INCOMING_REQUEST_PRE_HANDLED)
-  public void requestPreHandled(ServletRequestDetails theRequestDetails) {
-    // Record the time before the BFD Handler is called.
-    Long requestStartMilliseconds =
-        (Long) theRequestDetails.getServletRequest().getAttribute(BfdMDC.REQUEST_START_KEY);
-    if (requestStartMilliseconds != null) {
-      // convert requestStartMilliseconds to timestamp
-      Timestamp requestStart = new Timestamp(requestStartMilliseconds);
-      BfdMDC.put("hapi.server_incoming_request_pre_handle_time", requestStart.toString());
-    }
+  /** Pointcut to log time when a request is pre-processed. */
+  @Hook(Pointcut.SERVER_INCOMING_REQUEST_PRE_PROCESSED)
+  public void requestPreProcessed() {
+    // log current instance to MDC
+    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+    BfdMDC.put("hapi.server_incoming_request_pre_process_time", currentTime.toString());
   }
 
-  /**
-   * Pointcut to log duration between outgoing response and request start.
-   *
-   * @param theRequestDetails the {@link ServletRequestDetails} to get request start time
-   */
+  /** Pointcut to log time when a request is post-processed. */
+  @Hook(Pointcut.SERVER_INCOMING_REQUEST_POST_PROCESSED)
+  public void requestPostProcessed() {
+    // log current instance to MDC
+    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+    BfdMDC.put("hapi.server_incoming_request_post_process_time", currentTime.toString());
+  }
+
+  /** Pointcut to log time when a request is pre-handled. */
+  @Hook(Pointcut.SERVER_INCOMING_REQUEST_PRE_HANDLED)
+  public void requestPreHandled() {
+    // log current instance to MDC
+    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+    BfdMDC.put("hapi.server_incoming_request_pre_handle_time", currentTime.toString());
+  }
+
+  /** Pointcut when request has an outgoing response. */
   @Hook(Pointcut.SERVER_OUTGOING_RESPONSE)
-  public void serverOutgoingResponse(ServletRequestDetails theRequestDetails) {
-    // Record the response duration.
-    Long requestStartMilliseconds =
-        (Long) theRequestDetails.getServletRequest().getAttribute(BfdMDC.REQUEST_START_KEY);
-    if (requestStartMilliseconds != null)
-      BfdMDC.put(
-          "hapi.server_outgoing_response_duration",
-          Long.toString(System.currentTimeMillis() - requestStartMilliseconds));
+  public void serverOutgoingResponse() {
+    // log current instance to MDC
+    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+    BfdMDC.put("hapi.server_outgoing_response_time", currentTime.toString());
+  }
+
+  /** Pointcut when request has completed processing normally */
+  @Hook(Pointcut.SERVER_PROCESSING_COMPLETED_NORMALLY)
+  public void processingCompletedNormally() {
+    // log current instance to MDC
+    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+    BfdMDC.put("hapi.server_processing_completed_normally_time", currentTime.toString());
+  }
+
+  /** Pointcut when request has completed processing */
+  @Hook(Pointcut.SERVER_PROCESSING_COMPLETED)
+  public void serverProcessCompleted() {
+    // log current instance to MDC
+    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+    BfdMDC.put("hapi.server_processing_completed_time", currentTime.toString());
+  }
+
+  /** Pointcut to log time when a request in pre-processing has an outgoing exception. */
+  @Hook(Pointcut.SERVER_PRE_PROCESS_OUTGOING_EXCEPTION)
+  public void preProcessOutgoingException() {
+    // log current instance to MDC
+    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+    BfdMDC.put("hapi.server_pre_process_outgoing_exception_time", currentTime.toString());
+  }
+
+  /** Pointcut to log time when for outgoing failure. */
+  @Hook(Pointcut.SERVER_OUTGOING_FAILURE_OPERATIONOUTCOME)
+  public void serverOutgoingFailureOperationOutcome() {
+    // log current instance to MDC
+    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+    BfdMDC.put("hapi.server_outgoing_failure_operation_outcome_time", currentTime.toString());
+  }
+
+  /** Pointcut to log time when for server handle exception. */
+  @Hook(Pointcut.SERVER_HANDLE_EXCEPTION)
+  public void serverHandleException() {
+    // log current instance to MDC
+    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+    BfdMDC.put("hapi.server_handle_exception_time", currentTime.toString());
   }
 }
