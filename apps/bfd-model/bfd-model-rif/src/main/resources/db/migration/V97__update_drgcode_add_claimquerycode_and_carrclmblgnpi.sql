080979837123b671e45032b88f1606e0d5857886
Chris Brune <cbrune@fearless.tech>
2022-10-28 11:59:39 -0400

    Sql script to modify the size of the claim query code and to add claim query code and carrier claim blg npi fields.

---
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V97__update_drgcode_add_claimquerycode_and_carrclmblgnpi.sql
@@ -0,0 +1,14 @@
+-- This script is to alter the clm_drg_code datatype size from 3 to 4 characters for
+-- the inpatient and snf claims tables, add the claim_query_code to the hha 
+-- and hospice claims tables, and add the carr_clm_blg_npi_num to the carrier claims table.
+
+-- Update DRG Code from 3 to 4 characters
+ALTER TABLE public.inpatient_claims ALTER COLUMN clm_drg_cd TYPE varying(4) COLLATE pg_catalog."default";
+ALTER TABLE public.snf_claims ALTER COLUMN clm_drg_cd TYPE varying(4) COLLATE pg_catalog."default";
+
+-- Add claim query code to the HHA Claims and Hospice Claims table.
+ALTER TABLE public.hha_claims ADD claim_query_code character(1) COLLATE pg_catalog."default";
+ALTER TABLE public.hospice_claims ADD claim_query_code character(1) COLLATE pg_catalog."default";
+
+-- Add carrier claim blg npi number to the Carrier Claims table.
+ALTER TABLE public.carrier_claims ADD carr_clm_blg_npi_num character varying(10) COLLATE pg_catalog."default";
