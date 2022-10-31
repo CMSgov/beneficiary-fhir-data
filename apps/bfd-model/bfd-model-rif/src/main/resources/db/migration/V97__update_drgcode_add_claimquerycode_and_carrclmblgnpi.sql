b778e7dbd872d381f6932af5abb827a91730521b
Chris Brune <cbrune@fearless.tech>
2022-10-31 11:36:39 -0400

    Query changes

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V97__update_drgcode_add_claimquerycode_and_carrclmblgnpi.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V97__update_drgcode_add_claimquerycode_and_carrclmblgnpi.sql
@@ -3,12 +3,12 @@
 -- and hospice claims tables, and add the carr_clm_blg_npi_num to the carrier claims table.
 
 -- Update DRG Code from 3 to 4 characters
-ALTER TABLE public.inpatient_claims ALTER COLUMN clm_drg_cd TYPE varying(4) COLLATE pg_catalog."default";
-ALTER TABLE public.snf_claims ALTER COLUMN clm_drg_cd TYPE varying(4) COLLATE pg_catalog."default";
+${logic.psql-only} ALTER TABLE public.inpatient_claims ALTER COLUMN clm_drg_cd TYPE varying(4) COLLATE pg_catalog."default";
+${logic.psql-only} ALTER TABLE public.snf_claims ALTER COLUMN clm_drg_cd TYPE varying(4) COLLATE pg_catalog."default";
 
 -- Add claim query code to the HHA Claims and Hospice Claims table.
-ALTER TABLE public.hha_claims ADD claim_query_code character(1) COLLATE pg_catalog."default";
-ALTER TABLE public.hospice_claims ADD claim_query_code character(1) COLLATE pg_catalog."default";
+${logic.psql-only} ALTER TABLE public.hha_claims ADD claim_query_code character(1) COLLATE pg_catalog."default";
+${logic.psql-only} ALTER TABLE public.hospice_claims ADD claim_query_code character(1) COLLATE pg_catalog."default";
 
 -- Add carrier claim blg npi number to the Carrier Claims table.
-ALTER TABLE public.carrier_claims ADD carr_clm_blg_npi_num character varying(10) COLLATE pg_catalog."default";
+${logic.psql-only} ALTER TABLE public.carrier_claims ADD carr_clm_blg_npi_num character varying(10) COLLATE pg_catalog."default";
