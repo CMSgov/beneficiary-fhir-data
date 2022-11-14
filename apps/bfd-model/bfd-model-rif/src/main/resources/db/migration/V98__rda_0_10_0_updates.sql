8e8b534095a16d497ea4895e616832d3cd5348c5
Kyle Robbins <83452106+kyrobbins@users.noreply.github.com>
2022-11-14 12:32:19 -0500

    [PACA-825] PR Fixes - Update accesory classes, fix migration script

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V98__rda_0_10_0_updates.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V98__rda_0_10_0_updates.sql
@@ -1,9 +1,8 @@
 -- As part of the 0.10 RDA API update, new raw text fields are added for select date fields and FISS diag_cd_2 is now nullable
 
-alter table rda.fiss_claims
-    add adm_date_cymd_text varchar(10),
-    add stmt_cov_from_cymd_text varchar(10),
-    add stmt_cov_to_cymd_text varchar(10);
+alter table rda.fiss_claims add adm_date_cymd_text varchar(10);
+alter table rda.fiss_claims add stmt_cov_from_cymd_text varchar(10);
+alter table rda.fiss_claims add stmt_cov_to_cymd_text varchar(10);
 
 alter table rda.fiss_diagnosis_codes alter column diag_cd_2 drop not null;
 
