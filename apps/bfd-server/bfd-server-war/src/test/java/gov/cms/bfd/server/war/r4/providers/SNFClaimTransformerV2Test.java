75d8b1e0546eb624b368cbfc6dc1e37d3afc16bf
Jeremy Rosenfeld <jeremy.rosenfeld@adhocteam.us>
2022-12-01 08:30:32 -0800

    remove duplicate tests

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/SNFClaimTransformerV2Test.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/SNFClaimTransformerV2Test.java
@@ -408,54 +408,6 @@ public class SNFClaimTransformerV2Test {
     assertTrue(compare.equalsDeep(sic));
   }
 
-  /** Tests to make sure a four digit DiagnosisRelatedGroupCd exists for snf claims. */
-  @Test
-  public void shouldHaveFourCharacterDRGClmDrgCdSupInfo() throws IOException {
-    List<Object> parsedRecords =
-        ServerTestUtils.parseData(
-            Arrays.asList(StaticRifResourceGroup.SAMPLE_A_FOUR_CHARACTER_DRG_CODE.getResources()));
-
-    SNFClaim claim =
-        parsedRecords.stream()
-            .filter(r -> r instanceof SNFClaim)
-            .map(r -> (SNFClaim) r)
-            .findFirst()
-            .get();
-    ExplanationOfBenefit genEob =
-        SNFClaimTransformerV2.transform(
-            new TransformerContext(
-                new MetricRegistry(),
-                Optional.empty(),
-                FdaDrugCodeDisplayLookup.createDrugCodeLookupForTesting(),
-                NPIOrgLookup.createNpiOrgLookupForTesting()),
-            claim);
-    IParser parser = fhirContext.newJsonParser();
-    String json = parser.encodeResourceToString(genEob);
-    eob = parser.parseResource(ExplanationOfBenefit.class, json);
-    SupportingInformationComponent sic =
-        TransformerTestUtilsV2.findSupportingInfoByCode(
-            "https://bluebutton.cms.gov/resources/variables/clm_drg_cd", eob.getSupportingInfo());
-
-    SupportingInformationComponent compare =
-        TransformerTestUtilsV2.createSupportingInfo(
-            // We don't care what the sequence number is here
-            sic.getSequence(),
-            // Category
-            Arrays.asList(
-                new Coding(
-                    "http://terminology.hl7.org/CodeSystem/claiminformationcategory",
-                    "info",
-                    "Information"),
-                new Coding(
-                    "https://bluebutton.cms.gov/resources/codesystem/information",
-                    "https://bluebutton.cms.gov/resources/variables/clm_drg_cd",
-                    "Claim Diagnosis Related Group Code (or MS-DRG Code)")),
-            // Code
-            new Coding("https://bluebutton.cms.gov/resources/variables/clm_drg_cd", "6455", null));
-
-    assertTrue(compare.equalsDeep(sic));
-  }
-
   @Test
   public void shouldHaveFourCharacterDRGClmDrgCdSupInfo() throws IOException {
     List<Object> parsedRecords =
