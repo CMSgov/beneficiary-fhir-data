2c9627ce8b0884c3da70df12917d258c4b23bbc7
Jeremy Rosenfeld <jeremy.rosenfeld@adhocteam.us>
2022-12-09 15:03:46 -0800

    remove rebase junk code

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V99__add_medicare_coverage_fields_to_beneiciary.sql
+++
@@ -1,9 +0,0 @@
--- This script is to add the Medicare Part A Start and End Dates,
--- Part B Start and End Dates, and Part D Start and End Dates to the Beneficiaries table.
-
-ALTER TABLE public.beneficiaries ADD pta_cvrg_strt_dt date;
-ALTER TABLE public.beneficiaries ADD pta_cvrg_end_dt date;
-ALTER TABLE public.beneficiaries ADD ptb_cvrg_strt_dt date;
-ALTER TABLE public.beneficiaries ADD ptb_cvrg_end_dt date;
-ALTER TABLE public.beneficiaries ADD ptd_cvrg_strt_dt date;
-ALTER TABLE public.beneficiaries ADD ptd_cvrg_end_dt date;
