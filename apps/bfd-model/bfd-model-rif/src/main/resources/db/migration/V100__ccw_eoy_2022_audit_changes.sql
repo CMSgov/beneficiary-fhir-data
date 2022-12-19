a6a20fbb507e37d59b3f97a3cb0d14c5bdc84681
mackec1 <chris.mackenzie@adhocteam.us>
2022-12-19 11:49:16 -0500

    add surrogate key for primary key to handle idempotent

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V100__ccw_eoy_2022_audit_changes.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V100__ccw_eoy_2022_audit_changes.sql
@@ -6,6 +6,11 @@
 ${logic.psql-only} DROP TABLE IF EXISTS public.beneficiary_monthly_audit;
 ${logic.psql-only} DROP TRIGGER IF EXISTS audit_ccw_update ON beneficiary_monthly;
 
+-- The sequence used for the `skipped_rif_records.record_id` column.
+CREATE SEQUENCE bene_monthly_audit_seq
+  AS bigint ${logic.sequence-start} 1 ${logic.sequence-increment} 1
+  NO CYCLE;
+
 -- Create beneficiary_monthly_audit table; we'll create the table in both HSQL & PSQL
 -- but we don't bother with setting up the trigger in HSQL as this will only be useful
 -- during that timeframe when we are actively vetting CCW end-of-year/new year data.
@@ -28,18 +33,24 @@ CREATE TABLE public.beneficiary_monthly_audit(
   partc_plan_type_code                   varchar(3),
   operation                              char(1) NOT NULL,
   update_ts                              timestamp NOT NULL,
- CONSTRAINT beneficiary_monthly_audit_pkey PRIMARY KEY (bene_id, year_month));
+  seq_id                                 bigint NOT NULL,
+ CONSTRAINT beneficiary_monthly_audit_pkey PRIMARY KEY (seq_id));
 
+CREATE INDEX beneficiary_monthly_audit_bene_id_year_month_idx
+    on beneficiary_monthly_audit (bene_id, year_month);
 --
 -- Create function that tracks UPDATE operations to the beneficiary_monthly table.
--- This will create a row in beneficiary_monthly_audit to reflect the update operation performed
--- on beneficiary_monthly; variable TG_OP denotes the operation.}
+-- This will create a row in beneficiary_monthly_audit to reflect the update operation
+-- performed on beneficiary_monthly; psql variable TG_OP denotes the operation.}
 --
 ${logic.psql-only} CREATE OR REPLACE FUNCTION public.track_bene_monthly_change() RETURNS TRIGGER
 ${logic.psql-only} AS $beneficiary_monthly_audit$
 ${logic.psql-only}     BEGIN
-${logic.psql-only}         INSERT INTO public.beneficiary_monthly_audit VALUES (OLD.*, 'U', now());
-${logic.psql-only}         RETURN NEW;
+${logic.psql-only}        IF (TG_OP = 'UPDATE') THEN
+${logic.psql-only}            INSERT INTO public.beneficiary_monthly_audit VALUES (OLD.*, 'U', now(), nextval('public.bene_monthly_audit_seq'));
+${logic.psql-only}            RETURN NEW;
+${logic.psql-only}        END IF;
+${logic.psql-only}        RETURN NULL; -- result is ignored since this is an AFTER trigger
 ${logic.psql-only}     END;
 ${logic.psql-only} $beneficiary_monthly_audit$ LANGUAGE plpgsql;
 
