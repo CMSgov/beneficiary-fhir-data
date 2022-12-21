ed93d2ccb2e5dcc1466bcf6e546fb73a479ce549
Mitch Alessio <mitch.alessio@adhocteam.us>
2022-12-21 13:48:26 -0500

    Enable future claims splitting when future_months is specified

--- ops/ccs-ops-misc/synthetic-data/scripts/synthea-automation/docker/entrypoint.sh
+++ ops/ccs-ops-misc/synthetic-data/scripts/synthea-automation/docker/entrypoint.sh
@@ -70,17 +70,31 @@ echo "Preparing to run Synthea generation..."
 current_datetime=$(date '+%F_%H:%M:%S')
 
 echo "Running Synthea generation with $num_generated_benes benes and $num_future_months future months..."
-echo "View the Synthea generation log in real-time by tailing the synthea.latest.log in the bind-mounted logs directory"
+echo "View logs in real-time by tailing *.latest.log logs in the bind-mounted logs directory"
 {
   python3 prepare-and-run-synthea.py \
     "${BFD_END_STATE_PROPERTIES}" \
     "${TARGET_SYNTHEA_DIR}" \
     "${num_generated_benes}" \
-    "${num_future_months}" &>"$TARGET_SYNTHEA_DIR/logs/prepare_and_run_synthea-$current_datetime.log" &&
-    echo "Synthea generation finished, synthetic data should be available in the bind mounted output directory"
+    "${num_future_months}" &>"$TARGET_SYNTHEA_DIR/logs/prepare_and_run_synthea.latest.log" &&
+  echo "Synthea generation finished, synthetic data should be available in the bind mounted output directory"
 } || {
   echo "Synthea generation failed to complete. View the logs in the bind-mounted logs directory for more information"
 }
-echo "Renaming synthea.latest.log to synthea-$current_datetime.log..."
+
+if [ "$generate_future" == 'true' ]; then
+  echo "Generating future months was specified, splitting future claims to allow for proper loading..."
+  {
+    python3 split-future-claims.py "$TARGET_SYNTHEA_DIR" \
+      &>"$TARGET_SYNTHEA_DIR/logs/split_future_claims.latest.log" &&
+    echo "Future claims splitting succeeded, view the split_future_claims-$current_datetime.log for more information"
+  } || {
+    echo "Future claims splitting failed, view the split_future_claims-$current_datetime.log for more information"
+  }
+fi
+
+echo "Renaming *.latest.log logs to *$current_datetime.log..."
 mv "$TARGET_SYNTHEA_DIR/logs/synthea.latest.log" "$TARGET_SYNTHEA_DIR/logs/synthea-$current_datetime.log"
+mv "$TARGET_SYNTHEA_DIR/logs/prepare_and_run_synthea.latest.log" "$TARGET_SYNTHEA_DIR/logs/prepare_and_run_synthea-$current_datetime.log"
+mv "$TARGET_SYNTHEA_DIR/logs/split_future_claims.latest.log" "$TARGET_SYNTHEA_DIR/logs/split_future_claims-$current_datetime.log"
 mv "$TARGET_SYNTHEA_DIR"/synthea-*.log "$TARGET_SYNTHEA_DIR/logs/national_script-$current_datetime.log"
