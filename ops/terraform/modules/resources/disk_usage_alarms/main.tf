b80a9f76c84067c60807ae558072d2f4c861692b
Mitch Alessio <mitch.alessio@adhocteam.us>
2023-01-25 11:21:50 -0500

    Re-format multi-line JSON in IAM policies; add EventBridge rule, target and permission

--- ops/terraform/modules/resources/disk_usage_alarms/main.tf
+++ ops/terraform/modules/resources/disk_usage_alarms/main.tf
@@ -5,29 +5,60 @@ locals {
   lambda_name            = "manage-disk-usage-alarms"
 }
 
+resource "aws_cloudwatch_event_rule" "autoscaling_instance_launch_terminate" {
+  name = "bfd-${var.env}-autoscaling-instance-launch-terminate"
+  description = "Filters for bfd-server EC2 instance launches and terminations in ${var.env} ASG"
+  event_pattern = <<-EOF
+{
+  "source": ["aws.autoscaling"],
+  "detail-type": [
+    "EC2 Instance Launch Successful",
+    "EC2 Instance Terminate Successful"
+  ],
+  "detail": {
+    "AutoScalingGroupName": [
+      {
+        "prefix": "bfd-${var.env}-fhir"
+      }
+    ]
+  }
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_target" "invoke_lambda_from_autoscaling_event" {
+  arn = aws_lambda_function.this.arn
+  rule = aws_cloudwatch_event_rule.autoscaling_instance_launch_terminate.name
+}
+
+resource "aws_lambda_permission" "allow_eventbridge_to_invoke_lambda" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.autoscaling_instance_launch_terminate.arn
+}
+
 resource "aws_iam_policy" "logs" {
   name        = "bfd-${var.env}-${local.lambda_name}-logs"
   description = "Permissions to create and write to bfd-${var.env}-${local.lambda_name} logs"
   policy      = <<-EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "logs:CreateLogGroup",
-            "Resource": "arn:aws:logs:us-east-1:${local.account_id}:*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": [
-                "arn:aws:logs:us-east-1:${local.account_id}:log-group:/aws/lambda/bfd-${var.env}-${local.lambda_name}:*"
-            ]
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "logs:CreateLogGroup",
+      "Resource": "arn:aws:logs:us-east-1:${local.account_id}:*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": ["logs:CreateLogStream", "logs:PutLogEvents"],
+      "Resource": [
+        "arn:aws:logs:us-east-1:${local.account_id}:log-group:/aws/lambda/bfd-${var.env}-${local.lambda_name}:*"
+      ]
+    }
+  ]
 }
 EOF
 }
@@ -39,16 +70,16 @@ resource "aws_iam_role" "this" {
 
   assume_role_policy = <<-EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "lambda.amazonaws.com"
-            }
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      }
+    }
+  ]
 }
 EOF
 
