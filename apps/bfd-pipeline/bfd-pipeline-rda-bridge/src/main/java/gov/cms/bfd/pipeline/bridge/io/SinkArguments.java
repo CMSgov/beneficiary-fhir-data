584c790aedaaac1188508badd576b65d19b92cbe
Chris Brune <cbrune@fearless.tech>
2023-02-09 16:59:42 -0500

    PR comments

--- apps/bfd-pipeline/bfd-pipeline-rda-bridge/src/main/java/gov/cms/bfd/pipeline/bridge/io/SinkArguments.java
+++ apps/bfd-pipeline/bfd-pipeline-rda-bridge/src/main/java/gov/cms/bfd/pipeline/bridge/io/SinkArguments.java
@@ -9,6 +9,6 @@ import lombok.Data;
 public class SinkArguments {
   /** Output Path is the path of the associated Fiss or MCS file. */
   private final Path outputPath;
-  /** this field is what keeps track of the processed sequence numbers of the consumed files. */
+  /** This field is what keeps track of the processed sequence numbers of the consumed files. */
   private final WrappedCounter sequenceCounter;
 }
