4e42c092fd4e7a4e6f79f936937838ff666c8064
Chris Brune <64486927+cbrunefearless@users.noreply.github.com>
2023-02-10 13:07:38 -0500

    Update apps/bfd-pipeline/bfd-pipeline-rda-bridge/src/test/java/gov/cms/bfd/pipeline/bridge/etl/FissTransformerIT.java
    Co-authored-by: Logan Mitchell <logan.mitchell@adhocteam.us>

--- apps/bfd-pipeline/bfd-pipeline-rda-bridge/src/test/java/gov/cms/bfd/pipeline/bridge/etl/FissTransformerIT.java
+++ apps/bfd-pipeline/bfd-pipeline-rda-bridge/src/test/java/gov/cms/bfd/pipeline/bridge/etl/FissTransformerIT.java
@@ -90,7 +90,7 @@ public class FissTransformerIT {
   }
 
   /**
-   * Happy path test to produce expected claims and data samples.
+   * Happy path test to ensure for various scenarios, claim responses are generated as expected and certain pieces of data are set correctly when passed through the {@link FissTransformer}.
    *
    * @param testName for the test name
    * @param arguments for the arguments of the test
