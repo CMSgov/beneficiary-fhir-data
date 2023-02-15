0f0039add62590558f27dbc2b201d65418adfade
Michael J Burling <michael.burling@adhocteam.us>
2023-02-15 12:31:41 -0600

    BFD-1872: Address unknown SQL Queries (#1589)

--- ops/terraform/modules/resources/bfd_server_log_alarms/main.tf
+++ ops/terraform/modules/resources/bfd_server_log_alarms/main.tf
@@ -1,8 +1,7 @@
 locals {
-  is_prod = var.env == "prod"
-
-  alarm_arn   = var.alarm_notification_arn == null ? [] : [var.alarm_notification_arn]
-  ok_arn      = var.ok_notification_arn == null ? [] : [var.ok_notification_arn]
+  alarm_arn        = var.alarm_notification_arn == null ? [] : [var.alarm_notification_arn]
+  notification_arn = var.alarm_notification_arn == null ? [] : [var.alarm_notification_arn]
+  ok_arn           = var.ok_notification_arn == null ? [] : [var.ok_notification_arn]
 
   server_log_availability = {
     period       = 1 * 60 * 60 # 1 hour 
@@ -36,5 +35,37 @@ resource "aws_cloudwatch_metric_alarm" "server-log-availability-1hr" {
   ok_actions    = local.ok_arn
 
   datapoints_to_alarm = local.server_log_availability.datapoints
+  treat_missing_data  = "notBreaching"
+}
+
+# NOTE: alarm is triggered when gov.cms.bfd.server.war QueryLoggingListener encounters an
+#       unknown query, signaling that the application is missing a required pattern.
+resource "aws_cloudwatch_metric_alarm" "server-query-logging-listener-warning" {
+  alarm_name          = "bfd-${var.env}-server-query-logging-listener-warning"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  period              = 1 * 60 # 1 Minute
+  statistic           = "Sum"
+  threshold           = 1
+
+  alarm_description = join("", [
+    "BFD Server QueryLoggingListener has encountered an unknown query ",
+    "in APP-ENV: bfd-${var.env}"
+  ])
+
+  metric_name = "query-logging-listener/count/warning"
+  namespace   = "bfd-${var.env}/bfd-server"
+
+  dimensions = {
+    LogGroupName = "/bfd/${var.env}/bfd-server/messages.json"
+  }
+
+  # NOTE: alarm should always be a low severity notification
+  alarm_actions = local.notification_arn
+
+  # NOTE: alarm has no meaningful transition from 'In alarm' to 'OK'
+  ok_actions = []
+
+  datapoints_to_alarm = 1
   treat_missing_data  = "notBreaching"
 }
