09f449d1a5298b16dbddc354fdfdba638a31f536
Mitch Alessio <mitch.alessio@adhocteam.us>
2023-02-16 11:57:13 -0500

    Determine SNS topics based upon environment for log alarms module

--- ops/terraform/modules/resources/bfd_server_log_alarms/main.tf
+++ ops/terraform/modules/resources/bfd_server_log_alarms/main.tf
@@ -1,7 +1,36 @@
 locals {
-  alarm_arn        = var.alarm_notification_arn == null ? [] : [var.alarm_notification_arn]
-  notification_arn = var.alarm_notification_arn == null ? [] : [var.alarm_notification_arn]
-  ok_arn           = var.ok_notification_arn == null ? [] : [var.ok_notification_arn]
+  victor_ops_sns         = "bfd-${var.env}-cloudwatch-alarms"
+  bfd_test_slack_sns     = "bfd-${var.env}-cloudwatch-alarms-slack-bfd-notices"
+  bfd_warnings_slack_sns = "bfd-${var.env}-cloudwatch-alarms-slack-bfd-notices"
+  default_ok_sns         = "bfd-${var.env}-cloudwatch-ok"
+  topic_names_by_env = {
+    prod = {
+      alert  = local.victor_ops_sns
+      notify = local.bfd_warnings_slack_sns
+      ok     = local.default_ok_sns
+    }
+    prod-sbx = {
+      alert  = local.victor_ops_sns
+      notify = local.bfd_warnings_slack_sns
+      ok     = null
+    }
+    test = {
+      alert  = null # test is expected to not always be running, so alerts could be false positives
+      notify = local.bfd_test_slack_sns
+      ok     = null
+    }
+  }
+  env_sns = lookup(local.topic_names_by_env, var.env, {
+    alert  = null
+    notify = null
+    ok     = null
+  })
+  alert_sns_name  = try(coalesce(var.alert_sns_override, local.env_sns.alert), null)
+  notify_sns_name = try(coalesce(var.notify_sns_override, local.env_sns.notify), null)
+  ok_sns_name     = try(coalesce(var.ok_sns_override, local.env_sns.ok), null)
+  alert_arn       = try([data.aws_sns_topic.alert_sns[0].arn], [])
+  notify_arn      = try([data.aws_sns_topic.notify_sns[0].arn], [])
+  ok_arn          = try([data.aws_sns_topic.ok_sns[0].arn], [])
 
   server_log_availability = {
     period       = 1 * 60 * 60 # 1 hour 
@@ -31,7 +60,7 @@ resource "aws_cloudwatch_metric_alarm" "server-log-availability-1hr" {
     LogGroupName = "/bfd/${var.env}/bfd-server/access.json"
   }
 
-  alarm_actions = local.alarm_arn
+  alarm_actions = local.alert_arn
   ok_actions    = local.ok_arn
 
   datapoints_to_alarm = local.server_log_availability.datapoints
@@ -61,7 +90,7 @@ resource "aws_cloudwatch_metric_alarm" "server-query-logging-listener-warning" {
   }
 
   # NOTE: alarm should always be a low severity notification
-  alarm_actions = local.notification_arn
+  alarm_actions = local.notify_arn
 
   # NOTE: alarm has no meaningful transition from 'In alarm' to 'OK'
   ok_actions = []
