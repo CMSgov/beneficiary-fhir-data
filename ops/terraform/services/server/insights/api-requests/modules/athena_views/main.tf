90bc21e8c1a15f291c6fc4f3a24c39433219cb9c
Mitch Alessio <mitch.alessio@adhocteam.us>
2023-02-21 09:55:00 -0500

    Initial implementation of submodule for creating athena views via Terraform

---
+++ ops/terraform/services/server/insights/api-requests/modules/athena_views/main.tf
@@ -0,0 +1,40 @@
+locals {
+  sql_views_dir = "${path.module}/sql_views"
+  view_to_filepath = {
+    for filename in fileset("${local.sql_views_dir}/", "**/*.sql") :
+    replace(replace(filename, "/", "_"), ".sql", "") => "${local.sql_views_dir}/${filename}"
+  }
+}
+
+resource "null_resource" "athena_views" {
+  for_each = local.view_to_filepath
+
+  triggers = {
+    md5 = filemd5(each.value)
+
+    # External references from destroy provisioners are not allowed -
+    # they may only reference attributes of the related resource.
+    database_name = var.database_name
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOF
+aws athena start-query-execution \
+  --output json \
+  --query-string file://${each.value} \
+  --query-execution-context "Database=${var.database_name}" \
+  --work-group "bfd"
+EOF
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<-EOF
+aws athena start-query-execution \
+  --output json \
+  --query-string 'DROP VIEW IF EXISTS ${each.key}' \
+  --query-execution-context "Database=${self.triggers.database_name}" \
+  --work-group "bfd"
+EOF
+  }
+}
---
+++ ops/terraform/services/server/insights/api-requests/modules/athena_views/variables.tf
@@ -0,0 +1,4 @@
+variable "database_name" {
+  description = "The name of the BFD Insights database that the athena views will be applied to"
+  type        = string
+}
