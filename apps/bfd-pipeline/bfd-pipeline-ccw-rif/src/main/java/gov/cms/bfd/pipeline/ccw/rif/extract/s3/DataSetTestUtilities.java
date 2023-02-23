9c0c0642fc02aadd124a11c212efb72108505df3
Billie Rose <billiexrose@gmail.com>
2023-02-23 12:43:06 -0600

    Update DataSetTestUtilities.java

--- apps/bfd-pipeline/bfd-pipeline-ccw-rif/src/main/java/gov/cms/bfd/pipeline/ccw/rif/extract/s3/DataSetTestUtilities.java
+++ apps/bfd-pipeline/bfd-pipeline-ccw-rif/src/main/java/gov/cms/bfd/pipeline/ccw/rif/extract/s3/DataSetTestUtilities.java
@@ -14,12 +14,15 @@ import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import software.amazon.awssdk.services.s3.AmazonS3;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.Bucket;
-import software.amazon.awssdk.services.s3.model.ObjectMetadata;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 /**
@@ -32,66 +35,25 @@ public class DataSetTestUtilities {
   /**
    * Creates an S3 test bucket.
    *
-   * @param s3Client the {@link AmazonS3} client to use
-   * @return a new, random {@link Bucket} for use in an integration test
+   * @param s3Client the {@link S3Client} client to use
+   * @return the bucket name of the new, random {@link Bucket} for use in an integration test
    */
-  public static Bucket createTestBucket(AmazonS3 s3Client) {
+  public static String createTestBucket(S3Client s3Client) {
     return SharedS3Utilities.createTestBucket(s3Client);
   }
 
   /**
    * Deletes the specified {@link Bucket} and all objects in it.
    *
-   * @param s3Client the {@link AmazonS3} client to use
-   * @param bucket the {@link Bucket} to empty and delete
+   * @param s3Client the {@link S3Client} client to use
+   * @param bucket the name of the bucket to empty and delete
    */
-  public static void deleteObjectsAndBucket(AmazonS3 s3Client, Bucket bucket) {
+  public static void deleteObjectsAndBucket(S3Client s3Client, String bucket) {
     SharedS3Utilities.deleteTestBucket(s3Client, bucket);
   }
 
-  /**
-   * Creates a put request for the specified S3 bucket.
-   *
-   * @param bucket the {@link Bucket} to place the new object in
-   * @param manifest the {@link DataSetManifest} to push as an object
-   * @return a {@link PutObjectRequest} for the specified {@link DataSetManifest}
-   */
-  public static PutObjectRequest createPutRequest(Bucket bucket, DataSetManifest manifest) {
-    String keyPrefix =
-        String.format(
-            "%s/%s", CcwRifLoadJob.S3_PREFIX_PENDING_DATA_SETS, manifest.getTimestampText());
-    return createPutRequest(bucket, keyPrefix, manifest);
-  }
-
-  /**
-   * Create put request within a given bucket for a manifest at a keyed location.
-   *
-   * @param bucket the {@link Bucket} to place the new object in
-   * @param manifest the {@link DataSetManifest} to push as an object
-   * @param location the location to store the manifest, should be {@link
-   *     CcwRifLoadJob#S3_PREFIX_PENDING_DATA_SETS} or {@link
-   *     CcwRifLoadJob#S3_PREFIX_PENDING_SYNTHETIC_DATA_SETS}
-   * @return a {@link PutObjectRequest} for the specified {@link DataSetManifest}
-   */
-  public static PutObjectRequest createPutRequest(
-      Bucket bucket, DataSetManifest manifest, String location) {
-    String keyPrefix = String.format("%s/%s", location, manifest.getTimestampText());
-    return createPutRequest(bucket, keyPrefix, manifest);
-  }
-
-  /**
-   * Creates a put request for the specified S3 bucket.
-   *
-   * @param bucket the {@link Bucket} to place the new object in
-   * @param keyPrefix the S3 key prefix to store the new object under
-   * @param manifest the {@link DataSetManifest} to push as an object
-   * @return a {@link PutObjectRequest} for the specified {@link DataSetManifest}
-   */
-  public static PutObjectRequest createPutRequest(
-      Bucket bucket, String keyPrefix, DataSetManifest manifest) {
-    String objectKey =
-        String.format("%s/%d_%s", keyPrefix, manifest.getSequenceId(), "manifest.xml");
-
+  private static void putObjectHelper(
+      S3Client s3Client, PutObjectRequest putObjectRequest, DataSetManifest manifest) {
     try {
       // Serialize the manifest to a byte array.
       JAXBContext jaxbContext = JAXBContext.newInstance(DataSetManifest.class);
@@ -102,34 +64,73 @@ public class DataSetTestUtilities {
       byte[] manifestByteArray = manifestOutputStream.toByteArray();
       InputStream manifestInputStream = new ByteArrayInputStream(manifestByteArray);
 
-      // If this isn't specified, the AWS API logs annoying warnings.
-      ObjectMetadata manifestMetadata = new ObjectMetadata();
-      manifestMetadata.setContentLength(manifestByteArray.length);
-
-      PutObjectRequest request =
-          new PutObjectRequest(bucket.getName(), objectKey, manifestInputStream, manifestMetadata);
-      return request;
+      s3Client.putObject(
+          putObjectRequest,
+          RequestBody.fromInputStream(manifestInputStream, Long.valueOf(manifestByteArray.length)));
     } catch (JAXBException e) {
       throw new RuntimeException(e);
     }
   }
 
   /**
+   * Creates a put request for the specified S3 bucket.
+   *
+   * @param bucket the name of the bucket to place the new object in
+   * @param manifest the {@link DataSetManifest} to push as an object
+   * @return a {@link PutObjectRequest} for the specified {@link DataSetManifest}
+   */
+  public static void putObject(S3Client s3Client, String bucket, DataSetManifest manifest) {
+    String keyPrefix =
+        String.format(
+            "%s/%s", CcwRifLoadJob.S3_PREFIX_PENDING_DATA_SETS, manifest.getTimestampText());
+    putObject(s3Client, bucket, keyPrefix, manifest);
+  }
+
+  /**
+   * Create put request within a given bucket for a manifest at a keyed location.
+   *
+   * @param bucket the name of the bucket to place the new object in
+   * @param manifest the {@link DataSetManifest} to push as an object
+   * @param location the location to store the manifest, should be {@link
+   *     CcwRifLoadJob#S3_PREFIX_PENDING_DATA_SETS} or {@link
+   *     CcwRifLoadJob#S3_PREFIX_PENDING_SYNTHETIC_DATA_SETS}
+   * @return a {@link PutObjectRequest} for the specified {@link DataSetManifest}
+   */
+  public static void putObject(
+      S3Client s3Client, String bucket, DataSetManifest manifest, String location) {
+    String keyPrefix = String.format("%s/%s", location, manifest.getTimestampText());
+    putObject(s3Client, bucket, keyPrefix, manifest);
+  }
+
+  public static void putObject(
+      S3Client s3Client, String bucket, String keyPrefix, DataSetManifest manifest) {
+    String objectKey =
+        String.format("%s/%d_%s", keyPrefix, manifest.getSequenceId(), "manifest.xml");
+
+    PutObjectRequest putObjectRequest =
+        PutObjectRequest.builder().bucket(bucket).key(objectKey).build();
+
+    putObjectHelper(s3Client, putObjectRequest, manifest);
+  }
+
+  /**
    * Creates a put request, placing the items within the {@link
    * CcwRifLoadJob#S3_PREFIX_PENDING_DATA_SETS} key inside the given bucket.
    *
-   * @param bucket the {@link Bucket} to place the new object in
+   * @param bucket the name of the bucket to place the new object in
    * @param manifest the {@link DataSetManifest} to create an object for
    * @param manifestEntry the {@link DataSetManifestEntry} to create an object for
    * @param objectContentsUrl a {@link URL} to the data to push as the new object's content
    * @return a {@link PutObjectRequest} for the specified content
    */
-  public static PutObjectRequest createPutRequest(
-      Bucket bucket,
+  public static void putObject(
+      S3Client s3Client,
+      String bucket,
       DataSetManifest manifest,
       DataSetManifestEntry manifestEntry,
       URL objectContentsUrl) {
-    return createPutRequest(
+    putObject(
+        s3Client,
         bucket,
         manifest,
         manifestEntry,
@@ -140,7 +141,7 @@ public class DataSetTestUtilities {
   /**
    * Creates a put request, placing the items within the location key inside the given bucket.
    *
-   * @param bucket the {@link Bucket} to place the new object in
+   * @param bucket the name of the bucket to place the new object in
    * @param manifest the {@link DataSetManifest} to create an object for
    * @param manifestEntry the {@link DataSetManifestEntry} to create an object for
    * @param objectContentsUrl a {@link URL} to the data to push as the new object's content
@@ -149,28 +150,30 @@ public class DataSetTestUtilities {
    *     CcwRifLoadJob#S3_PREFIX_PENDING_SYNTHETIC_DATA_SETS}
    * @return a {@link PutObjectRequest} for the specified content
    */
-  public static PutObjectRequest createPutRequest(
-      Bucket bucket,
+  public static void putObject(
+      S3Client s3Client,
+      String bucket,
       DataSetManifest manifest,
       DataSetManifestEntry manifestEntry,
       URL objectContentsUrl,
       String incomingLocation) {
     String keyPrefix = String.format("%s/%s", incomingLocation, manifest.getTimestampText());
-    return createPutRequest(bucket, keyPrefix, manifest, manifestEntry, objectContentsUrl);
+    putObject(s3Client, bucket, keyPrefix, manifest, manifestEntry, objectContentsUrl);
   }
 
   /**
    * Creates a put request for the specified S3 bucket.
    *
-   * @param bucket the {@link Bucket} to place the new object in
+   * @param bucket the name of the bucket to place the new object in
    * @param keyPrefix the S3 key prefix to store the new object under
    * @param manifest the {@link DataSetManifest} to create an object for
    * @param manifestEntry the {@link DataSetManifestEntry} to create an object for
    * @param objectContentsUrl a {@link URL} to the data to push as the new object's content
    * @return a {@link PutObjectRequest} for the specified content
    */
-  public static PutObjectRequest createPutRequest(
-      Bucket bucket,
+  public static void putObject(
+      S3Client s3Client,
+      String bucket,
       String keyPrefix,
       DataSetManifest manifest,
       DataSetManifestEntry manifestEntry,
@@ -179,26 +182,19 @@ public class DataSetTestUtilities {
 
     try {
       // If this isn't specified, the AWS API logs annoying warnings.
-      int objectContentLength = objectContentsUrl.openConnection().getContentLength();
-      ObjectMetadata objectMetadata = new ObjectMetadata();
-      objectMetadata.setContentLength(objectContentLength);
+      Map<String, String> metadata = new HashMap<>();
+      metadata.put("x-amz-meta-myVal", "test");
 
-      // create md5chksum on file to be uploaded
-      objectMetadata.addUserMetadata(
-          "md5chksum", ManifestEntryDownloadTask.computeMD5ChkSum(objectContentsUrl.openStream()));
+      PutObjectRequest putObjectRequest =
+          PutObjectRequest.builder()
+              .bucket(bucket)
+              .key(objectKey)
+              .metadata(metadata)
+              .contentMD5(
+                  ManifestEntryDownloadTask.computeMD5ChkSum(objectContentsUrl.openStream()))
+              .build();
 
-      PutObjectRequest request =
-          new PutObjectRequest(
-              bucket.getName(), objectKey, objectContentsUrl.openStream(), objectMetadata);
-
-      /*
-       * Per https://github.com/aws/aws-sdk-java/issues/427, this is
-       * required when PUTing objects from an InputStream (as opposed to a
-       * File). Without it, was seeing intermittent errors.
-       */
-      request.getRequestClientOptions().setReadLimit(objectContentLength + 1);
-
-      return request;
+      putObjectHelper(s3Client, putObjectRequest, manifest);
     } catch (IOException e) {
       throw new UncheckedIOException(e);
     } catch (NoSuchAlgorithmException e) {
@@ -217,16 +213,16 @@ public class DataSetTestUtilities {
    * <p>This is needed because Amazon's S3 API is only <em>eventually</em> consistent for deletes,
    * per <a href="https://aws.amazon.com/s3/faqs/">Amazon S3 FAQs</a>.
    *
-   * @param s3Client the {@link AmazonS3} client to use
-   * @param bucket the {@link Bucket} to check
+   * @param s3Client the {@link S3Client} client to use
+   * @param bucket the name of the bucket to check
    * @param keyPrefix the S3 object key prefix of the objects to include in the count
    * @param expectedObjectCount the number of objects that should be in the specified {@link Bucket}
    * @param waitDuration the length of time to wait for the condition to be met before throwing an
    *     error
    */
   public static void waitForBucketObjectCount(
-      AmazonS3 s3Client,
-      Bucket bucket,
+      S3Client s3Client,
+      String bucket,
       String keyPrefix,
       int expectedObjectCount,
       Duration waitDuration) {
@@ -234,11 +230,12 @@ public class DataSetTestUtilities {
 
     int actualObjectCount = -1;
     while (Instant.now().isBefore(endTime)) {
-      actualObjectCount =
-          s3Client
-              .listObjects(bucket.getName(), String.format("%s/", keyPrefix))
-              .getObjectSummaries()
-              .size();
+      ListObjectsRequest listObjectsRequest =
+          ListObjectsRequest.builder()
+              .bucket(bucket)
+              .prefix(String.format("%s/", keyPrefix))
+              .build();
+      actualObjectCount = s3Client.listObjects(listObjectsRequest).contents().size();
       if (expectedObjectCount == actualObjectCount) return;
 
       try {
