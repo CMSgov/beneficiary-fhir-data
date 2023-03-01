1fca08c6cd0e3af85920398a0a15cec7f64616a9
Mitch Alessio <mitch.alessio@adhocteam.us>
2023-03-01 16:46:41 -0500

    Use bash's "process substitution" operator to treat the STDOUT of AWS CLI as temporary file when decrypting ansible vault files; remove unnecessary get-vault-password script

--- ops/terraform/env/mgmt/base_config/scripts/edit-eyaml.sh
+++ ops/terraform/env/mgmt/base_config/scripts/edit-eyaml.sh
@@ -9,6 +9,7 @@
 #   SCRIPTS_DIR the parent directory of this script
 #   MODULE_DIR the parent directory of SCRIPTS_DIR, assumed to be the "base_config" module
 #   EYAML_FILE formatted "$1" positional argument for relative file resolution
+#   SSM_VAULT_PASSWORD_PATH SSM Hierarchy containing the ansible vault password
 #
 # Arguments:
 #   $1 is the desired eyaml file that can be entered with or without qualifying path or
@@ -16,8 +17,15 @@
 SCRIPTS_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
 MODULE_DIR="$(dirname "$SCRIPTS_DIR")"
 EYAML_FILE="$(basename "$1" | sed 's/\.eyaml//').eyaml"
+SSM_VAULT_PASSWORD_PATH="/bfd/mgmt/jenkins/sensitive/ansible_vault_password"
 
 # open a desired eyaml file from a temporary, decrypted, user-owned location on disk
 # in the user's desired `$EDITOR`
-ansible-vault edit "--vault-password-file=$SCRIPTS_DIR/get-vault-password.sh" \
-  "$MODULE_DIR/values/${EYAML_FILE}"
+ansible-vault edit --vault-password-file=<(
+  aws ssm get-parameter \
+    --region us-east-1 \
+    --output text \
+    --with-decryption \
+    --query 'Parameter.Value' \
+    --name "$SSM_VAULT_PASSWORD_PATH"
+) "$MODULE_DIR/values/${EYAML_FILE}"
--- ops/terraform/env/mgmt/base_config/scripts/read-and-decrypt-eyaml.sh
+++ ops/terraform/env/mgmt/base_config/scripts/read-and-decrypt-eyaml.sh
@@ -11,6 +11,7 @@
 #   SCRIPTS_DIR the parent directory of this script
 #   MODULE_DIR the parent directory of SCRIPTS_DIR, assumed to be the "base_config" module
 #   EYAML_FILE formatted "$1" positional argument for relative file resolution
+#   SSM_VAULT_PASSWORD_PATH SSM Hierarchy containing the ansible vault password
 #
 # Arguments:
 #   $1 is the desired eyaml file that can be entered with or without qualifying path or
@@ -18,9 +19,16 @@
 SCRIPTS_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
 MODULE_DIR="$(dirname "$SCRIPTS_DIR")"
 EYAML_FILE="$(basename "$1" | sed 's/\.eyaml//').eyaml"
+SSM_VAULT_PASSWORD_PATH="/bfd/mgmt/jenkins/sensitive/ansible_vault_password"
 
 # decrypt desired eyaml file to stdout, reformat as JSON, and explicitly cast all values to strings
-ansible-vault decrypt "--vault-password-file=$SCRIPTS_DIR/get-vault-password.sh" \
-  "$MODULE_DIR/values/${EYAML_FILE}" --output - |
+ansible-vault decrypt --vault-password-file=<(
+  aws ssm get-parameter \
+    --region us-east-1 \
+    --output text \
+    --with-decryption \
+    --query 'Parameter.Value' \
+    --name "$SSM_VAULT_PASSWORD_PATH"
+) "$MODULE_DIR/values/${EYAML_FILE}" --output - |
   yq eval -o=j |
   jq 'with_entries(.value |= tostring)'
