ccaac3343195f09cd3d29fd68a0893e8eca2f4f8
Mitch Alessio <mitch.alessio@adhocteam.us>
2023-03-03 13:12:20 -0500

    Fix invalid IAM policy by adding missing Effect key

--- ops/terraform/env/mgmt/iam.tf
+++ ops/terraform/env/mgmt/iam.tf
@@ -68,6 +68,7 @@ resource "aws_iam_policy" "packer_ssm" {
         "ssm:GetParameters",
         "ssm:GetParameter"
       ],
+      "Effect": "Allow",
       "Resource": [
         %{ for env in local.established_envs ~}
         "arn:aws:ssm:us-east-1:${local.account_id}:parameter/bfd/${env}/common/*",
