fbfc8ab1f9920e535feed5f770618021612d4e82
Brian Burton <brian.burton@adhocteam.us>
2023-03-06 10:32:49 -0500

    id_hashes allows same id with multiple hashes.

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V105__add_id_hashes.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V105__add_id_hashes.sql
@@ -1,6 +1,9 @@
+-- hash is the primary key but we usually search for the plain id.
 CREATE TABLE id_hashes (
     id varchar(80) NOT NULL,
     hash varchar(64) NOT NULL,
-    CONSTRAINT id_hashes_key PRIMARY KEY (id)
+    CONSTRAINT id_hashes_key PRIMARY KEY (hash)
 );
 
+-- non unique index although generally there will only be one record per id.
+CREATE INDEX id_hashes_id on id_hashes (id);
