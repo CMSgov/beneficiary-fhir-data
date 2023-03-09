e94de7c8a6b85f7ad38af73e097f07fa08fa3db1
Brian Burton <brian.burton@adhocteam.us>
2023-03-09 12:30:10 -0500

    Updates migration to set intermediary_nb not null.

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V107__reingest_fiss_claims.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V107__reingest_fiss_claims.sql
@@ -1,9 +1,14 @@
 -- PACA-752 uses rdaClaimKey field for claimId instead of dcn
 
--- Truncate the fiss_claims table since all claimIds are going to change.
+-- Truncate the fiss_claims table since all claimIds are going to change and
+-- we need to set intermediary_nb to be not null.
 
 ${logic.psql-only} TRUNCATE rda.fiss_claims CASCADE;
 
--- Reset the progress table so we can reingest all FISS claims using the new claimIds.
+-- Now that the table is truncated we can make intermediary_nb not nullable.
+
+ALTER TABLE rda.fiss_claims ALTER COLUMN intermediary_nb SET NOT NULL;
+
+-- Reset the progress table so we can re-ingest all FISS claims using the new claimIds.
 
 DELETE FROM rda.rda_api_progress WHERE claim_type = 'FISS';
