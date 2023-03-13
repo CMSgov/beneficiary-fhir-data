bbcc59fbe52b72fa2ac514ecacd019e00b85ec53
Mitch Alessio <mitch.alessio@adhocteam.us>
2023-03-13 16:53:14 -0400

    Make build-push.sh safer by allowing it to be invoked from any working directory

--- apps/utils/locust_tests/lambda/server-regression/build-push.sh
+++ apps/utils/locust_tests/lambda/server-regression/build-push.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-# Constants
-GIT_SHORT_HASH="$(git rev-parse --short HEAD)"
+# SCRIPT_DIR will return the directory where this script is located, and CONTEXT_DIR will be the
+# directory of the Docker build context (locust_tests). This way, this script can be called from
+# _any_ directory and there will be no issues
+SCRIPT_DIR="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+CONTEXT_DIR="$(cd "$SCRIPT_DIR"; cd ../../; pwd)"
+GIT_SHORT_HASH="$(cd "$SCRIPT_DIR"; git rev-parse --short HEAD)"
 AWS_REGION="us-east-1"
 PRIVATE_REGISTRY_URI="$(aws ecr describe-registry --region "$AWS_REGION" | jq -r '.registryId').dkr.ecr.$AWS_REGION.amazonaws.com"
 IMAGE_NAME="$PRIVATE_REGISTRY_URI/bfd-mgmt-server-regression"
@@ -19,8 +23,8 @@ aws ecr-public get-login-password --region "$AWS_REGION" | docker login --userna
 aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$PRIVATE_REGISTRY_URI"
 
 DOCKER_BUILDKIT=1 # Specified to enable Dockerfile local Dockerignore, see https://stackoverflow.com/a/57774684
-docker build . \
-  --file "./lambda/server-regression/Dockerfile" \
+docker build "$CONTEXT_DIR" \
+  --file "$CONTEXT_DIR/lambda/server-regression/Dockerfile" \
   --tag "$IMAGE_TAGGED_HASH" \
   --tag "$IMAGE_TAGGED_LATEST" \
   --platform "linux/amd64"
