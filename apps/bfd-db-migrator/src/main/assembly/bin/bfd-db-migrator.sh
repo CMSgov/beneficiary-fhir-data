660d6b71396992e27c3ef6ad4b7d8a31f731ac98
Michael J Burling <michael.burling@adhocteam.us>
2023-03-17 10:26:34 -0500

    BFD-2506: Fix bfd-db-migrator script

--- apps/bfd-db-migrator/src/main/assembly/bin/bfd-db-migrator.sh
+++ apps/bfd-db-migrator/src/main/assembly/bin/bfd-db-migrator.sh
@@ -4,6 +4,6 @@ scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bfdDBMigratorAppJar="$(echo ${scriptDirectory}/bfd-db-migrator*.jar)"
 classpath="${bfdDBMigratorAppJar}:${scriptDirectory}/lib/*"
 mainClass="gov.cms.bfd.migrator.app.MigratorApp"
-[ -n "$JAVA_HOME" ] && "javaExecutable=${JAVA_HOME}/bin/java" || javaExecutable=java
+[ -n "$JAVA_HOME" ] && javaExecutable="${JAVA_HOME}/bin/java" || javaExecutable=java
 
 exec "$javaExecutable" -cp "$classpath" "$@" "$mainClass"
