f84e13aa9546f487c6a11f937d3cafebc00921f9
Brian Burton <brian.burton@adhocteam.us>
2023-03-27 10:40:01 -0400

    Uses id as primary key for id_hashes.

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V109__add_id_hashes.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V109__add_id_hashes.sql
@@ -1,9 +1,6 @@
--- hash is the primary key but we usually search for the plain id.
+-- Lookup table for DatabaseIdHasher persistent cache.
 CREATE TABLE id_hashes (
     id varchar(80) NOT NULL,
     hash varchar(64) NOT NULL,
-    CONSTRAINT id_hashes_key PRIMARY KEY (hash)
+    CONSTRAINT id_hashes_key PRIMARY KEY (id)
 );
-
--- non unique index although generally there will only be one record per id.
-CREATE INDEX id_hashes_id on id_hashes (id);
