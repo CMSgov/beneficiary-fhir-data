b660a02e75b8cd3c7a5fa8e785f953610d3053d0
Mitch Alessio <mitch.alessio@adhocteam.us>
2023-03-29 16:16:52 -0400

    Add local for Jenkins target job name; use variables in Lambda description

--- ops/terraform/services/pipeline/modules/bfd_pipeline_manager/main.tf
+++ ops/terraform/services/pipeline/modules/bfd_pipeline_manager/main.tf
@@ -9,6 +9,7 @@ locals {
 
   jenkins_job_queue_arn  = data.aws_sqs_queue.jenkins_job_queue.arn
   jenkins_job_queue_name = data.aws_sqs_queue.jenkins_job_queue.name
+  jenkins_target_job_name = "bfd-deploy-pipeline-terraservice"
 }
 
 resource "aws_lambda_permission" "this" {
@@ -26,9 +27,9 @@ resource "aws_lambda_function" "this" {
   description = join("", [
     "Invoked whenever a new file appears in either the root or Synthetic Done/ and Incoming/ paths ",
     "of the ${local.env} BFD ETL S3 Bucket (${data.aws_s3_bucket.etl.id}), this Lambda runs the ",
-    "bfd-deploy-pipeline-terraservice Jenkins job through the bfd-mgmt-run-jenkins-job SQS queue ",
-    "specifiying whether the BFD Pipeline instance should run to load data when data arrives or ",
-    "stop running when data has finished loading"
+    "${local.jenkins_target_job_name} Jenkins job through the ${local.jenkins_job_queue_name} SQS ",
+    "queue specifiying whether the BFD Pipeline instance should run to load data when data arrives ",
+    "or stop running when data has finished loading"
   ])
 
   tags = {
@@ -49,7 +50,7 @@ resource "aws_lambda_function" "this" {
     variables = {
       BFD_ENVIRONMENT          = local.env
       DEPLOYED_GIT_BRANCH      = var.deployed_git_branch
-      JENKINS_TARGET_JOB_NAME  = "bfd-deploy-pipeline-terraservice"
+      JENKINS_TARGET_JOB_NAME  = local.jenkins_target_job_name
       JENKINS_JOB_RUNNER_QUEUE = local.jenkins_job_queue_name
       ONGOING_LOAD_QUEUE       = ""
       ETL_BUCKET_ID            = data.aws_s3_bucket.etl.id
