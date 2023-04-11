35c94fc6d93056de38c8f89074de6a2a6798a05b
Michael J Burling <michael.burling@adhocteam.us>
2023-04-11 15:06:23 -0500

    Use al2, corretto, python3.9 in JenkinsCI

--- ops/jenkins/bfd-cbc-build/build-push.sh
+++ ops/jenkins/bfd-cbc-build/build-push.sh
@@ -3,16 +3,16 @@ set -eou pipefail
 
 # Overridable Defaults
 GIT_SHORT_HASH="$(git rev-parse --short HEAD)"
-DOCKER_TAG="${CBC_DOCKER_TAG:-"jdk17-mvn3-an29-tfenv-${GIT_SHORT_HASH}"}"
-DOCKER_TAG_LATEST="${CBC_DOCKER_TAG_LATEST:-"jdk17-mvn3-an29-tfenv-latest"}"
+DOCKER_TAG="${CBC_DOCKER_TAG:-"jdk17-mvn3-tfenv3-${GIT_SHORT_HASH}"}"
+DOCKER_TAG_LATEST="${CBC_DOCKER_TAG_LATEST:-"jdk17-mvn3-tfenv3-latest"}"
 
 docker build . \
   --build-arg JAVA_VERSION="${CBC_JAVA_VERSION:-17}" \
   --build-arg MAVEN_VERSION="${CBC_MAVEN_VERSION:-3}" \
-  --build-arg ANSIBLE_VERSION="${CBC_ANSIBLE_VERSION:-2.9.27}" \
   --build-arg PACKER_VERSION="${CBC_PACKER_VERSION:-1.6.6}" \
-  --build-arg TFENV_REPO_HASH="${CBC_TFENV_REPO_HASH:-7e89520}" \
+  --build-arg TFENV_REPO_HASH="${CBC_TFENV_REPO_HASH:-c05c364}" \
   --build-arg TFENV_VERSIONS="${CBC_TFENV_VERSIONS:-0.12.31 1.1.9}" \
+  --build-arg PYTHON3_TAR_SOURCE="${PYTHON3_TAR_SOURCE:-https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz}" \
   --tag "public.ecr.aws/c2o1d8s9/bfd-cbc-build:${DOCKER_TAG}"
 
 docker tag "public.ecr.aws/c2o1d8s9/bfd-cbc-build:${DOCKER_TAG}" "public.ecr.aws/c2o1d8s9/bfd-cbc-build:${DOCKER_TAG_LATEST}"
