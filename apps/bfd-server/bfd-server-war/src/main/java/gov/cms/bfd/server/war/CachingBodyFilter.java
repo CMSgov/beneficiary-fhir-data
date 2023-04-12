64fa65bd2e3f281044a3eb63761a0c99202857da
Billie Rose <billiexrose@gmail.com>
2023-04-12 12:50:36 -0500

    Update CachingBodyFilter.java

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/CachingBodyFilter.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/CachingBodyFilter.java
@@ -8,6 +8,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.util.ContentCachingRequestWrapper;
@@ -16,15 +18,20 @@ import org.springframework.web.util.ContentCachingResponseWrapper;
 /** Testing. */
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CachingBodyFilter implements Filter {
+  /** Logger that logs. */
+  private static final Logger LOGGER_MISC = LoggerFactory.getLogger(CachingBodyFilter.class);
 
   /** {@inheritDoc} */
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
     ContentCachingRequestWrapper reqWrapper =
         new ContentCachingRequestWrapper((HttpServletRequest) request);
     ContentCachingResponseWrapper resWrapper =
         new ContentCachingResponseWrapper((HttpServletResponse) response);
-    chain.doFilter(reqWrapper, resWrapper);
+    try {
+      chain.doFilter(reqWrapper, resWrapper);
+    } catch (IOException | ServletException e) {
+      LOGGER_MISC.error("Error extracting body", e);
+    }
   }
 }
