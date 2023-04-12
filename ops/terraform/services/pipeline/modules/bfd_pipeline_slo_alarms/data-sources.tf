80efec1bbe7bb5b7fefc19d158aa8adf75a7b517
Mitch Alessio <mitch.alessio@adhocteam.us>
2023-04-12 10:38:07 -0400

    Inline get_est_or_edt.sh; inline TZ with date command to properly support Mac OS

--- ops/terraform/services/pipeline/modules/bfd_pipeline_slo_alarms/data-sources.tf
+++ ops/terraform/services/pipeline/modules/bfd_pipeline_slo_alarms/data-sources.tf
@@ -21,5 +21,12 @@ data "aws_sns_topic" "warning_ok_sns" {
 }
 
 data "external" "edt_or_est" {
-  program = ["bash", "${path.module}/get_est_or_edt.sh"]
+  program = [
+    "bash",
+    "-c",
+    # heredoc is used to make quote escaping a little clearer
+    <<-EOF
+    echo "{\"timezone\":\"$(TZ=America/New_York date +%Z)\"}"
+    EOF
+  ]
 }
