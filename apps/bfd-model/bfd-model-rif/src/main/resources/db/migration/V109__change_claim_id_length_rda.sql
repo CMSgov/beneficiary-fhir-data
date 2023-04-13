e1720652ef5d1fd4e619e29c06017a8be82f67bb
Kyle Robbins <83452106+kyrobbins@users.noreply.github.com>
2023-04-13 11:40:35 -0400

    [PACA-1016] Drop fiss tables

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V109__change_claim_id_length_rda.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V109__change_claim_id_length_rda.sql
@@ -1,5 +1,5 @@
 
--- Allow extra space for the new claim_id to be larger than dcn was.
+-- Allow extra space for the new claim_id to accommodate the Base64 encoding.
 
 ALTER TABLE rda.fiss_claims ALTER COLUMN claim_id ${logic.alter-column-type} varchar(43);
 ALTER TABLE rda.fiss_audit_trails ALTER COLUMN claim_id ${logic.alter-column-type} varchar(43);
@@ -9,3 +9,12 @@ ALTER TABLE rda.fiss_proc_codes ALTER COLUMN claim_id ${logic.alter-column-type}
 ALTER TABLE rda.fiss_revenue_lines ALTER COLUMN claim_id ${logic.alter-column-type} varchar(43);
 ALTER TABLE rda.claim_message_meta_data ALTER COLUMN claim_id ${logic.alter-column-type} varchar(43);
 ALTER TABLE rda.message_errors ALTER COLUMN claim_id ${logic.alter-column-type} varchar(43);
+
+-- Truncate FISS claims and associated meta data, the key they use is invalid now
+
+TRUNCATE TABLE rda.fiss_claims CASCADE;
+DELETE FROM rda.claim_message_meta_data WHERE claim_type = 'F';
+
+-- Reset the progress table so we can re-ingest all FISS claims with the new key.
+
+DELETE FROM rda.rda_api_progress WHERE claim_type = 'FISS';
