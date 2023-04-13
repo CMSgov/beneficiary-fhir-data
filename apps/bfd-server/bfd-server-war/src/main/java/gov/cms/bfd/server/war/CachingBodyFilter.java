0369906421d59a3c1ce7593e0f2bf057d4ef9151
Billie Rose <billiexrose@gmail.com>
2023-04-13 12:15:02 -0500

    Update CachingBodyFilter.java

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/CachingBodyFilter.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/CachingBodyFilter.java
@@ -1,50 +1,38 @@
 package gov.cms.bfd.server.war;
 
 import java.io.IOException;
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 /** Testing. */
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class CachingBodyFilter implements Filter {
+public class CachingBodyFilter extends OncePerRequestFilter {
   /** Logger that logs. */
   private static final Logger LOGGER_MISC = LoggerFactory.getLogger(CachingBodyFilter.class);
 
   /** {@inheritDoc} */
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
-    ContentCachingRequestWrapper reqWrapper =
-        new ContentCachingRequestWrapper((HttpServletRequest) request);
-    ContentCachingResponseWrapper resWrapper =
-        new ContentCachingResponseWrapper((HttpServletResponse) response);
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain chain) {
+    ContentCachingRequestWrapper reqWrapper = new ContentCachingRequestWrapper(request);
+    ContentCachingResponseWrapper resWrapper = new ContentCachingResponseWrapper(response);
     try {
       chain.doFilter(reqWrapper, resWrapper);
+      if (!resWrapper.isCommitted() && resWrapper.getStatus() == HttpStatus.SC_OK) {
+        resWrapper.copyBodyToResponse();
+      }
     } catch (IOException | ServletException e) {
       LOGGER_MISC.error("Error extracting body", e.getStackTrace().toString());
     }
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
-    // Nothing to do here.
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public void destroy() {
-    // Nothing to do here.
   }
 }
