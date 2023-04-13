d300278a0faf77c7c77fe4583bf7e6ea0d2bff61
Jeremy Rosenfeld <jeremy.rosenfeld@adhocteam.us>
2023-04-13 09:03:55 -0700

    relocate migration file and update comment

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V109__add_loaded_batch_loaded_file_idx.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V109__add_loaded_batch_loaded_file_idx.sql
@@ -2,9 +2,9 @@
  BFD-1505
  There are periodic JDBC errors related to the Loaded Filter Manager bloom filter implementation.
  
- This corrects that issue by eliminating the sequence scan on loaded_batches with an index.
+ This may correct the issue by having postgres not do a sequential scan on loaded_batches with an index.
  */
  
-CREATE INDEX IF NOT EXISTS loaded_batches_loaded_file_id_idx
+CREATE INDEX CONCURRENTLY IF NOT EXISTS loaded_batches_loaded_file_id_idx
     ON public.loaded_batches USING btree
     (loaded_file_id DESC NULLS LAST);
