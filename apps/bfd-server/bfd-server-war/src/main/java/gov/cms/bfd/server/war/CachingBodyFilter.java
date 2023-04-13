df6145734aea2f9b99078f3219de80017f340c86
Billie Rose <billiexrose@gmail.com>
2023-04-13 14:46:59 -0500

    Update CachingBodyFilter.java

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/CachingBodyFilter.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/CachingBodyFilter.java
@@ -5,6 +5,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
@@ -27,8 +28,13 @@ public class CachingBodyFilter extends OncePerRequestFilter {
     ContentCachingResponseWrapper resWrapper = new ContentCachingResponseWrapper(response);
     try {
       chain.doFilter(reqWrapper, resWrapper);
+      if (!resWrapper.isCommitted() && resWrapper.getStatus() == HttpStatus.SC_OK) {
+        resWrapper.resetBuffer();
+        resWrapper.getOutputStream().write(1024);
+        resWrapper.copyBodyToResponse();
+      }
     } catch (IOException | ServletException e) {
-      LOGGER_MISC.error("Error extracting body", e);
+      LOGGER_MISC.error("Error extracting body", e.getStackTrace().toString());
     }
   }
 }
