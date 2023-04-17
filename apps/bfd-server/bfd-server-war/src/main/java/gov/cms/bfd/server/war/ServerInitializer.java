d51a7caf18ca265c467373c56cd08d5130799502
Jeremy Rosenfeld <jeremy.rosenfeld@adhocteam.us>
2023-04-17 15:27:57 -0700

    update server initializer comment to not include v2 check

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/ServerInitializer.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/ServerInitializer.java
@@ -57,7 +57,7 @@ public final class ServerInitializer implements WebApplicationInitializer {
     cxfServletReg.setLoadOnStartup(1);
     cxfServletReg.addMapping("/v1/fhir/*");
 
-    // Register the Blue Button R4 Server/Servlet if v2 is ENABLED!!
+    // Register the Blue Button R4 Server/Servlet.
     V2Server r4Servlet = new V2Server();
     cxfServletReg = servletContext.addServlet("r4Servlet", r4Servlet);
     cxfServletReg.setLoadOnStartup(1);
