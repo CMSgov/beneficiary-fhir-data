65792c9394d2765f0d3688cfa1ce8cc0f9100639
Mitch Alessio <mitch.alessio@adhocteam.us>
2023-04-24 12:11:52 -0400

    Remove unused policy related to s3 buckets and corresponding variable

--- ops/terraform/services/server/modules/bfd_server_iam/main.tf
+++ ops/terraform/services/server/modules/bfd_server_iam/main.tf
@@ -30,28 +30,6 @@ resource "aws_iam_role" "instance" {
   EOF
 }
 
-# policy to allow full s3 privs
-resource "aws_iam_role_policy" "s3_policy" {
-  count = length(var.s3_bucket_arns) > 0 ? 1 : 0
-  name  = "bfd-${local.env}-${var.name}-s3-policy"
-  role  = aws_iam_role.instance.id
-
-  policy = <<-EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      %{for arn in var.s3_bucket_arns}
-      {
-        "Action": "s3:*",
-        "Effect": "Allow",
-        "Resource": ["${arn}/*"]
-      }
-      %{endfor}
-    ]
-  }
-  EOF
-}
-
 # attach AWS managed CloudWatchAgentServerPolicy to all EC2 instances
 resource "aws_iam_role_policy_attachment" "cloudwatch_agent_policy_attachment" {
   role       = aws_iam_role.instance.id
--- ops/terraform/services/server/modules/bfd_server_iam/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "s3_bucket_arns" {
-  description = "A list of S3 bucket arns to allow access to"
-  type        = list(string)
-  default     = []
-}
