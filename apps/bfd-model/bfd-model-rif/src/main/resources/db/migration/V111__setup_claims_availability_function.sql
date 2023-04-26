d3d7d3bdd5410389274547e3af836a5746bf1e1d
mackec1 <chris.mackenzie@adhocteam.us>
2023-04-26 08:56:42 -0400

    psql vs.hsql changes

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V111__setup_claims_availability_function.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V111__setup_claims_availability_function.sql
@@ -6,69 +6,101 @@
 -- improve performance, especially for the case(s) when there are no claims wahtsoever
 -- for a given Claim type for the Beneficiary.
 --
--- NOTE: HSQL does not support mathematical 'bitwise &' functionality; thus when computing
---       the bitmask result, we'll simply perform additive functions to derive the same.
---       PSQL does support support mathematical 'bitwise &' and will therefore be used.
---
-${logic.hsql-only} CREATE 
-${logic.psql-only} CREATE OR REPLACE 
-FUNCTION check_claims_mask(v_bene_id bigint)
+-- ${logic.hsql-only} set database event log level 4
+${logic.psql-only} CREATE OR REPLACE FUNCTION check_claims_mask(v_bene_id bigint)
+${logic.hsql-only} CREATE FUNCTION check_claims_mask(IN v_bene_id bigint)
 RETURNS integer
 ${logic.psql-only} LANGUAGE plpgsql AS
 ${logic.psql-only} $func$
-DECLARE
-	v_rslt           integer  := 0;
-	v_cnt            integer  := 0;         
-                                       -- Java definitions
-	V_CARRIER        integer  := 1;    -- public static final int V_CARRIER_HAS_DATA     = (1 << 0);
-	V_INPATIENT      integer  := 2;    -- public static final int V_INPATIENT_HAS_DATA   = (1 << 1);
-	V_OUTPATIENT     integer  := 4;    -- public static final int V_OUTPATIENT_HAS_DATA  = (1 << 2);
-	V_SNF            integer  := 8;    -- public static final int V_SNF_HAS_DATA         = (1 << 3);
-	V_DME            integer  := 16;   -- public static final int V_DME_HAS_DATA         = (1 << 4);
-	V_HHA            integer  := 32;   -- public static final int V_HHA_HAS_DATA         = (1 << 5);
-	V_HOSPICE        integer  := 64;   -- public static final int V_HOSPICE_HAS_DATA     = (1 << 6);
-	V_PART_D         integer  := 128;  -- public static final int V_PART_D_HAS_DATA      = (1 << 7);
-BEGIN
-	SELECT COUNT(BENE_ID) INTO v_cnt FROM carrier_claims WHERE bene_id = v_bene_id limit 1;
-	IF v_cnt > 0 THEN
-		v_rslt = V_CARRIER;
-	END IF;
-	SELECT COUNT(BENE_ID) INTO v_cnt  FROM inpatient_claims WHERE bene_id = v_bene_id limit 1;
-	IF v_cnt > 0 THEN
-${logic.hsql-only} v_rslt = v_rslt + V_INPATIENT;
-${logic.psql-only} v_rslt = v_rslt # V_INPATIENT;
-	END IF;
-	SELECT COUNT(BENE_ID) INTO v_cnt  FROM outpatient_claims WHERE bene_id = v_bene_id limit 1;
-	IF v_cnt > 0 THEN
-${logic.hsql-only} v_rslt = v_rslt + V_OUTPATIENT;
-${logic.psql-only} v_rslt = v_rslt # V_OUTPATIENT;
-	END IF;
-	SELECT COUNT(BENE_ID) INTO v_cnt  FROM snf_claims WHERE bene_id = v_bene_id limit 1;
-	IF v_cnt > 0 THEN
-${logic.hsql-only} v_rslt = v_rslt + V_SNF;
-${logic.psql-only} v_rslt = v_rslt # V_SNF;
-	END IF;
-	SELECT COUNT(BENE_ID) INTO v_cnt  FROM dme_claims WHERE bene_id = v_bene_id limit 1;
-	IF v_cnt > 0 THEN
-${logic.hsql-only} v_rslt = v_rslt + V_DME;
-${logic.psql-only} v_rslt = v_rslt # V_DME;
-	END IF;
-	SELECT COUNT(BENE_ID) INTO v_cnt  FROM hha_claims WHERE bene_id = v_bene_id limit 1;
-	IF v_cnt > 0 THEN
-${logic.hsql-only} v_rslt = v_rslt + V_HHA;
-${logic.psql-only} v_rslt = v_rslt # V_HHA;
-	END IF;
-	SELECT COUNT(BENE_ID) INTO v_cnt  FROM hospice_claims WHERE bene_id = v_bene_id limit 1;
-	IF v_cnt > 0 THEN
-		v_rslt = v_rslt # V_HOSPICE;
-${logic.hsql-only} v_rslt = v_rslt + V_HOSPICE;
-${logic.psql-only} v_rslt = v_rslt # V_HOSPICE;
-	END IF;
-	SELECT COUNT(BENE_ID) INTO v_cnt  FROM partd_events WHERE bene_id = v_bene_id limit 1;
-	IF v_cnt > 0 THEN
-${logic.hsql-only} v_rslt = v_rslt + V_PART_D;
-${logic.psql-only} v_rslt = v_rslt # V_PART_D;
-	END IF;
+${logic.psql-only} DECLARE
+${logic.psql-only}	v_rslt           integer  := 0;       
+${logic.psql-only}                                     -- Java definitions
+${logic.psql-only}	V_CARRIER        integer  := 1;    -- public static final int V_CARRIER_HAS_DATA     = (1 << 0);
+${logic.psql-only}	V_INPATIENT      integer  := 2;    -- public static final int V_INPATIENT_HAS_DATA   = (1 << 1);
+${logic.psql-only}	V_OUTPATIENT     integer  := 4;    -- public static final int V_OUTPATIENT_HAS_DATA  = (1 << 2);
+${logic.psql-only}	V_SNF            integer  := 8;    -- public static final int V_SNF_HAS_DATA         = (1 << 3);
+${logic.psql-only}	V_DME            integer  := 16;   -- public static final int V_DME_HAS_DATA         = (1 << 4);
+${logic.psql-only}	V_HHA            integer  := 32;   -- public static final int V_HHA_HAS_DATA         = (1 << 5);
+${logic.psql-only}	V_HOSPICE        integer  := 64;   -- public static final int V_HOSPICE_HAS_DATA     = (1 << 6);
+${logic.psql-only}	V_PART_D         integer  := 128;  -- public static final int V_PART_D_HAS_DATA      = (1 << 7);
+${logic.psql-only} BEGIN
+
+${logic.hsql-only} READS SQL DATA
+${logic.hsql-only} BEGIN ATOMIC
+${logic.hsql-only}	 DECLARE v_rslt           integer DEFAULT 0;
+${logic.hsql-only}	 DECLARE v_cnt            integer DEFAULT 0;         
+${logic.hsql-only}	 DECLARE V_CARRIER        integer DEFAULT 1;
+${logic.hsql-only}	 DECLARE V_INPATIENT      integer DEFAULT 2;
+${logic.hsql-only}	 DECLARE V_OUTPATIENT     integer DEFAULT 4;
+${logic.hsql-only}	 DECLARE V_SNF            integer DEFAULT 8;
+${logic.hsql-only}	 DECLARE V_DME            integer DEFAULT 16;
+${logic.hsql-only}	 DECLARE V_HHA            integer DEFAULT 32;
+${logic.hsql-only}	 DECLARE V_HOSPICE        integer DEFAULT 64;
+${logic.hsql-only}	 DECLARE V_PART_D         integer DEFAULT 128;
+
+${logic.hsql-only}	SELECT COUNT(BENE_ID) INTO v_cnt FROM carrier_claims WHERE bene_id = v_bene_id;
+${logic.hsql-only}	IF v_cnt > 0 THEN
+${logic.hsql-only}    SET v_rslt = V_CARRIER;
+${logic.psql-only}  PERFORM 1 FROM carrier_claims WHERE bene_id = v_bene_id limit 1;
+${logic.psql-only}	IF FOUND THEN
+${logic.psql-only}    v_rslt = V_CARRIER;
+	                END IF;
+
+${logic.hsql-only}	SELECT COUNT(BENE_ID) INTO v_cnt FROM inpatient_claims WHERE bene_id = v_bene_id;
+${logic.hsql-only}	IF v_cnt > 0 THEN
+${logic.hsql-only}    SET v_rslt = v_rslt + V_INPATIENT;
+${logic.psql-only}  PERFORM 1 FROM inpatient_claims WHERE bene_id = v_bene_id limit 1;
+${logic.psql-only}	IF FOUND THEN
+${logic.psql-only}    v_rslt = v_rslt + V_INPATIENT;
+	                END IF;
+
+${logic.hsql-only}	SELECT COUNT(BENE_ID) INTO v_cnt FROM outpatient_claims WHERE bene_id = v_bene_id;
+${logic.hsql-only}	IF v_cnt > 0 THEN
+${logic.hsql-only}    SET v_rslt = v_rslt + V_OUTPATIENT;
+${logic.psql-only}  PERFORM 1 FROM outpatient_claims WHERE bene_id = v_bene_id limit 1;
+${logic.psql-only}	IF FOUND THEN
+${logic.psql-only}    v_rslt = v_rslt + V_OUTPATIENT;
+	                END IF;
+
+${logic.hsql-only}	SELECT COUNT(BENE_ID) INTO v_cnt FROM snf_claims WHERE bene_id = v_bene_id;
+${logic.hsql-only}	IF v_cnt > 0 THEN
+${logic.hsql-only}    SET v_rslt = v_rslt + V_SNF;
+${logic.psql-only}  PERFORM 1 FROM snf_claims WHERE bene_id = v_bene_id limit 1;
+${logic.psql-only}	IF FOUND THEN
+${logic.psql-only}    v_rslt = v_rslt + V_SNF;
+	                END IF;
+
+${logic.hsql-only}	SELECT COUNT(BENE_ID) INTO v_cnt FROM dme_claims WHERE bene_id = v_bene_id;
+${logic.hsql-only}	IF v_cnt > 0 THEN
+${logic.hsql-only}    SET v_rslt = v_rslt + V_DME;
+${logic.psql-only}  PERFORM 1 FROM dme_claims WHERE bene_id = v_bene_id limit 1;
+${logic.psql-only}	IF FOUND THEN
+${logic.psql-only}    v_rslt = v_rslt + V_DME;
+	                END IF;
+
+${logic.hsql-only}	SELECT COUNT(BENE_ID) INTO v_cnt FROM hha_claims WHERE bene_id = v_bene_id;
+${logic.hsql-only}	IF v_cnt > 0 THEN
+${logic.hsql-only}    SET v_rslt = v_rslt + V_HHA;
+${logic.psql-only}  PERFORM 1 FROM hha_claims WHERE bene_id = v_bene_id limit 1;
+${logic.psql-only}	IF FOUND THEN
+${logic.psql-only}    v_rslt = v_rslt + V_HHA;
+	                END IF;
+
+${logic.hsql-only}	SELECT COUNT(BENE_ID) INTO v_cnt FROM hospice_claims WHERE bene_id = v_bene_id;
+${logic.hsql-only}	IF v_cnt > 0 THEN
+${logic.hsql-only}    SET v_rslt = v_rslt + V_HOSPICE;
+${logic.psql-only}  PERFORM 1 FROM hospice_claims WHERE bene_id = v_bene_id limit 1;
+${logic.psql-only}	IF FOUND THEN
+${logic.psql-only}    v_rslt = v_rslt + V_HOSPICE;
+	                END IF;
+
+${logic.hsql-only}	SELECT COUNT(BENE_ID) INTO v_cnt FROM partd_events WHERE bene_id = v_bene_id;
+${logic.hsql-only}	IF v_cnt > 0 THEN
+${logic.hsql-only}    SET v_rslt = v_rslt + V_PART_D;
+${logic.psql-only}  PERFORM 1 FROM partd_events WHERE bene_id = v_bene_id limit 1;
+${logic.psql-only}	IF FOUND THEN
+${logic.psql-only}    v_rslt = v_rslt + V_PART_D;
+	                END IF;
 	RETURN v_rslt;
-END
+END;
 ${logic.psql-only} $func$;
