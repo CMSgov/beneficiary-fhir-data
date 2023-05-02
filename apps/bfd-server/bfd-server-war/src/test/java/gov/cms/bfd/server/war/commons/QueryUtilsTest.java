46d2b2a2b899cc5d2e27649ce87deb638b8c3d84
Chris MacKenzie <chris.mackenzie@adhocteam.us>
2023-05-02 11:38:28 -0400

    BFD-2549: Introduce EOB performance enhancement for claim types with no claim data (#1704)

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/commons/QueryUtilsTest.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/commons/QueryUtilsTest.java
@@ -1,5 +1,8 @@
 package gov.cms.bfd.server.war.commons;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 
@@ -8,6 +11,7 @@ import ca.uhn.fhir.rest.param.DateRangeParam;
 import ca.uhn.fhir.rest.param.ParamPrefixEnum;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
+import java.util.BitSet;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Predicate;
@@ -126,6 +130,54 @@ public class QueryUtilsTest {
     doReturn(upperBoundPredicate).when(builder).lessThan(dateExpression, upper);
     QueryUtils.createDateRangePredicate(builder, dateRange, dateExpression);
     verify(builder).and(notNullPredicate, lowerBoundPredicate, upperBoundPredicate);
+  }
+
+  /**
+   * Test {@link QueryUtils#hasClaimsData} asserting various ways an integer mask value can be
+   * created (mathematical XOR or additive integers) and result in the same integer value. The test
+   * further verifies that leveraging operator AND functionality to extract a subset of the bits in
+   * the state, one can arrive at a boolean true/false outcome.
+   *
+   * <p>The {@link QueryUtils#hasClaimsData} method takes in an arithmetic integer and returns a
+   * {@link BitSet} object that denotes availability of claims data, using predefined {@link
+   * QueryUtils} bitset identifiers.
+   */
+  public void testHasClaimsDataMaskValues() {
+    // 0001001 testVal
+    // 0000001 maskVal
+    // -------
+    // 0000001 expected
+    int testVal = QueryUtils.V_CARRIER_HAS_DATA | QueryUtils.V_SNF_HAS_DATA;
+    assertEquals(QueryUtils.V_CARRIER_HAS_DATA, (testVal & QueryUtils.V_CARRIER_HAS_DATA));
+
+    // same test as previous, but use additive values instead of XOR
+    testVal = QueryUtils.V_CARRIER_HAS_DATA + QueryUtils.V_SNF_HAS_DATA;
+    assertEquals(QueryUtils.V_CARRIER_HAS_DATA, (testVal & QueryUtils.V_CARRIER_HAS_DATA));
+
+    // 0001001 testVal
+    // 0010000 maskVal
+    // -------
+    // 0000000 expected
+    assertEquals(0, (testVal & QueryUtils.V_DME_HAS_DATA));
+
+    // test additive vs. XOR of values by comparing two integer values, each derived
+    // in a different manner.
+    testVal = QueryUtils.V_DME_HAS_DATA | QueryUtils.V_SNF_HAS_DATA | QueryUtils.V_HHA_HAS_DATA;
+    assertEquals(
+        testVal, QueryUtils.V_DME_HAS_DATA + QueryUtils.V_SNF_HAS_DATA + QueryUtils.V_HHA_HAS_DATA);
+
+    BitSet testBits = QueryUtils.convertClaimsBitmaskValue(testVal);
+    assertTrue(testBits.get(QueryUtils.CARRIER_HAS_DATA));
+    assertTrue(testBits.get(QueryUtils.INPATIENT_HAS_DATA));
+    assertTrue(testBits.get(QueryUtils.SNF_HAS_DATA));
+    assertTrue(testBits.get(QueryUtils.HHA_HAS_DATA));
+    assertTrue(testBits.get(QueryUtils.PART_D_HAS_DATA));
+    // the following bits were not set.
+    assertFalse(testBits.get(QueryUtils.OUTPATIENT_HAS_DATA));
+    assertFalse(testBits.get(QueryUtils.HOSPICE_HAS_DATA));
+    assertFalse(testBits.get(QueryUtils.DME_HAS_DATA));
+    // verify that the BitSet represents only those bit that are set.
+    assertEquals("{0, 1, 3, 5, 7}", testBits.toString());
   }
 
   /**
