5857bab3b20224decc2d0ede47b0f12db17ff59d
Mitch Alessio <mitch.alessio@adhocteam.us>
2023-05-03 17:33:31 -0400

    Add per-subnet IP reservations for EFT NLB to sensitive mgmt configuration; update base_config to support EFT SSM parameter creation

--- ops/terraform/env/mgmt/base_config/sensitive.tf
+++ ops/terraform/env/mgmt/base_config/sensitive.tf
@@ -6,6 +6,10 @@ locals {
     for key, value in local.eyaml
     : key => value if contains(split("/", key), "common") && value != "UNDEFINED"
   }
+  eft_sensitive = {
+    for key, value in local.eyaml
+    : key => value if contains(split("/", key), "eft") && value != "UNDEFINED"
+  }
 }
 
 data "external" "eyaml" {
@@ -14,6 +18,16 @@ data "external" "eyaml" {
 
 resource "aws_ssm_parameter" "common_sensitive" {
   for_each = local.common_sensitive
+
+  key_id    = var.kms_key_id
+  name      = each.key
+  overwrite = true
+  type      = "SecureString"
+  value     = each.value
+}
+
+resource "aws_ssm_parameter" "eft_sensitive" {
+  for_each = local.eft_sensitive
 
   key_id    = var.kms_key_id
   name      = each.key
