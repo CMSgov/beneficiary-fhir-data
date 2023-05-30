e71c953064ea307017646d0970b5c369ff769793
Billie Rose <billiexrose@gmail.com>
2023-05-30 16:05:05 -0500

    Update variables.tf

--- ops/terraform/services/server/server-load/variables.tf
+++ ops/terraform/services/server/server-load/variables.tf
@@ -31,7 +31,7 @@ variable "test_host" {
 variable "locust_tags" {
   description = "Space-delimited. The locust tests with ANY of the given tags will be executed."
   type        = string
-  default     = null
+  default     = "eob_test_id_count"
 }
 
 variable "initial_worker_nodes" {
