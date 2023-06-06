84a18e464d5c0fd86f83739aca00d839229beeba
Dave Tisza <dtisza@fearless.tech>
2023-06-06 13:51:07 -0400

    Add module for shared log processor lambda func

--- insights/terraform/projects/bb2/services/log_stream/main.tf
+++ insights/terraform/projects/bb2/services/log_stream/main.tf
@@ -9,16 +9,37 @@ locals {
   application = "bfd-insights"
   project = "bb2"
   env = terraform.workspace
+  region = "us-east-1"
 
+  # Shared lambda name/role (TODO: Split out by env)
+  lambda_name = "bb2-kinesis-firehose-cloudwatch-logs-processor-python"
+  lambda_role = "bb2-kinesis-firehose-cloudwatch-logs-processor-pyt-role-s0acxwoq"
+
+  firehose_name = "${local.env}-perf-mon"
+  glue_crawler_name = "${local.env}-perf-mon"
+
+}
+
+module "lambda" {
+  source          = "./modules/lambda"
+
+  name = local.lambda_name
+  description = "An Amazon Kinesis Firehose stream processor that extracts individual log events from records sent by Cloudwatch Logs subscription filters."
+  role = local.lambda_role
+  region = local.region
+  application   = local.application
+  project       = local.project
 }
 
 module "firehose" {
   source          = "./modules/firehose"
 
-  firehose_name = "${local.env}-perf-mon"
+  name          = local.firehose_name
   table_name    = "events_${local.env}_perf_mon"
+  lambda_name   = local.lambda_name
   project       = local.project
   database      = local.project
+  region        = local.region
 
   firehose_s3_buffer_interval = var.firehose_s3_buffer_interval
   firehose_s3_buffer_size     = var.firehose_s3_buffer_size
@@ -30,13 +51,14 @@ module "cwl_destination" {
   bb2_acct      = var.bb2_acct
   firehose_name = "${local.env}-perf-mon"
   project       = local.project
+  region        = local.region
 
 }
 
 module "glue_crawler" {
   source = "./modules/glue_crawler"
 
-  firehose_name = "${local.env}-perf-mon"
+  name          = local.glue_crawler_name
   project       = local.project
   database      = local.project
 
---
+++ insights/terraform/projects/bb2/services/log_stream/modules/lambda/variables.tf
@@ -0,0 +1,23 @@
+variable "name" {
+  type = string
+}
+
+variable "description" {
+  type = string
+}
+
+variable "role" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "application" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
