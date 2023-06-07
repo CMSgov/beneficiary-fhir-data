52d3d29be79a795ba34e7094f70f9b20d361f90d
Chris Brune <64486927+cbrunefearless@users.noreply.github.com>
2023-06-07 09:27:31 -0400

    Update and rename NPIOrgLookupE2ETest.java to NPIOrgLookupE2E.java

--- apps/bfd-data-npi/src/test/java/gov/cms/bfd/data/npi/lookup/NPIOrgLookupE2E.java
+++ apps/bfd-data-npi/src/test/java/gov/cms/bfd/data/npi/lookup/NPIOrgLookupE2E.java
@@ -9,13 +9,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /** NPI lookup end to end test. */
-public class NPIOrgLookupE2ETest {
+public class NPIOrgLookupE2E {
 
   /** Global variable for NPILookup. */
   NPIOrgLookup npiOrgDataLookup;
 
   /** Global variable for npiOrgDisplay. */
   Optional<String> npiOrgDisplay;
+
+  /** Global variable for valid npiOrgNumber. */
+  private final String npiOrgNumber = "1992903249";
+
+  /** Global variable for invalidNpiOrgNumber. */
+  private final String invalidNpiOrgNumber = "000";
 
   /** Setup method. */
   @BeforeEach
@@ -27,14 +33,14 @@ public class NPIOrgLookupE2ETest {
   /** End to End test for Npi Org Data with real npi number. */
   @Test
   public void shouldReturnRealOrgNPIDataReturnsRightOrganization() throws IOException {
-    npiOrgDisplay = npiOrgDataLookup.retrieveNPIOrgDisplay(Optional.of("1992903249"));
+    npiOrgDisplay = npiOrgDataLookup.retrieveNPIOrgDisplay(Optional.of(npiOrgNumber));
     assertEquals("CAMPBELL CLINIC", npiOrgDisplay.get());
   }
 
   /** End to End test for Npi Org Data with wrong npi number. */
   @Test
   public void shouldReturnEmptyOrganizationWithAWrongNpiNumber() throws IOException {
-    npiOrgDisplay = npiOrgDataLookup.retrieveNPIOrgDisplay(Optional.of("000"));
+    npiOrgDisplay = npiOrgDataLookup.retrieveNPIOrgDisplay(Optional.of(invalidNpiOrgNumber));
     assertFalse(npiOrgDisplay.isPresent());
   }
 }
