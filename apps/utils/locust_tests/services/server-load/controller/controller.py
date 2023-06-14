7b6d35a8884dc5e9fbf03ea8a57dba21668f5e9c
Billie Rose <billiexrose@gmail.com>
2023-06-14 11:59:28 -0500

    how did i miss these

--- apps/utils/locust_tests/services/server-load/controller/controller.py
+++ apps/utils/locust_tests/services/server-load/controller/controller.py
@@ -42,7 +42,7 @@ asg_name = os.environ.get("ASG_NAME", "")
 test_host = os.environ.get("TEST_HOST", "https://test.bfd.cms.gov")
 region = os.environ.get("AWS_CURRENT_REGION", "us-east-1")
 locust_tags = os.environ.get("LOCUST_TAGS", "")
-locust_tags_excluded = os.environ.get("LOCUST_EXCLUDE_TAGS", "")
+locust_exclude_tags = os.environ.get("LOCUST_EXCLUDE_TAGS", "")
 # Default dangerous variables to values that will not cause any issues
 initial_worker_nodes = int(os.environ.get("INITIAL_WORKER_NODES", 0))
 node_spawn_time = int(os.environ.get("NODE_SPAWN_TIME", 10))
@@ -131,7 +131,7 @@ def _main():
         ]
         + ([f"--expect-workers={initial_worker_nodes}"] if initial_worker_nodes > 0 else [])
         + ([f"--locust-tags=\"{locust_tags}\""] if locust_tags else [])
-        + ([f"--locust-exclude-tags=\"{locust_tags_excluded}\""] if locust_tags_excluded else []),
+        + ([f"--locust-exclude-tags=\"{locust_exclude_tags}\""] if locust_exclude_tags else []),
         cwd="../../../",
         stderr=subprocess.STDOUT,
     )
@@ -145,8 +145,8 @@ def _main():
     if locust_tags:
         print(f"Running tasks with an annotated @tag including any of the following: {locust_tags}")
 
-    if locust_tags_excluded:
-        print(f"Skipping tasks with an annotated @tag including any of the following: {locust_tags_excluded}")
+    if locust_exclude_tags:
+        print(f"Skipping tasks with an annotated @tag including any of the following: {locust_exclude_tags}")
 
     spawn_count = 0
     for _ in range(0, initial_worker_nodes):
