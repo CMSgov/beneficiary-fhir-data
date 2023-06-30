80584bfd754badd2d0a3d701b4a6c849a78ec105
Dave Tisza <dtisza@fearless.tech>
2023-06-30 13:41:30 -0400

    Update vars in main.tf

--- insights/terraform/projects/bb2/services/log_stream/main.tf
+++ insights/terraform/projects/bb2/services/log_stream/main.tf
@@ -9,16 +9,25 @@ locals {
   application = "bfd-insights"
   project = "bb2"
   env = terraform.workspace
+  region = "us-east-1"
+
+  # Shared lambda name/role (TODO: Split out by env)
+  lambda_name = "bb2-kinesis-firehose-cloudwatch-logs-processor-python"
+
+  firehose_name = "${local.env}-perf-mon"
+  glue_crawler_name = "${local.env}-perf-mon"
 
 }
 
 module "firehose" {
   source          = "./modules/firehose"
 
-  firehose_name = "${local.env}-perf-mon"
+  name = local.firehose_name
   table_name    = "events_${local.env}_perf_mon"
+  lambda_name   = local.lambda_name
   project       = local.project
   database      = local.project
+  region        = local.region
 
   firehose_s3_buffer_interval = var.firehose_s3_buffer_interval
   firehose_s3_buffer_size     = var.firehose_s3_buffer_size
@@ -30,13 +39,14 @@ module "cwl_destination" {
   bb2_acct      = var.bb2_acct
   firehose_name = "${local.env}-perf-mon"
   project       = local.project
+  region        = local.region
 
 }
 
 module "glue_crawler" {
   source = "./modules/glue_crawler"
 
-  firehose_name = "${local.env}-perf-mon"
+  name          = local.glue_crawler_name
   project       = local.project
   database      = local.project
 
