f7787dec2ae5a8184d2c7ace295c1cea9f4be452
Billie Rose <billiexrose@gmail.com>
2023-07-06 14:31:25 -0500

    BFD-2541: Organize Jenkins CI Jobs (#1800)
    Move Jenkinsfiles and their Docker dependencies into a centralized ops directory

---
+++ ops/jenkins/bfd-server-load-build/README.md
@@ -0,0 +1,21 @@
+# `bfd-server-load` Node
+
+## Overview
+
+The files within this directory consist of the Docker files used to build the node Lambda Docker image, 
+and the Jenkinsfile and related build script for the `bfd-server-load-build` Jenkins pipeline.
+
+## Building the `bfd-mgmt-server-load-node` Docker Image
+
+### Building the Image Locally
+
+As the `Dockerfile` for this image is not at the root of this `locust_tests` project, we need to
+ensure the [Docker build
+context](https://docs.docker.com/engine/reference/commandline/build/#description) is properly set to
+the root. 
+
+Ensure your current working directory is `/apps/utils/locust_tests` and run:
+
+```bash
+docker build -f "/ops/jenkins/bfd-server-load-build/Dockerfile" -t "<your-tag>" --platform linux/amd64 .
+```
--- ops/jenkins/bfd-server-regression-build/build-push.sh
+++ ops/jenkins/bfd-server-regression-build/build-push.sh
@@ -5,8 +5,9 @@ set -eou pipefail
 # directory of the Docker build context (locust_tests). This way, this script can be called from
 # _any_ directory and there will be no issues
 SCRIPT_DIR="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
-CONTEXT_DIR="$(cd "$SCRIPT_DIR"; cd ../../; pwd)"
-GIT_SHORT_HASH="$(cd "$SCRIPT_DIR"; git rev-parse --short HEAD)"
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+CONTEXT_DIR="${REPO_ROOT}/apps/utils/locust_tests/"
+GIT_SHORT_HASH="$(git rev-parse --short HEAD)"
 AWS_REGION="us-east-1"
 PRIVATE_REGISTRY_URI="$(aws ecr describe-registry --region "$AWS_REGION" | jq -r '.registryId').dkr.ecr.$AWS_REGION.amazonaws.com"
 IMAGE_NAME="$PRIVATE_REGISTRY_URI/bfd-mgmt-server-regression"
@@ -22,9 +23,8 @@ IMAGE_TAGGED_LATEST="$IMAGE_NAME:$DOCKER_TAG_LATEST"
 aws ecr-public get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "public.ecr.aws"
 aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$PRIVATE_REGISTRY_URI"
 
-DOCKER_BUILDKIT=1 # Specified to enable Dockerfile local Dockerignore, see https://stackoverflow.com/a/57774684
 docker build "$CONTEXT_DIR" \
-  --file "$CONTEXT_DIR/lambda/server-regression/Dockerfile" \
+  --file "$SCRIPT_DIR/Dockerfile" \
   --tag "$IMAGE_TAGGED_HASH" \
   --tag "$IMAGE_TAGGED_LATEST" \
   --platform "linux/amd64"
