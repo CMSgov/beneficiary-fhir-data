9eae3571e183993be487b0d0bde6204e36dbc663
Brian Burton <brian.burton@adhocteam.us>
2023-07-12 13:04:12 -0400

    Adds tests and javadocs.

--- apps/bfd-db-migrator/src/main/java/gov/cms/bfd/migrator/app/MigratorProgressTracker.java
+++ apps/bfd-db-migrator/src/main/java/gov/cms/bfd/migrator/app/MigratorProgressTracker.java
@@ -4,30 +4,44 @@ import gov.cms.bfd.sharedutils.database.DatabaseMigrationStage;
 import java.util.function.Consumer;
 import lombok.Data;
 
+/**
+ * Provides methods to report the current stage of processing. Wraps these into {@link
+ * MigratorProgress} objects and passes them on to a {@link Consumer} to report them.
+ */
 @Data
 public class MigratorProgressTracker {
+  /** Sends the progress to some destination such as logs or SQS. */
   private final Consumer<MigratorProgress> progressReporter;
 
+  /** Report app has started. */
   public void appStarted() {
     var progress = new MigratorProgress(MigratorProgress.Stage.Started, null);
     progressReporter.accept(progress);
   }
 
+  /** Report app has connected to the database. */
   public void appConnected() {
     var progress = new MigratorProgress(MigratorProgress.Stage.Connected, null);
     progressReporter.accept(progress);
   }
 
+  /** Report app has finished without errors. */
   public void appFinished() {
-    var progress = new MigratorProgress(MigratorProgress.Stage.Finished, null);
+    var progress = new MigratorProgress(MigratorProgress.Stage.Connected, null);
     progressReporter.accept(progress);
   }
 
+  /** Report app has finished due to an error. */
   public void appFailed() {
     var progress = new MigratorProgress(MigratorProgress.Stage.Failed, null);
     progressReporter.accept(progress);
   }
 
+  /**
+   * Reports app has completed a stage of the migration.
+   *
+   * @param migrationStage migration stage just completed
+   */
   public void migrating(DatabaseMigrationStage migrationStage) {
     var progress = new MigratorProgress(MigratorProgress.Stage.Migrating, migrationStage);
     progressReporter.accept(progress);
---
+++ apps/bfd-db-migrator/src/test/java/gov/cms/bfd/migrator/app/MigratorProgressTrackerTest.java
@@ -0,0 +1,44 @@
+package gov.cms.bfd.migrator.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import gov.cms.bfd.sharedutils.database.DatabaseMigrationStage;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for {@link MigratorProgressTracker}.
+ */
+class MigratorProgressTrackerTest {
+  /**
+   * Calls all possible methods and ensures they are passed on to the reporter as expected.
+   */
+  @Test
+  void testAllMethods() {
+    var progressList = new ArrayList<MigratorProgress>();
+    var tracker = new MigratorProgressTracker(progressList::add);
+    tracker.appStarted();
+    tracker.appConnected();
+    tracker.appFinished();
+    tracker.appFailed();
+
+    final var migration1 =
+        new DatabaseMigrationStage(DatabaseMigrationStage.Stage.Preparing, "preparing");
+    final var migration2 =
+        new DatabaseMigrationStage(DatabaseMigrationStage.Stage.Finished, "finished");
+
+    tracker.migrating(migration1);
+    tracker.migrating(migration2);
+
+    assertEquals(
+        List.of(
+            new MigratorProgress(MigratorProgress.Stage.Started, null),
+            new MigratorProgress(MigratorProgress.Stage.Connected, null),
+            new MigratorProgress(MigratorProgress.Stage.Connected, null),
+            new MigratorProgress(MigratorProgress.Stage.Failed, null),
+            new MigratorProgress(MigratorProgress.Stage.Migrating, migration1),
+            new MigratorProgress(MigratorProgress.Stage.Migrating, migration2)),
+        progressList);
+  }
+}
