48a615d4c3e72003cf11a9168b24b35a3b3fe30c
mackec1 <chris.mackenzie@adhocteam.us>
2023-07-12 08:41:11 -0400

    fix time strings to use Zulu time

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/InpatientClaimTransformerV2Test.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/InpatientClaimTransformerV2Test.java
@@ -958,7 +958,7 @@ public final class InpatientClaimTransformerV2Test {
                     "http://hl7.org/fhir/sid/icd-10",
                     "BQ0HZZZ",
                     "PLAIN RADIOGRAPHY OF LEFT ANKLE")),
-            "2016-01-16T00:00:00-06:00");
+            "2016-01-16T00:00:00Z");
 
     assertTrue(cmp1.equalsDeep(proc1), "Comparing Procedure code BQ0HZZZ");
 
@@ -977,7 +977,7 @@ public final class InpatientClaimTransformerV2Test {
                     "http://hl7.org/fhir/sid/icd-10",
                     "CD1YYZZ",
                     "PLANAR NUCL MED IMAG OF DIGESTIVE SYS USING OTH RADIONUCLIDE")),
-            "2016-01-16T00:00:00-06:00");
+            "2016-01-16T00:00:00Z");
 
     assertTrue(cmp2.equalsDeep(proc2), "Comparing Procedure code CD1YYZZ");
 
@@ -996,7 +996,7 @@ public final class InpatientClaimTransformerV2Test {
                     "http://hl7.org/fhir/sid/icd-10",
                     "2W52X6Z",
                     "REMOVAL OF PRESSURE DRESSING ON NECK")),
-            "2016-01-15T00:00:00-06:00");
+            "2016-01-15T00:00:00Z");
 
     assertTrue(cmp3.equalsDeep(proc3), "Comparing Procedure code 2W52X6Z");
 
@@ -1013,7 +1013,7 @@ public final class InpatientClaimTransformerV2Test {
                     "FLUOROSCOPY OF LEFT SCAPULA"),
                 new Coding(
                     "http://hl7.org/fhir/sid/icd-10", "BP17ZZZ", "FLUOROSCOPY OF LEFT SCAPULA")),
-            "2016-01-17T00:00:00-06:00");
+            "2016-01-17T00:00:00Z");
 
     assertTrue(cmp4.equalsDeep(proc4), "Comparing Procedure code BP17ZZZ");
 
@@ -1030,7 +1030,7 @@ public final class InpatientClaimTransformerV2Test {
                     "HYPERTHERMIA OF NASOPHARYNX"),
                 new Coding(
                     "http://hl7.org/fhir/sid/icd-10", "D9YD8ZZ", "HYPERTHERMIA OF NASOPHARYNX")),
-            "2016-01-24T00:00:00-06:00");
+            "2016-01-24T00:00:00Z");
 
     assertTrue(cmp5.equalsDeep(proc5), "Comparing Procedure code D9YD8ZZ");
 
@@ -1049,7 +1049,7 @@ public final class InpatientClaimTransformerV2Test {
                     "http://hl7.org/fhir/sid/icd-10",
                     "F00ZCKZ",
                     "APHASIA ASSESSMENT USING AUDIOVISUAL EQUIPMENT")),
-            "2016-01-24T00:00:00-06:00");
+            "2016-01-24T00:00:00Z");
 
     assertTrue(cmp6.equalsDeep(proc6), "Comparing Procedure code F00ZCKZ");
   }
--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/OutpatientClaimTransformerV2Test.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/OutpatientClaimTransformerV2Test.java
@@ -535,7 +535,7 @@ public final class OutpatientClaimTransformerV2Test {
                     "http://hl7.org/fhir/sid/icd-10",
                     "CD1YYZZ",
                     "PLANAR NUCL MED IMAG OF DIGESTIVE SYS USING OTH RADIONUCLIDE")),
-            "2016-01-16T00:00:00-08:00");
+            "2016-01-16T00:00:00Z");
 
     assertTrue(cmp1.equalsDeep(proc1), "Comparing Procedure code CD1YYZZ");
   }
