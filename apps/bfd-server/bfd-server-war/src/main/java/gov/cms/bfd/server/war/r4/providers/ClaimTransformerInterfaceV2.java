c07e6faef58efd8e3ccdf65434ab38eade7a0dfc
mackec1 <chris.mackenzie@adhocteam.us>
2023-07-13 11:21:13 -0400

    additional interface method; replaced Optional<Boolean> from transformer interface with boolean

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/r4/providers/ClaimTransformerInterfaceV2.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/r4/providers/ClaimTransformerInterfaceV2.java
@@ -1,17 +1,34 @@
 package gov.cms.bfd.server.war.r4.providers;
 
-import java.util.Optional;
 import org.hl7.fhir.r4.model.ExplanationOfBenefit;
 
 /** Interface defining a transform method for claim processing. */
-@FunctionalInterface
 public interface ClaimTransformerInterfaceV2 {
   /**
-   * Defines a single interface method to transform claim entities.
+   * Defines interface method to transform claims that do not require processing of an NPI tax ID
+   * number; those claims are: {@link ClaimTypeV2.HHA}. {@link ClaimTypeV2.HOSPICE}. {@link
+   * ClaimTypeV2.INPATIENT}. {@link ClaimTypeV2.OUTPATIENT}. {@link ClaimTypeV2.PDE}. {@link
+   * ClaimTypeV2.SNF}.
+   *
+   * <p>Will throw {@link BadCodeMonkeyException} if this transformer is used for processing {@link
+   * ClaimTypeV2.CARRIER} or {@link ClaimTypeV2.DME} claims.
    *
    * @param claimEntity generic entity {@link Object} associated with a claim type.
-   * @param includeTaxNumbers optional {@link Boolean} denoting inclusion of NPI tax info.
+   * @return {@link ExplanationOfBenefit} claims for
+   */
+  ExplanationOfBenefit transform(Object claimEntity);
+
+  /**
+   * Defines interface method to transform claims that do require processing of an NPI tax ID
+   * number.; those claims are: {@link ClaimTypeV2.CARRIER}. {@link ClaimTypeV2.DME}.
+   *
+   * <p>Will throw {@link BadCodeMonkeyException} if this transformer is used for processing {@link
+   * ClaimTypeV2.HHA}, {@link ClaimTypeV2.HOSPICE}, {@link ClaimTypeV2.INPATIENT}, {@link
+   * ClaimTypeV2.OUTPATIENT}, {@link ClaimTypeV2.PDE} or {@link ClaimTypeV2.SNF} claims.
+   *
+   * @param claimEntity generic entity {@link Object} associated with a claim type.
+   * @param includeTaxNumbers {@link boolean} denoting inclusion of tax numbers.
    * @return {@link ExplanationOfBenefit}
    */
-  public ExplanationOfBenefit transform(Object claimEntity, Optional<Boolean> includeTaxNumbers);
+  ExplanationOfBenefit transform(Object claimEntity, boolean includeTaxNumbers);
 }
