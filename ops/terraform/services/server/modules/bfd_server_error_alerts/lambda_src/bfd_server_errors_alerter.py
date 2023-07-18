ff5c25958f3059c75d1df65119dba683b374b509
Mitch Alessio <mitch.alessio@adhocteam.us>
2023-07-18 16:26:48 -0400

    Add Log Insights links to Slack notification

--- ops/terraform/services/server/modules/bfd_server_error_alerts/lambda_src/bfd_server_errors_alerter.py
+++ ops/terraform/services/server/modules/bfd_server_error_alerts/lambda_src/bfd_server_errors_alerter.py
@@ -5,8 +5,9 @@ import os
 import sys
 import time
 from datetime import datetime, timedelta
-from typing import Any, Callable, TypedDict
+from typing import Any, TypedDict
 from urllib.error import URLError
+from urllib.parse import quote_plus
 from urllib.request import Request, urlopen
 
 import boto3
@@ -26,13 +27,34 @@ BOTO_CONFIG = Config(
     },
 )
 
-LOG_INSIGHTS_QUERY = """
+LOG_INSIGHTS_NOTIF_QUERY = """
 fields
 mdc.http_access_response_status as status,
 mdc.http_access_request_clientSSL_DN as partner,
 mdc.http_access_request_operation as op
 | filter status == 500
 | stats count(status) by partner, op
+"""
+LOG_INSIGHTS_ALL_ERRORS_QUERY = f"""
+# This query returns the details for all 500s in {BFD_ENVIRONMENT}'s BFD Server for the selected
+# period (top-right). If you've opened Log Insights from a Slack alert and would like to
+# cross-reference a particular 500 with its exception (only available in messages.json), you will
+# need to also click the "View in ... messages.json ..." link from the Slack alert. You will then
+# need to run this query, and copy the original timestamp to use as a unique value for cross-
+# referencing
+
+fields @message,
+mdc.http_access_response_status as status
+| filter status == 500
+"""
+LOG_INSIGHTS_CROSS_REF_MESSAGES_QUERY = """
+# To cross-reference the exception for a particular 500, you'll need to query on a field with a
+# matching and unique value that is logged in both access.json and messages.json. Fortunately, the
+# original query timestamp is such a field.
+
+fields @message,
+`mdc.http_access_request_header_Bluebutton-Originalquerytimestamp` as orig_timestamp
+| filter orig_timestamp == 'REPLACE WITH TIMESTAMP OF MESSAGE FROM ACCESS.JSON'
 """
 
 
@@ -66,6 +88,38 @@ def __get_value_by_key(
     )["value"]
 
 
+def __escape_log_insights_url_str(unescaped_str: str) -> str:
+    return quote_plus(unescaped_str).replace("%", "*")
+
+
+def __gen_log_insights_url(
+    start_time: datetime, end_time: datetime, editor_string: str, source_log_group: str
+) -> str:
+    # Log Insights uses a _strange_ escape and query string scheme that fully and properly
+    # implementing would be too much effort. Instead, some shortcuts are made here with the
+    # assumption that these URLs will have _very_ little variation in what data is used to generate
+    # them.
+
+    url_prefix = f"https://{REGION}.console.aws.amazon.com/cloudwatch/home?region={REGION}#logsV2:logs-insights$3FqueryDetail$3D~"
+    start_time_iso = f"{__escape_log_insights_url_str(start_time.isoformat(timespec='seconds'))}.000Z"
+    end_time_iso = (
+        f"{__escape_log_insights_url_str(end_time.isoformat(timespec='seconds'))}.000Z"
+    )
+    editor_string = __escape_log_insights_url_str(editor_string)
+    query_params = {
+        "end": end_time_iso,
+        "start": start_time_iso,
+        "timeType": "ABSOLUTE",
+        "tz": "UTC",
+        "editorString": editor_string,
+    }
+    # For whatever reason, '=' is escaped as "~'"
+    query_param_fragments = [f"{k}~'{v}" for k, v in query_params.items()]
+    query_params_str = f"({'~'.join(query_param_fragments)}~source~(~'{__escape_log_insights_url_str(source_log_group)}))"
+
+    return f"{url_prefix}{query_params_str}"
+
+
 def handler(event: Any, context: Any):
     if not all(
         [
@@ -78,7 +132,8 @@ def handler(event: Any, context: Any):
         logger.error("Not all necessary environment variables were defined, exiting...")
         return
 
-    utc_now = datetime.utcnow()
+    utc_now = datetime.fromisoformat("2023-03-03T15:55:00")
+    # utc_now = datetime.utcnow()
     start_time = utc_now - timedelta(seconds=LOG_LOOKBACK_SECONDS)
     start_time_timestamp = calendar.timegm(start_time.utctimetuple())
     end_time = utc_now
@@ -96,7 +151,7 @@ def handler(event: Any, context: Any):
             logGroupName=LOG_GROUP,
             startTime=start_time_timestamp,
             endTime=end_time_timestamp,
-            queryString=LOG_INSIGHTS_QUERY,
+            queryString=LOG_INSIGHTS_NOTIF_QUERY,
         )
     except logs_client.exceptions.ClientError:
         logger.error(
@@ -197,6 +252,18 @@ def handler(event: Any, context: Any):
 
         per_partner_tables[partner] = table_block_str
 
+    log_insights_access_json_url = __gen_log_insights_url(
+        start_time=start_time,
+        end_time=end_time,
+        editor_string=LOG_INSIGHTS_ALL_ERRORS_QUERY,
+        source_log_group=LOG_GROUP,
+    )
+    log_insights_messages_json_url = __gen_log_insights_url(
+        start_time=start_time,
+        end_time=end_time,
+        editor_string=LOG_INSIGHTS_CROSS_REF_MESSAGES_QUERY,
+        source_log_group=f"/bfd/{BFD_ENVIRONMENT}/bfd-server/messages.json",
+    )
     slack_message = {
         "blocks": [
             {
@@ -232,6 +299,18 @@ def handler(event: Any, context: Any):
                             f"*{partner}*\n{table}"
                             for partner, table in per_partner_tables.items()
                         )
+                    ),
+                },
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": (
+                        f"\n<{log_insights_access_json_url}|View"
+                        f" {BFD_ENVIRONMENT} access.json in Log"
+                        f" Insights>\n<{log_insights_messages_json_url}| View"
+                        f" {BFD_ENVIRONMENT} messages.json in Log Insights>"
                     ),
                 },
             },
