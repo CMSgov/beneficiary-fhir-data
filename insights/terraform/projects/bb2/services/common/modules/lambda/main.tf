1a1c436cfb16feaeb84d2e260be917183dccb38e
Dave Tisza <dtisza@fearless.tech>
2023-07-18 11:56:04 -0400

    Update dashes to underscores per change req

--- insights/terraform/projects/bb2/services/common/modules/lambda/main.tf
+++ insights/terraform/projects/bb2/services/common/modules/lambda/main.tf
@@ -5,7 +5,7 @@ data "aws_kms_key" "bucket_cmk" {
 }
 
 # Zip File containing Lambda script
-data "archive_file" "zip-archive-format-firehose-logs" {
+data "archive_file" "zip_archive_format_firehose_logs" {
   type        = "zip"
   source_file = "${path.module}/lambda_src/lambda_function.py"
   output_path = "${path.module}/lambda_function.zip"
@@ -27,7 +27,7 @@ resource "aws_lambda_function" "firehose_log_processor_lambda" {
     "x86_64",
   ]
   description                    = var.description
-  filename                       = data.archive_file.zip-archive-format-firehose-logs.output_path
+  filename                       = data.archive_file.zip_archive_format_firehose_logs.output_path
   handler                        = "lambda_function.lambda_handler"
   layers                         = []
   memory_size                    = 128
@@ -36,7 +36,7 @@ resource "aws_lambda_function" "firehose_log_processor_lambda" {
   role                           = local.lambda_role_arn
   publish                        = false
   runtime                        = "python3.8"
-  source_code_hash               = data.archive_file.zip-archive-format-firehose-logs.output_base64sha256
+  source_code_hash               = data.archive_file.zip_archive_format_firehose_logs.output_base64sha256
 
   tags = {
     "lambda-console:blueprint" = "kinesis-firehose-cloudwatch-logs-processor-python",
