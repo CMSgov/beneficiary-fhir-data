5744b8ac651ab1ee03b21304354e4ea77da959bb
mackec1 <chris.mackenzie@adhocteam.us>
2023-07-20 11:31:09 -0400

    revert back to single method for the interface

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/r4/providers/ClaimTransformerInterfaceV2.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/r4/providers/ClaimTransformerInterfaceV2.java
@@ -1,35 +1,18 @@
 package gov.cms.bfd.server.war.r4.providers;
 
-import gov.cms.bfd.sharedutils.exceptions.BadCodeMonkeyException;
 import org.hl7.fhir.r4.model.ExplanationOfBenefit;
 
 /** Interface defining a transform method for claim processing. */
 public interface ClaimTransformerInterfaceV2 {
   /**
-   * Defines interface method to transform claims that do not require processing of an NPI tax ID
-   * number; those claims are: {@link ClaimTypeV2#HHA}. {@link ClaimTypeV2#HOSPICE}. {@link
-   * ClaimTypeV2#INPATIENT}. {@link ClaimTypeV2#OUTPATIENT}. {@link ClaimTypeV2#PDE}. {@link
-   * ClaimTypeV2#SNF}.
-   *
-   * <p>Will throw {@link BadCodeMonkeyException} if this transformer is used for processing {@link
-   * ClaimTypeV2#CARRIER} or {@link ClaimTypeV2#DME} claims.
-   *
-   * @param claimEntity generic entity {@link Object} associated with a claim type.
-   * @return {@link ExplanationOfBenefit} claims for
-   */
-  ExplanationOfBenefit transform(Object claimEntity);
-
-  /**
-   * Defines interface method to transform claims that do require processing of an NPI tax ID
-   * number.; those claims are: {@link ClaimTypeV2#CARRIER}. {@link ClaimTypeV2#DME}.
-   *
-   * <p>Will throw {@link BadCodeMonkeyException} if this transformer is used for processing {@link
-   * ClaimTypeV2#HHA}, {@link ClaimTypeV2#HOSPICE}, {@link ClaimTypeV2#INPATIENT}, {@link
-   * ClaimTypeV2#OUTPATIENT}, {@link ClaimTypeV2#PDE} or {@link ClaimTypeV2#SNF} claims.
+   * Interface method to transform claims; if the Claim type supports inclusion of NPI tax
+   * information, and the boolean includeTaxNumbers is true, then the transformer will attempt to
+   * provide NPI tax info.
    *
    * @param claimEntity generic entity {@link Object} associated with a claim type.
    * @param includeTaxNumbers {@link boolean} denoting inclusion of tax numbers.
    * @return {@link ExplanationOfBenefit}
+   * @throws {@link Exception}
    */
   ExplanationOfBenefit transform(Object claimEntity, boolean includeTaxNumbers);
 }
--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/stu3/providers/ClaimTransformerInterface.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/stu3/providers/ClaimTransformerInterface.java
@@ -1,31 +1,13 @@
 package gov.cms.bfd.server.war.stu3.providers;
 
-import gov.cms.bfd.sharedutils.exceptions.BadCodeMonkeyException;
 import org.hl7.fhir.dstu3.model.ExplanationOfBenefit;
 
 /** Interface defining a transform method for claim processing. */
 public interface ClaimTransformerInterface {
   /**
-   * Defines interface method to transform claims that do not require processing of an NPI tax ID
-   * number; those claims are: {@link ClaimType#HHA}. {@link ClaimType#HOSPICE}. {@link
-   * ClaimType#INPATIENT}. {@link ClaimType#OUTPATIENT}. {@link ClaimType#PDE}. {@link
-   * ClaimType#SNF}.
-   *
-   * <p>Will throw {@link BadCodeMonkeyException} if this transformer is used for processing {@link
-   * ClaimType#CARRIER} or {@link ClaimType#DME} claims.
-   *
-   * @param claimEntity generic entity {@link Object} associated with a claim type.
-   * @return {@link ExplanationOfBenefit} claims for
-   */
-  ExplanationOfBenefit transform(Object claimEntity);
-
-  /**
-   * Defines interface method to transform claims that do require processing of an NPI tax ID
-   * number.; those claims are: {@link ClaimType#CARRIER}. {@link ClaimType#DME}.
-   *
-   * <p>Will throw {@link BadCodeMonkeyException} if this transformer is used for processing {@link
-   * ClaimType#HHA}, {@link ClaimType#HOSPICE}, {@link ClaimType#INPATIENT}, {@link
-   * ClaimType#OUTPATIENT}, {@link ClaimType#PDE} or {@link ClaimType#SNF} claims.
+   * Interface method to transform claims; if the Claim type supports inclusion of NPI tax
+   * information, and the boolean includeTaxNumbers is true, then the transformer will attempt to
+   * provide NPI tax info.
    *
    * @param claimEntity generic entity {@link Object} associated with a claim type.
    * @param includeTaxNumbers {@link boolean} denoting inclusion of tax numbers.
