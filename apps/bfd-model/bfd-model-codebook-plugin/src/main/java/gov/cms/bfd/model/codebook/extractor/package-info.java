3fee99ef1f192044349ec5e88e468766770080e8
Brian Burton <brian.burton@adhocteam.us>
2023-08-11 10:39:59 -0400

    Refactoring package-infos.

--- apps/bfd-model/bfd-model-codebook-plugin/src/main/java/gov/cms/bfd/model/codebook/extractor/package-info.java
+++ apps/bfd-model/bfd-model-codebook-plugin/src/main/java/gov/cms/bfd/model/codebook/extractor/package-info.java
@@ -1,6 +1,6 @@
 /**
- * Contains utilties for converting <a
+ * Contains maven plugin for converting <a
  * href="https://www.ccwdata.org/web/guest/data-dictionaries">CMS Chronic Conditions Warehouse (CCW)
- * data dictionary</a> codebooks to XML.
+ * data dictionary</a> codebooks to XML and an enum class.
  */
 package gov.cms.bfd.model.codebook.extractor;
