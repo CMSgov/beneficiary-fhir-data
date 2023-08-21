cd9c98d9085c115c4ea9e8bc549ebc33cf6a3048
Brian Burton <brian.burton@adhocteam.us>
2023-08-21 09:00:09 -0400

    Adds TTL to passwords received from RDS.

--- apps/bfd-shared-utils/src/main/java/gov/cms/bfd/sharedutils/database/RdsDataSourceFactory.java
+++ apps/bfd-shared-utils/src/main/java/gov/cms/bfd/sharedutils/database/RdsDataSourceFactory.java
@@ -1,23 +1,35 @@
 package gov.cms.bfd.sharedutils.database;
 
 import com.zaxxer.hikari.HikariDataSource;
+import java.time.Clock;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.services.rds.RdsClient;
 import software.amazon.awssdk.services.rds.RdsClientBuilder;
 import software.amazon.awssdk.services.rds.model.GenerateAuthenticationTokenRequest;
 
+import javax.annotation.concurrent.GuardedBy;
+
 @AllArgsConstructor
 public class RdsDataSourceFactory implements DataSourceFactory {
+  private static final long TokenTTLMillis = 5_000;
+  private final Clock clock;
   private final RdsClientConfig rdsClientConfig;
   private final HikariDataSourceFactory realDataSourceFactory;
   private final RdsClient rdsClient;
 
-  public RdsDataSourceFactory(RdsClientConfig rdsClientConfig, DatabaseOptions databaseOptions) {
+  public RdsDataSourceFactory(
+      Clock clock, RdsClientConfig rdsClientConfig, DatabaseOptions databaseOptions) {
+    this.clock = clock;
     this.rdsClientConfig = rdsClientConfig;
     realDataSourceFactory = new HikariDataSourceFactory(databaseOptions, RdsHikariDataSource::new);
     rdsClient = createRdsClient(rdsClientConfig);
+  }
+
+  public RdsDataSourceFactory(RdsClientConfig rdsClientConfig, DatabaseOptions databaseOptions) {
+    this(Clock.systemUTC(), rdsClientConfig, databaseOptions);
   }
 
   @Override
@@ -45,10 +57,38 @@ public class RdsDataSourceFactory implements DataSourceFactory {
   }
 
   @NoArgsConstructor
-  class RdsHikariDataSource extends HikariDataSource {
+  public class RdsHikariDataSource extends HikariDataSource {
+    /**
+     * Because we are subclassing {@link HikariDataSource} its safest not to use a synchronized
+     * method to synchronized token updates. Instead we synchronize on this private object.
+     */
+    private final Object tokenLock = new Object();
+
+    /** Time in millis at which we consider our token to have expired. */
+    @GuardedBy("tokenLock")
+    @Getter
+    private long expires;
+
+    /** Most recently generated token. */
+    @GuardedBy("tokenLock")
+    @Getter
+    private String token;
+
+    /**
+     * Overrides default method to return a token obtained from RDS.
+     *
+     * @return a token from RDS
+     */
     @Override
     public String getPassword() {
-      return generateToken();
+      synchronized (tokenLock) {
+        long now = clock.millis();
+        if (token == null || now > expires) {
+          token = generateToken();
+          expires = now + TokenTTLMillis;
+        }
+        return token;
+      }
     }
   }
 }
