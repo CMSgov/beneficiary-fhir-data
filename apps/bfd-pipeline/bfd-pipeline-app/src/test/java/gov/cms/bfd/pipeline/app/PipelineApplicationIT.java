149ca7ae4c637f91e3af8131275fa9c3de98c8f2
Brian Burton <brian.burton@adhocteam.us>
2023-09-05 16:12:18 -0400

    Verifies that all files have been moved after pipeline exits.

--- apps/bfd-pipeline/bfd-pipeline-app/src/test/java/gov/cms/bfd/pipeline/app/PipelineApplicationIT.java
+++ apps/bfd-pipeline/bfd-pipeline-app/src/test/java/gov/cms/bfd/pipeline/app/PipelineApplicationIT.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
 
+import com.google.common.collect.ImmutableSet;
 import gov.cms.bfd.DataSourceComponents;
 import gov.cms.bfd.DatabaseTestUtils;
 import gov.cms.bfd.FileBasedAssertionHelper;
@@ -24,12 +25,14 @@ import gov.cms.bfd.pipeline.rda.grpc.RdaMcsClaimLoadJob;
 import gov.cms.bfd.pipeline.rda.grpc.server.RandomClaimGeneratorConfig;
 import gov.cms.bfd.pipeline.rda.grpc.server.RdaMessageSourceFactory;
 import gov.cms.bfd.pipeline.rda.grpc.server.RdaServer;
+import gov.cms.bfd.pipeline.sharedutils.s3.S3Dao;
 import gov.cms.bfd.sharedutils.config.ConfigLoader;
 import java.nio.file.Path;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Predicate;
 import javax.sql.DataSource;
 import org.apache.commons.codec.binary.Hex;
@@ -192,6 +195,19 @@ public final class PipelineApplicationIT extends AbstractLocalStackS3Test {
       assertEquals(PipelineApplication.EXIT_CODE_SUCCESS, exitCode);
       assertCcwRifLoadJobCompleted(logLines);
       assertADataSetBeenProcessed(logLines);
+
+      // Verify all files were moved into the done "folder".
+      final String keyPrefix =
+          DataSetTestUtilities.keyPrefixForManifest(
+                  CcwRifLoadJob.S3_PREFIX_COMPLETED_DATA_SETS, manifest)
+              + "/";
+      final Set<String> completedFiles =
+          s3Dao
+              .listObjects(bucket, keyPrefix)
+              .map(S3Dao.S3ObjectSummary::getKey)
+              .map(key -> key.substring(keyPrefix.length()))
+              .collect(ImmutableSet.toImmutableSet());
+      assertEquals(Set.of("0_manifest.xml", "beneficiaries.rif", "carrier.rif"), completedFiles);
     } finally {
       if (StringUtils.isNotBlank(bucket)) {
         s3Dao.deleteTestBucket(bucket);
