9f39a6a38f154aa9edc7734df6a963137d281165
Mitch Alessio <mitch.alessio@adhocteam.us>
2023-09-11 12:51:10 -0400

    Use partner hierarchies instead of partner-suffixed keys within BFD's SSM hierarchy; update placeholder keys to new format

--- ops/terraform/services/eft/main.tf
+++ ops/terraform/services/eft/main.tf
@@ -11,53 +11,76 @@ module "terraservice" {
 }
 
 locals {
-
   default_tags     = module.terraservice.default_tags
   env              = module.terraservice.env
   seed_env         = module.terraservice.seed_env
   is_ephemeral_env = module.terraservice.is_ephemeral_env
 
-
   service   = "eft"
   layer     = "data"
   full_name = "bfd-${local.env}-${local.service}"
 
-  nonsensitive_common_map = zipmap(
-    data.aws_ssm_parameters_by_path.nonsensitive_common.names,
-    nonsensitive(data.aws_ssm_parameters_by_path.nonsensitive_common.values)
-  )
-  nonsensitive_common_config = {
-    for key, value in local.nonsensitive_common_map
-    : split("/", key)[5] => value
+  eft_partners    = jsondecode(nonsensitive(data.aws_ssm_parameter.partners_list_json.value))
+  ssm_hierarchies = concat(["bfd"], local.eft_partners)
+  ssm_services    = ["common", local.service]
+  ssm_all_paths = {
+    for x in setproduct(local.ssm_hierarchies, local.ssm_services) :
+    "${x[0]}-${x[1]}" => {
+      hierarchy = x[0]
+      service   = x[1]
+    }
   }
-  sensitive_service_map = zipmap(
-    data.aws_ssm_parameters_by_path.sensitive_service.names,
-    nonsensitive(data.aws_ssm_parameters_by_path.sensitive_service.values)
-  )
-  sensitive_service_config = {
-    for key, value in local.sensitive_service_map : split("/", key)[5] => value
+  # This returns an object with nested keys in the following format: ssm_config.<top-level hierarchy
+  # name>.<service name>; for example, to get a parameter named "vpc_name" in BFD's hierarchy in the
+  # "common" service, it would be: local.ssm_config.bfd.vpc_name.
+  # FUTURE: Refactor this out into a distinct module much like bfd-terraservice above
+  ssm_config = {
+    # This would be much easier if Terraform had a reduce() or if merge() was a deep merge instead
+    # of shallow. We must do a double iteration otherwise we end up with objects with the same
+    # top-level keys but different inner keys (i.e. {bfd = common = ...} and {bfd = eft = ...}).
+    # Using merge() to merge those would result in only the last object being taken ({bfd = eft = ...})
+    for hierarchy in local.ssm_hierarchies :
+    hierarchy => {
+      for key, meta in local.ssm_all_paths :
+      # We know all services within a given hierarchy are distinct, so we can just iterate over them
+      # and build a full object at once.
+      "${meta.service}" => zipmap(
+        [
+          for name in concat(
+            data.aws_ssm_parameters_by_path.nonsensitive[key].names,
+            data.aws_ssm_parameters_by_path.sensitive[key].names
+          ) : element(split("/", name), length(split("/", name)) - 1)
+        ],
+        nonsensitive(
+          concat(
+            data.aws_ssm_parameters_by_path.nonsensitive[key].values,
+            data.aws_ssm_parameters_by_path.sensitive[key].values
+          )
+        )
+      )
+      if hierarchy == meta.hierarchy
+    }
   }
 
   # SSM Lookup
-  kms_key_alias = local.nonsensitive_common_config["kms_key_alias"]
-  vpc_name      = local.nonsensitive_common_config["vpc_name"]
+  kms_key_alias = local.ssm_config.bfd.common["kms_key_alias"]
+  vpc_name      = local.ssm_config.bfd.common["vpc_name"]
 
   subnet_ip_reservations = jsondecode(
-    local.sensitive_service_config["subnet_to_ip_reservations_nlb_json"]
+    local.ssm_config.bfd[local.service]["subnet_to_ip_reservations_nlb_json"]
   )
-  host_key              = local.sensitive_service_config["sftp_transfer_server_host_private_key"]
-  eft_r53_hosted_zone   = local.sensitive_service_config["r53_hosted_zone"]
-  eft_user_sftp_pub_key = local.sensitive_service_config["sftp_eft_user_public_key"]
-  eft_user_username     = local.sensitive_service_config["sftp_eft_user_username"]
-  eft_partners          = jsondecode(local.sensitive_service_config["partners_list_json"])
+  host_key              = local.ssm_config.bfd[local.service]["sftp_transfer_server_host_private_key"]
+  eft_r53_hosted_zone   = local.ssm_config.bfd[local.service]["r53_hosted_zone"]
+  eft_user_sftp_pub_key = local.ssm_config.bfd[local.service]["sftp_eft_user_public_key"]
+  eft_user_username     = local.ssm_config.bfd[local.service]["sftp_eft_user_username"]
   eft_partners_config = {
     for partner in local.eft_partners :
     partner => {
-      bucket_iam_assumer_arn             = local.sensitive_service_config["partner_bucket_iam_assumer_arn_${partner}"]
-      bucket_home_path                   = trim(local.sensitive_service_config["partner_bucket_home_path_${partner}"], "/")
-      bucket_notifs_subscriber_principal = lookup(local.sensitive_service_config, "partner_bucket_notifications_subscriber_principal_arn_${partner}", null)
-      bucket_notifs_subscriber_arn       = lookup(local.sensitive_service_config, "partner_bucket_notifications_subscriber_arn_${partner}", null)
-      bucket_notifs_subscriber_protocol  = lookup(local.sensitive_service_config, "partner_bucket_notifications_subscriber_protocol_${partner}", null)
+      bucket_iam_assumer_arn             = local.ssm_config[partner][local.service]["bucket_iam_assumer_arn"]
+      bucket_home_path                   = trim(local.ssm_config[partner][local.service]["bucket_home_path"], "/")
+      bucket_notifs_subscriber_principal = lookup(local.ssm_config[partner][local.service], "bucket_notifications_subscriber_principal_arn", null)
+      bucket_notifs_subscriber_arn       = lookup(local.ssm_config[partner][local.service], "bucket_notifications_subscriber_arn", null)
+      bucket_notifs_subscriber_protocol  = lookup(local.ssm_config[partner][local.service], "bucket_notifications_subscriber_protocol", null)
     }
   }
   eft_partners_with_s3_notifs = [
