b3072c498f6869a43d26f02617a61eb28a9b5337
mackec1 <chris.mackenzie@adhocteam.us>
2023-09-12 14:24:55 -0400

    minor cleanup and formatting

--- ops/ccs-ops-misc/tls-certificates/update_ssm_param_client_certs.sh
+++ ops/ccs-ops-misc/tls-certificates/update_ssm_param_client_certs.sh
@@ -5,6 +5,7 @@ set -e
 
 CERT_FILE_OR_DIR="$1"
 MODE="${2:-prod}"
+SHOW_SSM="${3:-false}"
 SSM_DIR=""
 SUBJ_CN=""
 
@@ -21,13 +22,13 @@ fi
 
 
 function getCertCN () {
-	cn_val=$(openssl x509 -in $1 -inform pem -noout -subject | awk '{ print $3 }')
+  cn_val=$(openssl x509 -noout -subject -nameopt multiline -in $1  | sed -n 's/ *commonName *= //p')
 	if [[ -z "$cn_val" ]]; then
       echo "Failed to derive CN from provided file: $1...exiting!"
       exit ;
-    fi
-    # replace any spaces with underscores
-    cn_val="${cn_val// /_}"
+  fi
+  # replace any spaces with underscores
+  cn_val="${cn_val// /_}"
 	echo $cn_val
 }
 
@@ -36,23 +37,26 @@ function updateSsm () {
     aws ssm put-parameter --name "$SSM_KEY" --type "String" --overwrite --value "$(cat $2)"
     echo "Updated SSM Parameter Store using: $SSM_KEY"
     
-    aws ssm get-parameter --name $SSM_KEY
+    if [[ "${SHOW_SSM}" == true ]]; then
+      aws ssm get-parameter --name $SSM_KEY
+    fi
     echo "-------------------------------------------"
+    echo " "
 }
 
 function checkFileFromDir {
-  echo "checking file: $1"
-  if grep -q "-----BEGIN CERTIFICATE-----" "$1"; then
+  if grep -q -e "-----BEGIN CERTIFICATE-----" "$1" && grep -q -e "-----END CERTIFICATE-----" "$1"; then
+    echo "+++++++++++++++++++++++++++++++++++++++++++"
     SUBJ_CN=$(getCertCN $1)
     updateSsm $SUBJ_CN $1
-    echo "-------------------------------------------"
   else
-    echo "Ignoring file that does not appear to be PEM-encoded cert: $1"
+    echo "***** Ignoring file that does not appear to be PEM-encoded cert: $1"
   fi
 }
 
 # are we dealing with a single file or directory of files?
 if [[ -f "$CERT_FILE_OR_DIR" ]]; then
+  echo "+++++++++++++++++++++++++++++++++++++++++++"
   SUBJ_CN=$(getCertCN $CERT_FILE_OR_DIR)
   updateSsm $SUBJ_CN $CERT_FILE_OR_DIR
 elif [[ -d "$CERT_FILE_OR_DIR" ]]; then
@@ -63,7 +67,3 @@ else
 fi
 
 exit 0;
-
-
-
-
