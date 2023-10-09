e48614fb1e494a80a980c8715686ee2873c8d23b
Logan Mitchell <logan.mitchell@adhocteam.us>
2023-10-09 13:08:09 -0700

    fix doc

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/CoverageE2E.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/CoverageE2E.java
@@ -3,7 +3,13 @@ package gov.cms.bfd.server.war.r4.providers;
 import gov.cms.bfd.server.war.CoverageE2EBase;
 import org.junit.jupiter.api.BeforeEach;
 
-/** Endpoint end-to-end test for the V2 Coverage endpoint. */
+/**
+ * Endpoint end-to-end test for the V2 Coverage endpoint. Most test logic should be placed in {@link
+ * CoverageE2EBase} to be shared, unless there are version-specific paths or functionality to test.
+ *
+ * <p>To run individual tests in-IDE, ensure you use a view that shows inherited tests (like
+ * IntelliJ's Structure panel with the "Inherited" option at the top)
+ */
 public class CoverageE2E extends CoverageE2EBase {
 
   /** Sets up the test resources. */
