1782f55b700c87cb3b045854e5fce77f6286496b
Ade Adedeji <aadedeji@fearless.tech>
2023-10-20 13:03:42 -0400

    Removed disabled annotation for serializeSampleARecord Test

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/CarrierClaimTransformerV2Test.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/r4/providers/CarrierClaimTransformerV2Test.java
@@ -52,7 +52,6 @@ import org.hl7.fhir.r4.model.Money;
 import org.hl7.fhir.r4.model.Quantity;
 import org.hl7.fhir.r4.model.Reference;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -147,11 +146,9 @@ public class CarrierClaimTransformerV2Test {
    * Serializes the EOB and prints to the command line.
    *
    * @throws FHIRException if there is an issue with transforming the claim
-   * @throws IOException if there is an issue with reading the test file
    */
-  @Disabled
   @Test
-  public void serializeSampleARecord() throws FHIRException, IOException {
+  public void serializeSampleARecord() throws FHIRException {
     ExplanationOfBenefit eob = carrierClaimTransformer.transform(generateClaim(), false);
     System.out.println(fhirContext.newJsonParser().encodeResourceToString(eob));
   }
