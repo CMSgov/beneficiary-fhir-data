14d18080210019bf5df26d03202651cf759be045
Logan Mitchell <logan.mitchell@adhocteam.us>
2023-10-25 11:37:29 -0700

    remove retired header from MDC list, adjust test to clear access json between tests, fix assertion to be more useful

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/AccessJsonE2E.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/AccessJsonE2E.java
@@ -1,18 +1,28 @@
 package gov.cms.bfd.server.war;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import gov.cms.bfd.model.rif.entities.Beneficiary;
 import gov.cms.bfd.server.sharedutils.BfdMDC;
 import gov.cms.bfd.server.war.commons.TransformerConstants;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Verifies that access.json is written to as expected. */
 public class AccessJsonE2E extends ServerRequiredTest {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(AccessJsonE2E.class);
+
   /** Verifies that access.json is written to within BFD-server-war via API call. */
   @Test
   public void verifyAccessJson() throws IOException {
@@ -26,62 +36,88 @@ public class AccessJsonE2E extends ServerRequiredTest {
             + "|"
             + mbiHash;
 
-    given().spec(requestAuth).expect().statusCode(200).when().get(requestString);
-
-    // Verify that the access JSON is working, as expected.
-    /*try {
-      TimeUnit.MILLISECONDS.sleep(1000); // Needed to resolve a race condition
-    } catch (InterruptedException ignored) {
-    }*/
-
     Path accessLogJson =
         ServerTestUtils.getWarProjectDirectory()
             .resolve("target")
             .resolve("server-work")
             .resolve("access.json");
+
+    // Empty the access json to avoid pollution from other tests
+    try (BufferedWriter writer = Files.newBufferedWriter(accessLogJson)) {
+      writer.write("");
+      writer.flush();
+    }
+
+    given()
+        .spec(requestAuth)
+        .contentType("application/json")
+        .expect()
+        .statusCode(200)
+        .when()
+        .get(requestString);
+
+    // Verify that the access JSON is working, as expected.
+    try {
+      TimeUnit.MILLISECONDS.sleep(1000); // Needed to resolve a race condition
+    } catch (InterruptedException ignored) {
+    }
+
     assertTrue(Files.isReadable(accessLogJson));
     assertTrue(Files.size(accessLogJson) > 0);
 
     String content = Files.readString(accessLogJson);
-    assertTrue(content.contains(BfdMDC.BENE_ID));
-    assertTrue(content.contains(BfdMDC.DATABASE_QUERY_BATCH));
-    assertTrue(content.contains(BfdMDC.DATABASE_QUERY_TYPE));
-    assertTrue(content.contains(BfdMDC.DATABASE_QUERY_BATCH_SIZE));
-    assertTrue(content.contains(BfdMDC.DATABASE_QUERY_SIZE));
-    assertTrue(content.contains(BfdMDC.DATABASE_QUERY_MILLI));
-    assertTrue(content.contains(BfdMDC.DATABASE_QUERY_SUCCESS));
-    assertTrue(content.contains(BfdMDC.DATABASE_QUERY_SOURCE_NAME));
-    assertTrue(content.contains(BfdMDC.HAPI_RESPONSE_TIMESTAMP_MILLI));
-    assertTrue(content.contains(BfdMDC.HAPI_POST_PROCESS_TIMESTAMP_MILLI));
-    assertTrue(content.contains(BfdMDC.HAPI_PRE_HANDLE_TIMESTAMP_MILLI));
-    assertTrue(content.contains(BfdMDC.HAPI_PRE_HANDLE_TIMESTAMP_MILLI));
-    assertTrue(content.contains(BfdMDC.HAPI_PRE_PROCESS_TIMESTAMP_MILLI));
-    assertTrue(content.contains(BfdMDC.HAPI_PROCESSING_COMPLETED_TIMESTAMP_MILLI));
-    assertTrue(content.contains(BfdMDC.HAPI_PROCESSING_COMPLETED_NORM_TIMESTAMP_MILLI));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_REQUEST_CLIENTSSL_DN));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_REQUEST_HEADER_ACCEPT));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_REQUEST_HEADER_ACCEPT_CHARSET));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_REQUEST_HEADER_ACCEPT_ENCODING));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_REQUEST_HEADER_CONN_ENCODING));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_REQUEST_HEADER_HOST_ENCODING));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_REQUEST_HEADER_USER_AGENT));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_REQUEST_HTTP_METHOD));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_REQUEST_OPERATION));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_REQUEST_QUERY_STR));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_REQUEST_TYPE));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_REQUEST_URI));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_REQUEST_URL));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_RESPONSE_DURATION_PER_KB));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_RESPONSE_DURATION_MILLISECONDS));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_RESPONSE_HEADER_ENCODING));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_RESPONSE_HEADER_LOCATION));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_RESPONSE_HEADER_TYPE));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_RESPONSE_HEADER_DATE));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_RESPONSE_HEADER_LAST_MODIFIED));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_RESPONSE_HEADER_POWERED_BY));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_RESPONSE_HEADER_REQUEST_ID));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_RESPONSE_OUTPUT_SIZE_IN_BYTES));
-    assertTrue(content.contains(BfdMDC.HTTP_ACCESS_RESPONSE_STATUS));
-    assertTrue(content.contains(BfdMDC.RESOURCES_RETURNED));
+
+    LOGGER.info(content);
+
+    List<String> headersToCheck =
+        List.of(
+            BfdMDC.BENE_ID,
+            BfdMDC.DATABASE_QUERY_BATCH,
+            BfdMDC.DATABASE_QUERY_TYPE,
+            BfdMDC.DATABASE_QUERY_BATCH_SIZE,
+            BfdMDC.DATABASE_QUERY_SIZE,
+            BfdMDC.DATABASE_QUERY_MILLI,
+            BfdMDC.DATABASE_QUERY_SUCCESS,
+            BfdMDC.DATABASE_QUERY_SOURCE_NAME,
+            BfdMDC.HAPI_RESPONSE_TIMESTAMP_MILLI,
+            BfdMDC.HAPI_POST_PROCESS_TIMESTAMP_MILLI,
+            BfdMDC.HAPI_PRE_HANDLE_TIMESTAMP_MILLI,
+            BfdMDC.HAPI_PRE_HANDLE_TIMESTAMP_MILLI,
+            BfdMDC.HAPI_PRE_PROCESS_TIMESTAMP_MILLI,
+            BfdMDC.HAPI_PROCESSING_COMPLETED_TIMESTAMP_MILLI,
+            BfdMDC.HAPI_PROCESSING_COMPLETED_NORM_TIMESTAMP_MILLI,
+            BfdMDC.HTTP_ACCESS_REQUEST_CLIENTSSL_DN,
+            BfdMDC.HTTP_ACCESS_REQUEST_HEADER_ACCEPT,
+            BfdMDC.HTTP_ACCESS_REQUEST_HEADER_ACCEPT_ENCODING,
+            BfdMDC.HTTP_ACCESS_REQUEST_HEADER_CONN_ENCODING,
+            BfdMDC.HTTP_ACCESS_REQUEST_HEADER_HOST_ENCODING,
+            BfdMDC.HTTP_ACCESS_REQUEST_HEADER_USER_AGENT,
+            BfdMDC.HTTP_ACCESS_REQUEST_HTTP_METHOD,
+            BfdMDC.HTTP_ACCESS_REQUEST_OPERATION,
+            BfdMDC.HTTP_ACCESS_REQUEST_QUERY_STR,
+            BfdMDC.HTTP_ACCESS_REQUEST_TYPE,
+            BfdMDC.HTTP_ACCESS_REQUEST_URI,
+            BfdMDC.HTTP_ACCESS_REQUEST_URL,
+            BfdMDC.HTTP_ACCESS_RESPONSE_DURATION_PER_KB,
+            BfdMDC.HTTP_ACCESS_RESPONSE_DURATION_MILLISECONDS,
+            BfdMDC.HTTP_ACCESS_RESPONSE_DURATION_MILLISECONDS,
+            BfdMDC.HTTP_ACCESS_RESPONSE_HEADER_ENCODING,
+            BfdMDC.HTTP_ACCESS_RESPONSE_HEADER_TYPE,
+            BfdMDC.HTTP_ACCESS_RESPONSE_HEADER_DATE,
+            BfdMDC.HTTP_ACCESS_RESPONSE_HEADER_LAST_MODIFIED,
+            BfdMDC.HTTP_ACCESS_RESPONSE_HEADER_POWERED_BY,
+            BfdMDC.HTTP_ACCESS_RESPONSE_HEADER_REQUEST_ID,
+            BfdMDC.HTTP_ACCESS_RESPONSE_OUTPUT_SIZE_IN_BYTES,
+            BfdMDC.HTTP_ACCESS_RESPONSE_STATUS,
+            BfdMDC.RESOURCES_RETURNED);
+
+    List<String> missingHeader = new ArrayList<>();
+    for (String header : headersToCheck) {
+      if (!content.contains(header)) {
+        missingHeader.add(header);
+      }
+    }
+    // Check we have no missing headers in a way that makes a useful assertion error if not
+    assertEquals(new ArrayList<>(), missingHeader, "Missing expected headers in access.json!");
   }
 }
