948d40d79f67db048feb6d71611193b992d86ec5
Brandon Cruz <brandon@adhocteam.us>
2023-11-01 13:23:14 -0400

    add back in public access block to prevent removal on older s3 buckets

--- ops/terraform/services/common/s3.tf
+++ ops/terraform/services/common/s3.tf
@@ -8,6 +8,15 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
+# block public access to the bucket
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.this.id
   rule {
@@ -36,6 +45,14 @@ resource "aws_s3_bucket" "logging" {
     Layer = local.layer
     role  = "logs"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "logging" {
+  bucket                  = aws_s3_bucket.logging.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "logging" {
--- ops/terraform/services/pipeline/s3.tf
+++ ops/terraform/services/pipeline/s3.tf
@@ -10,6 +10,15 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
+# block public access to the bucket
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.this.id
   rule {
