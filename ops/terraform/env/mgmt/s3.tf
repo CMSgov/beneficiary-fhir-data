6b47eff8e353de8f8c6864b5f0c7ccf1821afb6a
Randall Rupp <randall.rupp@adhocteam.us>
2023-11-03 11:49:49 -0400

    BFD-3006 Moved to env/mgmt other PR comments

--- ops/terraform/env/mgmt/s3.tf
+++ ops/terraform/env/mgmt/s3.tf
@@ -119,3 +119,61 @@ resource "aws_s3_bucket_policy" "logging" {
 }
 POLICY
 }
+
+resource "aws_s3_bucket" "bfd-public-test-data" {
+  bucket = "bfd-public-test-data"
+
+  tags = {
+    "cms-cloud-exempt:public-s3-bucket" = "https://confluence.cms.gov/x/VFNBGg"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bfd-public-test-data" {
+  bucket = aws_s3_bucket.bfd-public-test-data.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "bfd-public-test-data" {
+  bucket = aws_s3_bucket.bfd-public-test-data.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bfd-public-test-data" {
+  bucket = aws_s3_bucket.bfd-public-test-data.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "bfd-public-test-data" {
+  bucket = aws_s3_bucket.bfd-public-test-data.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "bfd-public-test-data" {
+  depends_on = [aws_s3_bucket_versioning.bfd-public-test-data]
+  bucket = aws_s3_bucket.bfd-public-test-data.id
+
+  rule {
+    id = "rule-1"
+
+    filter {}
+
+    noncurrent_version_transition {
+      noncurrent_days = 60
+      storage_class   = "GLACIER"
+    }
+
+    status = "Enabled"
+  }
+}
