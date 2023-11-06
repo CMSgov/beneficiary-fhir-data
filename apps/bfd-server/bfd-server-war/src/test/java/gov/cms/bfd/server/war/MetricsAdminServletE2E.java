48c5d45edbecff0ce0bb73134c11b4f75cd292bd
Logan Mitchell <logan.mitchell@adhocteam.us>
2023-11-06 08:39:39 -0800

    remove thrown exceptions

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/MetricsAdminServletE2E.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/MetricsAdminServletE2E.java
@@ -11,23 +11,17 @@ import org.junit.jupiter.api.Test;
 public final class MetricsAdminServletE2E extends ServerRequiredTest {
   /**
    * Verifies that the <code>/metrics/ping</code> endpoint works as expected.
-   *
-   * @throws IOException (indicates test failure)
-   * @throws ClientProtocolException (indicates test failure)
    */
   @Test
-  public void ping() throws ClientProtocolException, IOException {
+  public void ping() {
     given().spec(requestAuth).expect().statusCode(200).when().get(baseServerUrl + "/metrics/ping");
   }
 
   /**
    * Verifies that the <code>/metrics/metrics</code> endpoint works as expected.
-   *
-   * @throws IOException (indicates test failure)
-   * @throws ClientProtocolException (indicates test failure)
    */
   @Test
-  public void metrics() throws ClientProtocolException, IOException {
+  public void metrics() {
     given()
         .spec(requestAuth)
         .expect()
