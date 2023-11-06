8b92edf41b041d9f3db75f1177cd43f3a0065ea8
Logan Mitchell <logan.mitchell@adhocteam.us>
2023-11-06 08:44:25 -0800

    formatting

--- apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/MetricsAdminServletE2E.java
+++ apps/bfd-server/bfd-server-war/src/test/java/gov/cms/bfd/server/war/MetricsAdminServletE2E.java
@@ -3,23 +3,17 @@ package gov.cms.bfd.server.war;
 import static io.restassured.RestAssured.given;
 
 import com.codahale.metrics.servlets.AdminServlet;
-import java.io.IOException;
-import org.apache.http.client.ClientProtocolException;
 import org.junit.jupiter.api.Test;
 
 /** Verifies that the metrics {@link AdminServlet} works as expected (as configured in web.xml). */
 public final class MetricsAdminServletE2E extends ServerRequiredTest {
-  /**
-   * Verifies that the <code>/metrics/ping</code> endpoint works as expected.
-   */
+  /** Verifies that the <code>/metrics/ping</code> endpoint works as expected. */
   @Test
   public void ping() {
     given().spec(requestAuth).expect().statusCode(200).when().get(baseServerUrl + "/metrics/ping");
   }
 
-  /**
-   * Verifies that the <code>/metrics/metrics</code> endpoint works as expected.
-   */
+  /** Verifies that the <code>/metrics/metrics</code> endpoint works as expected. */
   @Test
   public void metrics() {
     given()
