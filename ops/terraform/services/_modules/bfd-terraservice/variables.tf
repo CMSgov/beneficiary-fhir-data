235b2394b4909195005f6765045cc531e70b0459
Brandon Cruz <brandon@adhocteam.us>
2023-11-08 18:44:41 -0500

    encode CPM params and recreate ec2_user_private_key

--- ops/terraform/services/_modules/bfd-terraservice/variables.tf
+++ ops/terraform/services/_modules/bfd-terraservice/variables.tf
@@ -6,7 +6,7 @@ variable "environment_name" {
     // Simple validation ensures that the environment is either one of the established environments or ends with a combined
     // suffix of "-" and an established environment, e.g. `prod-sbx`, `2554-test`, `2554-ii-prod-sbx` are valid, `-prod`, `2554--test` are not
     condition     = one([for x in ["test", "prod-sbx", "prod"] : x if can(regex("^${x}$$|^([a-z0-9]+[a-z0-9-])+([^--])-${x}$$", var.environment_name))]) != null
-    error_message = "Invalid evironment/workspace name. https://github.com/CMSgov/beneficiary-fhir-data/wiki/Environments#ephemeral-environments for more details."
+    error_message = "Invalid environment/workspace name. https://github.com/CMSgov/beneficiary-fhir-data/wiki/Environments#ephemeral-environments for more details."
   }
 }
 
