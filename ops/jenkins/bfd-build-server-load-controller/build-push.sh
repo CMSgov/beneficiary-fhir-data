080ab0f5a1c2901446ec4213b4b7cec6be33f10e
Billie Rose <billiexrose@gmail.com>
2023-11-09 13:11:18 -0600

    formatting

--- ops/jenkins/bfd-build-server-load-controller/build-push.sh
+++ ops/jenkins/bfd-build-server-load-controller/build-push.sh
@@ -11,32 +11,32 @@ CONTEXT_DIR="${REPO_ROOT}/apps/utils/locust_tests/"
 # Constants
 GIT_SHORT_HASH="$(git rev-parse --short HEAD)"
 AWS_REGION="us-east-1"
-PRIVATE_REGISTRY_URI="$(aws ecr describe-registry --region "$AWS_REGION" | jq -r '.registryId').dkr.ecr.${AWS_REGION}.amazonaws.com"
+PRIVATE_REGISTRY_URI="$(aws ecr describe-registry --region "${AWS_REGION}" | jq -r '.registryId').dkr.ecr.${AWS_REGION}.amazonaws.com"
 IMAGE_NAME_NODE="${PRIVATE_REGISTRY_URI}/bfd-mgmt-server-load-controller"
 SSM_IMAGE_TAG_NODE="/bfd/mgmt/server/nonsensitive/server_load_controller_latest_image_tag"
 
 # Overridable defaults
-DOCKER_TAG="${DOCKER_TAG_OVERRIDE:-"$GIT_SHORT_HASH"}"
+DOCKER_TAG="${DOCKER_TAG_OVERRIDE:-"${GIT_SHORT_HASH}"}"
 DOCKER_TAG_LATEST="${DOCKER_TAG_LATEST_OVERRIDE:-"latest"}"
 
 # Build tagged node image
-docker build "$CONTEXT_DIR" \
-  --file "$SCRIPT_DIR/Dockerfile" \
+docker build "${CONTEXT_DIR}" \
+  --file "${SCRIPT_DIR}/Dockerfile" \
   --tag "${IMAGE_NAME_NODE}:${DOCKER_TAG}" \
   --tag "${IMAGE_NAME_NODE}:${DOCKER_TAG_LATEST}" \
   --platform "linux/amd64"
 
 # Get registry password and tell docker to login
-aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$PRIVATE_REGISTRY_URI"
+aws ecr get-login-password --region "${AWS_REGION}" | docker login --username AWS --password-stdin "${PRIVATE_REGISTRY_URI}"
 
 # Push image to ECR
-docker push "$IMAGE_NAME_NODE" --all-tags
+docker push "${IMAGE_NAME_NODE}" --all-tags
 
 # Put the image tag in SSM
 aws ssm put-parameter \
-  --name "$SSM_IMAGE_TAG_NODE" \
+  --name "${SSM_IMAGE_TAG_NODE}" \
   --value "${IMAGE_NAME_NODE}:${DOCKER_TAG}" \
-  --overwrite \
   --type String \
-  --region "$AWS_REGION"
+  --overwrite \
+  --region "${AWS_REGION}"
 
