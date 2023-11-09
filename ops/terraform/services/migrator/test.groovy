c50aded716ed9a3020cae46934ae0a4b8e325d46
Brandon Cruz <brandon@adhocteam.us>
2023-11-08 21:43:54 -0500

    update test

--- ops/terraform/services/migrator/test.groovy
+++ ops/terraform/services/migrator/test.groovy
@@ -1,18 +1,46 @@
-#!/usr/bin/env groovy
+pipeline {
+  agent {
+    kubernetes {
+      defaultContainer 'bfd-cbc-build'
+      yaml """
+apiVersion: v1
+kind: Pod
+spec:
+  serviceAccount: bfd
+  restartPolicy: Never
+  containers:
+    - name: bfd-cbc-build
+      image: "public.ecr.aws/c2o1d8s9/bfd-cbc-build:jdk17-mvn3-tfenv3-kt1.9-latest" # TODO: consider a smarter solution for resolving this image
+      command:
+        - cat
+      tty: true
+      imagePullPolicy: IfNotPresent
+"""
+    }
+  }
 
-// authenticate
-awsAuth.assumeRole()
+ stages {
+    stage('Create Parameter') {
+        steps {
+            script {
+                // authenticate
+                awsAuth.assumeRole()
 
-awsSsm.putParameter(
-            parameterName: "/bfd/test/common/nonsensitive/brandon_test",
-            parameterValue: "test",
-            parameterType: "String",
-            parameterTags: [
-                "Key=Environment,Value=test}",
-                "Key=stack,Value=test}",
-                "Key=Terraform,Value=False",
-                "Key=application,Value=bfd",
-                "Key=business,Value=oeda"
-            ],
-            shouldOverwrite: true
-        )
+                awsSsm.putParameter(
+                            parameterName: "/bfd/test/common/nonsensitive/brandon_test",
+                            parameterValue: "test",
+                            parameterType: "String",
+                            parameterTags: [
+                                "Key=Environment,Value=test}",
+                                "Key=stack,Value=test}",
+                                "Key=Terraform,Value=False",
+                                "Key=application,Value=bfd",
+                                "Key=business,Value=oeda"
+                            ],
+                            shouldOverwrite: true
+                        )
+            }
+        }
+    }
+ }
+}
