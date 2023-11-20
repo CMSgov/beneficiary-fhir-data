c4da91bcc5f2c3dd1dd3132105ea355c7a7564f7
keithdadkins <keithdadkins@users.noreply.github.com>
2023-11-19 19:56:19 -0600

    update certs

--- ops/terraform/services/common/aurora-cluster.tf
+++ ops/terraform/services/common/aurora-cluster.tf
@@ -115,7 +115,7 @@ resource "aws_db_parameter_group" "aurora_cluster" {
 resource "aws_rds_cluster_instance" "nodes" {
   count                           = local.rds_instance_count
   auto_minor_version_upgrade      = true
-  ca_cert_identifier              = "rds-ca-2019" # NOTE: This seems like an invariant
+  ca_cert_identifier              = "rds-ca-rsa4096-g1"
   cluster_identifier              = aws_rds_cluster.aurora_cluster.id
   copy_tags_to_snapshot           = true
   db_subnet_group_name            = aws_rds_cluster.aurora_cluster.db_subnet_group_name
