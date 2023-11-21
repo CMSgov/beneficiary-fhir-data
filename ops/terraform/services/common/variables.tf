bb5a220e604eeabbd404de90d30b945a5b742008
keithdadkins <keithdadkins@users.noreply.github.com>
2023-11-21 12:38:23 -0600

    remove unused var

--- ops/terraform/services/common/variables.tf
+++ ops/terraform/services/common/variables.tf
@@ -1,9 +1,3 @@
-variable "rds_cluster_identifier_override" {
-  default     = null
-  description = "RDS Cluster Identifier Override. Defaults to cluster identifier specified in centralized environmental configuration."
-  type        = string
-}
-
 variable "rds_apply_immediately" {
   default     = false
   description = "Apply any changes to an rds cluster immediately. Use caution as this may cause downtime. Defaults to false."
