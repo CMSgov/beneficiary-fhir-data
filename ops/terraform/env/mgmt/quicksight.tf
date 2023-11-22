64b27ce5437aac408728f602fa9362871d1129b1
Billie Rose <billiexrose@gmail.com>
2023-11-22 12:57:35 -0600

    init

---
+++ ops/terraform/env/mgmt/quicksight.tf
@@ -0,0 +1,52 @@
+locals {
+  quicksight_config = zipmap(
+    [for name in data.aws_ssm_parameters_by_path.sensitive_quicksight_config.names :
+    element(split("/", name), length(split("/", name)) - 1)],
+    [for value in nonsensitive(data.aws_ssm_parameters_by_path.sensitive_quicksight_config.values) : jsondecode(value)]
+  )
+
+  quicksight_users  = local.quicksight_config["users"]
+  quicksight_groups = [for group in local.quicksight_config["groups"] : group["group_name"]]
+  quicksight_group_memberships = flatten([
+    for user in local.quicksight_users : [
+      for group_name in user["groups"] : { "member_name" : lookup(user, "iam", user["email"]), "group_name" : group_name }
+    ]
+  ])
+}
+
+data "aws_ssm_parameters_by_path" "sensitive_quicksight_config" {
+  path = "/bfd/mgmt/quicksight/sensitive/"
+}
+
+# Quicksight users are expected to adhere to the following json format:
+# {"email":"", "user_role":"AUTHOR|ADMIN|READER", "identity_type":"QUICKSIGHT|IAM","groups":[""]}
+# If "IAM" is specified as the identity_type, then "iam_arn" must be provided
+# If "QUICKSIGHT" is specified as the identity_type, then "user_name" must be provided
+resource "aws_quicksight_user" "quicksight_user" {
+  for_each = { for idx, user in local.quicksight_users : idx => user }
+
+  email         = sensitive(each.value["email"])
+  identity_type = each.value["identity_type"]
+  user_role     = upper(each.value["user_role"])
+  iam_arn       = each.value["identity_type"] == "IAM" ? "arn:aws:quicksight:us-east-1:${local.account_id}:user/default/${each.value["iam"]}" : null
+  user_name     = each.value["identity_type"] == "QUICKSIGHT" ? sensitive(each.value["email"]) : null
+}
+
+# Quicksight groups are expected to adhere to the following json format:
+# {"group_name":""}
+resource "aws_quicksight_group" "quicksight_group" {
+  for_each = toset(local.quicksight_groups)
+
+  group_name = each.key
+}
+
+# Quicksight group memberships are specified as part of the quicksight users json object's "groups" attribute
+# For each user, for each group, we add an entry to a map and then create that group membership iteratively
+# e.g., { "0" => "user_name": "group_name", "1" => "user_name": "group_name" }
+resource "aws_quicksight_group_membership" "quicksight_group_membership" {
+  for_each = { for idx, group_membership in local.quicksight_group_memberships : idx => group_membership }
+
+  group_name  = each.value["group_name"]
+  member_name = sensitive(each.value["member_name"])
+  depends_on  = [aws_quicksight_group.quicksight_group, aws_quicksight_user.quicksight_user]
+}
