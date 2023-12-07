bd4462adc7b8a9f079334198e6d93f0777bd2322
Brian Burton <brian.burton@adhocteam.us>
2023-12-07 15:07:40 -0500

    Updated run scripts to use new SSM parameter names.

--- apps/bfd-server/bfd-server-launcher/src/main/java/gov/cms/bfd/server/launcher/AppConfiguration.java
+++ apps/bfd-server/bfd-server-launcher/src/main/java/gov/cms/bfd/server/launcher/AppConfiguration.java
@@ -3,10 +3,13 @@ package gov.cms.bfd.server.launcher;
 import gov.cms.bfd.sharedutils.config.ConfigException;
 import gov.cms.bfd.sharedutils.config.ConfigLoader;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Optional;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /** Models the configuration options for the launcher. */
+@AllArgsConstructor
+@Getter
 public final class AppConfiguration {
 
   /**
@@ -39,8 +42,14 @@ public final class AppConfiguration {
    */
   public static final String ENV_VAR_KEY_WAR = "paths.files.war";
 
-  /** The host/address that the server will bind to and listen for HTTPS connections on. * */
-  private final String host;
+  /**
+   * The host/address that the server will bind to and listen for HTTPS connections on.
+   *
+   * <p>If {@link Optional#empty()} or {@code "0.0.0.0"}, then it will try to bind to all interfaces
+   * (though note that the port may not be available on all of them, and Jetty just kinda' silently
+   * ignores that).
+   */
+  private final Optional<String> host;
 
   /** The port that the server will listen for HTTPS connections on. * */
   private final int port;
@@ -49,83 +58,16 @@ public final class AppConfiguration {
    * The {@link Path} of the Java keystore ({@code .pfx} file) containing the private key and
    * certificate to use for this server. *
    */
-  private final String keystore;
+  private final Path keystore;
 
   /**
    * The {@link Path} of the Java trust store ({@code .pfx} file) containing the client certificates
    * to use (i.e. trust/authenticate) for this server. *
    */
-  private final String truststore;
+  private final Path truststore;
 
   /** The {@link Path} of the WAR file to run. * */
-  private final String war;
-
-  /**
-   * Constructs a new {@link AppConfiguration} instance.
-   *
-   * @param host the value to use for {@link #getHost()}
-   * @param port the value to use for {@link #getPort()}
-   * @param keystore the value to use for {@link #getKeystore()}
-   * @param truststore the value to use for {@link #getTruststore()}
-   * @param war the value to use for {@link #getWar()}
-   */
-  public AppConfiguration(
-      Optional<String> host, int port, Path keystore, Path truststore, Path war) {
-    this.host = host.orElse(null);
-    this.port = port;
-    this.keystore = keystore.toString();
-    this.truststore = truststore.toString();
-    this.war = war.toString();
-  }
-
-  /**
-   * Gets the {@link #host}.
-   *
-   * <p>If {@link Optional#empty()} or <code>"0.0.0.0"</code>, then it will try to bind to all
-   * interfaces (though note that the port may not be available on all of them, and Jetty just
-   * kinda' silently ignores that).
-   *
-   * @return the host/address that the server will bind to and listen for HTTPS connections on
-   */
-  public Optional<String> getHost() {
-    return Optional.ofNullable(host);
-  }
-
-  /**
-   * Gets the {@link #port}.
-   *
-   * @return the port
-   */
-  public int getPort() {
-    return port;
-  }
-
-  /**
-   * Gets the {@link #keystore}.
-   *
-   * @return the {@link Path} of the Java keystore
-   */
-  public Path getKeystore() {
-    return Paths.get(keystore);
-  }
-
-  /**
-   * Gets the {@link #truststore}.
-   *
-   * @return the {@link Path} of the Java trust store
-   */
-  public Path getTruststore() {
-    return Paths.get(truststore);
-  }
-
-  /**
-   * Gets the {@link #war}.
-   *
-   * @return the {@link Path} of the WAR file
-   */
-  public Path getWar() {
-    return Paths.get(war);
-  }
+  private final Path war;
 
   @Override
   public String toString() {
