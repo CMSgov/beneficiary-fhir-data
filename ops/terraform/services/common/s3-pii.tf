cdbd3798fd1c0f138267b849b2750457d6ec4dc2
Billie Rose <billiexrose@gmail.com>
2023-12-29 14:34:44 -0600

    BFD-3083: Move local zones and s3 pii from stateful

---
+++ ops/terraform/services/common/s3-pii.tf
@@ -0,0 +1,23 @@
+## S3 bucket, policy, and KMS key for medicare opt out data
+
+locals {
+  name         = "medicare-opt-out"
+  log_bucket   = "bfd-${local.env}-${name}-${local.account_id}"
+  read_arns    = data.aws_ssm_parameter.medicare_opt_out_config_read_roles.value
+  write_accts  = data.aws_ssm_parameter.medicare_opt_out_config_write_accts.value
+  admins_users = data.aws_ssm_parameter.medicare_opt_out_config_admin_users.value
+}
+
+module "medicare_opt_out" {
+  source = "../modules/resources/s3_pii"
+  env    = local.env
+
+  pii_bucket_config = {
+    name        = local.name
+    log_bucket  = local.log_bucket
+    read_arns   = local.read_arns
+    write_accts = local.write_accts
+    admin_arns  = local.admin_users
+    account_id  = local.account_id
+  }
+}
