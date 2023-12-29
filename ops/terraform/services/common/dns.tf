66d0a5119ee82d1e06743fba95116942cbbbd0d4
Billie Rose <billiexrose@gmail.com>
2023-12-29 14:36:29 -0600

    BFD-3083: Move data source def

--- ops/terraform/services/common/dns.tf
+++ ops/terraform/services/common/dns.tf
@@ -1,12 +1,4 @@
 ## VPC Private Local Zone for CNAME Records
-
-data "aws_vpc" "main" {
-  filter {
-    name   = "tag:Name"
-    values = ["bfd-${local.env}-vpc"]
-  }
-}
-
 module "local_zone" {
   source     = "../modules/resources/dns"
   env_config = { env = local.env, vpc_id = data.aws_vpc.main.id }
