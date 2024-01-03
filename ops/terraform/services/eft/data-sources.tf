328b6a90c0a1d6d6f3b2aa9ba83130c170515cd5
Mitchell Alessio <5306896+malessi@users.noreply.github.com>
2024-01-02 20:42:29 -0500

    BFD-3115: Implement BFD EFT Outbound (#2112)

--- ops/terraform/services/eft/data-sources.tf
+++ ops/terraform/services/eft/data-sources.tf
@@ -1,27 +1,39 @@
+data "aws_region" "current" {}
+
 data "aws_caller_identity" "current" {}
 
 data "aws_kms_key" "cmk" {
   key_id = local.kms_key_alias
 }
 
+data "aws_kms_key" "config_cmk" {
+  key_id = local.kms_config_key_alias
+}
+
 # This is a distinct parameter as we need to retrieve the list of partners first so that we know
 # which SSM hierarchies to get
 data "aws_ssm_parameter" "partners_list_json" {
-  name            = "/bfd/${local.env}/${local.service}/sensitive/partners_list_json"
+  for_each = toset(["inbound", "outbound"])
+
+  name            = "/bfd/${local.env}/${local.service}/sensitive/${each.key}/partners_list_json"
   with_decryption = true
 }
 
-data "aws_ssm_parameters_by_path" "sensitive" {
-  for_each = local.ssm_all_paths
+data "aws_ssm_parameters_by_path" "params" {
+  for_each = toset(local.ssm_hierarchies)
 
-  path            = "/${each.value.hierarchy}/${local.env}/${each.value.service}/sensitive"
+  recursive       = true
+  path            = each.value
   with_decryption = true
 }
 
-data "aws_ssm_parameters_by_path" "nonsensitive" {
-  for_each = local.ssm_all_paths
+data "aws_ecr_repository" "ecr" {
+  name = "bfd-mgmt-${local.service}-${local.outbound_lambda_name}-lambda"
+}
 
-  path = "/${each.value.hierarchy}/${local.env}/${each.value.service}/nonsensitive"
+data "aws_ecr_image" "sftp_outbound_transfer" {
+  repository_name = data.aws_ecr_repository.ecr.name
+  image_tag       = local.latest_version
 }
 
 data "aws_ec2_managed_prefix_list" "vpn" {
@@ -35,6 +47,17 @@ data "aws_vpc" "this" {
   filter {
     name   = "tag:Name"
     values = [local.vpc_name]
+  }
+}
+
+data "aws_subnets" "sftp_outbound_transfer" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.this.id]
+  }
+  filter {
+    name   = "tag:Layer"
+    values = [local.layer]
   }
 }
 
@@ -64,12 +87,12 @@ data "aws_vpc_endpoint_service" "transfer_server" {
 }
 
 data "aws_ssm_parameter" "zone_name" {
-  name            = "/bfd/mgmt/common/sensitive/r53_hosted_zone_${local.eft_r53_hosted_zone}_domain"
+  name            = "/bfd/mgmt/common/sensitive/r53_hosted_zone_${local.inbound_r53_hosted_zone}_domain"
   with_decryption = true
 }
 
 data "aws_ssm_parameter" "zone_is_private" {
-  name            = "/bfd/mgmt/common/sensitive/r53_hosted_zone_${local.eft_r53_hosted_zone}_is_private"
+  name            = "/bfd/mgmt/common/sensitive/r53_hosted_zone_${local.inbound_r53_hosted_zone}_is_private"
   with_decryption = true
 }
 
@@ -77,6 +100,6 @@ data "aws_route53_zone" "this" {
   name         = nonsensitive(data.aws_ssm_parameter.zone_name.value)
   private_zone = nonsensitive(data.aws_ssm_parameter.zone_is_private.value)
   tags = {
-    "ConfigId" = local.eft_r53_hosted_zone
+    "ConfigId" = local.inbound_r53_hosted_zone
   }
 }
