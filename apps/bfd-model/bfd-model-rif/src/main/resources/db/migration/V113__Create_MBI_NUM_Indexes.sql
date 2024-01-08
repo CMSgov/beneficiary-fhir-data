8a6f522da87532b242aecdbd362d90fabcbb57ef
mackec1 <chris.mackenzie@adhocteam.us>
2024-01-08 12:38:33 -0500

    remove psql-only directives

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V113__Create_MBI_NUM_Indexes.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V113__Create_MBI_NUM_Indexes.sql
@@ -3,8 +3,8 @@
 -- needs to check both the current beneficiaries table as well as the
 -- beneficiaries_history table.
 --
-${logic.psql-only}  CREATE INDEX CONCURRENTLY IF NOT EXISTS beneficiaries_history_mbi_idx
-${logic.psql-only}      ON beneficiaries_history (mbi_num);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS beneficiaries_history_mbi_idx
+    ON beneficiaries_history (mbi_num);
 
-${logic.psql-only}  CREATE INDEX CONCURRENTLY IF NOT EXISTS beneficiaries_mbi_idx
-${logic.psql-only}      ON beneficiaries (mbi_num);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS beneficiaries_mbi_idx
+    ON beneficiaries (mbi_num);
