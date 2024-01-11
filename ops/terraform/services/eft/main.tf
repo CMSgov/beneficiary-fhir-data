ea92c8623dbd39e68f65d934ec121cf221324367
Mitchell Alessio <5306896+malessi@users.noreply.github.com>
2024-01-11 16:42:41 -0500

    BFD-3161: Deploy EFT Terraservice during regular deployment (#2135)

--- ops/terraform/services/eft/main.tf
+++ ops/terraform/services/eft/main.tf
@@ -198,7 +198,8 @@ locals {
       local.outbound_sftp_host_key,
       local.outbound_sftp_username,
       local.outbound_sftp_user_priv_key
-    ] : trimspace(coalesce(x, "INVALID")) != "INVALID"]) ? [
+    ] : trimspace(coalesce(x, "INVALID")) != "INVALID"]
+    ) ? [
     for partner, _ in local.eft_partners_config :
     partner
     if contains(local.outbound_eft_partners, partner) && length(
@@ -259,12 +260,12 @@ resource "aws_s3_bucket" "this" {
 }
 
 resource "aws_s3_bucket_notification" "bucket_notifications" {
-  count = length(concat([
+  count = length(concat(
     local.eft_partners_with_inbound_received_notifs,
     local.eft_partners_with_outbound_enabled,
     local.eft_partners_with_outbound_sent_notifs,
     local.eft_partners_with_outbound_failed_notifs
-  ])) > 0 ? 1 : 0
+  )) > 0 ? 1 : 0
 
   bucket = aws_s3_bucket.this.id
 
