31e5428c7aeb5e520608f1479a1c91abd1d40c89
Billie Rose <billiexrose@gmail.com>
2024-01-30 11:10:46 -0600

    BFD-3236: Move CW Slack alarms module resources

--- ops/terraform/services/common/cloudwatch-slack-notifier.tf
+++ ops/terraform/services/common/cloudwatch-slack-notifier.tf
@@ -1,8 +1,7 @@
 ## Slack notifications for CloudWatch Alarms by environment
-
 module "cw_alarms_slack_notifier" {
   count = local.is_ephemeral_env ? 0 : 1
 
-  source = "../../modules/resources/cw_alarms_slack_notifier"
+  source = "./modules/bfd_cw_alarms_slack_notifier"
   env    = local.env
 }
