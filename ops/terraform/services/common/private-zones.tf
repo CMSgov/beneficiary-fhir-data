02a2ae77c7602533df55e8fc546ea99dddef2dc8
Billie Rose <billiexrose@gmail.com>
2024-01-30 12:23:32 -0600

    BFD-3236: Remove unnecessary locals

--- ops/terraform/services/common/private-zones.tf
+++ ops/terraform/services/common/private-zones.tf
@@ -1,9 +1,4 @@
 ## VPC Private Local Zone for CNAME Records
-locals {
-  vpc_id = data.aws_vpc.main.id
-  public = false
-}
-
 module "local_zone" {
   count      = local.is_ephemeral_env ? 0 : 1
   source     = "./modules/bfd_common_private_zones"
