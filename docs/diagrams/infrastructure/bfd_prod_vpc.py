449f68b7466c9943e3b9bca77e7e50e5944fb737
Brandon Cruz <brandon@adhocteam.us>
2024-02-07 20:39:27 -0500

    Production VPC

---
+++ docs/diagrams/infrastructure/bfd_prod_vpc.py
@@ -0,0 +1,67 @@
+from diagrams import Cluster, Diagram, Node
+from diagrams.aws.compute import EC2, Lambda, ApplicationAutoScaling
+from diagrams.aws.database import Aurora
+from diagrams.aws.network import ELB, PrivateSubnet, VPC
+from diagrams.aws.storage import S3
+
+#######################################################
+# Setup Some Input Variables for Easier Customization #
+#######################################################
+
+legend_text = (
+"legend: \l"
+"AZs - BFD does not exercise the assignment of resources per AZ. \l"
+"Colocation of pipeline/migrator workloads reside with master aurora node \l"
+"AutoScaling Group Cluster is actually nested inside each application AZ.\l"
+"Due to limitations of diagram app, ASG is showing outside AZs.\l" #FIX_ME
+"--------------\l"
+"Data Sources\l"
+"CCW Pipeline Instance is triggered via lambda script when RIF files arrive in s3 bucket.\l"
+"RDA Pipeline runs 24x7\l"
+)
+title = "BFD FHIR Service - Production"
+outformat = "png"
+filename = "out/BFD FHIR Service - Production"
+filenamegraph = "out//BFD FHIR Service - Production.gv"
+show = False
+direction = "TB"
+
+with Diagram(
+    name=title,
+    direction=direction,
+    show=show,
+    filename=filename,
+    outformat=outformat,
+) as diag:
+    Node(label=legend_text, width="8", shape="plaintext", )
+    # Cluster = Group, so this outline will group all the items nested in it automatically
+    with Cluster(label='bfd-prod-vpc'):
+        s3 = S3("bfd-prod-etl")
+        lamb = Lambda("bfd-prod-pipeline-scheduler")
+        with Cluster("bfd-prod-az(1,2,3)-dmz"):
+            elb = ELB("Prod ELB")
+        with Cluster("AutoScaling Group"):
+            with Cluster("brd-prod-az1-app"):
+                server1 = EC2("bfd-prod-fhir-ap1")
+            with Cluster("brd-prod-az2-app"):
+                server2 = EC2("bfd-prod-fhir-ap2")
+            with Cluster("brd-prod-az3-app"):
+                server3 = EC2("bfd-prod-fhir-ap3")
+        with Cluster("bfd-prod-az1-data"):
+            db1 = Aurora("bfd-prod-replica1")
+        with Cluster("bfd-prod-az2-data"):
+            db2 = Aurora("bfd-prod-replica2")
+            db_writer = Aurora("bfd-prod-master")
+            ccw = EC2("CCW bfd-prod-etl1")
+            rda = EC2("RDA bfd-prod-etl1")
+            migrator = EC2("Db Migrator")
+        with Cluster("bfd-prod-az3-data"):
+            db3 = Aurora("bfd-prod-replica3")
+            
+    elb >> server1 >> db1 >> db_writer
+    elb >> server2  >> db2 >> db_writer 
+    elb >> server3  >> db3 >> db_writer
+    db_writer << ccw << lamb << s3
+    db_writer << rda
+    db_writer << migrator
+diag
