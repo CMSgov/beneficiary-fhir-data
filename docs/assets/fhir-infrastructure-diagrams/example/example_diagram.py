d9133d92ed620ac7ca87842be1b6e42b2440b1fa
Brandon Cruz <brandon@adhocteam.us>
2024-02-13 12:05:41 -0500

    Update example with embedded images and svg file type

--- docs/assets/fhir-infrastructure-diagrams/example/example_diagram.py
+++ docs/assets/fhir-infrastructure-diagrams/example/example_diagram.py
@@ -3,5 +3,6 @@ from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
-with Diagram("Web Service", show=False):
+outformat = "svg"
+with Diagram("Web Service", show=False, outformat=outformat):
     ELB("lb") >> EC2("web") >> RDS("userdb")
