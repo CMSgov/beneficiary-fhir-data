ae3629cc05f5cd522de2545a7490f011503a7f0b
Ade Adedeji <aadedeji@fearless.tech>
2024-02-13 16:04:22 -0500

    BFD-3281:Resolve zipSlip vulnerability re to SQ:s6096 in bfd-npi

--- apps/bfd-data-npi/src/main/java/gov/cms/bfd/data/npi/utility/DataUtilityCommons.java
+++ apps/bfd-data-npi/src/main/java/gov/cms/bfd/data/npi/utility/DataUtilityCommons.java
@@ -86,24 +86,26 @@ public class DataUtilityCommons {
    * @throws IOException (any errors encountered will be bubbled up)
    */
   public static void unzip(Path zipFilePath, Path destDirectory) throws IOException {
-    ZipInputStream zipIn =
-        new ZipInputStream(new FileInputStream(zipFilePath.toFile().getAbsolutePath()));
-    ZipEntry entry = zipIn.getNextEntry();
-    // iterates over entries in the zip file
-    while (entry != null) {
-      Path filePath = Paths.get(destDirectory.toFile().getAbsolutePath(), entry.getName());
-      if (!entry.isDirectory()) {
-        // if the entry is a file, extracts it
-        extractFile(zipIn, filePath);
-      } else {
-        // if the entry is a directory, make the directory
-        File dir = new File(filePath.toFile().getAbsolutePath());
-        dir.mkdir();
+    try (ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath.toFile()))) {
+      ZipEntry entry = zipIn.getNextEntry();
+      // iterates over entries in the zip file
+      while (entry != null) {
+        Path filePath = destDirectory.resolve(entry.getName()).normalize();
+        if (!filePath.startsWith(destDirectory)) {
+          // Checks if resolved path is trying to escape from the destination directory
+          throw new IOException("ZipSlip: Entry is outside of the target directory");
+        }
+        if (!entry.isDirectory()) {
+          // if the entry is a file, extracts it
+          extractFile(zipIn, filePath);
+        } else {
+          // if the entry is a directory, make the directory
+          Files.createDirectories(filePath);
+        }
+        zipIn.closeEntry();
+        entry = zipIn.getNextEntry();
       }
-      zipIn.closeEntry();
-      entry = zipIn.getNextEntry();
     }
-    zipIn.close();
   }
 
   /**
@@ -114,14 +116,15 @@ public class DataUtilityCommons {
    * @throws IOException (any errors encountered will be bubbled up)
    */
   public static void extractFile(ZipInputStream zipIn, Path filePath) throws IOException {
-    BufferedOutputStream bos =
-        new BufferedOutputStream(new FileOutputStream(filePath.toFile().getAbsolutePath()));
-    byte[] bytesIn = new byte[BUFFER_SIZE];
-    int read = 0;
-    while ((read = zipIn.read(bytesIn)) != -1) {
-      bos.write(bytesIn, 0, read);
+    Files.createDirectories(filePath.getParent());
+    try (BufferedOutputStream bos =
+        new BufferedOutputStream(new FileOutputStream(filePath.toFile().getAbsolutePath()))) {
+      byte[] bytesIn = new byte[BUFFER_SIZE];
+      int read;
+      while ((read = zipIn.read(bytesIn)) != -1) {
+        bos.write(bytesIn, 0, read);
+      }
     }
-    bos.close();
   }
 
   /**
