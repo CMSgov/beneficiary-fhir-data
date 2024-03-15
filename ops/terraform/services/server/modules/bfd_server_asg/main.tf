b46819c05e05004eb2ea89d0c5f0dc6c55fd6bf8
Mitch Alessio <mitch.alessio@adhocteam.us>
2024-03-15 15:10:39 -0400

    Switch to specialized CPU-based scaling policies

--- ops/terraform/services/server/modules/bfd_server_asg/main.tf
+++ ops/terraform/services/server/modules/bfd_server_asg/main.tf
@@ -26,23 +26,16 @@ locals {
       consecutive_periods_to_alarm = 10
     }
   }
-  # begin is inclusive, end is exclusive
-  scaling_stages = [
-    # If RequestCount is less than 1 * var.scaling_request_count_interval count/min, exclusive, we
-    # want to be at 3 instances
-    { begin = null, end = 1 * var.scaling_request_count_interval, desired_capacity = local.scaling_capacity_step * 1 },
-    # If RequestCount is greater than or equal to 1 * var.scaling_request_count_interval count/min,
-    # but less than 2 * var.scaling_request_count_interval count/min we want to be at 6 instances
-    { begin = 1 * var.scaling_request_count_interval, end = 2 * var.scaling_request_count_interval, desired_capacity = local.scaling_capacity_step * 2 },
-    # If RequestCount is greater than or equal to 2 * var.scaling_request_count_interval count/min,
-    # but less than 4 * var.scaling_request_count_interval count/min we want to be at 9 instances
-    { begin = 2 * var.scaling_request_count_interval, end = 3 * var.scaling_request_count_interval, desired_capacity = local.scaling_capacity_step * 3 },
-    # Whenever traffic is greater than or equal to 4 * var.scaling_request_count_interval count/min
-    # we want to be at 12 instances
-    { begin = 3 * var.scaling_request_count_interval, end = null, desired_capacity = local.scaling_capacity_step * 4 },
+  scale_in_cpu_threshold = 15 # 15 percent average cpu for above period will cause scale-in
+  # 3 ranges of CPU utilization to scale out at, with each range intended to _increase_ the scaling
+  # response when average CPU utilization is increased. For example, if CPU utilization hits 90%
+  # over the evaluation period of scale-out at 3 instances, we want to scale-out to 12 instances
+  # immediately
+  scale_out_cpu_thresholds = [
+    { begin = 50, end = 75 },
+    { begin = 75, end = 90 },
+    { begin = 90, end = null }
   ]
-  scalein_config  = slice(local.scaling_stages, 0, length(local.scaling_stages) - 1)
-  scaleout_config = slice(local.scaling_stages, 1, length(local.scaling_stages))
 
   on_launch_lifecycle_hook_name = "bfd-${local.env}-${var.role}-on-launch"
 }
@@ -240,83 +233,53 @@ resource "aws_autoscaling_group" "main" {
 ## Autoscaling Policies and Cloudwatch Alarms
 #
 
-resource "aws_cloudwatch_metric_alarm" "filtered_req_count_low" {
-  for_each = { for v in local.scalein_config : "${v.desired_capacity}-instances" => v }
-
-  alarm_name          = "bfd-${var.role}-${local.env}-req-count-low-${each.key}"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
+resource "aws_cloudwatch_metric_alarm" "avg_cpu_low" {
+  alarm_name          = "bfd-${var.role}-${local.env}-avg-cpu-low"
+  comparison_operator = "LessThanThreshold"
   datapoints_to_alarm = local.scaling_alarms_config.scale_in.datapoints_to_alarm
   evaluation_periods  = local.scaling_alarms_config.scale_in.consecutive_periods_to_alarm
   threshold           = 1
   treat_missing_data  = "notBreaching"
-  alarm_actions       = [aws_autoscaling_policy.filtered_req_count_low_scaling[each.key].arn]
+  alarm_actions       = [aws_autoscaling_policy.avg_cpu_low.arn]
 
   metric_query {
     id          = "m1"
-    return_data = false
-
-    metric {
-      dimensions = {
-        LoadBalancerName = "bfd-${local.env}-${var.role}"
-      }
-      metric_name = "RequestCount"
-      namespace   = "AWS/ELB"
-      period      = local.scaling_alarms_config.scale_in.eval_period
-      stat        = "Sum"
-    }
-  }
-
-  metric_query {
-    id          = "m2"
-    return_data = false
+    return_data = true
 
     metric {
       dimensions = {
         AutoScalingGroupName = aws_autoscaling_group.main.name
       }
-      metric_name = "GroupDesiredCapacity"
+      metric_name = "CPUUtilization"
       namespace   = "AWS/AutoScaling"
       period      = local.scaling_alarms_config.scale_in.eval_period
       stat        = "Average"
     }
   }
-
-  metric_query {
-    id    = "e1"
-    label = "Set to ${each.value.desired_capacity} capacity units"
-    expression = "IF(${join(" && ", compact([
-      each.value.begin != null ? "m1 > ${each.value.begin}" : null,
-      each.value.end != null ? "m1 <= ${each.value.end}" : null,
-      "m2 > ${each.value.desired_capacity}"
-    ]))}, 1, 0)"
-    return_data = true
-  }
 }
 
-resource "aws_autoscaling_policy" "filtered_req_count_low_scaling" {
-  for_each = { for v in local.scalein_config : "${v.desired_capacity}-instances" => v }
-
-  name                      = "bfd-${var.role}-${local.env}-req-count-low-scalein-${each.key}"
+resource "aws_autoscaling_policy" "avg_cpu_low" {
+  name                      = "bfd-${var.role}-${local.env}-avg-cpu-low"
   autoscaling_group_name    = aws_autoscaling_group.main.name
   estimated_instance_warmup = 0 # explicitly disable warmup as lifecycle hooks handle it more accurately
-  adjustment_type           = "ExactCapacity"
+  adjustment_type           = "ChangeInCapacity"
   metric_aggregation_type   = "Average"
   policy_type               = "StepScaling"
 
   step_adjustment {
     metric_interval_lower_bound = 0
-    scaling_adjustment          = each.value.desired_capacity
+    scaling_adjustment          = "-${local.scaling_capacity_step}"
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "filtered_req_count_high" {
-  alarm_name          = "bfd-${var.role}-${local.env}-req-count-high"
+resource "aws_cloudwatch_metric_alarm" "avg_cpu_high" {
+  alarm_name          = "bfd-${var.role}-${local.env}-avg-cpu-high"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   datapoints_to_alarm = local.scaling_alarms_config.scale_out.datapoints_to_alarm
   evaluation_periods  = local.scaling_alarms_config.scale_out.consecutive_periods_to_alarm
   threshold           = 1
   treat_missing_data  = "notBreaching"
-  alarm_actions       = [aws_autoscaling_policy.filtered_req_count_high_scaling.arn]
+  alarm_actions       = [aws_autoscaling_policy.avg_cpu_high.arn]
 
   metric_query {
     id          = "m1"
@@ -324,14 +287,15 @@ resource "aws_cloudwatch_metric_alarm" "filtered_req_count_high" {
 
     metric {
       dimensions = {
-        LoadBalancerName = "bfd-${local.env}-${var.role}"
+        AutoScalingGroupName = aws_autoscaling_group.main.name
       }
-      metric_name = "RequestCount"
-      namespace   = "AWS/ELB"
-      period      = local.scaling_alarms_config.scale_out.eval_period
-      stat        = "Sum"
+      metric_name = "CPUUtilization"
+      namespace   = "AWS/AutoScaling"
+      period      = local.scaling_alarms_config.scale_in.eval_period
+      stat        = "Average"
     }
   }
+
   metric_query {
     id          = "m2"
     return_data = false
@@ -342,35 +306,56 @@ resource "aws_cloudwatch_metric_alarm" "filtered_req_count_high" {
       }
       metric_name = "GroupDesiredCapacity"
       namespace   = "AWS/AutoScaling"
-      period      = local.scaling_alarms_config.scale_out.eval_period
+      period      = local.scaling_alarms_config.scale_in.eval_period
       stat        = "Average"
     }
   }
 
+  metric_query {
+    id          = "m3"
+    return_data = false
+
+    metric {
+      dimensions = {
+        AutoScalingGroupName = aws_autoscaling_group.main.name
+      }
+      metric_name = "GroupInServiceInstances"
+      namespace   = "AWS/AutoScaling"
+      period      = local.scaling_alarms_config.scale_in.eval_period
+      stat        = "Average"
+    }
+  }
+
+  # This is a bit complex, but in short this creates an IF() metric math for each defined range that
+  # will scale proportionally to the incoming load. However, because of the quirks of default Step
+  # Scaling policies, these generated expressions guard against additional, erroneous scaling by
+  # ensuring these ranges are only evaluated when the desired capacity matches the existing
+  # capacity. Otherwise, normal Step Scaling policies will continue to scale indefinitely (until the
+  # maximum) as instances take too long to warmup and contribute to aggregated CPU metrics
   dynamic "metric_query" {
-    for_each = local.scaleout_config
+    for_each = local.scale_out_cpu_thresholds
     content {
       id    = "e${metric_query.key}"
-      label = "Set to ${metric_query.value.desired_capacity} capacity units"
+      label = "Scaling Capacity Scalar (DesiredCapacity = (${local.scaling_capacity_step} + (${local.scaling_capacity_step} * SCS)))"
       expression = "IF(${join(" && ", compact([
         metric_query.value.begin != null ? "m1 > ${metric_query.value.begin}" : null,
         metric_query.value.end != null ? "m1 <= ${metric_query.value.end}" : null,
-        "m2 < ${metric_query.value.desired_capacity}"
-      ]))}, ${metric_query.key + 1}, 0)"
+        "m2 == m3"
+      ]))}, MIN([m3/${local.scaling_capacity_step} + ${metric_query.key}, TIME_SERIES(${length(local.scale_out_cpu_thresholds)})]), 0)"
       return_data = false
     }
   }
 
   metric_query {
-    expression  = "MAX([${join(",", [for i in range(length(local.scaleout_config)) : "e${i}"])}])"
-    id          = "e${length(local.scaleout_config)}"
+    expression  = "MAX([${join(",", [for i in range(length(local.scale_out_cpu_thresholds)) : "e${i}"])}])"
+    id          = "e${length(local.scale_out_cpu_thresholds)}"
     label       = "ScalingCapacityScalar"
     return_data = true
   }
 }
 
-resource "aws_autoscaling_policy" "filtered_req_count_high_scaling" {
-  name                      = "bfd-${var.role}-${local.env}-req-count-high-scaleout"
+resource "aws_autoscaling_policy" "avg_cpu_high" {
+  name                      = "bfd-${var.role}-${local.env}-avg-cpu-high"
   autoscaling_group_name    = aws_autoscaling_group.main.name
   estimated_instance_warmup = 0 # explicitly disable warmup as lifecycle hooks handle it more accurately
   adjustment_type           = "ExactCapacity"
@@ -378,10 +363,10 @@ resource "aws_autoscaling_policy" "filtered_req_count_high_scaling" {
   policy_type               = "StepScaling"
 
   dynamic "step_adjustment" {
-    for_each = local.scaleout_config
+    for_each = local.scale_out_cpu_thresholds
     content {
       metric_interval_lower_bound = step_adjustment.key
-      metric_interval_upper_bound = step_adjustment.key + 1 != length(local.scaleout_config) ? step_adjustment.key + 1 : null
+      metric_interval_upper_bound = step_adjustment.key + 1 != length(local.scale_out_cpu_thresholds) ? step_adjustment.key + 1 : null
       scaling_adjustment          = step_adjustment.value.desired_capacity
     }
   }
