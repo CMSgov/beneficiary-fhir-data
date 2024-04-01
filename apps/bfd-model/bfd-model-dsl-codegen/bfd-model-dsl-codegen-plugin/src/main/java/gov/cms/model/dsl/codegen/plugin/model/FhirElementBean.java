b5c2c32b976f41d347958f7d22a4f295b88508f4
Melissa Guzman <melissa.guzman@cms.hhs.gov>
2024-04-01 14:12:20 -0700

    BFD-3377: Adds base POJOs for Data Dictionary

---
+++ apps/bfd-model/bfd-model-dsl-codegen/bfd-model-dsl-codegen-plugin/src/main/java/gov/cms/model/dsl/codegen/plugin/model/FhirElementBean.java
@@ -0,0 +1,54 @@
+package gov.cms.model.dsl.codegen.plugin.model;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/** Model class for FHIR Elements specifications in a mapping. */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FhirElementBean implements ModelBean {
+
+  /** Id. */
+  private int id;
+
+  /** Name. */
+  private String name;
+
+  /** Description. */
+  private String description;
+
+  /** List of String, Applies To. */
+  private List<String> appliesTo;
+
+  /** List of String, Supplied In. */
+  private List<String> suppliedIn;
+
+  /** BFD Database Table Type. */
+  private String bfdTableType;
+
+  /** BFD Database Column Name. */
+  private String bfdColumnName;
+
+  /** BFD Database Type. */
+  private String bfdDbType;
+
+  /** BFD Database Column Size. */
+  private Integer bfdDbSize;
+
+  /** BFD Java Field Name. */
+  private String bfdJavaFieldName;
+
+  /** List of String, of CCW Mappings. */
+  private List<String> ccwMapping;
+
+  /** List of String, of CCLF Mappings. */
+  private List<String> cclfMapping;
+
+  /** List of FHIR Mappings. */
+  private List<FhirMapping> fhirMapping;
+}
