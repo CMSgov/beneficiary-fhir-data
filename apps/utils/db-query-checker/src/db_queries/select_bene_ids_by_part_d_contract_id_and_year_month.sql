4c1c571e027ce37e7cd094a9d89b4c4f3d64ad98
Don DeVun <dondevun@navapbc.com>
2024-05-13 18:23:16 -0400

    Fixed missing ccw schema in locust tests

--- apps/utils/db-query-checker/src/db_queries/select_bene_ids_by_part_d_contract_id_and_year_month.sql
+++ apps/utils/db-query-checker/src/db_queries/select_bene_ids_by_part_d_contract_id_and_year_month.sql
@@ -1,5 +1,5 @@
 SELECT bene_id::text
-FROM beneficiary_monthly
+FROM ccw.beneficiary_monthly
 WHERE year_month = $1::date
 AND partd_contract_number_id = $2
 ORDER BY bene_id ASC
--- apps/utils/db-query-checker/src/db_queries/select_distinct_part_d_contract_ids_quicker.sql
+++ apps/utils/db-query-checker/src/db_queries/select_distinct_part_d_contract_ids_quicker.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT partd_contract_number_id
 FROM (
   SELECT *
-  FROM beneficiary_monthly
+  FROM ccw.beneficiary_monthly
   WHERE partd_contract_number_id IS NOT NULL
   LIMIT 1000000
 ) bene_monthly
