5b7ba752008f840ea2f56bdd029dbb4acf4009a6
Don DeVun <160619336+dondevun@users.noreply.github.com>
2024-05-13 08:32:13 -0400

    Update ops/terraform/services/server/main.tf
    Co-authored-by: Michael J Burling <michael.burling@cms.hhs.gov>

--- ops/terraform/services/server/main.tf
+++ ops/terraform/services/server/main.tf
@@ -13,7 +13,6 @@ locals {
   latest_bfd_release = module.terraservice.latest_bfd_release
 
   db_environment = var.db_environment_override != null ? var.db_environment_override : local.seed_env
-  # db_cluster_identifier = "bfd-${local.db_environment}-aurora-cluster"
   db_cluster_identifier = var.db_environment_override != null ? "bfd-${var.db_environment_override}-aurora-cluster" : "bfd-${local.seed_env}-aurora-cluster"
   azs                   = ["us-east-1a", "us-east-1b", "us-east-1c"]
   legacy_service        = "fhir"
