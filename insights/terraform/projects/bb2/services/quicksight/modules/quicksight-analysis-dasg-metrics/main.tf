da5ac43bc8ee33fa541bbd2f8db9f8e8320e425f
Austin Schey <austin.schey@forpeople.us>
2024-05-24 10:12:22 -0500

    fix bb2 dashboard label and calculation

--- insights/terraform/projects/bb2/services/quicksight/modules/quicksight-analysis-dasg-metrics/main.tf
+++ insights/terraform/projects/bb2/services/quicksight/modules/quicksight-analysis-dasg-metrics/main.tf
@@ -68,7 +68,7 @@ resource "aws_quicksight_analysis" "quicksight_analysis_dasg_metrics" {
 
     calculated_fields {
       data_set_identifier = "prod_global_state"
-      expression          = "round(({total_grant_and_archived_real_bene_deduped_count} - 265513) / (400000 - 265513), 3)"
+      expression          = "round(({total_grant_and_archived_real_bene_deduped_count} - 265513) / (450000), 3)"
       name                = "bene_served_progress_to_target"
     }
     calculated_fields {
@@ -810,7 +810,7 @@ resource "aws_quicksight_analysis" "quicksight_analysis_dasg_metrics" {
                 visibility   = "VISIBLE"
               }
               selected_field_options {
-                custom_label = "Enrollees served-Progress to FY2023 Target 400K"
+                custom_label = "Enrollees served-Progress to FY2024 Target 400K"
                 field_id     = "0581b3bf-d5ad-47a1-ad31-c6cc9c1408ec.4.1641486129331"
                 visibility   = "VISIBLE"
               }
