1b668f01568342757c1129f0347add0d9852eede
Don DeVun <160619336+dondevun@users.noreply.github.com>
2024-06-24 15:48:34 -0400

    BFD-3134: Remove HSQL placeholders and migration.old folder (#2363)

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration.old/V105__rda_0_12_0_hotfix2.sql
+++
@@ -1,3 +0,0 @@
--- Hot fix for the 0.12 RDA API update, remove not null constraint on clm_typ_ind
-
-alter table rda.fiss_claims alter column clm_typ_ind drop not null;
--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration.old/V106__rda_0_13_0_updates.sql
+++
@@ -1,3 +0,0 @@
--- Add new column introduced in RDA API v0.13
-
-ALTER TABLE rda.fiss_claims ADD intermediary_nb varchar(5);
