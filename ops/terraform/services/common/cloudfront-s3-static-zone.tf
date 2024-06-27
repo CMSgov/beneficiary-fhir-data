41b27620e94dbe2aee79b9d16dc569d3578da1da
timothy-ellis-ky <tim.ellis@semanticbits.com>
2024-06-27 16:24:49 -0400

    move off 3462-test workspace

--- ops/terraform/services/common/cloudfront-s3-static-zone.tf
+++ ops/terraform/services/common/cloudfront-s3-static-zone.tf
@@ -235,7 +235,7 @@ resource "aws_s3_bucket_policy" "cloudfront_logging" {
 resource "aws_cloudfront_distribution" "static_site_distribution" {
   origin {
     domain_name = aws_s3_bucket.cloudfront_bucket.bucket_regional_domain_name
-    origin_id   = "S3-${aws_s3_bucket.cloudfront_bucket.id}"
+    origin_id   = "S3-${aws_s3_bucket.cloudfront_bucket.name}"
     origin_access_control_id = aws_cloudfront_origin_access_control.static_site_control.id
 
     s3_origin_config {
@@ -259,7 +259,7 @@ resource "aws_cloudfront_distribution" "static_site_distribution" {
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD", "OPTIONS"]
     cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "S3-${aws_s3_bucket.cloudfront_bucket.id}"
+    target_origin_id       = "S3-${aws_s3_bucket.cloudfront_bucket.name}"
     viewer_protocol_policy = "redirect-to-https"
 
     forwarded_values {
