bdd539007b54703e4a8ce71a1dcd5e43f00f5fe5
Austin Schey <austin.schey@forpeople.us>
2024-07-11 17:21:21 -0700

    use relative path

--- static-site/_plugins/copy_data_dictionary.rb
+++ static-site/_plugins/copy_data_dictionary.rb
@@ -8,11 +8,11 @@ Jekyll::Hooks.register :site, :after_init do ||
   end
 
 def copy_data_dictionary(version)
-    path = ENV["BFD_PATH"]
-    dd_source = Dir[File.join(path,"/dist/V#{version}-data-dictionary-*json")].sort[-1]
+    current_dir = File.dirname(__FILE__)
+    dd_source = Dir[File.join(current_dir,"../../dist/V#{version}-data-dictionary-*json")].sort[-1]
     contents = File.read(dd_source)
     replaced = contents.gsub(/\\\\n/, "\\n")
-    data_folder = File.join(path, "/static-site/_data");
+    data_folder = File.join(current_dir, "../_data");
     FileUtils.mkdir_p data_folder
     dd_dest = File.join(data_folder, "/data-dictionary-v#{version}.json")
     File.open(dd_dest, "w") { |file| file.puts replaced }
