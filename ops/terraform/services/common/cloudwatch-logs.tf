a8643d691f0fc6b429b2a257d3826effad20e9bc
Mitch Alessio <mitch.alessio@forpeople.us>
2024-08-14 12:03:58 -0400

    Add GHA ci-static-site CW Log Group resource to common

--- ops/terraform/services/common/cloudwatch-logs.tf
+++ ops/terraform/services/common/cloudwatch-logs.tf
@@ -9,3 +9,9 @@ resource "aws_cloudwatch_log_group" "var_log_secure" {
   name       = "/bfd/${local.env}/var/log/secure"
   kms_key_id = data.aws_kms_key.cmk.arn
 }
+
+# CloudWatch Log Group for the "CI - Deploy Static Site" Terraform plan/apply logs
+resource "aws_cloudwatch_log_group" "gha_ci_static_site" {
+  name       = "/bfd/${local.env}/gha/ci-static-site"
+  kms_key_id = data.aws_kms_key.cmk.arn
+}
