f33f9574f2f387defb956b1c9e1a9abe35976a2f
Don DeVun <dondevun@navapbc.com>
2024-08-23 12:43:56 -0400

    Add shellcheck to precommit hook.

--- .github/scripts/pre-commit.sh
+++ .github/scripts/pre-commit.sh
@@ -49,4 +49,31 @@ checkSecretFilesForPlainText() {
   set -e
 }
 
+# Runs shellcheck against any committed shell scripts
+runShellCheckForCommitFiles() {
+  echo 'Running shellcheck on committed files'
+  if [ "$(which shellcheck)" ]; then
+    tmpfile=$(mktemp)
+    git diff --cached --name-only --diff-filter=ACM >"$tmpfile"
+    commits=`cat $tmpfile`
+    for file in $commits; do
+      firstTwo=$( sed 's/^\(..\).*/\1/;q' "$file" )
+      filename=$(basename -- "$file")
+      extension="${filename##*.}"
+      # check for a hashbang or a .sh extension to determine if this is a shell script.
+      if [ "$firstTwo" == "#!" ] || [ $extension == "sh" ]; then
+        # run shellcheck with severity level warning, and suppress warnings about invalid hashbangs (allows it to ignore other types of scripts, e.g. python)
+        shellcheck -e SC1071,SC2239 -S warning "$file"
+        if [[ $? != 0 ]]; then
+          echo "Please fix errors before continuing."
+          return
+        fi
+      fi
+    done
+  else
+    echo 'Please install shellcheck before committing.'
+    exit 1
+  fi
+}
 checkSecretFilesForPlainText
+runShellCheckForCommitFiles
