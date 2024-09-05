c0decedd5ef292e4141163a2a06fbea13c890c64
Michael J Burling <michael.burling@cms.hhs.gov>
2024-09-05 09:40:26 -0500

    BFD-3606: Add gitleaks configuration on pre-commit (#2420)

--- .github/scripts/pre-commit.sh
+++ .github/scripts/pre-commit.sh
@@ -74,5 +74,19 @@ runShellCheckForCommitFiles() {
     exit 1
   fi
 }
+
+checkGitleaks() {
+  echo "Attempting to execute gitleaks. This may take a minute..."
+  if ! command -v gitleaks >/dev/null; then
+    echo "'gitleaks' not found. Install gitleaks before pushing your changes."
+    return 1
+  fi
+  if gitleaks protect --staged --verbose; then
+    return 0
+  else
+    return 1
+  fi
+}
 checkSecretFilesForPlainText
 runShellCheckForCommitFiles
+checkGitleaks
