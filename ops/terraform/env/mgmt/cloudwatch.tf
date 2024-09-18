8396a917cd2de12e89b972465d326a3ccc7021cb
timothy-ellis-ky <tim.ellis@semanticbits.com>
2024-09-18 15:29:07 -0400

    typo

--- ops/terraform/env/mgmt/cloudwatch.tf
+++ ops/terraform/env/mgmt/cloudwatch.tf
@@ -2,7 +2,7 @@
 # CloudWatch Log Group for the "CI - Update OPS Infrastructure" Terraform plan/apply logs
 resource "aws_cloudwatch_log_group" "gha_ci_ops_infra" {
   name       = "/bfd/${local.env}/gha/ci-ops-infra"
-  kms_key_id = aws_kms_key.primary_config[local.env].arn
+  kms_key_id = data.aws_kms_key.cmk.arn
 }
 
 resource "aws_iam_policy" "github_actions_ci_ops" {
