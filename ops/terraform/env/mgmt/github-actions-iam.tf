127dadf70bc805081d0f463d54a490d6dfdad4e5
Michael J Burling <michael.burling@cms.hhs.gov>
2024-09-19 09:11:48 -0500

    Expand thumbprints on OIDC provider certificates

--- ops/terraform/env/mgmt/github-actions-iam.tf
+++ ops/terraform/env/mgmt/github-actions-iam.tf
@@ -340,7 +340,7 @@ data "tls_certificate" "github_actions" {
 
 resource "aws_iam_openid_connect_provider" "github_actions" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.github_actions.certificates[0].sha1_fingerprint]
+  thumbprint_list = data.tls_certificate.github_actions.certificates[*].sha1_fingerprint
   url             = "https://token.actions.githubusercontent.com"
 }
 
