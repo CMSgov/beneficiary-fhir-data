893fd9df4174ab6b018e23b7566099a0b8f30a34
Michael J Burling <michael.burling@cms.hhs.gov>
2024-10-11 17:29:51 -0500

    Prepare for a blue/green deployment model

--- ops/terraform/services/server/modules/bfd_server_asg/scripts/asg-data.sh
+++ ops/terraform/services/server/modules/bfd_server_asg/scripts/asg-data.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env sh
 #######################################
-# Fetch an autoscaling group's instances.
-# Returns a well-formatted json object instance "count" and "instance_launch_templates" as strings.
+# Fetch the even/odd autoscaling group instance and warm_pool counts.
+# Returns json object includes...
+# "odd_desired_capacity", "odd_warmpool_min_size", "even_desired_capacity",
+# and "even_warmpool_min_size" as strings.
 # This is intended for use in terraform as an external data resource.
 # Requires yq and awscli
 #
 # Example usage:
 # `./asg-data.sh 2558-test` might yield the following for an existing ASG:
-#   {
-#     "count": "3",
-#     "instance_launch_templates": "[\"3\",\"3\",\"3\"]"
-#   }
+#
+#  {
+#    "odd_desired_capacity": "3",
+#    "odd_warmpool_min_size": "3",
+#    "even_desired_capacity": "0",
+#    "even_warmpool_min_size": "0"
+#  }
 #
 # `./asg-data.sh foo` yields the following for a non-existent ASG:
-#   {
-#     "count": "0",
-#     "instance_launch_templates": "[]"
-#   }
+#
+#  {
+#    "odd_desired_capacity": "0",
+#    "odd_warmpool_min_size": "0",
+#    "even_desired_capacity": "0",
+#    "even_warmpool_min_size": "0"
+#  }
 #
 # Arguments:
 #   $1 maps to desired environment, used in templated "bfd-${1}-fhir", e.g. "bfd-2558-test-fhir"
@@ -25,11 +33,18 @@ set -eu
 BFD_ENV="$1"
 
 # shellcheck disable=SC2155
-export asg="$(aws autoscaling describe-auto-scaling-groups \
-    --auto-scaling-group-names "bfd-${BFD_ENV}-fhir" \
+export odd="$(aws autoscaling describe-auto-scaling-groups \
+    --auto-scaling-group-names "bfd-${BFD_ENV}-fhir-odd" \
+    --output json --query 'AutoScalingGroups[0]')"
+
+# shellcheck disable=SC2155
+export even="$(aws autoscaling describe-auto-scaling-groups \
+    --auto-scaling-group-names "bfd-${BFD_ENV}-fhir-even" \
     --output json --query 'AutoScalingGroups[0]')"
 
 yq  --output-format=json --null-input '{
-  "desired_capacity": env(asg) | .DesiredCapacity // 0 | tostring,
-  "instance_launch_templates": env(asg) | [.Instances[].LaunchTemplate.Version] | @json
+  "odd_desired_capacity": env(odd) | .DesiredCapacity // 0 | tostring,
+  "odd_warmpool_min_size": env(odd) | .WarmPoolConfiguration.MinSize // 0 | tostring,
+  "even_desired_capacity": env(even) | .DesiredCapacity // 0 | tostring,
+  "even_warmpool_min_size": env(even) | .WarmPoolConfiguration.MinSize // 0 | tostring
 }'
