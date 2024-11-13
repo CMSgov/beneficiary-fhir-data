e38ccf0e1c4145b78ca0c73d7e49cfb21221a8c5
Michael J Burling <michael.burling@cms.hhs.gov>
2024-11-12 18:06:40 -0600

    Improve shellcheck logic

--- .github/scripts/pre-commit.sh
+++ .github/scripts/pre-commit.sh
@@ -53,24 +53,13 @@ checkSecretFilesForPlainText() {
 runShellCheckForCommitFiles() {
   echo 'Running shellcheck on committed files'
   if [ "$(which shellcheck)" ]; then
-    tmpfile=$(mktemp)
-    git diff --cached --name-only --diff-filter=ACM >"$tmpfile"
-    commits=$(cat "$tmpfile")
-    for file in $commits; do
-      filename=$(basename -- "$file")
-      extension="${filename##*.}"
-      if [ "$extension" == "zip" ]; then
-        continue
-      fi
-      firstTwo=$( sed 's/^\(..\).*/\1/;q' "$file" )
-      # check for a hashbang or a .sh extension to determine if this is a shell script.
-      if [ "$firstTwo" == "#!" ] && [ "$filename" != "Jenkinsfile" ] || [ "$extension" == "sh" ]; then
+    files="$(git diff --cached --name-only --diff-filter=ACM | xargs file | grep -E '(sh|shell) script' | cut -f1 -d ':')"
+    for file in $files; do
         # run shellcheck with severity level warning, and suppress warnings about invalid hashbangs (allows it to ignore other types of scripts, e.g. python)
         if ! shellcheck -e SC1071,SC2239 -S warning "$file"; then
           echo "Please fix errors before continuing."
           exit 1 
         fi
-      fi
     done
   else
     echo 'Please install shellcheck before committing.'
