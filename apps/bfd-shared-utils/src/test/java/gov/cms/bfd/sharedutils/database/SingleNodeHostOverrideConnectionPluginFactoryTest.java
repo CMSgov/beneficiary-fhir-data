b31b88a65673e2b22f723a779096d15d951764c1
Mitch Alessio <mitch.alessio@forpeople.us>
2024-11-25 16:52:38 -0500

    Add simple test for plugin factory

---
+++ apps/bfd-shared-utils/src/test/java/gov/cms/bfd/sharedutils/database/SingleNodeHostOverrideConnectionPluginFactoryTest.java
@@ -0,0 +1,26 @@
+package gov.cms.bfd.sharedutils.database;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+import java.util.Properties;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import software.amazon.jdbc.PluginService;
+
+class SingleNodeHostOverrideConnectionPluginFactoryTest {
+
+  @Test
+  void testGetInstanceShouldReturnCorrectPluginTypeWhenCalled() {
+    // Arrange
+    final var mockPluginService = mock(PluginService.class);
+    final var mockProperties = mock(Properties.class);
+    final var pluginFactory = new SingleNodeHostOverrideConnectionPluginFactory();
+
+    // Act
+    final var plugin = pluginFactory.getInstance(mockPluginService, mockProperties);
+
+    // Assert
+    assertInstanceOf(SingleNodeHostOverrideConnectionPlugin.class, plugin);
+  }
+}
