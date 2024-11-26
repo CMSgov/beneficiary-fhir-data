ace9eab82609ddc2c3763c1cc73c48b6a192f056
aschey-forpeople <162160982+aschey-forpeople@users.noreply.github.com>
2024-11-26 12:46:42 -0800

    Update apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/AllowSamhsaFilter.java

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/AllowSamhsaFilter.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/AllowSamhsaFilter.java
@@ -27,7 +27,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-/***
+/**
  * Filter class to add metadata to the request that says whether clients can see SAMHSA data or not.
  */
 @Component("AllowSamhsaFilterBean")
