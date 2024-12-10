12a68b270899366be9c6656c9dee4e031749ad72
timothy-ellis-ky <tim.ellis@semanticbits.com>
2024-12-10 13:42:40 -0500

    PR Review rework

--- ops/terraform/services/server/modules/bfd_server_slo_alarms/data-sources.tf
+++ ops/terraform/services/server/modules/bfd_server_slo_alarms/data-sources.tf
@@ -1,18 +1,18 @@
 data "aws_region" "current" {}
 
 data "aws_sns_topic" "high_alert_sns" {
-  count = local.env_sns.high_alert != null ? 1 : 0
-  name  = (local.env_sns.high_alert != null ? local.env_sns.high_alert : "EMPTY")
+  count = local.env_sns.high_alert != "null" ? 1 : 0
+  name  = local.env_sns.high_alert
 }
 
 data "aws_sns_topic" "alert_sns" {
-  count = local.env_sns.alert != null ? 1 : 0
-  name  = (local.env_sns.alert != null ? local.env_sns.alert : "EMPTY")
+  count = local.env_sns.alert != "null" ? 1 : 0
+  name  = local.env_sns.alert
 }
 
 data "aws_sns_topic" "warning_sns" {
-  count = local.env_sns.warning != null ? 1 : 0
-  name  = (local.env_sns.warning != null ? local.env_sns.warning : "EMPTY")
+  count = local.env_sns.warning != "null" ? 1 : 0
+  name  = local.env_sns.warning
 }
 
 data "external" "client_ssls_by_partner" {
