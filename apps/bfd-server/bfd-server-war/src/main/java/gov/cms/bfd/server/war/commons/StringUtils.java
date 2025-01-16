498d00fefd41e8f775ef30a6ebc534355466d76b
aschey-forpeople <162160982+aschey-forpeople@users.noreply.github.com>
2025-01-16 13:19:34 -0800

    BBFD-3829: Gracefully handle improperly formatted numeric request parameters (#2525)

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/commons/StringUtils.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/commons/StringUtils.java
@@ -1,5 +1,6 @@
 package gov.cms.bfd.server.war.commons;
 
+import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
 import com.google.common.collect.ImmutableList;
 
 /** Helper Utils class for Functions shared across. multiple classes. */
@@ -30,5 +31,41 @@ public class StringUtils {
       builder.add(curr.toString().trim());
     }
     return builder.build().toArray(new String[0]);
+  }
+
+  /**
+   * Attempts to parse the input as a Long, throwing a bad request error if it fails.
+   *
+   * @param input numeric input
+   * @param fieldName field name for error message
+   * @return parsed value
+   */
+  public static Long parseLongOrBadRequest(String input, String fieldName) {
+    try {
+      return Long.parseLong(input);
+    } catch (NumberFormatException ex) {
+      // Intentionally omitting the supplied input from the message here because it could be
+      // sensitive
+      throw new InvalidRequestException(
+          String.format("Failed to parse value for %s as a number.", fieldName));
+    }
+  }
+
+  /**
+   * Attempts to parse the input as an int, throwing a bad request error if it fails.
+   *
+   * @param input numeric input
+   * @param fieldName field name for error message
+   * @return parsed value
+   */
+  public static int parseIntOrBadRequest(String input, String fieldName) {
+    try {
+      return Integer.parseInt(input);
+    } catch (NumberFormatException ex) {
+      // Intentionally omitting the supplied input from the message here because it could be
+      // sensitive
+      throw new InvalidRequestException(
+          String.format("Failed to parse value for %s as a number.", fieldName));
+    }
   }
 }
