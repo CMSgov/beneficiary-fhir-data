3093f9df63920ab77726102070ee46f8c8a9809a
TimRotenSB <Tim.Roten@icf.com>
2025-02-17 07:44:38 -0500

    BFD-3886:Fix logging, permissions in MGMT Action

--- ops/terraform/env/mgmt/cloudwatch.tf
+++ ops/terraform/env/mgmt/cloudwatch.tf
@@ -1,5 +1,5 @@
 # CloudWatch Log Group for the "CI - Update OPS Infrastructure" Terraform plan/apply logs
-resource "aws_cloudwatch_log_group" "gha_ci_ops_infra" {
-  name       = "/bfd/${local.env}/gha/ci-ops-infra"
+resource "aws_cloudwatch_log_group" "cd-terraform-mgmt-deploy" {
+  name       = "/bfd/${local.env}/gha/cd-terraform-mgmt-deploy"
   kms_key_id = data.aws_kms_key.cmk.arn
 }
