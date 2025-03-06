7de8cf66ee28bb612331c03a98803be375079426
Mitch Alessio <mitch.alessio@forpeople.us>
2025-03-06 11:32:10 -0500

    Support properly versioned server-regression container Images

--- ops/terraform/services/server/server-regression/data-sources.tf
+++ ops/terraform/services/server/server-regression/data-sources.tf
@@ -43,11 +43,6 @@ data "aws_ecr_image" "image" {
   image_tag       = local.docker_image_tag
 }
 
-data "aws_ssm_parameter" "docker_image_tag" {
-  # TODO: consider making this more environment-specific, versioning RFC in BFD-1743 may inform us of how
-  name = "/bfd/mgmt/server/nonsensitive/server_regression_latest_image_tag"
-}
-
 data "aws_security_group" "rds" {
   vpc_id = data.aws_vpc.main.id
   filter {
