240b6cdb93926ae65df7864b35170a30f70f7115
Don DeVun <dondevun@navapbc.com>
2025-03-24 15:27:12 +0000

    Add NOT VALID to foreign key creation

--- apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V20250322101944077__add_cascade_delete_to_rda.sql
+++ apps/bfd-model/bfd-model-rif/src/main/resources/db/migration/V20250322101944077__add_cascade_delete_to_rda.sql
@@ -3,11 +3,13 @@ DROP CONSTRAINT mcs_tags_clm_id_fkey,
 ADD CONSTRAINT mcs_tags_clm_id_fkey
 FOREIGN KEY (clm_id)
 REFERENCES rda.mcs_claims(idr_clm_hd_icn)
-ON DELETE CASCADE;
+ON DELETE CASCADE
+NOT VALID;
 
 ALTER TABLE rda.fiss_tags
 DROP CONSTRAINT fiss_tags_clm_id_fkey,
 ADD CONSTRAINT fiss_tags_clm_id_fkey
 FOREIGN KEY (clm_id)
 REFERENCES rda.fiss_claims(claim_id)
-ON DELETE CASCADE;
+ON DELETE CASCADE
+NOT VALID;
