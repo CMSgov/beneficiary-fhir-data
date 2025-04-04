f74d96599e5ec5e84669ac97e1425624f07a0949
Mitch Alessio <mitch.alessio@forpeople.us>
2025-04-03 20:10:21 -0400

    Consolidate ccw-pipeline Task terraform into main.tf

--- ops/services/05-ccw-pipeline/iam.tf
+++ ops/services/05-ccw-pipeline/iam.tf
@@ -1,3 +1,14 @@
+data "aws_iam_policy_document" "service_assume_role" {
+  for_each = toset(["ecs-tasks", "lambda", "scheduler"])
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["${each.value}.amazonaws.com"]
+    }
+  }
+}
+
 data "aws_iam_policy_document" "ccw_s3" {
   statement {
     sid = "AllowCCWBucketAccess"
