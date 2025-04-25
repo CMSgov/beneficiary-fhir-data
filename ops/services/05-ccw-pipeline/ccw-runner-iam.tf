402b53e85f1c9b0b8c62f8b25edecfc68c8e6943
Mitch Alessio <mitch.alessio@forpeople.us>
2025-04-25 14:35:01 -0400

    Fix invalid SSM IAM Policy

--- ops/services/05-ccw-pipeline/ccw-runner-iam.tf
+++ ops/services/05-ccw-pipeline/ccw-runner-iam.tf
@@ -21,8 +21,8 @@ data "aws_iam_policy_document" "ccw_runner_ssm" {
     actions = ["ssm:GetParameter"]
     resources = [
       # TODO: Remove "/ng/" prefix
-      "arn:aws:ssm:${local.region}:${local.account_id}:parameter/ng/bfd/${local.env}/pipeline/sensitive/db/username",
-      "arn:aws:ssm:${local.region}:${local.account_id}:parameter/ng/bfd/${local.env}/pipeline/sensitive/db/password"
+      "arn:aws:ssm:${local.region}:${local.account_id}:parameter/ng/bfd/${local.env}/${local.service}/sensitive/db/username",
+      "arn:aws:ssm:${local.region}:${local.account_id}:parameter/ng/bfd/${local.env}/${local.service}/sensitive/db/password"
     ]
   }
 }
