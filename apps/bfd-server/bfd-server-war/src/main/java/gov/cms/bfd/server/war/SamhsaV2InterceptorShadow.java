def21339ef5875ac5e26b603ceb69fb01aa5b7d5
Mahi Fentaye <168698136+MahiFentaye@users.noreply.github.com>
2025-04-30 09:57:26 -0400

    BFD-4034: skip synthetic claim id when logging SAMHSA filter mismatch (#2642)
    Co-authored-by: aschey-forpeople <162160982+aschey-forpeople@users.noreply.github.com>

--- apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/SamhsaV2InterceptorShadow.java
+++ apps/bfd-server/bfd-server-war/src/main/java/gov/cms/bfd/server/war/SamhsaV2InterceptorShadow.java
@@ -2,9 +2,13 @@ package gov.cms.bfd.server.war;
 
 import static gov.cms.bfd.server.war.SpringConfiguration.SSM_PATH_SAMHSA_V2_SHADOW;
 
+import gov.cms.bfd.model.rda.entities.RdaFissClaim;
+import gov.cms.bfd.model.rda.entities.RdaMcsClaim;
 import gov.cms.bfd.server.war.commons.SecurityTagManager;
 import gov.cms.bfd.server.war.r4.providers.pac.common.ClaimWithSecurityTags;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.hl7.fhir.r4.model.Coding;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +37,14 @@ public class SamhsaV2InterceptorShadow {
   /** The securityTagManager. */
   private final SecurityTagManager securityTagManager;
 
+  private static final String SAMHSA_MISMATCH_ERROR_MESSAGE =
+      "Samhsa: Claim ID mismatch between old SAMHSA filter (hasSamhsaData) and new SAMHSA 2.0 (hasSamhsaDataV2) - ";
+
+  /** Regex used to recognize real mbis. */
+  private static final Pattern pattern =
+      Pattern.compile(
+          "\\b[1-9](?![SLOIBZ])[A-Z](?![SLOIBZ])[A-Z\\d]\\d(?![SLOIBZ])[A-Z](?![SLOIBZ])[A-Z\\d]\\d(?![SLOIBZ])[A-Z]{2}\\d{2}\\b");
+
   protected SamhsaV2InterceptorShadow(
       @Value("${" + SSM_PATH_SAMHSA_V2_SHADOW + ":false}") Boolean samhsaV2Shadow,
       SecurityTagManager securityTagManager) {
@@ -56,19 +68,22 @@ public class SamhsaV2InterceptorShadow {
     boolean hasSamhsaDataV2 = v2SamhsaConsentInterceptor.shouldRedactResource(securityTags);
 
     if (hasSamhsaDataV2 != hasSamhsaData) {
-      String claimId = securityTagManager.getClaimId(claimWithSecurityTags.getClaimEntity());
-      String claimClass = claimWithSecurityTags.getClaimEntity().getClass().getSimpleName();
+      Object claimEntity = claimWithSecurityTags.getClaimEntity();
+      String claimId = securityTagManager.getClaimId(claimEntity);
+      String claimClass = claimEntity.getClass().getSimpleName();
+      boolean isSynthetic = isSynthetic(claimEntity, claimId);
 
       // Identify which flag has the mismatch
       String missingSource = (hasSamhsaDataV2 ? "hasSamhsaData" : "hasSamhsaDataV2");
 
       // Log the mismatch between the old and new SAMHSA filters
-      logger.error(
-          "Samhsa: Claim ID mismatch between old SAMHSA filter (hasSamhsaData) and new SAMHSA 2.0 (hasSamhsaDataV2) - "
-              + "Claim ID: {} (Class: {}) - Missing in: {}",
-          claimId,
-          claimClass,
-          missingSource);
+      if (!isSynthetic) {
+        logger.error(
+            SAMHSA_MISMATCH_ERROR_MESSAGE + "Claim ID: {} (Class: {}) - Missing in: {}",
+            claimId,
+            claimClass,
+            missingSource);
+      }
     }
   }
 
@@ -88,19 +103,50 @@ public class SamhsaV2InterceptorShadow {
     boolean hasSamhsaDataV2 = v1SamhsaConsentInterceptor.shouldRedactResource(securityTags);
 
     if (hasSamhsaDataV2 != hasSamhsaData) {
-      String claimId = securityTagManager.getClaimId(claimWithSecurityTags.getClaimEntity());
-      String claimClass = claimWithSecurityTags.getClaimEntity().getClass().getSimpleName();
+      Object claimEntity = claimWithSecurityTags.getClaimEntity();
+      String claimId = securityTagManager.getClaimId(claimEntity);
+      String claimClass = claimEntity.getClass().getSimpleName();
+      boolean isSynthetic = isSynthetic(claimEntity, claimId);
 
       // Identify which flag has the mismatch
       String missingSource = (hasSamhsaDataV2 ? "hasSamhsaData" : "hasSamhsaDataV2");
 
       // Log the mismatch between the old and new SAMHSA filters
-      logger.error(
-          "Samhsa: Claim ID mismatch between old SAMHSA filter (hasSamhsaData) and new SAMHSA 2.0 (hasSamhsaDataV2) - "
-              + "Claim ID: {} (Class: {}) - Missing in: {}",
-          claimId,
-          claimClass,
-          missingSource);
+      if (!isSynthetic) {
+        logger.error(
+            SAMHSA_MISMATCH_ERROR_MESSAGE + "Claim ID: {} (Class: {}) - Missing in: {}",
+            claimId,
+            claimClass,
+            missingSource);
+      }
+    }
+  }
+
+  /**
+   * Validate if mbi is real.
+   *
+   * @param mbi the hasNoSamhsaData boolean
+   * @return boolean if it is a Valid MBI
+   */
+  public static boolean isValidMBI(String mbi) {
+    Matcher matcher = pattern.matcher(mbi);
+    return matcher.find();
+  }
+
+  /**
+   * Validate if data is Synthetic.
+   *
+   * @param claimEntity the claim Entity
+   * @param claimId the claimId
+   * @return boolean if it is a synthetic claim
+   */
+  public static boolean isSynthetic(Object claimEntity, String claimId) {
+    if (claimEntity instanceof RdaFissClaim fissClaim) {
+      return !isValidMBI(fissClaim.getMbi());
+    } else if (claimEntity instanceof RdaMcsClaim mcsClaim) {
+      return !isValidMBI(String.valueOf(mcsClaim.getMbiRecord().getMbi()));
+    } else {
+      return Long.parseLong(claimId) < 0;
     }
   }
 }
