d059b9ff09391abeb59d793b5ba09b9042fc8734
Austin Schey <austin.schey@forpeople.us>
2025-04-30 16:30:18 -0700

    java docs

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/types/DateTimeBound.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/types/DateTimeBound.java
@@ -4,6 +4,12 @@ import ca.uhn.fhir.rest.param.DateParam;
 import gov.cms.bfd.server.ng.DateUtil;
 import java.time.LocalDateTime;
 
+/**
+ * Represents a boundary condition for a DateTime filter.
+ *
+ * @param bound boundary DateTime
+ * @param boundType Denotes if the bound is inclusive or exclusive
+ */
 public record DateTimeBound(LocalDateTime bound, DateTimeBoundType boundType) {
   public DateTimeBound(DateParam dateParam) {
     this(
