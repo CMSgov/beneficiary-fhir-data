8f6db488015754f1c68ba5add4163fea89cbe205
Austin Schey <austin.schey@forpeople.us>
2025-04-30 15:28:54 -0700

    simplify date search tests

--- apps/bfd-server-ng/src/test/java/gov/cms/bfd/server/ng/PatientSearchIT.java
+++ apps/bfd-server-ng/src/test/java/gov/cms/bfd/server/ng/PatientSearchIT.java
@@ -5,13 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import ca.uhn.fhir.rest.api.Constants;
 import ca.uhn.fhir.rest.gclient.DateClientParam;
+import ca.uhn.fhir.rest.gclient.ICriterion;
 import ca.uhn.fhir.rest.gclient.IQuery;
 import ca.uhn.fhir.rest.gclient.TokenClientParam;
 import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
 import java.time.LocalDateTime;
+import java.util.stream.Stream;
+
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Patient;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class PatientSearchIT extends IntegrationTestBase {
   private IQuery<Bundle> searchBundle() {
@@ -72,56 +78,71 @@ public class PatientSearchIT extends IntegrationTestBase {
                 .execute());
   }
 
-  @Test
-  void patientSearchByDateExact() {
+  private static Stream<Arguments> patientSearchByDate() {
+    return Stream.of(
+        Arguments.of(new TokenClientParam(Patient.SP_RES_ID).exactly().identifier("1")),
+        Arguments.of(
+            new TokenClientParam(Patient.SP_IDENTIFIER)
+                .exactly()
+                .systemAndIdentifier(SystemUrls.CMS_MBI, "1S000000000")));
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void patientSearchByDate(ICriterion<TokenClientParam> searchCriteriaId) {
+    // Search date exact
     var patientBundle =
         searchBundle()
-            .where(new TokenClientParam(Patient.SP_RES_ID).exactly().identifier("1"))
+            .where(searchCriteriaId)
             .and(
                 new DateClientParam(Constants.PARAM_LASTUPDATED)
                     .exactly()
                     .day(DateUtil.toDate(LocalDateTime.parse("2024-01-01T00:00:00"))))
             .execute();
     assertEquals(1, patientBundle.getEntry().size());
-    expect.serializer("fhir+json").toMatchSnapshot(patientBundle);
-  }
 
-  @Test
-  void patientSearchByDateGreaterThan() {
-    var patientBundle =
+    // Search date greater than
+    patientBundle =
         searchBundle()
-            .where(new TokenClientParam(Patient.SP_RES_ID).exactly().identifier("1"))
+            .where(searchCriteriaId)
             .and(
                 new DateClientParam(Constants.PARAM_LASTUPDATED)
                     .after()
                     .day(DateUtil.toDate(LocalDateTime.parse("2024-01-01T00:00:00"))))
             .execute();
     assertEquals(0, patientBundle.getEntry().size());
-  }
 
-  @Test
-  void patientSearchByDateGreaterThanEqual() {
-    var patientBundle =
+    // Search date less than
+    patientBundle =
         searchBundle()
-            .where(new TokenClientParam(Patient.SP_RES_ID).exactly().identifier("1"))
+            .where(searchCriteriaId)
+            .and(
+                new DateClientParam(Constants.PARAM_LASTUPDATED)
+                    .before()
+                    .day(DateUtil.toDate(LocalDateTime.parse("2024-01-01T00:00:00"))))
+            .execute();
+    assertEquals(0, patientBundle.getEntry().size());
+
+    // Search date greater than or equal
+    patientBundle =
+        searchBundle()
+            .where(searchCriteriaId)
             .and(
                 new DateClientParam(Constants.PARAM_LASTUPDATED)
                     .afterOrEquals()
                     .day(DateUtil.toDate(LocalDateTime.parse("2024-01-01T00:00:00"))))
             .execute();
     assertEquals(1, patientBundle.getEntry().size());
-  }
 
-  @Test
-  void patientSearchByDateLessThan() {
-    var patientBundle =
+    // Search date less than or equal
+    patientBundle =
         searchBundle()
-            .where(new TokenClientParam(Patient.SP_RES_ID).exactly().identifier("1"))
+            .where(searchCriteriaId)
             .and(
                 new DateClientParam(Constants.PARAM_LASTUPDATED)
-                    .after()
+                    .beforeOrEquals()
                     .day(DateUtil.toDate(LocalDateTime.parse("2024-01-01T00:00:00"))))
             .execute();
-    assertEquals(0, patientBundle.getEntry().size());
+    assertEquals(1, patientBundle.getEntry().size());
   }
 }
