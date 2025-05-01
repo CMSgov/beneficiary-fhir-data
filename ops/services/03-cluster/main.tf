2feb1b10ea41abffa03516a94f28924c43c4a11f
Mitchell Alessio <5306896+malessi@users.noreply.github.com>
2025-05-01 14:29:50 -0400

    BFD-3945: Formalize ECS Cluster Definition (#2648)

--- ops/services/03-cluster/main.tf
+++ ops/services/03-cluster/main.tf
@@ -13,32 +13,31 @@ module "terraservice" {
 }
 
 locals {
-  # Local module definitions
-  service   = "cluster"
-  layer     = "data"
-  full_name = "bfd-${local.env}-${local.service}"
-
   # Derived Values
   ## Module Lookups
+  service          = module.terraservice.service
   default_tags     = module.terraservice.default_tags
   env              = module.terraservice.env
   is_ephemeral_env = module.terraservice.is_ephemeral_env
-  kms_key_alias    = nonsensitive(module.terraservice.ssm_config["/bfd/common/kms_key_alias"])
+  env_key_arn      = module.terraservice.env_key_arn
 
-  ## Data Source Lookups
-  kms_data_key_arn = data.aws_kms_key.cmk.arn
+  # Local module definitions
+  layer     = "data"
+  full_name = "bfd-${local.env}-${local.service}"
 }
 
 resource "aws_cloudwatch_log_group" "this" {
   name         = "/aws/ecs/${local.full_name}"
-  kms_key_id   = local.kms_data_key_arn
+  kms_key_id   = local.env_key_arn
   skip_destroy = true
 }
 
 resource "aws_ecs_cluster" "this" {
   name = local.full_name
 
-  # TODO: More fully address security concerns such as this in BFD-3945
+  # This may or may not be necessary, but it does not hurt to include. We do not manage GuardDuty
+  # Runtime Monitoring configuration, but we can at least tag this cluster as opting into Runtime
+  # Monitoring.
   tags = {
     GuardDutyManaged = true
   }
@@ -48,13 +47,14 @@ resource "aws_ecs_cluster" "this" {
     value = !local.is_ephemeral_env || var.container_insights_enabled_ephemeral_override ? "enhanced" : "disabled"
   }
 
-  # TODO: Introduce a `managed_storage_configuration` block for cluster-level encryption
-  #       Without this block, encryption on storage defaults to an AWS-managed key.
-  #       This requires adjustments to the `mgmt` module
-  #       Address in BFD-3945
   configuration {
+    managed_storage_configuration {
+      fargate_ephemeral_storage_kms_key_id = local.env_key_arn
+      kms_key_id                           = local.env_key_arn
+    }
+
     execute_command_configuration {
-      kms_key_id = local.kms_data_key_arn
+      kms_key_id = local.env_key_arn
       logging    = "OVERRIDE"
 
       log_configuration {
@@ -64,13 +64,10 @@ resource "aws_ecs_cluster" "this" {
   }
 }
 
-# TODO: Consider a dynamic selection based on prod/non-prod with FARGATE and FARGATE_SPOT
-# TODO: Formalize `default_capacity_provider_strategy`
-#       Address in BFD-3945
 resource "aws_ecs_cluster_capacity_providers" "this" {
   cluster_name = aws_ecs_cluster.this.name
 
-  capacity_providers = ["FARGATE_SPOT"]
+  capacity_providers = ["FARGATE_SPOT", "FARGATE"]
 
   default_capacity_provider_strategy {
     base              = 1
