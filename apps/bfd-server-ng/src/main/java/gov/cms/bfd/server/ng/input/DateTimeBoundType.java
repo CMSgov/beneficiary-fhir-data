b1393815dd86a67375c51f9799e96033a44ac856
Austin Schey <austin.schey@forpeople.us>
2025-05-12 13:30:02 -0700

    handle null prefix

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/input/DateTimeBoundType.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/input/DateTimeBoundType.java
@@ -16,6 +16,10 @@ public enum DateTimeBoundType {
    * @return bound type
    */
   public static DateTimeBoundType fromPrefix(ParamPrefixEnum prefix) {
+    // If prefix is missing, assume it's using '='
+    if (prefix == null) {
+      return DateTimeBoundType.INCLUSIVE;
+    }
     // Note that not all prefix values are valid for a datetime filter.
     return switch (prefix) {
       case ParamPrefixEnum.LESSTHAN, ParamPrefixEnum.GREATERTHAN -> DateTimeBoundType.EXCLUSIVE;
