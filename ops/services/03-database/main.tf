fbf8a6302ee09bc854fd5980494e7324ac07fd54
Mitch Alessio <mitch.alessio@forpeople.us>
2025-05-15 15:51:53 -0400

    Init database Terraservice

---
+++ ops/services/03-database/main.tf
@@ -0,0 +1,187 @@
+module "terraservice" {
+  source = "../../terraform-modules/bfd/bfd-terraservice"
+
+  environment_name     = terraform.workspace
+  service              = "database"
+  relative_module_root = "ops/services/03-database"
+  subnet_layers        = ["data"]
+}
+
+locals {
+  service                  = module.terraservice.service
+  region                   = module.terraservice.region
+  account_id               = module.terraservice.account_id
+  default_tags             = module.terraservice.default_tags
+  env                      = module.terraservice.env
+  seed_env                 = module.terraservice.seed_env
+  is_ephemeral_env         = module.terraservice.is_ephemeral_env
+  latest_bfd_release       = module.terraservice.latest_bfd_release
+  ssm_config               = module.terraservice.ssm_config
+  env_key_alias            = module.terraservice.env_key_alias
+  env_config_key_alias     = module.terraservice.env_config_key_alias
+  env_key_arn              = module.terraservice.env_key_arn
+  env_config_key_arns      = module.terraservice.env_config_key_arns
+  iam_path                 = module.terraservice.default_iam_path
+  permissions_boundary_arn = module.terraservice.default_permissions_boundary_arn
+  vpc                      = module.terraservice.vpc
+  azs                      = module.terraservice.default_azs
+  data_subnets             = module.terraservice.subnets_map["data"]
+  external_sgs             = [module.terraservice.tools_sg, module.terraservice.vpn_sg, module.terraservice.management_sg]
+
+  rds_aurora_family                       = "aurora-postgresql16"
+  rds_cluster_id                          = nonsensitive(local.ssm_config["/bfd/database/rds_cluster_identifier"])
+  rds_cluster_sg                          = nonsensitive(local.ssm_config["/bfd/database/rds_security_group"])
+  rds_backup_retention_period             = nonsensitive(local.ssm_config["/bfd/database/rds_backup_retention_period"])
+  rds_iam_database_authentication_enabled = nonsensitive(local.ssm_config["/bfd/database/rds_iam_database_authentication_enabled"])
+  rds_instance_class                      = nonsensitive(local.ssm_config["/bfd/database/rds_instance_class"])
+  rds_min_reader_nodes                    = nonsensitive(local.ssm_config["/bfd/database/scaling/min_nodes"])
+  rds_max_reader_nodes                    = nonsensitive(local.ssm_config["/bfd/database/scaling/max_nodes"])
+  rds_scaling_cpu_target                  = nonsensitive(local.ssm_config["/bfd/database/scaling/cpu_target"])
+  rds_scale_in_cooldown                   = nonsensitive(local.ssm_config["/bfd/database/scaling/cooldown/scale_in"])
+  rds_scale_out_cooldown                  = nonsensitive(local.ssm_config["/bfd/database/scaling/cooldown/scale_out"])
+  rds_master_password                     = lookup(local.ssm_config, "/bfd/database/rds_master_password", null)
+  rds_master_username                     = nonsensitive(lookup(local.ssm_config, "/bfd/database/rds_master_username", sensitive(null)))
+  rds_snapshot_identifier                 = lookup(local.ssm_config, "/bfd/database/rds_snapshot_identifier", null)
+
+  db_cluster_parameter_group_file = fileexists("${path.module}/db-cluster-parameters/${local.env}.yaml") ? "${path.module}/db-cluster-parameters/${local.env}.yaml" : "${path.module}/db-cluster-parameters/${local.rds_aurora_family}.yaml"
+  db_node_parameter_group_file    = fileexists("${path.module}/db-node-parameters/${local.env}.yaml") ? "${path.module}/db-node-parameters/${local.env}.yaml" : "${path.module}/db-node-parameters/${local.rds_aurora_family}.yaml"
+  db_cluster_parameters           = toset(yamldecode(file(local.db_cluster_parameter_group_file)))
+  db_parameters                   = toset(yamldecode(file(local.db_node_parameter_group_file)))
+}
+
+resource "aws_db_subnet_group" "this" {
+  description = "Subnet group for aurora cluster"
+  name        = local.rds_cluster_id
+  subnet_ids  = [for x in local.data_subnets : x.id]
+}
+
+resource "aws_security_group" "this" {
+  description            = "Security group for aurora cluster"
+  name                   = local.rds_cluster_sg
+  revoke_rules_on_delete = true
+  vpc_id                 = local.vpc.id
+
+  tags = { Name = local.rds_cluster_sg }
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_rds_cluster_parameter_group" "this" {
+  description = "Sets cluster parameters for ${local.rds_aurora_family}"
+  name_prefix = local.rds_cluster_id
+  family      = local.rds_aurora_family
+
+  dynamic "parameter" {
+    for_each = local.db_cluster_parameters
+
+    content {
+      apply_method = parameter.value["apply_method"]
+      name         = parameter.value["name"]
+      value        = parameter.value["value"]
+    }
+  }
+}
+
+resource "aws_db_parameter_group" "this" {
+  description = "Sets node parameters for ${local.rds_aurora_family}"
+  family      = local.rds_aurora_family
+  name_prefix = "bfd-${local.env}-aurora-node"
+
+  dynamic "parameter" {
+    for_each = local.db_parameters
+
+    content {
+      apply_method = parameter.value["apply_method"]
+      name         = parameter.value["name"]
+      value        = parameter.value["value"]
+    }
+  }
+}
+
+resource "aws_rds_cluster" "this" {
+  allow_major_version_upgrade = false
+  engine                      = "aurora-postgresql"
+  engine_mode                 = "provisioned"
+  engine_version              = "16.4"
+  apply_immediately           = false
+
+  backtrack_window                    = 0
+  backup_retention_period             = local.rds_backup_retention_period
+  cluster_identifier                  = local.rds_cluster_id
+  copy_tags_to_snapshot               = true
+  db_cluster_parameter_group_name     = aws_rds_cluster_parameter_group.this.name
+  db_instance_parameter_group_name    = aws_db_parameter_group.this.name
+  db_subnet_group_name                = aws_db_subnet_group.this.name
+  iam_database_authentication_enabled = local.rds_iam_database_authentication_enabled
+  kms_key_id                          = local.env_key_arn
+  port                                = 5432
+  preferred_backup_window             = "05:00-06:00"
+  preferred_maintenance_window        = "fri:07:00-fri:08:00"
+  skip_final_snapshot                 = true
+  storage_encrypted                   = true
+  storage_type                        = "aurora-iopt1"
+
+  # if deletion_protection_override is null, use the default value for the environment, otherwise use the override
+  deletion_protection = !local.is_ephemeral_env
+
+  tags = { "cpm backup" = local.is_ephemeral_env ? "" : "Weekly", "Layer" = "data" }
+
+  # master username and password are null when a snapshot identifier is specified (clone and ephemeral support)
+  master_password     = local.rds_master_password
+  master_username     = local.rds_master_username
+  snapshot_identifier = local.rds_snapshot_identifier
+
+  availability_zones = [for _, v in local.azs : v.name]
+
+  enabled_cloudwatch_logs_exports = [
+    "postgresql",
+  ]
+
+  vpc_security_group_ids = concat(
+    [aws_security_group.this.id],
+    local.external_sgs[*].id
+  )
+
+  monitoring_interval             = 15
+  monitoring_role_arn             = aws_iam_role.db_monitoring.arn
+  performance_insights_enabled    = true
+  performance_insights_kms_key_id = local.env_key_arn
+
+  # Autoscaled reader nodes are not managed by Terraform and Terraform is unable to destroy a
+  # cluster with nodes still within it. To support simply running "terraform destroy" in
+  # environments with autoscaling enabled, a helper script is used that will automatically mark all
+  # autoscaled nodes for deletion and wait for them to be deleted before exiting. This runs only on
+  # destroy
+  provisioner "local-exec" {
+    when        = destroy
+    command     = "${path.module}/scripts/destroy-autoscaled-nodes.sh"
+    interpreter = ["/bin/bash", "-c"]
+    environment = {
+      DB_CLUSTER_ID = self.cluster_identifier
+      # This may seem strange, but provisioners can only refer to properties of the resource which
+      # local.env is not. Fortunately, the "stack" tag _is_ the name of the current environment, so
+      # we can use that to pass the environment to the script
+      BFD_ENVIRONMENT = self.tags_all["stack"]
+    }
+  }
+}
+
+resource "aws_rds_cluster_instance" "writer" {
+  auto_minor_version_upgrade   = true
+  ca_cert_identifier           = "rds-ca-rsa4096-g1"
+  cluster_identifier           = aws_rds_cluster.this.id
+  copy_tags_to_snapshot        = true
+  db_subnet_group_name         = aws_rds_cluster.this.db_subnet_group_name
+  db_parameter_group_name      = aws_rds_cluster.this.db_instance_parameter_group_name
+  engine                       = aws_rds_cluster.this.engine
+  engine_version               = aws_rds_cluster.this.engine_version
+  identifier                   = "${aws_rds_cluster.this.id}-writer-node"
+  instance_class               = local.rds_instance_class
+  preferred_maintenance_window = aws_rds_cluster.this.preferred_maintenance_window
+  publicly_accessible          = false
+  tags                         = { Layer = "data" }
+}
