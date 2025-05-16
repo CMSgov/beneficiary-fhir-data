724d086487c0c212f9db4cd3ac1c13be22bf8900
Mitch Alessio <mitch.alessio@forpeople.us>
2025-05-16 11:29:30 -0400

    Introduce app autoscaling resources

--- ops/services/03-database/variables.tf
+++ ops/services/03-database/variables.tf
@@ -3,3 +3,12 @@ variable "ephemeral_rds_snapshot_id_override" {
   description = "Specify DB Cluster Snapshot ID from `seed_env`. Defaults to null which indicates latest snapshot from the seed cluster on initial definition, falls back to previously specified snapshot on subsequent execution."
   type        = string
 }
+
+variable "disable_rds_scheduling_override" {
+  default     = false
+  description = <<EOF
+  If true, RDS off hours scheduled scale-in actions will be disabled for this environment. Defaults
+  to false.
+  EOF
+  type        = bool
+}
