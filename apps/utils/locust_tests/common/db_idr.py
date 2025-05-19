83d9e76e0a5e9ba6a03a5949c879f3d2d71bf194
Austin Schey <austin.schey@forpeople.us>
2025-05-19 16:02:43 -0700

    add tests for v3

---
+++ apps/utils/locust_tests/common/db_idr.py
@@ -0,0 +1,56 @@
+from typing import List
+import os
+
+import psycopg
+
+
+def _execute(uri: str, query: str) -> List:
+    """
+    Execute a PSQL select statement and return its results
+    """
+    if uri == "":
+        uri = f"user={os.environ["PGUSER"]} password={os.environ["PGPASSWORD"]} host={os.environ["PGHOST"]} dbname={os.environ["PGDATABASE"]}"
+    conn = None
+    results = []
+    try:
+        with psycopg.connect(uri) as conn:
+            with conn.cursor() as cursor:
+                # There's additional type validation here to prevent SQL injections,
+                # but this unfortunately doesn't support templated strings like we're using here
+                cursor.execute(query)  # type: ignore
+                results = cursor.fetchall()
+    except Exception as ex:
+        print("Error creating database connection", ex)
+    finally:
+        if conn:
+            conn.close()
+
+    return results
+
+
+def get_regression_bene_sks(uri: str, table_sample_pct=None) -> List[str]:
+    """Retrieves a list of beneficiary IDs within the range of 20,000 contiguous synthetic
+    beneficiaries that exist in each environment. Returned list is sorted in ascending order
+
+    Args:
+        uri (str): Database URI
+
+    Returns:
+        List[str]: A list of synthetic beneficiary IDs used for the regression suites
+    """
+    bene_query = "SELECT bene_sk FROM idr.beneficiary limit 10"
+    return [str(r[0]) for r in _execute(uri, bene_query)]
+
+
+def get_regression_bene_mbis(uri: str, table_sample_pct=None) -> List[str]:
+    """Retrieves a list of beneficiary IDs within the range of 20,000 contiguous synthetic
+    beneficiaries that exist in each environment. Returned list is sorted in ascending order
+
+    Args:
+        uri (str): Database URI
+
+    Returns:
+        List[str]: A list of synthetic beneficiary IDs used for the regression suites
+    """
+    bene_query = "SELECT bene_mbi_id FROM idr.beneficiary limit 10"
+    return [str(r[0]) for r in _execute(uri, bene_query)]
