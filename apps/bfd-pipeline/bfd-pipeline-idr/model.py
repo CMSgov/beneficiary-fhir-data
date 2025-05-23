382a12f001b9945c10fc687c508782282ca59be6
Austin Schey <austin.schey@forpeople.us>
2025-05-23 16:54:07 -0700

    add claim tables to pipeline

--- apps/bfd-pipeline/bfd-pipeline-idr/model.py
+++ apps/bfd-pipeline/bfd-pipeline-idr/model.py
@@ -1,8 +1,6 @@
 from datetime import date, datetime
-from typing import Annotated, TypeVar
-from pydantic import BaseModel, BeforeValidator
-
-T = TypeVar("T", bound=BaseModel)
+from typing import Annotated, ClassVar, Iterable, Literal, TypeVar
+from pydantic import BaseModel, BeforeValidator, Field
 
 
 def transform_null_date(value: date | None) -> date:
@@ -24,7 +22,29 @@ def transform_default_string(value: str | None) -> str:
     return value
 
 
-class IdrBeneficiary(BaseModel):
+class IdrBaseModel(BaseModel):
+    @classmethod
+    def _format_column_alias(cls, key: str) -> str:
+        metadata = cls.model_fields[key].metadata
+
+        if (
+            len(metadata) > 0
+            and isinstance(metadata[0], Iterable)
+            and "alias" in metadata[0]
+        ):
+            return f"{metadata[0]['alias']}.{key}"
+        else:
+            return key
+
+    @classmethod
+    def column_aliases(cls) -> list[str]:
+        return [cls._format_column_alias(key) for key in cls.model_fields.keys()]
+
+
+T = TypeVar("T", bound=IdrBaseModel)
+
+
+class IdrBeneficiary(IdrBaseModel):
     bene_sk: int
     bene_xref_efctv_sk: int
     bene_mbi_id: str
@@ -51,7 +71,7 @@ class IdrBeneficiary(BaseModel):
     idr_updt_ts: Annotated[datetime, BeforeValidator(transform_null_date)]
 
 
-class IdrBeneficiaryHistory(BaseModel):
+class IdrBeneficiaryHistory(IdrBaseModel):
     bene_sk: int
     bene_xref_efctv_sk: int
     bene_mbi_id: Annotated[str, BeforeValidator(transform_null_string)]
@@ -60,7 +80,7 @@ class IdrBeneficiaryHistory(BaseModel):
     idr_updt_ts: Annotated[datetime, BeforeValidator(transform_null_date)]
 
 
-class IdrBeneficiaryMbi(BaseModel):
+class IdrBeneficiaryMbi(IdrBaseModel):
     bene_mbi_id: str
     bene_mbi_efctv_dt: date
     bene_mbi_obslt_dt: Annotated[date, BeforeValidator(transform_null_date)]
@@ -69,7 +89,7 @@ class IdrBeneficiaryMbi(BaseModel):
     idr_updt_ts: Annotated[datetime, BeforeValidator(transform_null_date)]
 
 
-class IdrBeneficiaryThirdParty(BaseModel):
+class IdrBeneficiaryThirdParty(IdrBaseModel):
     bene_sk: int
     bene_buyin_cd: str
     bene_tp_type_cd: str
@@ -80,7 +100,7 @@ class IdrBeneficiaryThirdParty(BaseModel):
     idr_updt_ts: Annotated[datetime, BeforeValidator(transform_null_date)]
 
 
-class IdrBeneficiaryStatus(BaseModel):
+class IdrBeneficiaryStatus(IdrBaseModel):
     bene_sk: int
     bene_mdcr_stus_cd: str
     mdcr_stus_bgn_dt: date
@@ -90,7 +110,7 @@ class IdrBeneficiaryStatus(BaseModel):
     idr_updt_ts: Annotated[datetime, BeforeValidator(transform_null_date)]
 
 
-class IdrBeneficiaryEntitlement(BaseModel):
+class IdrBeneficiaryEntitlement(IdrBaseModel):
     bene_sk: int
     bene_rng_bgn_dt: date
     bene_rng_end_dt: date
@@ -102,7 +122,7 @@ class IdrBeneficiaryEntitlement(BaseModel):
     idr_updt_ts: Annotated[datetime, BeforeValidator(transform_null_date)]
 
 
-class IdrBeneficiaryEntitlementReason(BaseModel):
+class IdrBeneficiaryEntitlementReason(IdrBaseModel):
     bene_sk: int
     bene_rng_bgn_dt: date
     bene_rng_end_dt: date
@@ -112,7 +132,7 @@ class IdrBeneficiaryEntitlementReason(BaseModel):
     idr_updt_ts: Annotated[datetime, BeforeValidator(transform_null_date)]
 
 
-class IdrElectionPeriodUsage(BaseModel):
+class IdrElectionPeriodUsage(IdrBaseModel):
     bene_sk: int
     cntrct_pbp_sk: int
     bene_cntrct_num: str
@@ -124,18 +144,21 @@ class IdrElectionPeriodUsage(BaseModel):
     idr_trans_obslt_ts: datetime
 
 
-class IdrContractPbpNumber(BaseModel):
+class IdrContractPbpNumber(IdrBaseModel):
     cntrct_pbp_sk: int
     cntrct_drug_plan_ind_cd: str
     cntrct_pbp_type_cd: str
 
 
-class IdrClaim(BaseModel):
+class IdrClaim(IdrBaseModel):
+    claim_alias: ClassVar[str] = "clm"
+    dcmtn_alias: ClassVar[str] = "dcmtn"
+
     clm_uniq_id: int
-    geo_bene_sk: int
-    clm_dt_sgntr_sk: int
-    clm_type_cd: int
-    clm_num_sk: int
+    geo_bene_sk: Annotated[int, {"alias": claim_alias}]
+    clm_dt_sgntr_sk: Annotated[int, {"alias": claim_alias}]
+    clm_type_cd: Annotated[int, {"alias": claim_alias}]
+    clm_num_sk: Annotated[int, {"alias": claim_alias}]
     bene_sk: int
     clm_cntl_num: str
     clm_orig_cntl_num: str
@@ -143,7 +166,6 @@ class IdrClaim(BaseModel):
     clm_thru_dt: date
     clm_efctv_dt: date
     clm_finl_actn_ind: str
-    clm_type_cd: int
     clm_src_id: str
     clm_query_cd: str
     clm_mdcr_coinsrnc_amt: int
@@ -165,10 +187,10 @@ class IdrClaim(BaseModel):
     clm_blg_prvdr_oscar_num: str
     clm_mdcr_coinsrnc_amt: int
     clm_idr_ld_dt: date
-    clm_nrln_ric_cd: str
+    clm_nrln_ric_cd: Annotated[str, {"alias": dcmtn_alias}]
 
 
-class IdrClaimDateSignature(BaseModel):
+class IdrClaimDateSignature(IdrBaseModel):
     clm_dt_sgntr_sk: int
     clm_cms_proc_dt: date
     clm_actv_care_from_dt: date
@@ -180,7 +202,7 @@ class IdrClaimDateSignature(BaseModel):
     clm_mdcr_exhstd_dt: date
 
 
-class LoadProgress(BaseModel):
+class LoadProgress(IdrBaseModel):
     table_name: str
     last_ts: datetime
     batch_completion_ts: datetime
