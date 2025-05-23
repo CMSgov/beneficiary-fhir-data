84d56170d07afa0c19006c24121da71229825f53
Mitch Alessio <mitch.alessio@forpeople.us>
2025-05-23 19:36:18 -0400

    Use bfd-platform-service in bootstrap service

--- ops/platform/000-bootstrap/main.tf
+++ ops/platform/000-bootstrap/main.tf
@@ -1,22 +1,18 @@
+module "terraservice" {
+  source = "../../terraform-modules/bfd/bfd-platform-service"
+
+  greenfield           = var.greenfield
+  service              = local.service
+  relative_module_root = "ops/services/000-bootstrap"
+  lookup_kms_keys      = false
+}
+
 locals {
   service = "bootstrap"
 
-  default_tags = {
-    application    = "bfd"
-    business       = "oeda"
-    service        = "bootstrap"
-    Terraform      = true
-    tf_module_root = "ops/platform/000-bootstrap"
-  }
+  region       = module.terraservice.region
+  account_id   = module.terraservice.account_id
+  default_tags = module.terraservice.default_tags
 
-  account_id     = data.aws_caller_identity.current.account_id
-  region         = data.aws_region.current.name
-  account_alias  = data.aws_iam_account_alias.current.account_alias
-  state_variants = strcontains(local.account_alias, "non-prod") ? ["test", "platform-non-prod"] : ["prod-sbx", "prod", "platform-prod"]
+  state_variants = local.account_type == "non-prod" ? ["test", "platform-non-prod"] : ["prod-sbx", "prod", "platform-prod"]
 }
-
-data "aws_caller_identity" "current" {}
-
-data "aws_region" "current" {}
-
-data "aws_iam_account_alias" "current" {}
