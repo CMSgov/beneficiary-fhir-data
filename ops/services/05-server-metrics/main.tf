21f603c770d6b253c8208a76531510d7be2c956e
Mitch Alessio <mitch.alessio@forpeople.us>
2025-05-30 12:48:21 -0400

    Refactor server-metrics to support legacy and greenfield

--- ops/services/05-server-metrics/main.tf
+++ ops/services/05-server-metrics/main.tf
@@ -1,7 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.9"
+    }
+  }
+}
+
 module "terraservice" {
   source = "../../terraform-modules/bfd/bfd-terraservice"
 
-  environment_name     = terraform.workspace
+  greenfield           = var.greenfield
   service              = local.service
   relative_module_root = "ops/services/05-server-metrics"
 }
@@ -9,14 +18,12 @@ module "terraservice" {
 locals {
   service = "server-metrics"
 
-  region             = module.terraservice.region
-  account_id         = module.terraservice.account_id
-  default_tags       = module.terraservice.default_tags
-  env                = module.terraservice.env
-  seed_env           = module.terraservice.seed_env
-  is_ephemeral_env   = module.terraservice.is_ephemeral_env
-  latest_bfd_release = module.terraservice.latest_bfd_release
-  ssm_config         = module.terraservice.ssm_config
+  region           = module.terraservice.region
+  account_id       = module.terraservice.account_id
+  default_tags     = module.terraservice.default_tags
+  env              = module.terraservice.env
+  is_ephemeral_env = module.terraservice.is_ephemeral_env
+  ssm_config       = module.terraservice.ssm_config
 
   target_service = "server"
   # TODO: Remove ecs suffix part when server is fully migrated to Fargate
