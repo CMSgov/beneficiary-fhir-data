51f950358f9edbf89228730d70503a982f5d2fef
Mitch Alessio <mitch.alessio@forpeople.us>
2025-05-30 09:26:43 -0400

    Refactor ccw-pipeline-metrics to work in legacy and greenfield

--- ops/services/05-ccw-pipeline-metrics/main.tf
+++ ops/services/05-ccw-pipeline-metrics/main.tf
@@ -1,7 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.9"
+    }
+  }
+}
+
 module "terraservice" {
   source = "../../terraform-modules/bfd/bfd-terraservice"
 
-  environment_name     = terraform.workspace
+  greenfield           = var.greenfield
   service              = local.service
   relative_module_root = "ops/services/05-ccw-pipeline-metrics"
 }
@@ -13,14 +22,7 @@ locals {
   account_id               = module.terraservice.account_id
   default_tags             = module.terraservice.default_tags
   env                      = module.terraservice.env
-  seed_env                 = module.terraservice.seed_env
-  is_ephemeral_env         = module.terraservice.is_ephemeral_env
-  latest_bfd_release       = module.terraservice.latest_bfd_release
   ssm_config               = module.terraservice.ssm_config
-  env_key_alias            = module.terraservice.env_key_alias
-  env_config_key_alias     = module.terraservice.env_config_key_alias
-  env_key_arn              = module.terraservice.env_key_arn
-  env_config_key_arns      = module.terraservice.env_config_key_arns
   iam_path                 = module.terraservice.default_iam_path
   permissions_boundary_arn = module.terraservice.default_permissions_boundary_arn
   vpc                      = module.terraservice.vpc
