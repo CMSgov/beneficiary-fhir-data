69a15ab8d86379981e48594e1367b85f0185b56f
Mitch Alessio <mitch.alessio@forpeople.us>
2025-06-05 11:27:35 -0400

    Scaffold insights Terraservice for future BFD Insights work

---
+++ ops/services/02-insights/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.9"
+    }
+  }
+}
+
+module "terraservice" {
+  source = "../../terraform-modules/bfd/bfd-terraservice"
+
+  greenfield           = var.greenfield
+  service              = local.service
+  relative_module_root = "ops/services/02-insights"
+}
+
+locals {
+  service = "insights"
+
+  region                   = module.terraservice.region
+  account_id               = module.terraservice.account_id
+  default_tags             = module.terraservice.default_tags
+  env                      = module.terraservice.env
+  is_ephemeral_env         = module.terraservice.is_ephemeral_env
+  latest_bfd_release       = module.terraservice.latest_bfd_release
+  ssm_config               = module.terraservice.ssm_config
+  env_key_alias            = module.terraservice.env_key_alias
+  env_key_arn              = module.terraservice.env_key_arn
+  iam_path                 = module.terraservice.default_iam_path
+  permissions_boundary_arn = module.terraservice.default_permissions_boundary_arn
+  vpc                      = module.terraservice.vpc
+  azs                      = keys(module.terraservice.default_azs)
+}
+
+# Temporary placeholder for Greenfield Insights Bucket structure to enable applying server-insights.
+# Legacy has a single Bucket, but Greenfield will have a bucket per-environment, per-consumer
+# TODO: Make this Terraservice more useful
+module "bucket_insights_bfd" {
+  count  = !var.greenfield ? 0 : 1
+  source = "../../terraform-modules/general/secure-bucket"
+
+  bucket_kms_key_arn = local.env_key_arn
+  bucket_prefix      = "bfd-${local.env}-insights-bfd"
+  force_destroy      = local.is_ephemeral_env
+
+  ssm_param_name = "/bfd/${local.env}/${local.service}/nonsensitive/bucket"
+}
