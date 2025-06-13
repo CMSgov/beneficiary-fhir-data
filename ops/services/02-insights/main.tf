6a7a9538d18b87e0339f8d34ddc163a47e62dc63
Johan Robles <johanrobles@navapbc.com>
2025-06-12 20:37:26 -0400

    BFD-4124: Refactor BFD-Insights S3-Bucket and Athena Resources prior to migrating to greenfield

--- ops/services/02-insights/main.tf
+++ ops/services/02-insights/main.tf
@@ -17,6 +17,7 @@ module "terraservice" {
 
 locals {
   service = "insights"
+  partners = ["bcda", "bfd", "bb2", "ab2d"]
 
   region                   = module.terraservice.region
   account_id               = module.terraservice.account_id
@@ -46,3 +47,107 @@ module "bucket_insights_bfd" {
 
   ssm_param_name = "/bfd/${local.env}/${local.service}/nonsensitive/bucket"
 }
+
+resource "aws_s3_object" "env_prefixes" {
+  for_each = merge(
+    {
+      for partner in local.partners: "${partner}-streams" => {
+        key = "${partner}/streams/"
+      }
+    },
+    {
+      for partner in local.partners: "${parner}-results" => {
+        key = "${partner}/results/"
+      }
+    }
+  )
+
+  bucket = module.bucket_insights_bfd.bucket.name
+  key = each.value.key
+  content = ""
+  content_type = "application/octet-stream"
+}
+
+resource "aws_athena_workgroup" "this" {
+  for_each = toset([local.partners]) 
+  name = "${local.env}-${each.key}"
+
+  configuration {
+    enforce_workgroup_configuration = true
+    publish_cloudwatch_metrics_enabled = true
+
+    result_configuration {
+      output_location = "s3://${module.bucket_insights_bfd.bucket.name}/${each.key}/results/"
+  
+      encryption_configuration {
+        encryption_option = "SSE_KMS"
+        kms_key_arn = module.terraservice.aws_kms_key.env.arn
+      }
+    }
+  }
+
+  state = "ENABLED"
+}
+
+resource "aws_glue_catalog_database" "this" {
+  for_each = toset(local.partners)
+  name = "${local.env}-${each.key}"
+}
+
+resource "aws_glue_crawler" "this" {
+  for_each = toset(local.partners)
+  name = "bfd-${local.env}-${each.key}-crawler"
+  role = aws_iam_role.glue_role.arn
+  database_name = aws_glue_catalog_database.this["${local.env}-${each.key}"].name
+  #table_prefix 
+
+  s3_target {
+   path = "s3://${module.bucket_insights_bfd.bucket.name}/${each.key}/streams/" 
+  }
+
+  configuration = jsonencode({
+    Version = "1.0",
+    CrawlerOutput = {
+      Tables = {
+        AddOrUpdateBehavior = "MergeNewColumns"
+      },
+      Partitions = {
+        AddOrUpdateBehavior = "InheritFromTable"
+      }
+    }
+  })
+
+  schema_change_policy {
+    delete_behavior = "LOG"
+    update_behavior = "UPDATE_IN_DATABASE"
+  }
+
+  recrawl_policy {
+    recrawl_behavior = "CRAWL_EVERYTHING"
+  }
+
+  schedule = "cron(0/15 * * * ? *)"
+}
+
+resource "aws_iam_role" "this" {
+  name = "bfd-insights-glue-crawler-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "glue.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role = aws_iam_role.this
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+
+
