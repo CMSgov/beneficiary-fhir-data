a04d21d1af099d108f0cc32dc92b899c512b08b4
MahiFentaye <168698136+MahiFentaye@users.noreply.github.com>
2025-06-17 10:01:55 -0400

    BFD-4073 Cleaning up un used method

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/coverage/CoverageRepository.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/coverage/CoverageRepository.java
@@ -1,7 +1,5 @@
 package gov.cms.bfd.server.ng.coverage;
 
-import gov.cms.bfd.server.ng.beneficiary.BeneficiaryRepository;
-import gov.cms.bfd.server.ng.beneficiary.model.Beneficiary;
 import gov.cms.bfd.server.ng.input.CoverageCompositeId;
 import gov.cms.bfd.server.ng.input.DateTimeRange;
 import jakarta.persistence.EntityManager;
@@ -20,85 +18,6 @@ import org.springframework.stereotype.Repository;
 public class CoverageRepository {
 
   private final EntityManager entityManager;
-  private final BeneficiaryRepository beneficiaryRepository;
-
-  /**
-   * Finds all necessary details to construct a FHIR Coverage resource.
-   *
-   * @param coverageCompositeId Parsed ID containing beneSk and CoveragePart.
-   * @param lastUpdatedRange Optional date range to filter records by their last update.
-   * @return An Optional containing {@link CoverageDetails} if the beneficiary is found and is
-   *     current-effective, otherwise empty.
-   */
-  public Optional<CoverageDetails> findCoverageDetailsSingleQueryAttempt(
-      CoverageCompositeId coverageCompositeId, DateTimeRange lastUpdatedRange) {
-
-    var beneSk = coverageCompositeId.beneSk();
-    String partTypeCode = coverageCompositeId.coveragePart().getStandardCode();
-
-    Optional<Beneficiary> beneficiaryOpt =
-        beneficiaryRepository
-            .findById(beneSk, lastUpdatedRange)
-            .filter(b -> b.getBeneSk() == b.getXrefSk());
-
-    if (beneficiaryOpt.isEmpty()) {
-      return Optional.empty();
-    }
-    Beneficiary beneficiary = beneficiaryOpt.get();
-    LocalDate currentDateForPeriods = OffsetDateTime.now(ZoneOffset.ofHours(-12)).toLocalDate();
-
-    String jpqlRelatedDetails =
-        """
-            SELECT NEW gov.cms.bfd.server.ng.coverage.CoverageDetails( tp, bs, be, ber
-            )
-            FROM BeneficiaryEntitlement be
-            LEFT JOIN BeneficiaryThirdParty tp ON be.beneSk = tp.beneSk
-                AND tp.benefitRangeBeginDate = be.benefitRangeBeginDate
-                ANd tp.benefitRangeEndDate = be.benefitRangeEndDate
-                AND tp.thirdPartyTypeCode = be.medicareEntitlementTypeCode
-                AND tp.idrTransObsoleteTimestamp >= gov.cms.bfd.server.ng.IdrConstants.DEFAULT_ZONED_DATE
-            LEFT JOIN BeneficiaryStatus bs ON bs.beneSk = tp.beneSk
-                AND bs.medicareStatusBeginDate = tp.benefitRangeBeginDate
-                AND bs.medicareStatusEndDate= tp.benefitRangeEndDate
-                AND bs.idrTransObsoleteTimestamp >= gov.cms.bfd.server.ng.IdrConstants.DEFAULT_ZONED_DATE
-            LEFT JOIN BeneficiaryEntitlementReason ber ON ber.beneSk = tp.beneSk
-
-                AND ber.benefitRangeBeginDate = be.benefitRangeBeginDate
-                AND ber.benefitRangeEndDate = be.benefitRangeEndDate
-                AND ber.idrTransObsoleteTimestamp >= gov.cms.bfd.server.ng.IdrConstants.DEFAULT_ZONED_DATE
-            WHERE bs.beneSk = :beneSkParam
-                AND be.idrTransObsoleteTimestamp >= gov.cms.bfd.server.ng.IdrConstants.DEFAULT_ZONED_DATE
-                AND be.benefitRangeBeginDate <= :referenceDate
-                AND be.benefitRangeEndDate >= :referenceDate
-                AND be.medicareEntitlementTypeCode = :partTypeCode
-            """;
-
-    TypedQuery<CoverageDetails> query =
-        entityManager
-            .createQuery(jpqlRelatedDetails, CoverageDetails.class)
-            .setParameter("beneSkParam", beneSk)
-            .setParameter("partTypeCode", partTypeCode)
-            .setParameter("referenceDate", currentDateForPeriods)
-            .setMaxResults(1);
-
-    List<CoverageDetails> relatedResults = query.getResultList();
-
-    if (relatedResults.isEmpty()) {
-      return Optional.of(
-          CoverageDetails.builder()
-              .beneficiary(beneficiary)
-              .thirdPartyDetails(Optional.empty())
-              .currentStatus(Optional.empty())
-              .partEntitlement(Optional.empty())
-              .currentEntitlementReason(Optional.empty())
-              .build());
-    }
-
-    CoverageDetails details = relatedResults.get(0);
-    //    details.setBeneficiary(beneficiary);
-
-    return Optional.of(details);
-  }
 
   /**
    * Finds all necessary details to construct a FHIR Coverage resource in a single query. This
