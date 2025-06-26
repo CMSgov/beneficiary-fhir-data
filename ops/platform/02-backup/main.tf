b365a9561d5f40be9b72cd7aa3970bc9db3b770b
TimRotenSB <Tim.Roten@semanticbits.com>
2025-06-26 12:02:37 -0400

    Adding backup plan and necessary lookups

---
+++ ops/platform/02-backup/main.tf
@@ -0,0 +1,55 @@
+data "aws_backup_vault" "main" {
+  name = "CMS_OIT_Backups_Vault"
+}
+
+data "aws_backup_vault" "dr" {
+  provider = aws.secondary
+  name     = "CMS_OIT_Backups_Vault"
+}
+
+data "aws_iam_role" "backup" {
+  name = "cms-oit-aws-backup-service-role"
+}
+
+module "terraservice" {
+  source = "../../terraform-modules/bfd/bfd-platform-service"
+
+  greenfield           = var.greenfield
+  service              = local.service
+  relative_module_root = "ops/platform/02-backup"
+}
+
+resource "aws_backup_plan" "main" {
+  name = "bfd-weekly"
+
+  rule {
+    rule_name         = "weekly90"
+    target_vault_name = data.aws_backup_vault.main.name
+    schedule          = "cron(0 3 ? * SAT *)"
+
+    lifecycle {
+      delete_after = 35
+    }
+
+    copy_action {
+      destination_vault_arn = data.aws_backup_vault.dr.arn
+      lifecycle {
+        delete_after = 7
+      }
+    }
+  }
+
+  tags = { layer = "data" }
+}
+
+resource "aws_backup_selection" "main" {
+  iam_role_arn = data.aws_iam_role.backup.arn
+  name         = "bfd-weekly-backup-selection"
+  plan_id      = aws_backup_plan.main.id
+
+  selection_tag {
+    type  = "STRINGEQUALS"
+    key   = "bfd_backup"
+    value = "weekly90"
+  }
+}
