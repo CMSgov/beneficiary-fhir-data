8cf5ba5d4873a3fbfbe9540807515e102c0c521d
aschey-forpeople <162160982+aschey-forpeople@users.noreply.github.com>
2025-07-10 07:46:09 -0700

    BFD-4174: Fix some optional fields for V3 EOB (#2739)

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/ClaimRecordTypeCode.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/ClaimRecordTypeCode.java
@@ -3,20 +3,24 @@ package gov.cms.bfd.server.ng.claim.model;
 import gov.cms.bfd.server.ng.SystemUrls;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import java.util.Optional;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Extension;
 
 @Embeddable
 class ClaimRecordTypeCode {
   @Column(name = "clm_nrln_ric_cd")
-  private String claimRecordType;
+  private Optional<String> claimRecordType;
 
-  Extension toFhir() {
-    return new Extension()
-        .setUrl(SystemUrls.BLUE_BUTTON_STRUCTURE_DEFINITION_CLAIM_RECORD_TYPE)
-        .setValue(
-            new Coding()
-                .setSystem(SystemUrls.BLUE_BUTTON_CODE_SYSTEM_CLAIM_RECORD_TYPE)
-                .setCode(claimRecordType));
+  Optional<Extension> toFhir() {
+
+    return claimRecordType.map(
+        c ->
+            new Extension()
+                .setUrl(SystemUrls.BLUE_BUTTON_STRUCTURE_DEFINITION_CLAIM_RECORD_TYPE)
+                .setValue(
+                    new Coding()
+                        .setSystem(SystemUrls.BLUE_BUTTON_CODE_SYSTEM_CLAIM_RECORD_TYPE)
+                        .setCode(c)));
   }
 }
