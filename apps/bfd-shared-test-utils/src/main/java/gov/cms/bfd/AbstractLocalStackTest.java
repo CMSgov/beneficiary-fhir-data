e31057cb94c2198b6146c55fe48a43a16be23d93
Michael J Burling <michael.burling@cms.hhs.gov>
2025-08-26 13:44:57 -0500

    Remove unnecessary default property

--- apps/bfd-shared-test-utils/src/main/java/gov/cms/bfd/AbstractLocalStackTest.java
+++ apps/bfd-shared-test-utils/src/main/java/gov/cms/bfd/AbstractLocalStackTest.java
@@ -12,17 +12,13 @@ public abstract class AbstractLocalStackTest {
   /** The system property defined in pom.xml containing the image to use for test AWS stack. */
   public static final String TEST_CONTAINER_AWS_IMAGE_PROPERTY = "its.testcontainer.aws.image";
 
-  /** The default test container image to use when nothing is provided. */
-  public static final String TEST_CONTAINER_AWS_IMAGE_DEFAULT = "localstack/localstack:2.2.0";
-
   /** Global container used by multiple tests. Will be destroyed automatically by test container. */
   protected static final LocalStackContainer localstack;
 
   // Creates the instance when first test class loads.   Starts all services that we use in BFD.
   // Add more here if we need any others.
   static {
-    var localStackImageName =
-        System.getProperty(TEST_CONTAINER_AWS_IMAGE_PROPERTY, TEST_CONTAINER_AWS_IMAGE_DEFAULT);
+    var localStackImageName = System.getProperty(TEST_CONTAINER_AWS_IMAGE_PROPERTY);
     localstack =
         new LocalStackContainer(
                 DockerImageName.parse(localStackImageName)
