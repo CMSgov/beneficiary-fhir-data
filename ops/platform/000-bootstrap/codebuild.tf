98d99923e305ac3855aed61726005c940dab2e03
Michael J Burling <michael.burling@cms.hhs.gov>
2025-09-02 11:44:14 -0500

    BFD-3764: Enable SonarQube Analysis in GHA (#2786)

--- ops/platform/000-bootstrap/codebuild.tf
+++ ops/platform/000-bootstrap/codebuild.tf
@@ -8,6 +8,7 @@ locals {
       image        = "${aws_ecr_repository.codebuild_runner.repository_url}:${local.bfd_version}"
       privileged   = false
       timeout      = 15
+      vpc_config   = []
     }
     # Runner for long-running (>= 15 minutes) Terraservice applies or other jobs
     small = {
@@ -17,6 +18,21 @@ locals {
       image        = "${aws_ecr_repository.codebuild_runner.repository_url}:${local.bfd_version}"
       privileged   = false
       timeout      = 60
+      vpc_config   = []
+    }
+    # Runner for longer running, larger workloads, with need for CMSNet connectivity, like java-ci
+    large = {
+      name         = "bfd-${local.account_type}-platform-large"
+      compute_type = "BUILD_GENERAL1_LARGE"
+      type         = "ARM_CONTAINER"
+      image        = "aws/codebuild/amazonlinux-aarch64-standard:3.0"
+      privileged   = true # Enables this runner to interact with container daemon
+      timeout      = 60
+      vpc_config = [{
+        vpc_id             = data.aws_vpc.this.id
+        subnets            = flatten(data.aws_subnets.private[*].ids)
+        security_group_ids = [aws_security_group.this.id]
+      }]
     }
     # Runner for building Docker images and nothing else
     docker = {
@@ -26,8 +42,44 @@ locals {
       image        = "aws/codebuild/amazonlinux-aarch64-standard:3.0"
       privileged   = true # Enables this Runner to build Docker images
       timeout      = 60
+      vpc_config   = []
     }
   }
+  env_vpc = local.account_type == "non-prod" ? "bfd-east-test" : "bfd-east-prod"
+}
+
+data "aws_vpc" "this" {
+  filter {
+    name   = "tag:Name"
+    values = [local.env_vpc]
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.this.id]
+  }
+  filter {
+    name   = "tag:use"
+    values = ["private"]
+  }
+}
+
+resource "aws_security_group" "this" {
+  description            = "Egress-only security group for CodeBuild Runners"
+  name                   = "bfd-${data.aws_vpc.this.tags.stack}-codebuild-egress"
+  revoke_rules_on_delete = true
+  vpc_id                 = data.aws_vpc.this.id
+  tags = {
+    Name = "bfd-${data.aws_vpc.this.tags.stack}-codebuild-egress"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
 
 # ECR resources are defined in this Terraservice opposed to "ecr" as the CodeBuild Runner image
@@ -139,6 +191,18 @@ resource "aws_codebuild_project" "runner" {
     location            = "https://github.com/CMSgov/beneficiary-fhir-data"
     report_build_status = false
     type                = "GITHUB"
+    git_submodules_config {
+      fetch_submodules = false
+    }
+  }
+
+  dynamic "vpc_config" {
+    for_each = each.value.vpc_config
+    content {
+      security_group_ids = vpc_config.value["security_group_ids"]
+      subnets            = vpc_config.value["subnets"]
+      vpc_id             = vpc_config.value["vpc_id"]
+    }
   }
 }
 
