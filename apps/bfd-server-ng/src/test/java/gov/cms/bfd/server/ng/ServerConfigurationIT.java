53240863066da7a5b6903501b85a0161f1dd8743
MahiFentaye <168698136+MahiFentaye@users.noreply.github.com>
2025-09-04 11:57:18 -0400

    adding unit test

--- apps/bfd-server-ng/src/test/java/gov/cms/bfd/server/ng/ServerConfigurationIT.java
+++ apps/bfd-server-ng/src/test/java/gov/cms/bfd/server/ng/ServerConfigurationIT.java
@@ -4,8 +4,21 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 public class ServerConfigurationIT extends IntegrationTestBase {
+
+  @DynamicPropertySource
+  static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+    // These properties will apply to all tests in this class.
+    // For this test, we want to ensure Patient and Coverage are ON by default, and EOB is OFF by
+    // default.
+    // This setup mirrors what we expect in 'prod-like' environments initially.
+    registry.add("bfd.nonsensitive.eob_enabled", () -> "false");
+    registry.add("bfd.nonsensitive.patient_enabled", () -> "true");
+    registry.add("bfd.nonsensitive.coverage_enabled", () -> "true");
+  }
 
   @Test
   void disallowedMethodReturns405() {
@@ -20,5 +33,12 @@ public class ServerConfigurationIT extends IntegrationTestBase {
         .then()
         .statusCode(404)
         .body(containsString("No static resource"));
+  }
+
+  @Test
+  void eobEndpointIsDisabledWhenConfigured() {
+    given().when().get(getServerUrl() + "/ExplanationOfBenefit/178083966").then().statusCode(404);
+    given().when().get(getServerUrl() + "/Patient/178083966").then().statusCode(200);
+    given().when().get(getServerUrl() + "/Coverage/part-b-365359727").then().statusCode(200);
   }
 }
