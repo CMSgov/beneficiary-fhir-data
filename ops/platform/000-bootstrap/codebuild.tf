df753ecbecff4f389774216c36496fbb9cd95081
Mitch Alessio <mitch.alessio@forpeople.us>
2025-09-08 15:18:38 -0400

    Add VPC IAM Policy and attach to each Role

--- ops/platform/000-bootstrap/codebuild.tf
+++ ops/platform/000-bootstrap/codebuild.tf
@@ -66,6 +66,12 @@ data "aws_subnets" "private" {
   }
 }
 
+data "aws_subnet" "private" {
+  for_each = toset(data.aws_subnets.private.ids)
+
+  id = each.key
+}
+
 resource "aws_security_group" "this" {
   description            = "Egress-only security group for CodeBuild Runners"
   name                   = "bfd-${data.aws_vpc.this.tags.stack}-codebuild-egress"
