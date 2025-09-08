eaaff8d82bbaa7f01476578fe81b7f070443e28c
Brick Green <brickgreen@navapbc.com>
2025-09-08 12:26:59 -0400

    fix adjudication component

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/ClaimAnsiSignature.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/ClaimAnsiSignature.java
@@ -20,63 +20,71 @@ public class ClaimAnsiSignature {
   @Column(name = "clm_ansi_sgntr_sk", insertable = false, updatable = false)
   private long ansiSignatureSk;
 
+    @Column(name = "clm_1_rev_cntr_ansi_grp_cd")
+    private Optional<RevenueCenterAnsiGroupCode> revenueCenterAnsiGroupCode1;
+
+    @Column(name = "clm_2_rev_cntr_ansi_grp_cd")
+    private Optional<RevenueCenterAnsiGroupCode> revenueCenterAnsiGroupCode2;
+
+    @Column(name = "clm_3_rev_cntr_ansi_grp_cd")
+    private Optional<RevenueCenterAnsiGroupCode> revenueCenterAnsiGroupCode3;
+
+    @Column(name = "clm_4_rev_cntr_ansi_grp_cd")
+    private Optional<RevenueCenterAnsiGroupCode> revenueCenterAnsiGroupCode4;
+
   @Column(name = "clm_1_rev_cntr_ansi_rsn_cd")
-  private Optional<String> revenueCenterAnsiReasonCode1;
+  private Optional<RevenueCenterAnsiReasonCode> revenueCenterAnsiReasonCode1;
 
   @Column(name = "clm_2_rev_cntr_ansi_rsn_cd")
-  private Optional<String> revenueCenterAnsiReasonCode2;
+  private Optional<RevenueCenterAnsiReasonCode> revenueCenterAnsiReasonCode2;
 
   @Column(name = "clm_3_rev_cntr_ansi_rsn_cd")
-  private Optional<String> revenueCenterAnsiReasonCode3;
+  private Optional<RevenueCenterAnsiReasonCode> revenueCenterAnsiReasonCode3;
 
   @Column(name = "clm_4_rev_cntr_ansi_rsn_cd")
-  private Optional<String> revenueCenterAnsiReasonCode4;
+  private Optional<RevenueCenterAnsiReasonCode> revenueCenterAnsiReasonCode4;
 
-  @Column(name = "clm_1_rev_cntr_ansi_grp_cd")
-  private Optional<String> revenueCenterAnsiGroupCode1;
+    List<ExplanationOfBenefit.AdjudicationComponent> toFhir() {
+        return Stream.of(
+                        toAdjudicationComponent(revenueCenterAnsiGroupCode1, revenueCenterAnsiReasonCode1),
+                        toAdjudicationComponent(revenueCenterAnsiGroupCode2, revenueCenterAnsiReasonCode2),
+                        toAdjudicationComponent(revenueCenterAnsiGroupCode3, revenueCenterAnsiReasonCode3),
+                        toAdjudicationComponent(revenueCenterAnsiGroupCode4, revenueCenterAnsiReasonCode4)
+                )
+                .flatMap(Optional::stream)
+                .toList();
+    }
 
-  @Column(name = "clm_2_rev_cntr_ansi_grp_cd")
-  private Optional<String> revenueCenterAnsiGroupCode2;
 
-  @Column(name = "clm_3_rev_cntr_ansi_grp_cd")
-  private Optional<String> revenueCenterAnsiGroupCode3;
+    private Optional<ExplanationOfBenefit.AdjudicationComponent> toAdjudicationComponent(
+            Optional<RevenueCenterAnsiGroupCode> groupOpt,
+            Optional<RevenueCenterAnsiReasonCode> reasonOpt
+    ) {
+        var groupCodings = groupOpt
+                .flatMap(RevenueCenterAnsiGroupCode::toFhirCodings)
+                .orElse(List.of());
 
-  @Column(name = "clm_4_rev_cntr_ansi_grp_cd")
-  private Optional<String> revenueCenterAnsiGroupCode4;
+        if (groupCodings.isEmpty()) {
+            return Optional.empty();
+        }
 
-  List<ExplanationOfBenefit.AdjudicationComponent> toFhir() {
-    return Stream.of(
-            new Pair(revenueCenterAnsiGroupCode1, revenueCenterAnsiReasonCode1),
-            new Pair(revenueCenterAnsiGroupCode2, revenueCenterAnsiReasonCode2),
-            new Pair(revenueCenterAnsiGroupCode3, revenueCenterAnsiReasonCode3),
-            new Pair(revenueCenterAnsiGroupCode4, revenueCenterAnsiReasonCode4))
-        .flatMap(
-            pair -> pair.group().map(code -> new Pair(Optional.of(code), pair.reason())).stream())
-        .filter(pair -> pair.group().map(code -> code.length() == 2).orElse(false))
-        .map(pair -> toAdjudicationComponent(pair.group().get(), String.valueOf(pair.reason())))
-        .toList();
-  }
+        var codings = Stream.concat(
+                groupCodings.stream(),
+                reasonOpt.flatMap(RevenueCenterAnsiReasonCode::toFhirCodings).orElse(List.of()).stream()
+        ).toList();
 
-  private ExplanationOfBenefit.AdjudicationComponent toAdjudicationComponent(
-      String groupCode, String reasonCode) {
-    return new ExplanationOfBenefit.AdjudicationComponent()
-        .setCategory(
-            new CodeableConcept(
-                new Coding()
-                    .setSystem(SystemUrls.CARIN_CODE_SYSTEM_ADJUDICATION_DISCRIMINATOR)
-                    .setCode("adjustmentreason")
-                    .setDisplay("Adjustment Reason")))
-        .setReason(
-            new CodeableConcept()
-                .addCoding(
-                    new Coding()
-                        .setSystem(SystemUrls.X12_CLAIM_ADJUSTMENT_REASON_CODES)
-                        .setCode(groupCode))
-                .addCoding(
-                    new Coding()
-                        .setSystem(SystemUrls.X12_CLAIM_ADJUSTMENT_REASON_CODES)
-                        .setCode(reasonCode)));
-  }
+        return Optional.of(
+                new ExplanationOfBenefit.AdjudicationComponent()
+                        .setCategory(
+                                new CodeableConcept(
+                                        new Coding()
+                                                .setSystem(SystemUrls.CARIN_CODE_SYSTEM_ADJUDICATION_DISCRIMINATOR)
+                                                .setCode("adjustmentreason")
+                                                .setDisplay("Adjustment Reason")
+                                )
+                        )
+                        .setReason(new CodeableConcept().setCoding(codings))
+        );
+    }
 
-  private record Pair(Optional<String> group, Optional<String> reason) {}
 }
