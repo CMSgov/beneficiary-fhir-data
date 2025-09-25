9709d6ac831d635da7dc1da8eae3e52e4d409f15
o.murat <np.comp0@gmail.com>
2025-09-25 09:05:44 -0500

    Format ICD-9 codes.

---
+++ apps/bfd-server-ng/src/test/java/gov/cms/bfd/server/ng/claim/model/IcdIndicatorTest.java
@@ -0,0 +1,69 @@
+package gov.cms.bfd.server.ng.claim.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/** Unit tests for ICD code formatting rules implemented in IcdIndicator. */
+public class IcdIndicatorTest {
+  private static final String ICD10_F1010 = "F10.10";
+  private static final String ICD9_123_45 = "123.45";
+  private static final String ICD9_PROC_12_345 = "12.345";
+
+  @Test
+  public void icd10FormatsDiagnosisCorrectly() {
+    var formatted = IcdIndicator.ICD_10.formatCode("F1010");
+    assertEquals(ICD10_F1010, formatted);
+
+    var shortCode = IcdIndicator.ICD_10.formatCode("A12");
+    assertEquals("A12", shortCode);
+  }
+
+  @Test
+  public void icd9NumericDiagnosisFormatting() {
+    var formatted = IcdIndicator.ICD_9.formatCode("12345");
+    assertEquals(ICD9_123_45, formatted);
+  }
+
+  @Test
+  public void icd9EDiagnosisFormatting() {
+    var formatted = IcdIndicator.ICD_9.formatCode("E0000");
+    assertEquals("E000.0", formatted);
+  }
+
+  @Test
+  public void icd9VDiagnosisFormatting() {
+    var formatted = IcdIndicator.ICD_9.formatCode("V1234");
+    assertEquals("V12.34", formatted);
+  }
+
+  @Test
+  public void icd9ProcedureFormatting() {
+    var formatted = IcdIndicator.ICD_9.formatProcedureCode("12345");
+    assertEquals(ICD9_PROC_12_345, formatted);
+
+    var shortProc = IcdIndicator.ICD_9.formatProcedureCode("12");
+    assertEquals("12", shortProc);
+  }
+
+  @Test
+  public void formattedCodesRemainUnchanged() {
+    assertEquals(ICD9_123_45, IcdIndicator.ICD_9.formatCode(ICD9_123_45));
+    assertEquals(ICD10_F1010, IcdIndicator.ICD_10.formatCode(ICD10_F1010));
+    assertEquals(ICD9_PROC_12_345, IcdIndicator.ICD_9.formatProcedureCode(ICD9_PROC_12_345));
+  }
+
+  @Test
+  public void defaultIndicatorReturnsRawCode() {
+    var raw = "ABC123";
+    var formatted = IcdIndicator.DEFAULT.formatCode(raw);
+    assertEquals(raw, formatted);
+  }
+
+  @Test
+  public void defaultIndicatorReturnsRawProcedureCode() {
+    var rawProcedureCode = "XYZ789";
+    var formatted = IcdIndicator.DEFAULT.formatProcedureCode(rawProcedureCode);
+    assertEquals(rawProcedureCode, formatted);
+  }
+}
