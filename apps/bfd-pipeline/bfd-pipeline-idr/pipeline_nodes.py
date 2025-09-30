45cc26a6051a05e5a283121e517ef8d35d3dbe07
Melissa Guzman <melissa.guzman@forpeople.us>
2025-09-30 16:02:15 -0700

    Support parameterizization for pipeline nodes

--- apps/bfd-pipeline/bfd-pipeline-idr/pipeline_nodes.py
+++ apps/bfd-pipeline/bfd-pipeline-idr/pipeline_nodes.py
@@ -26,39 +26,27 @@ from model import (
     IdrClaimProfessional,
 )
 from pipeline_utils import extract_and_load
+from hamilton.function_modifiers import parameterize, value
 
 # Stage 1: Load auxiliary claim tables in parallel
-def idr_claim_institutional(config_connection_string: str,
-                            config_mode: str,
-                            config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrClaimInstitutional, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
-
-def idr_claim_date_signature(config_connection_string: str,
-                             config_mode: str,
-                             config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrClaimDateSignature, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
-
-def idr_claim_fiss(config_connection_string: str, config_mode: str, config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrClaimFiss, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
-
-def idr_claim_item(config_connection_string: str, config_mode: str, config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrClaimItem, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
-
-def idr_claim_line_institutional(config_connection_string: str, config_mode: str, config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrClaimLineInstitutional, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
-
-def idr_claim_ansi_signature(config_connection_string: str, config_mode: str, config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrClaimAnsiSignature, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
-
-def idr_claim_professional(config_connection_string: str, config_mode: str, config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrClaimProfessional, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
-
-def idr_claim_line_professional(config_connection_string: str, config_mode: str, config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrClaimLineProfessional, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
+@parameterize(
+    idr_claim_institutional=dict(cls=value(IdrClaimInstitutional)),
+    idr_claim_date_signature=dict(cls=value(IdrClaimDateSignature)),
+    idr_claim_fiss=dict(cls=value(IdrClaimFiss)),
+    idr_claim_item=dict(cls=value(IdrClaimItem)),
+    idr_claim_line_institutional=dict(cls=value(IdrClaimLineInstitutional)),
+    idr_claim_ansi_signature=dict(cls=value(IdrClaimAnsiSignature)),
+    idr_claim_professional=dict(cls=value(IdrClaimProfessional)),
+    idr_claim_line_professional=dict(cls=value(IdrClaimLineProfessional))
+)
+def idr_claim_aux_table(cls: type,
+                        config_connection_string: str,
+                        config_mode: str,
+                        config_batch_size: int) -> tuple[PostgresLoader, bool]:
+    return extract_and_load(cls=cls, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
 
 # Stage 2: Load main claim table
-def idr_claim(config_connection_string: str,
-              idr_claim_institutional: tuple[PostgresLoader, bool],
+def idr_claim(idr_claim_institutional: tuple[PostgresLoader, bool],
               idr_claim_date_signature: tuple[PostgresLoader, bool],
               idr_claim_fiss: tuple[PostgresLoader, bool],
               idr_claim_item: tuple[PostgresLoader, bool],
@@ -66,37 +54,36 @@ def idr_claim(config_connection_string: str,
               idr_claim_ansi_signature: tuple[PostgresLoader, bool],
               idr_claim_professional: tuple[PostgresLoader, bool],
               idr_claim_line_professional: tuple[PostgresLoader, bool],
-              config_mode: str, config_batch_size: int
+              config_mode: str,
+              config_batch_size: int,
+              config_connection_string: str,
               ) -> tuple[PostgresLoader, bool]:
     return extract_and_load(cls=IdrClaim, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
 
 # Stage 3: Load auxiliary beneficiary tables in parallel
-def idr_beneficiary_status(idr_claim: tuple[PostgresLoader, bool], config_connection_string: str, config_mode: str, config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrBeneficiaryStatus, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
-
-def idr_beneficiary_third_party(idr_claim: tuple[PostgresLoader, bool], config_connection_string: str, config_mode: str, config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrBeneficiaryThirdParty, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
-
-def idr_beneficiary_entitlement(idr_claim: tuple[PostgresLoader, bool], config_connection_string: str, config_mode: str, config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrBeneficiaryEntitlement, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
-
-def idr_beneficiary_entitlement_reason(idr_claim: tuple[PostgresLoader, bool], config_connection_string: str, config_mode: str, config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrBeneficiaryEntitlementReason, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
-
-def idr_beneficiary_dual_eligibility(idr_claim: tuple[PostgresLoader, bool], config_connection_string: str, config_mode: str, config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrBeneficiaryDualEligibility, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
-
-def idr_beneficiary_mbi_id(idr_claim: tuple[PostgresLoader, bool], config_connection_string: str, config_mode: str, config_batch_size: int) -> tuple[PostgresLoader, bool]:
-    return extract_and_load(cls=IdrBeneficiaryMbiId, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
+@parameterize(
+    idr_beneficiary_status=dict(cls=value(IdrBeneficiaryStatus)),
+    idr_beneficiary_third_party=dict(cls=value(IdrBeneficiaryThirdParty)),
+    idr_beneficiary_entitlement=dict(cls=value(IdrBeneficiaryEntitlement)),
+    idr_beneficiary_entitlement_reason=dict(cls=value(IdrBeneficiaryEntitlementReason)),
+    idr_beneficiary_dual_eligibility=dict(cls=value(IdrBeneficiaryDualEligibility)),
+    idr_beneficiary_mbi_id=dict(cls=value(IdrBeneficiaryMbiId))
+)
+def idr_beneficiary_aux_table(idr_claim: tuple[PostgresLoader, bool],
+                              cls: type,
+                              config_connection_string: str,
+                              config_mode: str,
+                              config_batch_size: int) -> tuple[PostgresLoader, bool]:
+    return extract_and_load(cls=cls, connection_string=config_connection_string, mode=config_mode, batch_size=config_batch_size)
 
 # Stage 4: Load main beneficiary tables last
-def idr_beneficiary(config_connection_string: str,
-                    idr_beneficiary_status: tuple[PostgresLoader, bool],
+def idr_beneficiary(idr_beneficiary_status: tuple[PostgresLoader, bool],
                     idr_beneficiary_third_party: tuple[PostgresLoader, bool],
                     idr_beneficiary_entitlement: tuple[PostgresLoader, bool],
                     idr_beneficiary_entitlement_reason: tuple[PostgresLoader, bool],
                     idr_beneficiary_dual_eligibility: tuple[PostgresLoader, bool],
                     idr_beneficiary_mbi_id: tuple[PostgresLoader, bool],
+                    config_connection_string: str,
                     config_mode: str,
                     config_batch_size: int
                     ) -> tuple[PostgresLoader, bool]:
