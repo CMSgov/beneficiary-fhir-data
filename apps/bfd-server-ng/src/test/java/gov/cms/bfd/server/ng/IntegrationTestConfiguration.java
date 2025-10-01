4f9e6be5473e9dd9f774cc8be04eb5a43fe062b3
Brick Green <brickgreen@navapbc.com>
2025-10-01 17:15:45 -0500

    format the fix to integration tests

--- apps/bfd-server-ng/src/test/java/gov/cms/bfd/server/ng/IntegrationTestConfiguration.java
+++ apps/bfd-server-ng/src/test/java/gov/cms/bfd/server/ng/IntegrationTestConfiguration.java
@@ -41,15 +41,16 @@ public class IntegrationTestConfiguration {
     runPython(container, "uv", "sync");
     runPython(container, "uv", "run", "load_synthetic.py", "./test_samples2");
 
-      container.execInContainer(
-              "psql",
-              "-U", "bfdtest",
-              "-d", "testdb",
-              "-c", "UPDATE cms_vdm_view_mdcr_prd.v2_mdcr_clm SET \"clm_idr_ld_dt\" = CURRENT_DATE;"
-      );
+    container.execInContainer(
+        "psql",
+        "-U",
+        "bfdtest",
+        "-d",
+        "testdb",
+        "-c",
+        "UPDATE cms_vdm_view_mdcr_prd.v2_mdcr_clm SET \"clm_idr_ld_dt\" = CURRENT_DATE;");
 
-
-      runPython(container, "uv", "run", "pipeline.py", "synthetic");
+    runPython(container, "uv", "run", "pipeline.py", "synthetic");
 
     return container;
   }
