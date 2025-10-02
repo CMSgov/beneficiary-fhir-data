c9075027d18021a6120a60c459de228a7b054cca
Mitch Alessio <mitch.alessio@forpeople.us>
2025-10-02 11:28:01 -0400

    Rename MDCFilter; refactor cert alias extraction from auth filter into metadata filter

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/filter/AuthenticationFilter.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/filter/AuthenticationFilter.java
@@ -9,23 +9,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.MDC;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
- * Authenticates each request by checking the leaf/client certificate provided by the client to see
- * if it is in the trusted certificate configuration map. If not, the request is denied.
+ * Authenticates each request by checking if a certificate alias was extracted from the request. If
+ * not, the request is denied.
  */
 @Component
 @RequiredArgsConstructor
-// This should run directly after the MDC filter
+// This should run directly after the metadata filter
 @Order(2)
 @WebFilter(filterName = "AuthenticationFilter")
 public class AuthenticationFilter extends OncePerRequestFilter {
   private static final String MISSING_INVALID_HEADER_MSG = "Missing or invalid certificate header.";
-  private static final String CLIENT = "client";
 
   private final CertificateUtil certificateUtil;
 
@@ -40,18 +38,14 @@ public class AuthenticationFilter extends OncePerRequestFilter {
       @NotNull HttpServletResponse response,
       @NotNull FilterChain filterChain)
       throws IOException, ServletException {
-
-    final var certAlias = certificateUtil.getAliasFromCert(request);
-
+    // An empty certAlias implies that the upstream ExtractMetadataFilter did not find a matching
+    // certificate. For non-local requests, this means the request is not authorized
+    final var certAlias = certificateUtil.getAliasAttribute(request);
     if (certAlias.isEmpty()) {
       response.sendError(401, MISSING_INVALID_HEADER_MSG);
       return;
     }
-    certificateUtil.attachCertAliasToRequest(request, certAlias.get());
-    MDC.put(CLIENT, certAlias.get());
-    filterChain.doFilter(request, response);
 
-    // Clean up to prevent leaks
-    MDC.remove(CLIENT);
+    filterChain.doFilter(request, response);
   }
 }
