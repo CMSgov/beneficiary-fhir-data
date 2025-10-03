059f83f0299a2c9ec385c86facadb44c685fcdb7
o.murat <np.comp0@gmail.com>
2025-10-03 09:27:58 -0500

    Replace samhsa code with non-samhsa one.

--- apps/bfd-server-ng/src/test/java/gov/cms/bfd/server/ng/IcdIndicatorIT.java
+++ apps/bfd-server-ng/src/test/java/gov/cms/bfd/server/ng/IcdIndicatorIT.java
@@ -15,11 +15,11 @@ public class IcdIndicatorIT extends IntegrationTestBase {
   private static final String FORMATTED_ICD_9_CODE_25000 = "250.00";
   private static final String FORMATTED_ICD_9_CODE_E8889 = "E888.9";
   private static final String FORMATTED_ICD_9_CODE_V1005 = "V10.05";
-  private static final String RAW_30492 = "30492";
+  private static final String RAW_30495 = "30495";
   private static final String RAW_25000 = "25000";
   private static final String RAW_E8889 = "E8889";
   private static final String RAW_V1005 = "V1005";
-  private static final String FORMATTED_ICD_9_CODE_30492 = "304.92";
+  private static final String FORMATTED_ICD_9_CODE_30495 = "304.95";
 
   private IReadTyped<ExplanationOfBenefit> eobRead() {
     return getFhirClient().read().resource(ExplanationOfBenefit.class);
@@ -36,7 +36,7 @@ public class IcdIndicatorIT extends IntegrationTestBase {
             .map(d -> d.getDiagnosisCodeableConcept().getCodingFirstRep().getCode())
             .toList();
 
-    var rawCodes = List.of(RAW_30492, RAW_25000, RAW_E8889, RAW_V1005);
+    var rawCodes = List.of(RAW_E8889, RAW_V1005, RAW_30495, RAW_25000);
     var independentlyFormatted =
         rawCodes.stream()
             .collect(
@@ -48,10 +48,10 @@ public class IcdIndicatorIT extends IntegrationTestBase {
 
     var expectedOrder =
         List.of(
-            FORMATTED_ICD_9_CODE_30492,
-            FORMATTED_ICD_9_CODE_25000,
             FORMATTED_ICD_9_CODE_E8889,
-            FORMATTED_ICD_9_CODE_V1005);
+            FORMATTED_ICD_9_CODE_V1005,
+            FORMATTED_ICD_9_CODE_30495,
+            FORMATTED_ICD_9_CODE_25000);
     assertEquals(expectedOrder, eobCodes, "ICD-9 codes should be formatted and ordered");
     assertEquals(FORMATTED_ICD_9_CODE_25000, independentlyFormatted.get(RAW_25000));
     assertEquals(FORMATTED_ICD_9_CODE_E8889, independentlyFormatted.get(RAW_E8889));
@@ -59,10 +59,10 @@ public class IcdIndicatorIT extends IntegrationTestBase {
 
     // assert string representation instead of a snapshot.
     var expectedMap = new java.util.LinkedHashMap<String, String>();
-    expectedMap.put(RAW_30492, FORMATTED_ICD_9_CODE_30492);
-    expectedMap.put(RAW_25000, FORMATTED_ICD_9_CODE_25000);
     expectedMap.put(RAW_E8889, FORMATTED_ICD_9_CODE_E8889);
     expectedMap.put(RAW_V1005, FORMATTED_ICD_9_CODE_V1005);
+    expectedMap.put(RAW_30495, FORMATTED_ICD_9_CODE_30495);
+    expectedMap.put(RAW_25000, FORMATTED_ICD_9_CODE_25000);
 
     var actualSnapshotString = eobCodes.toString() + "|" + independentlyFormatted.toString();
     var expectedSnapshotString = expectedOrder.toString() + "|" + expectedMap.toString();
@@ -82,10 +82,10 @@ public class IcdIndicatorIT extends IntegrationTestBase {
 
     var expected =
         List.of(
-            FORMATTED_ICD_9_CODE_30492,
-            FORMATTED_ICD_9_CODE_25000,
             FORMATTED_ICD_9_CODE_E8889,
-            FORMATTED_ICD_9_CODE_V1005);
+            FORMATTED_ICD_9_CODE_V1005,
+            FORMATTED_ICD_9_CODE_30495,
+            FORMATTED_ICD_9_CODE_25000);
     assertEquals(expected, codes, "ICD-9 diagnosis codes should be formatted and ordered");
 
     // compare string representations rather than using snapshot storage.
