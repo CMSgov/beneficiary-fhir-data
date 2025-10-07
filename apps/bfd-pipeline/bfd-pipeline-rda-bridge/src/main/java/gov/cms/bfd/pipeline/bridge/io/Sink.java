7f1a2787ead0303a8d35fff6d5ba5fbbe194b025
o.murat <np.comp0@gmail.com>
2025-10-07 13:35:05 -0500

    Fix javadoc issues for stricter JDK-25.

--- apps/bfd-pipeline/bfd-pipeline-rda-bridge/src/main/java/gov/cms/bfd/pipeline/bridge/io/Sink.java
+++ apps/bfd-pipeline/bfd-pipeline-rda-bridge/src/main/java/gov/cms/bfd/pipeline/bridge/io/Sink.java
@@ -2,12 +2,16 @@ package gov.cms.bfd.pipeline.bridge.io;
 
 import java.io.Closeable;
 
-/** Interface for the Sink that extends Closeable. */
+/**
+ * Interface for the Sink that extends Closeable.
+ *
+ * @param <T> the type of items accepted by this Sink
+ */
 public interface Sink<T> extends Closeable {
   /**
-   * Interface method for write out to a file.
+   * Writes a value to the sink (for example, to a file or stream).
    *
-   * @param value that is written out to the file
+   * @param value the value to be written
    */
   void write(T value);
 }
