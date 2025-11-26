e041c5d32480154d883506b0caff4e915bc65f72
Austin Schey <austin.schey@forpeople.us>
2025-11-26 13:49:36 -0800

    trying process executor

--- apps/bfd-pipeline/bfd-pipeline-idr/pipeline.py
+++ apps/bfd-pipeline/bfd-pipeline-idr/pipeline.py
@@ -1,10 +1,14 @@
 import logging
 import os
 import sys
+from abc import ABC
+from concurrent.futures import Future, ProcessPoolExecutor
 from datetime import UTC, datetime
+from typing import Any
 
 from hamilton import driver, telemetry  # type: ignore
 from hamilton.execution import executors  # type: ignore
+from hamilton.execution.grouping import TaskImplementation  # type: ignore
 
 import pipeline_nodes
 from loader import get_connection_string
@@ -17,6 +21,38 @@ console_handler.setFormatter(formatter)
 logging.basicConfig(level=logging.INFO, handlers=[console_handler])
 
 logger = logging.getLogger(__name__)
+
+
+class SingleProcessExecutor(executors.TaskExecutor, ABC):
+    def __init__(self, max_tasks: int) -> None:
+        self.active_futures: list[tuple[ProcessPoolExecutor, Future[Any]]] = []
+        self.max_tasks = max_tasks
+
+    def _prune_active_futures(self) -> None:
+        new_futures: list[tuple[ProcessPoolExecutor, Future[Any]]] = []
+        for f in self.active_futures:
+            if f[1].done():
+                f[0].shutdown()
+            else:
+                new_futures.append(f)
+        self.active_futures = new_futures
+
+    def init(self) -> None:
+        pass
+
+    def finalize(self) -> None:
+        for f in self.active_futures:
+            f[0].shutdown(cancel_futures=True)
+
+    def submit_task(self, task: TaskImplementation) -> executors.TaskFuture:
+        executor = ProcessPoolExecutor(max_workers=1)
+        future = executor.submit(executors.base_execute_task, task)
+        self.active_futures.append((executor, future))
+        return executors.TaskFutureWrappingPythonFuture(future)
+
+    def can_submit_task(self) -> bool:
+        self._prune_active_futures()
+        return len(self.active_futures) < self.max_tasks
 
 
 def main() -> None:
@@ -33,8 +69,10 @@ def run(mode: str) -> None:
         driver.Builder()
         .enable_dynamic_execution(allow_experimental_mode=True)
         .with_modules(pipeline_nodes)
-        .with_local_executor(executors.MultiProcessingExecutor(max_tasks=32))
-        .with_remote_executor(executors.MultiProcessingExecutor(max_tasks=32))
+        .with_local_executor(SingleProcessExecutor(max_tasks=32))
+        .with_remote_executor(SingleProcessExecutor(max_tasks=32))
+        # .with_local_executor(executors.MultiProcessingExecutor(max_tasks=32))
+        # .with_remote_executor(executors.MultiProcessingExecutor(max_tasks=32))
         .build()
     )
 
