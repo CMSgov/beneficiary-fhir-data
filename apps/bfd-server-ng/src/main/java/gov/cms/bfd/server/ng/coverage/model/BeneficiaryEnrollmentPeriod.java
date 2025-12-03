129f0f63c38761c9c1490dcb096f7f91c595979c
Melissa Guzman <melissa.guzman@forpeople.us>
2025-12-03 07:59:53 -0800

    reformat

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/coverage/model/BeneficiaryEnrollmentPeriod.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/coverage/model/BeneficiaryEnrollmentPeriod.java
@@ -3,37 +3,36 @@ package gov.cms.bfd.server.ng.coverage.model;
 import gov.cms.bfd.server.ng.util.DateUtil;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import java.time.LocalDate;
 import lombok.Getter;
 import org.hl7.fhir.r4.model.Coverage;
 import org.hl7.fhir.r4.model.Period;
-
-import java.time.LocalDate;
 
 /** Entitlement period. */
 @Getter
 @Embeddable
 public class BeneficiaryEnrollmentPeriod {
-    @Column(name = "bene_enrlmt_bgn_dt")
-    private LocalDate enrollmentBeginDate;
+  @Column(name = "bene_enrlmt_bgn_dt")
+  private LocalDate enrollmentBeginDate;
 
-    @Column(name = "bene_enrlmt_end_dt")
-    private LocalDate enrollmentEndDate;
+  @Column(name = "bene_enrlmt_end_dt")
+  private LocalDate enrollmentEndDate;
 
-    Period toFhirPeriod() {
-        var period = new Period().setStartElement(DateUtil.toFhirDate(enrollmentBeginDate));
-        final LocalDate defaultMaxDate = LocalDate.of(9999, 12, 31);
-        if (enrollmentEndDate != null && enrollmentEndDate.isBefore(defaultMaxDate)) {
-            period.setEndElement(DateUtil.toFhirDate(enrollmentEndDate));
-        }
-        return period;
+  Period toFhirPeriod() {
+    var period = new Period().setStartElement(DateUtil.toFhirDate(enrollmentBeginDate));
+    final LocalDate defaultMaxDate = LocalDate.of(9999, 12, 31);
+    if (enrollmentEndDate != null && enrollmentEndDate.isBefore(defaultMaxDate)) {
+      period.setEndElement(DateUtil.toFhirDate(enrollmentEndDate));
+    }
+    return period;
+  }
+
+  Coverage.CoverageStatus toFhirStatus() {
+    LocalDate today = LocalDate.now();
+    if (today.isBefore(enrollmentBeginDate) || today.isAfter(enrollmentEndDate)) {
+      return Coverage.CoverageStatus.CANCELLED;
     }
 
-    Coverage.CoverageStatus toFhirStatus() {
-        LocalDate today = LocalDate.now();
-        if (today.isBefore(enrollmentBeginDate) || today.isAfter(enrollmentEndDate)) {
-            return Coverage.CoverageStatus.CANCELLED;
-        }
-
-        return Coverage.CoverageStatus.ACTIVE;
-    }
+    return Coverage.CoverageStatus.ACTIVE;
+  }
 }
