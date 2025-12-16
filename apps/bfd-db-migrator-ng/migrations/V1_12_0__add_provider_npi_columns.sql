d313d8e8db63482966f1313e65e563c15f9a8644
victor-chaparro <victorchaparro@navapbc.com>
2025-12-16 12:38:36 -0700

    Added Provider NPI columns to the pipeline

---
+++ apps/bfd-db-migrator-ng/migrations/V1_12_0__add_provider_npi_columns.sql
@@ -0,0 +1,16 @@
+ALTER TABLE idr.claim
+ADD COLUMN prvdr_srvc_prvdr_npi_num VARCHAR(10);
+
+ALTER TABLE idr.claim
+ADD COLUMN prvdr_atndg_prvdr_npi_num VARCHAR(10);
+
+ALTER TABLE idr.claim
+ADD COLUMN prvdr_othr_prvdr_npi_num VARCHAR(10);
+
+ALTER TABLE idr.claim
+ADD COLUMN prvdr_rndrng_prvdr_npi_num VARCHAR(10);
+
+ALTER TABLE idr.claim_item
+ADD COLUMN prvdr_rndrng_prvdr_npi_num VARCHAR(10);
+
+
