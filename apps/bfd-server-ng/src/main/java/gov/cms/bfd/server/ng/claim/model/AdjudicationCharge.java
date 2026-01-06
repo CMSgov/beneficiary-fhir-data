2bc375d706ad1b78af039c51240082be104de6fb
mel1-G <melissa.guzman@forpeople.us>
2026-01-06 10:31:10 -0800

    BFD-4401: Implement financial field mapping changes (#2938)

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/AdjudicationCharge.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/AdjudicationCharge.java
@@ -2,7 +2,6 @@ package gov.cms.bfd.server.ng.claim.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import java.util.ArrayList;
 import java.util.List;
 import org.hl7.fhir.r4.model.ExplanationOfBenefit;
 
@@ -10,10 +9,10 @@ import org.hl7.fhir.r4.model.ExplanationOfBenefit;
 class AdjudicationCharge {
 
   @Column(name = "clm_alowd_chrg_amt")
-  private float allowedChargeAmount;
+  private double allowedChargeAmount;
 
   @Column(name = "clm_sbmt_chrg_amt")
-  private float submittedChargeAmount;
+  private double submittedChargeAmount;
 
   @Column(name = "clm_bene_pmt_amt")
   private double benePaymentAmount;
@@ -21,12 +20,81 @@ class AdjudicationCharge {
   @Column(name = "clm_prvdr_pmt_amt")
   private double providerPaymentAmount;
 
-  List<ExplanationOfBenefit.TotalComponent> toFhir() {
-    return new ArrayList<>(
-        List.of(
-            AdjudicationChargeType.ALLOWED_CHARGE_AMOUNT.toFhirTotal(allowedChargeAmount),
-            AdjudicationChargeType.SUBMITTED_CHARGE_AMOUNT.toFhirTotal(submittedChargeAmount),
-            AdjudicationChargeType.BENE_PAYMENT_AMOUNT.toFhirTotal(benePaymentAmount),
-            AdjudicationChargeType.PROVIDER_PAYMENT_AMOUNT.toFhirTotal(providerPaymentAmount)));
+  @Column(name = "clm_bene_intrst_pd_amt")
+  private double beneInterestPaidAmount;
+
+  @Column(name = "clm_bene_pmt_coinsrnc_amt")
+  private double beneCoinsuranceAmount;
+
+  @Column(name = "clm_mdcr_ddctbl_amt")
+  private double deductibleAmount;
+
+  @Column(name = "clm_mdcr_coinsrnc_amt")
+  private double coinsuranceAmount;
+
+  @Column(name = "clm_ncvrd_chrg_amt")
+  private double noncoveredChargeAmount;
+
+  @Column(name = "clm_othr_tp_pd_amt")
+  private double otherThirdPartyPayerPaidAmount;
+
+  @Column(name = "clm_blood_chrg_amt")
+  private double bloodChargeAmount;
+
+  @Column(name = "clm_blood_lblty_amt")
+  private double bloodLiabilityAmount;
+
+  @Column(name = "clm_tot_cntrctl_amt")
+  private double totalContractualAmountDiscrepancy;
+
+  @Column(name = "clm_cob_ptnt_resp_amt")
+  private double cobPatientResponsibilityAmount;
+
+  @Column(name = "clm_prvdr_otaf_amt")
+  private double providerObligationToAcceptAmount;
+
+  @Column(name = "clm_prvdr_rmng_due_amt")
+  private double remainingAmountToProvider;
+
+  @Column(name = "clm_blood_ncvrd_chrg_amt")
+  private double bloodNoncoveredChargeAmount;
+
+  @Column(name = "clm_prvdr_intrst_pd_amt")
+  private double providerInterestPaidAmount;
+
+  List<ExplanationOfBenefit.TotalComponent> toFhirTotal() {
+    return List.of(
+        AdjudicationChargeType.ALLOWED_CHARGE_AMOUNT.toFhirTotal(allowedChargeAmount),
+        AdjudicationChargeType.SUBMITTED_CHARGE_AMOUNT.toFhirTotal(submittedChargeAmount),
+        AdjudicationChargeType.BENE_PAYMENT_AMOUNT.toFhirTotal(benePaymentAmount),
+        AdjudicationChargeType.PROVIDER_PAYMENT_AMOUNT.toFhirTotal(providerPaymentAmount),
+        AdjudicationChargeType.BENE_PART_B_DEDUCTIBLE_AMOUNT.toFhirTotal(deductibleAmount),
+        AdjudicationChargeType.BENE_PART_A_COINSURANCE_LIABILITY_AMOUNT.toFhirTotal(
+            coinsuranceAmount),
+        AdjudicationChargeType.INPATIENT_NON_COVERED_CHARGE_AMOUNT.toFhirTotal(
+            noncoveredChargeAmount),
+        AdjudicationChargeType.OTHER_THIRD_PARTY_PAYER_PAID_AMOUNT.toFhirTotal(
+            otherThirdPartyPayerPaidAmount),
+        AdjudicationChargeType.BENE_COINSURANCE_AMOUNT.toFhirTotal(beneCoinsuranceAmount));
+  }
+
+  List<ExplanationOfBenefit.AdjudicationComponent> toFhirAdjudication() {
+    return List.of(
+        AdjudicationChargeType.BENE_INTEREST_PAID_AMOUNT.toFhirAdjudication(beneInterestPaidAmount),
+        AdjudicationChargeType.BLOOD_CHARGE_AMOUNT.toFhirAdjudication(bloodChargeAmount),
+        AdjudicationChargeType.BENE_BLOOD_DEDUCTIBLE_LIABILITY_AMOUNT.toFhirAdjudication(
+            bloodLiabilityAmount),
+        AdjudicationChargeType.BLOOD_NONCOVERED_CHARGE_AMOUNT.toFhirAdjudication(
+            bloodNoncoveredChargeAmount),
+        AdjudicationChargeType.COB_PATIENT_RESPONSIBILITY_AMOUNT.toFhirAdjudication(
+            cobPatientResponsibilityAmount),
+        AdjudicationChargeType.PROVIDER_INTEREST_PAID_AMOUNT.toFhirAdjudication(
+            providerInterestPaidAmount),
+        AdjudicationChargeType.PROVIDER_OBLIGATION_TO_ACCEPT_AMOUNT.toFhirAdjudication(
+            providerObligationToAcceptAmount),
+        AdjudicationChargeType.REMAINING_AMOUNT_TO_PROVIDER.toFhirAdjudication(
+            remainingAmountToProvider),
+        AdjudicationChargeType.TOTAL_CONTRACTUAL_AMOUNT_DISCREPANCY.toFhirAdjudication(
+            totalContractualAmountDiscrepancy));
   }
 }
--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/AdjudicationChargeType.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/AdjudicationChargeType.java
@@ -10,8 +10,8 @@ import org.hl7.fhir.r4.model.ExplanationOfBenefit;
 // Suppress warnings for duplicate string literals.
 // There's too many values here - creating constants for all of these adds too much noise.
 @SuppressWarnings("java:S1192")
-@Getter
 @AllArgsConstructor
+@Getter
 enum AdjudicationChargeType {
   LINE_NONCOVERED_CHARGE_AMOUNT(
       SystemUrls.CARIN_CODE_SYSTEM_ADJUDICATION,
@@ -68,15 +68,11 @@ enum AdjudicationChargeType {
       "CLM_LINE_MDCR_DDCTBL_AMT",
       "Revenue Center Cash Deductible Amount"),
   LINE_INSTITUTIONAL_ADJUSTED_AMOUNT(
-      SystemUrls.CARIN_CODE_SYSTEM_ADJUDICATION,
-      "coinsurance",
-      "Co-insurance",
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
       "CLM_LINE_INSTNL_ADJSTD_AMT",
       "Revenue Center Coinsurance/Wage Adjusted Coinsurance Amount"),
   LINE_INSTITUTIONAL_REDUCED_AMOUNT(
-      SystemUrls.CARIN_CODE_SYSTEM_ADJUDICATION,
-      "coinsurance",
-      "Co-insurance",
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
       "CLM_LINE_INSTNL_RDCD_AMT",
       "Revenue Center Reduced Coinsurance Amount"),
   LINE_INSTITUTIONAL_1ST_MSP_PAID_AMOUNT(
@@ -92,9 +88,7 @@ enum AdjudicationChargeType {
       "CLM_LINE_INSTNL_MSP2_PD_AMT",
       "Revenue Center 2nd MSP Paid Amount"),
   LINE_INSTITUTIONAL_RATE_AMOUNT(
-      SystemUrls.HL7_ADJUDICATION,
-      "submitted",
-      "Submitted Amount",
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
       "CLM_LINE_INSTNL_RATE_AMT",
       "Revenue Center Rate Amount"),
   ALLOWED_CHARGE_AMOUNT(
@@ -122,9 +116,7 @@ enum AdjudicationChargeType {
       "CLM_PRVDR_PMT_AMT",
       "Provider Payment Amount"),
   PAYER_PAID_AMOUNT(
-      SystemUrls.CARIN_CODE_SYSTEM_ADJUDICATION,
-      "paidtoprovider",
-      "Paid to provider",
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
       "CLM_MDCR_PRFNL_PRMRY_PYR_AMT",
       "Primary Payer Paid Amount"),
   GAP_DISCOUNT_AMOUNT(
@@ -150,7 +142,291 @@ enum AdjudicationChargeType {
   GROSS_DRUG_COST_ABOVE_THRESHOLD_AMOUNT(
       SystemUrls.HL7_ADJUDICATION,
       "CLM_LINE_GRS_ABOVE_THRSHLD_AMT",
-      "Gross Drug Cost Above Out Of Pocket Threshold");
+      "Gross Drug Cost Above Out Of Pocket Threshold"),
+  BENE_MEDICARE_LRD_USED_COUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_IP_LRD_USE_CNT",
+      "Beneficiary Medicare Lifetime Reserve Days (LRD) Used Count"),
+  BENE_TOTAL_COINSURANCE_DAYS_COUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_INSTNL_MDCR_COINS_DAY_CNT",
+      "Beneficiary Total Coinsurance Days Count"),
+  NON_UTILIZATION_DAYS_COUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_INSTNL_NCVRD_DAY_CNT",
+      "Claim Medicare Non Utilization Days Count"),
+  HOSPICE_PERIOD_COUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_HOSPC_PRD_CNT",
+      "Beneficiary's Hospice Period Count."),
+  HHA_TOTAL_VISIT_COUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_HHA_TOT_VISIT_CNT",
+      "Claim HHA Total Visit Count."),
+  PPS_CAPITAL_DRUG_WEIGHT_NUMBER(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_IP_PPS_DRG_WT_NUM",
+      "Claim PPS Capital DRG Weight Number"),
+  UTILIZATION_DAYS_COUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_INSTNL_CVRD_DAY_CNT",
+      "Claim Medicare Utilization Day Count"),
+  PER_DIEM_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_INSTNL_PER_DIEM_AMT",
+      "Claim Pass Thru Per Diem Amount"),
+  PPS_CAPITAL_DISPROPORTIONATE_SHARE_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_IP_PPS_DSPRPRTNT_AMT",
+      "Claim PPS Capital Disproportionate Share Amount"),
+  PPS_CAPITAL_EXCEPTION_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_IP_PPS_EXCPTN_AMT",
+      "Claim PPS Capital Exception Amount"),
+  PPS_CAPITAL_FEDERAL_SPECIFIC_PORTION_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_IP_PPS_CPTL_FSP_AMT",
+      "Claim PPS Capital Federal Specific Portion (FSP) Amount"),
+  PPS_CAPITAL_INDIRECT_MEDICAL_EDUCATION_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_IP_PPS_CPTL_IME_AMT",
+      "Claim PPS Capital Indirect Medical Education (IME) Amount"),
+  PPS_CAPITAL_OUTLIER_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_IP_PPS_OUTLIER_AMT",
+      "Claim PPS Capital Outlier Amount"),
+  PPS_OLD_CAPITAL_HOLD_HARMLESS_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_IP_PPS_CPTL_HRMLS_AMT",
+      "Claim PPS Old Capital Hold Harmless Amount"),
+  PPS_CAPITAL_HOLD_TOTAL_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_IP_PPS_CPTL_TOT_AMT",
+      "Claim Total PPS Capital Amount"),
+  PRIMARY_PAYER_NON_MEDICARE_PAID_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_INSTNL_PRMRY_PYR_AMT",
+      "Primary Payer (if not Medicare) Claim Paid Amount"),
+  PROFESSIONAL_COMPONENT_CHARGE_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_INSTNL_PRFNL_AMT",
+      "Professional Component Charge Amount"),
+  DRUG_OUTLIER_APPROVED_PAYMENT_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_INSTNL_DRG_OUTLIER_AMT",
+      "DRG Outlier Approved Payment Amount"),
+  UNCOMPENSATED_CARE_PAYMENT_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_HIPPS_UNCOMPD_CARE_AMT",
+      "Claim Uncompensated Care Payment Amount"),
+  BENE_INPATIENT_DEDUCTIBLE_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_IP_BENE_DDCTBL_AMT",
+      "Beneficiary Inpatient (or other Part A) Deductible Amount"),
+  BENE_PAID_AMOUNT(
+      SystemUrls.CARIN_CODE_SYSTEM_ADJUDICATION,
+      "paidtopatient",
+      "Paid to patient",
+      "CLM_MDCR_INSTNL_BENE_PD_AMT",
+      "The amount paid, from the Medicare trust fund, to the beneficiary for the services reported on the claim."),
+  STANDARDIZED_PAYMENT_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_FINL_STDZD_PYMT_AMT",
+      "Standardized Payment Amount"),
+  HOSPITAL_ACQUIRED_CONDITION_REDUCTION_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_HAC_RDCTN_PYMT_AMT",
+      "Hospital Acquired Condition Reduction Amount"),
+  BLENDED_PAYMENT_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_HIPPS_MODEL_BNDLD_PMT_AMT",
+      "Blended Payment Amount"),
+  READMISSION_REDUCTION_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_HIPPS_READMSN_RDCTN_AMT",
+      "Readmission Reduction Amount"),
+  HIPPS_VALUE_BASED_PURCHASING_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_HIPPS_VBP_AMT",
+      "HIPPS Value Based Purchasing Amount"),
+  LOW_VOLUME_PAYMENT_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_INSTNL_LOW_VOL_PMT_AMT",
+      "Low Volume Payment Amount"),
+  FIRST_YEAR_RATE_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_IP_1ST_YR_RATE_AMT",
+      "First Year Rate Amount"),
+  SECOND_YEAR_RATE_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_IP_SCND_YR_RATE_AMT",
+      "Second Year Rate Amount"),
+  MARYLAND_WAIVER_STANDARDIZED_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_PPS_MD_WVR_STDZD_VAL_AMT",
+      "Maryland Waiver Standardized Amount"),
+  SITE_NEUTRAL_COST_BASED_PAYMENT_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_SITE_NTRL_CST_BSD_PYMT_AMT",
+      "Site-Neutral Cost-Based Payment Amount"),
+  SITE_NEUTRAL_IPPS_PAYMENT_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_SITE_NTRL_IP_PPS_PYMT_AMT",
+      "Site-Neutral IPPS Payment Amount"),
+  SHORT_STAY_OUTLIER_PAYMENT_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_SS_OUTLIER_STD_PYMT_AMT",
+      "Short Stay Outlier Payment Amount"),
+  OPERATING_DISPROPORTIONATE_SHARE_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_OPRTNL_DSPRTNT_AMT",
+      "Operating Disproportionate Share Amount"),
+  OPERATING_INDIRECT_MEDICAL_EDUCATION_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_OPRTNL_IME_AMT",
+      "Operating Indirect Medical Education Amount"),
+  BENE_PART_B_DEDUCTIBLE_AMOUNT(
+      SystemUrls.HL7_ADJUDICATION,
+      "deductible",
+      "Deductible",
+      "CLM_MDCR_DDCTBL_AMT",
+      "Beneficiary Part B Deductible Amount"),
+  BENE_PART_A_COINSURANCE_LIABILITY_AMOUNT(
+      SystemUrls.HL7_ADJUDICATION,
+      "deductible",
+      "Deductible",
+      "CLM_MDCR_COINSRNC_AMT",
+      "Beneficiary Part A Coinsurance Liability Amount"),
+  BENE_BLOOD_DEDUCTIBLE_LIABILITY_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_BLOOD_LBLTY_AMT",
+      "Beneficiary Blood Deductible Liability Amount"),
+  INPATIENT_NON_COVERED_CHARGE_AMOUNT(
+      SystemUrls.CARIN_CODE_SYSTEM_ADJUDICATION,
+      "noncovered",
+      "Noncovered",
+      "CLM_NCVRD_CHRG_AMT",
+      "Inpatient(or other Part A) Non-covered Charge Amount"),
+  BENE_INTEREST_PAID_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_BENE_INTRST_PD_AMT",
+      "Beneficiary Interest Paid Amount"),
+  BENE_COINSURANCE_AMOUNT(
+      SystemUrls.CARIN_CODE_SYSTEM_ADJUDICATION,
+      "coinsurance",
+      "Co-insurance",
+      "CLM_BENE_PMT_COINSRNC_AMT",
+      "Beneficiary Coinsurance Amount"),
+  BLOOD_CHARGE_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION, "CLM_BLOOD_CHRG_AMT", "Blood Charge Amount"),
+  BLOOD_NONCOVERED_CHARGE_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_BLOOD_NCVRD_CHRG_AMT",
+      "Blood Noncovered Charge Amount"),
+  COB_PATIENT_RESPONSIBILITY_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_COB_PTNT_RESP_AMT",
+      "Coordination of Benefits Patient Responsibility Amount"),
+  OTHER_THIRD_PARTY_PAYER_PAID_AMOUNT(
+      SystemUrls.CARIN_CODE_SYSTEM_ADJUDICATION,
+      "priorpayerpaid",
+      "Prior payer paid",
+      "CLM_OTHR_TP_PD_AMT",
+      "Other Third Party Payer Paid Amount"),
+  PROVIDER_INTEREST_PAID_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_PRVDR_INTRST_PD_AMT",
+      "Provider Interest Paid Amount"),
+  PROVIDER_OBLIGATION_TO_ACCEPT_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_PRVDR_OTAF_AMT",
+      "Provider Obligation To Accept as Full Amount"),
+  REMAINING_AMOUNT_TO_PROVIDER(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_PRVDR_RMNG_DUE_AMT",
+      "Remaining Amount to Provider"),
+  TOTAL_CONTRACTUAL_AMOUNT_DISCREPANCY(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_TOT_CNTRCTL_AMT",
+      "Total Contractual Amount Discrepancy"),
+  PROVIDER_OFFSET_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_PRVDR_ACNT_RCVBL_OFST_AMT",
+      "Provider Offset Amount"),
+  LINE_INSTITUTIONAL_ADD_ON_PAYMENT_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_LINE_ADD_ON_PYMT_AMT",
+      "Add On Payment Amount"),
+  LINE_INSTITUTIONAL_NON_EHR_REDUCTION_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_LINE_NON_EHR_RDCTN_AMT",
+      "Non-EHR Reduction Amount"),
+  LINE_PROFESSIONAL_CARRIER_CLINICAL_CHARGE_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_LINE_CARR_CLNCL_CHRG_AMT",
+      "Carrier Clinical Charge Amount"),
+  LINE_PROFESSIONAL_THERAPY_LMT_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_LINE_CARR_PSYCH_OT_LMT_AMT",
+      "Therapy Amount Applied to Limit"),
+  LINE_PROFESSIONAL_INTEREST_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_LINE_PRFNL_INTRST_AMT",
+      "Professional Interest Amount"),
+  LINE_PROFESSIONAL_PRIMARY_PAYER_ALLOWED_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_MDCR_PRMRY_PYR_ALOWD_AMT",
+      "Line Primary Payer Allowed Amount"),
+  LINE_INSTITUTIONAL_TRANSITIONAL_DRG_ADD_ON_PAYMENT_ADJUSTMENT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_REV_CNTR_TDAPA_AMT",
+      "Transitional Drug Add-On Payment Adjustment"),
+  LINE_RX_GROSS_COVERED_COST_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_LINE_GRS_CVRD_CST_TOT_AMT",
+      "Claim Line Gross Covered Cost Amount"),
+  LINE_NONCOVERED_PRODUCT_PAID_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_LINE_NCVRD_PD_AMT",
+      "Amount Paid for Noncovered Product or Service"),
+  LINE_OTHER_THIRD_PARTY_PAID_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_LINE_OTHR_TP_PD_AMT",
+      "Other Third Party Paid Amount"),
+  LINE_RX_MANUFACTURER_DISCOUNT_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_CMS_CALCD_MFTR_DSCNT_AMT",
+      "CMS Calculated Manufacturer Discount Amount"),
+  LINE_RX_REBATE_PASSTHROUGH_POS_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_LINE_REBT_PASSTHRU_POS_AMT",
+      "Rebate Passthrough POS Amount"),
+  LINE_RX_PRICE_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_PHRMCY_PRICE_DSCNT_AT_POS_AMT",
+      "Pharmacy price discount"),
+  LINE_PROVIDER_OBLIGATION_FULL_AMOUNT(
+      SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION,
+      "CLM_LINE_OTAF_AMT",
+      "Provider Obligation To Accept as Full Amount"),
+  LINE_PROFESSIONAL_PRIMARY_PAYER_PAID_AMOUNT(
+      SystemUrls.CARIN_CODE_SYSTEM_ADJUDICATION,
+      "priorpayerpaid",
+      "Prior payer paid",
+      "CLM_BENE_PRMRY_PYR_PD_AMT",
+      "Line Primary Payer Paid Amount"),
+  LINE_PROFESSIONAL_SCREEN_SAVINGS_AMOUNT(
+      SystemUrls.CARIN_CODE_SYSTEM_ADJUDICATION,
+      "discount",
+      "Discount",
+      "CLM_LINE_DMERC_SCRN_SVGS_AMT",
+      "Screen Savings Amount"),
+  LINE_PROFESSIONAL_PURCHASE_PRICE_AMOUNT(
+      SystemUrls.HL7_ADJUDICATION,
+      "eligible",
+      "Eligible Amount",
+      "CLM_LINE_PRFNL_DME_PRICE_AMT",
+      "Purchase Price Amount");
 
   private final String coding1System;
   private final String coding1Code;
@@ -162,37 +438,52 @@ enum AdjudicationChargeType {
     this(coding1System, coding1Code, coding1Display, "", "");
   }
 
+  private CodeableConcept buildCategory() {
+    var category =
+        new CodeableConcept()
+            .addCoding(
+                new Coding()
+                    .setSystem(coding1System)
+                    .setCode(coding1Code)
+                    .setDisplay(coding1Display));
+    if (!coding2Code.isBlank()) {
+      category.addCoding(
+          new Coding()
+              .setSystem(SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION)
+              .setCode(coding2Code)
+              .setDisplay(coding2Display));
+    }
+    return category;
+  }
+
   ExplanationOfBenefit.AdjudicationComponent toFhirAdjudication(double value) {
     return new ExplanationOfBenefit.AdjudicationComponent()
-        .setCategory(
-            new CodeableConcept()
-                .addCoding(
-                    new Coding()
-                        .setSystem(coding1System)
-                        .setCode(coding1Code)
-                        .setDisplay(coding1Display))
-                .addCoding(
-                    new Coding()
-                        .setSystem(SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION)
-                        .setCode(coding2Code)
-                        .setDisplay(coding2Display)))
+        .setCategory(buildCategory())
         .setAmount(USD.toFhir(value));
+  }
+
+  ExplanationOfBenefit.AdjudicationComponent toFhirAdjudicationUnsignedType(int value) {
+    return new ExplanationOfBenefit.AdjudicationComponent()
+        .setCategory(buildCategory())
+        .setValue(Integer.toUnsignedLong(value));
+  }
+
+  ExplanationOfBenefit.AdjudicationComponent toFhirAdjudicationUnsignedType(long value) {
+
+    return new ExplanationOfBenefit.AdjudicationComponent()
+        .setCategory(buildCategory())
+        .setValue(value);
+  }
+
+  ExplanationOfBenefit.AdjudicationComponent toFhirAdjudicationDecimalType(double value) {
+    return new ExplanationOfBenefit.AdjudicationComponent()
+        .setCategory(buildCategory())
+        .setValue(value);
   }
 
   ExplanationOfBenefit.TotalComponent toFhirTotal(double value) {
     return new ExplanationOfBenefit.TotalComponent()
-        .setCategory(
-            new CodeableConcept()
-                .addCoding(
-                    new Coding()
-                        .setSystem(coding1System)
-                        .setCode(coding1Code)
-                        .setDisplay(coding1Display))
-                .addCoding(
-                    new Coding()
-                        .setSystem(SystemUrls.BLUE_BUTTON_CODE_SYSTEM_ADJUDICATION)
-                        .setCode(coding2Code)
-                        .setDisplay(coding2Display)))
+        .setCategory(buildCategory())
         .setAmount(USD.toFhir(value));
   }
 }
