af072cdbb710ad8a5b7a6896bd9b0290bda7cfd3
Michael J Burling <michael.burling@cms.hhs.gov>
2026-02-09 14:35:10 -0600

    Add labels for EMF metrics, prometheus export

--- ops/services/04-server/service.tf
+++ ops/services/04-server/service.tf
@@ -288,6 +288,10 @@ resource "aws_ecs_task_definition" "server" {
         volumesFrom    = []
       },
       {
+        dockerLabels = {
+          Java_EMF_Metrics             = "true"
+          ECS_PROMETHEUS_EXPORTER_PORT = local.server_jmx_export_port
+        }
         name      = local.service
         image     = data.aws_ecr_image.server.image_uri
         essential = true
