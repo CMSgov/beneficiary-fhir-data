5ff9186e5e914610b21f761515ab42a850c28572
Michael J Burling <michael.burling@cms.hhs.gov>
2026-02-09 16:14:28 -0600

    Correct cluster name

--- ops/services/04-server/service.tf
+++ ops/services/04-server/service.tf
@@ -227,9 +227,8 @@ resource "aws_ecs_task_definition" "server" {
             value = templatefile(
               "${path.module}/adot/collector.yaml", {
                 log_group_name = aws_cloudwatch_log_group.adot_metrics.name
-                cluster_name   = data.aws_ecs_cluster.main.name
+                cluster_name   = data.aws_ecs_cluster.main.cluster_name
                 region         = local.region
-
             })
           }
         ]
