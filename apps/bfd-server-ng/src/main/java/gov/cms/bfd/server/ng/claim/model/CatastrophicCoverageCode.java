fcb267d5dad491bcfafc18245d991df394cf15a3
Allen Wright <allen.wright@icf.com>
2026-02-23 11:45:45 -0800

    lint fixes

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/CatastrophicCoverageCode.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/CatastrophicCoverageCode.java
@@ -3,7 +3,6 @@ package gov.cms.bfd.server.ng.claim.model;
 import gov.cms.bfd.server.ng.util.SystemUrls;
 import java.util.Arrays;
 import java.util.Optional;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.hl7.fhir.r4.model.CodeableConcept;
