9f73449548551299e4c316701135dee44ac2a3ff
Allen Wright <allen.wright@icf.com>
2026-02-23 11:35:30 -0800

    changes to use invalid enum

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/CatastrophicCoverageCode.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/CatastrophicCoverageCode.java
@@ -3,23 +3,26 @@ package gov.cms.bfd.server.ng.claim.model;
 import gov.cms.bfd.server.ng.util.SystemUrls;
 import java.util.Arrays;
 import java.util.Optional;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.ExplanationOfBenefit;
 
 /** Catastrophic coverage codes. */
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 public enum CatastrophicCoverageCode {
   /** A - Attachment point met on this event. */
   A("A", "Attachment point met on this event"),
   /** C - Above attachment point. */
-  C("C", "Above attachment point");
+  C("C", "Above attachment point"),
+  /** INVALID - Represents an invalid code that we still want to capture. */
+  INVALID("", "");
 
   private final String code;
   private final String display;
+  private String invalidValue;
 
   /**
    * Convert from a database code.
@@ -28,7 +31,19 @@ public enum CatastrophicCoverageCode {
    * @return catastrophic coverage code
    */
   public static Optional<CatastrophicCoverageCode> tryFromCode(String code) {
-    return Arrays.stream(values()).filter(v -> v.code.equals(code)).findFirst();
+    return Optional.of(Arrays.stream(values()).filter(v -> v.code.equals(code)).findFirst().orElse(captureInvalidValue(code)));
+  }
+
+  /**
+   * Captures an invalid value and maps it to the INVALID enum.
+   *
+   * @param invalidValue the invalid value to capture
+   * @return INVALID catastrophic coverage code
+   */
+  public static CatastrophicCoverageCode captureInvalidValue(String invalidValue) {
+    var invalidCatastrophicCoverageCode = CatastrophicCoverageCode.INVALID;
+    invalidCatastrophicCoverageCode.invalidValue = invalidValue;
+    return invalidCatastrophicCoverageCode;
   }
 
   ExplanationOfBenefit.SupportingInformationComponent toFhir(
