f66e330561d4738a05fbb5cb947b423c62d8b58e
jonathan-cantu-icf <jonathan.cantu@icf.com>
2026-02-24 09:43:56 -0500

    PR review recommended changes.

--- ops/services/02-bene-prefs/main.tf
+++ ops/services/02-bene-prefs/main.tf
@@ -18,25 +18,14 @@ locals {
   service        = "beneficiary-prefs"
   target_service = "beneprefs"
 
-  region                   = module.terraservice.region
-  account_id               = module.terraservice.account_id
-  default_tags             = module.terraservice.default_tags
-  env                      = module.terraservice.env
-  is_ephemeral_env         = module.terraservice.is_ephemeral_env
-  bfd_version              = module.terraservice.bfd_version
-  ssm_config               = nonsensitive(module.terraservice.ssm_config)
-  env_key_alias            = module.terraservice.env_key_alias
-  env_key_arn              = module.terraservice.env_key_arn
-  iam_path                 = module.terraservice.default_iam_path
-  permissions_boundary_arn = module.terraservice.default_permissions_boundary_arn
-  vpc                      = module.terraservice.vpc
-  azs                      = keys(module.terraservice.default_azs)
+  default_tags = module.terraservice.default_tags
+  env          = module.terraservice.env
+  env_key_arn  = module.terraservice.env_key_arn
 
   name_prefix = "bfd-${local.env}-${local.service}"
   partners    = toset(["bcda", "ab2d", "dpc"])
 }
 
-# Create the buckets
 module "eft_bucket" {
   for_each = local.partners
   source   = "../../terraform-modules/general/secure-bucket"
@@ -45,12 +34,9 @@ module "eft_bucket" {
   bucket_kms_key_arn = local.env_key_arn
   force_destroy      = false
 
-  tags = {
-    Partner = each.value
-  }
-
   ssm_param_name = "/bfd/${local.env}/${local.service}/${each.value}/nonsensitive/bucket"
 
+  tags = { Partner = each.value }
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "this" {
