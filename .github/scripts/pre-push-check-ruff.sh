924390bd213826a985f61939a5b6f481408d0321
Mark Lewis <mark.lewis@icf.com>
2026-02-25 09:21:45 -0500

    Add python checks for pre-push to match pre-commit for local pushes

---
+++ .github/scripts/pre-push-check-ruff.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+checkPythonRuff() {
+  if [ "$(which pyright)" ]; then
+    echo 'Verifying Python source code format...'
+    branch_name="$(git rev-parse --abbrev-ref HEAD 2>&1)"
+
+    git fetch origin 2>&1
+    doesRemoteExist="$(git ls-remote origin ${branch_name})"
+
+    tmpfile=$(mktemp)
+    if [ "$doesRemoteExist" = '' ]; then
+      echo 'No Remote exists, checking apps directory on local for changes to Python files.'
+      git diff --name-only origin/master..HEAD apps | grep .py$ > "$tmpfile"
+    else
+      echo "Remote exists, checking apps directory on branch: ${branch_name} for changes to Python files"
+      git diff --name-only "origin/${branch_name}:apps" "${branch_name}:apps" | grep .py$ > "$tmpfile"
+    fi
+
+    commits=$(cat "$tmpfile")
+    rm "$tmpfile"
+    for file in $commits
+    do
+      if ! ruff check "$file"; then
+        echo "Please fix errors before continuing."
+        exit 1
+      fi
+    done
+  else
+    echo 'Please install pyright before committing.'
+    exit 1
+  fi
+}
+
+checkPythonRuff
--- .github/scripts/pre-push.sh
+++ .github/scripts/pre-push.sh
@@ -1,36 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-checkJavaFormat() {
-  echo 'Verifying Java source code format...'
-  branch_name="$(git rev-parse --abbrev-ref HEAD 2>&1)"
-
-  git fetch origin 2>&1
-  localChanges=''
-  doesRemoteExist="$(git ls-remote origin ${branch_name})"
-
-  if [ "$doesRemoteExist" = '' ]; then
-    echo 'No Remote exists, checking apps directory on local for changes.'
-    localChanges="$(git log origin/master..HEAD apps)"
-  else
-     localChanges="$(git diff --name-only "origin/${branch_name}:apps" "${branch_name}:apps" 2>&1)"
-  fi
-
-  echo "Verifying Java source code format for branch: ${branch_name} ..."
-  if [ "$localChanges" = '' ]; then
-    echo 'No changes detected. Exiting.'
-  else
-    set +e
-
-    echo 'Relevant app-directory changes detected. Running fmt plugin check.'
-    if mvn -f apps/ com.spotify.fmt:fmt-maven-plugin:check --threads 1C >/dev/null 2>&1; then
-      echo 'Verified Java source code format: a-okay.'
-    else
-      echo "Inconsistencies discovered in formatting check. Run 'mvn com.spotify.fmt:fmt-maven-plugin:check --threads 1C' for details or 'mvn com.spotify.fmt:fmt-maven-plugin:format' to automatically apply the required formatting."
-      return 1
-    fi
-  fi
-  return 0
-}
-
-checkJavaFormat
+./pre-push-check-java.sh
+./pre-push-check-pyright.sh
+./pre-push-check-ruff.sh
