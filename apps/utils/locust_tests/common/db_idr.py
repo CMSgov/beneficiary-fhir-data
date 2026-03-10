b6b3807940fb8b0ae9388d9fc9957394fbfd28eb
Mitchell Alessio <5306896+malessi@users.noreply.github.com>
2026-03-10 14:53:33 -0400

    BFD-4552: Update performance and SAMHSA tests for new schema (#3015)

--- apps/utils/locust_tests/common/db_idr.py
+++ apps/utils/locust_tests/common/db_idr.py
@@ -37,7 +37,7 @@ def get_regression_bene_sks(uri: str, table_sample_pct: float | None = None) -> 
     Returns:
         List[str]: A list of synthetic beneficiary IDs used for the regression suites
     """
-    bene_query = "SELECT bene_sk FROM idr.beneficiary limit 1000"
+    bene_query = "SELECT bene_sk FROM idr.valid_beneficiary limit 1000"
     return [str(r[0]) for r in _execute(uri, bene_query)]
 
 
@@ -55,7 +55,7 @@ def get_regression_current_part_a_bene_sks(
     """
     bene_query = """
         SELECT b. bene_sk
-        FROM idr.beneficiary b
+        FROM idr.valid_beneficiary b
         join idr.beneficiary_third_party tp on b.bene_sk = tp.bene_sk
         where b. bene_sk = b. bene_xref_efctv_sk and tp.bene_tp_type_cd  = 'A'
         limit 1000
@@ -77,7 +77,7 @@ def get_regression_current_part_b_bene_sks(
     """
     bene_query = """
         SELECT b. bene_sk
-        FROM idr.beneficiary b
+        FROM idr.valid_beneficiary b
         join idr.beneficiary_third_party tp on b.bene_sk = tp.bene_sk
         where b. bene_sk = b. bene_xref_efctv_sk and tp.bene_tp_type_cd  = 'B' limit 1000
     """
@@ -93,7 +93,7 @@ def get_regression_bene_mbis(uri: str, table_sample_pct: float | None = None) ->
     Returns:
         List[str]: A list of synthetic beneficiary IDs used for the regression suites
     """
-    bene_query = "SELECT bene_mbi_id FROM idr.beneficiary limit 1000"
+    bene_query = "SELECT bene_mbi_id FROM idr.valid_beneficiary limit 1000"
     return [str(r[0]) for r in _execute(uri, bene_query)]
 
 
@@ -106,5 +106,58 @@ def get_regression_claim_ids(uri: str, table_sample_pct: float | None = None) ->
     Returns:
         List[str]: A list of synthetic beneficiary IDs used for the regression suites
     """
-    claim_query = "SELECT clm_uniq_id from idr.claim limit 1000"
-    return [str(r[0]) for r in _execute(uri, claim_query)]
+    # TODO: Update this with latest psycopg templated strings to reduce duplication
+    claim_query = """
+    SELECT clm_uniq_id FROM (
+        SELECT clm_uniq_id FROM idr.claim_rx
+        JOIN idr.valid_beneficiary
+            ON claim_rx.bene_sk = valid_beneficiary.bene_sk
+        WHERE idr_ltst_trans_flg = 'Y'
+        LIMIT 200
+        ) t1
+    UNION ALL
+    SELECT clm_uniq_id FROM (
+            SELECT DISTINCT claim_institutional_nch.clm_uniq_id FROM idr.claim_institutional_nch
+            JOIN idr.valid_beneficiary
+                ON claim_institutional_nch.bene_sk = valid_beneficiary.bene_sk
+            WHERE EXISTS (
+                SELECT 1 FROM idr.claim_item_institutional_nch
+                WHERE claim_item_institutional_nch.clm_uniq_id = claim_institutional_nch.clm_uniq_id
+            ) AND idr_ltst_trans_flg = 'Y'
+            LIMIT 200
+        ) t2
+    UNION ALL
+    SELECT clm_uniq_id FROM (
+            SELECT DISTINCT claim_institutional_ss.clm_uniq_id FROM idr.claim_institutional_ss
+            JOIN idr.valid_beneficiary
+                ON claim_institutional_ss.bene_sk = valid_beneficiary.bene_sk
+            WHERE EXISTS (
+                SELECT 1 FROM idr.claim_item_institutional_ss
+                WHERE claim_item_institutional_ss.clm_uniq_id = claim_institutional_ss.clm_uniq_id
+            ) AND idr_ltst_trans_flg = 'Y'
+            LIMIT 200
+        ) t3
+    UNION ALL
+    SELECT clm_uniq_id FROM (
+            SELECT DISTINCT claim_professional_nch.clm_uniq_id FROM idr.claim_professional_nch
+            JOIN idr.valid_beneficiary
+                ON claim_professional_nch.bene_sk = valid_beneficiary.bene_sk
+            WHERE EXISTS (
+                SELECT 1 FROM idr.claim_item_professional_nch
+                WHERE claim_item_professional_nch.clm_uniq_id = claim_professional_nch.clm_uniq_id
+            ) AND idr_ltst_trans_flg = 'Y'
+            LIMIT 200
+        ) t4
+    UNION ALL
+    SELECT clm_uniq_id FROM (
+            SELECT DISTINCT claim_professional_ss.clm_uniq_id FROM idr.claim_professional_ss
+            JOIN idr.valid_beneficiary
+                ON claim_professional_ss.bene_sk = valid_beneficiary.bene_sk
+            WHERE EXISTS (
+                SELECT 1 FROM idr.claim_item_professional_ss
+                WHERE claim_item_professional_ss.clm_uniq_id = claim_professional_ss.clm_uniq_id
+            ) AND idr_ltst_trans_flg = 'Y'
+            LIMIT 200
+        ) t5;
+    """
+    return [str(r[0]) for r in set(x for x in _execute(uri, claim_query))]
