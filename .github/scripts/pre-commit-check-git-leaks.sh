03269e7428f37682b0c3f4e10b557f996e73d88f
Mark Lewis <mark.lewis@icf.com>
2026-03-17 11:01:24 -0400

    Update sh file to test check

--- .github/scripts/pre-commit-check-git-leaks.sh
+++ .github/scripts/pre-commit-check-git-leaks.sh
@@ -2,7 +2,7 @@
 set -e
 
 checkGitleaks() {
-  echo "Attempting to execute gitleaks. This may take a minute..."
+  echo "Attempting to execute gitleaks. This may take a minute...."
   if ! command -v gitleaks >/dev/null; then
     echo "'gitleaks' not found. Install gitleaks before pushing your changes."
     return 1
