d3abd68fa2daa3ff95b5767678b1e7fd87b8313b
Mitch Alessio <mitch.alessio@forpeople.us>
2026-03-24 16:24:44 -0400

    Insert message data into database

--- ops/services/04-idr-pipeline/lambda_src/consume-idr-events/app/main.py
+++ ops/services/04-idr-pipeline/lambda_src/consume-idr-events/app/main.py
@@ -1,9 +1,11 @@
-from datetime import datetime
 import json
 import os
+from datetime import datetime
 from typing import Annotated, Any
 
+import psycopg
 from aws_lambda_powertools import Logger
+from aws_lambda_powertools.utilities import parameters
 from aws_lambda_powertools.utilities.data_classes import SQSEvent
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from pydantic.fields import Field
@@ -12,7 +14,10 @@ from pydantic.main import BaseModel
 BFD_ENVIRONMENT = os.environ.get("BFD_ENVIRONMENT", "")
 DB_ENDPOINT = os.environ.get("DB_ENDPOINT", default="")
 
+IDR_LOAD_EVENTS_TABLE = "idr.load_events"
+
 logger = Logger()
+
 
 class IdrLoadEventModel(BaseModel):
     id: str
@@ -20,8 +25,45 @@ class IdrLoadEventModel(BaseModel):
     message: Annotated[str, Field(validation_alias="jobMessage")]
     event_time: datetime
 
+
 def insert_event(event: IdrLoadEventModel) -> None:
-    pass
+    db_username = parameters.get_parameter(
+        f"/bfd/{BFD_ENVIRONMENT}/idr-pipeline/sensitive/db/username", decrypt=True
+    )
+    db_password = parameters.get_parameter(
+        f"/bfd/{BFD_ENVIRONMENT}/idr-pipeline/sensitive/db/password", decrypt=True
+    )
+    with (
+        psycopg.connect(
+            host=DB_ENDPOINT,
+            user=db_username,
+            password=db_password,
+            port=5432,
+            dbname="idr",
+        ) as conn,
+        conn.cursor() as curs,
+    ):
+        logger.info("Connected to %s", DB_ENDPOINT)
+
+        curs.execute(t"""
+            INSERT INTO {IDR_LOAD_EVENTS_TABLE}(
+                id,
+                job,
+                message,
+                event_time
+            )
+            VALUES(
+                {event.id},
+                {event.job},
+                {event.message},
+                {event.event_time}
+            )
+            """)
+
+        logger.info(
+            "Inserted '%s' into %s successfully", event.model_dump_json(), IDR_LOAD_EVENTS_TABLE
+        )
+
 
 @logger.inject_lambda_context(clear_state=True, log_event=True)
 def handler(event: dict[str, Any], context: LambdaContext) -> None:  # noqa: ARG001
