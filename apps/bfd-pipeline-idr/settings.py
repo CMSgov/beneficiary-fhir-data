06057a36f64e6bb1531d76f2ebd6813edb909ae5
Mitch Alessio <mitch.alessio@forpeople.us>
2026-03-26 16:20:22 -0400

    Use docstrings for settings constants

--- apps/bfd-pipeline-idr/settings.py
+++ apps/bfd-pipeline-idr/settings.py
@@ -11,57 +11,57 @@ def _parse_bool_default_true(var_name: str) -> bool:
     return getenv(var_name, "1").lower() not in ("0", "false")
 
 
-# Enables partitioning claims data based on dates.
-# It's useful to disable this for synthetic loads since
-# the smaller volume of data means this will probably be much slower
 ENABLE_DATE_PARTITIONS = _parse_bool_default_true("IDR_ENABLE_DATE_PARTITIONS")
+"""Enables partitioning claims data based on dates.
+It's useful to disable this for synthetic loads since
+the smaller volume of data means this will probably be much slower"""
 
-# Minimum claim date to load for NCH (and DDPS).
-# Any claims created before this date will be skipped.
-# Useful for partial loads with large amounts of data.
 MIN_CLAIM_NCH_TRANSACTION_DATE = getenv("IDR_MIN_CLAIM_NCH_TRANSACTION_DATE", MIN_CLAIM_LOAD_DATE)
+"""Minimum claim date to load for NCH (and DDPS).
+Any claims created before this date will be skipped.
+Useful for partial loads with large amounts of data."""
 
-# Minimum claim date to load for shared systems.
-# Any claims created before this date will be skipped.
-# Useful for partial loads with large amounts of data.
 MIN_CLAIM_SS_TRANSACTION_DATE = getenv("IDR_MIN_CLAIM_SS_TRANSACTION_DATE", MIN_CLAIM_LOAD_DATE)
+"""Minimum claim date to load for shared systems.
+Any claims created before this date will be skipped.
+Useful for partial loads with large amounts of data."""
 
-# Partition type - year/month/day
-# This should be set to "day" in prod to reduce the batch sizes
 PARTITION_TYPE = getenv("IDR_PARTITION_TYPE", "year").lower()
+"""Partition type (year/month/day).
+This should be set to "day" in prod to reduce the batch sizes"""
 
-# Only pull in latest claims
-# Useful for the initial data pull since we only want to pull in
-# the latest version of each claim.
 LATEST_CLAIMS = _parse_bool_default_false("IDR_LATEST_CLAIMS")
+"""Only pull in latest claims.
+Useful for the initial data pull since we only want to pull in
+the latest version of each claim."""
 
-# Load type - initial (first load) or incremental (adding on to an existing load)
-# The load type affects the shape of the DAG.
-# Only useful for prod data and testing.
 LOAD_TYPE = getenv("IDR_LOAD_TYPE", "incremental")
+"""Load type - initial (first load) or incremental (adding on to an existing load).
+The load type affects the shape of the DAG.
+Only useful for prod data and testing."""
 
-# Batch sizes are calculated based on the number of columns in the table
-# in order to keep memory usage stable relative to the number of concurrent tasks.
-# Change this to increase or decrease the number of rows loaded per batch.
-# Increasing this means the memory per task will also increase and
-# you will likely need to decrease the number of concurrent tasks
-# to prevent the server from running out of memory.
 BATCH_MULTIPLIER = int(getenv("IDR_BATCH_MULTIPLIER", "2_000_000"))
+"""Batch sizes are calculated based on the number of columns in the table
+in order to keep memory usage stable relative to the number of concurrent tasks.
+Change this to increase or decrease the number of rows loaded per batch.
+Increasing this means the memory per task will also increase and
+you will likely need to decrease the number of concurrent tasks
+to prevent the server from running out of memory."""
 
-# Minimum batch completion date to process
-# This is useful if you've already loaded some data and you do not want to reprocess
-# any batches that have already completed before this date.
 MIN_BATCH_COMPLETION_DATE = getenv("IDR_MIN_BATCH_COMPLETION_DATE")
+"""Minimum batch completion date to process
+This is useful if you've already loaded some data and you do not want to reprocess
+any batches that have already completed before this date."""
 
-# Maximum concurrent tasks to run.
-# Changing this has a drastic effect on the runtime.
-# In prod, we want to run as many tasks as possible without running out of memory.
 MAX_TASKS = int(getenv("IDR_MAX_TASKS", "32"))
+"""Maximum concurrent tasks to run.
+Changing this has a drastic effect on the runtime.
+In prod, we want to run as many tasks as possible without running out of memory."""
 
-# List of tables to include - any table not included will be skipped.
-# Useful if you only want to load a subset of data and don't want to wait
-# for the other tables to load.
 TABLES_TO_LOAD = [t for t in getenv("IDR_TABLES", "").split(",") if t]
+"""List of tables to include - any table not included will be skipped.
+Useful if you only want to load a subset of data and don't want to wait
+for the other tables to load."""
 
 # IDR credentials, these are pulled from SSM in prod.
 # You likely don't want to touch these otherwise.
