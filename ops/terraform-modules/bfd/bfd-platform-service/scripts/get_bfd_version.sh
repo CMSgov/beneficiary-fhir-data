617a23482785dd2927b6ed85f84fdfbbbc7e4fe5
Mitch Alessio <5306896+malessi@users.noreply.github.com>
2026-04-20 14:39:02 -0400

    Centralize get_bfd_version.sh

--- ops/terraform-modules/bfd/bfd-platform-service/scripts/get_bfd_version.sh
+++ ops/terraform-modules/bfd/bfd-platform-service/scripts/get_bfd_version.sh
@@ -1,8 +1,1 @@
-#!/usr/bin/env bash
-
-set -Eeou pipefail
-
-# Get the latest tag on the current branch that roughly matches the current release format
-bfd_version="$(git describe --abbrev=0 --tags --match='2.*.*' "$(git rev-list --tags --max-count=1)")"
-
-echo "{\"bfd_version\": \"$bfd_version\"}"
+../../../../../apps/utils/scripts/get_bfd_version.sh
