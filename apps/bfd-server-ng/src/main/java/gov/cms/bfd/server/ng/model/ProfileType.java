d302025af236f6749f046e24185ae6ba8479a958
alex-dzeda <120701369+alex-dzeda@users.noreply.github.com>
2026-04-27 10:29:53 -0500

    BFD-4648: Update BFD v3 to C4BB 2.2.0 (#3112)

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/model/ProfileType.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/model/ProfileType.java
@@ -11,8 +11,8 @@ import lombok.RequiredArgsConstructor;
 public enum ProfileType {
   /** C4BB - Carin Blue Button. */
   C4BB(
-      List.of(SystemUrls.PROFILE_C4BB_PATIENT_2_1_0, SystemUrls.PROFILE_US_CORE_PATIENT_6_1_0),
-      List.of(SystemUrls.PROFILE_C4BB_COVERAGE_2_1_0, SystemUrls.PROFILE_US_CORE_COVERAGE_6_1_0)),
+      List.of(SystemUrls.PROFILE_C4BB_PATIENT_2_2_0, SystemUrls.PROFILE_US_CORE_PATIENT_7_0_0),
+      List.of(SystemUrls.PROFILE_C4BB_COVERAGE_2_2_0, SystemUrls.PROFILE_US_CORE_COVERAGE_7_0_0)),
   /** C4DIC - Carin Digital Insurance Card. */
   C4DIC(
       List.of(SystemUrls.PROFILE_C4DIC_PATIENT_1_1_0),
