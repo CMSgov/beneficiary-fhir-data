df2738ad2a233c48bd55685fdc04bc6fbf5dacc6
Melissa Guzman <melissa.guzman@forpeople.us>
2026-05-12 16:58:51 -0700

    add publishing metrics permission and configure cw meter registry to export limited metrics

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/Application.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/Application.java
@@ -3,11 +3,6 @@ package gov.cms.bfd.server.ng;
 import ca.uhn.fhir.rest.server.RestfulServer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.cms.bfd.server.ng.log.AuditLogger;
-import io.micrometer.common.annotation.ValueResolver;
-import io.micrometer.core.aop.MeterTagAnnotationHandler;
-import io.micrometer.core.aop.TimedAspect;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.java21.instrument.binder.jdk.VirtualThreadMetrics;
 import jakarta.servlet.Servlet;
 import java.time.Clock;
 import javax.sql.DataSource;
@@ -63,39 +58,6 @@ public class Application {
     servletRegistrationBean.addUrlMappings("/v3/fhir/*");
 
     return servletRegistrationBean;
-  }
-
-  /**
-   * Configures Micrometer to support @MeterTag annotations with SPEL expressions.
-   *
-   * <p>IMPORTANT: @Timed annotations only work on public methods for classes that are registered
-   * beans. Attempting to use this for private methods or non-bean classes will not work since it
-   * relies on Spring AOP's proxies.
-   *
-   * @param registry meter registry
-   * @return timed aspect
-   */
-  @Bean
-  public TimedAspect timedAspect(MeterRegistry registry) {
-    var timedAspect = new TimedAspect(registry);
-    ValueResolver valueResolver = Object::toString;
-    var valueExpressionResolver = new SpelValueExpressionResolver();
-    timedAspect.setMeterTagAnnotationHandler(
-        new MeterTagAnnotationHandler(aClass -> valueResolver, aClass -> valueExpressionResolver));
-    return timedAspect;
-  }
-
-  /**
-   * Configures Micrometer to collect virtual thread metrics.
-   *
-   * @param registry meter registry
-   * @return virtual thread metrics
-   */
-  @Bean
-  public VirtualThreadMetrics virtualThreadMetrics(MeterRegistry registry) {
-    VirtualThreadMetrics metrics = new VirtualThreadMetrics();
-    metrics.bindTo(registry);
-    return metrics;
   }
 
   /**
---
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/MetricsConfiguration.java
@@ -0,0 +1,134 @@
+package gov.cms.bfd.server.ng;
+
+import io.micrometer.cloudwatch2.CloudWatchConfig;
+import io.micrometer.cloudwatch2.CloudWatchMeterRegistry;
+import io.micrometer.common.annotation.ValueResolver;
+import io.micrometer.core.aop.MeterTagAnnotationHandler;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.java21.instrument.binder.jdk.VirtualThreadMetrics;
+import java.time.Duration;
+import java.util.Set;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
+
+/** Configuration for Micrometer metrics integration. */
+@Configuration
+public class MetricsConfiguration {
+
+  /** List of metric names that are allowed to be published to Cloudwatch by Micrometer. */
+  public static final Set<String> MICROMETER_ALLOWED_METRIC_PREFIX_NAMES =
+      Set.of("application.", "jvm.threads.virtual.", "http.server.requests.", "executor.");
+
+  /**
+   * Configures Micrometer to support @MeterTag annotations with SPEL expressions.
+   *
+   * <p>IMPORTANT: @Timed annotations only work on public methods for classes that are registered
+   * beans. Attempting to use this for private methods or non-bean classes will not work since it
+   * relies on Spring AOP's proxies.
+   *
+   * @param registry meter registry
+   * @return timed aspect
+   */
+  @Bean
+  public TimedAspect timedAspect(MeterRegistry registry) {
+    var timedAspect = new TimedAspect(registry);
+    ValueResolver valueResolver = Object::toString;
+    var valueExpressionResolver = new SpelValueExpressionResolver();
+    timedAspect.setMeterTagAnnotationHandler(
+        new MeterTagAnnotationHandler(aClass -> valueResolver, aClass -> valueExpressionResolver));
+    return timedAspect;
+  }
+
+  /**
+   * Configures Micrometer to collect virtual thread metrics.
+   *
+   * @param registry meter registry
+   * @return virtual thread metrics
+   */
+  @Bean
+  public VirtualThreadMetrics virtualThreadMetrics(MeterRegistry registry) {
+    var metrics = new VirtualThreadMetrics();
+    metrics.bindTo(registry);
+    return metrics;
+  }
+
+  /**
+   * Configures Micrometer CloudWatch metric export properties. Currently, does not override
+   * application.properties.
+   *
+   * @return CloudWatch configuration
+   */
+  @Bean
+  @ConditionalOnProperty(
+      prefix = "management.metrics.export.cloudwatch",
+      name = "enabled",
+      havingValue = "true")
+  public CloudWatchConfig cloudWatchConfig() {
+    return new CloudWatchConfig() {
+
+      @Override
+      public @Nullable String get(String key) {
+        return null;
+      }
+
+      @Override
+      public String namespace() {
+        return "bfd-v3";
+      }
+
+      @Override
+      public Duration step() {
+        return Duration.ofMinutes(1);
+      }
+    };
+  }
+
+  /**
+   * Configures the asynchronous CloudWatch client used for publishing metrics.
+   *
+   * @return CloudWatch async client
+   */
+  @Bean
+  @ConditionalOnProperty(
+      prefix = "management.metrics.export.cloudwatch",
+      name = "enabled",
+      havingValue = "true")
+  public CloudWatchAsyncClient cloudWatchAsyncClient() {
+    return CloudWatchAsyncClient.create();
+  }
+
+  /**
+   * Creates the CloudWatch meter registry for publishing Micrometer metrics.
+   *
+   * @param cloudWatchConfig CloudWatch configuration
+   * @param cloudWatchAsyncClient CloudWatch async client
+   * @return CloudWatch meter registry
+   */
+  @Bean
+  @ConditionalOnProperty(
+      prefix = "management.metrics.export.cloudwatch",
+      name = "enabled",
+      havingValue = "true")
+  public CloudWatchMeterRegistry cloudWatchMeterRegistry(
+      CloudWatchConfig cloudWatchConfig, CloudWatchAsyncClient cloudWatchAsyncClient) {
+    final var micrometerClock = io.micrometer.core.instrument.Clock.SYSTEM;
+    var registry =
+        new CloudWatchMeterRegistry(cloudWatchConfig, micrometerClock, cloudWatchAsyncClient);
+
+    registry
+        .config()
+        .meterFilter(
+            MeterFilter.denyUnless(
+                id -> {
+                  var name = id.getName();
+                  return MICROMETER_ALLOWED_METRIC_PREFIX_NAMES.stream().anyMatch(name::startsWith);
+                }));
+
+    return registry;
+  }
+}
