3a2dc777cd676c8993a85b5a9e3c6179d77dea82
clewellyn-nava <connorlewellyn@navapbc.com>
2026-06-18 10:23:07 -0400

    Automatic changes

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/fhirtrimmer/FhirTrimmer.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/fhirtrimmer/FhirTrimmer.java
@@ -1,38 +1,36 @@
 package gov.cms.bfd.server.ng.fhirtrimmer;
 
 import ca.uhn.fhir.context.FhirContext;
+import java.util.List;
+import java.util.Map;
 import org.hl7.fhir.r4.hapi.fluentpath.FhirPathR4;
 import org.hl7.fhir.r4.model.Resource;
 
-import java.util.List;
-import java.util.Map;
-
-/**
- * Final attempt at the barber shop.
- */
+/** Final attempt at the barber shop. */
 public class FhirTrimmer {
 
-    FhirContext fhirContext;
-    Map<BasisProfile, List<FhirPathR4>> profileMap;
+  FhirContext fhirContext;
+  Map<BasisProfile, List<FhirPathR4>> profileMap;
 
-    /**
-     * Constructor.
-     * @param context shared FhirContext
-     * @param profileMap the profile map generated on startup
-     */
-    public FhirTrimmer(FhirContext context, Map<BasisProfile, List<FhirPathR4>> profileMap){
-        this.fhirContext = context;
-        this.profileMap = profileMap;
-    }
+  /**
+   * Constructor.
+   *
+   * @param context shared FhirContext
+   * @param profileMap the profile map generated on startup
+   */
+  public FhirTrimmer(FhirContext context, Map<BasisProfile, List<FhirPathR4>> profileMap) {
+    this.fhirContext = context;
+    this.profileMap = profileMap;
+  }
 
-
-    /**
-     * Where the magic happens for the fourth or fifth time.
-     * @param base the base resource to be trimmed
-     * @param profile the profile to trim it to
-     * @return the trimmed resource
-     */
-    public Resource trim(Resource base, BasisProfile profile){
-        return base;
-    }
+  /**
+   * Where the magic happens for the fourth or fifth time.
+   *
+   * @param base the base resource to be trimmed
+   * @param profile the profile to trim it to
+   * @return the trimmed resource
+   */
+  public Resource trim(Resource base, BasisProfile profile) {
+    return base;
+  }
 }
