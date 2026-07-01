07c45c80c41fb8b13563b3572cfc4c76f272a34d
Josh Fisk <joshuafisk@navapbc.com>
2026-07-01 16:24:18 -0700

    Alter index creation to run concurrently

--- apps/bfd-db-migrator-ng/migrate.sh
+++ apps/bfd-db-migrator-ng/migrate.sh
@@ -48,5 +48,6 @@ db_endpoint="${DB_ENDPOINT:-"$env_db_endpoint"}"
         "-Dflyway.url=$db_endpoint" \
         "-Dflyway.user=$username" \
         "-Dflyway.password=$password" \
-        "-Dflyway.callbackLocations=filesystem:$SCRIPT_DIR/callbacks"
+        "-Dflyway.callbackLocations=filesystem:$SCRIPT_DIR/callbacks" \
+        "-Dflyway.postgresql.transactional.lock=false"
 )
--- apps/bfd-db-migrator-ng/migrations/V1_33_0__add_clm_idr_ld_dt_index.sql
+++ apps/bfd-db-migrator-ng/migrations/V1_33_0__add_clm_idr_ld_dt_index.sql
@@ -1,3 +1,3 @@
-CREATE INDEX ON idr.claim_professional_ss (clm_idr_ld_dt);
+CREATE INDEX CONCURRENTLY ON idr.claim_professional_ss (clm_idr_ld_dt);
 
-CREATE INDEX ON idr.claim_institutional_ss (clm_idr_ld_dt);
+CREATE INDEX CONCURRENTLY ON idr.claim_institutional_ss (clm_idr_ld_dt);
