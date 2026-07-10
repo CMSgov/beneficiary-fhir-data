777a02fa9bc189996f8ba55a5daa36f10f549fe6
Josh Fisk <joshuafisk@navapbc.com>
2026-07-10 15:37:44 -0700

    Fix migration

--- apps/bfd-db-migrator-ng/migrations/V1_33_0__add_clm_idr_ld_dt_index.sql
+++ apps/bfd-db-migrator-ng/migrations/V1_33_0__add_clm_idr_ld_dt_index.sql
@@ -1,3 +1,3 @@
-CREATE INDEX ON idr.claim_professional_ss(clm_type_cd, clm_idr_ld_dt) WHERE clm_type_cd BETWEEN 1000 and 1999
+CREATE INDEX ON idr.claim_professional_ss(clm_idr_ld_dt) WHERE clm_type_cd BETWEEN 1000 and 1999;
 
-CREATE INDEX ON idr.claim_institutional_ss(clm_type_cd, clm_idr_ld_dt) WHERE clm_type_cd BETWEEN 1000 and 1999
+CREATE INDEX ON idr.claim_institutional_ss(clm_idr_ld_dt) WHERE clm_type_cd BETWEEN 1000 and 1999;
