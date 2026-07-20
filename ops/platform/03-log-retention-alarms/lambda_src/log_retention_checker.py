3278a94c10d573eef3bee68d394b7e9dcdbf4222
marshallweekley-eng <marshallweekley@navapbc.com>
2026-07-20 17:10:14 -0500

    update to use powertools

--- ops/platform/03-log-retention-alarms/lambda_src/log_retention_checker.py
+++ ops/platform/03-log-retention-alarms/lambda_src/log_retention_checker.py
@@ -5,6 +5,8 @@ from typing import TYPE_CHECKING, Protocol, cast
 
 import boto3  # type: ignore[import-not-found]
 from botocore.config import Config# type: ignore[import-not-found]
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools.utilities.typing import LambdaContext
 
 if TYPE_CHECKING:
 	class CloudWatchLogsClient(Protocol):
