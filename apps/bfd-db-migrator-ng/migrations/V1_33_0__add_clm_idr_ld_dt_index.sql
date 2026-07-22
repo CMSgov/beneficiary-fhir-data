0ee7a463fd6473e9c3a315a2c52ab0f799865854
Josh Fisk <joshuafisk@navapbc.com>
2026-07-21 17:56:22 -0700

    Remove item migrations and rename file

--- apps/bfd-db-migrator-ng/migrations/V1_33_0__add_clm_idr_ld_dt_index.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX ON idr.claim_professional_ss(bfd_updated_ts) WHERE clm_type_cd BETWEEN 1000 and 1999;
-CREATE INDEX ON idr.claim_item_professional_ss(clm_uniq_id, bfd_updated_ts);
-
-CREATE INDEX ON idr.claim_institutional_ss(bfd_updated_ts) WHERE clm_type_cd BETWEEN 1000 and 1999;
-CREATE INDEX ON idr.claim_item_institutional_ss(clm_uniq_id, bfd_updated_ts);
