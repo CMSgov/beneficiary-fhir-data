118bcb501da0d286b39558a4d335312fc9c4df8d
marshallweekley-eng <marshallweekley@navapbc.com>
2026-07-27 11:20:17 -0500

    Update 03 services

--- ops/services/04-ccw-pipeline/iam.tf
+++ ops/services/04-ccw-pipeline/iam.tf
@@ -166,7 +166,7 @@ data "aws_iam_policy_document" "ccw_execution_logs" {
   statement {
     sid       = "AllowLogStreamControl"
     actions   = ["logs:CreateLogStream", "logs:PutLogEvents"]
-    resources = ["${aws_cloudwatch_log_group.ccw_messages.arn}:*"]
+    resources = ["${module.ccw_messages.arn}:*"]
   }
 }
 
