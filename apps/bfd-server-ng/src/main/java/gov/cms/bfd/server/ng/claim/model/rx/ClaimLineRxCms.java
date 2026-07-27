8e6f5bdd5bd86cde34a7ad1dedd3c5a01cab40c3
clewellyn-nava <connorlewellyn@navapbc.com>
2026-07-27 14:03:03 -0400

    Fixing duplicated column

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/rx/ClaimLineRxCms.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/rx/ClaimLineRxCms.java
@@ -3,9 +3,14 @@ package gov.cms.bfd.server.ng.claim.model.rx;
 import gov.cms.bfd.server.ng.ClaimFilterOptions;
 import gov.cms.bfd.server.ng.claim.model.common.ClaimLineBase;
 import gov.cms.bfd.server.ng.claim.model.common.RenderingCareTeamLine;
+import gov.cms.bfd.server.ng.claim.model.common.SupportingInfoFactory;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Embedded;
+
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
+
 import lombok.Getter;
 import org.hl7.fhir.r4.model.ExplanationOfBenefit;
 
@@ -16,8 +21,16 @@ public class ClaimLineRxCms implements ClaimLineBase {
 
   @Embedded private ClaimLineRx claimLineBase; // Composition over inheritance :nod:
   @Embedded private ClaimLineAdjudicationChargeRx adjudicationCharge;
-  @Embedded private ClaimLineRxSupportingInfo claimRxSupportingInfo;
   @Embedded private ClaimLineRxSupportingInfoCms claimRxSupportingInfoCms;
+
+  @Override
+  public List<ExplanationOfBenefit.SupportingInformationComponent> toFhirSupportingInfo(
+          SupportingInfoFactory supportingInfoFactory) {
+    return Stream.concat(
+                    claimLineBase.getSupportingInfo().toFhir(supportingInfoFactory).stream(),
+                    claimRxSupportingInfoCms.toFhir(supportingInfoFactory).stream())
+            .toList();
+  }
 
   @Override
   public Optional<ExplanationOfBenefit.ItemComponent> toFhirItemComponent(
