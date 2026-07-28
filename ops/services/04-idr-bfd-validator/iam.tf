9798d8f83ed04680a6e4c2d4cae1b9da4b234b52
Mitch Alessio <5306896+malessi@users.noreply.github.com>
2026-07-28 16:25:30 -0400

    Init 04-idr-bfd-validator

---
+++ ops/services/04-idr-bfd-validator/iam.tf
@@ -0,0 +1,237 @@
+data "aws_iam_policy_document" "service_assume_role" {
+  for_each = toset(["ecs-tasks", "scheduler"])
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["${each.value}.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "kms" {
+  statement {
+    sid = "AllowEnvCMKAccess"
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey",
+      "kms:ReEncrypt",
+      "kms:DescribeKey",
+      "kms:Encrypt"
+    ]
+    resources = [local.env_key_arn]
+  }
+}
+
+resource "aws_iam_policy" "kms" {
+  name        = "${local.name_prefix}-kms-policy"
+  path        = local.iam_path
+  description = "Permissions for the ${local.env} ${local.service} ECS task containers to use the ${local.env} CMK"
+  policy      = data.aws_iam_policy_document.kms.json
+}
+
+data "aws_iam_policy_document" "ssm_params" {
+  statement {
+    sid = "AllowGetPipelineAndCommonParameters"
+    actions = [
+      "ssm:GetParametersByPath",
+      "ssm:GetParameters",
+      "ssm:GetParameter"
+    ]
+    resources = [
+      for hierarchy in local.ssm_hierarchies
+      : "arn:aws:ssm:${local.region}:${local.account_id}:parameter/${trim(hierarchy, "/")}/*"
+    ]
+  }
+
+  statement {
+    sid       = "AllowDecryptParametersWithEnvCMK"
+    actions   = ["kms:Decrypt"]
+    resources = [local.env_key_arn]
+  }
+}
+
+resource "aws_iam_policy" "ssm_params" {
+  name        = "${local.name_prefix}-ssm-params-policy"
+  path        = local.iam_path
+  description = "Permissions for the ${local.env} ${local.service} ECS task containers to get required SSM parameeters"
+  policy      = data.aws_iam_policy_document.ssm_params.json
+}
+
+data "aws_iam_policy_document" "rds" {
+  statement {
+    sid       = "AllowDescribeRDSCluster"
+    actions   = ["rds:DescribeDBClusters"]
+    resources = [data.aws_rds_cluster.main.arn]
+  }
+}
+
+resource "aws_iam_policy" "rds" {
+  name        = "${local.name_prefix}-rds-policy"
+  path        = local.iam_path
+  description = "Permissions for the ${local.env} ${local.service} ECS task containers to describe RDS Cluster"
+  policy      = data.aws_iam_policy_document.rds.json
+}
+
+resource "aws_iam_role" "task" {
+  name                  = "${local.name_prefix}-task-role"
+  path                  = local.iam_path
+  description           = "Role for the ${local.env} ${local.service} ECS task containers"
+  assume_role_policy    = data.aws_iam_policy_document.service_assume_role["ecs-tasks"].json
+  permissions_boundary  = local.permissions_boundary_arn
+  force_detach_policies = true
+}
+
+resource "aws_iam_role_policy_attachment" "task" {
+  for_each = {
+    kms        = aws_iam_policy.kms.arn
+    rds        = aws_iam_policy.rds.arn
+    ssm_params = aws_iam_policy.ssm_params.arn
+  }
+
+  role       = aws_iam_role.task.name
+  policy_arn = each.value
+}
+
+data "aws_iam_policy_document" "execution_ecr" {
+  statement {
+    sid = "AllowGetAuthTokenECR"
+    actions = [
+      "ecr:GetAuthorizationToken",
+    ]
+    # This particular action has no conditions or resource types that can constrain access.
+    # See https://docs.aws.amazon.com/service-authorization/latest/reference/list_amazonelasticcontainerregistry.html
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "AllowPullFromECRRepos"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+    ]
+    resources = [
+      data.aws_ecr_repository.this.arn,
+      "arn:aws:ecr:us-east-1:593207742271:repository/aws-guardduty-agent-fargate",
+      "arn:aws:ecr:us-west-2:733349766148:repository/aws-guardduty-agent-fargate"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "execution_ecr" {
+  name        = "${local.name_prefix}-execution-ecr-policy"
+  path        = local.iam_path
+  description = "Grants permissions for the ${local.service} Execution Role to pull images from the ${data.aws_ecr_repository.this.name} ECR repository"
+  policy      = data.aws_iam_policy_document.execution_ecr.json
+}
+
+data "aws_iam_policy_document" "execution_logs" {
+  statement {
+    sid       = "AllowLogStreamControl"
+    actions   = ["logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = ["${aws_cloudwatch_log_group.messages.arn}:*"]
+  }
+}
+
+resource "aws_iam_policy" "execution_logs" {
+  name        = "${local.name_prefix}-execution-logs-policy"
+  path        = local.iam_path
+  description = "Grants permissions for the ${local.service} Execution Role to write to its corresponding CloudWatch Log Group and Log Streams"
+  policy      = data.aws_iam_policy_document.execution_logs.json
+}
+
+data "aws_iam_policy_document" "execution_ssm_params" {
+  statement {
+    sid       = "AllowGetPipelineTaskSecretsFromSSM"
+    actions   = ["ssm:GetParameter", "ssm:GetParameters"]
+    resources = values(local.task_ssm)
+  }
+
+  statement {
+    sid       = "AllowDecryptParametersWithEnvCMK"
+    actions   = ["kms:Decrypt"]
+    resources = [local.env_key_arn]
+  }
+}
+
+resource "aws_iam_policy" "execution_ssm_params" {
+  name        = "${local.name_prefix}-execution-ssm-params-policy"
+  path        = local.iam_path
+  description = "Permissions for the ${local.env} ${local.service} ECS task executor to get required SSM parameeters"
+  policy      = data.aws_iam_policy_document.execution_ssm_params.json
+}
+
+resource "aws_iam_role" "execution" {
+  name                  = "${local.name_prefix}-execution-role"
+  path                  = local.iam_path
+  description           = "${local.env} ${local.service} ECS task execution role"
+  assume_role_policy    = data.aws_iam_policy_document.service_assume_role["ecs-tasks"].json
+  permissions_boundary  = local.permissions_boundary_arn
+  force_detach_policies = true
+}
+
+resource "aws_iam_role_policy_attachment" "execution" {
+  for_each = {
+    ecr        = aws_iam_policy.execution_ecr.arn
+    logs       = aws_iam_policy.execution_logs.arn
+    ssm_params = aws_iam_policy.execution_ssm_params.arn
+  }
+
+  role       = aws_iam_role.execution.name
+  policy_arn = each.value
+}
+
+data "aws_iam_policy_document" "schedule_ecs" {
+  statement {
+    sid       = "AllowRunEcsTask"
+    actions   = ["ecs:RunTask"]
+    resources = [aws_ecs_task_definition.this.arn]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "ecs:cluster"
+      values   = [data.aws_ecs_cluster.main.arn]
+    }
+  }
+}
+
+resource "aws_iam_policy" "schedule_ecs" {
+  name        = "${local.name_prefix}-schedule-ecs-policy"
+  path        = local.iam_path
+  description = "Grants permissions for the ${local.service} Schedule to run ${local.service} ECS Tasks"
+  policy      = data.aws_iam_policy_document.schedule_ecs.json
+}
+
+data "aws_iam_policy_document" "schedule_iam" {
+  statement {
+    sid       = "AllowPassRole"
+    actions   = ["iam:PassRole"]
+    resources = [aws_iam_role.schedule.arn, aws_iam_role.execution.arn]
+  }
+}
+
+resource "aws_iam_policy" "schedule_iam" {
+  name   = "${local.name_prefix}-schedule-iam-policy"
+  path   = local.iam_path
+  policy = data.aws_iam_policy_document.schedule_iam.json
+}
+
+resource "aws_iam_role" "schedule" {
+  name                  = "${local.name_prefix}-schedule-role"
+  path                  = local.iam_path
+  description           = "${local.env} ${local.service} Scheduler Role"
+  assume_role_policy    = data.aws_iam_policy_document.service_assume_role["scheduler"].json
+  permissions_boundary  = local.permissions_boundary_arn
+  force_detach_policies = true
+}
+
+resource "aws_iam_role_policy_attachment" "schedule" {
+  for_each = {
+    ecs = aws_iam_policy.schedule_ecs.arn
+    iam = aws_iam_policy.schedule_iam.arn
+  }
+
+  role       = aws_iam_role.schedule.name
+  policy_arn = each.value
+}
---
+++ ops/services/04-idr-bfd-validator/main.tf
@@ -0,0 +1,247 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.52.0" # TODO: Replace with "~> 6" when ECS is fixed
+    }
+  }
+}
+
+module "terraservice" {
+  source = "../../terraform-modules/bfd/bfd-terraservice"
+
+  service              = local.service
+  relative_module_root = "ops/services/04-idr-bfd-validator"
+  subnet_layers        = ["private"]
+}
+
+locals {
+  service          = "idr-bfd-validator"
+  pipeline_service = "idr-pipeline"
+
+  region                   = module.terraservice.region
+  account_id               = module.terraservice.account_id
+  default_tags             = module.terraservice.default_tags
+  env                      = module.terraservice.env
+  is_ephemeral_env         = module.terraservice.is_ephemeral_env
+  bfd_version              = module.terraservice.bfd_version
+  ssm_config               = module.terraservice.ssm_config
+  env_key_arn              = module.terraservice.env_key_arn
+  iam_path                 = module.terraservice.default_iam_path
+  permissions_boundary_arn = module.terraservice.default_permissions_boundary_arn
+  vpc                      = module.terraservice.vpc
+  private_subnets          = module.terraservice.subnets_map["private"]
+
+  name_prefix = "bfd-${local.env}-${local.service}"
+
+  ssm_hierarchies = [
+    "/bfd/${local.env}/${local.service}/sensitive/",
+    "/bfd/${local.env}/${local.service}/nonsensitive/",
+    "/bfd/${local.env}/common/nonsensitive/",
+  ]
+
+  container_repository_name = coalesce(var.container_repository_override, "bfd-platform-${local.service}")
+  container_version         = coalesce(var.container_version_override, local.bfd_version)
+
+  db_environment        = coalesce(var.db_environment_override, local.env)
+  db_cluster_identifier = "bfd-${local.db_environment}-aurora-cluster"
+
+  # Unlike other services with ECS Tasks, this Service creates Tasks that are never going to need
+  # different resource limits, so we can avoid creating invariant/extraneous SSM configuration for
+  # such values
+  cpu       = 2048
+  memory    = 4096
+  disk_size = 21
+  task_ssm = {
+    for k, v in {
+      IDR_USERNAME    = "/bfd/${local.env}/${local.pipeline_service}/sensitive/idr_username"
+      IDR_PRIVATE_KEY = "/bfd/${local.env}/${local.pipeline_service}/sensitive/idr_private_key"
+      IDR_ACCOUNT     = "/bfd/${local.env}/${local.pipeline_service}/sensitive/idr_account"
+      IDR_WAREHOUSE   = "/bfd/${local.env}/${local.pipeline_service}/sensitive/idr_warehouse"
+      IDR_DATABASE    = "/bfd/${local.env}/${local.pipeline_service}/sensitive/idr_database"
+      IDR_SCHEMA      = "/bfd/${local.env}/${local.pipeline_service}/sensitive/idr_schema"
+      BFD_DB_USERNAME = "/bfd/${local.env}/${local.pipeline_service}/sensitive/db/username"
+      BFD_DB_PASSWORD = "/bfd/${local.env}/${local.pipeline_service}/sensitive/db/password"
+    } : k => "arn:aws:ssm:${local.region}:${local.account_id}:parameter/${trim(v, "/")}"
+  }
+  task_tmp_dir = "/app/.tmp"
+}
+
+resource "aws_cloudwatch_log_group" "messages" {
+  name         = "/aws/ecs/${data.aws_ecs_cluster.main.cluster_name}/${local.service}/${local.service}/messages"
+  kms_key_id   = local.env_key_arn
+  skip_destroy = !local.is_ephemeral_env
+}
+
+resource "aws_security_group" "this" {
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  name_prefix            = "${local.name_prefix}-sg"
+  description            = "Allow ${local.service} egress anywhere"
+  vpc_id                 = local.vpc.id
+  tags                   = { Name = "${local.name_prefix}-sg" }
+  revoke_rules_on_delete = true
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_db_access" {
+  security_group_id            = data.aws_security_group.aurora_cluster.id
+  referenced_security_group_id = aws_security_group.this.id
+  from_port                    = 5432
+  to_port                      = 5432
+  ip_protocol                  = "TCP"
+  description                  = "Grants ${local.env} ${local.service} ECS task containers access to the ${local.env} database"
+}
+
+resource "aws_ecs_task_definition" "this" {
+  family                   = local.name_prefix
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.execution.arn
+  task_role_arn            = aws_iam_role.task.arn
+  track_latest             = false
+
+  network_mode = "awsvpc"
+  cpu          = local.cpu
+  memory       = local.memory
+
+  ephemeral_storage {
+    size_in_gib = local.disk_size
+  }
+
+  runtime_platform {
+    cpu_architecture        = "ARM64"
+    operating_system_family = "LINUX"
+  }
+
+  tags = {
+    "${local.service}.version" = local.container_version
+  }
+
+  container_definitions = jsonencode(
+    [
+      {
+        name      = local.service
+        image     = data.aws_ecr_image.this.image_uri
+        essential = true
+        cpu       = 0
+        secrets = [
+          for k, v in local.task_ssm :
+          {
+            name      = k
+            valueFrom = v
+          }
+        ]
+        environment = [
+          {
+            name  = "TZ"
+            value = "UTC"
+          },
+          {
+            name  = "TMPDIR",
+            value = local.task_tmp_dir
+          },
+          {
+            name  = "BFD_ENV"
+            value = local.env
+          },
+          {
+            name  = "BFD_DB_ENDPOINT"
+            value = data.aws_rds_cluster.main.reader_endpoint
+          },
+          # TODO: Uncommment this after BFD-4796 resolves known inconsistencies
+          # {
+          #   name  = "ALERT_SNS_TOPIC_ARN",
+          #   value = data.aws_sns_topic.slack.arn
+          # }
+        ]
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = aws_cloudwatch_log_group.messages.name
+            awslogs-stream-prefix = "messages"
+            awslogs-region        = local.region
+            max-buffer-size       = "25m"
+            mode                  = "non-blocking"
+          }
+        }
+        stopTimeout = 120
+        linuxParameters = {
+          tmpfs = [
+            {
+              containerPath = "/app/.cache"
+              size          = min(max(128, floor(0.025 * local.memory)), 256) # Min 128 MiB/max 256 MiB
+              mountOptions = [
+                "uid=1001",
+                "gid=1001"
+              ]
+            },
+            {
+              containerPath = local.task_tmp_dir
+              size          = 32 # 32 MiB
+              mountOptions = [
+                "uid=1001",
+                "gid=1001"
+              ]
+            }
+          ]
+        }
+        mountPoints            = []
+        readonlyRootFilesystem = true
+        # Empty declarations reduce Terraform diff noise
+        portMappings   = []
+        systemControls = []
+        volumesFrom    = []
+      }
+    ]
+  )
+}
+
+resource "aws_scheduler_schedule_group" "this" {
+  name = "${local.name_prefix}-schedules"
+}
+
+resource "aws_scheduler_schedule" "this" {
+  depends_on = [aws_iam_role_policy_attachment.schedule]
+
+  name       = "${local.name_prefix}-every-1-hour"
+  group_name = aws_scheduler_schedule_group.this.name
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "cron(0 * ? * * *)"
+
+  target {
+    arn      = data.aws_ecs_cluster.main.arn
+    role_arn = aws_iam_role.schedule.arn
+
+    ecs_parameters {
+      task_definition_arn     = aws_ecs_task_definition.this.arn
+      task_count              = 1
+      group                   = local.service
+      enable_ecs_managed_tags = true
+      propagate_tags          = "TASK_DEFINITION"
+      platform_version        = "LATEST"
+
+      capacity_provider_strategy {
+        capacity_provider = "FARGATE"
+        base              = 0
+        weight            = 100
+      }
+
+      network_configuration {
+        assign_public_ip = false
+        security_groups  = [aws_security_group.this.id]
+        subnets          = local.private_subnets
+      }
+    }
+  }
+}
