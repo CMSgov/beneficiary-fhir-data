4cb57fe9e2c7b5fb4d25aa84abfe21814fc99b95
Mitch Alessio <5306896+malessi@users.noreply.github.com>
2026-08-04 18:48:21 -0400

    Unnest intermediate ExceptionGroup in __init__; don't re-raise unnecessarily

--- apps/bfd-pipeline-idr/src/idr_pipeline/__init__.py
+++ apps/bfd-pipeline-idr/src/idr_pipeline/__init__.py
@@ -126,13 +126,17 @@ def run(source: Source, load_mode: LoadMode, load_type: LoadType) -> None:
 
     async def run_worker_and_stages() -> None:
         stop_worker = anyio.Event()
-        async with anyio.create_task_group() as tg:
-            await tg.start(worker_manager.start, stop_worker)
-            # We don't submit staged_pipeline.start to the task group because we want to set the
-            # stop signal for the background worker once it's complete. If we don't do it this way
-            # the pipeline process will run forever
-            await staged_pipeline.start()
-            stop_worker.set()
+        try:
+            async with anyio.create_task_group() as tg:
+                await tg.start(worker_manager.start, stop_worker)
+                # We don't submit staged_pipeline.start to the task group because we want to set the
+                # stop signal for the background worker once it's complete. If we don't do it this
+                # way the pipeline process will run forever
+                await staged_pipeline.start()
+                stop_worker.set()
+        except* Exception as ex_group:
+            for ex in ex_group.exceptions:
+                raise ex from ex.__cause__
 
     try:
         anyio.run(run_worker_and_stages)
@@ -149,7 +153,6 @@ def run(source: Source, load_mode: LoadMode, load_type: LoadType) -> None:
                 failure_time=resolve_test_date(load_mode),
             )
         logger.opt(exception=True).error("Unrecoverable exception raised during pipeline load:")
-        raise
     finally:
         if idr_job_events:
             update_completion_times(
