6172a62c95a217517d50a626348c3a2adbbd801c
clewellyn-nava <connorlewellyn@navapbc.com>
2026-08-17 12:09:36 -0400

    changes to sortingnodefactory

--- apps/bfd-server-ng/src/test/java/gov/cms/bfd/server/ng/testUtil/SortingNodeFactory.java
+++ apps/bfd-server-ng/src/test/java/gov/cms/bfd/server/ng/testUtil/SortingNodeFactory.java
@@ -1,7 +1,13 @@
 package gov.cms.bfd.server.ng.testUtil;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
 import java.util.TreeMap;
 
 /**
@@ -10,8 +16,38 @@ import java.util.TreeMap;
  * order rather than insertion order.
  */
 public class SortingNodeFactory extends JsonNodeFactory {
+
+  private static final Set<String> COMPONENT_ARRAYS = Set.of("supportingInfo", "careTeam", "procedure", "diagnosis", "item", "extension");
+
   @Override
   public ObjectNode objectNode() {
-    return new ObjectNode(this, new TreeMap<>());
+    return new SortingObjectNode(this);
+  }
+
+  private static class SortingObjectNode extends ObjectNode {
+    SortingObjectNode(JsonNodeFactory nc) {
+      super(nc, new TreeMap<>());
+    }
+
+    @Override
+    public <T extends JsonNode> T set(String fieldName, JsonNode value) {
+      // only sort the lists of components we know / care about across all FHIR resources
+      if (COMPONENT_ARRAYS.contains(fieldName) && value instanceof ArrayNode) {
+        sortArray((ArrayNode) value);
+      }
+      return super.set(fieldName, value);
+    }
+
+    private void sortArray(ArrayNode array) {
+      List<JsonNode> elements = new ArrayList<>();
+      for (JsonNode n : array) {
+        elements.add(n);
+      }
+      elements.sort(Comparator.comparing(JsonNode::toString));
+      array.removeAll();
+      for (JsonNode n : elements) {
+        array.add(n);
+      }
+    }
   }
 }
