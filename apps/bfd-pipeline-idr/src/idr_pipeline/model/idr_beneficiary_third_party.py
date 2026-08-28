59a51f8bd0731fb45788b8f5fd52eae19397a6f0
Melissa Guzman <melissa.guzman@forpeople.us>
2026-08-27 17:54:27 -0700

    move table names from constants to settings and prefix them with their db

--- apps/bfd-pipeline-idr/src/idr_pipeline/model/idr_beneficiary_third_party.py
+++ apps/bfd-pipeline-idr/src/idr_pipeline/model/idr_beneficiary_third_party.py
@@ -3,7 +3,6 @@ from typing import Annotated, override
 
 from pydantic import BeforeValidator
 
-from ..constants import IDR_BENE_THIRD_PARTY_TABLE
 from ..load_partition import LoadPartition
 from ..model.base_model import (
     ALIAS_HSTRY,
@@ -19,6 +18,7 @@ from ..model.base_model import (
     transform_default_string,
     transform_null_date_to_min,
 )
+from ..settings import SETTINGS
 
 
 class IdrBeneficiaryThirdParty(IdrBaseModel):
@@ -59,7 +59,7 @@ class IdrBeneficiaryThirdParty(IdrBaseModel):
         hstry = ALIAS_HSTRY
         return f"""
             SELECT {{COLUMNS}}
-            FROM {IDR_BENE_THIRD_PARTY_TABLE} tp {{TABLESAMPLE}}
+            FROM {SETTINGS.idr_bene_third_party_table} tp {{TABLESAMPLE}}
             {{WHERE_CLAUSE}}
             AND NOT EXISTS (
                 {deceased_bene_filter(hstry, start_time)}
