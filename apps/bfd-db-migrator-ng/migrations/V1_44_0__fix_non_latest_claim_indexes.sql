b2364be0a019a58f5c7812f269e896f93c63fd11
Austin Schey <austin.schey@forpeople.us>
2026-08-28 15:39:12 -0700

    fix non-latest claim indexes

---
+++ apps/bfd-db-migrator-ng/migrations/V1_44_0__fix_non_latest_claim_indexes.sql
@@ -0,0 +1,9 @@
+DROP INDEX claim_professional_nch_clm_ltst_clm_ind_idx;
+DROP INDEX claim_professional_ss_clm_ltst_clm_ind_idx;
+DROP INDEX claim_institutional_nch_clm_ltst_clm_ind_idx;
+DROP INDEX claim_institutional_ss_clm_ltst_clm_ind_idx;
+
+CREATE INDEX ON idr.claim_professional_nch(clm_uniq_id, clm_ltst_clm_ind) WHERE clm_ltst_clm_ind = 'N';
+CREATE INDEX ON idr.claim_professional_ss(clm_uniq_id, clm_ltst_clm_ind) WHERE clm_ltst_clm_ind = 'N';
+CREATE INDEX ON idr.claim_institutional_nch(clm_uniq_id, clm_ltst_clm_ind) WHERE clm_ltst_clm_ind = 'N';
+CREATE INDEX ON idr.claim_institutional_ss(clm_uniq_id, clm_ltst_clm_ind) WHERE clm_ltst_clm_ind = 'N';
