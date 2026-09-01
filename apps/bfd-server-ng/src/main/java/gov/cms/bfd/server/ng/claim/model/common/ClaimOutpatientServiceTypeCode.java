0662192b81728569254c0159a6ea4486cda5725a
Josh Fisk <joshuafisk@navapbc.com>
2026-09-01 12:48:00 -0700

    Updating SRVC_TYPE_CD display values

--- apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/common/ClaimOutpatientServiceTypeCode.java
+++ apps/bfd-server-ng/src/main/java/gov/cms/bfd/server/ng/claim/model/common/ClaimOutpatientServiceTypeCode.java
@@ -69,24 +69,49 @@ public sealed interface ClaimOutpatientServiceTypeCode
   @Getter
   @SuppressWarnings("java:S1192")
   enum Valid implements ClaimOutpatientServiceTypeCode {
-    /** 0 - Blank. */
-    _0("0", "Blank"),
-    /** 1 - Emergency. */
-    _1("1", "Emergency"),
-    /** 2 - Urgent. */
-    _2("2", "Urgent"),
-    /** 3 - Elective. */
-    _3("3", "Elective"),
-    /** 5 - Reserved. */
-    _5("5", "Reserved"),
+    /** 0 - Unknown Value (but present in data). */
+    _0("0", "Unknown Value (but present in data)"),
+    /**
+     * 1 - Emergency - The patient required immediate medical intervention as a result of severe,
+     * life threatening, or potentially disabling conditions. Generally, the patient was admitted
+     * through the emergency room.
+     */
+    _1(
+        "1",
+        "Emergency - The patient required immediate medical intervention as a result of severe, life threatening, or potentially disabling conditions. Generally, the patient was admitted through the emergency room."),
+    /**
+     * 2 - Urgent - The patient required immediate attention for the care and treatment of a
+     * physical or mental disorder. Generally, the patient was admitted to the first available and
+     * suitable accommodation.
+     */
+    _2(
+        "2",
+        "Urgent - The patient required immediate attention for the care and treatment of a physical or mental disorder. Generally, the patient was admitted to the first available and suitable accommodation."),
+    /**
+     * 3 - Elective - The patient's condition permitted adequate time to schedule the availability
+     * of suitable accommodations.
+     */
+    _3(
+        "3",
+        "Elective - The patient's condition permitted adequate time to schedule the availability of suitable accommodations."),
+    /** 4 - Newborn - Necessitates the use of special source of admission codes. */
+    _4("4", "Newborn - Necessitates the use of special source of admission codes."),
+    /**
+     * 5 - Trauma Center - visits to a trauma center/hospital as licensed or designated by the State
+     * or local government authority authorized to do so, or as verified by the American College of
+     * Surgeons and involving a trauma activation.
+     */
+    _5(
+        "5",
+        "Trauma Center - visits to a trauma center/hospital as licensed or designated by the State or local government authority authorized to do so, or as verified by the American College of Surgeons and involving a trauma activation."),
     /** 6 - Reserved. */
     _6("6", "Reserved"),
     /** 7 - Reserved. */
     _7("7", "Reserved"),
     /** 8 - Reserved. */
     _8("8", "Reserved"),
-    /** 9 - Unknown (Information not available). */
-    _9("9", "Unknown (Information not available)");
+    /** 9 - Unknown - Information not available. */
+    _9("9", "Unknown - Information not available.");
 
     private final String code;
     private final String display;
