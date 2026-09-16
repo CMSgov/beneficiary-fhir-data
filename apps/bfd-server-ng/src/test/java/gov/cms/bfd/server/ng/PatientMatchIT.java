47898c44eb19b926480bb2452629ce15385262ad
marshallweekley-eng <marshallweekley@navapbc.com>
2026-09-16 11:46:35 -0500

    format update

--- apps/bfd-server-ng/src/test/java/gov/cms/bfd/server/ng/PatientMatchIT.java
+++ apps/bfd-server-ng/src/test/java/gov/cms/bfd/server/ng/PatientMatchIT.java
@@ -730,10 +730,7 @@ class PatientMatchIT extends IntegrationTestBase {
   }
 
   @ParameterizedTest
-  @ValueSource(strings = {
-          "999999999999-20260101000000000",
-          "-731271436-20260914164040787646024"
-  })
+  @ValueSource(strings = {"999999999999-20260101000000000", "-731271436-20260914164040787646024"})
   void testAuditEventByIdNotFound(String badId) {
     var res =
         getFhirClient().read().resource(AuditEvent.class).withId("999999999999-20260101000000000");
