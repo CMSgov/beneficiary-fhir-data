ead46d84ccbd19883132958fbb87d7e2208e9372
Austin Schey <austin.schey@forpeople.us>
2026-09-16 10:59:00 -0700

    add bootstrap scripts

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+brew install just
+just bootstrap
--- hooks/java-format.sh
+++ hooks/java-format.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-java -jar "$(git rev-parse --show-toplevel)/hooks/google-java-format-1.36.1-all-deps.jar" -r --skip-reflowing-long-strings "$@"
+GOOGLE_JAVA_FORMAT_VERSION="1.36.1"
+jar_path="$(git rev-parse --show-toplevel)/hooks/google-java-format.jar"
+
+if [ ! -f "$jar_path" ]; then
+	curl -L "https://github.com/google/google-java-format/releases/download/v${GOOGLE_JAVA_FORMAT_VERSION}/google-java-format-${GOOGLE_JAVA_FORMAT_VERSION}-all-deps.jar" >"$jar_path"
+fi
+
+java -jar "$jar_path" -r --skip-reflowing-long-strings "$@"
