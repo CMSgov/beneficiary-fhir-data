f5134ccb341c0e080a8b208977c8060664e23c55
marshallweekley-eng <marshallweekley@navapbc.com>
2026-09-16 09:24:04 -0500

    update for pipelines and tests for bfd-model-idr

--- apps/bfd-model-idr/src/generate_prior_auth_sample/test_generate_prior_auth_sample.py
+++ apps/bfd-model-idr/src/generate_prior_auth_sample/test_generate_prior_auth_sample.py
@@ -5,14 +5,14 @@ from pathlib import Path
 def test_generate_prior_auth_sample_runs_successfully() -> None:
     result = subprocess.run(
         [
-            "uv", 
-            "run", 
+            "uv",
+            "run",
             "generate-prior-auth-sample",
             "--utn",
             "-X3QFSBY07ICRD",
             "--output-directory",
             "./out-test",
-             "--source-directory",
+            "--source-directory",
             "../bfd-pipeline-idr/test_samples1",
         ],
         cwd=Path(__file__).parent.parent.parent,
